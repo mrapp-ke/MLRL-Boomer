e77557ee87d503af7f04325f5b89c878d39ad23f
Michael Rapp <michael.rapp90@googlemail.com>
2019-10-18 00:45:16 +0200

    Fix dependency cycle.

--- python/boomer/algorithm/boomer.py
+++ python/boomer/algorithm/boomer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 """
 @author: Michael Rapp (mrapp@ke.tu-darmstadt.de)
 
@@ -13,59 +12,12 @@ import numpy as np
 from sklearn.utils.validation import check_is_fitted
 
 from boomer.algorithm.model import Theory
+from boomer.algorithm.modules import RuleInduction, Prediction
 from boomer.algorithm.persistence import ModelPersistence
 from boomer.algorithm.prediction import LinearCombination
 from boomer.algorithm.rule_induction import GradientBoosting
 from boomer.algorithm.stats import Stats
 from boomer.learners import MLLearner
-
-
-class Module:
-    """
-    A base class for all modules, the "Boomer" algorithm consists of.
-
-    Attributes
-        random_state   The seed to be used by RNGs
-    """
-
-    random_state: int = 0
-
-
-class RuleInduction(Module):
-    """
-    A module that allows to induce a `Theory`, consisting of several classification rules.
-    """
-
-    def induce_rules(self, stats: Stats, x: np.ndarray, y: np.ndarray) -> Theory:
-        """
-        Creates and returns a 'Theory' that contains several candidate rules.
-
-        :param stats:   Statistics about the training data set
-        :param x:       An array of dtype float, shape `(num_examples, num_features)`, representing the features of the
-                        training examples
-        :param y:       An array of dtype float, shape `(num_examples, num_labels)`, representing the labels of the
-                        training examples
-        :return:        A 'Theory' that contains the generated candidate rules
-        """
-        pass
-
-
-class Prediction(Module):
-    """
-    A module that allows to make predictions using a 'Theory'.
-    """
-
-    def predict(self, stats: Stats, theory: Theory, x: np.ndarray) -> np.ndarray:
-        """
-        Predicts the labels of examples using a specific theory.
-
-        :param stats:   Statistics about the training data set
-        :param theory:  The theory that is used to make predictions
-        :param x:       An array of dtype float, shape `(num_examples, num_features)`, representing the features of the
-                        examples to be classified
-        :return:        An array of dtype float, shape `(num_examples, num_labels)', representing the predicted labels
-        """
-        pass
 
 
 class Boomer(MLLearner):
