796a6ffc843b21951f12752ac3a94bebf433da64
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2019-10-17 16:16:03 +0200

    Add file stats.py.

--- python/boomer/algorithm/model.py
+++ python/boomer/algorithm/model.py
@@ -3,7 +3,7 @@
 """
 @author: Michael Rapp (mrapp@ke.tu-darmstadt.de)
 
-Provides model classes, e.g., for representing rules.
+Provides classes for representing the model learned by a classifier or ranker.
 """
 from typing import List
 
@@ -96,38 +96,3 @@ class Rule:
         """
         self.body = body
         self.head = head
-
-
-class Stats:
-    """
-    Stores useful information about a multi-label training data set, such as the number of examples, features, and
-    labels.
-    """
-
-    def __init__(self, num_examples: int, num_features: int, num_labels: int):
-        """
-        :param num_examples:    The number of examples contained in the training data set
-        :param num_features:    The number of features contained in the training data set
-        :param num_labels:      The number of labels contained in the training data set
-        """
-        self.num_examples = num_examples
-        self.num_features = num_features
-        self.num_labels = num_labels
-
-    @staticmethod
-    def create_stats(x: np.ndarray, y: np.ndarray) -> 'Stats':
-        """
-        Creates 'Stats' storing information about a specific multi-label training data set.
-
-        :param x:   An array of dtype float, shape `(num_examples, num_features)`, representing the features of the
-                    training examples
-        :param y:   An array of dtype float, shape `(num_examples, num_labels)`, representing the labels of the training
-                    examples
-        :return:    'Stats' storing information about the given data set
-        """
-
-        x_shape = np.shape(x)
-        num_examples = x_shape[0]
-        num_features = x_shape[1]
-        num_labels = np.shape(y)[1]
-        return Stats(num_examples=num_examples, num_features=num_features, num_labels=num_labels)
