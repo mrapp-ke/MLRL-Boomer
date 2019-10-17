11a744c1db382d24dfb5a1bb55c65cef96160146
Michael Rapp <michael.rapp90@googlemail.com>
2019-10-18 00:39:29 +0200

    Implement induction of the default rule when using a decomposable loss function.

--- python/boomer/algorithm/rule_induction.py
+++ python/boomer/algorithm/rule_induction.py
@@ -10,8 +10,8 @@ import logging as log
 import numpy as np
 
 from boomer.algorithm.boomer import RuleInduction
-from boomer.algorithm.losses import Loss, SquaredErrorLoss
-from boomer.algorithm.model import Theory, Rule
+from boomer.algorithm.losses import Loss, DecomposableLoss, SquaredErrorLoss
+from boomer.algorithm.model import Theory, Rule, EmptyBody, Head
 from boomer.algorithm.stats import Stats
 
 
@@ -30,29 +30,62 @@ class GradientBoosting(RuleInduction):
 
     def induce_rules(self, stats: Stats, x: np.ndarray, y: np.ndarray) -> Theory:
         # Convert binary ground truth labeling into expected confidence scores {-1, 1}
-        ground_truth = np.where(np.nonzero(y), y, -1)
+        expected_scores = np.where(np.nonzero(y), y, -1)
+        # Initialize the confidence scores that are initially predicted for each example and label
+        predicted_scores = np.zeros(np.shape(expected_scores), dtype=float)
         theory = []
         t = 1
 
         if t <= self.num_rules:
             log.info('Learning rule %s / %s (default rule)...', t, self.num_rules)
-            theory.append(self.__induce_default_rule())
+            default_rule, predicted_scores = self.__induce_default_rule(expected_scores, predicted_scores)
+            theory.append(default_rule)
             t += 1
 
         while t <= self.num_rules:
             log.info('Learning rule %s / %s...', t, self.num_rules)
-            theory.append(self.__induce_rule())
+            # TODO theory.append(self.__induce_rule())
             t += 1
 
         return theory
 
-    def __induce_default_rule(self) -> Rule:
+    def __induce_default_rule(self, expected_scores: np.ndarray, predicted_scores: np.ndarray) -> (Rule, np.ndarray):
         """
         Induces the default rule.
 
-        :return: The induced default rule
+        :param expected_scores:     An array of dtype float, shape `(num_examples, num_labels)`, representing the
+                                    expected confidence scores according to the ground truth
+        :param predicted_scores:    An array of dtype float, shape `(num_examples, num_labels`, representing the
+                                    currently predicted confidence scores
+        :return:                    The induced default rule, as well as the confidence scores that are predicted after
+                                    including the rule in the theory
         """
-        pass
+
+        if self.loss is DecomposableLoss:
+            head, predicted_scores = self.__derive_full_head_using_decomposable_loss(expected_scores, predicted_scores)
+        else:
+            # TODO: Implement
+            pass
+
+        rule = Rule(EmptyBody(), head)
+        return rule, predicted_scores
+
+    def __derive_full_head_using_decomposable_loss(self, expected_scores: np.ndarray,
+                                                   predicted_scores: np.ndarray) -> (Head, np.ndarray):
+        """
+        Derives a full head, if the used loss function is decomposable.
+
+        :param expected_scores:     An array of dtype float, shape `(num_examples, num_labels)`, representing the
+                                    expected confidence scores according to the ground truth
+        :param predicted_scores:    An array of dtype float, shape `(num_examples, num_labels`, representing the
+                                    currently predicted confidence scores
+        :return:                    The derived head, as well as the confidence scores that predicted after applying the
+                                    head to the given predicted scores
+        """
+
+        head = self.loss.derive_scores(expected_scores, predicted_scores)
+        predicted_scores += head
+        return head, predicted_scores
 
     def __induce_rule(self) -> Rule:
         """
