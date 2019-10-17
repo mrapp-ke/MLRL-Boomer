20e900b67b8d22cb150f6c01ca3d808d9a15336b
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2019-10-17 18:15:40 +0200

    Add argument "loss" to class GradientBoosting.

--- python/boomer/algorithm/rule_induction.py
+++ python/boomer/algorithm/rule_induction.py
@@ -8,6 +8,7 @@ Provides class for inducing classification rules.
 import numpy as np
 
 from boomer.algorithm.boomer import RuleInduction
+from boomer.algorithm.losses import Loss, SquaredErrorLoss
 from boomer.algorithm.model import Theory
 from boomer.algorithm.stats import Stats
 
@@ -17,11 +18,13 @@ class GradientBoosting(RuleInduction):
     Implements the induction of (multi-label) classification rules using gradient boosting.
     """
 
-    def __init__(self, num_rules: int = 100):
+    def __init__(self, num_rules: int = 100, loss: Loss = SquaredErrorLoss()):
         """
         :param num_rules:   The number of rules to be induced (including the default rule)
+        :param loss:        The (surrogate) loss to be minimized
         """
         self.num_rules = num_rules
+        self.loss = loss
 
     def induce_rules(self, stats: Stats, x: np.ndarray, y: np.ndarray) -> Theory:
         pass
