4f828288ffeb25bfdcda9bb37a486b73902d3a40
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2019-10-17 16:01:05 +0200

    Add class Stats.

--- python/boomer/algorithm/model.py
+++ python/boomer/algorithm/model.py
@@ -96,3 +96,20 @@ class Rule:
         """
         self.body = body
         self.head = head
+
+
+class Stats:
+    """
+    Stores useful information about a multi-label training data set, such as the number of examples, features, and
+    labels.
+    """
+
+    def __init__(self, num_examples: int, num_features: int, num_labels: int):
+        """
+        :param num_examples:    The number of examples contained in the training data set
+        :param num_features:    The number of features contained in the training data set
+        :param num_labels:      The number of labels contained in the training data set
+        """
+        self.num_examples = num_examples
+        self.num_features = num_features
+        self.num_labels = num_labels
