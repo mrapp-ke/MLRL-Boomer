29b0844503f8fe26efe5a73c4d2ba1cbb5262d69
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2019-10-17 16:21:31 +0200

    Add file prediction.py.

---
+++ python/boomer/algorithm/prediction.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+"""
+@author: Michael Rapp (mrapp@ke.tu-darmstadt.de)
+
+Provides classes for making predictions based on rules.
+"""
+import numpy as np
+
+from boomer.algorithm.boomer import Prediction
+from boomer.algorithm.model import Theory, Body
+from boomer.algorithm.stats import Stats, get_num_examples
+
+
+class Bipartition(Prediction):
+    """
+    A base class for all subclasses of the class 'Prediction' that predict binary label vectors.
+    """
+
+    def predict(self, stats: Stats, theory: Theory, x: np.ndarray) -> np.ndarray:
+        pass
+
+
+class LinearCombination(Bipartition):
+    """
+    Predicts the linear combination of rules, i.e., the sum of the scores provided by all covering rules for each label.
+    """
+
+    def predict(self, stats: Stats, theory: Theory, x: np.ndarray) -> np.ndarray:
+        prediction = np.full((get_num_examples(x), stats.num_labels), 0, dtype=float)
+
+        for rule in theory:
+            mask = Body.match(rule.body, x)
+            prediction[mask] += rule.head
+
+        return prediction
