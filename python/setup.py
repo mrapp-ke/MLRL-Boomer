7a0c98457030496e87148b8dcd09f893d8627d10
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2019-10-17 14:19:03 +0200

    Add file setup.py.

---
+++ python/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='boomer',
+      version='0.1.0',
+      description='BOOMER - An algorithm for learning gradient boosted multi-label classification rules',
+      url='https://github.com/mrapp-ke/Boomer',
+      author='Michael Rapp',
+      author_email='mrapp@ke.tu-darmstadt.de',
+      license='MIT',
+      packages=['boomer'],
+      install_requires=[
+          'liac-arff',
+          'numpy',
+          'scikit-learn',
+          'scikit-multilearn',
+          'scipy',
+          'sklearn',
+          'requests'
+      ],
+      zip_safe=False)
