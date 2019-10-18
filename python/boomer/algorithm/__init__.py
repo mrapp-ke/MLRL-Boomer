ce23ab8b98dbd2f124436fdf05aa19b407e0fc71
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2019-10-18 15:24:28 +0200

    Rename file subsampling.py to sub_sampling.py.

--- python/boomer/algorithm/__init__.py
+++ python/boomer/algorithm/__init__.py
@@ -5,4 +5,4 @@ from boomer.algorithm import persistence
 from boomer.algorithm import prediction
 from boomer.algorithm import rule_induction
 from boomer.algorithm import stats
-from boomer.algorithm import subsampling
+from boomer.algorithm import sub_sampling
