3c0497ac5661bc7fd2716dc76523db134e0b7b74
Michael Rapp <michael.rapp90@googlemail.com>
2019-10-21 22:06:16 +0200

    Move functionality for loading data sets and meta data to file data.py.

--- python/boomer/data.py
+++ python/boomer/data.py
@@ -6,11 +6,14 @@
 Provides functions for handling multi-label data.
 """
 import logging as log
+import os.path as path
 from typing import List, Set
 from xml.dom import minidom
 
+import numpy as np
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
+from skmultilearn.dataset import load_from_arff
 
 
 class Attribute:
@@ -40,6 +43,84 @@ class NominalAttribute(Attribute):
         :param name:    The name of the attribute
         """
         super().__init__(name)
+
+
+class MetaData:
+
+    def __init__(self, num_labels: int, label_location: str, nominal_attributes: List[int]):
+        self.num_labels = num_labels
+        self.label_location = label_location
+        self.nominal_attributes = nominal_attributes
+
+
+def load_data_set_and_meta_data(data_dir: str, arff_file_name: str, xml_file_name: str) -> (np.ndarray, np.ndarray, MetaData):
+    """
+    Loads a multi-label data set from an ARFF file and the corresponding Mulan XML file..
+
+    :param data_dir:        The path of the directory that contains the files
+    :param arff_file_name:  The name of the ARFF file
+    :param xml_file_name:   The name of the XML file
+    :return:                An array of dtype float, shape `(num_examples, num_features)`, representing the features of
+                            the examples, an array of dtype float, shape `(num_examples, num_labels)`, representing the
+                            corresponding label vectors, as well as the data set's meta data
+    """
+
+    arff_file = path.join(data_dir, arff_file_name)
+    xml_file = path.join(data_dir, xml_file_name)
+    log.debug('Parsing meta data from file \"%s\"...', xml_file)
+    meta_data = __parse_meta_data(arff_file, xml_file)
+    x, y = load_data_set(data_dir, arff_file_name, meta_data)
+    return x, y, meta_data
+
+
+def load_data_set(data_dir: str, arff_file_name: str, meta_data: MetaData) -> (np.ndarray, np.ndarray):
+    """
+    Loads a multi-label data set from an ARFF file given its meta data.
+
+    :param data_dir:        The path of the directory that contains the ARFF file
+    :param arff_file_name:  The name of the ARFF file
+    :param meta_data:       The meta data
+    :return:                An array of dtype float, shape `(num_examples, num_features)`, representing the features of
+                            the examples, as well as an array of dtype float, shape `(num_examples, num_labels)`,
+                            representing the corresponding label vectors
+    """
+
+    arff_file = path.join(data_dir, arff_file_name)
+    log.debug('Loading data set from file \"%s\"...', arff_file)
+    x, y = load_from_arff(arff_file, label_count=meta_data.num_labels, label_location=meta_data.label_location)
+    return x, y
+
+
+def one_hot_encode(x, y, nominal_indices, encoder=None):
+    """
+    One-hot encodes the nominal attributes contained in a data set, if any.
+
+    :param x:               The features of the examples in the data set
+    :param y:               The labels of the examples in the data set
+    :param nominal_indices: A list containing the indices of all nominal attributes
+    :param encoder:         The 'OneHotEncoder' to be used or None, if a new encoder should be created
+    :return:                The encoded features of the given examples and the encoder that has been used
+    """
+
+    if len(nominal_indices) > 0:
+        x = x.toarray()
+        old_shape = x.shape
+
+        if encoder is None:
+            log.info('Applying one-hot encoding...')
+            encoder = ColumnTransformer(
+                [('one_hot_encoder', OneHotEncoder(handle_unknown='ignore', sparse=False), nominal_indices)],
+                remainder='passthrough')
+            encoder.fit(x, y)
+
+        x = encoder.transform(x)
+        new_shape = x.shape
+        log.info('Original data set contained %s attributes, one-hot encoded data set contains %s attributes',
+                 old_shape[1], new_shape[1])
+        return x, encoder
+    else:
+        log.debug('No need to apply one-hot encoding, as the data set does not contain any nominal attributes.')
+        return x, None
 
 
 def __parse_labels(metadata_file) -> Set[str]:
@@ -82,7 +163,7 @@ def __parse_attributes(arff_file) -> List[Attribute]:
     return attributes
 
 
-def parse_metadata(arff_file, metadata_file):
+def __parse_meta_data(arff_file, metadata_file) -> MetaData:
     """
     Parses meta data from an ARFF file and the corresponding Mulan XML file.
 
@@ -100,36 +181,4 @@ def parse_metadata(arff_file, metadata_file):
                        if isinstance(attribute, NominalAttribute) and attribute.name not in labels]
     log.info('Data set contains %s nominal and %s numerical attributes.', len(nominal_indices),
              (len(attributes) - len(nominal_indices)))
-    return len(labels), label_location, nominal_indices
-
-
-def one_hot_encode(x, y, nominal_indices, encoder=None):
-    """
-    One-hot encodes the nominal attributes contained in a data set, if any.
-
-    :param x:               The features of the examples in the data set
-    :param y:               The labels of the examples in the data set
-    :param nominal_indices: A list containing the indices of all nominal attributes
-    :param encoder:         The 'OneHotEncoder' to be used or None, if a new encoder should be created
-    :return:                The encoded features of the given examples and the encoder that has been used
-    """
-
-    if len(nominal_indices) > 0:
-        x = x.toarray()
-        old_shape = x.shape
-
-        if encoder is None:
-            log.info('Applying one-hot encoding...')
-            encoder = ColumnTransformer(
-                [('one_hot_encoder', OneHotEncoder(handle_unknown='ignore', sparse=False), nominal_indices)],
-                remainder='passthrough')
-            encoder.fit(x, y)
-
-        x = encoder.transform(x)
-        new_shape = x.shape
-        log.info('Original data set contained %s attributes, one-hot encoded data set contains %s attributes',
-                 old_shape[1], new_shape[1])
-        return x, encoder
-    else:
-        log.debug('No need to apply one-hot encoding, as the data set does not contain any nominal attributes.')
-        return x, None
+    return MetaData(len(labels), label_location, nominal_indices)
