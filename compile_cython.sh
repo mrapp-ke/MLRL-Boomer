5ca7ee737a6621568c29aac2d4210e69f6ccf2a0
Michael Rapp <michael.rapp90@googlemail.com>
2019-11-12 00:16:44 +0100

    Sub-sample features and/or instances if necessary.

--- compile_cython.sh
+++ compile_cython.sh
@@ -5,6 +5,7 @@ cd python/
 # Delete existing files
 find -type f -name "*.so" -delete
 find -type f -name "*.c" -delete
+find -type f -name "*.cpp" -delete
 
 # Compile .pyx files
 python setup.py build_ext --inplace
--- python/boomer/algorithm/_rule_induction.pyx
+++ python/boomer/algorithm/_rule_induction.pyx
@@ -1,7 +1,10 @@
-from boomer.algorithm._model cimport intp, uint8, float32, float64, Rule, FullHead, EmptyBody
-from boomer.algorithm._head_refinement cimport HeadRefinement
+from boomer.algorithm._model cimport intp, uint8, uint32, float32, float64, Rule, FullHead, EmptyBody
+from boomer.algorithm._head_refinement cimport HeadCandidate, HeadRefinement
 from boomer.algorithm._losses cimport Loss
 from boomer.algorithm._sub_sampling cimport InstanceSubSampling, FeatureSubSampling
+
+from libcpp.unordered_map cimport unordered_map as map
+
 
 cpdef Rule induce_default_rule(uint8[::1, :] y, Loss loss):
     """
@@ -39,28 +42,106 @@ cpdef Rule induce_rule(float32[::1, :] x, intp[::1, :] x_sorted_indices, HeadRef
     :param random_state:            The seed to be used by RNGs
     :return:                        The rule that has been induced
     """
+    cdef int current_random_state = random_state
+    cdef uint32[::1] weights
+
+    # Sub-sample examples, if necessary...
+    if instance_sub_sampling is None:
+        weights = None
+    else:
+        weights = instance_sub_sampling.sub_sample(x, loss, current_random_state)
+
+    # The head of the induced rule
+    cdef HeadCandidate head = None
+    # A map containing the feature indices of the rule's conditions that use the <= operator as keys and their
+    # thresholds as values
+    cdef map[intp, float32] leq_conditions
+    # A map containing the feature indices of the rule's conditions that use the > operator as keys and their thresholds
+    # as values
+    cdef map[intp, float32] gr_conditions
+
+    cdef int num_refinements = 1
+    cdef intp[::1] label_indices = None
     cdef intp num_examples = x.shape[0]
-    cdef intp num_features = x.shape[1]
+    cdef intp num_features
+    cdef intp[::1] feature_indices
     cdef float32 previous_threshold, current_threshold
-    cdef intp r, c, i
+    cdef intp c, f, r, i
+    cdef uint32 weight
 
-    # TODO Sub-sample instances if necessary
-    # TODO Sub-sample features if necessary
-    for c in range(num_features):
-        loss.begin_search(None)
-        i = x_sorted_indices[0, c]
-        loss.update_search(i, 1) # TODO: Use actual weight
-        previous_threshold = x[i, c]
+    while True:
+        # Sub-sample features, if necessary...
+        if feature_sub_sampling is None:
+            feature_indices = None
+            num_features = x.shape[1]
+        else:
+            feature_indices = feature_sub_sampling.sub_sample(x, current_random_state)
+            num_features = feature_indices.shape[0]
 
-        for r in range(1, num_examples - 1):
-            i = x_sorted_indices[r, c]
-            loss.update_search(i, 1) # TODO Use actual weight
-            current_threshold = x[i, c]
+        for c in range(num_features):
+            loss.begin_search(label_indices)
+            f = __get_feature_index(c, feature_indices)
 
-            #if previous_threshold != current_threshold:
-                # LEQ
-                # GR
+            # Find first example with weight > 0...
+            weight = 0
+
+            for r in range(0, num_examples - 1):
+                i = x_sorted_indices[r, f]
+                weight = __get_weight(i, weights)
+
+                if weight > 0:
+                    loss.update_search(i, weight)
+                    previous_threshold = x[i, f]
+                    break
+
+            # Traverse remaining instances (except for the last one)...
+            for r in range(r + 1, num_examples - 1):
+                i = x_sorted_indices[r, f]
+                weight = __get_weight(i, weights)
+
+                if weight > 0:
+                    loss.update_search(i, weight)
+                    current_threshold = x[i, f]
+
+                    if previous_threshold != current_threshold:
+                        # Evaluate condition using <= operator...
+                        head_refinement.find_head(head, loss, 1)
+
+                        # Evaluate condition using > operator...
+                        head_refinement.find_head(head, loss, 0)
 
             previous_threshold = current_threshold
 
+        num_refinements += 1
+        current_random_state *= num_refinements
+        break # TODO Use proper stopping criterion
+
     return None
+
+cdef inline intp __get_feature_index(intp i, intp[::1] feature_indices):
+    """
+    Retrieves and returns the index of the i-th feature from an array of feature indices, if such an array is available.
+    Otherwise i is returned.
+
+    :param i:               The position of the feature whose index should be retrieved
+    :param label_indices:   An array of the dtype int, shape `(num_features)`, representing the indices of features
+    :return:                A scalar of dtype int, representing the index of the i-th feature
+    """
+    if feature_indices is None:
+        return i
+    else:
+        return feature_indices[i]
+
+cdef inline uint32 __get_weight(intp example_index, uint32[::1] weights):
+    """
+    Retrieves and returns the weight of the example at a specific index from an array of weights, if such an array is
+    available.
+
+    :param example_index:   The index of the example whose weight should be retrieved
+    :param weights:         An array of dtype int, shape `(num_examples)`, representing the weights of examples
+    :return:                A scalar of dtype int, representing the weight of the example at the given index
+    """
+    if weights is None:
+        return 1
+    else:
+        return weights[example_index]
