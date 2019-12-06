47fc502d074a968fa38942828dffb482bd600261
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2019-12-06 14:07:16 +0100

    Add class RandomInstanceSubsetSelection.

--- python/boomer/algorithm/_sub_sampling.pyx
+++ python/boomer/algorithm/_sub_sampling.pyx
@@ -68,6 +68,59 @@ cdef class Bagging(InstanceSubSampling):
         return weights
 
 
+cdef class RandomInstanceSubsetSelection(InstanceSubSampling):
+    """
+    Implements random instance subset selection for drawing a subset (of predefined size) from the available training
+    examples without replacement.
+    """
+
+    def __cinit__(self, float sample_size = 0.5):
+        """
+        param sample_size: The fraction of examples to be included in the sample (e.g. a value of 0.6 corresponds to
+                           60 % of the available examples)
+        """
+        self.sample_size = sample_size
+
+    cdef uint32[::1] sub_sample(self, float32[::1, :] x, Loss loss, int random_state):
+        cdef intp num_examples = x.shape[0]
+        cdef float sample_size = self.sample_size
+        cdef int num_samples = <int>(sample_size * num_examples)
+        cdef int limit = num_examples
+        cdef uint32[::1] weights = cvarray(shape=(num_examples,), itemsize=sizeof(uint32), format='I', mode='c')
+        cdef uint32[::1] indices = cvarray(shape=(num_examples,), itemsize=sizeof(uint32), format='I', mode='c')
+        rng = check_random_state(random_state)
+        rng_randint = rng.randint
+        cdef uint32 tmp
+        cdef intp n, i, rand
+
+        # Tell the given loss function that instance sub-sampling is used...
+        loss.begin_instance_sub_sampling()
+
+        # Initialize arrays...
+        for n in range(num_examples):
+            weights[n] = 0
+            indices[n] = n
+
+        for n in range(num_samples):
+            # Select the index of an example that has not been drawn yet, i.e., which belongs to the region
+            # [0, limit]...
+            rand = rng_randint(limit)
+            i = indices[rand]
+
+            # Set weight at the selected index to 1...
+            weights[i] += 1
+
+            # Tell the given loss function that a new example has been chosen to be included in the sample...
+            loss.update_sub_sample(i)
+
+            # Shrink the region [0, limit] that contains the indices of the examples that have not been drawn yet and
+            # move the the element at the border to the position of the recently drawn element...
+            limit -= 1
+            indices[rand] = indices[limit]
+
+        return weights
+
+
 cdef class FeatureSubSampling:
     """
     A base class for all classes that implement a strategy for sub-sampling features.
