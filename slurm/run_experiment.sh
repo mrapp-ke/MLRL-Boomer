61bbcd099cb7f79549c38877f8535b9bbc8af457
Michael Rapp <michael.rapp90@googlemail.com>
2020-01-24 16:22:36 +0100

    Add script run_experiment.sh.

---
+++ slurm/run_experiment.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Command line arguments
+DATASET=$1
+LOSS=$2
+HEAD_REFINEMENT=$3
+
+# Constants
+FOLDS=10
+INSTANCE_SUB_SAMPLING="bagging"
+FEATURE_SUB_SAMPLING="random-feature-selection"
+NUM_RULES=5000
+SHRINKAGE=0.1
+
+# Paths
+ROOT_DIR="$(dirname "$PWD")"
+WORK_DIR="${ROOT_DIR}/results/${DATASET}/${LOSS}/${HEAD_REFINEMENT}/"
+MODEL_DIR="${ROOT_DIR}/models/${DATASET}/"
+DATA_DIR="${ROOT_DIR}/data/"
+
+MEMORY=4096
+
+FILE="${DATASET}_${LOSS}_${HEAD_REFINEMENT}.sh"
+PARAMETERS="--data-dir ${DATA_DIR} --dataset ${DATASET} --output-dir ${WORK_DIR} --model-dir ${MODEL_DIR} --folds ${FOLDS} --instance-sub-sampling ${INSTANCE_SUB_SAMPLING} --feature-sub-sampling ${FEATURE_SUB_SAMPLING} --num-rules ${NUM_RULES} --shrinkage ${SHRINKAGE} --loss ${LOSS} --head-refinement ${HEAD_REFINEMENT}"
+
+echo "$FILE"
+echo "#!/bin/sh" >> "$FILE"
+echo "#SBATCH -D ${WORK_DIR}" >> "$FILE"
+echo "#SBATCH --mem=${MEMORY}" >> "$FILE"
+echo "#SBATCH --time=24:00:00" >> "$FILE"
+echo "#SBATCH -c 1" >> "$FILE"
+echo "#SBATCH -N 1-1" >> "$FILE"
+echo "#SBATCH -o logs/%J.out" >> "$FILE"
+echo "#SBATCH -e logs/%J.err" >> "$FILE"
+echo "${ROOT_DIR}/venv/bin/python3.7 ${ROOT_DIR}/python/main.py ${PARAMETERS}" >> "$FILE"
+chmod +x "$FILE"
+mkdir -p "${WORK_DIR}/logs"
+mkdir -p "${MODEL_DIR}"
+sbatch "$FILE"
+rm "$FILE"
+echo "Started experiment with parameters ${PARAMETERS}"
