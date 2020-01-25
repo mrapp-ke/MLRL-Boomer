fc38c2363a8f870f74897bb4b808831c6dbb06c3
Michael Rapp <michael.rapp90@googlemail.com>
2020-01-25 23:04:35 +0100

    Add function "logistic_loss" to measures.py.

--- python/boomer/measures.py
+++ python/boomer/measures.py
@@ -20,3 +20,16 @@ def squared_error_loss(ground_truth: np.ndarray, predictions: np.ndarray) -> flo
     ground_truth = np.where(ground_truth > 0, 1, -1)
     predictions = np.clip(predictions, -1, 1)
     return np.average(np.square(ground_truth - predictions)).item()
+
+
+def logistic_loss(ground_truth: np.ndarray, predictions: np.ndarray) -> float:
+    """
+    Calculates and returns an evaluation score according to the (example-based) logistic loss.
+
+    :param ground_truth:    An array of dtype float, shape `(num_examples, num_labels)`, representing the ground truth
+    :param predictions:     An array of dtype float, shape `(num_examples, num_labels)`, representing the predictions
+    :return:                A scalar of dtype float, representing the calculated evaluation score
+    """
+    ground_truth = np.where(ground_truth > 0, 1, -1)
+    predictions = np.clip(predictions, -1, 1)
+    return np.average(np.log(1 + np.sum(np.exp(np.multiply(-ground_truth, predictions)), axis=1)))
