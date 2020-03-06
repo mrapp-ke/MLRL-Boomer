b3716be077e1da93ba50d564e72008170e2cb245
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2020-03-06 13:51:34 +0100

    Refactoring.

--- python/main_xgboost.py
+++ python/main_xgboost.py
@@ -10,28 +10,24 @@ from boomer.parameters import ParameterCsvInput
 from boomer.xgboost.xgboost_br import XGBoostBR
 
 
-def configure_argument_parser(p: argparse.ArgumentParser):
-    p.add_argument('--log-level', type=log_level, default='info', help='The log level to be used')
-    p.add_argument('--data-dir', type=str, help='The path of the directory where the data sets are located')
-    p.add_argument('--output-dir', type=optional_string, default=None,
-                   help='The path of the directory into which results should be written')
-    p.add_argument('--model-dir', type=optional_string, default=None,
-                   help='The path of the directory where models should be saved')
-    p.add_argument('--dataset', type=str, help='The name of the data set to be used')
-    p.add_argument('--folds', type=int, default=1, help='Total number of folds to be used by cross validation')
-    p.add_argument('--current-fold', type=current_fold_string, default=-1,
-                   help='The cross validation fold to be performed')
-    p.add_argument('--learning-rate', type=float, default=1.0, help='The learning rate to be used')
-    p.add_argument('--reg-lambda', type=float, default=0.0, help='The L2 regularization weight to be used')
-
-
 def create_learner(params) -> XGBoostBR:
     return XGBoostBR(model_dir=params.model_dir, learning_rate=params.learning_rate, reg_lambda=params.reg_lambda)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='An multi-label classification experiment using BR with XGBoost')
-    configure_argument_parser(parser)
+    parser.add_argument('--log-level', type=log_level, default='info', help='The log level to be used')
+    parser.add_argument('--data-dir', type=str, help='The path of the directory where the data sets are located')
+    parser.add_argument('--output-dir', type=optional_string, default=None,
+                        help='The path of the directory into which results should be written')
+    parser.add_argument('--model-dir', type=optional_string, default=None,
+                        help='The path of the directory where models should be saved')
+    parser.add_argument('--dataset', type=str, help='The name of the data set to be used')
+    parser.add_argument('--folds', type=int, default=1, help='Total number of folds to be used by cross validation')
+    parser.add_argument('--current-fold', type=current_fold_string, default=-1,
+                        help='The cross validation fold to be performed')
+    parser.add_argument('--learning-rate', type=float, default=1.0, help='The learning rate to be used')
+    parser.add_argument('--reg-lambda', type=float, default=0.0, help='The L2 regularization weight to be used')
     parser.add_argument('--random-state', type=int, default=1, help='The seed to be used by RNGs')
     parser.add_argument('--store-predictions', type=boolean_string, default=False,
                         help='True, if the predictions should be stored as CSV files, False otherwise')
