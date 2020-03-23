a2751378cd1d025dd42569f334f6ac4f51c56e45
JayJayJay1 <jakob.steeg@gmail.com>
2020-03-23 16:10:35 +0100

    Move intialization of confusion_matrix_default #15

--- python/boomer/algorithm/_label_wise_measure.pyx
+++ python/boomer/algorithm/_label_wise_measure.pyx
@@ -35,7 +35,6 @@ cdef class LabelWiseMeasure(Loss):
         cdef intp r, c
 
         default_rule[:] = 0
-        confusion_matrices_default[:,:] = 0
         confusion_matrices_covered[:,:] = 0
 
         for c in range(num_labels):
@@ -49,22 +48,6 @@ cdef class LabelWiseMeasure(Loss):
             else:
                 default_rule[c] = 0
                 minority_labels[c] = 1
-
-            # the confusion_matrix_default is the confusion matrix of the default rule
-            for r in range(num_examples):
-                true_label = y[r, c]
-                predicted_label = <uint8> default_rule[c]
-
-                if true_label == 0:
-                    if predicted_label == 0:
-                        confusion_matrices_default[c, _IN] += 1
-                    elif predicted_label == 1:
-                        confusion_matrices_default[c, _IP] += 1
-                elif true_label == 1:
-                    if predicted_label == 0:
-                        confusion_matrices_default[c, _RN] += 1
-                    elif predicted_label == 1:
-                        confusion_matrices_default[c, _RP] += 1
 
 
         self.confusion_matrices_default = confusion_matrices_default
@@ -87,19 +70,41 @@ cdef class LabelWiseMeasure(Loss):
         self.example_weights[example_index] = 1
 
     cdef begin_search(self, intp[::1] label_indices):
-        cdef uint32[::1] minority_labels
-        cdef intp num_labels
+        cdef uint32[::1] minority_labels = self.minority_labels
+        cdef float64[::1, :] confusion_matrices_default = self.confusion_matrices_default
+        cdef intp[::1] example_weights = self.example_weights
+        cdef uint8[::1, :] true_labels = self.true_labels
 
-        if label_indices is None:
-            minority_labels = self.minority_labels
-            num_labels = minority_labels.shape[0]
-        else:
-            num_labels = label_indices.shape[0]
-
+        cdef intp num_examples = true_labels.shape[0]
         cdef LabelIndependentPrediction prediction = self.prediction
         cdef float64[::1] predicted_scores
         cdef float64[::1] quality_scores
         cdef float64[::1, :] confusion_matrices_covered = self.confusion_matrices_covered
+        cdef intp num_labels
+
+        if label_indices is None:
+            num_labels = minority_labels.shape[0]
+        else:
+            num_labels = label_indices.shape[0]
+
+        confusion_matrices_default[:,:] = 0
+
+        for c in range(num_labels):
+            for r in range(num_examples):
+                if example_weights[r] == 1:
+                    true_label = true_labels[r, c]
+                    predicted_label = <uint8> minority_labels[c]
+
+                    if true_label == 0:
+                        if predicted_label == 0:
+                            confusion_matrices_default[c, _IN] += 1
+                        elif predicted_label == 1:
+                            confusion_matrices_default[c, _IP] += 1
+                    elif true_label == 1:
+                        if predicted_label == 0:
+                            confusion_matrices_default[c, _RN] += 1
+                        elif predicted_label == 1:
+                            confusion_matrices_default[c, _RP] += 1
 
         if confusion_matrices_covered is None or confusion_matrices_covered.shape[0] != num_labels:
             confusion_matrices_covered = matrix_float64(num_labels, 4)
