03ff01e7c7efdd9adfe9cc93c05bb4ed29e3a44a
JayJayJay1 <jakob.steeg@gmail.com>
2020-03-23 16:04:47 +0100

    Change HammingLoss #15

--- python/boomer/algorithm/_heuristics.pyx
+++ python/boomer/algorithm/_heuristics.pyx
@@ -8,4 +8,4 @@ cdef class Heuristic:
 cdef class HammingLoss(Heuristic):
     cdef float64 evaluate_confusion_matrix(self, float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
                                            float64 uip, float64 urn, float64 urp):
-        return (cip + crn + uip + urn) / (cin + cip + crn + crp + uin + uip + urn + urp)
+        return (cip + crn + uin + urp) / (cin + cip + crn + crp + uin + uip + urn + urp)
