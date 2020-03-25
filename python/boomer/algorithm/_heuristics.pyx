084d806c44ef3d36e887778b7f755094e851b5ed
JayJayJay1 <jakob.steeg@gmail.com>
2020-03-25 18:05:43 +0100

    Add precision heuristic, handle zero division, change HammingLoss Fixes #15

--- python/boomer/algorithm/_heuristics.pyx
+++ python/boomer/algorithm/_heuristics.pyx
@@ -8,4 +8,13 @@ cdef class Heuristic:
 cdef class HammingLoss(Heuristic):
     cdef float64 evaluate_confusion_matrix(self, float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
                                            float64 uip, float64 urn, float64 urp):
-        return (cip + crn + uin + urp) / (cin + cip + crn + crp + uin + uip + urn + urp)
+        if cin + cip + crn + crp + uin + uip + urn + urp == 0:
+            return 1
+        return (cip + crn + urn + urp) / (cin + cip + crn + crp + uin + uip + urn + urp)
+
+cdef class Precision(Heuristic):
+    cdef float64 evaluate_confusion_matrix(self, float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
+                                           float64 uip, float64 urn, float64 urp):
+        if cin + cip + crn + crp == 0:
+            return 1
+        return (cip + crn) / (cin + cip + crn + crp)
