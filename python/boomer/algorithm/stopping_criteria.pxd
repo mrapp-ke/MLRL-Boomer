ad10dfa23f47ff27b05ffb63c8f2754c3befc454
Michael Rapp <michael.rapp90@googlemail.com>
2020-05-06 19:30:27 +0200

    The UncoveredLabelsCriterion now works with any CoverageLoss.

--- python/boomer/algorithm/stopping_criteria.pxd
+++ python/boomer/algorithm/stopping_criteria.pxd
@@ -1,5 +1,5 @@
 from boomer.algorithm._arrays cimport intp, float64
-from boomer.algorithm.label_wise_averaging cimport LabelWiseAveraging
+from boomer.algorithm.coverage_losses cimport CoverageLoss
 
 
 cdef class StoppingCriterion:
@@ -39,7 +39,7 @@ cdef class UncoveredLabelsCriterion(StoppingCriterion):
 
     cdef readonly float64 threshold
 
-    cdef LabelWiseAveraging loss
+    cdef CoverageLoss loss
 
     # Functions:
 
--- python/boomer/algorithm/stopping_criteria.pyx
+++ python/boomer/algorithm/stopping_criteria.pyx
@@ -68,20 +68,20 @@ cdef class TimeStoppingCriterion(StoppingCriterion):
 
 cdef class UncoveredLabelsCriterion(StoppingCriterion):
     """
-    A stopping criterion that stops when the sum of the weight matrix used in `LabelWiseAveraging` is smaller than or
-    equal to a certain threshold.
+    A stopping criterion that stops when the sum of the weight matrix used by a `CoverageLoss` is smaller than or equal
+    to a certain threshold.
     """
 
-    def __cinit__(self, LabelWiseAveraging loss, float64 threshold):
+    def __cinit__(self, CoverageLoss loss, float64 threshold):
         """
-        :param loss:        The `LabelWiseAveraging`
+        :param loss:        The `CoverageLoss`
         :param threshold:   The threshold
         """
         self.loss = loss
         self.threshold = threshold
 
     cpdef bint should_continue(self, list theory):
-        cdef LabelWiseAveraging loss = self.loss
+        cdef CoverageLoss loss = self.loss
         cdef float64 sum_uncovered_labels = loss.sum_uncovered_labels
         cdef float64 threshold = self.threshold
         return sum_uncovered_labels > threshold
