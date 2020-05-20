bbf4380b47d2cfa334c901c103e00541f68ce2f9
Michael Rapp <michael.rapp90@googlemail.com>
2020-05-20 12:32:54 +0200

    Move line of code.

--- python/boomer/algorithm/rule_induction.pyx
+++ python/boomer/algorithm/rule_induction.pyx
@@ -324,11 +324,13 @@ cdef class ExactGreedyRuleInduction(RuleInduction):
                             loss.update_search(i, weight)
 
                 if found_refinement:
-                    # If a refinement has been found, add the new condition...
+                    # If a refinement has been found, add the new condition and update the labels for which the rule
+                    # predicts...
                     conditions.push_back(__make_condition(best_condition_index, best_condition_comparator,
                                                           best_condition_threshold))
                     num_conditions += 1
                     num_conditions_per_comparator[<intp>best_condition_comparator] += 1
+                    label_indices = head.label_indices
 
                     # If instance sub-sampling is used, examples that are not contained in the current sub-sample were
                     # not considered for finding the new condition. In the next step, we need to identify the examples
@@ -344,7 +346,6 @@ cdef class ExactGreedyRuleInduction(RuleInduction):
                                                             best_condition_threshold)
 
                     # Update the examples and labels for which the rule predicts...
-                    label_indices = head.label_indices
                     __filter_current_indices(best_condition_sorted_indices, num_examples, best_condition_index_array,
                                              best_condition_start, best_condition_end, best_condition_index,
                                              best_condition_comparator, num_conditions, loss)
