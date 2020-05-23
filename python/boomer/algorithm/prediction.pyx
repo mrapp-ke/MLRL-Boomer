455435bbcaa10f72c18cff1e43850f777d81a95c
Michael Rapp <michael.rapp90@googlemail.com>
2020-05-23 20:11:33 +0200

    Add classes Converter and Sign.

--- python/boomer/algorithm/prediction.pyx
+++ python/boomer/algorithm/prediction.pyx
@@ -3,8 +3,7 @@
 
 Provides classes for making predictions using rule-based models.
 """
-from boomer.algorithm._arrays cimport uint8, uint32, float64
-from boomer.algorithm._arrays cimport array_uint32, array_float32, c_matrix_uint8, c_matrix_float64
+from boomer.algorithm._arrays cimport uint8, uint32, array_uint32, array_float32, c_matrix_uint8, c_matrix_float64
 from boomer.algorithm.rules cimport Rule
 
 import numpy as np
@@ -97,3 +96,54 @@ cdef class RawPredictor(Predictor):
             n += 1
 
         return np.asarray(predictions)
+
+
+cdef class Converter(Predictor):
+    """
+    TODO
+    """
+
+    cpdef object predict(self, float32[:, ::1] x, intp num_labels, list rules):
+        cdef Predictor predictor = self.predictor
+        cdef float64[:, ::1] raw_predictions = predictor.predict(x, num_labels, rules)
+        return self._convert(raw_predictions)
+
+    cpdef object predict_csr(self, float32[::1] x_data, intp[::1] x_row_indices, intp[::1] x_col_indices,
+                             intp num_features, intp num_labels, list rules):
+        cdef Predictor predictor = self.predictor
+        cdef float64[:, ::1] raw_predictions = predictor.predict_csr(x_data, x_row_indices, x_col_indices, num_features,
+                                                           num_labels, rules)
+        return self._convert(raw_predictions)
+
+    cdef object _convert(self, float64[:, ::1] raw_predictions):
+        """
+        TODO
+
+        :param raw_predictions:
+        :return:
+        """
+        pass
+
+
+cdef class Sign(Converter):
+    """
+    TODO
+    """
+
+    def __cinit__(self, RawPredictor predictor):
+        """
+        :param predictor: The predictor that should be used to obtain raw predictions
+        """
+        self.predictor = predictor
+
+    cdef object _convert(self, float64[:, ::1] raw_predictions):
+        cdef intp num_examples = raw_predictions.shape[0]
+        cdef intp num_labels = raw_predictions.shape[1]
+        cdef uint8[:, ::1] converted_predictions = c_matrix_uint8(num_examples, num_labels)
+        cdef intp r, c
+
+        for r in range(num_examples):
+            for c in range(num_labels):
+                converted_predictions[r, c] = (raw_predictions[r, c] > 0)
+
+        return np.asarray(converted_predictions)
