62cdef1b4ce4f4ee256ac8f6ac10507e2eb0ec4e
JayJayJay1 <jakob.steeg@gmail.com>
2020-06-01 20:02:23 +0200

    Fix default parameter value bug

--- python/boomer/algorithm/rule_learners.py
+++ python/boomer/algorithm/rule_learners.py
@@ -522,7 +522,7 @@ class SeparateAndConquerRuleLearner(MLRuleLearner):
         name += '_lift-function=' + str(self.lift_function)
         name += '_loss=' + str(self.loss)
         name += '_heuristic=' + str(self.heuristic)
-        if int(self.label_sub_sampling) != -1:
+        if self.label_sub_sampling is not None:
             name += '_label-sub-sampling=' + str(self.label_sub_sampling)
             name += '_label-sub-sampling-num-samples=' + str(self.label_sub_sampling_num_samples)
         if self.instance_sub_sampling is not None:
