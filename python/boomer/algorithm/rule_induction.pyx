91d9719984f83f7230adc0dd5a9f1785e8ff3912
Michael Rapp <michael.rapp90@googlemail.com>
2020-06-03 14:32:35 +0200

    Remove unused function argument.

--- python/boomer/algorithm/rule_induction.pyx
+++ python/boomer/algorithm/rule_induction.pyx
@@ -414,7 +414,6 @@ cdef class ExactGreedyRuleInduction(RuleInduction):
                     covered_examples_target = __filter_current_indices(best_condition_indexed_values, num_examples,
                                                                        best_condition_indexed_array_wrapper,
                                                                        best_condition_start, best_condition_end,
-                                                                       best_condition_feature_index,
                                                                        best_condition_comparator, num_conditions,
                                                                        covered_examples_mask, covered_examples_target,
                                                                        loss, weights)
@@ -571,9 +570,9 @@ cdef inline intp __adjust_split(IndexedValue* indexed_values, intp position_star
 
 cdef inline uint32 __filter_current_indices(IndexedValue* indexed_values, intp num_indexed_values,
                                             IndexedArrayWrapper* indexed_array_wrapper, intp condition_start,
-                                            intp condition_end, intp condition_index, Comparator condition_comparator,
-                                            intp num_conditions, uint32[::1] covered_examples_mask,
-                                            uint32 covered_examples_target, Loss loss, uint32[::1] weights):
+                                            intp condition_end, Comparator condition_comparator, intp num_conditions,
+                                            uint32[::1] covered_examples_mask, uint32 covered_examples_target,
+                                            Loss loss, uint32[::1] weights):
     """
     Filters an array that contains the indices of the examples that are covered by the previous rule, as well as their
     values for a certain feature, after a new condition that corresponds to said feature has been added, such that the
@@ -593,7 +592,6 @@ cdef inline uint32 __filter_current_indices(IndexedValue* indexed_values, intp n
     :param condition_end:           The element in `indexed_values` that corresponds to the last example (exclusive)
                                     that has been passed to the loss function when searching for the new condition (must
                                     be smaller than `condition_start`)
-    :param condition_index:         The index of the feature, the new condition corresponds to
     :param condition_comparator:    The type of the operator that is used by the new condition
     :param num_conditions:          The total number of conditions in the rule's body (including the new one)
     :param covered_examples_mask:   An array of dtype uint, shape `(num_examples)` that is used to keep track of the
