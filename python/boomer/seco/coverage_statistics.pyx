f64aff996a52892124e5cfc539f2c345f51c1c78
Michael Rapp <michael.rapp90@googlemail.com>
2020-07-20 18:37:34 +0200

    Add new files coverage_statistics.pxd and coverage_statistics.pyx.

---
+++ python/boomer/seco/coverage_statistics.pyx
@@ -0,0 +1,27 @@
+"""
+@author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+
+Provides base classes for all classes that allow to store the elements of confusion matrices that are computed based on
+a weight matrix and the ground truth labels of the training examples.
+"""
+
+
+cdef class CoverageStatistics(Statistics):
+    """
+    A base class for all classes that store the elements of confusion matrices.
+    """
+
+    cdef void reset_statistics(self):
+        pass
+
+    cdef void add_sampled_statistic(self, intp statistic_index, uint32 weight):
+        pass
+
+    cdef void update_covered_statistic(self, intp statistic_index, uint32 weight, bint remove):
+        pass
+
+    cdef RefinementSearch begin_search(self, intp[::1] label_indices):
+        pass
+
+    cdef void apply_prediction(self, intp statistic_index, intp[::1] label_indices, HeadCandidate* head):
+        pass
