0e137bd61657a6f4c4122b726c62a4ba1060f05f
Michael Rapp <michael.rapp90@googlemail.com>
2020-07-21 01:59:55 +0200

    Add the new class LabelWiseDefaultRuleEvaluation.

--- python/boomer/boosting/label_wise_rule_evaluation.pyx
+++ python/boomer/boosting/label_wise_rule_evaluation.pyx
@@ -7,7 +7,66 @@ functions that are applied label-wise.
 from boomer.common._arrays cimport get_index
 from boomer.boosting.differentiable_losses cimport _l2_norm_pow
 
+from libc.stdlib cimport malloc
 from libc.math cimport pow
+
+from libcpp.pair cimport pair
+
+
+cdef class LabelWiseDefaultRuleEvaluation:
+    """
+    Allows to calculate the predictions of the default rule such that they minimize a loss function that is applied
+    label-wise.
+    """
+
+    def __cinit__(self, LabelWiseLossFunction loss_function, float64 l2_regularization_weight):
+        """
+        :param loss_function:               The loss function to be minimized
+        :param l2_regularization_weight:    The weight of the L2 regularization that is applied for calculating the
+                                            scores to be predicted by the default rule
+        """
+        self.loss_function = loss_function
+        self.l2_regularization_weight = l2_regularization_weight
+
+    cdef DefaultPrediction* calculate_default_prediction(self, LabelMatrix label_matrix):
+        """
+        Calculates the scores to be predicted by the default rule.
+
+        :param label_matrix:    A `LabelMatrix` that provides random access to the labels of the training examples
+        :return:                A pointer to an object of type `DefaultPrediction`, representing the predictions of the
+                                default rule
+        """
+        # Class members
+        cdef LabelWiseLossFunction loss_function = self.loss_function
+        cdef float64 l2_regularization_weight = self.l2_regularization_weight
+        # The number of examples
+        cdef intp num_examples = label_matrix.num_examples
+        # The number of labels
+        cdef intp num_labels = label_matrix.num_labels
+        # An array that stores the scores that are predicted by the default rule
+        cdef float64* predicted_scores = <float64*>malloc(num_labels * sizeof(float64))
+        # Temporary variables
+        cdef pair[float64, float64] gradient_and_hessian
+        cdef float64 gradient, sum_of_gradients, hessian, sum_of_hessians, predicted_score
+        cdef intp c, r
+
+        for c in range(num_labels):
+            sum_of_gradients = 0
+            sum_of_hessians = 0
+
+            for r in range(num_examples):
+                # Calculate the gradient and Hessian for the current example and label...
+                gradient_and_hessian = loss_function.calculate_gradient_and_hessian(label_matrix, r, c, 0)
+                gradient = gradient_and_hessian.first
+                sum_of_gradients += gradient
+                hessian = gradient_and_hessian.second
+                sum_of_hessians += hessian
+
+            # Calculate the score to be predicted by the default rule for the current label...
+            predicted_score = -sum_of_gradients / (sum_of_hessians + l2_regularization_weight)
+            predicted_scores[c] = predicted_score
+
+        return new DefaultPrediction(num_labels, predicted_scores)
 
 
 cdef class LabelWiseRuleEvaluation:
