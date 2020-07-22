5a7aa9cbb9d736cffa8d7e52ab473ce6703b04ea
Michael Rapp <michael.rapp90@googlemail.com>
2020-07-23 01:34:03 +0200

    The class UncoveredLabelsCriterion is now based on CoverageStatistics.

--- python/boomer/seco/stopping_criteria.pyx
+++ python/boomer/seco/stopping_criteria.pyx
@@ -1,19 +1,19 @@
 cdef class UncoveredLabelsCriterion(StoppingCriterion):
     """
-    A stopping criterion that stops when the sum of the weight matrix used by a `CoverageLoss` is smaller than or equal
-    to a certain threshold.
+    A stopping criterion that stops when the sum of the weight matrix stored by `CoverageStatistics` is smaller than or
+    equal to a certain threshold.
     """
 
-    def __cinit__(self, CoverageLoss loss, float64 threshold):
+    def __cinit__(self, CoverageStatistics statistics, float64 threshold):
         """
-        :param loss:        The `CoverageLoss`
+        :param loss:        The `CoverageStatistics`
         :param threshold:   The threshold
         """
-        self.loss = loss
+        self.statistics = statistics
         self.threshold = threshold
 
     cdef bint should_continue(self, intp num_rules):
-        cdef CoverageLoss loss = self.loss
-        cdef float64 sum_uncovered_labels = loss.sum_uncovered_labels
+        cdef CoverageStatistics statistics = self.statistics
+        cdef float64 sum_uncovered_labels = statistics.sum_uncovered_labels
         cdef float64 threshold = self.threshold
         return sum_uncovered_labels > threshold
