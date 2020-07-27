12dc19b0668d5b84671d5b321a717bc8fabc4485
Michael Rapp <michael.rapp90@googlemail.com>
2020-07-27 15:08:36 +0200

    Add abstract C++ class HeuristicFunction.

---
+++ python/boomer/seco/cpp/heuristics.cpp
@@ -0,0 +1,13 @@
+#include "heuristics.h"
+
+using namespace heuristics;
+
+
+HeuristicFunction::~HeuristicFunction() {
+
+}
+
+float64 HeuristicFunction::evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
+                                                   float64 uip, float64 urn, float64 urp) {
+    return 0;
+}
