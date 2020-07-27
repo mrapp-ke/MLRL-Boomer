d16a7526750357a56a36a994afbd5d0b6bd505b6
Michael Rapp <michael.rapp90@googlemail.com>
2020-07-27 15:20:31 +0200

    Add static function "wra".

--- python/boomer/seco/cpp/heuristics.cpp
+++ python/boomer/seco/cpp/heuristics.cpp
@@ -42,3 +42,19 @@ static float64 recall(float64 cin, float64 crp, float64 uin, float64 urp) {
 
     return numUncoveredEqual / numEqual;
 }
+
+static float64 wra(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin, float64 uip, float64 urn,
+                   float64 urp) {
+    float64 numCoveredEqual = cin + crp;
+    float64 numUncoveredEqual = uin + urp;
+    float64 numEqual = numUncoveredEqual + numCoveredEqual;
+    float64 numCovered = numCoveredEqual + cip + crn;
+    float64 numUncovered = numUncoveredEqual + uip + urn;
+    float64 numTotal = numCovered + numUncovered;
+
+    if (numCovered == 0 || numTotal == 0) {
+        return 1;
+    }
+
+    return 1 - ((numCovered / numTotal) * ((numCoveredEqual / numCovered) - (numEqual / numTotal)));
+}
