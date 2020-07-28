9fade1eb1580f10af8842922d7344e3683c0fabb
Michael Rapp <michael.rapp90@googlemail.com>
2020-07-29 00:59:03 +0200

    Add C++ class LabelWiseRuleRefinementImpl (not implemented yet).

---
+++ python/boomer/seco/cpp/label_wise_statistics.h
@@ -0,0 +1,106 @@
+/**
+ * TODO
+ *
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ */
+#pragma once
+
+#include "../../common/cpp/arrays.h"
+#include "label_wise_rule_evaluation.h"
+
+
+namespace statistics {
+
+    /**
+     * TODO
+     */
+    class LabelWiseRefinementSearchImpl {
+
+        private:
+
+            /**
+             * TODO
+             */
+            rule_evaluation::LabelWiseRuleEvaluationImpl* ruleEvaluation_;
+
+            /**
+             * TODO
+             */
+            const intp* labelIndices_;
+
+            /**
+             * TODO
+             */
+            // TODO LabelMatrix* labelMatrix_;
+
+            /**
+             * TODO
+             */
+            const float64* uncoveredLabels_;
+
+            /**
+             * TODO
+             */
+            const uint8* minorityLabels_;
+
+            /**
+             * TODO
+             */
+            const float64* confusionMatricesTotal_;
+
+            /**
+             * TODO
+             */
+            const float64* confusionMatricesSubset_;
+
+            /**
+             * TODO
+             */
+            rule_evaluation::LabelWisePrediction* prediction_;
+
+        public:
+
+            /**
+             * TODO
+             *
+             * @param ruleEvaluation
+             * @param labelIndices
+             * @param uncoveredLabels
+             * @param minorityLabels
+             * @param confusionMatricesTotal
+             * @param confusionMatricesSubset
+             */
+            LabelWiseRefinementSearchImpl(rule_evaluation::LabelWiseRuleEvaluationImpl* ruleEvaluation,
+                                          const intp* labelIndices, const float64* uncoveredLabels,
+                                          const uint8* minorityLabels, const float64* confusionMatricesTotal,
+                                          const float64* confusionMatricesSubset);
+
+            /**
+             * TODO
+             */
+            ~LabelWiseRefinementSearchImpl();
+
+            /**
+             * TODO
+             *
+             * @param statisticsIndex
+             * @param weight
+             */
+            void updateSearch(intp statisticIndex, uint32 weight);
+
+            /**
+             * TODO
+             */
+            void resetSearch();
+
+            /**
+             * TODO
+             *
+             * @param uncovered
+             * @param accumulated
+             */
+            rule_evaluation::LabelWisePrediction* calculateLabelWisePrediction(bool uncovered, bool accumulated);
+
+    };
+
+}
