9e1d21a3aceb522c2b5709908ff48eedf6d884b9
Michael Rapp <michael.rapp90@googlemail.com>
2020-07-29 02:43:08 +0200

    Add C++ class DenseLabelMatrixImpl (not implemented yet).

--- python/boomer/common/cpp/statistics.cpp
+++ python/boomer/common/cpp/statistics.cpp
@@ -10,3 +10,11 @@ AbstractLabelMatrix::~AbstractLabelMatrix() {
 uint8 AbstractLabelMatrix::getLabel(intp exampleIndex, intp labelIndex) {
     return 0;
 }
+
+DenseLabelMatrixImpl::~DenseLabelMatrixImpl() {
+
+}
+
+uint8 DenseLabelMatrixImpl::getLabel(intp exampleIndex, intp labelIndex) {
+    return 0;
+}
