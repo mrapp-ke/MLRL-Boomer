5bfd922e176bf42e83cb829ee31b6590c4ecf57e
Michael Rapp <michael.rapp90@googlemail.com>
2020-07-29 02:36:41 +0200

    Add abstract C++ class AbstractLabelMatrix.

---
+++ python/boomer/common/cpp/statistics.cpp
@@ -0,0 +1,12 @@
+#include "statistics.h"
+
+using namespace statistics;
+
+
+AbstractLabelMatrix::~AbstractLabelMatrix() {
+
+}
+
+uint8 AbstractLabelMatrix::getLabel(intp exampleIndex, intp labelIndex) {
+    return 0;
+}
