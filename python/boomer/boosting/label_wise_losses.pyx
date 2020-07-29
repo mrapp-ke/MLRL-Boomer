c60f643d75ac93836eff2bf3226d8b1d1db28b36
Michael Rapp <michael.rapp90@googlemail.com>
2020-07-29 18:09:57 +0200

    The Cython classes LabelWiseLoss, LabelWiseLogisticLoss and LabelWiseSquaredErrorLoss are now wrappers for the corresponding C++ classes.

--- python/boomer/boosting/label_wise_losses.pyx
+++ python/boomer/boosting/label_wise_losses.pyx
@@ -10,14 +10,14 @@ from libc.math cimport exp, pow
 
 cdef class LabelWiseLoss:
     """
-    A base class for all (decomposable) loss functions that are applied label-wise.
+    A wrapper for the abstract C++ class `AbstractLabelWiseLoss`.
     """
 
     cdef pair[float64, float64] calculate_gradient_and_hessian(self, LabelMatrix label_matrix, intp example_index,
-                                                               intp label_index, float64 predicted_score):
+                                                               intp label_index, float64 predicted_score) nogil:
         """
-        Must be implemented by subclasses to calculate the gradient (first derivative) and Hessian (second derivative)
-        of the loss function for a certain example and label.
+        Calculates the gradient (first derivative) and Hessian (second derivative) of the loss function for a certain
+        example and label.
 
         :param label_matrix:    A `LabelMatrix` that provides random access to the labels of the training examples
         :param example_index:   The index of the example for which the gradient and Hessian should be calculated
@@ -27,39 +27,30 @@ cdef class LabelWiseLoss:
         :return:                A pair that contains two scalars of dtype float64, representing the gradient and the
                                 Hessian that have been calculated
         """
-        pass
+        cdef AbstractLabelWiseLoss* loss_function = self.loss_function
+        cdef AbstractLabelMatrix* label_matrix_ptr = label_matrix.label_matrix
+        return loss_function.calculateGradientAndHessian(label_matrix_ptr, example_index, label_index, predicted_score)
 
 
 cdef class LabelWiseLogisticLoss(LabelWiseLoss):
     """
-    A multi-label variant of the logistic loss that is applied label-wise.
+    A wrapper for the C++ class `LabelWiseLogisticLossImpl`.
     """
 
-    cdef pair[float64, float64] calculate_gradient_and_hessian(self, LabelMatrix label_matrix, intp example_index,
-                                                               intp label_index, float64 predicted_score):
-        cdef uint8 true_label = label_matrix.get_label(example_index, label_index)
-        cdef float64 expected_score = 1 if true_label else -1
-        cdef float64 exponential = exp(expected_score * predicted_score)
-        cdef float64 gradient = -expected_score / (1 + exponential)
-        cdef float64 hessian = (pow(expected_score, 2) * exponential) / pow(1 + exponential, 2)
-        cdef pair[float64, float64] result  # Stack-allocated pair
-        result.first = gradient
-        result.second = hessian
-        return result
+    def __cinit__(self):
+        self.loss_function = new LabelWiseLogisticLossImpl()
+
+    def __dealloc__(self):
+        del self.loss_function
 
 
 cdef class LabelWiseSquaredErrorLoss(LabelWiseLoss):
     """
-    A multi-label variant of the squared error loss that is applied label-wise.
+    A wrapper for the C++ class `LabelWiseSquaredErrorLossImpl`.
     """
 
-    cdef pair[float64, float64] calculate_gradient_and_hessian(self, LabelMatrix label_matrix, intp example_index,
-                                                               intp label_index, float64 predicted_score):
-        cdef uint8 true_label = label_matrix.get_label(example_index, label_index)
-        cdef float64 expected_score = 1 if true_label else -1
-        cdef float64 gradient = 2 * predicted_score - 2 * expected_score
-        cdef float64 hessian = 2
-        cdef pair[float64, float64] result  # Stack-allocated pair
-        result.first = gradient
-        result.second = hessian
-        return result
+    def __cinit__(self):
+        self.loss_function = new LabelWiseSquaredErrorLossImpl()
+
+    def __dealloc__(self):
+        del self.loss_function
