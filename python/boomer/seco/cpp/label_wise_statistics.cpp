5530bbf4f7b30601523b0eb1268fb3fc147b24f9
Michael Rapp <michael.rapp90@googlemail.com>
2020-07-30 00:58:11 +0200

    Remove redundant namespace specification.

--- python/boomer/seco/cpp/label_wise_statistics.cpp
+++ python/boomer/seco/cpp/label_wise_statistics.cpp
@@ -6,7 +6,7 @@ using namespace statistics;
 
 LabelWiseRefinementSearchImpl::LabelWiseRefinementSearchImpl(
         rule_evaluation::LabelWiseRuleEvaluationImpl* ruleEvaluation, intp numLabels, const intp* labelIndices,
-        statistics::AbstractLabelMatrix* labelMatrix, const float64* uncoveredLabels, const uint8* minorityLabels,
+        AbstractLabelMatrix* labelMatrix, const float64* uncoveredLabels, const uint8* minorityLabels,
         const float64* confusionMatricesTotal, const float64* confusionMatricesSubset) {
     ruleEvaluation_ = ruleEvaluation;
     numLabels_ = numLabels;
