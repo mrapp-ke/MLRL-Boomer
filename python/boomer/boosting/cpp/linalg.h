877ae9b8a681a7771fdcfa4543e41e20f8cd834f
Michael Rapp <michael.rapp90@googlemail.com>
2020-07-30 18:36:22 +0200

    Add file linalg.h.

---
+++ python/boomer/boosting/cpp/linalg.h
@@ -0,0 +1,32 @@
+/**
+ * Provides commonly used functions that implement mathematical operations.
+ *
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ */
+#pragma once
+
+#include "../../common/cpp/arrays.h"
+#include <math.h>
+
+
+namespace linalg {
+
+    /**
+     * Computes and returns the square of the L2 norm of a specific array, i.e. the sum of the squares of its elements.
+     * To obtain the actual L2 norm, the square-root of the result provided by this function must be computed.
+     *
+     * @param a A pointer to an array of type `float64`, shape `(n)`
+     * @param n The number of elements in the array `a`
+     * @return  A scalar of type `float64`, representing the square of the L2 norm of the given array
+     */
+    static inline float64 l2NormPow(float64* a, intp n) {
+        float64 result = 0;
+
+        for (intp i = 0; i < n; i++) {
+            result += pow(a[i], 2);
+        }
+
+        return result;
+    }
+
+}
