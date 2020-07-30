ab57291e9fbc0ecb7e502d7e3adeb462d6cfc715
Michael Rapp <michael.rapp90@googlemail.com>
2020-07-30 18:36:46 +0200

    Add C++ class LabelWiseRuleEvaluationImpl.

---
+++ python/boomer/boosting/cpp/label_wise_rule_evaluation.cpp
@@ -0,0 +1,54 @@
+#include "label_wise_rule_evaluation.h"
+#include "linalg.h"
+
+using namespace rule_evaluation;
+
+
+LabelWiseRuleEvaluationImpl::LabelWiseRuleEvaluationImpl(float64 l2RegularizationWeight) {
+    l2RegularizationWeight_ = l2RegularizationWeight;
+}
+
+void LabelWiseRuleEvaluationImpl::calculateLabelWisePrediction(const intp* labelIndices,
+                                                               const float64* totalSumsOfGradients,
+                                                               const float64* sumsOfGradients,
+                                                               const float64* totalSumsOfHessians,
+                                                               const float64* sumsOfHessians, bool uncovered,
+                                                               LabelWisePrediction* prediction) {
+    // Class members
+    float64 l2RegularizationWeight = l2RegularizationWeight_;
+    // The number of labels to predict for
+    intp numPredictions = prediction->numPredictions_;
+    // The array that should be used to store the predicted scores
+    float64* predictedScores = prediction->predictedScores_;
+    // The array that should be used to store the quality scores
+    float64* qualityScores = prediction->qualityScores_;
+    // The overall quality score, i.e., the sum of the quality scores for each label plus the L2 regularization term
+    float64 overallQualityScore = 0;
+
+    // For each label, calculate a score to be predicted, as well as a corresponding quality score...
+    for (intp c = 0; c < numPredictions; c++) {
+        float64 sumOfGradients = sumsOfGradients[c];
+        float64 sumOfHessians =  sumsOfHessians[c];
+
+        if (uncovered) {
+            intp l = labelIndices != null ? labelIndices[c] : c;
+            sumOfGradients = totalSumsOfGradients[l] - sumOfGradients;
+            sumOfHessians = totalSumsOfHessians[l] - sumOfHessians;
+        }
+
+        // Calculate the score to be predicted for the current label...
+        float64 score = sumOfHessians + l2RegularizationWeight;
+        score = score != 0 ? -sumOfGradients / score : 0;
+        predictedScores[c] = score;
+
+        // Calculate the quality score for the current label...
+        float64 scorePow = pow(score, 2);
+        score = (sumOfGradients * score) + (0.5 * scorePow * sumOfHessians);
+        qualityScores[c] = score + (0.5 * l2RegularizationWeight * scorePow);
+        overallQualityScore += score;
+    }
+
+    // Add the L2 regularization term to the overall quality score...
+    overallQualityScore += 0.5 * l2RegularizationWeight * linalg::l2NormPow(predictedScores, numPredictions);
+    prediction->overallQualityScore_ = overallQualityScore;
+}
