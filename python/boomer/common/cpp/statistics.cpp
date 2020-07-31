76f88219766d38a4437e24e97c537f7d50a1cbd0
Michael Rapp <michael.rapp90@googlemail.com>
2020-07-31 23:28:20 +0200

    Format code.

--- python/boomer/common/cpp/statistics.cpp
+++ python/boomer/common/cpp/statistics.cpp
@@ -13,18 +13,15 @@ void AbstractRefinementSearch::resetSearch() {
 
 }
 
-LabelWisePrediction* AbstractRefinementSearch::calculateLabelWisePrediction(bool uncovered,
-                                                                                             bool accumulated) {
+LabelWisePrediction* AbstractRefinementSearch::calculateLabelWisePrediction(bool uncovered, bool accumulated) {
     return NULL;
 }
 
-Prediction* AbstractRefinementSearch::calculateExampleWisePrediction(bool uncovered,
-                                                                                      bool accumulated) {
+Prediction* AbstractRefinementSearch::calculateExampleWisePrediction(bool uncovered, bool accumulated) {
     return NULL;
 }
 
-Prediction* AbstractDecomposableRefinementSearch::calculateExampleWisePrediction(bool uncovered,
-                                                                                                  bool accumulated) {
+Prediction* AbstractDecomposableRefinementSearch::calculateExampleWisePrediction(bool uncovered, bool accumulated) {
     // In the decomposable case, the example-wise predictions are the same as the label-wise predictions...
     return (Prediction*) this->calculateLabelWisePrediction(uncovered, accumulated);
 }
