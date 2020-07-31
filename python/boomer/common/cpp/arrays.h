865600e0bede057aa1e30d789f1013cff161a95a
Michael Rapp <michael.rapp90@googlemail.com>
2020-08-01 01:49:13 +0200

    Add function "setToZeros" for setting all elements in an one-dimensional array to zero.

--- python/boomer/common/cpp/arrays.h
+++ python/boomer/common/cpp/arrays.h
@@ -40,10 +40,24 @@ namespace arrays {
     }
 
     /**
-     * Sets all elements in an array to zero.
+     * Sets all elements in an one-dimensional array to zero.
      *
-     * @param a             A pointer to an array of template type `T`, shape `(numRows, numCols)`
+     * @param a             A pointer to an array of template type `T`, shape `(numElements)`
      * @param numElements   The number of elements in the array
+     */
+    template<typename T>
+    static inline void setToZeros(T* a, intp numElements) {
+        for (intp i = 0; i < numElements; i++) {
+            a[i] = 0;
+        }
+    }
+
+    /**
+     * Sets all elements in a two-dimensional array to zero.
+     *
+     * @param a         A pointer to an array of template type `T`, shape `(numRows, numCols)`
+     * @param numRows   The number of rows in the array
+     * @param numCols   The number of columns in the array
      */
     template<typename T>
     static inline void setToZeros(T* a, intp numRows, intp numCols) {
