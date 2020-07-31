41f17a7bed27aaca9a5ac5566cfde9773980e692
Michael Rapp <michael.rapp90@googlemail.com>
2020-07-31 20:03:15 +0200

    Remove namespace "head_refinement".

--- python/boomer/common/cpp/head_refinement.h
+++ python/boomer/common/cpp/head_refinement.h
@@ -8,49 +8,45 @@
 #include "arrays.h"
 
 
-namespace head_refinement {
+/**
+ * Represents a potential head of a rule, whose quality is assessed in terms of a score.
+ */
+class HeadCandidate {
 
-    /**
-     * Represents a potential head of a rule, whose quality is assessed in terms of a score.
-     */
-    class HeadCandidate {
+    public:
 
-        public:
+        /**
+         * @param numPredictions    The number of labels for which the head predicts
+         * @param labelIndices      A pointer to an array of type intp, shape `(numPredictions)`, representing the
+         *                          indices of the labels for which the head predicts
+         * @param predictedScores   A pointer to an array of type float64, shape `(numPredictions)`, representing the
+         *                          scores that are predicted by the head
+         * @param qualityScore      A score that assesses the quality of the head
+         */
+        HeadCandidate(intp numPredictions, intp* labelIndices, float64* predictedScores, float64 qualityScore);
 
-            /**
-             * @param numPredictions    The number of labels for which the head predicts
-             * @param labelIndices      A pointer to an array of type intp, shape `(numPredictions)`, representing the
-             *                          indices of the labels for which the head predicts
-             * @param predictedScores   A pointer to an array of type float64, shape `(numPredictions)`, representing
-             *                          the scores that are predicted by the head
-             * @param qualityScore      A score that assesses the quality of the head
-             */
-            HeadCandidate(intp numPredictions, intp* labelIndices, float64* predictedScores, float64 qualityScore);
+        ~HeadCandidate();
 
-            ~HeadCandidate();
+        /**
+         * The number of labels for which the head predicts.
+         */
+        intp numPredictions_;
 
-            /**
-             * The number of labels for which the head predicts.
-             */
-            intp numPredictions_;
+        /**
+         * A pointer to an array of type intp, shape `(numPredictions_)`, representing the indices of the labels for
+         * which the head predicts
+         */
+        intp* labelIndices_;
 
-            /**
-             * A pointer to an array of type intp, shape `(numPredictions_)`, representing the indices of the labels for
-             * which the head predicts
-             */
-            intp* labelIndices_;
+        /**
+         * A pointer to an array of type float64, shape `(numPredictions_)`, representing the scores that are
+         * predicted by the head.
+         */
+        float64* predictedScores_;
 
-            /**
-             * A pointer to an array of type float64, shape `(numPredictions_)`, representing the scores that are
-             * predicted by the head.
-             */
-            float64* predictedScores_;
+        /**
+         * A score that assesses the quality of the head.
+         */
+        float64 qualityScore_;
 
-            /**
-             * A score that assesses the quality of the head.
-             */
-            float64 qualityScore_;
-
-    };
-
-}
+};
