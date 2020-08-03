f2eb819d2be69cfdf1dc7cf12cbd211f3b438cd5
Michael Rapp <michael.rapp90@googlemail.com>
2020-08-04 00:49:52 +0200

    The Cython classes boosting.LabelWiseRuleEvaluation and ExampleWiseRuleEvaluation do now only store a pointer to an object of the corresponding C++ class.

--- python/boomer/boosting/example_wise_rule_evaluation.pyx
+++ python/boomer/boosting/example_wise_rule_evaluation.pyx
@@ -6,6 +6,7 @@ they minimize a loss function that is applied example-wise.
 """
 from boomer.boosting._blas cimport init_blas
 from boomer.boosting._lapack cimport init_lapack
+from boomer.boosting.example_wise_losses cimport ExampleWiseLoss
 
 
 cdef class ExampleWiseDefaultRuleEvaluation(DefaultRuleEvaluation):
@@ -44,86 +45,3 @@ cdef class ExampleWiseRuleEvaluation:
         cdef Blas* blas = init_blas()
         cdef Lapack* lapack = init_lapack()
         self.rule_evaluation = new ExampleWiseRuleEvaluationImpl(l2_regularization_weight, blas, lapack)
-
-    cdef void calculate_label_wise_prediction(self, const intp[::1] label_indices,
-                                              const float64[::1] total_sums_of_gradients,
-                                              float64[::1] sums_of_gradients, const float64[::1] total_sums_of_hessians,
-                                              float64[::1] sums_of_hessians, bint uncovered,
-                                              LabelWisePrediction* prediction):
-        """
-        Calculates the scores to be predicted by a rule, as well as corresponding quality scores, based on the
-        label-wise sums of gradients and Hessians that are covered by the rule. The predicted scores and quality scores
-        are stored in a given object of type `LabelWisePrediction`.
-
-        If the argument `uncovered` is 1, the rule is considered to cover the difference between the sums of gradients
-        and Hessians that are stored in the arrays `total_sums_of_gradients` and `sums_of_gradients` and
-        `total_sums_of_hessians` and `sums_of_hessians`, respectively.
-
-        :param label_indices:           An array of dtype `intp`, shape `(num_gradients)`, representing the indices of
-                                        the labels for which the rule should predict or None, if the rule should predict
-                                        for all labels
-        :param total_sums_of_gradients: An array of dtype `float64`, shape `(num_gradients), representing the total sums
-                                        of gradients for individual labels
-        :param sums_of_gradients:       An array of dtype `float64`, shape `(num_gradients)`, representing the sums of
-                                        gradients for individual labels
-        :param total_sums_of_hessians:  An array of dtype `float64`, shape
-                                        `((num_gradients + (num_gradients + 1)) // 2)`, representing the total sums of
-                                        Hessians for individual labels
-        :param sums_of_hessians:        An array of dtype `float64`, shape
-                                        `((num_gradients + (num_gradients + 1)) // 2)`, representing the sums of
-                                        Hessians for individual labels
-        :param uncovered:               0, if the rule covers the sums of gradient and Hessians that are stored in the
-                                        array `sums_of_gradients` and `sums_of_hessians`, 1, if the rule covers the
-                                        difference between the sums of gradients and Hessians that are stored in the
-                                        arrays `total_sums_of_gradients` and `sums_of_gradients` and
-                                        `total_sums_of_hessians` and `sums_of_hessians`, respectively.
-        :param prediction:              A pointer to an object of type `LabelWisePrediction` that should be used to
-                                        store the predicted scores and quality scores
-        """
-        cdef ExampleWiseRuleEvaluationImpl* rule_evaluation = self.rule_evaluation
-        cdef const intp* label_indices_ptr = <const intp*>NULL if label_indices is None else &label_indices[0]
-        rule_evaluation.calculateLabelWisePrediction(label_indices_ptr, &total_sums_of_gradients[0],
-                                                     &sums_of_gradients[0], &total_sums_of_hessians[0],
-                                                     &sums_of_hessians[0], uncovered, prediction)
-
-    cdef void calculate_example_wise_prediction(self, const intp[::1] label_indices,
-                                                const float64[::1] total_sums_of_gradients,
-                                                float64[::1] sums_of_gradients,
-                                                const float64[::1] total_sums_of_hessians,
-                                                float64[::1] sums_of_hessians, bint uncovered,
-                                                Prediction* prediction):
-        """
-        Calculates the scores to be predicted by a rule, as well as an overall quality score, based on the sums of
-        gradients and Hessians that are covered by the rule. The predicted scores and quality scores are stored in a
-        given object of type `Prediction`.
-
-        If the argument `uncovered` is 1, the rule is considered to cover the difference between the sums of gradients
-        and Hessians that are stored in the arrays `total_sums_of_gradients` and `sums_of_gradients` and
-        `total_sums_of_hessians` and `sums_of_hessians`, respectively.
-
-        :param label_indices:           An array of dtype `intp`, shape `(num_gradients)`, representing the indices of
-                                        the labels for which the rule should predict or None, if the rule should predict
-                                        for all labels
-        :param total_sums_of_gradients: An array of dtype `float64`, shape `(num_gradients), representing the total sums
-                                        of gradients for individual labels
-        :param sums_of_gradients:       An array of dtype `float64`, shape `(prediction.numPredictions_)`, representing
-                                        the sums of gradients for individual labels
-        :param total_sums_of_hessians:  An array of dtype `float64`, shape
-                                        `((num_gradients + (num_gradients + 1)) // 2)`, representing the total sums of
-                                        Hessians for individual labels
-        :param sums_of_hessians:        An array of dtype `float64`, shape
-                                        `((prediction.numPredictions_ + (prediction.numPredictions_ + 1)) // 2)`,
-                                        representing the sums of Hessians for individual labels
-        :param uncovered:               0, if the rule covers the sums of gradient and Hessians that are stored in the
-                                        array `sums_of_gradients` and `sums_of_hessians`, 1, if the rule covers the
-                                        difference between the sums of gradients and Hessians that are stored in the
-                                        arrays `total_sums_of_gradients` and `sums_of_gradients` and
-                                        `total_sums_of_hessians` and `sums_of_hessians`, respectively.
-        :param prediction:              A pointer to an object of type `Prediction` that should be used to store the
-                                        predicted scores and quality score
-        """
-        cdef ExampleWiseRuleEvaluationImpl* rule_evaluation = self.rule_evaluation
-        cdef const intp* label_indices_ptr = <const intp*>NULL if label_indices is None else &label_indices[0]
-        rule_evaluation.calculateExampleWisePrediction(label_indices_ptr, &total_sums_of_gradients[0],
-                                                       &sums_of_gradients[0], &total_sums_of_hessians[0],
-                                                       &sums_of_hessians[0], uncovered, prediction)
