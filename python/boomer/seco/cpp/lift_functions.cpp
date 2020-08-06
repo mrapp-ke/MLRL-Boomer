c108c5bfd58f74b16dcfefd8c15234e2a5543fe1
Michael Rapp <michael.rapp90@googlemail.com>
2020-08-06 18:18:17 +0200

    Add C++ classes AbstractLiftFunction and PeakLiftFunctionImpl.

---
+++ python/boomer/seco/cpp/lift_functions.cpp
@@ -0,0 +1,46 @@
+#include "lift_functions.h"
+#include <math.h>
+
+using namespace seco;
+
+
+AbstractLiftFunction::~AbstractLiftFunction() {
+
+}
+
+float64 AbstractLiftFunction::calculateLift(intp numLabels) {
+    return 0;
+}
+
+float64 AbstractLiftFunction::getMaxLift() {
+    return 0;
+}
+
+PeakLiftFunctionImpl::PeakLiftFunctionImpl(intp numLabels, intp peakLabel, float64 maxLift, float64 curvature) {
+    numLabels_ = numLabels;
+    peakLabel_ = peakLabel;
+    maxLift_ = maxLift;
+    curvature_ = curvature;
+}
+
+PeakLiftFunctionImpl::~PeakLiftFunctionImpl() {
+
+}
+
+float64 PeakLiftFunctionImpl::calculateLift(intp numLabels) {
+    float64 normalization;
+
+    if (numLabels < peakLabel_) {
+        normalization = (numLabels - 1) / ((float64) (peakLabel_ - 1));
+    } else if (numLabels > peakLabel_) {
+        normalization = (numLabels - numLabels_) / ((float64) numLabels_ - peakLabel_);
+    } else {
+        return maxLift_;
+    }
+
+    return 1 + pow(normalization, curvature_) * (maxLift_ - 1);
+}
+
+float64 PeakLiftFunctionImpl::getMaxLift() {
+    return maxLift_;
+}
