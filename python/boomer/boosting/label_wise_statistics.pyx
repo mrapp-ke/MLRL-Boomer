9362189b1af03178e4474a93923340a936b83001
Michael Rapp <michael.rapp90@googlemail.com>
2020-08-28 17:01:38 +0200

    Remove Cython class Statistics and all of its subclasses.

--- python/boomer/boosting/label_wise_statistics.pyx
+++ python/boomer/boosting/label_wise_statistics.pyx
@@ -8,24 +8,7 @@ from boomer.common.input_data cimport RandomAccessLabelMatrix
 from boomer.boosting.label_wise_losses cimport LabelWiseLoss
 from boomer.boosting.label_wise_rule_evaluation cimport LabelWiseRuleEvaluation
 
-from libcpp.memory cimport unique_ptr, make_shared
-
-
-cdef class LabelWiseStatistics(GradientStatistics):
-    """
-    A wrapper for the C++ class `LabelWiseStatisticsImpl`.
-    """
-
-    def __cinit__(self, LabelWiseLoss loss_function, LabelWiseRuleEvaluation rule_evaluation):
-        """
-        :param loss_function:   The loss function to be used for calculating gradients and Hessians
-        :param rule_evaluation: The `LabelWiseRuleEvaluation` to be used for calculating the predictions, as well as
-                                corresponding quality scores, of rules
-        """
-        cdef shared_ptr[AbstractLabelWiseLoss] loss_function_ptr = loss_function.loss_function_ptr
-        cdef shared_ptr[LabelWiseRuleEvaluationImpl] rule_evaluation_ptr = rule_evaluation.rule_evaluation_ptr
-        self.statistics_ptr = <shared_ptr[AbstractStatistics]>make_shared[LabelWiseStatisticsImpl](loss_function_ptr,
-                                                                                                   rule_evaluation_ptr)
+from libcpp.memory cimport unique_ptr
 
 
 cdef class LabelWiseStatisticsFactory(StatisticsFactory):
