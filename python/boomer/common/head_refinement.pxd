f84c8e7833c55c19ee6eaa954ece417201b50f2f
Michael Rapp <michael.rapp90@googlemail.com>
2020-09-01 15:25:45 +0200

    Move class FullHeadRefinement to "common" package.

--- python/boomer/common/head_refinement.pxd
+++ python/boomer/common/head_refinement.pxd
@@ -25,3 +25,15 @@ cdef class SingleLabelHeadRefinement(HeadRefinement):
 
     cdef PredictionCandidate* calculate_prediction(self, AbstractRefinementSearch* refinement_search, bint uncovered,
                                                    bint accumulated) nogil
+
+
+cdef class FullHeadRefinement(HeadRefinement):
+
+    # Functions:
+
+    cdef PredictionCandidate* find_head(self, PredictionCandidate* best_head, PredictionCandidate* recyclable_head,
+                                        const intp* label_indices, AbstractRefinementSearch* refinement_search,
+                                        bint uncovered, bint accumulated) nogil
+
+    cdef PredictionCandidate* calculate_prediction(self, AbstractRefinementSearch* refinement_search, bint uncovered,
+                                                   bint accumulated) nogil
--- python/boomer/common/head_refinement.pyx
+++ python/boomer/common/head_refinement.pyx
@@ -121,3 +121,52 @@ cdef class SingleLabelHeadRefinement(HeadRefinement):
     cdef PredictionCandidate* calculate_prediction(self, AbstractRefinementSearch* refinement_search, bint uncovered,
                                                    bint accumulated) nogil:
         return refinement_search.calculateLabelWisePrediction(uncovered, accumulated)
+
+
+cdef class FullHeadRefinement(HeadRefinement):
+    """
+    Allows to find the best multi-label head that predicts for all labels.
+    """
+
+    cdef PredictionCandidate* find_head(self, PredictionCandidate* best_head, PredictionCandidate* recyclable_head,
+                                        const intp* label_indices, AbstractRefinementSearch* refinement_search,
+                                        bint uncovered, bint accumulated) nogil:
+        cdef PredictionCandidate* prediction = refinement_search.calculateExampleWisePrediction(uncovered, accumulated)
+        cdef intp num_predictions = prediction.numPredictions_
+        cdef float64* predicted_scores = prediction.predictedScores_
+        cdef float64 overall_quality_score = prediction.overallQualityScore_
+        cdef intp* candidate_label_indices = NULL
+        cdef float64* candidate_predicted_scores
+        cdef intp c
+
+        # The quality score must be better than that of `best_head`...
+        if best_head == NULL or overall_quality_score < best_head.overallQualityScore_:
+            if recyclable_head == NULL:
+                # Create a new `PredictionCandidate` and return it...
+                candidate_predicted_scores = <float64*>malloc(num_predictions * sizeof(float64))
+
+                for c in range(num_predictions):
+                    candidate_predicted_scores[c] = predicted_scores[c]
+
+                if label_indices != NULL:
+                    candidate_label_indices = <intp*>malloc(num_predictions * sizeof(intp))
+
+                    for c in range(num_predictions):
+                        candidate_label_indices[c] = label_indices[c]
+
+                return new PredictionCandidate(num_predictions, candidate_label_indices, candidate_predicted_scores,
+                                               overall_quality_score)
+            else:
+                # Modify the `recyclable_head` and return it...
+                for c in range(num_predictions):
+                    recyclable_head.predictedScores_[c] = predicted_scores[c]
+
+                recyclable_head.overallQualityScore_ = overall_quality_score
+                return recyclable_head
+
+        # Return NULL, as the quality score of the found head is worse than that of `best_head`...
+        return NULL
+
+    cdef PredictionCandidate* calculate_prediction(self, AbstractRefinementSearch* refinement_search, bint uncovered,
+                                                   bint accumulated) nogil:
+        return refinement_search.calculateExampleWisePrediction(uncovered, accumulated)
