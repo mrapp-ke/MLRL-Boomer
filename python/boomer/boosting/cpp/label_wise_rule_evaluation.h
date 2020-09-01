7dcfbd56fa6d0f3a0b65e49309d3b48ccfd249eb
Michael Rapp <michael.rapp90@googlemail.com>
2020-09-01 22:19:21 +0200

    Remove redundant comment.

--- python/boomer/boosting/cpp/label_wise_rule_evaluation.h
+++ python/boomer/boosting/cpp/label_wise_rule_evaluation.h
@@ -82,36 +82,6 @@ namespace boosting {
 
             ~RegularizedLabelWiseRuleEvaluationImpl();
 
-            /**
-             * Calculates the scores to be predicted by a rule, as well as corresponding quality scores, based on the
-             * label-wise sums of gradients and Hessians that are covered by the rule. The predicted scores and quality
-             * scores are stored in a given object of type `LabelWisePredictionCandidate`.
-             *
-             * If the argument `uncovered` is True, the rule is considered to cover the difference between the sums of
-             * gradients and Hessians that are stored in the arrays `totalSumsOfGradients` and `sumsOfGradients` and
-             * `totalSumsOfHessians` and `sumsOfHessians`, respectively.
-             *
-             * @param labelIndices          A pointer to an array of type `intp`, shape `(prediction.numPredictions_)`,
-             *                              representing the indices of the labels for which the rule should predict or
-             *                              NULL, if the rule should predict for all labels
-             * @param totalSumsOfGradients  A pointer to an array of type `float64`, shape `(num_labels), representing
-             *                              the total sums of gradients for individual labels
-             * @param sumsOfGradients       A pointer to an array of type `float64`, shape
-             *                              `(prediction.numPredictions_)`, representing the sums of gradients for
-             *                              individual labels
-             * @param totalSumsOfHessians   A pointer to an array of type `float64`, shape `(num_labels)`, representing
-             *                              the total sums of Hessians for individual labels
-             * @param sumsOfHessians        A pointer to an array of type `float64`, shape
-             *                              `(prediction.numPredictions_)`, representing the sums of Hessians for
-             *                              individual labels
-             * @param uncovered             False, if the rule covers the sums of gradient and Hessians that are stored
-             *                              in the array `sumsOfGradients` and `sumsOfHessians`, True, if the rule
-             *                              covers the difference between the sums of gradients and Hessians that are
-             *                              stored in the arrays `totalSumsOfGradients` and `sumsOfGradients` and
-             *                              `totalSumsOfHessians` and `sumsOfHessians`, respectively
-             * @param prediction            A pointer to an object of type `LabelWisePredictionCandidate` that should be
-             *                              used to store the predicted scores and quality scores
-             */
             void calculateLabelWisePrediction(const intp* labelIndices, const float64* totalSumsOfGradients,
                                               float64* sumsOfGradients, const float64* totalSumsOfHessians,
                                               float64* sumsOfHessians, bool uncovered,
