a301dd90b3c3aedb59f6d93b025429a9ac4e5c68
Michael Rapp <michael.rapp90@googlemail.com>
2020-09-03 01:22:29 +0200

    Add attribute "predictMajority_" to class HeuristicLabelWiseRuleEvaluationImpl.

--- python/boomer/seco/label_wise_rule_evaluation.pyx
+++ python/boomer/seco/label_wise_rule_evaluation.pyx
@@ -21,9 +21,11 @@ cdef class HeuristicLabelWiseRuleEvaluation(LabelWiseRuleEvaluation):
     A wrapper for the C++ class `HeuristicLabelWiseRuleEvaluationImpl`.
     """
 
-    def __cinit__(self, Heuristic heuristic):
+    def __cinit__(self, Heuristic heuristic, bint predictMajority = False):
         """
-        :param heuristic: The heuristic that should be used
+        :param heuristic:       The heuristic that should be used
+        :param predictMajority: True, if for each label the majority label should be predicted, False, if the minority
+                                label should be predicted
         """
         self.rule_evaluation_ptr = <shared_ptr[AbstractLabelWiseRuleEvaluation]>make_shared[HeuristicLabelWiseRuleEvaluationImpl](
-            heuristic.heuristic_ptr)
+            heuristic.heuristic_ptr, predictMajority)
