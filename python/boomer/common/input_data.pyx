02876e4be8f6aabf7bee321fd8f7ffb2ec53d9b2
Michael Rapp <michael.rapp90@googlemail.com>
2020-09-07 18:34:08 +0200

    Move utility functions to tuples.h and add corresponding Cython wrappers to _tuples.pxd.

--- python/boomer/common/input_data.pyx
+++ python/boomer/common/input_data.pyx
@@ -4,7 +4,7 @@
 Provides classes that provide access to the data that is provided for training.
 """
 from boomer.common._arrays cimport uint32
-from boomer.common._tuples cimport IndexedFloat32, compare_indexed_float32
+from boomer.common._tuples cimport IndexedFloat32, compareIndexedFloat32
 
 from libc.stdlib cimport qsort, malloc
 
@@ -124,7 +124,7 @@ cdef class DenseFeatureMatrix(FeatureMatrix):
             sorted_array[i].index = i
             sorted_array[i].value = x[i, feature_index]
 
-        qsort(sorted_array, num_elements, sizeof(IndexedFloat32), &compare_indexed_float32)
+        qsort(sorted_array, num_elements, sizeof(IndexedFloat32), &compareIndexedFloat32)
 
         # Update the given struct...
         indexed_array.numElements = num_elements
@@ -182,7 +182,7 @@ cdef class CscFeatureMatrix(FeatureMatrix):
                 sorted_array[i].value = x_data[j]
                 i += 1
 
-            qsort(sorted_array, num_elements, sizeof(IndexedFloat32), &compare_indexed_float32)
+            qsort(sorted_array, num_elements, sizeof(IndexedFloat32), &compareIndexedFloat32)
 
         # Update the given struct...
         indexed_array.numElements = num_elements
