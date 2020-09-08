8e31698cfb4dc8a284c41d2fd970acb85aac7e1e
LukasEberle <lukas.eberle95@gmail.com>
2020-09-08 11:42:50 +0200

    changed observer according to feedback

--- python/boomer/common/cpp/binning_observer.cpp
+++ python/boomer/common/cpp/binning_observer.cpp
@@ -1,9 +1,5 @@
 #include "binning_observer.h"
 
-void BinningObserver::onBinUpdate(intp binIndex, IndexedFloat32* updatedMatrix){
+void BinningObserver::onBinUpdate(intp binIndex, IndexedFloat32* indexedValue){
 
-}
-
-BinningObserver::BinningObserver(AbstractBinning* toWatch){
-    toWatch->addObserver(this);
 }
