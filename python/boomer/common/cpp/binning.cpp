ab728c7795e6509b83644cef6aeb12bb1156d5be
LukasEberle <lukas.eberle95@gmail.com>
2020-09-15 10:43:27 +0200

    preparing for observer

--- python/boomer/common/cpp/binning.cpp
+++ python/boomer/common/cpp/binning.cpp
@@ -25,30 +25,16 @@ void EqualFrequencyBinning::createBins(IndexedFloat32Array* indexedArray, Binnin
         //TODO: inform observer that no approximation is necessary
         return;
     }
-    /*  Simple Python Implementation as example
-     *  def equal_frequency(arr, k):    #arr -> originalMatrix, k -> numBins_
-     *      length = len(arr)           #length of originalMatrix
-     *      n = int(length/k)   #Number of elements per bin
-     *      result = []                 #has to be allocated - size numBins_
-     *      for i in range(0, k):       #for(intp i = 0; i < numBins_; i++)
-     *          tmp = []                #has not to be allocated - size number of features
-     *          for j in range(i * n, (i + 1) * n): #for(intp j = i * n; j < (i + 1) * n; j++)
-     *              if j >= length:     #break out if j >= originalMatrix' length
-     *                  break           #
-     *              tmp = tmp + [arr[j]]#add currently handled row to the bin
-     *          result.append(tmp)      #write tmp to i-st spot in result array
-     *      return result               #return pointer
-     */
     intp n = length/numBins_; //number of elements per bin
-    //Initializing result array
-    IndexedFloat32Array *results = (IndexedFloat32Array*)malloc(sizeof(IndexedFloat32Array));
-    results->numElements = numBins_;
-    IndexedFloat32 *resultData = (IndexedFloat32*)malloc(numBins_ * sizeof(IndexedFloat32));
-    results->data = resultData;
+    //Initializing result array | obsolete bc observer?
+    //IndexedFloat32Array *results = (IndexedFloat32Array*)malloc(sizeof(IndexedFloat32Array));
+    //results->numElements = numBins_;
+    //IndexedFloat32 *resultData = (IndexedFloat32*)malloc(numBins_ * sizeof(IndexedFloat32));
+    //results->data = resultData;
+    IndexedFloat32 *tmp = (IndexedFloat32*)malloc(sizeof(IndexedFloat32));
     //looping over bins
     for(intp i = 0; i < numBins_; i++){
-        float tmp;
-        tmp = 0;
+        tmp->value = 0;
         //looping over feature list between two bins
         for(intp j = i * n; j < ((i + 1) * n); j++){
             //if we would break out of bounds we have to break out of the loop
@@ -56,12 +42,12 @@ void EqualFrequencyBinning::createBins(IndexedFloat32Array* indexedArray, Binnin
                 break;
             }
             //here we aggregate the values
-            tmp = tmp + indexedArray->data[j].value;
+            tmp->value = tmp->value + indexedArray->data[j].value;
         }
-        results->data[i].value = tmp;
-        tmp = 0;
+        tmp->index = i;
+        observer->onBinUpdate(i, tmp);
+        tmp->value = 0;
     }
-    //TODO: Inform observer that a new matrix is available
 }
 
 
@@ -76,50 +62,34 @@ void EqualWidthBinning::createBins(IndexedFloat32Array* indexedArray, BinningObs
         //TODO: inform observer that no approximation is necessary
         return;
     }
-    /*  Simple Python Implementation as example
-     *  def equal_width(arr, k):    #arr -> originalMatrix, k -> numBins_
-     *                              #should define length as originalMatrix size
-     *      min_arr = min(arr)      #originalMatrix[0][currentFeature]
-     *      w = int(ceil((max(arr)-min_arr)/k))   #Width of the interval
-     *      result = []             #has to be allocated - size numBins_
-     *      boundaries = []         #has not to allocated - size numBins_+1
-     *      for i in range(0, k+1): #for(intp i = 0; i < numBins_ + 1; i++)
-     *          boundaries = boundaries + [min_arr + w * i]
-     *      for i in range(0, k):   #for(intp i = 0; i < numBins_; i++)
-     *          tmp = []            #has not to be allocated - size number of features
-     *          for j in arr:       #for(intp j = 0; j < length; j++)
-     *              if boundaries[i] <= j < boundaries[i + 1]:  #j = originalMatrix[i][currentFeature]
-     *                  tmp += [j]  #every element from tmp + every element from j
-     *          result += [tmp]     #write tmp to the i-st element of the result array
-     *      return result           #return pointer to the result array
-     */
      //defining minimal and maximum values
      float min = indexedArray->data[0].value;
      intp bound_min = floor(min);
      float max = indexedArray->data[indexedArray->numElements-1].value;
      //w stands for width and determines the span of values for a bin
      intp w = intp(ceil((max - min)/numBins_));
-      //Initializing result array
-     IndexedFloat32Array *results = (IndexedFloat32Array*)malloc(sizeof(IndexedFloat32Array));
-     results->numElements = numBins_;
-     IndexedFloat32 *resultData = (IndexedFloat32*)malloc(numBins_ * sizeof(IndexedFloat32));
-     results->data = resultData;
+      //Initializing result array | obsolete bc observer?
+     //IndexedFloat32Array *results = (IndexedFloat32Array*)malloc(sizeof(IndexedFloat32Array));
+     //results->numElements = numBins_;
+     //IndexedFloat32 *resultData = (IndexedFloat32*)malloc(numBins_ * sizeof(IndexedFloat32));
+     //results->data = resultData;
      //defining the boundaries of bins
      intp boundaries[numBins_ + 1] {0};
      for(intp i = 0; i < numBins_ + 1; i++){
         boundaries[i] = bound_min + w * i;
      }
+     IndexedFloat32 *tmp = (IndexedFloat32*)malloc(sizeof(IndexedFloat32));
      //looping over bins
      for(intp i = 0; i < numBins_; i++){
-        float tmp = 0;
+        tmp->value = 0;
         //looping over the list and adding every element in bin i
         for(intp j = 0; j < length; j++){
             if(boundaries[i]<= j && j < boundaries[i + 1]){
-                tmp = tmp + indexedArray->data[j].value;
+                tmp->value = tmp->value + indexedArray->data[j].value;
             }
         }
-        results->data[i].value = tmp;
+        tmp->index = i;
+        observer->onBinUpdate(i, tmp);
      }
-     //TODO: Inform observer that a new matrix is available
 }
 
