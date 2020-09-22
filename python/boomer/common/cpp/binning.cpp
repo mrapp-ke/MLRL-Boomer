6a4d216f674ad571ca87d3073a6347d4da4138a5
Lukas E <lukas.eberle95@gmail.com>
2020-09-22 11:51:40 +0200

    Updated comments

--- python/boomer/common/cpp/binning.cpp
+++ python/boomer/common/cpp/binning.cpp
@@ -17,35 +17,39 @@ void BinningObserver::onBinUpdate(intp binIndex, IndexedFloat32* indexedValue){
 
 
 void EqualFrequencyBinning::createBins(uint32 numBins, IndexedFloat32Array* indexedArray, BinningObserver* observer){
-    //TODO: Elemente mit selbem Wert sollen in den gleichen Bin
     //TODO: Sortieren
+    //Defining length of the list, because we'll use it at least four times
     intp length = indexedArray->numElements;
-    //Mandatory block skipping the process, if the condition is already satisfied
+    //Throwing an exception if the caller doesn't fulfil the requirement
     if(numBins > length){
         throw std::invalid_argument("numBins has to be less or equal to the length of the example array");
     }
     intp n; //number of elements per Bin
     if((length % numBins) == 0){    //if the division has no residual
-        n = length/numBins;         //n is the normal dievision
+        n = length/numBins;         //n is the normal division
     } else {
         n = length/numBins + 1;     //n is rounded up
     }
     //looping over bins
-    intp index = 0;             //Has to be initialized
-    float last_value = 0.0;     //Has to be initialized
+    intp index = 0;             //Has to be initialized for the first iteration
+    float last_value = 0.0;     //Has to be initialized for the first iteration
     for(intp i = 0; i < length; i++){
+        //if the value is equal to the last one it will be put in the same bin...
         if(last_value != indexedArray->data[i].value){
-            index = i / n;
+            index = i / n;  //... else we calculate it's own bin index
         }
+        //set last value to the current one for the next iteration
         last_value = indexedArray->data[i].value;
+        //notify observer
         observer->onBinUpdate(index, indexedArray->data[i]);
     }
 }
 
 
 void EqualWidthBinning::createBins(uint32 numBins, IndexedFloat32Array* indexedArray, BinningObserver* observer){
+    //Defining length of the list, because we'll use it at least four times
     intp length = indexedArray->numElements;
-    //Mandatory block skipping the process, if the condition is already satisfied
+    //Throwing an exception if the caller doesn't fulfil the requirement
     if(numBins > length){
         throw std::invalid_argument("numBins has to be less or equal to the length of the example array");
     }
@@ -59,15 +63,16 @@ void EqualWidthBinning::createBins(uint32 numBins, IndexedFloat32Array* indexedA
             max = indexedArray->data[i].value;
         }
     }
-    intp bound_min = floor(min);
     //w stands for width and determines the span of values for a bin
     intp w = intp(ceil((max - min)/numBins));
     intp index;
     for(intp i = 0; i < length; i++){
         index = floor((indexedArray->data[i] - min) / w);
+        //in some cases the calculated index can exceed the last bin, in which case we want the example in the last bin
         if(index >= numBins){
             index = numBins - 1;
         }
+        //notify observer
         observer->onBinUpdate(index, indexedArray->data[i]);
     }
 }
