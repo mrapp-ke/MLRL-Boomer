7690259c60a5c928c4202a644e45e7ac5644e7a5
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2020-09-22 15:04:27 +0200

    Edit comments.

--- python/boomer/common/cpp/binning.h
+++ python/boomer/common/cpp/binning.h
@@ -1,15 +1,19 @@
 /**
- * Implements the unsupervised binning methods on examples
+ * Implements different methods for assigning floating point values to bins.
  *
  * @author Lukas Johannes Eberle (lukasjohannes.eberle@stud.tu-darmstadt.de)
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
  */
-
 #pragma once
 
 #include "arrays.h"
 #include "statistics.h"
 #include "tuples.h"
 
+
+/**
+ * Defines an interface to be implemented by classes that should be notified when values are assigned to bins.
+ */
 class IBinningObserver{
 
     public:
@@ -17,15 +21,18 @@ class IBinningObserver{
         virtual ~IBinningObserver() { };
 
         /**
-        *   Will be called everytime an example is assigned to a bin.
-        *
-        *   @param binIndex         Index of the bin the example is assigned to
-        *   @param indexedValue     The example value which is assigned
-        */
+         * Will be called everytime a value is assigned to a bin.
+         *
+         * @param binIndex      Index of the bin the value is assigned to
+         * @param indexedValue  The value and corresponding index which is assigned
+         */
         virtual void onBinUpdate(uint32 binIndex, IndexedFloat32* indexedValue) = 0;
 
 };
 
+/**
+ * Defines an interface for methods that assign floating point values to bins.
+ */
 class IBinning{
 
     public:
@@ -33,46 +40,35 @@ class IBinning{
         virtual ~IBinning() { };
 
         /**
-        *   Must be implemented by subclasses. Create a number of bags and assigns examples to those bags. The results
-        *   will be passed to a observer which handles the bag management.
-        *
-        *   @param numBins          The number of bins which should be considered
-        *   @param indexedArray     An array of examples, which should be put in the bins
-        *   @param observer         The `IBinningObserver` who is notified, when new results are available
-        */
+         * Must be implemented by subclasses. Create a number of bags and assigns values to those bags. The results will
+         * be passed to an observer which handles the bag management.
+         *
+         * @param numBins       The number of bins which should be considered
+         * @param indexedArray  An array of examples, which should be put in the bins
+         * @param observer      The `IBinningObserver` who is notified, when new results are available
+         */
         virtual void createBins(uint32 numBins, IndexedFloat32Array* indexedArray, IBinningObserver* observer) = 0;
 
 };
 
+/**
+ * Assigns floating point values to bins in a way such that each bin contains approximately the same number of values.
+ */
 class EqualFrequencyBinningImpl : virtual public IBinning{
 
     public:
 
-        /**
-        *   Create a number of bags and assigns examples to those bags. The results will be passed to a observer which
-        *   handles the bag management. Each bin will get a fix number of examples with minor differences due to
-        *   divisibility and equality of examples
-        *
-        *   @param numBins          The number of bins which should be considered
-        *   @param indexedArray     An array of examples, which should be put in the bins
-        *   @param observer         The `IBinningObserver` who is notified, when new results are available
-        */
         void createBins(uint32 numBins, IndexedFloat32Array* indexedArray, IBinningObserver* observer) override;
 
 };
 
+/**
+ * Assigns floating point values to bins in a way such that each bin contains values from equally sized value ranges.
+ */
 class EqualWidthBinningImpl : virtual public IBinning{
 
     public:
 
-        /**
-        *   Create a number of bags and assigns examples to those bags. The results will be passed to a observer which
-        *   handles the bag management. Each bin will get examples in a certain range of values
-        *
-        *   @param numBins          The number of bins which should be considered
-        *   @param indexedArray     An array of examples, which should be put in the bins
-        *   @param observer         The `IBinningObserver` who is notified, when new results are available
-        */
         void createBins(uint32 numBins, IndexedFloat32Array* indexedArray, IBinningObserver* observer) override;
 
 };
