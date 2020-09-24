f67178967d79ad87a939444ad8970e78f4f034f3
Michael Rapp <michael.rapp90@googlemail.com>
2020-09-24 14:40:31 +0200

    Change the order of classes.

--- python/boomer/common/cpp/data.cpp
+++ python/boomer/common/cpp/data.cpp
@@ -1,6 +1,22 @@
 #include "data.h"
 
 
+BinaryDokVector::BinaryDokVector(uint32 numElements) {
+    numElements_ = numElements;
+}
+
+uint32 BinaryDokVector::getNumElements() {
+    return numElements_;
+}
+
+void BinaryDokVector::setValue(uint32 pos) {
+    data_.insert(pos);
+}
+
+uint8 BinaryDokVector::getValue(uint32 pos) {
+    return data_.find(pos) != data_.end();
+}
+
 BinaryDokMatrix::BinaryDokMatrix(uint32 numRows, uint32 numCols) {
     numRows_ = numRows;
     numCols_ = numCols;
@@ -20,20 +36,4 @@ void BinaryDokMatrix::setValue(uint32 row, uint32 column) {
 
 uint8 BinaryDokMatrix::getValue(uint32 row, uint32 column) {
     return data_.find(std::make_pair(row, column)) != data_.end();
-}
-
-BinaryDokVector::BinaryDokVector(uint32 numElements) {
-    numElements_ = numElements;
-}
-
-uint32 BinaryDokVector::getNumElements() {
-    return numElements_;
-}
-
-void BinaryDokVector::setValue(uint32 pos) {
-    data_.insert(pos);
-}
-
-uint8 BinaryDokVector::getValue(uint32 pos) {
-    return data_.find(pos) != data_.end();
 }
