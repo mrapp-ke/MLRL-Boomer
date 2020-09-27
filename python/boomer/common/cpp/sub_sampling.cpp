49f764e4fbba831e4ffa70c963a312fd8008d4f0
Michael Rapp <michael.rapp90@googlemail.com>
2020-09-28 00:59:19 +0200

    Add static function "sampleWeightsWithoutReplacement".

--- python/boomer/common/cpp/sub_sampling.cpp
+++ python/boomer/common/cpp/sub_sampling.cpp
@@ -1,5 +1,91 @@
 #include "sub_sampling.h"
+#include <unordered_set>
 
+
+/**
+ * Randomly selects `numSamples` out of `numTotal` elements and sets their weights to 1, while the remaining weights are
+ * set to 0, by using a set to keep track of the elements that have already been selected. This method is suitable if
+ * `numSamples` is much smaller than `numTotal`.
+ *
+ * @param numTotal      The total number of available elements
+ * @param numSamples    The number of weights to be set to 1
+ * @param rng           A pointer to an object of type `RNG`, implementing the random number generator to be used
+ * @return              A pointer to an object of type `IWeightVector` that provides access to the weights
+ */
+static inline IWeightVector* sampleWeightsWithoutReplacementViaTrackingSelection(uint32 numTotal, uint32 numSamples,
+                                                                                 RNG* rng) {
+    DenseVector<uint8>* weights = new DenseVector<uint8>(numTotal);
+    std::unordered_set<uint32> selectedIndices;
+
+    for (uint32 i = 0; i < numSamples; i++) {
+        bool shouldContinue = true;
+        uint32 randomIndex;
+
+        while (shouldContinue) {
+            randomIndex = rng->random(0, numTotal);
+            shouldContinue = !selectedIndices.insert(randomIndex).second;
+        }
+
+        weights->setValue(randomIndex, 1);
+    }
+
+    return new DenseWeightVector<uint8>(weights, numSamples);
+}
+
+/**
+ * Randomly selects `numSamples` out of `numTotal` elements and sets their weights to 1, while the remaining weights are
+ * set to 0, by using a pool, i.e., an array, to keep track of the elements that have not been selected yet.
+ *
+ * @param numTotal      The total number of available elements
+ * @param numSamples    The number of weights to be set to 1
+ * @param rng           A pointer to an object of type `RNG`, implementing the random number generator to be used
+ * @return              A pointer to an object of type `IWeightVector` that provides access to the weights
+ */
+static inline IWeightVector* sampleWeightsWithoutReplacementViaPool(uint32 numTotal, uint32 numSamples, RNG* rng) {
+    DenseVector<uint8>* weights = new DenseVector<uint8>(numTotal);
+    uint32 pool[numTotal];
+
+    // Initialize pool...
+    for (uint32 i = 0; i < numTotal; i++) {
+        pool[i] = i;
+    }
+
+    for (uint32 i = 0; i < numSamples; i++) {
+        // Randomly select an index that has not been drawn yet...
+        uint32 randomIndex = rng->random(0, numTotal - i);
+        uint32 j = pool[randomIndex];
+
+        // Set weight at the selected index to 1...
+        weights->setValue(j, 1);
+
+        // Move the index at the border to the position of the recently drawn index...
+        pool[randomIndex] = pool[numTotal - i - 1];
+    }
+
+    return new DenseWeightVector<uint8>(weights, numSamples);
+}
+
+/**
+ * Randomly selects `numSamples` out of `numTotal` elements and sets their weights to 1, while the remaining weights are
+ * set to 0. The method that is used internally is chosen automatically, depending on the ratio `numSamples / numTotal`.
+ *
+ * @param numTotal      The total number of available elements
+ * @param numSamples    The number of weights to be set to 1
+ * @param rng           A pointer to an object of type `RNG`, implementing the random number generator to be used
+ * @return              A pointer to an object of type `IWeightVector` that provides access to the weights
+ *
+ */
+static inline IWeightVector* sampleWeightsWithoutReplacement(uint32 numTotal, uint32 numSamples, RNG* rng) {
+    float64 ratio = numTotal > 0 ? ((float64) numSamples) / ((float64) numTotal) : 1;
+
+    if (ratio < 0.06) {
+        // For very small ratios use tracking selection
+        return sampleWeightsWithoutReplacementViaTrackingSelection(numTotal, numSamples, rng);
+    } else {
+        // Otherwise, use a pool as the default method
+        return sampleWeightsWithoutReplacementViaPool(numTotal, numSamples, rng);
+    }
+}
 
 template<class T>
 DenseWeightVector<T>::DenseWeightVector(DenseVector<T>* weights, uint32 sumOfWeights) {
