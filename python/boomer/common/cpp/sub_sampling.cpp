98f5c04b6bc907c0a9c8e48b643d48b72e4c4e0d
Michael Rapp <michael.rapp90@googlemail.com>
2020-09-28 01:26:03 +0200

    Add C++ class RandomInstanceSubsetSelectionImpl.

--- python/boomer/common/cpp/sub_sampling.cpp
+++ python/boomer/common/cpp/sub_sampling.cpp
@@ -291,6 +291,15 @@ IWeightVector* BaggingImpl::subSample(uint32 numExamples, RNG* rng) {
     return new DenseWeightVector(weights, numSamples);
 }
 
+RandomInstanceSubsetSelectionImpl::RandomInstanceSubsetSelectionImpl(float32 sampleSize) {
+    sampleSize_ = sampleSize;
+}
+
+IWeightVector* RandomInstanceSubsetSelectionImpl::subSample(uint32 numExamples, RNG* rng) {
+    uint32 numSamples = (uint32) (sampleSize_ * numExamples);
+    return sampleWeightsWithoutReplacement(numExamples, numSamples, rng);
+}
+
 IWeightVector* NoInstanceSubSamplingImpl::subSample(uint32 numExamples, RNG* rng) {
     return new EqualWeightVector(numExamples);
 }
--- python/boomer/common/cpp/sub_sampling.h
+++ python/boomer/common/cpp/sub_sampling.h
@@ -132,6 +132,28 @@ class BaggingImpl : virtual public IInstanceSubSampling {
 };
 
 /**
+ * Implements random instance subset selection for selecting a subset of the available training examples without
+ * replacement.
+ */
+class RandomInstanceSubsetSelectionImpl : virtual public IInstanceSubSampling {
+
+    private:
+
+        float32 sampleSize_;
+
+    public:
+
+        /**
+         * @param sampleSize The fraction of examples to be included in the sample (e.g. a value of 0.6 corresponds to
+         *                   60 % of the available examples). Must be in (0, 1)
+         */
+        RandomInstanceSubsetSelectionImpl(float32 sampleSize);
+
+        IWeightVector* subSample(uint32 numExamples, RNG* rng) override;
+
+};
+
+/**
  * An implementation of the class `IInstanceSubSampling` that does not perform any sampling, but assigns equal weights
  * to all examples.
  */
