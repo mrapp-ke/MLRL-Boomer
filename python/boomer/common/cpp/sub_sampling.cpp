ff3375428b4668cad1d587436e1cdc01f98d4e7c
Michael Rapp <michael.rapp90@googlemail.com>
2020-09-28 01:15:50 +0200

    Add static function "sampleIndicesWithoutReplacement".

--- python/boomer/common/cpp/sub_sampling.cpp
+++ python/boomer/common/cpp/sub_sampling.cpp
@@ -87,6 +87,139 @@ static inline IWeightVector* sampleWeightsWithoutReplacement(uint32 numTotal, ui
     }
 }
 
+/**
+ * Randomly selects `numSamples` out of `numTotal` indices without replacement by using a set to keep track of the
+ * indices that have already been selected. This method is suitable if `numSamples` is much smaller than `numTotal`
+ *
+ * @param numTotal      The total number of available indices
+ * @param numSamples    The number of indices to be sampled
+ * @param rng           A pointer to an object of type `RNG`, implementing the random number generator to be used
+ * @return              A pointer to an object of type `IIndexVector` that provides access to the indices that are
+ *                      contained in the sub-sample
+ */
+static inline IIndexVector* sampleIndicesWithoutReplacementViaTrackingSelection(uint32 numTotal, uint32 numSamples,
+                                                                                RNG* rng) {
+    DenseIndexVector* indices = new DenseIndexVector(numSamples);
+    std::unordered_set<uint32> selectedIndices;
+
+    for (uint32 i = 0; i < numSamples; i++) {
+        bool shouldContinue = true;
+        uint32 randomIndex;
+
+        while (shouldContinue) {
+            randomIndex = rng->random(0, numTotal);
+            shouldContinue = !selectedIndices.insert(randomIndex).second;
+        }
+
+        indices->setIndex(i, randomIndex);
+    }
+
+    return indices;
+}
+
+/**
+ * Randomly selects `numSamples` out of `numTotal` indices without replacement using a reservoir sampling algorithm.
+ * This method is suitable if `numSamples` is almost as large as `numTotal`.
+ *
+ * @param numTotal      The total number of available indices
+ * @param numSamples    The number of indices to be sampled
+ * @param rng           A pointer to an object of type `RNG`, implementing the random number generator to be used
+ * @return              A pointer to an object of type `IIndexVector` that provides access to the indices that are
+ *                      contained in the sub-sample
+ */
+static inline IIndexVector* sampleIndicesWithoutReplacementViaReservoirSampling(uint32 numTotal, uint32 numSamples,
+                                                                                RNG* rng) {
+    DenseIndexVector* indices = new DenseIndexVector(numSamples);
+
+    for (uint32 i = 0; i < numSamples; i++) {
+        indices->setIndex(i, i);
+    }
+
+    for (uint32 i = numSamples; i < numTotal; i++) {
+        uint32 randomIndex = rng->random(0, i + 1);
+
+        if (randomIndex < numSamples) {
+            indices->setIndex(randomIndex, i);
+        }
+    }
+
+    return indices;
+}
+
+/**
+ * Randomly selects `numSamples` out of `numTotal` indices without replacement by first generating a random permutation
+ * of the available indices using the Fisher-Yates shuffle and then returning the first `numSamples` indices.
+ *
+ * @param numTotal      The total number of available indices
+ * @param numSamples    The number of indices to be sampled
+ * @param rng           A pointer to an object of type `RNG`, implementing the random number generator to be used
+ * @return              A pointer to an object of type `IIndexVector` that provides access to the indices that are
+ *                      contained in the sub-sample
+ */
+static inline IIndexVector* sampleIndicesWithoutReplacementViaRandomPermutation(uint32 numTotal, uint32 numSamples,
+                                                                                RNG* rng) {
+    DenseIndexVector* indices = new DenseIndexVector(numSamples);
+    uint32* unusedIndices = new uint32[numTotal - numSamples];
+
+    for (uint32 i = 0; i < numSamples; i++) {
+        indices->setIndex(i, i);
+    }
+
+    for (uint32 i = numSamples; i < numTotal; i++) {
+        unusedIndices[i] = i;
+    }
+
+    for (uint32 i = 0; i < numTotal - 2; i++) {
+        // Swap elements at index i and at a randomly selected index...
+        uint32 randomIndex = rng->random(i, numTotal);
+        uint32 tmp1 = randomIndex < numSamples ? indices->getIndex(randomIndex) : unusedIndices[randomIndex];
+        uint32 tmp2;
+
+        if (i < numSamples) {
+            tmp2 = indices->getIndex(i);
+            indices->setIndex(i, tmp1);
+        } else {
+            tmp2 = unusedIndices[i];
+            unusedIndices[i] = tmp1;
+        }
+
+        if (randomIndex < numSamples) {
+            indices->setIndex(randomIndex, tmp2);
+        } else {
+            unusedIndices[randomIndex] = tmp2;
+        }
+    }
+
+    delete[] unusedIndices;
+    return indices;
+}
+
+/**
+ * Randomly selects `numSamples` out of `numTotal` indices without replacement. The method that is used internally is
+ * chosen automatically, depending on the ratio `numSamples / numTotal`.
+ *
+ * @param numTotal      The total number of available indices
+ * @param numSamples    The number of indices to be sampled
+ * @param rng           A pointer to an object of type `RNG`, implementing the random number generator to be used
+ * @return              A pointer to an object of type `IIndexVector` that provides access to the indices that are
+ *                      contained in the sub-sample
+ */
+static inline IIndexVector* sampleIndicesWithoutReplacement(uint32 numTotal, uint32 numSamples, RNG* rng) {
+    float64 ratio = numTotal > 0 ? ((float64) numSamples) / ((float64) numTotal) : 1;
+
+    // The thresholds for choosing a suitable method are based on empirical experiments
+    if (ratio < 0.06) {
+        // For very small ratios use tracking selection
+        return sampleIndicesWithoutReplacementViaTrackingSelection(numTotal, numSamples, rng);
+    } else if (ratio > 0.5) {
+        // For large ratios use reservoir sampling
+        return sampleIndicesWithoutReplacementViaReservoirSampling(numTotal, numSamples, rng);
+    } else {
+        // Otherwise, use random permutation as the default method
+        return sampleIndicesWithoutReplacementViaRandomPermutation(numTotal, numSamples, rng);
+    }
+}
+
 template<class T>
 DenseWeightVector<T>::DenseWeightVector(DenseVector<T>* weights, uint32 sumOfWeights) {
     weights_ = weights;
