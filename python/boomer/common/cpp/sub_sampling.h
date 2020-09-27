fa099e194790a70c5eb83fc45ff9f8939ab54e39
Michael Rapp <michael.rapp90@googlemail.com>
2020-09-28 00:34:10 +0200

    Add C++ class EqualWeightVector.

--- python/boomer/common/cpp/sub_sampling.h
+++ python/boomer/common/cpp/sub_sampling.h
@@ -25,3 +25,29 @@ class IWeightVector : virtual public ISparseRandomAccessVector<uint32> {
         virtual uint32 getSumOfWeights() = 0;
 
 };
+
+/**
+ * An one-dimensional that provides access to equal weights.
+ */
+class EqualWeightVector : virtual public IWeightVector {
+
+    private:
+
+        uint32 numElements_;
+
+    public:
+
+        /**
+         * @param numTotalElements The number of elements in the vector. Must be at least 1
+         */
+        EqualWeightVector(uint32 numElements);
+
+        uint32 getNumElements() override;
+
+        bool hasZeroElements();
+
+        uint32 getValue(uint32 pos) override;
+
+        uint32 getSumOfWeights() override;
+
+};
