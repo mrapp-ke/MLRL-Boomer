d6853be1c41d73452876072f7d20610db0bac82c
Michael Rapp <michael.rapp90@googlemail.com>
2020-09-28 15:01:11 +0200

    Remove class DenseVector.

--- python/boomer/common/cpp/data.cpp
+++ python/boomer/common/cpp/data.cpp
@@ -1,41 +1,6 @@
 #include "data.h"
 
 
-template<class T>
-DenseVector<T>::DenseVector(uint32 numElements)
-    : DenseVector(numElements, false) {
-
-}
-
-template<class T>
-DenseVector<T>::DenseVector(uint32 numElements, bool allZero) {
-    numElements_ = numElements;
-    data_ = allZero ? new T[numElements]{0} : new T[numElements];
-}
-
-template<class T>
-DenseVector<T>::~DenseVector() {
-    delete[] data_;
-}
-
-template<class T>
-uint32 DenseVector<T>::getNumElements() {
-    return numElements_;
-}
-
-template<class T>
-T DenseVector<T>::getValue(uint32 pos) {
-    return data_[pos];
-}
-
-template<class T>
-void DenseVector<T>::setValue(uint32 pos, T value) {
-    data_[pos] = value;
-}
-
-template class DenseVector<uint8>;
-template class DenseVector<uint32>;
-
 RangeIndexVector::RangeIndexVector(uint32 numIndices) {
     numIndices_ = numIndices;
 }
