611b078cc88b2c02715561b4ee3dd3c778cbb7fe
Michael Rapp <michael.rapp90@googlemail.com>
2020-09-28 19:01:54 +0200

    Add Cython class ThresholdsFactory.

---
+++ python/boomer/common/thresholds.pyx
@@ -0,0 +1,26 @@
+"""
+@author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+
+Provides Cython wrappers for classes that provide access to the thresholds that may be used by the conditions of rules.
+"""
+
+
+cdef class ThresholdsFactory:
+    """
+    A base class for all factories that allow to create instances of the class `AbstractThresholds`.
+    """
+
+    cdef AbstractThresholds* create(self, FeatureMatrix feature_matrix, NominalFeatureVector nominal_feature_vector,
+                                    StatisticsProvider statistics_provider):
+        """
+        Creates and returns a new instance of the class `AbstractThresholds`.
+
+        :param feature_matrix:          A `FeatureMatrix` that provides access to the feature values of the training
+                                        examples
+        :param nominal_feature_vector:  A `NominalFeatureVector` that provides access to the information whether
+                                        individual features are nominal or not
+        :param statistics_provider:     A `StatisticsProvider` that provides access to statistics about the labels of
+                                        training examples
+        :return:                        A pointer to an object of type `AbstractThresholds` that has been created
+        """
+        pass
