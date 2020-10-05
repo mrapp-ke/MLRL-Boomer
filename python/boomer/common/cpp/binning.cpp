e8d3f6847ceb793ccae32865736d5c450144672c
Michael Rapp <michael.rapp90@googlemail.com>
2020-10-05 22:06:57 +0200

    Pass arguments by reference.

--- python/boomer/common/cpp/binning.cpp
+++ python/boomer/common/cpp/binning.cpp
@@ -4,22 +4,23 @@
 #include <stdlib.h>
 
 
-void EqualFrequencyBinningImpl::createBins(uint32 numBins, IndexedFloat32Array* indexedArray,
-                                           IBinningObserver* observer) {
+void EqualFrequencyBinningImpl::createBins(uint32 numBins, IndexedFloat32Array& indexedArray,
+                                           IBinningObserver& observer) {
     //Defining length of the list, because we'll use it at least four times
-    uint32 length = indexedArray->numElements;
+    uint32 length = indexedArray.numElements;
+    IndexedFloat32* indexedValues = indexedArray.data;
     //Throwing an exception if the caller doesn't fulfil the requirement
     if (numBins > length) {
         throw std::invalid_argument("numBins has to be less or equal to the length of the example array");
     }
     //Sorting the array
-    qsort(indexedArray, length, sizeof(IndexedFloat32), &tuples::compareIndexedFloat32);
+    qsort(indexedValues, length, sizeof(IndexedFloat32), &tuples::compareIndexedFloat32);
     uint32 numElementsPerBin = (intp) round((float) length / (float) numBins);
     //looping over bins
     uint32 binIndex = 0;  //Has to be initialized for the first iteration
     float32 previousValue = 0.0;  //Has to be initialized for the first iteration
     for (uint32 i = 0; i < length; i++) {
-        float32 currentValue = indexedArray->data[i].value;
+        float32 currentValue = indexedValues[i].value;
         //if the value is equal to the last one it will be put in the same bin...
         if (previousValue != currentValue) {
             binIndex = i / numElementsPerBin;  //... else we calculate it's own bin index
@@ -27,22 +28,23 @@ void EqualFrequencyBinningImpl::createBins(uint32 numBins, IndexedFloat32Array* 
         //set last value to the current one for the next iteration
         previousValue = currentValue;
         //notify observer
-        observer->onBinUpdate(binIndex, &indexedArray->data[i]);
+        observer.onBinUpdate(binIndex, indexedValues[i]);
     }
 }
 
-void EqualWidthBinningImpl::createBins(uint32 numBins, IndexedFloat32Array* indexedArray, IBinningObserver* observer) {
+void EqualWidthBinningImpl::createBins(uint32 numBins, IndexedFloat32Array& indexedArray, IBinningObserver& observer) {
     //Defining length of the list, because we'll use it at least four times
-    uint32 length = indexedArray->numElements;
+    uint32 length = indexedArray.numElements;
+    IndexedFloat32* indexedValues = indexedArray.data;
     //Throwing an exception if the caller doesn't fulfil the requirement
     if (numBins > length) {
         throw std::invalid_argument("numBins has to be less or equal to the length of the example array");
     }
     //defining minimal and maximum values
-    float32 min = indexedArray->data[0].value;
+    float32 min = indexedValues[0].value;
     float32 max = min;
     for (uint32 i = 1; i < length; i++) {
-        float32 currentValue = indexedArray->data[i].value;
+        float32 currentValue = indexedValues[i].value;
 
         if (currentValue < min) {
             min = currentValue;
@@ -54,13 +56,13 @@ void EqualWidthBinningImpl::createBins(uint32 numBins, IndexedFloat32Array* inde
     float32 spanPerBin = (max - min) / numBins;
 
     for (uint32 i = 0; i < length; i++) {
-        float32 currentValue = indexedArray->data[i].value;
+        float32 currentValue = indexedValues[i].value;
         uint32 binIndex = (uint32) floor((currentValue - min) / spanPerBin);
         //in some cases the calculated index can exceed the last bin, in which case we want the example in the last bin
         if (binIndex >= numBins) {
             binIndex = numBins - 1;
         }
         //notify observer
-        observer->onBinUpdate(binIndex, &indexedArray->data[i]);
+        observer.onBinUpdate(binIndex, indexedValues[i]);
     }
 }
