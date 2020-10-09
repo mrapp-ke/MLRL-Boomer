13f13f47f9d84c144af5e1890009fa66761ba2e7
Michael Rapp <michael.rapp90@googlemail.com>
2020-10-09 12:23:13 +0200

    Add function "fetchFeatureVector" to class IFeatureMatrix.

--- python/boomer/common/cpp/input_data.h
+++ python/boomer/common/cpp/input_data.h
@@ -115,6 +115,17 @@ class IFeatureMatrix : virtual public IMatrix {
          */
         virtual void fetchFeatureValues(uint32 featureIndex, IndexedFloat32Array& indexedArray) const = 0;
 
+        /**
+         * Fetches a feature vector that stores the indices of the training examples, as well as their feature values,
+         * for a specific feature and stores it in a given unique pointer.
+         *
+         * @param featureIndex      The index of the feature
+         * @param featureVectorPtr  An unique pointer to an object of type `FeatureVector` that should be used to store
+         *                          the feature vector
+         */
+        virtual void fetchFeatureVector(uint32 featureIndex,
+                                        std::unique_ptr<FeatureVector>& featureVectorPtr) const = 0;
+
 };
 
 /**
@@ -145,6 +156,8 @@ class DenseFeatureMatrixImpl : virtual public IFeatureMatrix {
         uint32 getNumCols() const override;
 
         void fetchFeatureValues(uint32 featureIndex, IndexedFloat32Array& indexedArray) const override;
+
+        void fetchFeatureVector(uint32 featureIndex, std::unique_ptr<FeatureVector>& featureVectorPtr) const override;
 
 };
 
@@ -187,6 +200,8 @@ class CscFeatureMatrixImpl : virtual public IFeatureMatrix {
         uint32 getNumCols() const override;
 
         void fetchFeatureValues(uint32 featureIndex, IndexedFloat32Array& indexedArray) const override;
+
+        void fetchFeatureVector(uint32 featureIndex, std::unique_ptr<FeatureVector>& featureVectorPtr) const override;
 
 };
 
