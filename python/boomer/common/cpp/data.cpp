f78438e8f6e649e51170c2c888cfa0f356012f03
Michael Rapp <michael.rapp90@googlemail.com>
2020-10-09 12:13:33 +0200

    Add class SparseArrayVector.

--- python/boomer/common/cpp/data.cpp
+++ python/boomer/common/cpp/data.cpp
@@ -1,4 +1,6 @@
 #include "data.h"
+#include <algorithm>
+#include <cstdlib>
 
 
 template<class T>
@@ -66,6 +68,64 @@ uint32 RangeIndexVector::getNumElements() const {
 uint32 RangeIndexVector::getValue(uint32 pos) const {
     return pos;
 }
+
+template<class T>
+SparseArrayVector<T>::SparseArrayVector(uint32 numElements)
+    : array_((Entry*) malloc(numElements * sizeof(Entry))), numElements_(numElements) {
+
+}
+
+template<class T>
+SparseArrayVector<T>::~SparseArrayVector() {
+    free(array_);
+}
+
+template<class T>
+uint32 SparseArrayVector<T>::getNumElements() const {
+    return numElements_;
+}
+
+template<class T>
+void SparseArrayVector<T>::setNumElements(uint32 numElements) {
+    if (numElements != numElements_) {
+        numElements_ = numElements;
+        array_ = (Entry*) realloc(array_, numElements * sizeof(Entry));
+    }
+}
+
+template<class T>
+typename SparseArrayVector<T>::iterator SparseArrayVector<T>::begin() {
+    return &array_[0];
+}
+
+template<class T>
+typename SparseArrayVector<T>::iterator SparseArrayVector<T>::end() {
+    return &array_[numElements_];
+}
+
+template<class T>
+typename SparseArrayVector<T>::const_iterator SparseArrayVector<T>::cbegin() const {
+    return &array_[0];
+}
+
+template<class T>
+typename SparseArrayVector<T>::const_iterator SparseArrayVector<T>::cend() const {
+    return &array_[numElements_];
+}
+
+template<class T>
+void SparseArrayVector<T>::sortByValues() {
+    struct {
+
+        bool operator()(SparseArrayVector<T>::Entry a, SparseArrayVector<T>::Entry b) const {
+            return a.value < b.value;
+        }
+
+    } comparator;
+    std::sort(this->begin(), this->end(), comparator);
+}
+
+template class SparseArrayVector<float32>;
 
 BinaryDokVector::BinaryDokVector(uint32 numElements)
     : numElements_(numElements) {
