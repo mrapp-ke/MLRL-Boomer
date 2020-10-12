b9b271ae1fd82a4f6569ab5bbf9915ec08dd066a
Michael Rapp <michael.rapp90@googlemail.com>
2020-10-12 02:02:58 +0200

    Remove TODO.

--- python/boomer/common/cpp/rule_refinement.h
+++ python/boomer/common/cpp/rule_refinement.h
@@ -69,7 +69,6 @@ class IRuleRefinementCallback {
          * @param featureIndex  The index of the feature
          * @return              A reference to an object of template type `T` that stores the information
          */
-        // TODO make function const
         virtual T& get(uint32 featureIndex) = 0;
 
 };
