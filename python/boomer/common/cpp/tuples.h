737abd75fc4dc94d22f4bbe45f4035a9e1f59f36
Michael Rapp <michael.rapp90@googlemail.com>
2020-10-12 16:27:40 +0200

    Use struct IndexedValue in class PartialHeadRefinement.

--- python/boomer/common/cpp/tuples.h
+++ python/boomer/common/cpp/tuples.h
@@ -35,15 +35,6 @@ struct IndexedFloat32Array {
     uint32 numElements;
 };
 
-/**
- * A struct that stores a value of type `float64` and a corresponding index that refers to the (original) position of
- * the value in an array.
- */
-struct IndexedFloat64 {
-    uint32 index;
-    float64 value;
-};
-
 
 namespace tuples {
 
@@ -59,20 +50,6 @@ namespace tuples {
     static inline int compareIndexedValue(const void* a, const void* b) {
         T v1 = ((IndexedValue<T>*) a)->value;
         T v2 = ((IndexedValue<T>*) b)->value;
-        return v1 < v2 ? -1 : (v1 == v2 ? 0 : 1);
-    }
-
-    /**
-     * Compares the values of two structs of type `IndexedFloat64`.
-     *
-     * @param a A pointer to the first struct
-     * @param b A pointer to the second struct
-     * @return  -1 if the value of the first struct is smaller than the value of the second struct, 0 if both values are
-     *          equal, or 1 if the value of the first struct is greater than the value of the second struct
-     */
-    static inline int compareIndexedFloat64(const void* a, const void* b) {
-        float64 v1 = ((IndexedFloat64*) a)->value;
-        float64 v2 = ((IndexedFloat64*) b)->value;
         return v1 < v2 ? -1 : (v1 == v2 ? 0 : 1);
     }
 
