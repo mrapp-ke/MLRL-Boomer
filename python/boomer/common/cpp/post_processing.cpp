8071ff56ec5a84e393c4efac9b32ed939c64df0a
Michael Rapp <michael.rapp90@googlemail.com>
2020-10-12 21:19:08 +0200

    Add files post_processing.h/cpp.

---
+++ python/boomer/common/cpp/post_processing.cpp
@@ -0,0 +1,6 @@
+#include "post_processing.h"
+
+
+void NoPostProcessor::postProcess(Prediction& prediction) const {
+    return;
+}
---
+++ python/boomer/common/cpp/post_processing.h
@@ -0,0 +1,39 @@
+/**
+ * Provides classes that allow to post-process the predictions of rules once they have been learned.
+ *
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ */
+#pragma once
+
+#include "predictions.h"
+
+
+/**
+ * Defines an interface for all classes that allow to post-process the predictions of rules once they have been learned.
+ */
+class IPostProcessor {
+
+    public:
+
+        virtual ~IPostProcessor() { };
+
+        /**
+         * Post-processes the prediction of a rule.
+         *
+         * @param prediction A reference to an object of type `Prediction` that stores the predictions of a rule
+         */
+        virtual void postProcess(Prediction& prediction) const = 0;
+
+};
+
+/**
+ * An implementation of the class `IPostProcessor` that does not perform any post-processing, but retains the original
+ * predictions of rules.
+ */
+class NoPostProcessor : virtual public IPostProcessor {
+
+    public:
+
+        void postProcess(Prediction& prediction) const override;
+
+};
