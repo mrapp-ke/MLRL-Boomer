ad222d78b64d3b5c40a64bf4b37dc61b1920c222
Michael Rapp <michael.rapp90@googlemail.com>
2020-10-21 17:43:39 +0200

    Add classes EvaluatedPrediction and LabelWiseEvaluatedPrediction.

---
+++ python/boomer/common/cpp/rule_evaluation.cpp
@@ -0,0 +1,44 @@
+#include "rule_evaluation.h"
+
+
+EvaluatedPrediction::EvaluatedPrediction(uint32 numElements)
+    : scoreVector_(DenseVector<float64>(numElements)) {
+
+}
+
+EvaluatedPrediction::iterator EvaluatedPrediction::begin() {
+    return scoreVector_.begin();
+}
+
+EvaluatedPrediction::iterator EvaluatedPrediction::end() {
+    return scoreVector_.end();
+}
+
+EvaluatedPrediction::const_iterator EvaluatedPrediction::cbegin() const {
+    return scoreVector_.cbegin();
+}
+
+EvaluatedPrediction::const_iterator EvaluatedPrediction::cend() const {
+    return scoreVector_.cend();
+}
+
+LabelWiseEvaluatedPrediction::LabelWiseEvaluatedPrediction(uint32 numElements)
+    : EvaluatedPrediction(numElements), qualityScoreVector_(DenseVector<float64>(numElements)) {
+
+}
+
+LabelWiseEvaluatedPrediction::quality_score_iterator LabelWiseEvaluatedPrediction::quality_scores_begin() {
+    return qualityScoreVector_.begin();
+}
+
+LabelWiseEvaluatedPrediction::quality_score_iterator LabelWiseEvaluatedPrediction::quality_scores_end() {
+    return qualityScoreVector_.end();
+}
+
+LabelWiseEvaluatedPrediction::quality_score_const_iterator LabelWiseEvaluatedPrediction::quality_scores_cbegin() const {
+    return qualityScoreVector_.cbegin();
+}
+
+LabelWiseEvaluatedPrediction::quality_score_const_iterator LabelWiseEvaluatedPrediction::quality_scores_cend() const {
+    return qualityScoreVector_.cend();
+}
