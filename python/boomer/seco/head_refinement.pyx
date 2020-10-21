c7ad0e4c732a6e2f76f9a846bcc807b1d8390eb4
Michael Rapp <michael.rapp90@googlemail.com>
2020-10-22 00:52:25 +0200

    Use factories for creating instances of the class IHeadRefinement.

--- python/boomer/seco/head_refinement.pyx
+++ python/boomer/seco/head_refinement.pyx
@@ -3,11 +3,11 @@ from boomer.seco.lift_functions cimport LiftFunction
 from libcpp.memory cimport make_shared
 
 
-cdef class PartialHeadRefinement(HeadRefinement):
+cdef class PartialHeadRefinementFactory(HeadRefinementFactory):
     """
-    A wrapper for the C++ class `PartialHeadRefinementImpl`.
+    A wrapper for the C++ class `PartialHeadRefinementFactoryImpl`.
     """
 
     def __cinit__(self, LiftFunction lift_function):
-        self.head_refinement_ptr = <shared_ptr[IHeadRefinement]>make_shared[PartialHeadRefinementImpl](
+        self.head_refinement_factory_ptr = <shared_ptr[IHeadRefinementFactory]>make_shared[PartialHeadRefinementFactoryImpl](
             lift_function.lift_function_ptr)
