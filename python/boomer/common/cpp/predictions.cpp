6e5571aa08d66b6476d7fe598cb563856411a72a
Michael Rapp <michael.rapp90@googlemail.com>
2020-10-27 15:24:53 +0100

    Remove unused class.

--- python/boomer/common/cpp/predictions.cpp
+++ python/boomer/common/cpp/predictions.cpp
@@ -17,15 +17,3 @@ PredictionCandidate::PredictionCandidate(uint32 numPredictions, uint32* labelInd
     : Prediction(numPredictions, labelIndices, predictedScores), overallQualityScore_(overallQualityScore) {
 
 }
-
-LabelWisePredictionCandidate::LabelWisePredictionCandidate(uint32 numPredictions, uint32* labelIndices,
-                                                           float64* predictedScores, float64* qualityScores,
-                                                           float64 overallQualityScore)
-    : PredictionCandidate(numPredictions, labelIndices, predictedScores, overallQualityScore),
-      qualityScores_(qualityScores) {
-
-}
-
-LabelWisePredictionCandidate::~LabelWisePredictionCandidate() {
-    free(qualityScores_);
-}
--- python/boomer/common/cpp/predictions.h
+++ python/boomer/common/cpp/predictions.h
@@ -70,34 +70,3 @@ class PredictionCandidate : public Prediction {
         float64 overallQualityScore_;
 
 };
-
-/**
- * Stores the predictions of a rule for several labels, as well as corresponding quality scores.
- */
-class LabelWisePredictionCandidate : public PredictionCandidate {
-
-    public:
-
-        /**
-         * @param numPredictions        The number of labels for which the rule predicts
-         * @param labelIndices          A pointer to an array of type `uint32`, shape `(numPredictions)`, representing
-         *                              the indices of the labels for which the rule predicts or a null pointer, if the
-         *                              rule predicts for all labels
-         * @param predictedScores       A pointer to an array of type `float64`, shape `(numPredictions)`, representing
-         *                              the predicted scores
-         * @param qualityScores         A pointer to an array of type `float64`, shape `(numPredictions)`, representing
-         *                              the quality scores for individual labels
-         * @param overallQualityScore   A score that assesses the overall quality of the predictions
-         */
-        LabelWisePredictionCandidate(uint32 numPredictions, uint32* labelIndices, float64* predictedScores,
-                                     float64* qualityScores, float64 overallQualityScore);
-
-        ~LabelWisePredictionCandidate();
-
-        /**
-         * A pointer to an array of type `float64`, shape `(numPredictions_)`, representing the quality scores for
-         * individual labels.
-         */
-        float64* qualityScores_;
-
-};
