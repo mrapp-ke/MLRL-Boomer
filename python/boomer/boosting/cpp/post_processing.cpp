fd9e8c666e2978a4c58d7405936e741725d578c5
Michael Rapp <michael.rapp90@googlemail.com>
2020-10-27 21:32:44 +0100

    The class ConstantShrinkageImpl now uses the iterator provided by the class Prediction.

--- python/boomer/boosting/cpp/post_processing.cpp
+++ python/boomer/boosting/cpp/post_processing.cpp
@@ -9,10 +9,17 @@ ConstantShrinkageImpl::ConstantShrinkageImpl(float64 shrinkage)
 }
 
 void ConstantShrinkageImpl::postProcess(Prediction& prediction) const {
-    uint32 numPredictions = prediction.getNumElements();
+    uint32 numElements = prediction.getNumElements();
+    Prediction::iterator iterator = prediction.begin();
+
+    for (uint32 i = 0; i < numElements; i++) {
+        iterator[i] *= shrinkage_;
+    }
+
+    // TODO Remove the following
     float64* predictedScores = prediction.predictedScores_;
 
-    for (uint32 i = 0; i < numPredictions; i++) {
+    for (uint32 i = 0; i < numElements; i++) {
         predictedScores[i] *= shrinkage_;
     }
 }
