645f8a14ada080f65badc6314cbd6934d1cfd00e
Michael Rapp <michael.rapp90@googlemail.com>
2020-10-30 13:57:55 +0100

    Move classes ExactThresholdsImpl and ApproximateThresholdsImpl to new files thresholds_exact.h/cpp and thresholds_approximate.h/cpp, respectively.

---
+++ python/boomer/common/cpp/thresholds_approximate.h
@@ -0,0 +1,62 @@
+/**
+ * Implements classes that provide access to approximate thresholds that may be used by the conditions of rules.
+ *
+ * @author Lukas Johannes Eberle (lukasjohannes.eberle@stud.tu-darmstadt.de)
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ */
+#pragma once
+
+#include "thresholds.h"
+#include <unordered_map>
+
+
+/**
+ * Provides access to the thresholds that result from applying a binning method to the feature values of the training
+ * examples.
+ */
+class ApproximateThresholdsImpl : public AbstractThresholds {
+
+    private:
+
+        // Forward declarations
+        class ThresholdsSubset;
+
+        /**
+         * A wrapper for statistics and bins that is stored in the cache.
+         */
+        struct BinCacheEntry {
+            std::unique_ptr<AbstractStatistics> statisticsPtr;
+            std::unique_ptr<BinVector> binVectorPtr;
+        };
+
+        std::shared_ptr<IBinning> binningPtr_;
+
+        uint32 numBins_;
+
+        std::unordered_map<uint32, BinCacheEntry> cache_;
+
+    public:
+
+        /**
+         * @param featureMatrixPtr          A shared pointer to an object of type `IFeatureMatrix` that provides access
+         *                                  to the feature values of the training examples
+         * @param nominalFeatureVectorPtr   A shared pointer to an object of type `INominalFeatureVector` that provides
+         *                                  access to the information whether individual features are nominal or not
+         * @param statisticsPtr             A shared pointer to an object of type `AbstractStatistics` that provides
+         *                                  access to statistics about the labels of the training examples
+         * @param headRefinementFactoryPtr  A shared pointer to an object of type `IHeadRefinementFactory` that allows
+         *                                  to create instances of the class that should be used to find the heads of
+         *                                  rules
+         * @param binningPtr                A shared pointer to an object of type `IBinning` that implements the binning
+         *                                  method to be used
+         * @param numBins                   The number of bins that should be used by the given binning method
+         */
+        ApproximateThresholdsImpl(std::shared_ptr<IFeatureMatrix> featureMatrixPtr,
+                                  std::shared_ptr<INominalFeatureVector> nominalFeatureVectorPtr,
+                                  std::shared_ptr<AbstractStatistics> statisticsPtr,
+                                  std::shared_ptr<IHeadRefinementFactory> headRefinementFactoryPtr,
+                                  std::shared_ptr<IBinning> binningPtr, uint32 numBins);
+
+        std::unique_ptr<IThresholdsSubset> createSubset(const IWeightVector& weights) override;
+
+};
