7262a0beddffe60a72b63e20a34624ebb5c64cee
Michael Rapp <michael.rapp90@googlemail.com>
2020-10-30 14:45:40 +0100

    Add implementation of the functions of the class AbstractThresholds.

--- python/boomer/common/cpp/thresholds.cpp
+++ python/boomer/common/cpp/thresholds.cpp
@@ -252,3 +252,24 @@ static inline void filterAnyFeatureVector(FeatureVector& featureVector, CacheEnt
     filteredVector->setNumElements(i);
     cacheEntry.numConditions = numConditions;
 }
+
+AbstractThresholds::AbstractThresholds(std::shared_ptr<IFeatureMatrix> featureMatrixPtr,
+                                       std::shared_ptr<INominalFeatureVector> nominalFeatureVectorPtr,
+                                       std::shared_ptr<AbstractStatistics> statisticsPtr,
+                                       std::shared_ptr<IHeadRefinementFactory> headRefinementFactoryPtr)
+    : featureMatrixPtr_(featureMatrixPtr), nominalFeatureVectorPtr_(nominalFeatureVectorPtr),
+      statisticsPtr_(statisticsPtr), headRefinementFactoryPtr_(headRefinementFactoryPtr) {
+
+}
+
+uint32 AbstractThresholds::getNumRows() const {
+    return featureMatrixPtr_->getNumRows();
+}
+
+uint32 AbstractThresholds::getNumCols() const {
+    return featureMatrixPtr_->getNumCols();
+}
+
+uint32 AbstractThresholds::getNumLabels() const {
+    return statisticsPtr_->getNumCols();
+}
