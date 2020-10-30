867614ce1b3681e4395c52026c3eb8a78b77dc8d
Michael Rapp <michael.rapp90@googlemail.com>
2020-10-30 13:57:55 +0100

    Move classes ExactThresholdsImpl and ApproximateThresholdsImpl to new files thresholds_exact.h/cpp and thresholds_approximate.h/cpp, respectively.

---
+++ python/boomer/common/cpp/thresholds_exact.h
@@ -0,0 +1,44 @@
+/**
+ * Implements classes that provide access to exact thresholds that may be used by the conditions of rules.
+ *
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ */
+#pragma once
+
+#include "thresholds.h"
+#include <unordered_map>
+
+
+/**
+ * Provides access to all thresholds that result from the feature values of the training examples.
+ */
+class ExactThresholdsImpl : public AbstractThresholds {
+
+    private:
+
+        // Forward declarations
+        class ThresholdsSubset;
+
+        std::unordered_map<uint32, std::unique_ptr<FeatureVector>> cache_;
+
+    public:
+
+        /**
+         * @param featureMatrixPtr          A shared pointer to an object of type `IFeatureMatrix` that provides access
+         *                                  to the feature values of the training examples
+         * @param nominalFeatureVectorPtr   A shared pointer to an object of type `INominalFeatureVector` that provides
+         *                                  access to the information whether individual features are nominal or not
+         * @param statisticsPtr             A shared pointer to an object of type `AbstractStatistics` that provides
+         *                                  access to statistics about the labels of the training examples
+         * @param headRefinementFactoryPtr  A shared pointer to an object of type `IHeadRefinementFactory` that allows
+         *                                  to create instances of the class that should be used to find the heads of
+         *                                  rules
+         */
+        ExactThresholdsImpl(std::shared_ptr<IFeatureMatrix> featureMatrixPtr,
+                            std::shared_ptr<INominalFeatureVector> nominalFeatureVectorPtr,
+                            std::shared_ptr<AbstractStatistics> statisticsPtr,
+                            std::shared_ptr<IHeadRefinementFactory> headRefinementFactoryPtr);
+
+        std::unique_ptr<IThresholdsSubset> createSubset(const IWeightVector& weights) override;
+
+};
