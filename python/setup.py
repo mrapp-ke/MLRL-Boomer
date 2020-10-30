522731c9c8cfbd5fee83238429240495573377cd
Michael Rapp <michael.rapp90@googlemail.com>
2020-10-30 15:33:45 +0100

    Add cpp files to setup.py.

--- python/setup.py
+++ python/setup.py
@@ -23,6 +23,8 @@ sources = [
     'boomer/common/cpp/sub_sampling.cpp',
     'boomer/common/cpp/statistics.cpp',
     'boomer/common/cpp/thresholds.cpp',
+    'boomer/common/cpp/thresholds_exact.cpp',
+    'boomer/common/cpp/thresholds_approximate.cpp',
     'boomer/common/cpp/head_refinement.cpp',
     'boomer/common/cpp/rule_refinement.cpp',
     'boomer/common/cpp/binning.cpp',
