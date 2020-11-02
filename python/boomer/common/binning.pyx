1e17fbb483f38e66c66c000dd266d70e3ee98bd3
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2020-11-02 18:31:25 +0100

    Format code.

--- python/boomer/common/binning.pyx
+++ python/boomer/common/binning.pyx
@@ -15,6 +15,7 @@ cdef class EqualFrequencyBinning(Binning):
     def __cinit__(self, float32 bin_ratio):
         self.binning_ptr = <shared_ptr[IBinning]>make_shared[EqualFrequencyBinningImpl](bin_ratio)
 
+
 cdef class EqualWidthBinning(Binning):
     """
     A wrapper for the C++ class `EqualWidthBinningImpl`.
