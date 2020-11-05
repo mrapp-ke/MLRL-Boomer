a514f9d0dd95991ba9fa8061e4ee4051ee478aa4
Michael Rapp <michael.rapp90@googlemail.com>
2020-11-05 19:05:08 +0100

    Add functions "add" to new file data_operations.h.

---
+++ python/boomer/common/cpp/data_operations.h
@@ -0,0 +1,55 @@
+/**
+ * Provides utility functions that allow to apply commonly used operations on data that is stored in matrices or
+ * vectors.
+ *
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ */
+#pragma once
+
+#include "data.h"
+#include "indices.h"
+
+
+/**
+ * Adds a subset of the numeric data in a vector, identified via indices, to another vector.
+ *
+ * @tparam T            The type of the numeric data
+ * @param from          A `DenseVector<T>::const_iterator` to the beginning of the values in the vector, the values
+ *                      should be taken from
+ * @param to            A `DenseVector<T>::iterator` to the beginning of the values in the vector, the values should be
+ *                      added to
+ * @param indicesBegin  A `FullIndexVector::const_iterator` to the beginning of the indices
+ * @param indicesEnd    A `FullIndexVector::const_iterator` to the end of the indices
+ */
+template<class T>
+static inline void add(typename DenseVector<T>::const_iterator from, typename DenseVector<T>::iterator to,
+                       FullIndexVector::const_iterator indicesBegin, FullIndexVector::const_iterator indicesEnd) {
+    for (auto indexIterator = indicesBegin; indexIterator != indicesEnd; indexIterator++) {
+        uint32 index = *indexIterator;
+        T value = *from;
+        to[index] += value;
+        from++;
+    }
+}
+
+/**
+ * Adds a subset of the numeric data in a vector, identified via indices, to another vector.
+ *
+ * @tparam T            The type of the numeric data
+ * @param from          A `DenseVector<T>::const_iterator` to the beginning of the values in the vector, the values
+ *                      should be taken from
+ * @param to            A `DenseVector<T>::iterator` to the beginning of the values in the vector, the values should be
+ *                      added to
+ * @param indicesBegin  A `PartialIndexVector::const_iterator` to the beginning of the indices
+ * @param indicesEnd    A `PartialIndexVector::const_iterator` to the end of the indices
+ */
+template<class T>
+static inline void add(typename DenseVector<T>::const_iterator from, typename DenseVector<T>::iterator to,
+                       PartialIndexVector::const_iterator indicesBegin, PartialIndexVector::const_iterator indicesEnd) {
+    for (auto indexIterator = indicesBegin; indexIterator != indicesEnd; indexIterator++) {
+        uint32 index = *indexIterator;
+        T value = *from;
+        to[index] += value;
+        from++;
+    }
+}
