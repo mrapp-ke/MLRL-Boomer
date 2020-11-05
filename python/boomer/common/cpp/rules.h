54b2cb7ed722c51ceb9ed7eddc60253e94a38d72
Michael Rapp <michael.rapp90@googlemail.com>
2020-11-05 01:00:25 +0100

    Add class ConditionList.

--- python/boomer/common/cpp/rules.h
+++ python/boomer/common/cpp/rules.h
@@ -6,6 +6,8 @@
 #pragma once
 
 #include "arrays.h"
+#include <list>
+#include <array>
 
 
 /**
@@ -32,4 +34,54 @@ struct Condition {
     intp end;
     bool covered;
     uint32 coveredWeights;
+};
+
+/**
+ * A list that stores conditions in the order they have been learned.
+ */
+class ConditionList {
+
+    private:
+
+        std::list<Condition> list_;
+
+        std::array<uint32, 4> numConditionsPerComparator_ = {0, 0, 0, 0};
+
+    public:
+
+        typedef std::list<Condition>::size_type size_type;
+
+        typedef std::list<Condition>::const_iterator const_iterator;
+
+        const_iterator cbegin() const;
+
+        const_iterator cend() const;
+
+        /**
+         * Returns how many conditions are contained by the list in total.
+         *
+         * @return The number of conditions that are contained by the list
+         */
+        size_type getNumConditions() const;
+
+        /**
+         * Returns how many conditions with a specific comparator are contained by the list.
+         *
+         * @param comparator The comparator
+         * @return           The number of conditions with the given comparator that are contained by the list
+         */
+        uint32 getNumConditions(Comparator comparator) const;
+
+        /**
+         * Adds a new condition to the end of the list.
+         *
+         * @param condition The condition to be added
+         */
+        void append(Condition condition);
+
+        /**
+         * Removes the last condition from the list.
+         */
+        void removeLast();
+
 };
