f32c2b838f36b2cfabfbc3e83be46a712669574b
Michael Rapp <michael.rapp90@googlemail.com>
2020-11-06 20:00:21 +0100

    Change functions of the class ILabelWiseLoss.

--- python/boomer/boosting/cpp/losses_label_wise.h
+++ python/boomer/boosting/cpp/losses_label_wise.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #include "../../common/cpp/input_data.h"
+#include "../../common/cpp/indices.h"
 
 
 namespace boosting {
@@ -13,28 +14,67 @@ namespace boosting {
     /**
      * Defines an interface for all (decomposable) loss functions that are applied label-wise.
      */
+    // TODO Rename to AbstractLabelWiseLoss
     class ILabelWiseLoss {
+
+        protected:
+
+            /**
+             * Must be implemented by subclasses in order to update the gradient and Hessian for a single example and
+             * label.
+             *
+             * @param gradient          A `DenseVector::iterator` to the gradient that should be updated
+             * @param hessian           A `DenseVector::iterator` to the Hessian that should be updated
+             * @param trueLabel         True, if the label is relevant, false otherwise
+             * @param predictedScore    The score that is predicted for the label
+             */
+            virtual void updateGradientAndHessian(DenseVector<float64>::iterator gradient,
+                                                  DenseVector<float64>::iterator hessian, bool trueLabel,
+                                                  float64 predictedScore) const = 0;
 
         public:
 
             virtual ~ILabelWiseLoss() { };
 
             /**
-             * Must be implemented by subclasses to calculate the gradient (first derivative) and Hessian (second
-             * derivative) of the loss function for a certain example and label.
+             * Calculates the gradients and Hessians of the example at a specific index and the labels, whose indices
+             * are provided by a `FullIndexVector`, and updates the gradient and Hessian matrix accordingly.
              *
+             * @param gradients         A reference to an object of type `DenseMatrix` that stores the gradients
+             * @param hessians          A reference to an object of type `DenseMatrix` that stores the Hessians
+             * @param predictedScores   A reference to an object of type `DenseMatrix` that stores the scores that are
+             *                          currently predicted
              * @param labelMatrix       A reference to an object of type `IRandomAccessLabelMatrix` that provides random
              *                          access to the labels of the training examples
-             * @param exampleIndex      The index of the example for which the gradient and Hessian should be calculated
-             * @param labelIndex        The index of the label for which the gradient and Hessian should be calculated
-             * @param predictedScore    A scalar of type `float64`, representing the score that is predicted for the
-             *                          respective example and label
-             * @return                  A pair that contains two scalars of type `float64`, representing the gradient
-             *                          and the Hessian that have been calculated
+             * @param exampleIndex      The index of the example for which the gradients and Hessians should be updated
+             * @param labelIndicesBegin A `FullIndexVector::const_iterator` to the beginning of the label indices
+             * @param labelIndicesEnd   A `FullIndexVector::const_iterator` to the end of the label indices
              */
-            virtual std::pair<float64, float64> calculateGradientAndHessian(const IRandomAccessLabelMatrix& labelMatrix,
-                                                                            uint32 exampleIndex, uint32 labelIndex,
-                                                                            float64 predictedScore) const = 0;
+            void updateGradientsAndHessians(DenseMatrix<float64>& gradients, DenseMatrix<float64>& hessians,
+                                            const DenseMatrix<float64>& predictedScores,
+                                            const IRandomAccessLabelMatrix& labelMatrix, uint32 exampleIndex,
+                                            FullIndexVector::const_iterator labelIndicesBegin,
+                                            FullIndexVector::const_iterator labelIndicesEnd) const;
+
+            /**
+             * Calculates the gradients and Hessians of the example at a specific index and the labels, whose indices
+             * are provided by a `PartialIndexVector`, and updates the gradient and Hessian matrix accordingly.
+             *
+             * @param gradients         A reference to an object of type `DenseMatrix` that stores the gradients
+             * @param hessians          A reference to an object of type `DenseMatrix` that stores the Hessians
+             * @param predictedScores   A reference to an object of type `DenseMatrix` that stores the scores that are
+             * @param labelMatrix       A reference to an object of type `IRandomAccessLabelMatrix` that provides random
+             *                          access to the labels of the training examples
+             *                          currently predicted
+             * @param exampleIndex      The index of the example for which the gradients and Hessians should be updated
+             * @param labelIndicesBegin A `PartialIndexVector::const_iterator` to the beginning of the label indices
+             * @param labelIndicesEnd   A `PartialIndexVector::const_iterator` to the end of the label indices
+             */
+            void updateGradientsAndHessians(DenseMatrix<float64>& gradients, DenseMatrix<float64>& hessians,
+                                            const DenseMatrix<float64>& predictedScores,
+                                            const IRandomAccessLabelMatrix& labelMatrix, uint32 exampleIndex,
+                                            PartialIndexVector::const_iterator labelIndicesBegin,
+                                            PartialIndexVector::const_iterator labelIndicesEnd) const;
 
     };
 
@@ -43,11 +83,11 @@ namespace boosting {
      */
     class LabelWiseLogisticLossImpl : public ILabelWiseLoss {
 
-        public:
+        protected:
 
-            std::pair<float64, float64> calculateGradientAndHessian(const IRandomAccessLabelMatrix& labelMatrix,
-                                                                    uint32 exampleIndex, uint32 labelIndex,
-                                                                    float64 predictedScore) const override;
+            void updateGradientAndHessian(DenseVector<float64>::iterator gradient,
+                                          DenseVector<float64>::iterator hessian, bool trueLabel,
+                                          float64 predictedScore) const override;
 
     };
 
@@ -56,11 +96,11 @@ namespace boosting {
      */
     class LabelWiseSquaredErrorLossImpl : public ILabelWiseLoss {
 
-        public:
+        protected:
 
-            std::pair<float64, float64> calculateGradientAndHessian(const IRandomAccessLabelMatrix& labelMatrix,
-                                                                    uint32 exampleIndex, uint32 labelIndex,
-                                                                    float64 predictedScore) const override;
+            void updateGradientAndHessian(DenseVector<float64>::iterator gradient,
+                                          DenseVector<float64>::iterator hessian, bool trueLabel,
+                                          float64 predictedScore) const override;
 
     };
 
