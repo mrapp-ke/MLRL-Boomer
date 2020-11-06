e014e4ba65f860493ceae28e3ddc1a96a956c71c
Michael Rapp <michael.rapp90@googlemail.com>
2020-11-06 16:01:09 +0100

    Remove obsolete class attributes.

--- python/boomer/boosting/cpp/statistics_label_wise.cpp
+++ python/boomer/boosting/cpp/statistics_label_wise.cpp
@@ -32,17 +32,13 @@ class DenseLabelWiseStatistics : public AbstractLabelWiseStatistics {
 
                 const T& labelIndices_;
 
-                DenseFloat64Vector sumsOfGradientsV_;
+                DenseFloat64Vector sumsOfGradients_;
 
-                DenseFloat64Vector sumsOfHessiansV_;
+                DenseFloat64Vector sumsOfHessians_;
 
                 DenseFloat64Vector* accumulatedSumsOfGradients_;
 
                 DenseFloat64Vector* accumulatedSumsOfHessians_;
-
-                // TODO Remove
-                float64* sumsOfGradients_;
-                float64* sumsOfHessians_;
 
             public:
 
@@ -59,14 +55,10 @@ class DenseLabelWiseStatistics : public AbstractLabelWiseStatistics {
                                  std::unique_ptr<ILabelWiseRuleEvaluation> ruleEvaluationPtr, const T& labelIndices)
                     : statistics_(statistics), ruleEvaluationPtr_(std::move(ruleEvaluationPtr)),
                       labelIndices_(labelIndices),
-                      sumsOfGradientsV_(DenseFloat64Vector(labelIndices.getNumElements(), true)),
-                      sumsOfHessiansV_(DenseFloat64Vector(labelIndices.getNumElements(), true)) {
+                      sumsOfGradients_(DenseFloat64Vector(labelIndices.getNumElements(), true)),
+                      sumsOfHessians_(DenseFloat64Vector(labelIndices.getNumElements(), true)) {
                     accumulatedSumsOfGradients_ = nullptr;
                     accumulatedSumsOfHessians_ = nullptr;
-
-                    // TODO Remove
-                    sumsOfGradients_ = sumsOfGradientsV_.begin();
-                    sumsOfHessians_ = sumsOfHessiansV_.begin();
                 }
 
                 ~StatisticsSubset() {
@@ -75,11 +67,11 @@ class DenseLabelWiseStatistics : public AbstractLabelWiseStatistics {
                 }
 
                 void addToSubset(uint32 statisticIndex, uint32 weight) override {
-                    vector::addToSubset<float64>(statistics_.gradientsM_->row_cbegin(statisticIndex),
-                                                 sumsOfGradientsV_.begin(), labelIndices_.cbegin(),
+                    vector::addToSubset<float64>(statistics_.gradients_->row_cbegin(statisticIndex),
+                                                 sumsOfGradients_.begin(), labelIndices_.cbegin(),
                                                  labelIndices_.cend(), weight);
-                    vector::addToSubset<float64>(statistics_.hessiansM_->row_cbegin(statisticIndex),
-                                                 sumsOfHessiansV_.begin(), labelIndices_.cbegin(), labelIndices_.cend(),
+                    vector::addToSubset<float64>(statistics_.hessians_->row_cbegin(statisticIndex),
+                                                 sumsOfHessians_.begin(), labelIndices_.cbegin(), labelIndices_.cend(),
                                                  weight);
                 }
 
@@ -94,22 +86,22 @@ class DenseLabelWiseStatistics : public AbstractLabelWiseStatistics {
 
                     // Reset the sum of gradients and Hessians for each label to zero and add it to the accumulated sums
                     // of gradients and hessians...
-                    vector::add<float64>(sumsOfGradientsV_.cbegin(), sumsOfGradientsV_.cend(),
+                    vector::add<float64>(sumsOfGradients_.cbegin(), sumsOfGradients_.cend(),
                                          accumulatedSumsOfGradients_->begin());
-                    sumsOfGradientsV_.setAllToZero();
-                    vector::add<float64>(sumsOfHessiansV_.cbegin(), sumsOfHessiansV_.cend(),
+                    sumsOfGradients_.setAllToZero();
+                    vector::add<float64>(sumsOfHessians_.cbegin(), sumsOfHessians_.cend(),
                                          accumulatedSumsOfHessians_->begin());
-                    sumsOfHessiansV_.setAllToZero();
+                    sumsOfHessians_.setAllToZero();
                 }
 
                 const LabelWiseEvaluatedPrediction& calculateLabelWisePrediction(bool uncovered,
                                                                                  bool accumulated) override {
-                    float64* sumsOfGradients = accumulated ? accumulatedSumsOfGradients_->begin() : sumsOfGradients_;
-                    float64* sumsOfHessians = accumulated ? accumulatedSumsOfHessians_->begin() : sumsOfHessians_;
-                    return ruleEvaluationPtr_->calculateLabelWisePrediction(statistics_.totalSumsOfGradients_,
-                                                                            sumsOfGradients,
-                                                                            statistics_.totalSumsOfHessians_,
-                                                                            sumsOfHessians, uncovered);
+                    DenseFloat64Vector& sumsOfGradients = accumulated ? *accumulatedSumsOfGradients_ : sumsOfGradients_;
+                    DenseFloat64Vector& sumsOfHessians = accumulated ? *accumulatedSumsOfHessians_ : sumsOfHessians_;
+                    return ruleEvaluationPtr_->calculateLabelWisePrediction(statistics_.totalSumsOfGradients_.cbegin(),
+                                                                            sumsOfGradients.begin(),
+                                                                            statistics_.totalSumsOfHessians_.cbegin(),
+                                                                            sumsOfHessians.begin(), uncovered);
                 }
 
         };
@@ -143,15 +135,15 @@ class DenseLabelWiseStatistics : public AbstractLabelWiseStatistics {
 
                 void onBinUpdate(uint32 binIndex, const FeatureVector::Entry& entry) override {
                     uint32 index = entry.index;
-                    vector::add<float64>(statistics_.gradientsM_->row_cbegin(index),
-                                         statistics_.gradientsM_->row_cend(index), gradients_->row_begin(binIndex));
+                    vector::add<float64>(statistics_.gradients_->row_cbegin(index),
+                                         statistics_.gradients_->row_cend(index), gradients_->row_begin(binIndex));
                 }
 
                 std::unique_ptr<AbstractStatistics> build() const override {
                     return std::make_unique<DenseLabelWiseStatistics>(statistics_.lossFunctionPtr_,
                                                                       statistics_.ruleEvaluationFactoryPtr_,
                                                                       statistics_.labelMatrixPtr_, gradients_,
-                                                                      hessians_, statistics_.currentScoresM_);
+                                                                      hessians_, statistics_.currentScores_);
                 }
 
         };
@@ -161,32 +153,25 @@ class DenseLabelWiseStatistics : public AbstractLabelWiseStatistics {
 
         std::shared_ptr<IRandomAccessLabelMatrix> labelMatrixPtr_;
 
-        DenseFloat64Matrix* gradientsM_;
+        DenseFloat64Matrix* gradients_;
 
-        DenseFloat64Matrix* hessiansM_;
+        DenseFloat64Matrix* hessians_;
 
-        DenseFloat64Matrix* currentScoresM_;
+        DenseFloat64Matrix* currentScores_;
 
-        DenseFloat64Vector totalSumsOfGradientsV_;
+        DenseFloat64Vector totalSumsOfGradients_;
 
-        DenseFloat64Vector totalSumsOfHessiansV_;
-
-        // TODO Remove
-        float64* gradients_;
-        float64* hessians_;
-        float64* currentScores_;
-        float64* totalSumsOfGradients_;
-        float64* totalSumsOfHessians_;
+        DenseFloat64Vector totalSumsOfHessians_;
 
         template<class T>
         void applyPredictionInternally(uint32 statisticIndex, const T& prediction) {
             // Update the scores that are currently predicted for the example at the given index...
-            vector::addFromSubset<float64>(prediction.scores_cbegin(), currentScoresM_->row_begin(statisticIndex),
+            vector::addFromSubset<float64>(prediction.scores_cbegin(), currentScores_->row_begin(statisticIndex),
                                            prediction.indices_cbegin(), prediction.indices_cend());
 
-            DenseFloat64Matrix::iterator gradientIterator = gradientsM_->row_begin(statisticIndex);
-            DenseFloat64Matrix::iterator hessianIterator = hessiansM_->row_begin(statisticIndex);
-            DenseFloat64Matrix::const_iterator scoreIterator = currentScoresM_->row_cbegin(statisticIndex);
+            DenseFloat64Matrix::iterator gradientIterator = gradients_->row_begin(statisticIndex);
+            DenseFloat64Matrix::iterator hessianIterator = hessians_->row_begin(statisticIndex);
+            DenseFloat64Matrix::const_iterator scoreIterator = currentScores_->row_cbegin(statisticIndex);
 
             // Update the gradients and Hessians of the example at the given index...
             for (auto indexIterator = prediction.indices_cbegin(); indexIterator != prediction.indices_cend(); indexIterator++) {
@@ -225,35 +210,29 @@ class DenseLabelWiseStatistics : public AbstractLabelWiseStatistics {
                                  DenseFloat64Matrix* currentScores)
             : AbstractLabelWiseStatistics(labelMatrixPtr->getNumExamples(), labelMatrixPtr->getNumLabels(),
                                           ruleEvaluationFactoryPtr),
-              lossFunctionPtr_(lossFunctionPtr), labelMatrixPtr_(labelMatrixPtr), gradientsM_(gradients),
-              hessiansM_(hessians), currentScoresM_(currentScores),
-              totalSumsOfGradientsV_(DenseFloat64Vector(labelMatrixPtr->getNumLabels())),
-              totalSumsOfHessiansV_(DenseFloat64Vector(labelMatrixPtr->getNumLabels())) {
-            // TODO Remove
-            gradients_ = gradients->begin();
-            hessians_ = hessians->begin();
-            currentScores_ = currentScores->begin();
-            totalSumsOfGradients_ = totalSumsOfGradientsV_.begin();
-            totalSumsOfHessians_ = totalSumsOfHessiansV_.begin();
+              lossFunctionPtr_(lossFunctionPtr), labelMatrixPtr_(labelMatrixPtr), gradients_(gradients),
+              hessians_(hessians), currentScores_(currentScores),
+              totalSumsOfGradients_(DenseFloat64Vector(labelMatrixPtr->getNumLabels())),
+              totalSumsOfHessians_(DenseFloat64Vector(labelMatrixPtr->getNumLabels())) {
         }
 
         ~DenseLabelWiseStatistics() {
-            delete gradientsM_;
-            delete hessiansM_;
-            delete currentScoresM_;
+            delete gradients_;
+            delete hessians_;
+            delete currentScores_;
         }
 
         void resetCoveredStatistics() override {
-            totalSumsOfGradientsV_.setAllToZero();
-            totalSumsOfHessiansV_.setAllToZero();
+            totalSumsOfGradients_.setAllToZero();
+            totalSumsOfHessians_.setAllToZero();
         }
 
         void updateCoveredStatistic(uint32 statisticIndex, uint32 weight, bool remove) override {
             float64 signedWeight = remove ? -((float64) weight) : weight;
-            vector::add<float64>(gradientsM_->row_cbegin(statisticIndex), gradientsM_->row_cend(statisticIndex),
-                                 totalSumsOfGradientsV_.begin(), signedWeight);
-            vector::add<float64>(hessiansM_->row_cbegin(statisticIndex), hessiansM_->row_cend(statisticIndex),
-                                 totalSumsOfHessiansV_.begin(), signedWeight);
+            vector::add<float64>(gradients_->row_cbegin(statisticIndex), gradients_->row_cend(statisticIndex),
+                                 totalSumsOfGradients_.begin(), signedWeight);
+            vector::add<float64>(hessians_->row_cbegin(statisticIndex), hessians_->row_cend(statisticIndex),
+                                 totalSumsOfHessians_.begin(), signedWeight);
         }
 
         std::unique_ptr<IStatisticsSubset> createSubset(const FullIndexVector& labelIndices) const override {
