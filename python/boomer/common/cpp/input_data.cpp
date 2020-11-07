297ede89359ae5b99eded29d3f26fb88d54ef7fe
Michael Rapp <michael.rapp90@googlemail.com>
2020-11-07 14:09:40 +0100

    The class FeatureVector now provides access to the indices of examples with missing feature values.

--- python/boomer/common/cpp/input_data.cpp
+++ python/boomer/common/cpp/input_data.cpp
@@ -1,6 +1,23 @@
 #include "input_data.h"
 
 
+FeatureVector::FeatureVector(uint32 numElements)
+    : SparseArrayVector<float32>(numElements) {
+
+}
+
+FeatureVector::missing_index_const_iterator FeatureVector::missing_indices_cbegin() const {
+    return missingIndices_.indices_cbegin();
+}
+
+FeatureVector::missing_index_const_iterator FeatureVector::missing_indices_cend() const {
+    return missingIndices_.indices_cend();
+}
+
+void FeatureVector::addMissingIndex(uint32 index) {
+    missingIndices_.setValue(index);
+}
+
 DenseLabelMatrixImpl::DenseLabelMatrixImpl(uint32 numExamples, uint32 numLabels, const uint8* y)
     : numExamples_(numExamples), numLabels_(numLabels), y_(y) {
 
--- python/boomer/common/cpp/input_data.h
+++ python/boomer/common/cpp/input_data.h
@@ -10,9 +10,46 @@
 
 
 /**
- * Typedef for a vector that stores the indices of training examples, as well as their values for a certain feature.
+ * An one-dimensional sparse vector that stores the values of training examples for a certain feature, as well as the
+ * indices of examples with missing feature values.
  */
-typedef SparseArrayVector<float32> FeatureVector;
+class FeatureVector : public SparseArrayVector<float32> {
+
+    private:
+
+        BinaryDokVector missingIndices_;
+
+    public:
+
+        /**
+         * @param numElements The number of elements in the vector
+         */
+        FeatureVector(uint32 numElements);
+
+        typedef BinaryDokVector::index_const_iterator missing_index_const_iterator;
+
+        /**
+         * Returns a `missing_index_const_iterator` to the beginning of the missing indices.
+         *
+         * @return A `missing_index_const_iterator` to the beginning
+         */
+        missing_index_const_iterator missing_indices_cbegin() const;
+
+        /**
+         * Returns a `missing_index_const_iterator` to the end of the missing indices.
+         *
+         * @return A `missing_index_const_iterator` to the end
+         */
+        missing_index_const_iterator missing_indices_cend() const;
+
+        /**
+         * Adds the index of an example with missing feature value.
+         *
+         * @param index The index to be added
+         */
+        void addMissingIndex(uint32 index);
+
+};
 
 /**
  * Defines an interface for all label matrices that provide access to the labels of the training examples.
