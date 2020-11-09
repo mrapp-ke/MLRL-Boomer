df2f4086071f7d21175e33ffe2e36a509334cd57
Michael Rapp <michael.rapp90@googlemail.com>
2020-11-09 13:22:39 +0100

    The class RegularizedLabelWiseRuleEvaluation is not a template class anymore.

--- python/boomer/boosting/cpp/rule_evaluation_label_wise.cpp
+++ python/boomer/boosting/cpp/rule_evaluation_label_wise.cpp
@@ -7,15 +7,10 @@ using namespace boosting;
 /**
  * Allows to calculate the predictions of rules, as well as corresponding quality scores, based on the gradients and
  * Hessians that have been calculated according to a loss function that is applied label-wise using L2 regularization.
- *
- * @tparam T The type of the vector that provides access to the labels for which predictions should be calculated
  */
-template<class T>
 class RegularizedLabelWiseRuleEvaluation : public ILabelWiseRuleEvaluation {
 
     private:
-
-        const T& labelIndices_;
 
         float64 l2RegularizationWeight_;
 
@@ -24,14 +19,13 @@ class RegularizedLabelWiseRuleEvaluation : public ILabelWiseRuleEvaluation {
     public:
 
         /**
-         * @param labelIndices              A reference to an object of template type `T` that provides access to
-         *                                  the indices of the labels for which the rules may predict
+         * @param numPredictions            The number of labels for which the rules may predict
          * @param l2RegularizationWeight    The weight of the L2 regularization that is applied for calculating the
          *                                  scores to be predicted by rules
          */
-        RegularizedLabelWiseRuleEvaluation(const T& labelIndices, float64 l2RegularizationWeight)
-            : labelIndices_(labelIndices), l2RegularizationWeight_(l2RegularizationWeight),
-              prediction_(LabelWiseEvaluatedPrediction(labelIndices.getNumElements())) {
+        RegularizedLabelWiseRuleEvaluation(uint32 numPredictions, float64 l2RegularizationWeight)
+            : l2RegularizationWeight_(l2RegularizationWeight),
+              prediction_(LabelWiseEvaluatedPrediction(numPredictions)) {
 
         }
 
@@ -76,12 +70,10 @@ RegularizedLabelWiseRuleEvaluationFactoryImpl::RegularizedLabelWiseRuleEvaluatio
 
 std::unique_ptr<ILabelWiseRuleEvaluation> RegularizedLabelWiseRuleEvaluationFactoryImpl::create(
         const FullIndexVector& indexVector) const {
-    return std::make_unique<RegularizedLabelWiseRuleEvaluation<FullIndexVector>>(indexVector,
-                                                                                 l2RegularizationWeight_);
+    return std::make_unique<RegularizedLabelWiseRuleEvaluation>(indexVector.getNumElements(), l2RegularizationWeight_);
 }
 
 std::unique_ptr<ILabelWiseRuleEvaluation> RegularizedLabelWiseRuleEvaluationFactoryImpl::create(
         const PartialIndexVector& indexVector) const {
-    return std::make_unique<RegularizedLabelWiseRuleEvaluation<PartialIndexVector>>(indexVector,
-                                                                                    l2RegularizationWeight_);
+    return std::make_unique<RegularizedLabelWiseRuleEvaluation>(indexVector.getNumElements(), l2RegularizationWeight_);
 }
