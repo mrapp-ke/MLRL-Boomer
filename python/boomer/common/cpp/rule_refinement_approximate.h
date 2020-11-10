ebd1c63530b10457b81e35b762eeb01b609070dc
Michael Rapp <michael.rapp90@googlemail.com>
2020-11-10 18:51:43 +0100

    Add potential BinVector alternatives.

--- python/boomer/common/cpp/rule_refinement_approximate.h
+++ python/boomer/common/cpp/rule_refinement_approximate.h
@@ -6,7 +6,237 @@
  * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
  */
 #include "rule_refinement.h"
+#include <forward_list>
+#include <cstdlib>
 
+
+/**
+ * TODO
+ */
+class BinVectorNew {
+
+    public:
+
+        typedef IndexedValue<float32> Example;
+
+    private:
+
+        DenseVector<Bin> bins_;
+
+        std::forward_list<Example>** examplesPerBin_;
+
+    public:
+
+        BinVectorNew(uint32 numElements)
+            : bins_(DenseVector<Bin>(numElements, true)),
+              examplesPerBin_((std::forward_list<Example>**) calloc(numElements, sizeof(std::forward_list<Example>*))) {
+
+        }
+
+        ~BinVectorNew() {
+            uint32 numElements = this->getNumElements();
+
+            for (uint32 i = 0; i < numElements; i++) {
+                delete examplesPerBin_[i];
+            }
+
+            free(examplesPerBin_);
+        }
+
+        typedef DenseVector<Bin>::iterator iterator;
+
+        typedef DenseVector<Bin>::const_iterator const_iterator;
+
+        typedef std::forward_list<Example>::const_iterator example_const_iterator;
+
+        /**
+         * TODO
+         *
+         * @return TODO
+         */
+        iterator begin() {
+            return bins_.begin();
+        }
+
+        /**
+         * TODO
+         *
+         * @return TODO
+         */
+        iterator end() {
+            return bins_.end();
+        }
+
+        /**
+         * TODO
+         *
+         * @return TODO
+         */
+        const_iterator cbegin() const {
+            return bins_.cbegin();
+        }
+
+        /**
+         * TODO
+         *
+         * @return TODO
+         */
+        const_iterator cend() const {
+            return bins_.cend();
+        }
+
+        /**
+         * TODO
+         *
+         * @param binIndex  TODO
+         * @return          TODO
+         */
+        example_const_iterator examples_cbegin(uint32 binIndex) const {
+            std::forward_list<Example>* examples = examplesPerBin_[binIndex];
+
+            if (examples == nullptr) {
+                examples = new std::forward_list<IndexedValue<float32>>();
+            }
+
+            return examples->cbegin();
+        }
+
+        /**
+         * TODO
+         *
+         * @param binIndex  TODO
+         * @return          TODO
+         */
+        example_const_iterator examples_cend(uint32 binIndex) const {
+            std::forward_list<Example>* examples = examplesPerBin_[binIndex];
+
+            if (examples == nullptr) {
+                examples = new std::forward_list<IndexedValue<float32>>();
+            }
+
+            return examples->cend();
+        }
+
+        /**
+         * TODO
+         *
+         * @param binIndex  TODO
+         * @param example   TODO
+         */
+        void addExample(uint32 binIndex, Example example) {
+            std::forward_list<Example>* examples = examplesPerBin_[binIndex];
+
+            if (examples == nullptr) {
+                examples = new std::forward_list<IndexedValue<float32>>();
+            }
+
+            examples->push_front(example);
+        }
+
+        /**
+         * TODO
+         */
+        void clearAllExamples() {
+             uint32 numElements = this->getNumElements();
+
+            for (uint32 i = 0; i < numElements; i++) {
+                std::forward_list<Example>* examples = examplesPerBin_[i];
+
+                if (examples != nullptr) {
+                    examples->clear();
+                }
+            }
+        }
+
+        /**
+         * Returns the number of bins in the vector.
+         *
+         * @return The number of bins in the vector
+         */
+        uint32 getNumElements() const {
+            return bins_.getNumElements();
+        }
+
+        /**
+         * Sets the number of bins in the vector.
+         *
+         * @param numElements The number of bins to be set
+         */
+        void setNumElements(uint32 numElements) {
+            bins_.setNumElements(numElements);
+
+            examplesPerBin_ = (std::forward_list<Example>**) realloc(
+                examplesPerBin_, numElements * sizeof(std::forward_list<Example>**));
+        }
+
+};
+
+/**
+ * TODO
+ */
+class BinVectorNew2 : public DenseVector<Bin> {
+
+    public:
+
+        typedef IndexedValue<float32> Example;
+
+    private:
+
+        std::unordered_map<uint32, std::forward_list<Example>> examplesPerBin_;
+
+    public:
+
+        /**
+         * @param numElements TODO
+         */
+        BinVectorNew2(uint32 numElements)
+            : DenseVector<Bin>(numElements, true) {
+
+        }
+
+        typedef std::forward_list<Example>::const_iterator example_const_iterator;
+
+        /**
+         * TODO
+         *
+         * @param binIndex  TODO
+         * @return          TODO
+         */
+        example_const_iterator examples_cbegin(uint32 binIndex) {
+            std::forward_list<Example>& examples = examplesPerBin_[binIndex];
+            return examples.cbegin();
+        }
+
+        /**
+         * TODO
+         *
+         * @param binIndex  TODO
+         * @return          TODO
+         */
+        example_const_iterator examples_cend(uint32 binIndex) {
+            std::forward_list<Example>& examples = examplesPerBin_[binIndex];
+            return examples.cend();
+        }
+
+        /**
+         * TODO
+         *
+         * @param binIndex  TODO
+         * @param example   TODO
+         */
+        void addExample(uint32 binIndex, Example example) {
+            std::forward_list<Example>& examples = examplesPerBin_[binIndex];
+            examples.push_front(example);
+        }
+
+        /**
+         * TODO
+         */
+        void clearAllExamples() {
+            examplesPerBin_.clear();
+        }
+
+};
 
 /**
  * Allows to find the best refinements of existing rules, which result from adding a new condition that correspond to a
