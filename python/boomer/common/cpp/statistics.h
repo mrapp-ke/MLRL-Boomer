3621d17ea81eb0864e92a46ad769ff5840631f1a
Michael Rapp <michael.rapp90@googlemail.com>
2020-11-15 22:43:09 +0100

    Edit comment.

--- python/boomer/common/cpp/statistics.h
+++ python/boomer/common/cpp/statistics.h
@@ -140,8 +140,9 @@ class IStatisticsSubset {
 
 /**
  * An abstract base class for all classes that provide access to a subset of the statistics that are stores by an
- * instance of the class `IStatistics` and allow to calculate the scores to be predicted by rules that cover such a
- * subset in the decomposable case, i.e., if the label-wise predictions are the same as the example-wise predictions.
+ * instance of the class `IHistogram` or `IStatistics` and allow to calculate the scores to be predicted by rules that
+ * cover such a subset in the decomposable case, i.e., if the label-wise predictions are the same as the example-wise
+ * predictions.
  */
 class AbstractDecomposableStatisticsSubset : public IStatisticsSubset {
 
