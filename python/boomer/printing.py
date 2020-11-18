5588f062109c798fc1e5ee6b6412109adf782f90
Michael Rapp <michael.rapp90@googlemail.com>
2020-11-18 22:24:46 +0100

    Add the ability to print textual representations of nominal values.

--- python/boomer/printing.py
+++ python/boomer/printing.py
@@ -140,36 +140,41 @@ def format_model(meta_data: MetaData, model: RuleModel, print_feature_names: boo
 
 
 def format_rule(meta_data: MetaData, rule: Rule, print_feature_names: bool = True,
-                print_label_names: bool = True) -> str:
+                print_label_names: bool = True, print_nominal_values: bool = False) -> str:
     """
     Formats a specific rule as a text.
 
-    :param meta_data:           The meta data of the training data set
-    :param rule:                The rule to be formatted
-    :param print_feature_names: True, if the names of features should be printed, if available, False, if the indices of
-                                features should be printed
-    :param print_label_names:   True, if the names of labels should be printed, if available, False, if the indices of
-                                labels should be printed
-    :return:                    The text
+    :param meta_data:               The meta data of the training data set
+    :param rule:                    The rule to be formatted
+    :param print_feature_names:     True, if the names of features should be printed, if available, False, if the
+                                    indices of features should be printed
+    :param print_label_names:       True, if the names of labels should be printed, if available, False, if the indices
+                                    of labels should be printed
+    :param print_nominal_values:    True, if the textual representation of nominal values should be printed, False, if
+                                    the index of nominal values should be printed
+    :return:                        The text
     """
-    text = __format_body(meta_data, rule.body, print_feature_names=print_feature_names)
+    text = __format_body(meta_data, rule.body, print_feature_names=print_feature_names,
+                         print_nominal_values=print_nominal_values)
     text += ' -> '
     text += __format_head(meta_data, rule.head, print_label_names=print_label_names)
     return text
 
 
-def __format_rule_list(meta_data: MetaData, rule_list: RuleList, print_feature_names: bool,
-                       print_label_names: bool = True) -> str:
+def __format_rule_list(meta_data: MetaData, rule_list: RuleList, print_feature_names: bool = True,
+                       print_label_names: bool = True, print_nominal_values: bool = False) -> str:
     """
     Formats a specific rule list as a text.
 
-    :param meta_data:           The meta data of the training data set
-    :param rule_list:           The rule list to be formatted
-    :param print_feature_names: True, if the names of features should be printed, if available, False, if the indices of
-                                features should be printed
-    :param print_label_names:   True, if the names of labels should be printed, if available, False, if the indices of
-                                labels should be printed
-    :return:                    The text
+    :param meta_data:               The meta data of the training data set
+    :param rule_list:               The rule list to be formatted
+    :param print_feature_names:     True, if the names of features should be printed, if available, False, if the
+                                    indices of features should be printed
+    :param print_label_names:       True, if the names of labels should be printed, if available, False, if the indices
+                                    of labels should be printed
+    :param print_nominal_values:    True, if the textual representation of nominal values should be printed, False, if
+                                    the index of nominal values should be printed
+    :return:                        The text
     """
     text = ''
 
@@ -178,91 +183,108 @@ def __format_rule_list(meta_data: MetaData, rule_list: RuleList, print_feature_n
             text += '\n'
 
         text += format_rule(meta_data, rule, print_feature_names=print_feature_names,
-                            print_label_names=print_label_names)
+                            print_label_names=print_label_names, print_nominal_values=print_nominal_values)
 
     return text
 
 
-def __format_body(meta_data: MetaData, body: Body, print_feature_names: bool) -> str:
+def __format_body(meta_data: MetaData, body: Body, print_feature_names: bool, print_nominal_values: bool) -> str:
     """
     Formats the body of a rule as a text.
 
-    :param meta_data:           The meta data of the training data set
-    :param body:                The body to be formatted
-    :param print_feature_names: True, if the names of features should be printed, if available, False, if the indices of
-                                features should be printed
-    :return:                    The text
+    :param meta_data:               The meta data of the training data set
+    :param body:                    The body to be formatted
+    :param print_feature_names:     True, if the names of features should be printed, if available, False, if the
+                                    indices of features should be printed
+    :param print_nominal_values:    True, if the textual representation of nominal values should be printed, False, if
+                                    the index of nominal values should be printed
+    :return:                        The text
     """
     if isinstance(body, EmptyBody):
         return '{}'
     elif isinstance(body, ConjunctiveBody):
-        return '{' + __format_conjunctive_body(meta_data, body, print_feature_names=print_feature_names) + '}'
+        return '{' + __format_conjunctive_body(meta_data, body, print_feature_names=print_feature_names,
+                                               print_nominal_values=print_nominal_values) + '}'
     else:
         raise ValueError('Body has unknown type: ' + type(body).__name__)
 
 
-def __format_conjunctive_body(meta_data: MetaData, body: ConjunctiveBody, print_feature_names: bool) -> str:
+def __format_conjunctive_body(meta_data: MetaData, body: ConjunctiveBody, print_feature_names: bool,
+                              print_nominal_values: bool) -> str:
     """
     Formats the conjunctive body of a rule as a text.
 
-    :param meta_data:           The meta data of the training data set
-    :param body:                The conjunctive body to be formatted
-    :param print_feature_names: True, if the names of features should be printed, if available, False, if the indices of
-                                features should be printed
-    :return:                    The text
+    :param meta_data:               The meta data of the training data set
+    :param body:                    The conjunctive body to be formatted
+    :param print_feature_names:     True, if the names of features should be printed, if available, False, if the
+                                    indices of features should be printed
+    :param print_nominal_values:    True, if the textual representation of nominal values should be printed, False, if
+                                    the index of nominal values should be printed
+    :return:                        The text
     """
     text = ''
 
     if body.leq_feature_indices is not None and body.leq_thresholds is not None:
         text = __format_conditions(meta_data, np.asarray(body.leq_feature_indices), np.asarray(body.leq_thresholds),
-                                   '<=', text, print_feature_names=print_feature_names)
+                                   '<=', text, print_feature_names=print_feature_names,
+                                   print_nominal_values=print_nominal_values)
 
     if body.gr_feature_indices is not None and body.gr_thresholds is not None:
         text = __format_conditions(meta_data, np.asarray(body.gr_feature_indices), np.asarray(body.gr_thresholds),
-                                   '>', text, print_feature_names=print_feature_names)
+                                   '>', text, print_feature_names=print_feature_names,
+                                   print_nominal_values=print_nominal_values)
 
     if body.eq_feature_indices is not None and body.eq_thresholds is not None:
         text = __format_conditions(meta_data, np.asarray(body.eq_feature_indices), np.asarray(body.eq_thresholds),
-                                   '==', text, print_feature_names=print_feature_names)
+                                   '==', text, print_feature_names=print_feature_names,
+                                   print_nominal_values=print_nominal_values)
 
     if body.neq_feature_indices is not None and body.neq_thresholds is not None:
         text = __format_conditions(meta_data, np.asarray(body.neq_feature_indices), np.asarray(body.neq_thresholds),
-                                   '!=', text, print_feature_names=print_feature_names)
+                                   '!=', text, print_feature_names=print_feature_names,
+                                   print_nominal_values=print_nominal_values)
 
     return text
 
 
 def __format_conditions(meta_data: MetaData, feature_indices: np.ndarray, thresholds: np.ndarray, operator: str,
-                        text: str, print_feature_names: bool) -> str:
+                        text: str, print_feature_names: bool, print_nominal_values: bool) -> str:
     """
     Formats conditions that are contained by the body of a rule and the textual representation to an existing text.
 
-    :param meta_data:           The meta data of the training data set
-    :param feature_indices:     An array of type `int`, shape `(num_conditions)`, representing the feature indices that
-                                correspond to the conditions
-    :param thresholds:          An array of type `float`, shape `(num_conditions)`, representing the thresholds used by
-                                the conditions
-    :param operator:            A textual representation of the operator that is used by the conditions
-    :param text:                The text, the textual representation of the conditions should be appended to
-    :param print_feature_names: True, if the names of features should be printed, if available, False, if the indices of
-                                features should be printed
-    :return:                    The given text including the appended text
+    :param meta_data:               The meta data of the training data set
+    :param feature_indices:         An array of type `int`, shape `(num_conditions)`, representing the feature indices
+                                    that correspond to the conditions
+    :param thresholds:              An array of type `float`, shape `(num_conditions)`, representing the thresholds used
+                                    by the conditions
+    :param operator:                A textual representation of the operator that is used by the conditions
+    :param text:                    The text, the textual representation of the conditions should be appended to
+    :param print_feature_names:     True, if the names of features should be printed, if available, False, if the
+                                    indices of features should be printed
+    :param print_nominal_values:    True, if the textual representation of nominal values should be printed, False, if
+                                    the index of nominal values should be printed
+    :return:                        The given text including the appended text
     """
     for i in range(feature_indices.shape[0]):
         if len(text) > 0:
             text += ' & '
 
         feature_index = feature_indices[i]
+        attribute = meta_data.attributes[feature_index] if len(meta_data.attributes) > feature_index else None
 
-        if print_feature_names and len(meta_data.attributes) > feature_index:
-            text += meta_data.attributes[feature_index].attribute_name
+        if print_feature_names and attribute is not None:
+            text += attribute.attribute_name
         else:
             text += str(feature_index)
 
         text += ' '
         text += operator
         text += ' '
-        text += str(thresholds[i])
+
+        if print_nominal_values and attribute is not None and attribute.nominal_values is not None:
+            text += '"' + attribute.nominal_values[i] + '"'
+        else:
+            text += str(thresholds[i])
 
     return text
 
