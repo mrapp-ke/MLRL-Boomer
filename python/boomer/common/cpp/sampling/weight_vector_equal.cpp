675bac090beb2d3419673618a5e7673e08709655
Michael Rapp <michael.rapp90@googlemail.com>
2020-11-22 23:06:04 +0100

    Move classes in sub_sampling.h/cpp to separate header/implementation files.

---
+++ python/boomer/common/cpp/sampling/weight_vector_equal.cpp
@@ -0,0 +1,19 @@
+#include "weight_vector_equal.h"
+
+
+EqualWeightVector::EqualWeightVector(uint32 numElements)
+    : numElements_(numElements) {
+
+}
+
+bool EqualWeightVector::hasZeroWeights() const {
+    return false;
+}
+
+uint32 EqualWeightVector::getWeight(uint32 pos) const {
+    return 1;
+}
+
+uint32 EqualWeightVector::getSumOfWeights() const {
+    return numElements_;
+}
