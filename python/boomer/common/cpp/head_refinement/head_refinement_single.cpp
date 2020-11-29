e214c78236892ac5d5a8bcfbe6978f0f26f4a875
Michael Rapp <michael.rapp90@googlemail.com>
2020-11-30 00:47:07 +0100

    The class SingleLabelHeadRefinement now inherits from ILabelWiseScoreProcessor.

--- python/boomer/common/cpp/head_refinement/head_refinement_single.cpp
+++ python/boomer/common/cpp/head_refinement/head_refinement_single.cpp
@@ -1,5 +1,6 @@
 #include "head_refinement_single.h"
 #include "prediction_partial.h"
+#include "../rule_evaluation/score_processor_label_wise.h"
 
 
 /**
@@ -9,7 +10,7 @@
  *           for the best head
  */
 template<class T>
-class SingleLabelHeadRefinement : public IHeadRefinement {
+class SingleLabelHeadRefinement : public IHeadRefinement, public ILabelWiseScoreProcessor {
 
     private:
 
@@ -17,25 +18,11 @@ class SingleLabelHeadRefinement : public IHeadRefinement {
 
         std::unique_ptr<PartialPrediction> headPtr_;
 
-    public:
-
-        /**
-         * @param labelIndices A reference to an object of template type `T` that provides access to the indices of the
-         *                     labels that should be considered when searching for the best head
-         */
-        SingleLabelHeadRefinement(const T& labelIndices)
-            : labelIndices_(labelIndices) {
-
-        }
-
-        const AbstractEvaluatedPrediction* findHead(const AbstractEvaluatedPrediction* bestHead,
-                                                    IStatisticsSubset& statisticsSubset, bool uncovered,
-                                                    bool accumulated) override {
-            const DenseLabelWiseScoreVector& scoreVector = statisticsSubset.calculateLabelWisePrediction(uncovered,
-                                                                                                         accumulated);
+        template<class T2>
+        const AbstractEvaluatedPrediction* processScoresInternally(const AbstractEvaluatedPrediction* bestHead,
+                                                                   const T2& scoreVector) {
             uint32 numPredictions = scoreVector.getNumElements();
-            DenseLabelWiseScoreVector::quality_score_const_iterator qualityScoreIterator =
-                scoreVector.quality_scores_cbegin();
+            typename T2::quality_score_const_iterator qualityScoreIterator = scoreVector.quality_scores_cbegin();
             uint32 bestC = 0;
             float64 bestQualityScore = qualityScoreIterator[bestC];
 
@@ -50,7 +37,7 @@ class SingleLabelHeadRefinement : public IHeadRefinement {
 
             // The quality score must be better than that of `bestHead`...
             if (bestHead == nullptr || bestQualityScore < bestHead->overallQualityScore) {
-                DenseLabelWiseScoreVector::score_const_iterator scoreIterator = scoreVector.scores_cbegin();
+                typename T2::score_const_iterator scoreIterator = scoreVector.scores_cbegin();
                 typename T::const_iterator indexIterator = labelIndices_.cbegin();
 
                 if (headPtr_.get() == nullptr) {
@@ -66,6 +53,30 @@ class SingleLabelHeadRefinement : public IHeadRefinement {
             }
 
             return nullptr;
+        }
+
+    public:
+
+        /**
+         * @param labelIndices A reference to an object of template type `T` that provides access to the indices of the
+         *                     labels that should be considered when searching for the best head
+         */
+        SingleLabelHeadRefinement(const T& labelIndices)
+            : labelIndices_(labelIndices) {
+
+        }
+
+        const AbstractEvaluatedPrediction* processScores(const AbstractEvaluatedPrediction* bestHead,
+                                                         const DenseLabelWiseScoreVector& scoreVector) override {
+            return processScoresInternally<DenseLabelWiseScoreVector>(bestHead, scoreVector);
+        }
+
+        const AbstractEvaluatedPrediction* findHead(const AbstractEvaluatedPrediction* bestHead,
+                                                    IStatisticsSubset& statisticsSubset, bool uncovered,
+                                                    bool accumulated) override {
+            const DenseLabelWiseScoreVector& scoreVector = statisticsSubset.calculateLabelWisePrediction(uncovered,
+                                                                                                         accumulated);
+            return this->processScores(bestHead, scoreVector);
         }
 
         std::unique_ptr<AbstractEvaluatedPrediction> pollHead() override {
