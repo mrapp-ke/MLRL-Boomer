09236bd6e743a2ff7b830d52a00c9cbc2652ae75
Michael Rapp <michael.rapp90@googlemail.com>
2020-11-29 23:28:57 +0100

    Replace classes EvaluatedPrediction and LabelWiseEvaluatedPrediction with DenseScoreVector and DenseLabelWiseScoreVector.

---
+++ python/boomer/common/cpp/rule_evaluation/score_vector_label_wise_dense.cpp
@@ -0,0 +1,23 @@
+#include "score_vector_label_wise_dense.h"
+
+
+DenseLabelWiseScoreVector::DenseLabelWiseScoreVector(uint32 numElements)
+    : DenseScoreVector(numElements), qualityScoreVector_(DenseVector<float64>(numElements)) {
+
+}
+
+DenseLabelWiseScoreVector::quality_score_iterator DenseLabelWiseScoreVector::quality_scores_begin() {
+    return qualityScoreVector_.begin();
+}
+
+DenseLabelWiseScoreVector::quality_score_iterator DenseLabelWiseScoreVector::quality_scores_end() {
+    return qualityScoreVector_.end();
+}
+
+DenseLabelWiseScoreVector::quality_score_const_iterator DenseLabelWiseScoreVector::quality_scores_cbegin() const {
+    return qualityScoreVector_.cbegin();
+}
+
+DenseLabelWiseScoreVector::quality_score_const_iterator DenseLabelWiseScoreVector::quality_scores_cend() const {
+    return qualityScoreVector_.cend();
+}
