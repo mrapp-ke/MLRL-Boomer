d9cd513433776a9e4aa344da1abaadfba1480539
Michael Rapp <michael.rapp90@googlemail.com>
2020-11-30 20:12:58 +0100

    Add missing includes.

--- python/boomer/boosting/cpp/rule_evaluation/rule_evaluation_factory_example_wise.h
+++ python/boomer/boosting/cpp/rule_evaluation/rule_evaluation_factory_example_wise.h
@@ -4,6 +4,8 @@
 #pragma once
 
 #include "rule_evaluation_example_wise.h"
+#include "../../../common/cpp/indices/index_vector_full.h"
+#include "../../../common/cpp/indices/index_vector_partial.h"
 #include <memory>
 
 
