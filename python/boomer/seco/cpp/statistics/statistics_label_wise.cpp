ec192e1a609a07cef487f38dd1b610235347a715
Michael Rapp <michael.rapp90@googlemail.com>
2020-11-30 19:46:37 +0100

    Move classes HeuristicLabelWiseRuleEvaluationFactoryImpl to new header/implementation files.

--- python/boomer/seco/cpp/statistics/statistics_label_wise.cpp
+++ python/boomer/seco/cpp/statistics/statistics_label_wise.cpp
@@ -1,5 +1,5 @@
 #include "statistics_label_wise.h"
-#include "../confusion_matrices.h"
+#include "../heuristics/confusion_matrices.h"
 #include "../../../common/cpp/statistics/statistics_subset_decomposable.h"
 #include <cstdlib>
 
