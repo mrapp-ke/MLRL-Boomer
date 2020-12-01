931ed4ad798cfe8062cefd7013c540d9cb29526d
Michael Rapp <michael.rapp90@googlemail.com>
2020-12-02 00:02:16 +0100

    Add missing namespace.

--- python/boomer/boosting/cpp/binning/label_binning_equal_width.h
+++ python/boomer/boosting/cpp/binning/label_binning_equal_width.h
@@ -6,18 +6,22 @@
 #include "label_binning.h"
 
 
-/**
- * Assigns labels to bins, based on the corresponding gradients, in a way such that each bin contains labels with
- * gradients from equally sized value ranges.
- *
- * @tparam T The type of the vector that provides access to the gradients
- */
-template<class T>
-class EqualWidthLabelBinning : public ILabelBinning<T> {
+namespace boosting {
 
-    public:
+    /**
+     * Assigns labels to bins, based on the corresponding gradients, in a way such that each bin contains labels with
+     * gradients from equally sized value ranges.
+     *
+     * @tparam T The type of the vector that provides access to the gradients
+     */
+    template<class T>
+    class EqualWidthLabelBinning : public ILabelBinning<T> {
 
-        void createBins(uint32 numPositiveBins, uint32 numNegativeBins, const T& statisticVector,
-                        IBinningObserver<float64>& observer) const override;
+        public:
 
-};
+            void createBins(uint32 numPositiveBins, uint32 numNegativeBins, const T& statisticVector,
+                            IBinningObserver<float64>& observer) const override;
+
+    };
+
+}
