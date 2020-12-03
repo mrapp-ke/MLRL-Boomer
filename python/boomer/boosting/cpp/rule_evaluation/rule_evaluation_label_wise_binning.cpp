da8fb1b383ec7e96bd79f5ddc4601d06f429bd0f
Michael Rapp <michael.rapp90@googlemail.com>
2020-12-03 21:59:35 +0100

    Set bin indices.

--- python/boomer/boosting/cpp/rule_evaluation/rule_evaluation_label_wise_binning.cpp
+++ python/boomer/boosting/cpp/rule_evaluation/rule_evaluation_label_wise_binning.cpp
@@ -89,6 +89,7 @@ class BinningLabelWiseRuleEvaluation : public ILabelWiseRuleEvaluation, public I
         void onBinUpdate(uint32 binIndex, uint32 originalIndex, float64 value) override {
             tmpGradients_[binIndex] += value;
             tmpHessians_[binIndex] += currentStatisticVector_->hessians_cbegin()[originalIndex];
+            scoreVector_.indices_binned_begin()[originalIndex] = binIndex;
         }
 
 };
