8160eea9adb0a0ffcfb811f72c0518d6716a88c9
Michael Rapp <michael.rapp90@googlemail.com>
2020-12-11 16:01:54 +0100

    Fix warning.

--- python/setup.py
+++ python/setup.py
@@ -1,8 +1,8 @@
 import sys
 
 import numpy
+import setuptools
 from Cython.Build import cythonize
-from setuptools import setup, Extension
 
 # True, if annotated Cython source files that highlight Python interactions should be created
 ANNOTATE = True
@@ -90,8 +90,9 @@ sources = [
 COMPILE_FLAG_OPEN_MP = '/openmp' if sys.platform.startswith('win') else '-fopenmp'
 
 extensions = [
-    Extension(name='*', sources=sources, language='c++', extra_compile_args=[COMPILE_FLAG_OPEN_MP],
-              extra_link_args=[COMPILE_FLAG_OPEN_MP], define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])
+    setuptools.Extension(name='*', sources=sources, language='c++', extra_compile_args=[COMPILE_FLAG_OPEN_MP],
+                         extra_link_args=[COMPILE_FLAG_OPEN_MP],
+                         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])
 ]
 
 compiler_directives = {
@@ -101,24 +102,25 @@ compiler_directives = {
     'initializedcheck': DEBUG
 }
 
-setup(name='boomer',
-      version='0.4.0',
-      description='BOOMER - An algorithm for learning gradient boosted multi-label classification rules',
-      url='https://github.com/mrapp-ke/Boomer',
-      author='Michael Rapp',
-      author_email='mrapp@ke.tu-darmstadt.de',
-      license='MIT',
-      packages=['boomer'],
-      install_requires=[
-          "numpy>=1.19.0",
-          "scipy>=1.5.0",
-          "Cython>=0.29.0",
-          'scikit-learn>=0.23.0',
-          'scikit-multilearn>=0.2.0',
-          'liac-arff>=2.5.0',
-          'requests>=2.25.0'
-      ],
-      python_requires='>=3.8',
-      ext_modules=cythonize(extensions, language_level='3', annotate=ANNOTATE, compiler_directives=compiler_directives),
-      include_dirs=[numpy.get_include()],
-      zip_safe=False)
+setuptools.setup(name='boomer',
+                 version='0.4.0',
+                 description='BOOMER - An algorithm for learning gradient boosted multi-label classification rules',
+                 url='https://github.com/mrapp-ke/Boomer',
+                 author='Michael Rapp',
+                 author_email='mrapp@ke.tu-darmstadt.de',
+                 license='MIT',
+                 packages=['boomer'],
+                 install_requires=[
+                     "numpy>=1.19.0",
+                     "scipy>=1.5.0",
+                     "Cython>=0.29.0",
+                     'scikit-learn>=0.23.0',
+                     'scikit-multilearn>=0.2.0',
+                     'liac-arff>=2.5.0',
+                     'requests>=2.25.0'
+                 ],
+                 python_requires='>=3.8',
+                 ext_modules=cythonize(extensions, language_level='3', annotate=ANNOTATE,
+                                       compiler_directives=compiler_directives),
+                 include_dirs=[numpy.get_include()],
+                 zip_safe=False)
