cc6e5f145a1fb6f69678a9ee4bc083e6498b3425
Michael Rapp <michael.rapp90@googlemail.com>
2020-12-13 23:48:50 +0100

    Edit comment.

--- python/boomer/common/cpp/statistics/histogram.h
+++ python/boomer/common/cpp/statistics/histogram.h
@@ -8,7 +8,7 @@
 
 /**
  * Defines an interface for all classes that provide access to statistics that are organized as a histogram, i.e., where
- * the statistics of multiple training examples are aggregated into individual bins.
+ * the statistics of multiple training examples are aggregated into the same bin.
  */
 class IHistogram : virtual public IImmutableStatistics {
 
