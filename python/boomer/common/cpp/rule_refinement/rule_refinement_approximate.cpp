7f18facf4eec35e18631257165016b4da013d075
Lukas E <lukas.eberle95@gmail.com>
2020-12-15 16:29:22 +0100

    Refactoring

--- python/boomer/common/cpp/rule_refinement/rule_refinement_approximate.cpp
+++ python/boomer/common/cpp/rule_refinement/rule_refinement_approximate.cpp
@@ -34,13 +34,13 @@ void ApproximateRuleRefinement<T>::findRefinement(const AbstractEvaluatedPredict
 
     for (; r < numBins; r++) {
         uint32 numExamples = iterator[r].numExamples;
-        uint32 originalIndex = iterator[r].index;
+        uint32 index = iterator[r].index;
 
         if (numExamples > 0) {
             previousValue = iterator[r].maxValue;
             previousR = r;
             numCoveredExamples += numExamples;
-            statisticsSubsetPtr->addToSubset(originalIndex, 1);
+            statisticsSubsetPtr->addToSubset(index, 1);
             break;
         }
     }
@@ -51,7 +51,7 @@ void ApproximateRuleRefinement<T>::findRefinement(const AbstractEvaluatedPredict
 
             if (numExamples > 0) {
                 float32 currentValue = iterator[r].minValue;
-                uint32 originalIndex = iterator[r].index;
+                uint32 index = iterator[r].index;
 
                 const AbstractEvaluatedPrediction* head = headRefinementPtr_->findHead(bestHead, *statisticsSubsetPtr,
                                                                                        false, false);
@@ -81,7 +81,7 @@ void ApproximateRuleRefinement<T>::findRefinement(const AbstractEvaluatedPredict
                 previousValue = iterator[r].maxValue;
                 previousR = r;
                 numCoveredExamples += numExamples;
-                statisticsSubsetPtr->addToSubset(originalIndex, 1);
+                statisticsSubsetPtr->addToSubset(index, 1);
             }
         }
     }
