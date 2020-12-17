cdeff824e4cae655355feec481c2f5bf46b4a43c
LukasEberle <lukas.eberle95@gmail.com>
2020-12-17 16:32:49 +0100

    Revert "WIP: Zuordnung von Bins zu darin enthaltenen Beispielen korrekt filtern"

--- python/boomer/common/cpp/binning/bin_vector.h
+++ python/boomer/common/cpp/binning/bin_vector.h
@@ -5,8 +5,9 @@
 
 #include "../data/indexed_value.h"
 #include "../data/vector_dense.h"
-#include "../data/vector_mapping_dense.h"
 #include "../data/bin.h"
+#include <unordered_map>
+#include <forward_list>
 
 
 /**
@@ -19,11 +20,13 @@ class BinVector final {
 
         typedef IndexedValue<float32> Example;
 
+        typedef std::forward_list<Example> ExampleList;
+
     private:
 
         DenseVector<Bin> vector_;
 
-        DenseMappingVector<Example> mapping_;
+        std::unordered_map<uint32, ExampleList> examplesPerBin_;
 
     public:
 
@@ -41,12 +44,6 @@ class BinVector final {
         typedef DenseVector<Bin>::iterator bin_iterator;
 
         typedef DenseVector<Bin>::const_iterator bin_const_iterator;
-
-        typedef DenseMappingVector<Example>::iterator example_list_iterator;
-
-        typedef DenseMappingVector<Example>::const_iterator example_list_const_iterator;
-
-        typedef DenseMappingVector<Example>::Entry ExampleList;
 
         /**
          * Returns a `bin_iterator` to the beginning of the bins.
@@ -77,32 +74,12 @@ class BinVector final {
         bin_const_iterator bins_cend() const;
 
         /**
-         * Returns an `example_list_iterator` to the beginning of the examples.
+         * Returns the list that stores the examples that correspond to a specific bin.
          *
-         * @return An `example_list_iterator` to the beginning
+         * @param binIndex  The index of the bin
+         * @return          A reference to an object of type `ExampleList` that stores the examples in the bin
          */
-        example_list_iterator examples_begin();
-
-        /**
-         * Returns an `example_list_iterator` to the end of the examples.
-         *
-         * @return An `example_list_iterator` to the end
-         */
-        example_list_iterator examples_end();
-
-        /**
-         * Returns an `example_list_const_iterator` to the beginning of the examples.
-         *
-         * @return An `example_list_const_iterator` to the beginning
-         */
-        example_list_const_iterator examples_cbegin() const;
-
-        /**
-         * Returns an `example_list_const_iterator` to the end of the examples.
-         *
-         * @return An `example_list_const_iterator` to the end
-         */
-        example_list_const_iterator examples_cend() const;
+        ExampleList& getExamples(uint32 binIndex);
 
         /**
          * Returns the number of elements in the vector.
