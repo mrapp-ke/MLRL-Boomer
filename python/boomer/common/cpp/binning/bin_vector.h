172bf7e489f4e90355be9fe424009c1b57c2f78e
Michael Rapp <michael.rapp90@googlemail.com>
2020-12-19 19:45:53 +0100

    Add changes that were missing from the latest pull request.

--- python/boomer/common/cpp/binning/bin_vector.h
+++ python/boomer/common/cpp/binning/bin_vector.h
@@ -5,9 +5,8 @@
 
 #include "../data/indexed_value.h"
 #include "../data/vector_dense.h"
+#include "../data/vector_mapping_dense.h"
 #include "../data/bin.h"
-#include <unordered_map>
-#include <forward_list>
 
 
 /**
@@ -20,13 +19,11 @@ class BinVector final {
 
         typedef IndexedValue<float32> Example;
 
-        typedef std::forward_list<Example> ExampleList;
-
     private:
 
         DenseVector<Bin> vector_;
 
-        std::unordered_map<uint32, ExampleList> examplesPerBin_;
+        DenseMappingVector<Example> mapping_;
 
     public:
 
@@ -44,6 +41,12 @@ class BinVector final {
         typedef DenseVector<Bin>::iterator bin_iterator;
 
         typedef DenseVector<Bin>::const_iterator bin_const_iterator;
+
+        typedef DenseMappingVector<Example>::iterator example_list_iterator;
+
+        typedef DenseMappingVector<Example>::const_iterator example_list_const_iterator;
+
+        typedef DenseMappingVector<Example>::Entry ExampleList;
 
         /**
          * Returns a `bin_iterator` to the beginning of the bins.
@@ -74,12 +77,33 @@ class BinVector final {
         bin_const_iterator bins_cend() const;
 
         /**
+         * Returns an `example_list_iterator` to the beginning of the examples.
          * Returns the list that stores the examples that correspond to a specific bin.
          *
-         * @param binIndex  The index of the bin
-         * @return          A reference to an object of type `ExampleList` that stores the examples in the bin
+         * @return An `example_list_iterator` to the beginning
          */
-        ExampleList& getExamples(uint32 binIndex);
+        example_list_iterator examples_begin();
+
+        /**
+         * Returns an `example_list_iterator` to the end of the examples.
+         *
+         * @return An `example_list_iterator` to the end
+         */
+        example_list_iterator examples_end();
+
+        /**
+         * Returns an `example_list_const_iterator` to the beginning of the examples.
+         *
+         * @return An `example_list_const_iterator` to the beginning
+         */
+        example_list_const_iterator examples_cbegin() const;
+
+        /**
+         * Returns an `example_list_const_iterator` to the end of the examples.
+         *
+         * @return An `example_list_const_iterator` to the end
+         */
+        example_list_const_iterator examples_cend() const;
 
         /**
          * Returns the number of elements in the vector.
@@ -95,7 +119,6 @@ class BinVector final {
          * @param freeMemory    True, if unused memory should be freed, if possible, false otherwise
          */
         void setNumElements(uint32 numElements, bool freeMemory);
-
 
         /**
          * Swaps the examples that correspond to the bin at a specific position with the examples of another bin.
