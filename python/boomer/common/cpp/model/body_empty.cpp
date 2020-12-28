11acfcbfeb22049172e73d092031b0d9132a7ffe
Michael Rapp <michael.rapp90@googlemail.com>
2020-12-28 17:00:53 +0100

    Add class EmptyBody.

---
+++ python/boomer/common/cpp/model/body_empty.cpp
@@ -0,0 +1,15 @@
+#include "body_empty.h"
+
+
+bool EmptyBody::covers(CContiguousFeatureMatrix::const_iterator begin,
+                       CContiguousFeatureMatrix::const_iterator end) const {
+    return true;
+}
+
+bool EmptyBody::covers(CsrFeatureMatrix::index_const_iterator indicesBegin,
+                       CsrFeatureMatrix::index_const_iterator indicesEnd,
+                       CsrFeatureMatrix::value_const_iterator valuesBegin,
+                       CsrFeatureMatrix::value_const_iterator valuesEnd, float32* tmpArray1, uint32* tmpArray2,
+                       uint32 n) const {
+    return true;
+}
