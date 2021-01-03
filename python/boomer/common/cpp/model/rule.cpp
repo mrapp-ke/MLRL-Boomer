307d70e5ced3647b2decb2b11f765d7733e7079b
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-03 18:08:09 +0100

    Add the class Rule.

---
+++ python/boomer/common/cpp/model/rule.cpp
@@ -0,0 +1,70 @@
+#include "rule.h"
+
+
+Rule::Rule(std::unique_ptr<IBody> bodyPtr, std::unique_ptr<IHead> headPtr)
+    : bodyPtr_(std::move(bodyPtr)), headPtr_(std::move(headPtr)) {
+
+}
+
+void Rule::predict(const CContiguousFeatureMatrix& featureMatrix, DensePredictionMatrix& predictionMatrix) const {
+    uint32 numExamples = featureMatrix.getNumExamples();
+
+    for (uint32 r = 0; r < numExamples; r++) {
+        bool covers = bodyPtr_->covers(featureMatrix.row_cbegin(r), featureMatrix.row_cend(r));
+
+        if (covers) {
+            headPtr_->apply(predictionMatrix.row_begin(r), predictionMatrix.row_end(r));
+        }
+    }
+}
+
+void Rule::predict(const CContiguousFeatureMatrix& featureMatrix, DensePredictionMatrix& predictionMatrix,
+                   Mask& mask) const {
+    uint32 numExamples = featureMatrix.getNumExamples();
+
+    for (uint32 r = 0; r < numExamples; r++) {
+        bool covers = bodyPtr_->covers(featureMatrix.row_cbegin(r), featureMatrix.row_cend(r));
+
+        if (covers) {
+            headPtr_->apply(predictionMatrix.row_begin(r), predictionMatrix.row_end(r), mask.row_begin(r),
+                            mask.row_end(r));
+        }
+    }
+}
+
+void Rule::predict(const CsrFeatureMatrix& featureMatrix, DensePredictionMatrix& predictionMatrix, float32* tmpArray1,
+                   uint32* tmpArray2, uint32 n) const {
+    uint32 numExamples = featureMatrix.getNumExamples();
+    uint32 i = n;
+
+    for (uint32 r = 0; r < numExamples; r++) {
+        bool covers = bodyPtr_->covers(featureMatrix.row_indices_cbegin(r), featureMatrix.row_indices_cend(r),
+                                       featureMatrix.row_values_cbegin(r), featureMatrix.row_values_cend(r), tmpArray1,
+                                       tmpArray2, i);
+
+        if (covers) {
+            headPtr_->apply(predictionMatrix.row_begin(r), predictionMatrix.row_end(r));
+        }
+
+        i++;
+    }
+}
+
+void Rule::predict(const CsrFeatureMatrix& featureMatrix, DensePredictionMatrix& predictionMatrix, float32* tmpArray1,
+                   uint32* tmpArray2, uint32 n, Mask& mask) const {
+    uint32 numExamples = featureMatrix.getNumExamples();
+    uint32 i = n;
+
+    for (uint32 r = 0; r < numExamples; r++) {
+        bool covers = bodyPtr_->covers(featureMatrix.row_indices_cbegin(r), featureMatrix.row_indices_cend(r),
+                                       featureMatrix.row_values_cbegin(r), featureMatrix.row_values_cend(r), tmpArray1,
+                                       tmpArray2, i);
+
+        if (covers) {
+            headPtr_->apply(predictionMatrix.row_begin(r), predictionMatrix.row_end(r), mask.row_begin(r),
+                            mask.row_end(r));
+        }
+
+        i++;
+    }
+}
