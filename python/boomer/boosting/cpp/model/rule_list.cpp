2888f111159f63db165b89719f41f79c3f347661
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-03 22:44:34 +0100

    Forward declare class RuleList.

--- python/boomer/boosting/cpp/model/rule_list.cpp
+++ python/boomer/boosting/cpp/model/rule_list.cpp
@@ -1,31 +1,52 @@
 #include "rule_list.h"
+#include "../../../common/cpp/model/rule.h"
 #include "../../../common/cpp/model/body_empty.h"
 #include "../../../common/cpp/model/body_conjunctive.h"
+#include <list>
 
 
-void RuleList::append(std::unique_ptr<Rule> rulePtr) {
-    list_.push_back(std::move(rulePtr));
-}
+/**
+ * A model that stores several rules in a list.
+ */
+class RuleList final : public IModel {
 
-void RuleList::predict(const CContiguousFeatureMatrix& featureMatrix, DenseMatrix<float64>& predictionMatrix) const {
-    for (auto it = list_.cbegin(); it != list_.cend(); it++) {
-        const Rule& rule = **it;
-        rule.predict(featureMatrix, predictionMatrix);
-    }
-}
+    private:
 
-void RuleList::predict(const CsrFeatureMatrix& featureMatrix, DenseMatrix<float64>& predictionMatrix) const {
-    uint32 numFeatures = featureMatrix.getNumFeatures();
-    float32 tmpArray1[numFeatures];
-    uint32 tmpArray2[numFeatures] = {};
-    uint32 n = 1;
+        std::list<std::unique_ptr<Rule>> list_;
 
-    for (auto it = list_.cbegin(); it != list_.cend(); it++) {
-        const Rule& rule = **it;
-        rule.predict(featureMatrix, predictionMatrix, &tmpArray1[0], &tmpArray2[0], n);
-        n++;
-    }
-}
+    public:
+
+        /**
+         * Adds a new rule to the model.
+         *
+         * @param rulePtr An unique pointer to an object of type `Rule` that should be added
+         */
+        void append(std::unique_ptr<Rule> rulePtr) {
+            list_.push_back(std::move(rulePtr));
+        }
+
+        void predict(const CContiguousFeatureMatrix& featureMatrix,
+                     DenseMatrix<float64>& predictionMatrix) const override {
+            for (auto it = list_.cbegin(); it != list_.cend(); it++) {
+                const Rule& rule = **it;
+                rule.predict(featureMatrix, predictionMatrix);
+            }
+        }
+
+        void predict(const CsrFeatureMatrix& featureMatrix, DenseMatrix<float64>& predictionMatrix) const override {
+            uint32 numFeatures = featureMatrix.getNumFeatures();
+            float32 tmpArray1[numFeatures];
+            uint32 tmpArray2[numFeatures] = {};
+            uint32 n = 1;
+
+            for (auto it = list_.cbegin(); it != list_.cend(); it++) {
+                const Rule& rule = **it;
+                rule.predict(featureMatrix, predictionMatrix, &tmpArray1[0], &tmpArray2[0], n);
+                n++;
+            }
+        }
+
+};
 
 void RuleListBuilder::setDefaultRule(const AbstractPrediction* prediction) {
     if (prediction != nullptr) {
--- python/boomer/boosting/cpp/model/rule_list.h
+++ python/boomer/boosting/cpp/model/rule_list.h
@@ -4,34 +4,10 @@
 #pragma once
 
 #include "../../../common/cpp/model/model_builder.h"
-#include "../../../common/cpp/model/rule.h"
-#include <list>
 
+// Forward declarations
+class RuleList;
 
-/**
- * A model that stores several rules in a list.
- */
-class RuleList final : public IModel {
-
-    private:
-
-        std::list<std::unique_ptr<Rule>> list_;
-
-    public:
-
-        /**
-         * Adds a new rule to the model.
-         *
-         * @param rulePtr An unique pointer to an object of type `Rule` that should be added
-         */
-        void append(std::unique_ptr<Rule> rulePtr);
-
-        void predict(const CContiguousFeatureMatrix& featureMatrix,
-                     DenseMatrix<float64>& predictionMatrix) const override;
-
-        void predict(const CsrFeatureMatrix& featureMatrix, DenseMatrix<float64>& predictionMatrix) const override;
-
-};
 
 /**
  * Allows to build models that store several rules in a list.
