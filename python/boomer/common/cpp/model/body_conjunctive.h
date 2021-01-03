e2b952f99913d914cdcd00d5ab68e652a3ab40ee
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-03 18:16:32 +0100

    Add final modifier.

--- python/boomer/common/cpp/model/body_conjunctive.h
+++ python/boomer/common/cpp/model/body_conjunctive.h
@@ -11,7 +11,7 @@
  * A body that consists of a conjunction of conditions using the operators <= or > for numerical conditions, and = or !=
  * for nominal conditions, respectively.
  */
-class ConjunctiveBody : public IBody {
+class ConjunctiveBody final : public IBody {
 
     private:
 
