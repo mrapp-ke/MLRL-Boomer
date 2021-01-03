43b1ae5506e185f7620c7185d4b35db6f3edd1a6
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-03 19:01:32 +0100

    Add class FullHead.

---
+++ python/boomer/common/cpp/model/head_full.cpp
@@ -0,0 +1,31 @@
+#include "head_full.h"
+
+
+FullHead::FullHead(const FullPrediction& prediction)
+    : numScores_(prediction.getNumElements()), scores_(new float64[numScores_]) {
+    FullPrediction::score_const_iterator iterator = prediction.scores_cbegin();
+
+    for (uint32 i = 0; i < numScores_; i++) {
+        scores_[i] = iterator[i];
+    }
+}
+
+FullHead::~FullHead() {
+    delete[] scores_;
+}
+
+void FullHead::apply(DensePredictionMatrix::iterator begin, DensePredictionMatrix::iterator end) const {
+    for (uint32 i = 0; i < numScores_; i++) {
+        begin[i] += scores_[i];
+    }
+}
+
+void FullHead::apply(DensePredictionMatrix::iterator predictionsBegin, DensePredictionMatrix::iterator predictionsEnd,
+                     Mask::iterator maskBegin, Mask::iterator maskEnd) const {
+    for (uint32 i = 0; i < numScores_; i++) {
+        if (maskBegin[i]) {
+            predictionsBegin[i] += scores_[i];
+            maskBegin[i] = false;
+        }
+    }
+}
