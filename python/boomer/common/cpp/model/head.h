d8421067acae0bde64467d4f2bf2fab18e8ac6d8
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-03 17:41:47 +0100

    Edit type of function arguments.

--- python/boomer/common/cpp/model/head.h
+++ python/boomer/common/cpp/model/head.h
@@ -36,7 +36,8 @@ class IHead {
         /**
          * Adds the scores that are contained by the head to a given vector of predictions.
          *
-         * The prediction is restricted to certain labels according to a given mask.
+         * The prediction is restricted to certain labels according to a given mask. The mask will be updated by this
+         * function.
          *
          * @param predictionsBegin  An iterator to the beginning of the predictions to be updated
          * @param predictionsEnd    An iterator to the end of the predictions to be updated
@@ -44,7 +45,7 @@ class IHead {
          * @param maskEnd           An iterator to the end of the mask
          */
         virtual void apply(DensePredictionMatrix::iterator predictionsBegin,
-                           DensePredictionMatrix::iterator predictionsEnd, Mask::const_iterator maskBegin,
-                           Mask::const_iterator maskEnd) const = 0;
+                           DensePredictionMatrix::iterator predictionsEnd, Mask::iterator maskBegin,
+                           Mask::iterator maskEnd) const = 0;
 
 };
