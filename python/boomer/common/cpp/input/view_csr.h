cb6fc3c759e8fbfdacd2ad8e2b9528b4180f7641
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-04 19:30:33 +0100

    Add class CsrView.

---
+++ python/boomer/common/cpp/input/view_csr.h
@@ -0,0 +1,95 @@
+/**
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ */
+#pragma once
+
+#include "../data/types.h"
+
+
+/**
+ * Implements row-wise read-only access to the values that are stored in a pre-allocated matrix in the compressed sparse
+ * row (CSR) format.
+ *
+ * @tparam T The type of the values
+ */
+template<class T>
+class CsrView {
+
+    private:
+
+        uint32 numRows_;
+
+        uint32 numCols_;
+
+        const T* data_;
+
+        const uint32* rowIndices_;
+
+        const uint32* colIndices_;
+
+    public:
+
+        /**
+         * @param numRows       The number of rows in the view
+         * @param numCols       The number of cols in the view
+         * @param data          A pointer to an array of template type `T`, shape `(num_non_zero_values)`, that stores
+         *                      all non-zero values
+         * @param xRowIndices   A pointer to an array of type `uint32`, shape `(num_examples + 1)`, that stores the
+         *                      indices of the first element in `data` and `colIndices` that corresponds to a certain
+         *                      row. The index at the last position is equal to `num_non_zero_values`
+         * @param xColIndices   A pointer to an array of type `uint32`, shape `(num_non_zero_values)`, that stores the
+         *                      column-indices, the values in `data` correspond to
+         */
+        CsrView(uint32 numRows, uint32 numCols, const T* data, const uint32* rowIndices, const uint32* colIndices);
+
+        typedef const float32* value_const_iterator;
+
+        typedef const uint32* index_const_iterator;
+
+        /**
+         * Returns a `value_const_iterator` to the beginning of the values at a specific row.
+         *
+         * @param row   The row
+         * @return      A `value_const_iterator` to the beginning of the values
+         */
+        value_const_iterator row_values_cbegin(uint32 row) const;
+
+        /**
+         * Returns a `value_const_iterator` to the end of the values at a specific row.
+         *
+         * @param row   The row
+         * @return      A `value_const_iterator` to the end of the values
+         */
+        value_const_iterator row_values_cend(uint32 row) const;
+
+        /**
+         * Returns an `index_const_iterator` to the beginning of the indices at a specific row.
+         *
+         * @param row   The row
+         * @return      An `index_const_iterator` to the beginning of the indices
+         */
+        index_const_iterator row_indices_cbegin(uint32 row) const;
+
+        /**
+         * Returns an `index_const_iterator` to the end of the indices at a specific row.
+         *
+         * @param row   The row
+         * @return      An `index_const_iterator` to the end of the indices
+         */
+        index_const_iterator row_indices_cend(uint32 row) const;
+
+        /**
+         * Returns the number of rows in the view.
+         *
+         * @return The number of rows
+         */
+        uint32 getNumRows() const;
+
+        /**
+         * Returns the number of columns in the view.
+         *
+         * @return The number of columns
+         */
+        uint32 getNumCols() const;
+
+};
