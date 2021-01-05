368f2d70b9714ea9e27217fca424ce9171b4db66
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-05 16:00:56 +0100

    Add comments.

--- python/boomer/common/learners.py
+++ python/boomer/common/learners.py
@@ -32,6 +32,15 @@ class Learner(BaseEstimator):
     """
 
     def fit(self, x, y):
+        """
+        Fits a model according to given training examples and corresponding ground truth labels.
+
+        :param x:   A `numpy.ndarray` or `scipy.sparse` matrix, shape `(num_examples, num_features)`, that stores the
+                    feature values of the training examples
+        :param y:   A `numpy.ndarray` or `scipy.sparse` matrix, shape `(num_examples, num_labels)`, that stores the
+                    labels of the training examples according to the ground truth
+        :return:    The fitted learner
+        """
         log.info('Fitting model...')
         start_time = timer()
         model = self._fit(x, y)
@@ -42,6 +51,14 @@ class Learner(BaseEstimator):
         return self
 
     def predict(self, x):
+        """
+        Makes a prediction for given query examples.
+
+        :param x:   A `numpy.ndarray` or `scipy.sparse` matrix, shape `(num_examples, num_features)`, that stores the
+                    feature values of the query examples
+        :return:    A `numpy.ndarray` or `scipy.sparse` matrix of shape `(num_examples, num_labels)`, that stores the
+                    prediction for individual examples and labels
+        """
         check_is_fitted(self)
         log.info("Making a prediction for %s query instances...", x.shape[0])
         return self._predict(x)
