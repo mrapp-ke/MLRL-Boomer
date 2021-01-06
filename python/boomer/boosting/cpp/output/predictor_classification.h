26dece598e4f1363d37f8bec06d18cd543049577
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-06 21:28:06 +0100

    Add class ClassificationPredictor.

---
+++ python/boomer/boosting/cpp/output/predictor_classification.h
@@ -0,0 +1,36 @@
+/**
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ */
+#pragma once
+
+#include "../../../common/cpp/output/predictor.h"
+
+
+/**
+ * Allows to predict the labels of given query examples using an existing rule-based model that has been learned using a
+ * boosting algorithm.
+ *
+ * For prediction, the scores that are provided by the individual rules, are summed up. The aggregated scores are then
+ * transformed into binary values according to a certain threshold (1 if a score exceeds the threshold, i.e., the label
+ * is relevant, 0 otherwise).
+ */
+class ClassificationPredictor : public IPredictor<uint8> {
+
+    private:
+
+        float64 threshold_;
+
+    public:
+
+        /**
+         * @param threshold The threshold to be used
+         */
+        ClassificationPredictor(float64 threshold);
+
+        void predict(const CContiguousFeatureMatrix& featureMatrix, CContiguousView<uint8>& predictionMatrix,
+                     const RuleModel& model) const override;
+
+        void predict(const CsrFeatureMatrix& featureMatrix, CContiguousView<uint8>& predictionMatrix,
+                     const RuleModel& model) const override;
+
+};
