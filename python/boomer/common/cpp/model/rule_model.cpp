074cbfafb406119dac67c54d46144fdde3854313
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-06 21:21:10 +0100

    Add functionality to iterate the rules in a RuleModel.

--- python/boomer/common/cpp/model/rule_model.cpp
+++ python/boomer/common/cpp/model/rule_model.cpp
@@ -1,6 +1,14 @@
 #include "rule_model.h"
 
 
+RuleModel::const_iterator RuleModel::cbegin() const {
+    return list_.cbegin();
+}
+
+RuleModel::const_iterator RuleModel::cend() const {
+    return list_.cend();
+}
+
 void RuleModel::addRule(std::unique_ptr<IBody> bodyPtr, std::unique_ptr<IHead> headPtr) {
     list_.emplace_back(std::move(bodyPtr), std::move(headPtr));
 }
