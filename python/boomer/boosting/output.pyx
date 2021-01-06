c534f959ca60b36bd5dac9d442b375f44d2ecadb
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-06 23:42:57 +0100

    Add missing template argument.

--- python/boomer/boosting/output.pyx
+++ python/boomer/boosting/output.pyx
@@ -3,7 +3,15 @@
 
 Provides classes for making predictions using rule-based models that have been learned by the boosting algorithm.
 """
+from boomer.common._arrays cimport c_matrix_uint8
+from boomer.common._data cimport CContiguousView
+from boomer.common.input cimport CContiguousFeatureMatrixImpl
+
 from libcpp.memory cimport make_unique
+
+from cython.operator cimport dereference
+
+import numpy as np
 
 
 cdef class ClassificationPredictor(Predictor):
@@ -11,12 +19,26 @@ cdef class ClassificationPredictor(Predictor):
     A wrapper for the C++ class `ClassificationPredictor`.
     """
 
-    def __cinit__(self, float64 threshold):
+    def __cinit__(self, uint32 num_labels, float64 threshold):
+        """
+        :param num_labels:  The total number of available labels
+        :param thresholds:  The threshold to be used for making predictions
+        """
+        self.num_labels = num_labels
         self.predictor_ptr = make_unique[ClassificationPredictorImpl](threshold)
 
     cpdef object predict(self, float32[:, ::1] array, RuleModel model):
-        # TODO Implement
-        pass
+        cdef uint32 num_examples = array.shape[0]
+        cdef uint32 num_features = array.shape[1]
+        cdef uint32 num_labels = self.num_labels
+        cdef uint8[:, ::1] prediction_matrix = c_matrix_uint8(num_examples, num_labels)
+        cdef unique_ptr[CContiguousView[uint8]] view_ptr = make_unique[CContiguousView[uint8]](
+            num_examples, num_labels, &prediction_matrix[0, 0])
+        cdef unique_ptr[CContiguousFeatureMatrixImpl] feature_matrix_ptr = make_unique[CContiguousFeatureMatrixImpl](
+            num_examples, num_features, &array[0, 0])
+        self.predictor_ptr.get().predict(dereference(feature_matrix_ptr.get()), dereference(view_ptr.get()),
+                                         dereference(model.model_ptr.get()))
+        return np.asarray(prediction_matrix)
 
     cpdef object predict_csr(self, float32[::1] data, uint32[::1] row_indices, uint32[::1] col_indices,
                              uint32 num_features, RuleModel model):
