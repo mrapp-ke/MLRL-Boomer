ff2281d32fa91f769a7c91f8888f563089ee303d
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-08 01:52:40 +0100

    Edit comment.

--- python/boomer/common/cpp/model/head.h
+++ python/boomer/common/cpp/model/head.h
@@ -51,10 +51,11 @@ class IHead {
                            PredictionMask::iterator maskEnd) const = 0;
 
         /**
-         * Invokes one of the given visitors, depending on which one is able to handle this particular head.
+         * Invokes one of the given visitor functions, depending on which one is able to handle this particular type of
+         * head.
          *
-         * @param fullHeadVisitor       The visitor for handling objects of the type `FullHead`
-         * @param partialHeadVisitor    The visitor for handling objects of the type `PartialHead`
+         * @param fullHeadVisitor       The visitor function for handling objects of the type `FullHead`
+         * @param partialHeadVisitor    The visitor function for handling objects of the type `PartialHead`
          */
         virtual void visit(FullHeadVisitor fullHeadVisitor, PartialHeadVisitor partialHeadVisitor) const = 0;
 
