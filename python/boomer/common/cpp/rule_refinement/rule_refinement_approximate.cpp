ac3403b3b7d59189012cd381540ea698c4d2a4d3
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-19 23:41:17 +0100

    The class ApproximateRuleRefinement now properly handles nominal features.

--- python/boomer/common/cpp/rule_refinement/rule_refinement_approximate.cpp
+++ python/boomer/common/cpp/rule_refinement/rule_refinement_approximate.cpp
@@ -63,30 +63,49 @@ void ApproximateRuleRefinement<T>::findRefinement(const AbstractEvaluatedPredict
 
                 if (head != nullptr) {
                     bestHead = head;
-                    refinementPtr->comparator = LEQ;
-                    refinementPtr->threshold = calculateThreshold(previousValue, currentValue);
                     refinementPtr->end = r;
                     refinementPtr->previous = previousR;
                     refinementPtr->coveredWeights = sumOfWeights;
                     refinementPtr->covered = true;
+
+                    if (nominal_) {
+                        refinementPtr->comparator = LEQ;
+                        refinementPtr->threshold = calculateThreshold(previousValue, currentValue);
+                    } else {
+                        refinementPtr_->comparator = EQ;
+                        refinementPtr_->threshold = previousValue;
+                    }
                 }
 
                 head = headRefinementPtr_->findHead(bestHead, *statisticsSubsetPtr, true, false);
 
                 if (head != nullptr) {
                     bestHead = head;
-                    refinementPtr->comparator = GR;
-                    refinementPtr->threshold = calculateThreshold(previousValue, currentValue);
                     refinementPtr->end = r;
                     refinementPtr->previous = previousR;
                     refinementPtr->coveredWeights = sumOfWeights;
                     refinementPtr->covered = false;
+
+                    if (nominal_) {
+                        refinementPtr->comparator = GR;
+                        refinementPtr->threshold = calculateThreshold(previousValue, currentValue);
+                    } else {
+                        refinementPtr_->comparator = NEQ;
+                        refinementPtr_->threshold = previousValue;
+                    }
                 }
 
                 previousValue = binIterator[r].maxValue;
                 previousR = r;
                 sumOfWeights += weight;
                 statisticsSubsetPtr->addToSubset(binIndex, 1);
+
+                // Reset the subset in case of a nominal feature, as the previous bins will not be covered by the next
+                // condition...
+                if (nominal_) {
+                    statisticsSubsetPtr->resetSubset();
+                    sumOfWeights = 0;
+                }
             }
         }
     }
