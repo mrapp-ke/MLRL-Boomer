abd9fdd24a72664db87f093e8bf126f3628ee46d
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-25 01:11:19 +0100

    Edit comment.

--- python/boomer/common/cpp/model/head_partial.h
+++ python/boomer/common/cpp/model/head_partial.h
@@ -35,9 +35,9 @@ class PartialHead final : public IHead {
         typedef const uint32* index_const_iterator;
 
         /**
-         * Returns the number of numerical scores that are contained by the head.
+         * Returns the number of scores that are contained by the head.
          *
-         * @return The number of numerical scores
+         * @return The number of scores
          */
         uint32 getNumElements() const;
 
