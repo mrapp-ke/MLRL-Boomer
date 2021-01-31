a9d0bc3bc9932cda584aeb6896a5cf07794191de
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-31 23:58:40 +0100

    Implement function "induceDefaultRule" of the class TopDownRuleInduction.

--- python/boomer/common/cpp/rule_induction/rule_induction_top_down.cpp
+++ python/boomer/common/cpp/rule_induction/rule_induction_top_down.cpp
@@ -1,10 +1,35 @@
 #include "rule_induction_top_down.h"
+#include "../indices/index_vector_full.h"
 
 
 void TopDownRuleInduction::induceDefaultRule(IStatisticsProvider& statisticsProvider,
                                              const IHeadRefinementFactory* headRefinementFactory,
                                              IModelBuilder& modelBuilder) const {
-    // TODO
+    if (headRefinementFactory != nullptr) {
+        IStatistics& statistics = statisticsProvider.get();
+        uint32 numStatistics = statistics.getNumStatistics();
+        uint32 numLabels = statistics.getNumLabels();
+        statistics.resetSampledStatistics();
+
+        for (uint32 i = 0; i < numStatistics; i++) {
+            statistics.addSampledStatistic(i, 1);
+        }
+
+        FullIndexVector labelIndices(numLabels);
+        std::unique_ptr<IStatisticsSubset> statisticsSubsetPtr = labelIndices.createSubset(statistics);
+        std::unique_ptr<IHeadRefinement> headRefinementPtr = headRefinementFactory->create(labelIndices);
+        headRefinementPtr->findHead(nullptr, *statisticsSubsetPtr, true, false);
+        std::unique_ptr<AbstractEvaluatedPrediction> defaultPredictionPtr = headRefinementPtr->pollHead();
+        statisticsProvider.switchRuleEvaluation();
+
+        for (uint32 i = 0; i < numStatistics; i++) {
+            defaultPredictionPtr->apply(statistics, i);
+        }
+
+        modelBuilder.setDefaultRule(*defaultPredictionPtr);
+    } else {
+        statisticsProvider.switchRuleEvaluation();
+    }
 }
 
 bool TopDownRuleInduction::induceRule(IThresholds& thresholds, const IIndexVector& labelIndices,
