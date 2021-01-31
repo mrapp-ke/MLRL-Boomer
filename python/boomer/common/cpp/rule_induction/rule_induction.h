2021c05959d4853b368f79c588a8a2690a1f2133
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-31 23:43:00 +0100

    Add class IRuleInduction.

---
+++ python/boomer/common/cpp/rule_induction/rule_induction.h
@@ -0,0 +1,73 @@
+/**
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ */
+#pragma once
+
+#include "../head_refinement/head_refinement_factory.h"
+#include "../model/model_builder.h"
+#include "../pruning/pruning.h"
+#include "../sampling/feature_sampling.h"
+#include "../sampling/weight_vector.h"
+#include "../statistics/statistics_provider.h"
+#include "../thresholds/thresholds.h"
+
+
+
+/**
+ * Defines an interface for all classes that implement an algorithm for inducing individual classification rules.
+ */
+class IRuleInduction {
+
+    public:
+
+        virtual ~IRuleInduction() { };
+
+        /**
+         * Induces the default rule.
+         *
+         * @param statisticsProvider    A reference to an object of type `IStatisticsProvider` that provides access to
+         *                              the statistics which should serve as the basis for inducing the default rule
+         * @param headRefinementFactory A pointer to an object of type `IHeadRefinementFactory` that allows to create
+         *                              instances of the class that should be used to find the head of the default rule
+         *                              or a null pointer, if no default rule should be induced
+         * @param modelBuilder          A reference to an object of type `IModelBuilder`, the default rule should be
+         *                              added to
+         */
+        virtual void induceDefaultRule(IStatisticsProvider& statisticsProvider,
+                                       const IHeadRefinementFactory* headRefinementFactory,
+                                       IModelBuilder& modelBuilder) const = 0;
+
+        /**
+         * Induces a new classification rule.
+         *
+         * @param thresholds            A reference to an object of type `IThresholds` that provides access to the
+         *                              thresholds that may be used by the conditions of the rule
+         * @param labelIndices          A reference to an object of type `IIndexVector` that provides access to the
+         *                              indices of the labels for which the rule may predict
+         * @param weights               A reference to an object of type `IWeightVector` that provides access to the
+         *                              weights of individual training examples
+         * @param featureSubSampling    A reference to an object of type `IFeatureSubSampling` that should be used for
+         *                              sampling the features that may be used by a new condition
+         * @param pruning               A reference to an object of type `IPruning` that should be used to prune the
+         *                              rule
+         * @param postProcessor         A reference to an object of type `IPostProcessor` that should be used to
+         *                              post-process the predictions of the rule
+         * @param minCoverage           The minimum number of training examples that must be covered by the rule. Must
+         *                              be at least 1
+         * @param maxConditions         The maximum number of conditions to be included in the rule's body. Must be at
+         *                              least 1 or -1, if the number of conditions should not be restricted
+         * @param maxHeadRefinements    The maximum number of times, the head of the rule may be refinement after a new
+         *                              condition has been added to its body. Must be at least 1 or -1, if the number of
+         *                              refinements should not be restricted
+         * @param rng                   A reference to an object of type `RNG` that implements the random number
+         *                              generator to be used
+         * @param modelBuilder          A reference to an object of type `IModelBuilder`, the rule should be added to
+         * @return                      True, if a rule has been induced, false otherwise
+         */
+        virtual bool induceRule(IThresholds& thresholds, const IIndexVector& labelIndices,
+                                const IWeightVector& weights, const IFeatureSubSampling& featureSubSampling,
+                                const IPruning& pruning, const IPostProcessor& postProcessor, uint32 minCoverage,
+                                intp maxConditions, intp maxHeadRefinements, int numThreads, RNG& rng,
+                                IModelBuilder& modelBuilder) const = 0;
+
+};
