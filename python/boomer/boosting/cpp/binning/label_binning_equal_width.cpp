9807bda7e3b64b98c152ec2ef15c024c872aa9d1
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-31 22:44:35 +0100

    Use namespace blocks in cpp files.

--- python/boomer/boosting/cpp/binning/label_binning_equal_width.cpp
+++ python/boomer/boosting/cpp/binning/label_binning_equal_width.cpp
@@ -4,116 +4,120 @@
 #include "../data/vector_dense_example_wise.h"
 #include <limits>
 
-using namespace boosting;
 
+namespace boosting {
 
-template<class T>
-EqualWidthLabelBinning<T>::EqualWidthLabelBinning(float32 binRatio, uint32 minBins, uint32 maxBins)
-    : binRatio_(binRatio), minBins_(minBins), maxBins_(maxBins) {
+    template<class T>
+    EqualWidthLabelBinning<T>::EqualWidthLabelBinning(float32 binRatio, uint32 minBins, uint32 maxBins)
+        : binRatio_(binRatio), minBins_(minBins), maxBins_(maxBins) {
 
-}
+    }
 
-template<class T>
-uint32 EqualWidthLabelBinning<T>::getMaxBins(uint32 numLabels) const {
-    return calculateNumBins(numLabels, binRatio_, minBins_, maxBins_) + 1;
-}
+    template<class T>
+    uint32 EqualWidthLabelBinning<T>::getMaxBins(uint32 numLabels) const {
+        return calculateNumBins(numLabels, binRatio_, minBins_, maxBins_) + 1;
+    }
 
-template<class T>
-LabelInfo EqualWidthLabelBinning<T>::getLabelInfo(const T& statisticVector) const {
-    LabelInfo labelInfo;
-    uint32 numStatistics = statisticVector.getNumElements();
+    template<class T>
+    LabelInfo EqualWidthLabelBinning<T>::getLabelInfo(const T& statisticVector) const {
+        LabelInfo labelInfo;
+        uint32 numStatistics = statisticVector.getNumElements();
 
-    if (numStatistics > 0) {
-        // Find minimum and maximum among the positive gradients and negative gradients, respectively...
-        uint32 numPositive = 0;
-        uint32 numNegative = 0;
-        labelInfo.minPositive = std::numeric_limits<float64>::infinity();
-        labelInfo.maxPositive = 0;
-        labelInfo.minNegative = 0;
-        labelInfo.maxNegative = -std::numeric_limits<float64>::infinity();
+        if (numStatistics > 0) {
+            // Find minimum and maximum among the positive gradients and negative gradients, respectively...
+            uint32 numPositive = 0;
+            uint32 numNegative = 0;
+            labelInfo.minPositive = std::numeric_limits<float64>::infinity();
+            labelInfo.maxPositive = 0;
+            labelInfo.minNegative = 0;
+            labelInfo.maxNegative = -std::numeric_limits<float64>::infinity();
 
+            typename T::gradient_const_iterator iterator = statisticVector.gradients_cbegin();
+
+            for (uint32 i = 0; i < numStatistics; i++) {
+                float64 value = iterator[i];
+
+                if (value < 0) {
+                    numNegative++;
+
+                    if (value < labelInfo.minNegative) {
+                        labelInfo.minNegative = value;
+                    }
+
+                    if (value > labelInfo.maxNegative) {
+                        labelInfo.maxNegative = value;
+                    }
+                } else if (value > 0) {
+                    numPositive++;
+
+                    if (value < labelInfo.minPositive) {
+                        labelInfo.minPositive = value;
+                    }
+
+                    if (value > labelInfo.maxPositive) {
+                        labelInfo.maxPositive = value;
+                    }
+                }
+            }
+
+            labelInfo.numNegativeBins =
+                numPositive > 0 ? calculateNumBins(numPositive, binRatio_, minBins_, maxBins_) : 0;
+            labelInfo.numPositiveBins =
+                numNegative > 0 ? calculateNumBins(numNegative, binRatio_, minBins_, maxBins_) : 0;
+        } else {
+            labelInfo.numPositiveBins = 0;
+            labelInfo.numNegativeBins = 0;
+        }
+
+        return labelInfo;
+    }
+
+    template<class T>
+    void EqualWidthLabelBinning<T>::createBins(LabelInfo labelInfo, const T& statisticVector,
+                                               typename ILabelBinning<T>::Callback callback,
+                                               typename ILabelBinning<T>::ZeroCallback zeroCallback) const {
+        uint32 numPositiveBins = labelInfo.numPositiveBins;
+        float64 minPositive = labelInfo.minPositive;
+        float64 maxPositive = labelInfo.maxPositive;
+        uint32 numNegativeBins = labelInfo.numNegativeBins;
+        float64 minNegative = labelInfo.minNegative;
+        float64 maxNegative = labelInfo.minNegative;
+
+        float64 spanPerPositiveBin = minNegative < 0 ? (maxNegative - minNegative) / numPositiveBins : 0;
+        float64 spanPerNegativeBin = maxPositive > 0 ? (maxPositive - minPositive) / numNegativeBins : 0;
+
+        // Assign labels to bins...
+        uint32 numStatistics = statisticVector.getNumElements();
         typename T::gradient_const_iterator iterator = statisticVector.gradients_cbegin();
 
         for (uint32 i = 0; i < numStatistics; i++) {
             float64 value = iterator[i];
 
-            if (value < 0) {
-                numNegative++;
+            if (value > 0) {
+                // Gradient is positive, i.e., label belongs to a negative bin...
+                uint32 binIndex = std::floor((value - minPositive) / spanPerNegativeBin);
 
-                if (value < labelInfo.minNegative) {
-                    labelInfo.minNegative = value;
+                if (binIndex >= numNegativeBins) {
+                    binIndex = numNegativeBins - 1;
                 }
 
-                if (value > labelInfo.maxNegative) {
-                    labelInfo.maxNegative = value;
-                }
-            } else if (value > 0) {
-                numPositive++;
+                callback(binIndex, i, value);
+            } else if (value < 0) {
+                // Gradient is negative, i.e., label belongs to a positive bin...
+                uint32 binIndex = std::floor((value - minNegative) / spanPerPositiveBin);
 
-                if (value < labelInfo.minPositive) {
-                    labelInfo.minPositive = value;
+                if (binIndex >= numPositiveBins) {
+                    binIndex = numPositiveBins - 1;
                 }
 
-                if (value > labelInfo.maxPositive) {
-                    labelInfo.maxPositive = value;
-                }
+                callback(numNegativeBins + binIndex, i, value);
+            } else {
+                zeroCallback(i);
             }
         }
-
-        labelInfo.numNegativeBins = numPositive > 0 ? calculateNumBins(numPositive, binRatio_, minBins_, maxBins_) : 0;
-        labelInfo.numPositiveBins = numNegative > 0 ? calculateNumBins(numNegative, binRatio_, minBins_, maxBins_) : 0;
-    } else {
-        labelInfo.numPositiveBins = 0;
-        labelInfo.numNegativeBins = 0;
     }
 
-    return labelInfo;
+    template class EqualWidthLabelBinning<DenseLabelWiseStatisticVector>;
+    template class EqualWidthLabelBinning<DenseExampleWiseStatisticVector>;
+
 }
-
-template<class T>
-void EqualWidthLabelBinning<T>::createBins(LabelInfo labelInfo, const T& statisticVector,
-                                           typename ILabelBinning<T>::Callback callback,
-                                           typename ILabelBinning<T>::ZeroCallback zeroCallback) const {
-    uint32 numPositiveBins = labelInfo.numPositiveBins;
-    float64 minPositive = labelInfo.minPositive;
-    float64 maxPositive = labelInfo.maxPositive;
-    uint32 numNegativeBins = labelInfo.numNegativeBins;
-    float64 minNegative = labelInfo.minNegative;
-    float64 maxNegative = labelInfo.minNegative;
-
-    float64 spanPerPositiveBin = minNegative < 0 ? (maxNegative - minNegative) / numPositiveBins : 0;
-    float64 spanPerNegativeBin = maxPositive > 0 ? (maxPositive - minPositive) / numNegativeBins : 0;
-
-    // Assign labels to bins...
-    uint32 numStatistics = statisticVector.getNumElements();
-    typename T::gradient_const_iterator iterator = statisticVector.gradients_cbegin();
-
-    for (uint32 i = 0; i < numStatistics; i++) {
-        float64 value = iterator[i];
-
-        if (value > 0) {
-            // Gradient is positive, i.e., label belongs to a negative bin...
-            uint32 binIndex = std::floor((value - minPositive) / spanPerNegativeBin);
-
-            if (binIndex >= numNegativeBins) {
-                binIndex = numNegativeBins - 1;
-            }
-
-            callback(binIndex, i, value);
-        } else if (value < 0) {
-            // Gradient is negative, i.e., label belongs to a positive bin...
-            uint32 binIndex = std::floor((value - minNegative) / spanPerPositiveBin);
-
-            if (binIndex >= numPositiveBins) {
-                binIndex = numPositiveBins - 1;
-            }
-
-            callback(numNegativeBins + binIndex, i, value);
-        } else {
-            zeroCallback(i);
-        }
-    }
-}
-
-template class EqualWidthLabelBinning<DenseLabelWiseStatisticVector>;
-template class EqualWidthLabelBinning<DenseExampleWiseStatisticVector>;
