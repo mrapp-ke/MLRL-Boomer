0aebd5283f5a9f8a697a8327eb88c59bb1535692
Michael Rapp <michael.rapp90@googlemail.com>
2021-01-31 22:36:24 +0100

    Use namespace block in cpp file.

--- python/boomer/boosting/cpp/losses/loss_label_wise_squared_hinge.cpp
+++ python/boomer/boosting/cpp/losses/loss_label_wise_squared_hinge.cpp
@@ -1,26 +1,28 @@
 #include "loss_label_wise_squared_hinge.h"
 
-using namespace boosting;
 
+namespace boosting {
 
-void LabelWiseSquaredErrorLoss::updateGradientAndHessian(DenseVector<float64>::iterator gradient,
-                                                         DenseVector<float64>::iterator hessian, bool trueLabel,
-                                                         float64 predictedScore) const {
-    if (trueLabel) {
-        if (predictedScore < 1) {
-            *gradient = 2 * (predictedScore - 1);
-            *hessian = 2;
+    void LabelWiseSquaredErrorLoss::updateGradientAndHessian(DenseVector<float64>::iterator gradient,
+                                                             DenseVector<float64>::iterator hessian, bool trueLabel,
+                                                             float64 predictedScore) const {
+        if (trueLabel) {
+            if (predictedScore < 1) {
+                *gradient = 2 * (predictedScore - 1);
+                *hessian = 2;
+            } else {
+                *gradient = 0;
+                *hessian = 0;
+            }
         } else {
-            *gradient = 0;
-            *hessian = 0;
-        }
-    } else {
-        if (predictedScore > 0) {
-            *gradient = 2 * predictedScore;
-            *hessian = 2;
-        } else {
-            *gradient = 0;
-            *hessian = 0;
+            if (predictedScore > 0) {
+                *gradient = 2 * predictedScore;
+                *hessian = 2;
+            } else {
+                *gradient = 0;
+                *hessian = 0;
+            }
         }
     }
+
 }
