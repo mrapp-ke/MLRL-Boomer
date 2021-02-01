6ad3f45692fc975c89586407206e2775d3c6840e
Michael Rapp <michael.rapp90@googlemail.com>
2021-02-01 14:11:18 +0100

    Add class attribute "rng_" to class SequentialRuleModelInduction.

--- python/boomer/common/cpp/rule_induction/rule_model_induction_sequential.cpp
+++ python/boomer/common/cpp/rule_induction/rule_model_induction_sequential.cpp
@@ -10,7 +10,7 @@ SequentialRuleModelInduction::SequentialRuleModelInduction(
         std::shared_ptr<IInstanceSubSampling> instanceSubSamplingPtr,
         std::shared_ptr<IFeatureSubSampling> featureSubSamplingPtr, std::shared_ptr<IPruning> pruningPtr,
         std::shared_ptr<IPostProcessor> postProcessorPtr, uint32 minCoverage, intp maxConditions,
-        intp maxHeadRefinements, uint32 numThreads,
+        intp maxHeadRefinements, uint32 numThreads, uint32 randomState,
         std::unique_ptr<std::forward_list<IStoppingCriterion>> stoppingCriteriaPtr)
     : statisticsProviderFactoryPtr_(statisticsProviderFactoryPtr), ruleInductionPtr_(ruleInductionPtr),
       defaultRuleHeadRefinementFactoryPtr_(defaultRuleHeadRefinementFactoryPtr),
@@ -18,7 +18,7 @@ SequentialRuleModelInduction::SequentialRuleModelInduction(
       instanceSubSamplingPtr_(instanceSubSamplingPtr), featureSubSamplingPtr_(featureSubSamplingPtr),
       pruningPtr_(pruningPtr), postProcessorPtr_(postProcessorPtr), minCoverage_(minCoverage),
       maxConditions_(maxConditions), maxHeadRefinements_(maxHeadRefinements), numThreads_(numThreads),
-      stoppingCriteriaPtr_(std::move(stoppingCriteriaPtr)) {
+      stoppingCriteriaPtr_(std::move(stoppingCriteriaPtr)), rng_(RNG(randomState)) {
 
 }
 
