11d2fba5b030ceeca801185e96989cb7cdcf3302
Michael Rapp <michael.rapp90@googlemail.com>
2021-02-02 17:29:01 +0100

    Move class DenseNumericMatrix to "common" package.

--- python/boomer/boosting/cpp/losses/loss_label_wise.h
+++ python/boomer/boosting/cpp/losses/loss_label_wise.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #include "../../../common/cpp/input/label_matrix.h"
-#include "../data/matrix_dense_numeric.h"
+#include "../../../common/cpp/data/matrix_dense_numeric.h"
 #include "../data/matrix_dense_label_wise.h"
 
 
