2f9dfc5c9e092e791b7b941d1ef4319d39a9cca4
Michael Rapp <michael.rapp90@googlemail.com>
2021-02-03 19:37:21 +0100

    Remove debug statements.

--- python/boomer/boosting/cpp/output/predictor_classification_example_wise.cpp
+++ python/boomer/boosting/cpp/output/predictor_classification_example_wise.cpp
@@ -2,94 +2,31 @@
 #include "predictor_common.h"
 #include "../../../common/cpp/data/matrix_dense.h"
 #include <algorithm>
-#include <iostream>
 
 
 namespace boosting {
 
     template<class T>
-    static inline void printKnownLabelVectors(const T& labelVectors, uint32 numLabels) {
-        std::cout << "Known label vectors\n";
-        std::cout << "===================\n";
-
-        uint32 no = 0;
-
-        for (auto it = labelVectors.cbegin(); it != labelVectors.cend(); it++) {
-            std::cout << no << ": ";
-            const auto& entry = *it;
-            const std::unique_ptr<LabelVector>& labelVectorPtr = entry.first;
-            uint32 count = entry.second;
-            auto indexIterator = labelVectorPtr->indices_cbegin();
-            auto indicesEnd = labelVectorPtr->indices_cend();
-
-            for (uint32 i = 0; i < numLabels; i++) {
-                bool label;
-
-                if (indexIterator != indicesEnd && *indexIterator == i) {
-                    indexIterator++;
-                    label = true;
-                } else {
-                    label = false;
-                }
-
-                std::cout << (label ? "1" : "0") << " ";
-            }
-            std::cout << " (occurred " << count << " times)\n";
-            no++;
-        }
-
-        std::cout << "\n";
-    }
-
-    template<class T>
     static inline void predictClosestLabelVector(uint32 exampleIndex, const DenseMatrix<float64>& scoreMatrix,
                                                  CContiguousView<uint8>& predictionMatrix, const IMeasure& measure,
                                                  const T& labelVectors) {
-        std::cout << "\n====================================================================\n";
-        std::cout << "Score vector\n";
-        std::cout << "------------\n";
-
-        for (auto it = scoreMatrix.row_cbegin(exampleIndex); it != scoreMatrix.row_cend(exampleIndex); it++) {
-            float64 score = *it;
-            std::cout << score << " ";
-        }
-
-        std::cout << "\n\n";
-
-
         std::fill(predictionMatrix.row_begin(exampleIndex), predictionMatrix.row_end(exampleIndex), 0);
         const LabelVector* closestLabelVector = nullptr;
         float64 bestScore = 0;
         uint32 bestCount = 0;
 
-
-        std::cout << "Comparisons\n";
-        std::cout << "-----------\n";
-
-        uint32 no = 0;
-
         for (auto it = labelVectors.cbegin(); it != labelVectors.cend(); it++) {
-            std::cout << no << ": ";
             const auto& entry = *it;
             const std::unique_ptr<LabelVector>& labelVectorPtr = entry.first;
             uint32 count = entry.second;
             float64 score = measure.evaluate(exampleIndex, *labelVectorPtr, scoreMatrix);
-            std::cout << "score = " << score;
 
             if (closestLabelVector == nullptr || score < bestScore || (score == bestScore && count > bestCount)) {
                 closestLabelVector = labelVectorPtr.get();
                 bestScore = score;
                 bestCount = count;
-                std::cout << " --> best seen so far";
             }
-
-            std::cout << "\n";
-            no++;
         }
-
-        std::cout << "\n";
-        std::cout << "Prediction\n";
-        std::cout << "----------\n";
 
         if (closestLabelVector != nullptr) {
             CContiguousView<uint8>::iterator iterator = predictionMatrix.row_begin(exampleIndex);
@@ -98,14 +35,6 @@ namespace boosting {
                 uint32 labelIndex = *it;
                 iterator[labelIndex] = 1;
             }
-
-            for (auto it = predictionMatrix.row_cbegin(exampleIndex); it != predictionMatrix.row_cend(exampleIndex); it++) {
-                std::cout << *it << " ";
-            }
-
-            std::cout << "\n";
-        } else {
-            std::cout << "no prediction possible!?\n";
         }
     }
 
@@ -168,10 +97,8 @@ namespace boosting {
     void ExampleWiseClassificationPredictor::predict(const CContiguousFeatureMatrix& featureMatrix,
                                                      CContiguousView<uint8>& predictionMatrix,
                                                      const RuleModel& model) const {
-                                                     uint32 numExamples = predictionMatrix.getNumRows();
-        std::cout << "Predicting based on CContiguousFeatureMatrix...\n";
+        uint32 numExamples = predictionMatrix.getNumRows();
         uint32 numLabels = predictionMatrix.getNumCols();
-        printKnownLabelVectors(labelVectors_, numLabels);
         DenseMatrix<float64> scoreMatrix(numExamples, numLabels);
         predictInternally<LabelVectorSet>(model, featureMatrix, scoreMatrix, predictionMatrix, *measurePtr_,
                                           labelVectors_);
@@ -180,10 +107,8 @@ namespace boosting {
     void ExampleWiseClassificationPredictor::predict(const CsrFeatureMatrix& featureMatrix,
                                                      CContiguousView<uint8>& predictionMatrix,
                                                      const RuleModel& model) const {
-        std::cout << "Predicting based on CsrFeatureMatrix...\n";
         uint32 numExamples = predictionMatrix.getNumRows();
         uint32 numLabels = predictionMatrix.getNumCols();
-        printKnownLabelVectors(labelVectors_, numLabels);
         DenseMatrix<float64> scoreMatrix(numExamples, numLabels);
         predictInternally<LabelVectorSet>(model, featureMatrix, scoreMatrix, predictionMatrix, *measurePtr_,
                                           labelVectors_);
