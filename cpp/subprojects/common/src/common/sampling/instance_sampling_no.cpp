ee9798306d503e622bee1f21213d0f31fcfea8d3
Michael Rapp <michael.rapp90@googlemail.com>
2021-02-08 18:10:03 +0100

    Implement new functions of the class NoInstanceSubSampling.

--- cpp/subprojects/common/src/common/sampling/instance_sampling_no.cpp
+++ cpp/subprojects/common/src/common/sampling/instance_sampling_no.cpp
@@ -1,5 +1,6 @@
 #include "common/sampling/instance_sampling_no.hpp"
 #include "common/sampling/weight_vector_equal.hpp"
+#include "common/sampling/weight_vector_dense.hpp"
 
 
 std::unique_ptr<IWeightVector> NoInstanceSubSampling::subSample(uint32 numExamples, RNG& rng) const {
@@ -8,10 +9,22 @@ std::unique_ptr<IWeightVector> NoInstanceSubSampling::subSample(uint32 numExampl
 
 std::unique_ptr<IWeightVector> NoInstanceSubSampling::subSample(std::unique_ptr<SinglePartition> partitionPtr,
                                                                 RNG& rng) const {
-    // TODO
+    return std::make_unique<EqualWeightVector>(partitionPtr->getNumElements());
 }
 
 std::unique_ptr<IWeightVector> NoInstanceSubSampling::subSample(std::unique_ptr<BiPartition> partitionPtr,
                                                                 RNG& rng) const {
-    // TODO
+    uint32 numExamples = partitionPtr->getNumElements();
+    uint32 numTrainingExamples = partitionPtr->getNumFirst();
+    BiPartition::const_iterator indexIterator = partitionPtr->first_cbegin();
+    std::unique_ptr<DenseWeightVector> weightVectorPtr = std::make_unique<DenseWeightVector>(numExamples,
+                                                                                             numTrainingExamples);
+    DenseWeightVector::iterator weightIterator = weightVectorPtr->begin();
+
+    for (uint32 i = 0; i < numTrainingExamples; i++) {
+        uint32 index = indexIterator[i];
+        weightIterator[index] = 1;
+    }
+
+    return std::move(weightVectorPtr);
 }
