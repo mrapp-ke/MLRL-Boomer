8359a1261975a3c76921f0b6e76163d765cf1c13
Michael Rapp <michael.rapp90@googlemail.com>
2021-02-08 14:44:39 +0100

    Add class SinglePartition.

---
+++ cpp/subprojects/common/src/common/sampling/partition_single.cpp
@@ -0,0 +1,19 @@
+#include "common/sampling/partition_single.hpp"
+
+
+SinglePartition::SinglePartition(uint32 numElements)
+    : numElements_(numElements) {
+
+}
+
+SinglePartition::const_iterator SinglePartition::cbegin() const {
+    return IndexIterator(0);
+}
+
+SinglePartition::const_iterator SinglePartition::cend() const {
+    return IndexIterator(numElements_);
+}
+
+uint32 SinglePartition::getNumElements() const {
+    return numElements_;
+}
