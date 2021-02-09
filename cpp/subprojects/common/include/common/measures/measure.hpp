930e53451bfa78cdde07bd2e86d707d9b22217a5
Michael Rapp <michael.rapp90@googlemail.com>
2021-02-09 16:25:24 +0100

    Add function "evaluate" to class IMeasure.

--- cpp/subprojects/common/include/common/measures/measure.hpp
+++ cpp/subprojects/common/include/common/measures/measure.hpp
@@ -5,6 +5,7 @@
 
 #include "common/data/view_c_contiguous.hpp"
 #include "common/input/label_vector.hpp"
+#include "common/input/label_matrix.hpp"
 
 
 /**
@@ -16,6 +17,20 @@ class IMeasure {
     public:
 
         virtual ~IMeasure() { };
+
+        /**
+         * Calculates and returns a numerical score that assesses the quality of predictions for the example at a
+         * specific index by comparing them to the corresponding ground truth labels.
+         *
+         * @param exampleIndex  The index of the example for which the predictions should be evaluated
+         * @param labelMatrix   A reference to an object of type `IRandomAccessMatrix` that provides random access to
+         *                      the labels of the training examples
+         * @param scoreMatrix   A reference to an object of type `CContiguousView` that stores the currently predicted
+         *                      scores
+         * @return              The numerical score that has been calculated
+         */
+        virtual float64 evaluate(uint32 exampleIndex, const IRandomAccessLabelMatrix& labelMatrix,
+                                 const CContiguousView<float64>& scoreMatrix) const = 0;
 
         /**
          * Calculates and returns a numerical score that assesses the quality of predictions for a single example by
