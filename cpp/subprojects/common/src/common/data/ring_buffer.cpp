e728b4ac2d51d5c163705fc3766b0ad69288f49d
Michael Rapp <michael.rapp90@googlemail.com>
2021-02-10 16:04:20 +0100

    Add class RingBuffer.

---
+++ cpp/subprojects/common/src/common/data/ring_buffer.cpp
@@ -0,0 +1,41 @@
+#include "common/data/ring_buffer.hpp"
+
+
+template<class T>
+RingBuffer<T>::RingBuffer(uint32 capacity)
+    : array_(new T[capacity]), capacity_(capacity), pos_(0), full_(false) {
+
+}
+
+template<class T>
+RingBuffer<T>::~RingBuffer() {
+    delete[] array_;
+}
+
+template<class T>
+typename RingBuffer<T>::const_iterator RingBuffer<T>::cbegin() const {
+    return array_;
+}
+
+template<class T>
+typename RingBuffer<T>::const_iterator RingBuffer<T>::cend() const {
+    return &array_[full_ ? capacity_ : pos_];
+}
+
+template<class T>
+uint32 RingBuffer<T>::getNumElements() const {
+    return full_ ? capacity_ : pos_;
+}
+
+template<class T>
+void RingBuffer<T>::push(T value) {
+    array_[pos_] = value;
+    pos_++;
+
+    if (pos_ >= capacity_) {
+        pos_ = 0;
+        full_ = true;
+    }
+}
+
+template class RingBuffer<float64>;
