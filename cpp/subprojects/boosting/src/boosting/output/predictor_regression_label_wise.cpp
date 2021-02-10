51fa2de971b21d3fe9e2b9687c9ab3278fd1caf7
Michael Rapp <michael.rapp90@googlemail.com>
2021-02-10 12:31:56 +0100

    Add "predict" functions that take a single rules as an argument to the class LabelWiseRegressionPredictor.

--- cpp/subprojects/boosting/src/boosting/output/predictor_regression_label_wise.cpp
+++ cpp/subprojects/boosting/src/boosting/output/predictor_regression_label_wise.cpp
@@ -11,6 +11,40 @@ namespace boosting {
     }
 
     void LabelWiseRegressionPredictor::predict(const CContiguousFeatureMatrix& featureMatrix,
+                                               CContiguousView<float64>& predictionMatrix, const Rule& rule) const {
+        uint32 numExamples = featureMatrix.getNumRows();
+        const CContiguousFeatureMatrix* featureMatrixPtr = &featureMatrix;
+        CContiguousView<float64>* predictionMatrixPtr = &predictionMatrix;
+        const Rule* rulePtr = &rule;
+
+        #pragma omp parallel for firstprivate(numExamples) firstprivate(rulePtr) firstprivate(featureMatrixPtr) \
+        firstprivate(predictionMatrixPtr) schedule(dynamic) num_threads(numThreads_)
+        for (uint32 i = 0; i < numExamples; i++) {
+            applyRule(*rulePtr, featureMatrixPtr->row_cbegin(i), featureMatrixPtr->row_cend(i),
+                      predictionMatrixPtr->row_begin(i));
+        }
+    }
+
+    void LabelWiseRegressionPredictor::predict(const CsrFeatureMatrix& featureMatrix,
+                                               CContiguousView<float64>& predictionMatrix, const Rule& rule) const {
+        uint32 numExamples = featureMatrix.getNumRows();
+        uint32 numFeatures = featureMatrix.getNumCols();
+        const CsrFeatureMatrix* featureMatrixPtr = &featureMatrix;
+        CContiguousView<float64>* predictionMatrixPtr = &predictionMatrix;
+        const Rule* rulePtr = &rule;
+
+        #pragma omp parallel for firstprivate(numExamples) firstprivate(rulePtr) firstprivate(featureMatrixPtr) \
+        firstprivate(predictionMatrixPtr) schedule(dynamic) num_threads(numThreads_)
+        for (uint32 i = 0; i < numExamples; i++) {
+            float32 tmpArray1[numFeatures];
+            uint32 tmpArray2[numFeatures] = {};
+            applyRuleCsr(*rulePtr, featureMatrixPtr->row_indices_cbegin(i), featureMatrixPtr->row_indices_cend(i),
+                         featureMatrixPtr->row_values_cbegin(i), featureMatrixPtr->row_values_cend(i),
+                         predictionMatrixPtr->row_begin(i), &tmpArray1[0], &tmpArray2[0], 1);
+        }
+    }
+
+    void LabelWiseRegressionPredictor::predict(const CContiguousFeatureMatrix& featureMatrix,
                                                CContiguousView<float64>& predictionMatrix,
                                                const RuleModel& model) const {
         uint32 numExamples = featureMatrix.getNumRows();
