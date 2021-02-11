ee513d806a7fb37753618b0138a67b20be2bdf88
Michael Rapp <michael.rapp90@googlemail.com>
2021-02-11 12:03:42 +0100

    Implement simple stopping criterion.

--- cpp/subprojects/common/src/common/stopping/stopping_criterion_measure.cpp
+++ cpp/subprojects/common/src/common/stopping/stopping_criterion_measure.cpp
@@ -28,19 +28,33 @@ MeasureStoppingCriterion::MeasureStoppingCriterion(std::shared_ptr<IEvaluationMe
 
 bool MeasureStoppingCriterion::shouldContinue(const IPartition& partition, const IStatistics& statistics,
                                               uint32 numRules) {
+    bool result = true;
+
     if (numRules % updateInterval_ == 0) {
         const BiPartition& biPartition = static_cast<const BiPartition&>(partition);
         float64 score = evaluateOnHoldoutSet(biPartition, statistics, *measurePtr_);
+        uint32 numElements = buffer_.getNumElements();
 
-        if (numRules % stopInterval_ == 0) {
-            std::cout << score << "\n";
+        if (numElements > 0 && numRules % stopInterval_ == 0) {
+            RingBuffer<float64>::const_iterator iterator = buffer_.cbegin();
+            float64 max = iterator[0];
+
+            for (uint32 i = 1; i < numElements; i++) {
+                float64 value = iterator[i];
+
+                if (value > max) {
+                    max = value;
+                }
+            }
+
             // TODO Apply aggregation function
             // TODO Apply decision function
-            return true;
+            result = (score + 0.0001) < max;
+            std::cout << numRules << ": (" << score << " + 0.0001) < " << max << " = " << result << "\n";
         }
 
         buffer_.push(score);
     }
 
-    return true;
+    return result;
 }
