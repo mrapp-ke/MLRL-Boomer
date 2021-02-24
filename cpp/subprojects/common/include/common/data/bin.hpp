3b9e3c62b20f656599dde461bdc43be154d73e22
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2021-02-24 22:33:42 +0100

    Add field "numExamples" to struct Bin.

--- cpp/subprojects/common/include/common/data/bin.hpp
+++ cpp/subprojects/common/include/common/data/bin.hpp
@@ -11,8 +11,10 @@
  *  A struct that stores information about the examples that are contained by a bin.
  */
 struct Bin {
-    Bin() : minValue(std::numeric_limits<float32>::infinity()), maxValue(-std::numeric_limits<float32>::infinity()) { };
+    Bin() : numExamples(0), minValue(std::numeric_limits<float32>::infinity()),
+            maxValue(-std::numeric_limits<float32>::infinity()) { };
     uint32 index;
+    uint32 numExamples;
     float32 minValue;
     float32 maxValue;
 };
