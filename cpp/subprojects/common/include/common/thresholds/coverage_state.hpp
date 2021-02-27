a1a79190ac7cf31f95a0f0d71e6967c45c4bbcc5
Michael Rapp <michael.rapp90@googlemail.com>
2021-02-28 00:45:19 +0100

    Add function "copy" to class ICoverageState.

--- cpp/subprojects/common/include/common/thresholds/coverage_state.hpp
+++ cpp/subprojects/common/include/common/thresholds/coverage_state.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 
 /**
  * Defines an interface for all classes that allow to keep track of the examples that are covered by a rule.
@@ -9,5 +11,12 @@ class ICoverageState {
     public:
 
         virtual ~ICoverageState() { };
+
+        /**
+         * Creates and returns a deep copy of the coverage state.
+         *
+         * @return An unique pointer to an object of type `ICoverageState` that has been created
+         */
+        virtual std::unique_ptr<ICoverageState> copy() const = 0;
 
 };
