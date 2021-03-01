5d3e920534a8c7c8a041b90dac2104bcf8933e6e
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2021-03-01 14:08:42 +0100

    Remove field "numExamples" from struct Bin.

--- cpp/subprojects/common/include/common/data/bin.hpp
+++ cpp/subprojects/common/include/common/data/bin.hpp
@@ -11,9 +11,7 @@
  *  A struct that stores information about the examples that are contained by a bin.
  */
 struct Bin {
-    Bin() : numExamples(0), minValue(std::numeric_limits<float32>::infinity()),
-            maxValue(-std::numeric_limits<float32>::infinity()) { };
-    uint32 numExamples; // TODO Currently this is only used to check if a bin is empty. Can be removed and the check can be based on minValue or maxValue instead.
+    Bin() : minValue(std::numeric_limits<float32>::infinity()), maxValue(-std::numeric_limits<float32>::infinity()) { };
     float32 minValue;
     float32 maxValue;
 };
