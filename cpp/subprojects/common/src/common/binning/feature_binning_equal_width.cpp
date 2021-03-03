aca047848292f5b00e7fbdc604fe8c77c8dd56ed
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2021-03-03 16:56:20 +0100

    Rename local variable.

--- cpp/subprojects/common/src/common/binning/feature_binning_equal_width.cpp
+++ cpp/subprojects/common/src/common/binning/feature_binning_equal_width.cpp
@@ -50,15 +50,15 @@ EqualWidthFeatureBinning::EqualWidthFeatureBinning(float32 binRatio, uint32 minB
 
 IFeatureBinning::Result EqualWidthFeatureBinning::createBins(FeatureVector& featureVector) const {
     Result result;
-    std::tuple<uint32, float32, float32> info = preprocess(featureVector, binRatio_, minBins_, maxBins_);
-    uint32 numBins = std::get<0>(info);
+    std::tuple<uint32, float32, float32> tuple = preprocess(featureVector, binRatio_, minBins_, maxBins_);
+    uint32 numBins = std::get<0>(tuple);
     result.thresholdVectorPtr = std::make_unique<ThresholdVector>(numBins, true);
     uint32 numElements = featureVector.getNumElements();
     result.binIndicesPtr = std::make_unique<BinIndexVector>(numElements);
 
     if (numBins > 0) {
-        float32 min = std::get<1>(info);
-        float32 max = std::get<2>(info);
+        float32 min = std::get<1>(tuple);
+        float32 max = std::get<2>(tuple);
         float32 width = (max - min) / numBins;
         FeatureVector::const_iterator featureIterator = featureVector.cbegin();
         BinIndexVector::iterator binIndexIterator = result.binIndicesPtr->begin();
