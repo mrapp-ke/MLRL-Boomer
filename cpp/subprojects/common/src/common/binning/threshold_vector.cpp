b424d022fd426c2be8f297b70f59bfcebe0c2846
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2021-03-04 00:44:59 +0100

    The class ThresholdVector now inherits from MissingFeatureVector.

--- cpp/subprojects/common/src/common/binning/threshold_vector.cpp
+++ cpp/subprojects/common/src/common/binning/threshold_vector.cpp
@@ -7,6 +7,30 @@ ThresholdVector::ThresholdVector(uint32 numElements)
 }
 
 ThresholdVector::ThresholdVector(uint32 numElements, bool init)
-    : DenseVector<float32>(numElements, init) {
+    : vector_(DenseVector<float32>(numElements, init)) {
 
+}
+
+ThresholdVector::iterator ThresholdVector::begin() {
+    return vector_.begin();
+}
+
+ThresholdVector::iterator ThresholdVector::end() {
+    return vector_.end();
+}
+
+ThresholdVector::const_iterator ThresholdVector::cbegin() const {
+    return vector_.cbegin();
+}
+
+ThresholdVector::const_iterator ThresholdVector::cend() const {
+    return vector_.cend();
+}
+
+uint32 ThresholdVector::getNumElements() const {
+    return vector_.getNumElements();
+}
+
+void ThresholdVector::setNumElements(uint32 numElements, bool freeMemory) {
+    return vector_.setNumElements(numElements, freeMemory);
 }
