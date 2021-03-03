cd4c18ed763375779ddc1599c62d32fbe2fbc3bc
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2021-03-03 02:02:16 +0100

    Add class ThresholdVector.

---
+++ cpp/subprojects/common/include/common/binning/threshold_vector.hpp
@@ -0,0 +1,21 @@
+/**
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ */
+#pragma once
+
+#include "common/data/vector_dense.hpp"
+
+
+/**
+ * An one-dimensional vector that stores thresholds that may be used by conditions.
+ */
+class ThresholdVector final : public DenseVector<float32> {
+
+    public:
+
+        /**
+         * @param numElements The number of elements in the vector
+         */
+        ThresholdVector(uint32 numElements);
+
+};
