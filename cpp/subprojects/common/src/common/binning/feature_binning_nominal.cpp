1b180b4cba96ece29a432bc31fb1709644d9a825
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2021-03-04 15:50:15 +0100

    Use a DokBinIndexVector when dealing with sparse feature values.

--- cpp/subprojects/common/src/common/binning/feature_binning_nominal.cpp
+++ cpp/subprojects/common/src/common/binning/feature_binning_nominal.cpp
@@ -1,13 +1,20 @@
 #include "common/binning/feature_binning_nominal.hpp"
 #include "common/binning/bin_index_vector_dense.hpp"
+#include "common/binning/bin_index_vector_dok.hpp"
 #include <unordered_map>
 
 
 IFeatureBinning::Result NominalFeatureBinning::createBins(FeatureVector& featureVector, uint32 numExamples) const {
     Result result;
     uint32 numElements = featureVector.getNumElements();
-    result.binIndicesPtr = std::make_unique<DenseBinIndexVector>(numExamples);
     result.thresholdVectorPtr = std::make_unique<ThresholdVector>(featureVector, numElements);
+    bool sparse = numElements < numExamples;
+
+    if (sparse) {
+        result.binIndicesPtr = std::make_unique<DokBinIndexVector>();
+    } else {
+        result.binIndicesPtr = std::make_unique<DenseBinIndexVector>(numExamples);
+    }
 
     if (numElements > 0) {
         IBinIndexVector& binIndices = *result.binIndicesPtr;
@@ -20,7 +27,7 @@ IFeatureBinning::Result NominalFeatureBinning::createBins(FeatureVector& feature
         for (uint32 i = 0; i < numElements; i++) {
             float32 currentValue = featureIterator[i].value;
 
-            if (currentValue != 0) {
+            if (!sparse || currentValue != 0) {
                 uint32 index = featureIterator[i].index;
                 auto mapIterator = mapping.emplace(currentValue, nextBinIndex);
 
