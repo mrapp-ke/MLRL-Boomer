0d74572c4202aaac0b01d20add132d02038d01e1
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2021-03-04 17:02:51 +0100

    Remove unused functions from class DenseBinIndexVector.

--- cpp/subprojects/common/include/common/binning/bin_index_vector_dense.hpp
+++ cpp/subprojects/common/include/common/binning/bin_index_vector_dense.hpp
@@ -23,59 +23,6 @@ class DenseBinIndexVector final : public IBinIndexVector {
          */
         DenseBinIndexVector(uint32 numElements);
 
-        /**
-         * @param numElements   The number of elements in the vector
-         * @param init          True, if all elements in the vector should be value-initialized, false otherwise
-         */
-        DenseBinIndexVector(uint32 numElements, bool init);
-
-        typedef DenseVector<uint32>::iterator iterator;
-
-        typedef DenseVector<uint32>::const_iterator const_iterator;
-
-        /**
-         * Returns an `iterator` to the beginning of the indices.
-         *
-         * @return An `iterator` to the beginning
-         */
-        iterator begin();
-
-        /**
-         * Returns an `iterator` to the end of the indices.
-         *
-         * @return An `iterator` to the end
-         */
-        iterator end();
-
-        /**
-         * Returns a `const_iterator` to the beginning of the indices.
-         *
-         * @return A `const_iterator` to the beginning
-         */
-        const_iterator cbegin() const;
-
-        /**
-         * Returns a `const_iterator` to the end of the indices.
-         *
-         * @return A `const_iterator` to the end
-         */
-        const_iterator cend() const;
-
-        /**
-         * Returns the number of elements in the vector.
-         *
-         * @return The number of elements in the vector
-         */
-        uint32 getNumElements() const;
-
-        /**
-         * Sets the number of elements in the vector.
-         *
-         * @param numElements   The number of elements to be set
-         * @param freeMemory    True, if unused memory should be freed, if possible, false otherwise
-         */
-        void setNumElements(uint32 numElements, bool freeMemory);
-
         uint32 getBinIndex(uint32 exampleIndex) const override;
 
         void setBinIndex(uint32 exampleIndex, uint32 binIndex) override;
