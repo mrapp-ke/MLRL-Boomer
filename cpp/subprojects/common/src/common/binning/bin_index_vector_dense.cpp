f3926b68e2443ad76a0f4394842f4e28968aae6f
mrapp-ke <mrapp@ke.tu-darmstadt.de>
2021-03-04 13:54:20 +0100

    Add additional functions to class DenseBinIndexVector.

--- cpp/subprojects/common/src/common/binning/bin_index_vector_dense.cpp
+++ cpp/subprojects/common/src/common/binning/bin_index_vector_dense.cpp
@@ -2,8 +2,37 @@
 
 
 DenseBinIndexVector::DenseBinIndexVector(uint32 numElements)
-    : vector_(DenseVector<uint32>(numElements)) {
+    : DenseBinIndexVector(numElements, false) {
 
+}
+
+DenseBinIndexVector::DenseBinIndexVector(uint32 numElements, bool init)
+    : vector_(DenseVector<uint32>(numElements, init)) {
+
+}
+
+DenseBinIndexVector::iterator DenseBinIndexVector::begin() {
+    return vector_.begin();
+}
+
+DenseBinIndexVector::iterator DenseBinIndexVector::end() {
+    return vector_.end();
+}
+
+DenseBinIndexVector::const_iterator DenseBinIndexVector::cbegin() const {
+    return vector_.cbegin();
+}
+
+DenseBinIndexVector::const_iterator DenseBinIndexVector::cend() const {
+    return vector_.cend();
+}
+
+uint32 DenseBinIndexVector::getNumElements() const {
+    return vector_.getNumElements();
+}
+
+void DenseBinIndexVector::setNumElements(uint32 numElements, bool freeMemory) {
+    vector_.setNumElements(numElements, freeMemory);
 }
 
 uint32 DenseBinIndexVector::getBinIndex(uint32 exampleIndex) const {
