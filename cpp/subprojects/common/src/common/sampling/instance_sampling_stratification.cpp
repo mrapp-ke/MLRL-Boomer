ff205eb2d95c3455e5370e47710e6c2dc559021d
Anna-inf <anna_kulischkin@web.de>
2021-03-29 13:34:39 +0200

    erweitert um labelset-based stratification

--- cpp/subprojects/common/src/common/sampling/instance_sampling_stratification.cpp
+++ cpp/subprojects/common/src/common/sampling/instance_sampling_stratification.cpp
@@ -7,16 +7,53 @@
 #include <list>
 #include <algorithm>
 
-uint32 getCombinationWithFewestExamples(std::map<uint32, std::list<uint32>>& samplesPerLabel){
-    int currentChoose = -1;
+std::map<std::vector<uint32>, std::list<uint32>> findExamplesPerLabel(const IRandomAccessLabelMatrix& labelMatrixPtr, bool strategy ){
+    uint32 numExamples_ = labelMatrixPtr.getNumRows();
+    uint32 numLabels_ = labelMatrixPtr.getNumCols();
+
+    std::map<std::vector<uint32>, std::list<uint32>> examplesPerLabel_;
+    if(strategy){
+        for(uint32 exampleIndex=0; exampleIndex<numExamples_; exampleIndex++){
+            std::vector<uint32> labelSet;
+            for(uint32 labelIndex=0; labelIndex<numLabels_; labelIndex++){
+                labelSet.push_back(labelMatrixPtr.getValue(exampleIndex, labelIndex));
+            }
+            if(examplesPerLabel_.find({labelSet}) != examplesPerLabel_.end()){
+                examplesPerLabel_[{labelSet}].push_back(exampleIndex);
+            }else{
+                examplesPerLabel_.insert({{labelSet}, {exampleIndex}});
+            }
+        }
+    }else{
+        for(uint32 exampleIndex=0; exampleIndex<numExamples_; exampleIndex++){
+            for(uint32 labelIndex=0; labelIndex<numLabels_; labelIndex++){
+                uint8 label = labelMatrixPtr.getValue(exampleIndex, labelIndex);
+                if (label == 0){
+                    continue;
+                }else{
+                    if(examplesPerLabel_.find({labelIndex}) != examplesPerLabel_.end()){
+                        examplesPerLabel_[{labelIndex}].push_back(exampleIndex);
+                    }else{
+                        examplesPerLabel_.insert({{labelIndex}, {exampleIndex}});
+                    }
+                }
+            }
+        }
+    }
+
+    return examplesPerLabel_;
+}
+
+std::vector<uint32> getCombinationWithFewestExamples(std::map<std::vector<uint32>, std::list<uint32>>& samplesPerLabel){
+    std::vector<uint32> currentChoose = {};
     uint32 minValue = 0;
-    std::map<uint32, std::list<uint32>>::iterator it;
+    std::map<std::vector<uint32>, std::list<uint32>>::iterator it;
     for (it = samplesPerLabel.begin(); it != samplesPerLabel.end(); it++){
         uint32 size = it->second.size();
         if (size == 0){
             continue;
         }
-        if (currentChoose == -1 || (size != 0 && size < minValue)){
+        if (currentChoose.empty() || (size != 0 && size < minValue)){
             currentChoose = it->first;
             minValue = size;
         }
@@ -54,34 +91,17 @@ uint32 getSubsetWithMaxDesiredSamples(std::vector<float32>& desiredSamplesPerLab
 
 std::unique_ptr<IWeightVector> subSample_(const IRandomAccessLabelMatrix& labelMatrixPtr, uint32 sampleSize_, RNG& rng){
    uint32 numExamples = labelMatrixPtr.getNumRows();
-   uint32 numLabels = labelMatrixPtr.getNumCols();
-
 
    // calculate the desired number of examples at each subset
    uint32 numSamples = (uint32) (numExamples * sampleSize_);
    std::vector<uint32> desiredSamplesPerSet = {numSamples, numExamples - numSamples};
 
    // Find the examples of each label in the initial set
-   std::map<uint32, std::list<uint32>> examplesPerLabel;
-
-    for(uint32 exampleIndex=0; exampleIndex<numExamples; exampleIndex++){
-        for(uint32 labelIndex=0; labelIndex<numLabels; labelIndex++){
-            uint8 label = labelMatrixPtr.getValue(exampleIndex, labelIndex);
-            if (label == 0){
-                continue;
-            }else{
-                if(examplesPerLabel.find(labelIndex) != examplesPerLabel.end()){
-                    examplesPerLabel[labelIndex].push_back(exampleIndex);
-                }else{
-                    examplesPerLabel.insert({labelIndex, {exampleIndex}});
-                }
-            }
-        }
-    }
+   std::map<std::vector<uint32>, std::list<uint32>> examplesPerLabel = findExamplesPerLabel(labelMatrixPtr, false);
 
    // calculate the desired number of examples of each label at each subset
-   std::map<uint32, std::vector<float32>> desiredSamplesPerLabelPerSet;
-   std::map<uint32, std::list<uint32>>::iterator it;
+   std::map<std::vector<uint32>, std::vector<float32>> desiredSamplesPerLabelPerSet;
+   std::map<std::vector<uint32>, std::list<uint32>>::iterator it;
 
    for (it = examplesPerLabel.begin(); it != examplesPerLabel.end(); it++){
        std::vector<float32> desiredSamples = {(float32)(sampleSize_ * it->second.size()), (float32)((1-sampleSize_) * it->second.size())};
@@ -97,8 +117,8 @@ std::unique_ptr<IWeightVector> subSample_(const IRandomAccessLabelMatrix& labelM
    std::unique_ptr<DenseWeightVector> weightVectorPtr = std::make_unique<DenseWeightVector>(numExamples, numSamples);
    DenseWeightVector::iterator weightIterator = weightVectorPtr->begin();
 
-   int l = getCombinationWithFewestExamples(examplesPerLabel);
-   while (l != -1){
+   std::vector<uint32> l = getCombinationWithFewestExamples(examplesPerLabel);
+   while (!l.empty()){
        while(examplesPerLabel[l].size() > 0){
            uint32 randomIndex = rng.random(0, examplesPerLabel[l].size());
            auto indexIter = examplesPerLabel[l].begin();
@@ -111,7 +131,7 @@ std::unique_ptr<IWeightVector> subSample_(const IRandomAccessLabelMatrix& labelM
                weightIterator[currentSample] += 1;
             }
 
-           std::map<uint32, std::list<uint32>>::iterator labelIter;
+           std::map<std::vector<uint32>, std::list<uint32>>::iterator labelIter;
            for (labelIter=examplesPerLabel.begin(); labelIter!=examplesPerLabel.end();labelIter++){
                if((std::find(labelIter->second.begin(), labelIter->second.end(), currentSample) != labelIter->second.end())){
                   desiredSamplesPerLabelPerSet[labelIter->first][m] -= 1;
