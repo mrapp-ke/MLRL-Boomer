54ef9c42cd33f3f49bffeccde8b51b7dc8420eac
Michael Rapp <michael.rapp90@googlemail.com>
2021-03-31 02:55:04 +0200

    Add template argument to class DenseWeightVector.

--- cpp/subprojects/common/src/common/sampling/instance_sampling_bagging.cpp
+++ cpp/subprojects/common/src/common/sampling/instance_sampling_bagging.cpp
@@ -12,8 +12,9 @@ Bagging::Bagging(float32 sampleSize)
 std::unique_ptr<IWeightVector> Bagging::subSample(const SinglePartition& partition, RNG& rng) const {
     uint32 numExamples = partition.getNumElements();
     uint32 numSamples = (uint32) (sampleSize_ * numExamples);
-    std::unique_ptr<DenseWeightVector> weightVectorPtr = std::make_unique<DenseWeightVector>(numExamples);
-    DenseWeightVector::iterator weightIterator = weightVectorPtr->begin();
+    std::unique_ptr<DenseWeightVector<uint32>> weightVectorPtr = std::make_unique<DenseWeightVector<uint32>>(
+        numExamples);
+    typename DenseWeightVector<uint32>::iterator weightIterator = weightVectorPtr->begin();
     uint32 numNonZeroWeights = 0;
 
     for (uint32 i = 0; i < numSamples; i++) {
@@ -38,8 +39,9 @@ std::unique_ptr<IWeightVector> Bagging::subSample(const BiPartition& partition, 
     uint32 numTrainingExamples = partition.getNumFirst();
     uint32 numSamples = (uint32) (sampleSize_ * numTrainingExamples);
     BiPartition::const_iterator indexIterator = partition.first_cbegin();
-    std::unique_ptr<DenseWeightVector> weightVectorPtr = std::make_unique<DenseWeightVector>(numExamples);
-    DenseWeightVector::iterator weightIterator = weightVectorPtr->begin();
+    std::unique_ptr<DenseWeightVector<uint32>> weightVectorPtr = std::make_unique<DenseWeightVector<uint32>>(
+        numExamples);
+    typename DenseWeightVector<uint32>::iterator weightIterator = weightVectorPtr->begin();
     uint32 numNonZeroWeights = 0;
 
     for (uint32 i = 0; i < numSamples; i++) {
