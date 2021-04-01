eead815905ac701639508d3756e4f4fdeb5cd558
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-01 15:12:22 +0200

    The class CsrView now inherits from BinaryCsrView.

--- cpp/subprojects/common/include/common/data/view_csr.hpp
+++ cpp/subprojects/common/include/common/data/view_csr.hpp
@@ -3,7 +3,7 @@
  */
 #pragma once
 
-#include "common/data/types.hpp"
+#include "common/data/view_csr_binary.hpp"
 
 
 /**
@@ -13,19 +13,11 @@
  * @tparam T The type of the values
  */
 template<class T>
-class CsrView final {
+class CsrView final : public BinaryCsrView {
 
     private:
 
-        uint32 numRows_;
-
-        uint32 numCols_;
-
         const T* data_;
-
-        const uint32* rowIndices_;
-
-        const uint32* colIndices_;
 
     public:
 
@@ -48,11 +40,6 @@ class CsrView final {
         typedef const T* value_const_iterator;
 
         /**
-         * An iterator that provides read-only access to the indices in the view.
-         */
-        typedef const uint32* index_const_iterator;
-
-        /**
          * Returns a `value_const_iterator` to the beginning of the values at a specific row.
          *
          * @param row   The row
@@ -67,43 +54,5 @@ class CsrView final {
          * @return      A `value_const_iterator` to the end of the values
          */
         value_const_iterator row_values_cend(uint32 row) const;
-
-        /**
-         * Returns an `index_const_iterator` to the beginning of the indices at a specific row.
-         *
-         * @param row   The row
-         * @return      An `index_const_iterator` to the beginning of the indices
-         */
-        index_const_iterator row_indices_cbegin(uint32 row) const;
-
-        /**
-         * Returns an `index_const_iterator` to the end of the indices at a specific row.
-         *
-         * @param row   The row
-         * @return      An `index_const_iterator` to the end of the indices
-         */
-        index_const_iterator row_indices_cend(uint32 row) const;
-
-        /**
-         * Returns the number of rows in the view.
-         *
-         * @return The number of rows
-         */
-        uint32 getNumRows() const;
-
-        /**
-         * Returns the number of columns in the view.
-         *
-         * @return The number of columns
-         */
-        uint32 getNumCols() const;
-
-        /**
-         * Returns the number of elements at a specific row that correspond to a non-zero value.
-         *
-         * @param row   The row
-         * @return      The number of non-zero elements
-         */
-        uint32 getNumNonZeroElements(uint32 row) const;
 
 };
