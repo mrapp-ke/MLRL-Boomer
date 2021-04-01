979347786f07e11f6b4bfcef802af6c9b81694de
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-01 15:11:45 +0200

    Add class BinaryCsrView.

---
+++ cpp/subprojects/common/include/common/data/view_csr_binary.hpp
@@ -0,0 +1,87 @@
+/*
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ */
+#pragma once
+
+#include "common/data/types.hpp"
+
+
+/**
+ * Implements row-wise read-only access to binary values that are stored in a pre-allocated matrix in the compressed
+ * sparse row (CSR) format.
+ */
+class BinaryCsrView {
+
+    private:
+
+        uint32 numRows_;
+
+        uint32 numCols_;
+
+        const uint32* colIndices_;
+
+    protected:
+
+        /**
+         * A pointer to an array that stores the indices of the first element in `colIndices_` that corresponds to a
+         * certain row.
+         */
+        const uint32* rowIndices_;
+
+    public:
+
+        /**
+         * @param numRows       The number of rows in the view
+         * @param numCols       The number of columns in the view
+         * @param rowIndices    A pointer to an array of type `uint32`, shape `(numRows + 1)`, that stores the indices
+         *                      of the first element in `colIndices` that corresponds to a certain row. The index at the
+         *                      last position is equal to `num_non_zero_values`
+         * @param colIndices    A pointer to an array of type `uint32`, shape `(num_non_zero_values)`, that stores the
+         *                      column-indices, the non-zero elements correspond to
+         */
+        BinaryCsrView(uint32 numRows, uint32 numCols, const uint32* rowIndices, const uint32* colIndices);
+
+        /**
+         * An iterator that provides read-only access to the indices in the view.
+         */
+        typedef const uint32* index_const_iterator;
+
+        /**
+         * Returns an `index_const_iterator` to the beginning of the indices at a specific row.
+         *
+         * @param row   The row
+         * @return      An `index_const_iterator` to the beginning of the indices
+         */
+        index_const_iterator row_indices_cbegin(uint32 row) const;
+
+        /**
+         * Returns an `index_const_iterator` to the end of the indices at a specific row.
+         *
+         * @param row   The row
+         * @return      An `index_const_iterator` to the end of the indices
+         */
+        index_const_iterator row_indices_cend(uint32 row) const;
+
+        /**
+         * Returns the number of rows in the view.
+         *
+         * @return The number of rows
+         */
+        uint32 getNumRows() const;
+
+        /**
+         * Returns the number of columns in the view.
+         *
+         * @return The number of columns
+         */
+        uint32 getNumCols() const;
+
+        /**
+         * Returns the number of elements at a specific row that correspond to a non-zero value.
+         *
+         * @param row   The row
+         * @return      The number of non-zero elements
+         */
+        uint32 getNumNonZeroElements(uint32 row) const;
+
+};
---
+++ cpp/subprojects/common/src/common/data/view_csr_binary.cpp
@@ -0,0 +1,27 @@
+#include "common/data/view_csr_binary.hpp"
+
+
+BinaryCsrView::BinaryCsrView(uint32 numRows, uint32 numCols, const uint32* rowIndices, const uint32* colIndices)
+    : numRows_(numRows), numCols_(numCols), colIndices_(colIndices), rowIndices_(rowIndices) {
+
+}
+
+BinaryCsrView::index_const_iterator BinaryCsrView::row_indices_cbegin(uint32 row) const {
+    return &colIndices_[rowIndices_[row]];
+}
+
+BinaryCsrView::index_const_iterator BinaryCsrView::row_indices_cend(uint32 row) const {
+    return &colIndices_[rowIndices_[row + 1]];
+}
+
+uint32 BinaryCsrView::getNumRows() const {
+    return numRows_;
+}
+
+uint32 BinaryCsrView::getNumCols() const {
+    return numCols_;
+}
+
+uint32 BinaryCsrView::getNumNonZeroElements(uint32 row) const {
+    return rowIndices_[row + 1] - rowIndices_[row];
+}
