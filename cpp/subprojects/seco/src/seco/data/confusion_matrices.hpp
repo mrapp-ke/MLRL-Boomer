f05ee9fca82017411d5a7ab1a4ed3f3ae7d6c2af
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-04 23:01:26 +0200

    Move enum ConfusionMatrixElement and function "getConfusionMatrixElement" to file seco/data/confusion_matrices.hpp.

---
+++ cpp/subprojects/seco/src/seco/data/confusion_matrices.hpp
@@ -0,0 +1,35 @@
+/*
+ * @author Jakob Steeg (jakob.steeg@gmail.com)
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ */
+#include "common/data/types.hpp"
+
+
+namespace seco {
+
+    /**
+     * An enum that specifies all positive elements of a confusion matrix.
+     */
+    enum ConfusionMatrixElement : uint32 {
+        IN = 0,
+        IP = 1,
+        RN = 2,
+        RP = 3
+    };
+
+    /**
+     * Returns the confusion matrix element, a label corresponds to, depending on the ground truth an a prediction.
+     *
+     * @param trueLabel         The true label according to the ground truth
+     * @param majorityLabel     The prediction of the default rule. The prediction is assumed to be the inverse
+     * @return                  The confusion matrix element
+     */
+    static inline ConfusionMatrixElement getConfusionMatrixElement(uint8 trueLabel, uint8 majorityLabel) {
+        if (trueLabel) {
+            return majorityLabel ? RN : RP;
+        } else {
+            return majorityLabel ? IN : IP;
+        }
+    }
+
+}
