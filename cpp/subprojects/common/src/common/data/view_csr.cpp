4d6759f5d66a8793dd15150b28bf38b762445af7
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-05 18:46:26 +0200

    The class CsrView does not inherit from the class BinaryCsrView anymore.

--- cpp/subprojects/common/src/common/data/view_csr.cpp
+++ cpp/subprojects/common/src/common/data/view_csr.cpp
@@ -3,7 +3,7 @@
 
 template<class T>
 CsrView<T>::CsrView(uint32 numRows, uint32 numCols, const T* data, const uint32* rowIndices, const uint32* colIndices)
-    : BinaryCsrView(numRows, numCols, rowIndices, colIndices), data_(data) {
+    : numRows_(numRows), numCols_(numCols), data_(data), rowIndices_(rowIndices), colIndices_(colIndices) {
 
 }
 
@@ -15,6 +15,26 @@ typename CsrView<T>::value_const_iterator CsrView<T>::row_values_cbegin(uint32 r
 template<class T>
 typename CsrView<T>::value_const_iterator CsrView<T>::row_values_cend(uint32 row) const {
     return &data_[rowIndices_[row + 1]];
+}
+
+template<class T>
+typename CsrView<T>::index_const_iterator CsrView<T>::row_indices_cbegin(uint32 row) const {
+    return &colIndices_[rowIndices_[row]];
+}
+
+template<class T>
+typename CsrView<T>::index_const_iterator CsrView<T>::row_indices_cend(uint32 row) const {
+    return &colIndices_[rowIndices_[row + 1]];
+}
+
+template<class T>
+uint32 CsrView<T>::getNumRows() const {
+    return numRows_;
+}
+
+template<class T>
+uint32 CsrView<T>::getNumCols() const {
+    return numCols_;
 }
 
 template class CsrView<float32>;
