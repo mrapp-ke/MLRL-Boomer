3ad8f9f4ac2756792b4197feac64997ad5c98569
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-08 16:03:30 +0200

    Add function "getLabelVector" to class ILabelMatrix.

--- cpp/subprojects/common/src/common/input/label_matrix_c_contiguous.cpp
+++ cpp/subprojects/common/src/common/input/label_matrix_c_contiguous.cpp
@@ -14,6 +14,23 @@ uint32 CContiguousLabelMatrix::getNumCols() const {
     return view_.getNumCols();
 }
 
+std::unique_ptr<LabelVector> CContiguousLabelMatrix::getLabelVector(uint32 row) const {
+    uint32 numCols = this->getNumCols();
+    std::unique_ptr<LabelVector> labelVectorPtr = std::make_unique<LabelVector>(numCols);
+    LabelVector::index_iterator iterator = labelVectorPtr->indices_begin();
+    uint32 n = 0;
+
+    for (uint32 i = 0; i < numCols; i++) {
+        if (this->getValue(row, i)) {
+            iterator[n] = i;
+            n++;
+        }
+    }
+
+    labelVectorPtr->setNumElements(n, true);
+    return labelVectorPtr;
+}
+
 uint8 CContiguousLabelMatrix::getValue(uint32 exampleIndex, uint32 labelIndex) const {
     return view_.row_cbegin(exampleIndex)[labelIndex];
 }
