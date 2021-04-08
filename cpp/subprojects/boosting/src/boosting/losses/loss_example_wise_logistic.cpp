e59cbb3a34e872199a89e48c03d3d34cf8f18bb3
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-08 22:15:35 +0200

    Do not traverse labels in reversed order.

--- cpp/subprojects/boosting/src/boosting/losses/loss_example_wise_logistic.cpp
+++ cpp/subprojects/boosting/src/boosting/losses/loss_example_wise_logistic.cpp
@@ -60,11 +60,10 @@ namespace boosting {
         // triangle of the Hessian matrix)...
         zeroExp = divideOrZero<float64>(zeroExp, sumExp2);
 
-        // Calculate the gradients and Hessians by traversing the labels in reverse order (to ensure that the values
-        // that have temporarily been stored in the array of gradients have not been overwritten yet)
-        intp i = triangularNumber(numLabels) - 1;
+        // Calculate the gradients and Hessians...
+        uint32 i = 0;
 
-        for (intp c = numLabels - 1; c >= 0; c--) {
+        for (uint32 c = 0; c < numLabels; c++) {
             float64 predictedScore = scoreIterator[c];
             uint8 trueLabel = labelIterator[c];
             float64 invertedExpectedScore = trueLabel ? -1 : 1;
@@ -77,25 +76,25 @@ namespace boosting {
             float64 tmp = divideOrZero<float64>(xExp, sumExp);
             gradientIterator[c] = invertedExpectedScore * tmp;
 
-            // Calculate the Hessian on the diagonal of the Hessian matrix that corresponds to the current label. Such
-            // Hessian calculates as `exp(x_c) * (1 + exp(x_1) + exp(x_2) + ...) / (1 + exp(x_1) + exp(x_2) + ...)^2`,
-            // or as `(exp(x_c - max) / sumExp) * (1 - exp(x_c - max) / sumExp)`
-            hessianIterator[i] = tmp * (1 - tmp);
-            i--;
-
             // Calculate the Hessians that belong to the part of the Hessian matrix' upper triangle that corresponds to
             // the current label. Such Hessian calculates as
             // `-expectedScore_c * expectedScore_r * exp(x_c + x_r) / (1 + exp(x_1) + exp(x_2) + ...)^2`, or as
             // `-expectedScore_c * expectedScore_r * (exp(x_c + x_r - max) / sumExp) * (exp(0 - max) / sumExp)`
-            for (intp r = c - 1; r >= 0; r--) {
+            for (uint32 r = 0; r < c; r++) {
                 float64 predictedScore2 = scoreIterator[r];
                 uint32 trueLabel2 = labelIterator[r];
                 float64 expectedScore2 = trueLabel2 ? 1 : -1;
                 float64 x2 = predictedScore2 * -expectedScore2;
                 hessianIterator[i] = invertedExpectedScore * expectedScore2
                                      * divideOrZero<float64>(std::exp(x + x2 - max2), sumExp2) * zeroExp;
-                i--;
+                i++;
             }
+
+            // Calculate the Hessian on the diagonal of the Hessian matrix that corresponds to the current label. Such
+            // Hessian calculates as `exp(x_c) * (1 + exp(x_1) + exp(x_2) + ...) / (1 + exp(x_1) + exp(x_2) + ...)^2`,
+            // or as `(exp(x_c - max) / sumExp) * (1 - exp(x_c - max) / sumExp)`
+            hessianIterator[i] = tmp * (1 - tmp);
+            i++;
         }
     }
 
