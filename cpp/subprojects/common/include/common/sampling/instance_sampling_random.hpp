21707cb14d9898e7208958cc0f03e8fc257af97a
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-13 16:42:04 +0200

    Add factories for creating instances of the type IInstanceSubSampling.

--- cpp/subprojects/common/include/common/sampling/instance_sampling_random.hpp
+++ cpp/subprojects/common/include/common/sampling/instance_sampling_random.hpp
@@ -29,3 +29,27 @@ class RandomInstanceSubsetSelection final : public IInstanceSubSampling {
         std::unique_ptr<IWeightVector> subSample(const BiPartition& partition, RNG& rng) const override;
 
 };
+
+/**
+ * Allows to create instances of the type `IInstanceSubSampling` that allow to select a subset of the available training
+ * examples without replacement.
+ */
+class RandomInstanceSubsetSelectionFactory final : public IInstanceSubSamplingFactory {
+
+    private:
+
+        float32 sampleSize_;
+
+    public:
+
+        /**
+         * @param sampleSize The fraction of examples to be included in the sample (e.g. a value of 0.6 corresponds to
+         *                   60 % of the available examples). Must be in (0, 1)
+         */
+        RandomInstanceSubsetSelectionFactory(float32 sampleSize);
+
+        std::unique_ptr<IInstanceSubSampling> create(const CContiguousLabelMatrix& labelMatrix) const override;
+
+        std::unique_ptr<IInstanceSubSampling> create(const CsrLabelMatrix& labelMatrix) const override;
+
+};
