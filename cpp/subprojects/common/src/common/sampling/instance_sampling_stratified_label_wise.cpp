18d6ae773d85406e97a9a874eba792145d36f27b
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-15 18:44:17 +0200

    Implement class LabelWiseStratifiedSampling.

--- cpp/subprojects/common/src/common/sampling/instance_sampling_stratified_label_wise.cpp
+++ cpp/subprojects/common/src/common/sampling/instance_sampling_stratified_label_wise.cpp
@@ -1,8 +1,66 @@
 #include "common/sampling/instance_sampling_stratified_label_wise.hpp"
+#include "common/sampling/weight_vector_dense.hpp"
 #include "common/sampling/partition_bi.hpp"
 #include "common/sampling/partition_single.hpp"
 #include "common/input/label_matrix_csc.hpp"
+#include "common/data/arrays.hpp"
+#include <limits>
 
+#define UNSET_WEIGHT 2
+
+
+static inline std::unique_ptr<DenseVector<uint32>> getNumExamplesPerLabel(const CscLabelMatrix& labelMatrix) {
+    uint32 numCols = labelMatrix.getNumCols();
+    std::unique_ptr<DenseVector<uint32>> vectorPtr = std::make_unique<DenseVector<uint32>>(numCols);
+    DenseVector<uint32>::iterator iterator = vectorPtr->begin();
+
+    for (uint32 i = 0; i < numCols; i++) {
+        uint32 numExamples = labelMatrix.column_indices_cend(i) - labelMatrix.column_indices_cbegin(i);
+        iterator[i] = numExamples;
+    }
+
+    return vectorPtr;
+}
+
+static inline uint32 getLabelWithFewestExamples(DenseVector<uint32>::const_iterator numExamplesIterator,
+                                                uint32 numElements) {
+    uint32 minNumExamples = std::numeric_limits<uint32>::max();
+    uint32 index = numElements;
+
+    for (uint32 i = 1; i < numElements; i++) {
+        uint32 numExamples = numExamplesIterator[i];
+
+        if (numExamples > 0 && numExamples < minNumExamples) {
+            minNumExamples = numExamples;
+            index = i;
+        }
+    }
+
+    return index;
+}
+
+static inline void updateNumExamplesPerLabel(const CContiguousLabelMatrix& labelMatrix, uint32 exampleIndex,
+                                             DenseVector<uint32>::iterator numExamplesIterator) {
+    CContiguousLabelMatrix::value_const_iterator labelIterator = labelMatrix.row_values_cbegin(exampleIndex);
+    uint32 numLabels = labelMatrix.getNumCols();
+
+    for (uint32 i = 0; i < numLabels; i++) {
+        if (labelIterator[i]) {
+            numExamplesIterator[i]--;
+        }
+    }
+}
+
+static inline void updateNumExamplesPerLabel(const CsrLabelMatrix& labelMatrix, uint32 exampleIndex,
+                                             DenseVector<uint32>::iterator numExamplesIterator) {
+    CsrLabelMatrix::index_const_iterator indexIterator = labelMatrix.row_indices_cbegin(exampleIndex);
+    uint32 numLabels = labelMatrix.row_indices_cend(exampleIndex) - indexIterator;
+
+    for (uint32 i = 0; i < numLabels; i++) {
+        uint32 labelIndex = indexIterator[i];
+        numExamplesIterator[labelIndex]--;
+    }
+}
 
 /**
  * Implements iterative stratified sampling for selecting a subset of the available training examples, such that for
@@ -43,8 +101,61 @@ class LabelWiseStratifiedSampling final : public IInstanceSubSampling {
         }
 
         std::unique_ptr<IWeightVector> subSample(RNG& rng) const override {
-            // TODO Implement
-            return nullptr;
+            // Create a vector to store the weights of individual examples...
+            uint32 numExamples = partition_.getNumElements();
+            std::unique_ptr<DenseWeightVector<uint32>> weightVectorPtr =
+                std::make_unique<DenseWeightVector<uint32>>(numExamples);
+            DenseWeightVector<uint32>::iterator weightIterator = weightVectorPtr->begin();
+
+            // Initialize the weights of all examples with the value `UNSET_WEIGHT`, which allows to identify examples
+            // for which no weight has been set yet...
+            setArrayToValue<uint32>(weightIterator, numExamples, UNSET_WEIGHT);
+
+            // Create a vector that stores the number of examples that are associated with individual labels...
+            std::unique_ptr<DenseVector<uint32>> numExamplesVectorPtr = getNumExamplesPerLabel(*cscLabelMatrixPtr_);
+            DenseVector<uint32>::iterator numExamplesIterator = numExamplesVectorPtr->begin();
+            uint32 numLabels = numExamplesVectorPtr->getNumElements();
+
+            // For each label, assign a weight to the examples that are associated with the label, if no weight has been
+            // set yet. Labels with few examples are processed first...
+            uint32 numNonZeroWeights = 0;
+            uint32 labelIndex;
+
+            while ((labelIndex = getLabelWithFewestExamples(numExamplesIterator, numLabels)) < numLabels) {
+                CscLabelMatrix::index_iterator indexIterator = cscLabelMatrixPtr_->column_indices_begin(labelIndex);
+                uint32 numExamples = cscLabelMatrixPtr_->column_indices_end(labelIndex) - indexIterator;
+                uint32 numSamples = (uint32) (sampleSize_ * numExamplesIterator[labelIndex]);
+                numNonZeroWeights += numSamples;
+                uint32 i;
+
+                // Use the Fisher-Yates shuffle to randomly draw `numSamples` examples for which no weight has been set
+                // yet and set their weight to 1...
+                for (i = 0; i < numExamples && numSamples > 0; i++) {
+                    uint32 randomIndex = rng.random(i, numExamples);
+                    uint32 exampleIndex = indexIterator[randomIndex];
+                    indexIterator[randomIndex] = indexIterator[i];
+                    indexIterator[i] = exampleIndex;
+
+                    if (weightIterator[exampleIndex] == UNSET_WEIGHT) {
+                        weightIterator[exampleIndex] = 1;
+                        updateNumExamplesPerLabel(labelMatrix_, exampleIndex, numExamplesIterator);
+                        numSamples--;
+                    }
+                }
+
+                // Set the weights of the remaining examples to 0, if no weight has been set yet...
+                for (i = i + 1; i < numExamples; i++) {
+                    uint32 exampleIndex = indexIterator[i];
+
+                    if (weightIterator[exampleIndex] == UNSET_WEIGHT) {
+                        weightIterator[exampleIndex] = 0;
+                        updateNumExamplesPerLabel(labelMatrix_, exampleIndex, numExamplesIterator);
+                    }
+                }
+            }
+
+            weightVectorPtr->setNumNonZeroWeights(numNonZeroWeights);
+            return weightVectorPtr;
         }
 
 };
