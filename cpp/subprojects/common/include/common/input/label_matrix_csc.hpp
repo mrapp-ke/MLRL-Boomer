9a3798f05ecf7af2769efc3d98599cd65316a45e
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-15 21:14:23 +0200

    Allow to specify the indices of the examples that should be considered when creating a CscLabelMatrix.

--- cpp/subprojects/common/include/common/input/label_matrix_csc.hpp
+++ cpp/subprojects/common/include/common/input/label_matrix_csc.hpp
@@ -6,11 +6,17 @@
 #include "common/data/view_csc_binary.hpp"
 #include "common/input/label_matrix_c_contiguous.hpp"
 #include "common/input/label_matrix_csr.hpp"
+#include "common/indices/index_vector_full.hpp"
+#include "common/indices/index_vector_partial.hpp"
 
 
 /**
  * Implements column-wise read-only access to the labels of individual training examples that are stored in a matrix in
  * the compressed sparse column (CSC) format.
+ *
+ * This class provides copy constructors for copying an existing `CContiguousLabelMatrix`, which provides random access,
+ * or a `CsrLabelMatrix`, which provides row-wise access to the labels of the training examples. These constructors
+ * expect the indices of the examples to be considered when copying the existing label matrix to be provided.
  */
 class CscLabelMatrix final {
 
@@ -25,14 +31,44 @@ class CscLabelMatrix final {
     public:
 
         /**
-         * @param labelMatrix A reference to an object of type `CContiguousLabelMatrix` to be copied
+         * @param labelMatrix   A reference to an object of type `CContiguousLabelMatrix` to be copied
+         * @param indicesBegin  A `FullIndexVector::const_iterator` to the beginning of the indices of the examples to
+         *                      be considered
+         * @param indicesEnd    A `FullIndexVector::const_iterator` to the end of the indices of the examples to be
+         *                      considered
          */
-        CscLabelMatrix(const CContiguousLabelMatrix& labelMatrix);
+        CscLabelMatrix(const CContiguousLabelMatrix& labelMatrix, FullIndexVector::const_iterator indicesBegin,
+                       FullIndexVector::const_iterator indicesEnd);
 
         /**
-         * @param labelMatrix A reference to an object of type `CsrLabelMatrix` to be copied
+         * @param labelMatrix   A reference to an object of type `CContiguousLabelMatrix` to be copied
+         * @param indicesBegin  A `PartialIndexVector::const_iterator` to the beginning of the indices of the examples
+         *                      to be considered
+         * @param indicesEnd    A `PartialIndexVector::const_iterator` to the end of the indices of the examples to be
+         *                      considered
          */
-        CscLabelMatrix(const CsrLabelMatrix& labelMatrix);
+        CscLabelMatrix(const CContiguousLabelMatrix& labelMatrix, PartialIndexVector::const_iterator indicesBegin,
+                       PartialIndexVector::const_iterator indicesEnd);
+
+        /**
+         * @param labelMatrix   A reference to an object of type `CsrLabelMatrix` to be copied
+         * @param indicesBegin  A `FullIndexVector::const_iterator` to the beginning of the indices of the examples to
+         *                      be considered
+         * @param indicesEnd    A `FullIndexVector::const_iterator` to the end of the indices of the examples to be
+         *                      considered
+         */
+        CscLabelMatrix(const CsrLabelMatrix& labelMatrix, FullIndexVector::const_iterator indicesBegin,
+                       FullIndexVector::const_iterator indicesEnd);
+
+        /**
+         * @param labelMatrix   A reference to an object of type `CsrLabelMatrix` to be copied
+         * @param indicesBegin  A `PartialIndexVector::const_iterator` to the beginning of the indices of the examples
+         *                      to be considered
+         * @param indicesEnd    A `PartialIndexVector::const_iterator` to the end of the indices of the examples to be
+         *                      considered
+         */
+        CscLabelMatrix(const CsrLabelMatrix& labelMatrix, PartialIndexVector::const_iterator indicesBegin,
+                       PartialIndexVector::const_iterator indicesEnd);
 
         ~CscLabelMatrix();
 
