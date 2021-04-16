533dc825f84cb46d094e8bd6089db3bbd89316c2
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-16 15:35:42 +0200

    Add class LabelVectorSet.

---
+++ cpp/subprojects/common/src/common/input/label_vector_set.cpp
@@ -0,0 +1,17 @@
+#include "common/input/label_vector_set.hpp"
+
+
+template<class T>
+typename LabelVectorSet<T>::const_iterator LabelVectorSet<T>::cbegin() const {
+    return map_.cbegin();
+}
+
+template<class T>
+typename LabelVectorSet<T>::const_iterator LabelVectorSet<T>::cend() const {
+    return map_.cend();
+}
+
+template<class T>
+T& LabelVectorSet<T>::addLabelVector(std::unique_ptr<LabelVector> labelVectorPtr) {
+    return map_[std::move(labelVectorPtr)];
+}
