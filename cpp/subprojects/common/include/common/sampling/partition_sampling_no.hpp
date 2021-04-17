983c592340c98eeb43d46ca055653e0f47dd21e1
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-18 00:49:47 +0200

    Add class NoPartitionSamplingFactory.

--- cpp/subprojects/common/include/common/sampling/partition_sampling_no.hpp
+++ cpp/subprojects/common/include/common/sampling/partition_sampling_no.hpp
@@ -10,10 +10,22 @@
  * An implementation of the class `IPartitionSampling` that does not split the training examples, but includes all of
  * them in the training set.
  */
-class NoPartitionSampling : public IPartitionSampling {
+class NoPartitionSampling final : public IPartitionSampling {
 
     public:
 
         std::unique_ptr<IPartition> partition(uint32 numExamples, RNG& rng) const override;
+
+};
+
+/**
+ * Allows to create objects of the type `IPartitionSampling` that do not split the training examples, but include all of
+ * them in the training set.
+ */
+class NoPartitionSamplingFactory final : public IPartitionSamplingFactory {
+
+    public:
+
+        std::unique_ptr<IPartitionSampling> create(uint32 numExamples) const override;
 
 };
