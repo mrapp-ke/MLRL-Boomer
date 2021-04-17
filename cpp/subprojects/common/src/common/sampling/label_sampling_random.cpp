ed91fe68cb8d229ee94b986e7956c03e9fb9a515
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-18 00:29:12 +0200

    Move classes to cpp files.

--- cpp/subprojects/common/src/common/sampling/label_sampling_random.cpp
+++ cpp/subprojects/common/src/common/sampling/label_sampling_random.cpp
@@ -2,14 +2,35 @@
 #include "common/indices/index_iterator.hpp"
 #include "index_sampling.hpp"
 
-RandomLabelSubsetSelection::RandomLabelSubsetSelection(uint32 numLabels, uint32 numSamples)
-    : numLabels_(numLabels), numSamples_(numSamples) {
 
-}
+/**
+ * Allows to select a subset of the available labels without replacement.
+ */
+class RandomLabelSubsetSelection final : public ILabelSubSampling {
 
-std::unique_ptr<IIndexVector> RandomLabelSubsetSelection::subSample(RNG& rng) const {
-    return sampleIndicesWithoutReplacement<IndexIterator>(IndexIterator(numLabels_), numLabels_, numSamples_, rng);
-}
+    private:
+
+        uint32 numLabels_;
+
+        uint32 numSamples_;
+
+    public:
+
+        /**
+         * @param numLabels     The total number of available labels
+         * @param numSamples    The number of labels to be included in the sample
+         */
+        RandomLabelSubsetSelection(uint32 numLabels, uint32 numSamples)
+            : numLabels_(numLabels), numSamples_(numSamples) {
+
+        }
+
+        std::unique_ptr<IIndexVector> subSample(RNG& rng) const override {
+            return sampleIndicesWithoutReplacement<IndexIterator>(IndexIterator(numLabels_), numLabels_, numSamples_,
+                                                                  rng);
+        }
+
+};
 
 std::unique_ptr<ILabelSubSampling> RandomLabelSubsetSelectionFactory::create(uint32 numLabels) const {
     return std::make_unique<RandomLabelSubsetSelection>(numLabels, numSamples_);
