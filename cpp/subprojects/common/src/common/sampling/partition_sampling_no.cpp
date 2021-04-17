88785fa6d9a7bf75318f1991c966788746e382d6
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-18 00:49:47 +0200

    Add class NoPartitionSamplingFactory.

--- cpp/subprojects/common/src/common/sampling/partition_sampling_no.cpp
+++ cpp/subprojects/common/src/common/sampling/partition_sampling_no.cpp
@@ -5,3 +5,7 @@
 std::unique_ptr<IPartition> NoPartitionSampling::partition(uint32 numExamples, RNG& rng) const {
     return std::make_unique<SinglePartition>(numExamples);
 }
+
+std::unique_ptr<IPartitionSampling> NoPartitionSamplingFactory::create(uint32 numExamples) const {
+    return std::make_unique<NoPartitionSampling>();
+}
