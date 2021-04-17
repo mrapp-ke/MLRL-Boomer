450b289b9fb0dfa3bb1313d002e0ac05383aa3df
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-17 23:23:24 +0200

    Add class NoFeatureSubSamplingFactory.

--- cpp/subprojects/common/src/common/sampling/feature_sampling_no.cpp
+++ cpp/subprojects/common/src/common/sampling/feature_sampling_no.cpp
@@ -5,3 +5,7 @@
 std::unique_ptr<IIndexVector> NoFeatureSubSampling::subSample(uint32 numFeatures, RNG& rng) const {
     return std::make_unique<FullIndexVector>(numFeatures);
 }
+
+std::unique_ptr<IFeatureSubSampling> NoFeatureSubSamplingFactory::create(uint32 numFeatures) const {
+    return std::make_unique<NoFeatureSubSampling>();
+}
