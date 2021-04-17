0f26e6590e7ee1fd4dc218acd3b8c7a3c8b8edf4
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-18 00:29:12 +0200

    Move classes to cpp files.

--- cpp/subprojects/common/include/common/sampling/label_sampling_random.hpp
+++ cpp/subprojects/common/include/common/sampling/label_sampling_random.hpp
@@ -7,29 +7,6 @@
 
 
 /**
- * Implements random label subset selection for selecting a random subset of the available features without replacement.
- */
-class RandomLabelSubsetSelection final : public ILabelSubSampling {
-
-    private:
-
-        uint32 numLabels_;
-
-        uint32 numSamples_;
-
-    public:
-
-        /**
-         * @param numLabels     The total number of available labels
-         * @param numSamples    The number of labels to be included in the sample
-         */
-        RandomLabelSubsetSelection(uint32 numLabels, uint32 numSamples);
-
-        std::unique_ptr<IIndexVector> subSample(RNG& rng) const override;
-
-};
-
-/**
  * Allows to create objects of type `ILabelSubSampling` that select a random subset of the available features without
  * replacement.
  */
