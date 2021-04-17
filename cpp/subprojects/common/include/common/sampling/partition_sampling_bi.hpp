6ff43a23663daacb677ff83602ca89833ea17872
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-18 01:10:19 +0200

    Move classes to cpp files.

--- cpp/subprojects/common/include/common/sampling/partition_sampling_bi.hpp
+++ cpp/subprojects/common/include/common/sampling/partition_sampling_bi.hpp
@@ -7,31 +7,6 @@
 
 
 /**
- * An implementation of the class `IPartitionSampling` that splits the training examples into two mutually exclusive
- * sets that may be used as a training set and a holdout set.
- */
-class BiPartitionSampling final : public IPartitionSampling {
-
-    private:
-
-        uint32 numHoldout_;
-
-        uint32 numTraining_;
-
-    public:
-
-        /**
-         * @param numExamples       The total number of available training examples
-         * @param holdoutSetSize    The fraction of examples to be included in the holdout set (e.g. a value of 0.6
-         *                          corresponds to 60 % of the available examples). Must be in (0, 1)
-         */
-        BiPartitionSampling(uint32 numExamples, float32 holdoutSetSize);
-
-        std::unique_ptr<IPartition> createPartition(RNG& rng) const override;
-
-};
-
-/**
  * Allows to create objects of the type `IPartitionSampling` that split the training examples into two mutually
  * exclusive sets that may be used as a training set and a holdout set.
  */
--- cpp/subprojects/common/src/common/sampling/partition_sampling_bi.cpp
+++ cpp/subprojects/common/src/common/sampling/partition_sampling_bi.cpp
@@ -3,28 +3,50 @@
 #include "index_sampling.hpp"
 
 
-BiPartitionSampling::BiPartitionSampling(uint32 numExamples, float32 holdoutSetSize)
-    : numHoldout_((uint32) (holdoutSetSize * numExamples)), numTraining_(numExamples - numHoldout_) {
+/**
+ * Allows to split the training examples into two mutually exclusive sets that may be used as a training set and a
+ * holdout set.
+ */
+class BiPartitionSampling final : public IPartitionSampling {
 
-}
+    private:
 
-std::unique_ptr<IPartition> BiPartitionSampling::createPartition(RNG& rng) const {
-    std::unique_ptr<BiPartition> partitionPtr = std::make_unique<BiPartition>(numTraining_, numHoldout_);
-    BiPartition::iterator trainingIterator = partitionPtr->first_begin();
-    BiPartition::iterator holdoutIterator = partitionPtr->second_begin();
+        uint32 numHoldout_;
 
-    for (uint32 i = 0; i < numTraining_; i++) {
-        trainingIterator[i] = i;
-    }
+        uint32 numTraining_;
 
-    for (uint32 i = 0; i < numHoldout_; i++) {
-        holdoutIterator[i] = numTraining_ + i;
-    }
+    public:
 
-    randomPermutation<BiPartition::iterator, BiPartition::iterator>(trainingIterator, holdoutIterator, numTraining_,
-                                                                    partitionPtr->getNumElements(), rng);
-    return partitionPtr;
-}
+        /**
+         * @param numExamples       The total number of available training examples
+         * @param holdoutSetSize    The fraction of examples to be included in the holdout set (e.g. a value of 0.6
+         *                          corresponds to 60 % of the available examples). Must be in (0, 1)
+         */
+        BiPartitionSampling(uint32 numExamples, float32 holdoutSetSize)
+            : numHoldout_((uint32) (holdoutSetSize * numExamples)), numTraining_(numExamples - numHoldout_) {
+
+        }
+
+        std::unique_ptr<IPartition> createPartition(RNG& rng) const override {
+            std::unique_ptr<BiPartition> partitionPtr = std::make_unique<BiPartition>(numTraining_, numHoldout_);
+            BiPartition::iterator trainingIterator = partitionPtr->first_begin();
+            BiPartition::iterator holdoutIterator = partitionPtr->second_begin();
+
+            for (uint32 i = 0; i < numTraining_; i++) {
+                trainingIterator[i] = i;
+            }
+
+            for (uint32 i = 0; i < numHoldout_; i++) {
+                holdoutIterator[i] = numTraining_ + i;
+            }
+
+            randomPermutation<BiPartition::iterator, BiPartition::iterator>(trainingIterator, holdoutIterator,
+                                                                            numTraining_,
+                                                                            partitionPtr->getNumElements(), rng);
+            return partitionPtr;
+        }
+
+};
 
 BiPartitionSamplingFactory::BiPartitionSamplingFactory(float32 holdoutSetSize)
     : holdoutSetSize_(holdoutSetSize) {
