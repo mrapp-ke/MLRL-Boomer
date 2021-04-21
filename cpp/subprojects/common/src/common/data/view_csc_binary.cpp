248c89ee651d2756713b11610b68734eb178519e
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-22 01:15:11 +0200

    Add class BinaryCscConstView.

--- cpp/subprojects/common/src/common/data/view_csc_binary.cpp
+++ cpp/subprojects/common/src/common/data/view_csc_binary.cpp
@@ -1,43 +1,48 @@
 #include "common/data/view_csc_binary.hpp"
 
 
-BinaryCscView::BinaryCscView(uint32 numRows, uint32 numCols, uint32* rowIndices, uint32* colIndices)
+BinaryCscConstView::BinaryCscConstView(uint32 numRows, uint32 numCols, uint32* rowIndices, uint32* colIndices)
     : numRows_(numRows), numCols_(numCols), rowIndices_(rowIndices), colIndices_(colIndices) {
 
 }
 
-BinaryCscView::index_iterator BinaryCscView::column_indices_begin(uint32 col) {
+BinaryCscConstView::index_const_iterator BinaryCscConstView::column_indices_cbegin(uint32 col) const {
     return &rowIndices_[colIndices_[col]];
 }
 
-BinaryCscView::index_iterator BinaryCscView::column_indices_end(uint32 col) {
+BinaryCscConstView::index_const_iterator BinaryCscConstView::column_indices_cend(uint32 col) const {
     return &rowIndices_[colIndices_[col + 1]];
 }
 
-BinaryCscView::index_const_iterator BinaryCscView::column_indices_cbegin(uint32 col) const {
-    return &rowIndices_[colIndices_[col]];
-}
-
-BinaryCscView::index_const_iterator BinaryCscView::column_indices_cend(uint32 col) const {
-    return &rowIndices_[colIndices_[col + 1]];
-}
-
-BinaryCscView::value_const_iterator BinaryCscView::column_values_cbegin(uint32 col) const {
+BinaryCscConstView::value_const_iterator BinaryCscConstView::column_values_cbegin(uint32 col) const {
     return make_index_forward_iterator(this->column_indices_cbegin(col), this->column_indices_cend(col));
 }
 
-BinaryCscView::value_const_iterator BinaryCscView::column_values_cend(uint32 col) const {
+BinaryCscConstView::value_const_iterator BinaryCscConstView::column_values_cend(uint32 col) const {
     return make_index_forward_iterator(this->column_indices_cbegin(col), this->column_indices_cend(col), numRows_);
 }
 
-uint32 BinaryCscView::getNumRows() const {
+uint32 BinaryCscConstView::getNumRows() const {
     return numRows_;
 }
 
-uint32 BinaryCscView::getNumCols() const {
+uint32 BinaryCscConstView::getNumCols() const {
     return numCols_;
 }
 
-uint32 BinaryCscView::getNumNonZeroElements() const {
+uint32 BinaryCscConstView::getNumNonZeroElements() const {
     return colIndices_[numCols_];
+}
+
+BinaryCscView::BinaryCscView(uint32 numRows, uint32 numCols, uint32* rowIndices, uint32* colIndices)
+    : BinaryCscConstView(numRows, numCols, rowIndices, colIndices) {
+
+}
+
+BinaryCscView::index_iterator BinaryCscView::column_indices_begin(uint32 col) {
+    return &BinaryCscConstView::rowIndices_[BinaryCscConstView::colIndices_[col]];
+}
+
+BinaryCscView::index_iterator BinaryCscView::column_indices_end(uint32 col) {
+    return &BinaryCscConstView::rowIndices_[BinaryCscConstView::colIndices_[col + 1]];
 }
