171b65bfff2a144d091fb7fdfbe16f2690771f47
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-22 00:37:00 +0200

    Add class CscConstView.

--- cpp/subprojects/common/include/common/data/view_csc.hpp
+++ cpp/subprojects/common/include/common/data/view_csc.hpp
@@ -13,9 +13,9 @@
  * @tparam T The type of the values
  */
 template<class T>
-class CscView final {
+class CscConstView {
 
-    private:
+    protected:
 
         uint32 numRows_;
 
@@ -40,12 +40,7 @@ class CscView final {
          *                      of the first element in `data` and `rowIndices` that corresponds to a certain column.
          *                      The index at the last position is equal to `num_non_zero_values`
          */
-        CscView(uint32 numRows, uint32 numCols, T* data, uint32* rowIndices, uint32* colIndices);
-
-        /**
-         * An iterator that provides access to the values in the view and allows to modify them.
-         */
-        typedef T* value_iterator;
+        CscConstView(uint32 numRows, uint32 numCols, T* data, uint32* rowIndices, uint32* colIndices);
 
         /**
          * An iterator that provides read-only access to the values in the view.
@@ -53,30 +48,9 @@ class CscView final {
         typedef const T* value_const_iterator;
 
         /**
-         * An iterator that provides access to the indices in the view and allows to modify them.
-         */
-        typedef uint32* index_iterator;
-
-        /**
          * An iterator that provides read-only access to the indices in the view.
          */
         typedef const uint32* index_const_iterator;
-
-        /**
-         * Returns a `value_iterator` to the beginning of the values at a specific column.
-         *
-         * @param col   The column
-         * @return      A `value_iterator` to the beginning of the values
-         */
-        value_iterator column_values_begin(uint32 col);
-
-        /**
-         * Returns a `value_iterator` to the end of the values at a specific column.
-         *
-         * @param col   The column
-         * @return      A `value_iterator` to the end of the values
-         */
-        value_iterator column_values_end(uint32 col);
 
         /**
          * Returns a `value_const_iterator` to the beginning of the values at a specific column.
@@ -93,22 +67,6 @@ class CscView final {
          * @return      A `value_const_iterator` to the end of the values
          */
         value_const_iterator column_values_cend(uint32 col) const;
-
-        /**
-         * Returns an `index_iterator` to the beginning of the indices at a specific column.
-         *
-         * @param col   The column
-         * @return      An `index_iterator` to the beginning of the indices
-         */
-        index_iterator column_indices_begin(uint32 col);
-
-        /**
-         * Returns an `index_iterator` to the end of the indices at a specific column.
-         *
-         * @param col   The column
-         * @return      An `index_iterator` to the end of the indices
-         */
-        index_iterator column_indices_end(uint32 col);
 
         /**
          * Returns an `index_const_iterator` to the beginning of the indices at a specific column.
@@ -146,5 +104,74 @@ class CscView final {
          * @return The number of non-zero elements
          */
         uint32 getNumNonZeroElements() const;
+
+};
+
+
+/**
+ * Implements column-wise read and write access to the values that are stored in a pre-allocated matrix in the
+ * compressed sparse column (CSC) format.
+ *
+ * @tparam T The type of the values
+ */
+template<class T>
+class CscView final : public CscConstView<T> {
+
+    public:
+
+        /**
+         * @param numRows       The number of rows in the view
+         * @param numCols       The number of columns in the view
+         * @param data          A pointer to an array of template type `T`, shape `(num_non_zero_values)`, that stores
+         *                      all non-zero values
+         * @param rowIndices    A pointer to an array of type `uint32`, shape `(num_non_zero_values)`, that stores the
+         *                      row-indices, the values in `data` correspond to
+         * @param colIndices    A pointer to an array of type `uint32`, shape `(numCols + 1)`, that stores the indices
+         *                      of the first element in `data` and `rowIndices` that corresponds to a certain column.
+         *                      The index at the last position is equal to `num_non_zero_values`
+         */
+        CscView(uint32 numRows, uint32 numCols, T* data, uint32* rowIndices, uint32* colIndices);
+
+        /**
+         * An iterator that provides access to the values in the view and allows to modify them.
+         */
+        typedef T* value_iterator;
+
+        /**
+         * An iterator that provides access to the indices in the view and allows to modify them.
+         */
+        typedef uint32* index_iterator;
+
+        /**
+         * Returns a `value_iterator` to the beginning of the values at a specific column.
+         *
+         * @param col   The column
+         * @return      A `value_iterator` to the beginning of the values
+         */
+        value_iterator column_values_begin(uint32 col);
+
+        /**
+         * Returns a `value_iterator` to the end of the values at a specific column.
+         *
+         * @param col   The column
+         * @return      A `value_iterator` to the end of the values
+         */
+        value_iterator column_values_end(uint32 col);
+
+        /**
+         * Returns an `index_iterator` to the beginning of the indices at a specific column.
+         *
+         * @param col   The column
+         * @return      An `index_iterator` to the beginning of the indices
+         */
+        index_iterator column_indices_begin(uint32 col);
+
+        /**
+         * Returns an `index_iterator` to the end of the indices at a specific column.
+         *
+         * @param col   The column
+         * @return      An `index_iterator` to the end of the indices
+         */
+        index_iterator column_indices_end(uint32 col);
 
 };
--- cpp/subprojects/common/src/common/data/view_csc.cpp
+++ cpp/subprojects/common/src/common/data/view_csc.cpp
@@ -2,64 +2,73 @@
 
 
 template<class T>
-CscView<T>::CscView(uint32 numRows, uint32 numCols, T* data, uint32* rowIndices, uint32* colIndices)
+CscConstView<T>::CscConstView(uint32 numRows, uint32 numCols, T* data, uint32* rowIndices, uint32* colIndices)
     : numRows_(numRows), numCols_(numCols), data_(data), rowIndices_(rowIndices), colIndices_(colIndices) {
 
 }
 
 template<class T>
-typename CscView<T>::value_iterator CscView<T>::column_values_begin(uint32 col) {
+typename CscConstView<T>::value_const_iterator CscConstView<T>::column_values_cbegin(uint32 col) const {
     return &data_[colIndices_[col]];
 }
 
 template<class T>
-typename CscView<T>::value_iterator CscView<T>::column_values_end(uint32 col) {
+typename CscConstView<T>::value_const_iterator CscConstView<T>::column_values_cend(uint32 col) const {
     return &data_[colIndices_[col + 1]];
 }
 
 template<class T>
-typename CscView<T>::value_const_iterator CscView<T>::column_values_cbegin(uint32 col) const {
-    return &data_[colIndices_[col]];
-}
-
-template<class T>
-typename CscView<T>::value_const_iterator CscView<T>::column_values_cend(uint32 col) const {
-    return &data_[colIndices_[col + 1]];
-}
-
-template<class T>
-typename CscView<T>::index_iterator CscView<T>::column_indices_begin(uint32 col) {
+typename CscConstView<T>::index_const_iterator CscConstView<T>::column_indices_cbegin(uint32 col) const {
     return &rowIndices_[colIndices_[col]];
 }
 
 template<class T>
-typename CscView<T>::index_iterator CscView<T>::column_indices_end(uint32 col) {
+typename CscConstView<T>::index_const_iterator CscConstView<T>::column_indices_cend(uint32 col) const {
     return &rowIndices_[colIndices_[col + 1]];
 }
 
 template<class T>
-typename CscView<T>::index_const_iterator CscView<T>::column_indices_cbegin(uint32 col) const {
-    return &rowIndices_[colIndices_[col]];
-}
-
-template<class T>
-typename CscView<T>::index_const_iterator CscView<T>::column_indices_cend(uint32 col) const {
-    return &rowIndices_[colIndices_[col + 1]];
-}
-
-template<class T>
-uint32 CscView<T>::getNumRows() const {
+uint32 CscConstView<T>::getNumRows() const {
     return numRows_;
 }
 
 template<class T>
-uint32 CscView<T>::getNumCols() const {
+uint32 CscConstView<T>::getNumCols() const {
     return numCols_;
 }
 
 template<class T>
-uint32 CscView<T>::getNumNonZeroElements() const {
+uint32 CscConstView<T>::getNumNonZeroElements() const {
     return colIndices_[numCols_];
+}
+
+template class CscConstView<float32>;
+template class CscConstView<const float32>;
+
+template<class T>
+CscView<T>::CscView(uint32 numRows, uint32 numCols, T* data, uint32* rowIndices, uint32* colIndices)
+    : CscConstView<T>(numRows, numCols, data, rowIndices, colIndices) {
+
+}
+
+template<class T>
+typename CscView<T>::value_iterator CscView<T>::column_values_begin(uint32 col) {
+    return &CscConstView<T>::data_[CscConstView<T>::colIndices_[col]];
+}
+
+template<class T>
+typename CscView<T>::value_iterator CscView<T>::column_values_end(uint32 col) {
+    return &CscConstView<T>::data_[CscConstView<T>::colIndices_[col + 1]];
+}
+
+template<class T>
+typename CscView<T>::index_iterator CscView<T>::column_indices_begin(uint32 col) {
+    return &CscConstView<T>::rowIndices_[CscConstView<T>::colIndices_[col]];
+}
+
+template<class T>
+typename CscView<T>::index_iterator CscView<T>::column_indices_end(uint32 col) {
+    return &CscConstView<T>::rowIndices_[CscConstView<T>::colIndices_[col + 1]];
 }
 
 template class CscView<float32>;
