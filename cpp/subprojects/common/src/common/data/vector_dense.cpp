7ece2381338ccc4668ee3c195f09ec5faaa60137
Michael Rapp <michael.rapp90@googlemail.com>
2021-04-21 23:54:05 +0200

    Add class VectorConstView.

--- cpp/subprojects/common/src/common/data/vector_dense.cpp
+++ cpp/subprojects/common/src/common/data/vector_dense.cpp
@@ -18,22 +18,22 @@ DenseVector<T>::DenseVector(uint32 numElements, bool init)
 
 template<class T>
 DenseVector<T>::~DenseVector() {
-    free(VectorView<T>::array_);
+    free(this->array_);
 }
 
 template<class T>
 void DenseVector<T>::setNumElements(uint32 numElements, bool freeMemory) {
     if (numElements < maxCapacity_) {
         if (freeMemory) {
-            VectorView<T>::array_ = (T*) realloc(VectorView<T>::array_, numElements * sizeof(T));
+            this->array_ = (T*) realloc(this->array_, numElements * sizeof(T));
             maxCapacity_ = numElements;
         }
     } else if (numElements > maxCapacity_) {
-        VectorView<T>::array_ = (T*) realloc(VectorView<T>::array_, numElements * sizeof(T));
+        this->array_ = (T*) realloc(this->array_, numElements * sizeof(T));
         maxCapacity_ = numElements;
     }
 
-    VectorView<T>::numElements_ = numElements;
+    this->numElements_ = numElements;
 }
 
 template class DenseVector<uint8>;
