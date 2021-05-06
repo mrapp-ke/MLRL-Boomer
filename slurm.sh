f7c57674fbf1754342f136c206e2945e034cb506
Andreas Seidl Fernandez <aseidlfernandez@gmail.com>
2021-05-06 15:44:23 +0200

    added slurm.sh for execution on cluster

---
+++ slurm.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of arguments!"
+    exit -1
+fi
+
+# Command line arguments
+DATASET=$1
+HEURISTIC=$2
+PRUNING_HEURISTIC=$3
+
+# Constants
+MEMORY=2048
+CORES=1
+LOG_LEVEL="ERROR"
+MAX_RULES=500
+FOLDS=10
+INSTANCE_SUB_SAMPLING="random-instance-selection"
+HEAD_REFINEMENT="single-label"
+TIME_LIMIT_HOURS=24
+TIME_LIMIT_SECONDS=$((${TIME_LIMIT_HOURS}*3600))
+
+# Paths
+ROOT_DIR="${PWD}"
+DATA_DIR="${ROOT_DIR}/data"
+SUB_DIR="${DATASET}_pruning_${HEURISTIC}_${PRUNING_HEURISTIC}"
+LOG_DIR="${ROOT_DIR}/results/${SUB_DIR}/logs"
+OUTPUT_DIR="${ROOT_DIR}/results/${SUB_DIR}/evaluation"
+MODEL_DIR="${ROOT_DIR}/models/${SUB_DIR}"
+WORK_DIR="${LOG_DIR}"
+
+# Create directories
+echo "Creating directory ${LOG_DIR}"
+mkdir -p "${LOG_DIR}"
+echo "Creating directory ${MODEL_DIR}"
+mkdir -p "${MODEL_DIR}"
+echo "Creating directory ${OUTPUT_DIR}"
+mkdir -p "${OUTPUT_DIR}"
+
+FILE="${SUB_DIR}.sh"
+PARAMETERS="--log-level ${LOG_LEVEL} --data-dir ${DATA_DIR} --dataset ${DATASET} --model-dir ${MODEL_DIR} --output-dir ${OUTPUT_DIR} --folds ${FOLDS} --current-fold \$SLURM_ARRAY_TASK_ID --max-rules ${MAX_RULES} --time-limit ${TIME_LIMIT_SECONDS} --instance-sub-sampling ${INSTANCE_SUB_SAMPLING} --heuristic ${HEURISTIC} --pruning-heuristic ${PRUNING_HEURISTIC} --head-refinement ${HEAD_REFINEMENT}"
+
+echo "$FILE"
+echo "#!/bin/bash" >> "$FILE"
+echo "#SBATCH -a 1-${FOLDS}" >> "$FILE"
+echo "#SBATCH -J ${SUB_DIR}_fold=%a" >> "$FILE"
+echo "#SBATCH -D ${WORK_DIR}" >> "$FILE"
+echo "#SBATCH -t ${TIME_LIMIT_HOURS}:00:00" >> "$FILE"
+echo "#SBATCH -n 1" >> "$FILE"
+echo "#SBATCH -c ${CORES}" >> "$FILE"
+echo "#SBATCH -o fold_%a.log" >> "$FILE"
+echo "#SBATCH -e fold_%a.err" >> "$FILE"
+echo "#SBATCH --mem-per-cpu=${MEMORY}" >> "$FILE"
+echo "${ROOT_DIR}/venv/bin/python3 ${ROOT_DIR}/python/main_seco.py ${PARAMETERS}" >> "$FILE"
+
+# Run SLURM jobs
+sbatch "$FILE"
+rm "$FILE"
+echo "Started array of experiments with parameters ${PARAMETERS}"
+
