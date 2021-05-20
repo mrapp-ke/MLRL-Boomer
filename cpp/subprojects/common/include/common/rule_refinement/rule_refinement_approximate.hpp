2bdfd6841832ecc7c5d69e874ed99b42300ba1e2
Michael Rapp <michael.rapp90@googlemail.com>
2021-05-20 23:45:12 +0200

    Use a BitVector to store the weights of bins.

--- cpp/subprojects/common/include/common/rule_refinement/rule_refinement_approximate.hpp
+++ cpp/subprojects/common/include/common/rule_refinement/rule_refinement_approximate.hpp
@@ -4,6 +4,7 @@
  */
 #pragma once
 
+#include "common/data/vector_bit.hpp"
 #include "common/rule_refinement/rule_refinement.hpp"
 #include "common/rule_refinement/rule_refinement_callback.hpp"
 #include "common/binning/threshold_vector.hpp"
@@ -14,7 +15,7 @@
 /**
  * A vector that stores the weights of individual bins.
  */
-typedef DenseVector<uint8> BinWeightVector;
+typedef BitVector BinWeightVector;
 
 /**
  * Allows to find the best refinements of existing rules, which result from adding a new condition that correspond to a
