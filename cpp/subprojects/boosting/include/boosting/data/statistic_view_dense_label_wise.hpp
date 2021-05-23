120288f0d7538def8f670cd807ed47626ce1f953
Michael Rapp <michael.rapp90@googlemail.com>
2021-05-23 15:42:38 +0200

    Add parent classes.

--- cpp/subprojects/boosting/include/boosting/data/statistic_view_dense_label_wise.hpp
+++ cpp/subprojects/boosting/include/boosting/data/statistic_view_dense_label_wise.hpp
@@ -12,9 +12,9 @@ namespace boosting {
      * Implements row-wise read-only access to the gradients and Hessians that have been calculated using a label-wise
      * decomposable loss function and are stored in pre-allocated C-contiguous arrays.
      */
-    class DenseLabelWiseStatisticView final {
+    class DenseLabelWiseStatisticConstView {
 
-        private:
+        protected:
 
             /**
              * The number of rows in the view.
@@ -39,25 +39,14 @@ namespace boosting {
         public:
 
             /**
-             * @param numRows   The number of rows in the matrix
-             * @param numCols   The number of columns in the matrix
+             * @param numRows   The number of rows in the view
+             * @param numCols   The number of columns in the view
+             * @param gradients A pointer to a C-contiguous array of type `float64` that stores the gradients, the view
+             *                  provides access to
+             * @param hessians  A pointer to a C-contiguous array of type `float64` that stores the Hessians, the view
+             *                  provides access to
              */
-            DenseLabelWiseStatisticView(uint32 numRows, uint32 numCols);
-
-            /**
-             * @param numRows   The number of rows in the matrix
-             * @param numCols   The number of columns in the matrix
-             * @param init      True, if all gradients and Hessians in the matrix should be initialized with zero, false
-             *                  otherwise
-             */
-            DenseLabelWiseStatisticView(uint32 numRows, uint32 numCols, bool init);
-
-            ~DenseLabelWiseStatisticView();
-
-            /**
-             * An iterator that provides access to the gradients and allows to modify them.
-             */
-            typedef float64* gradient_iterator;
+            DenseLabelWiseStatisticConstView(uint32 numRows, uint32 numCols, float64* gradients, float64* hessians);
 
             /**
              * An iterator that provides read-only access to the gradients.
@@ -65,30 +54,9 @@ namespace boosting {
             typedef const float64* gradient_const_iterator;
 
             /**
-             * An iterator that provides access to the Hessians and allows to modify them.
-             */
-            typedef float64* hessian_iterator;
-
-            /**
              * An iterator that provides read-only access to the Hessians.
              */
             typedef const float64* hessian_const_iterator;
-
-            /**
-             * Returns a `gradient_iterator` to the beginning of the gradients at a specific row.
-             *
-             * @param row   The row
-             * @return      A `gradient_iterator` to the beginning of the given row
-             */
-            gradient_iterator gradients_row_begin(uint32 row);
-
-            /**
-             * Returns a `gradient_iterator` to the end of the gradients at a specific row.
-             *
-             * @param row   The row
-             * @return      A `gradient_iterator` to the end of the given row
-             */
-            gradient_iterator gradients_row_end(uint32 row);
 
             /**
              * Returns a `gradient_const_iterator` to the beginning of the gradients at a specific row.
@@ -105,22 +73,6 @@ namespace boosting {
              * @return      A `gradient_const_iterator` to the end of the given row
              */
             gradient_const_iterator gradients_row_cend(uint32 row) const;
-
-            /**
-             * Returns a `hessian_iterator` to the beginning of the Hessians at a specific row.
-             *
-             * @param row   The row
-             * @return      A `hessian_iterator` to the beginning of the given row
-             */
-            hessian_iterator hessians_row_begin(uint32 row);
-
-            /**
-             * Returns a `hessian_iterator` to the end of the Hessians at a specific row.
-             *
-             * @param row   The row
-             * @return      A `hessian_iterator` to the end of the given row
-             */
-            hessian_iterator hessians_row_end(uint32 row);
 
             /**
              * Returns a `hessian_const_iterator` to the beginning of the Hessians at a specific row.
@@ -151,6 +103,74 @@ namespace boosting {
              * @return The number of gradients
              */
             uint32 getNumCols() const;
+
+    };
+
+    /**
+     * Implements row-wise read and write access to the gradients and Hessians that have been calculated using a
+     * label-wise decomposable loss function and are stored in pre-allocated C-contiguous arrays.
+     */
+    class DenseLabelWiseStatisticView final : public DenseLabelWiseStatisticConstView {
+
+        public:
+
+            /**
+             * @param numRows   The number of rows in the view
+             * @param numCols   The number of columns in the view
+             */
+            DenseLabelWiseStatisticView(uint32 numRows, uint32 numCols);
+
+            /**
+             * @param numRows   The number of rows in the view
+             * @param numCols   The number of columns in the view
+             * @param init      True, if all gradients and Hessians in the matrix should be initialized with zero, false
+             *                  otherwise
+             */
+            DenseLabelWiseStatisticView(uint32 numRows, uint32 numCols, bool init);
+
+            ~DenseLabelWiseStatisticView();
+
+            /**
+             * An iterator that provides access to the gradients and allows to modify them.
+             */
+            typedef float64* gradient_iterator;
+
+            /**
+             * An iterator that provides access to the Hessians and allows to modify them.
+             */
+            typedef float64* hessian_iterator;
+
+            /**
+             * Returns a `gradient_iterator` to the beginning of the gradients at a specific row.
+             *
+             * @param row   The row
+             * @return      A `gradient_iterator` to the beginning of the given row
+             */
+            gradient_iterator gradients_row_begin(uint32 row);
+
+            /**
+             * Returns a `gradient_iterator` to the end of the gradients at a specific row.
+             *
+             * @param row   The row
+             * @return      A `gradient_iterator` to the end of the given row
+             */
+            gradient_iterator gradients_row_end(uint32 row);
+
+            /**
+             * Returns a `hessian_iterator` to the beginning of the Hessians at a specific row.
+             *
+             * @param row   The row
+             * @return      A `hessian_iterator` to the beginning of the given row
+             */
+            hessian_iterator hessians_row_begin(uint32 row);
+
+            /**
+             * Returns a `hessian_iterator` to the end of the Hessians at a specific row.
+             *
+             * @param row   The row
+             * @return      A `hessian_iterator` to the end of the given row
+             */
+            hessian_iterator hessians_row_end(uint32 row);
 
             /**
              * Sets all gradients and Hessians in the matrix to zero.
