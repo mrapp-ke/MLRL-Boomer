3549dc425cf22e11678abe68ea429f541571095a
Michael Rapp <michael.rapp90@googlemail.com>
2021-06-29 16:13:59 +0200

    Rename value "bagging" of the parameter "--instance-sampling" to "with-replacement".

--- python/args.py
+++ python/args.py
@@ -12,7 +12,7 @@ from ast import literal_eval
 import sklearn.metrics as metrics
 
 from mlrl.boosting.boosting_learners import LOSS_LABEL_WISE_LOGISTIC
-from mlrl.common.rule_learners import INSTANCE_SAMPLING_BAGGING, FEATURE_SAMPLING_RANDOM
+from mlrl.common.rule_learners import INSTANCE_SAMPLING_WITH_REPLACEMENT, FEATURE_SAMPLING_RANDOM
 from mlrl.seco.seco_learners import HEURISTIC_PRECISION, LIFT_FUNCTION_PEAK, AVERAGING_LABEL_WISE
 
 
@@ -196,7 +196,7 @@ class ArgumentParserBuilder:
 
     def add_boosting_learner_arguments(self, **kwargs) -> 'ArgumentParserBuilder':
         self.add_rule_learner_arguments(LOSS_LABEL_WISE_LOGISTIC, max_rules=1000,
-                                        instance_sampling=INSTANCE_SAMPLING_BAGGING,
+                                        instance_sampling=INSTANCE_SAMPLING_WITH_REPLACEMENT,
                                         feature_sampling=FEATURE_SAMPLING_RANDOM, **kwargs)
         parser = self.parser
         parser.add_argument('--default-rule', type=boolean_string,
