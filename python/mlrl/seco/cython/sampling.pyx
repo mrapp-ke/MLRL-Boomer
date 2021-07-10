8e550e844066e336059dc48889ca240fcaf754fa
Michael Rapp <michael.rapp90@googlemail.com>
2021-07-10 14:08:17 +0200

    Add Cython wrapper.

--- python/mlrl/seco/cython/sampling.pyx
+++ python/mlrl/seco/cython/sampling.pyx
@@ -11,3 +11,17 @@ cdef class NoInstanceSamplingFactory(InstanceSamplingFactory):
 
     def __cinit__(self):
         self.instance_sampling_factory_ptr = <shared_ptr[IInstanceSamplingFactory]>make_shared[NoInstanceSamplingFactoryImpl]()
+
+
+cdef class InstanceSamplingWithoutReplacementFactory(InstanceSamplingFactory):
+    """
+    A wrapper for the C++ class `InstanceSamplingWithoutReplacementFactory`.
+    """
+
+    def __cinit__(self, float32 sample_size):
+        """
+        :param sample_size: The fraction of examples to be included in the sample (e.g. a value of 0.6 corresponds to
+                            60 % of the available examples). Must be in (0, 1]
+        """
+        self.instance_sampling_factory_ptr = <shared_ptr[IInstanceSamplingFactory]>make_shared[InstanceSamplingWithoutReplacementFactoryImpl](
+            sample_size)
