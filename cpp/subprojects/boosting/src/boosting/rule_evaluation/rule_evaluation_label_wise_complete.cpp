ea47955a77e72743ff19a98d2cd8530fdbcf5109
Michael Rapp <michael.rapp90@googlemail.com>
2021-07-16 01:54:41 +0200

    Add class LabelWiseCompleteRuleEvaluationFactory.

---
+++ cpp/subprojects/boosting/src/boosting/rule_evaluation/rule_evaluation_label_wise_complete.cpp
@@ -0,0 +1,66 @@
+#include "boosting/rule_evaluation/rule_evaluation_label_wise_complete.hpp"
+#include "common/rule_evaluation/score_vector_dense.hpp"
+#include "rule_evaluation_label_wise_common.hpp"
+
+
+namespace boosting {
+
+    /**
+     * Allows to calculate the predictions of complete rules, as well as an overall quality score, based on the
+     * gradients and Hessians that are stored by a `DenseLabelWiseStatisticVector` using L2 regularization.
+     *
+     * @tparam T The type of the vector that provides access to the labels for which predictions should be calculated
+     */
+    template<typename T>
+    class LabelWiseCompleteRuleEvaluation final : public ILabelWiseRuleEvaluation<DenseLabelWiseStatisticVector> {
+
+        private:
+
+            DenseScoreVector<T> scoreVector_;
+
+            float64 l2RegularizationWeight_;
+
+        public:
+
+            /**
+             * @param labelIndices              A reference to an object of template type `T` that provides access to
+             *                                  the indices of the labels for which the rules may predict
+             * @param l2RegularizationWeight    The weight of the L2 regularization that is applied for calculating the
+             *                                  scores to be predicted by rules
+             */
+            LabelWiseCompleteRuleEvaluation(const T& labelIndices, float64 l2RegularizationWeight)
+                : scoreVector_(DenseScoreVector<T>(labelIndices)), l2RegularizationWeight_(l2RegularizationWeight) {
+
+            }
+
+            const ILabelWiseScoreVector& calculateLabelWisePrediction(
+                    const DenseLabelWiseStatisticVector& statisticVector) override {
+
+            }
+
+            const IScoreVector& calculatePrediction(const DenseLabelWiseStatisticVector& statisticVector) override {
+                // TODO Calculate scores
+                scoreVector_.overallQualityScore = 0; // TODO Calculate quality score
+                return scoreVector_;
+            }
+
+    };
+
+    LabelWiseCompleteRuleEvaluationFactory::LabelWiseCompleteRuleEvaluationFactory(float64 l2RegularizationWeight)
+        : l2RegularizationWeight_(l2RegularizationWeight) {
+
+    }
+
+    std::unique_ptr<ILabelWiseRuleEvaluation<DenseLabelWiseStatisticVector>> LabelWiseCompleteRuleEvaluationFactory::createDense(
+            const CompleteIndexVector& indexVector) const {
+        return std::make_unique<LabelWiseCompleteRuleEvaluation<CompleteIndexVector>>(indexVector,
+                                                                                      l2RegularizationWeight_);
+    }
+
+    std::unique_ptr<ILabelWiseRuleEvaluation<DenseLabelWiseStatisticVector>> LabelWiseCompleteRuleEvaluationFactory::createDense(
+            const PartialIndexVector& indexVector) const {
+        return std::make_unique<LabelWiseCompleteRuleEvaluation<PartialIndexVector>>(indexVector,
+                                                                                     l2RegularizationWeight_);
+    }
+
+}
