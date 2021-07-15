ce3a1cc6d5b85945e5dfcea91c5a3eccec6e3cb7
Michael Rapp <michael.rapp90@googlemail.com>
2021-07-16 01:40:53 +0200

    Add comment.

--- cpp/subprojects/boosting/src/boosting/rule_evaluation/rule_evaluation_label_wise_common.hpp
+++ cpp/subprojects/boosting/src/boosting/rule_evaluation/rule_evaluation_label_wise_common.hpp
@@ -8,6 +8,15 @@
 
 namespace boosting {
 
+    /**
+     * Calculates and returns the optimal score to be predicted for a single label, based on the corresponding gradient
+     * and Hessian and taking L2 regularization into account.
+     *
+     * @param gradient                  The gradient that corresponds to the label
+     * @param hessian                   The Hessian that corresponds to the label
+     * @param l2RegularizationWeight    The weight of the L2 regularization
+     * @return                          The score that has been calculated
+     */
     static inline constexpr float64 calculateLabelWiseScore(float64 gradient, float64 hessian,
                                                             float64 l2RegularizationWeight) {
         return divideOrZero(-gradient, hessian + l2RegularizationWeight);
