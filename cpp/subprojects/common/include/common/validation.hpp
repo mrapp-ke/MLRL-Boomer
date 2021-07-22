368674ec79cc0d02ecaa8620e32a5064809bdfd4
Michael Rapp <michael.rapp90@googlemail.com>
2021-07-23 00:37:07 +0200

    Add function "assertMultiple".

--- cpp/subprojects/common/include/common/validation.hpp
+++ cpp/subprojects/common/include/common/validation.hpp
@@ -65,3 +65,18 @@ static inline constexpr void assertSmallerOrEqual(const T value, const T thresho
                                     + std::to_string(value));
     }
 }
+
+/**
+ * Throws an exception if a given value is not a multiple of another value.
+ *
+ * @tparam T    The type of the values
+ * @param value The value that should be a multiple of `other`
+ * @param other The other value
+ */
+template<typename T>
+static inline constexpr void assertMultiple(const T value, const T other) {
+    if (value % other != 0) {
+        throw std::invalid_argument("Value must be a multiple of " + std::to_string(other) + ", but is "
+                                    + std::to_string(value));
+    }
+}
