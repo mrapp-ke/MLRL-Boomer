63db290d7ad20f20f611175d668425ca833bdcb4
Michael Rapp <michael.rapp90@googlemail.com>
2021-07-22 23:12:41 +0200

    Add file validation.hpp.

---
+++ cpp/subprojects/common/include/common/validation.hpp
@@ -0,0 +1,35 @@
+/*
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ */
+#pragma once
+
+#include <stdexcept>
+
+
+/**
+ * Throws an exception if a given value is not greater than a threshold.
+ *
+ * @param value     The value
+ * @param threshold The threshold
+ */
+template<typename T>
+static inline constexpr void assertGreater(const T value, const T threshold) {
+    if (value <= threshold) {
+        throw std::invalid_argument("Value must be greater than " + std::to_string(threshold) + ", but is "
+                                    + std::to_string(value));
+    }
+}
+
+/**
+ * Throws an exception if a given value is not smaller than a threshold.
+ *
+ * @param value     The value
+ * @param threshold The threshold
+ */
+template<typename T>
+static inline constexpr void assertSmaller(const T value, const T threshold) {
+    if (value >= threshold) {
+        throw std::invalid_argument("Value must be smaller than " + std::to_string(threshold) + ", but is "
+                                    + std::to_string(value));
+    }
+}
