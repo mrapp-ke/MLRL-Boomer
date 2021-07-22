912562355b97f85e46a3755f467eff2654c009bb
Michael Rapp <michael.rapp90@googlemail.com>
2021-07-22 23:50:15 +0200

    Add validation to class PeakLiftFunction.

--- cpp/subprojects/seco/src/seco/head_refinement/lift_function_peak.cpp
+++ cpp/subprojects/seco/src/seco/head_refinement/lift_function_peak.cpp
@@ -1,4 +1,5 @@
 #include "seco/head_refinement/lift_function_peak.hpp"
+#include "common/validation.hpp"
 #include <cmath>
 
 
@@ -6,7 +7,11 @@ namespace seco {
 
     PeakLiftFunction::PeakLiftFunction(uint32 numLabels, uint32 peakLabel, float64 maxLift, float64 curvature)
         : numLabels_(numLabels), peakLabel_(peakLabel), maxLift_(maxLift), exponent_(1.0 / curvature) {
-
+        assertGreater<uint32>(numLabels, 0);
+        assertGreaterOrEqual<uint32>(peakLabel, 0);
+        assertSmallerOrEqual<uint32>(peakLabel, numLabels);
+        assertGreaterOrEqual<float64>(maxLift, 1);
+        assertGreater<float64>(curvature, 0);
     }
 
     float64 PeakLiftFunction::calculateLift(uint32 numLabels) const {
