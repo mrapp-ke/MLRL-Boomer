8aec1dd1c4e4b6cec481b626027657167aa62bb0
Michael Rapp <michael.rapp90@googlemail.com>
2021-07-22 23:21:58 +0200

    Add function "assertGreaterOrEqual".

--- cpp/subprojects/common/include/common/validation.hpp
+++ cpp/subprojects/common/include/common/validation.hpp
@@ -22,6 +22,21 @@ static inline constexpr void assertGreater(const T value, const T threshold) {
 }
 
 /**
+ * Throws an exception if a given value not greater or equal to a specific threshold.
+ *
+ * @tparam T        The type of the value and threshold
+ * @param value     The value
+ * @param threshold The threshold
+ */
+template<typename T>
+static inline constexpr void assertGreaterOrEqual(const T value, const T threshold) {
+    if (value < threshold) {
+        throw std::invalid_argument("Value must be greater or equal to " + std::to_string(threshold) + ", but is "
+                                    + std::to_string(value));
+    }
+}
+
+/**
  * Throws an exception if a given value is not smaller than a specific threshold.
  *
  * @tparam T        The type of the value and threshold
