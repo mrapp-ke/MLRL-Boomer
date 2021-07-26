7615cc4dec9d53420a0b3d4c2a6435e1e7a2f0f9
Michael Rapp <michael.rapp90@googlemail.com>
2021-07-26 13:07:45 +0200

    Rename class SeparateAndConquerRuleLearner to SeCoRuleLearner.

--- python/main_seco.py
+++ python/main_seco.py
@@ -4,32 +4,29 @@
 Author: Michael Rapp (mrapp@ke.tu-darmstadt.de)
 """
 from args import ArgumentParserBuilder
-from mlrl.seco.seco_learners import SeparateAndConquerRuleLearner
+from mlrl.seco.seco_learners import SeCoRuleLearner
 from runnables import RuleLearnerRunnable
 
 
-class SecoRunnable(RuleLearnerRunnable):
+class SeCoRunnable(RuleLearnerRunnable):
 
     def _create_learner(self, args):
-        return SeparateAndConquerRuleLearner(random_state=args.random_state, feature_format=args.feature_format,
-                                             label_format=args.label_format, max_rules=args.max_rules,
-                                             time_limit=args.time_limit, heuristic=args.heuristic,
-                                             pruning_heuristic=args.pruning_heuristic, pruning=args.pruning,
-                                             label_sampling=args.label_sampling,
-                                             instance_sampling=args.instance_sampling,
-                                             feature_sampling=args.feature_sampling, holdout=args.holdout,
-                                             feature_binning=args.feature_binning, head_type=args.head_type,
-                                             min_coverage=args.min_coverage, max_conditions=args.max_conditions,
-                                             lift_function=args.lift_function,
-                                             max_head_refinements=args.max_head_refinements,
-                                             num_threads_rule_refinement=args.num_threads_rule_refinement,
-                                             num_threads_statistic_update=args.num_threads_statistic_update,
-                                             num_threads_prediction=args.num_threads_prediction)
+        return SeCoRuleLearner(random_state=args.random_state, feature_format=args.feature_format,
+                               label_format=args.label_format, max_rules=args.max_rules, time_limit=args.time_limit,
+                               heuristic=args.heuristic, pruning_heuristic=args.pruning_heuristic, pruning=args.pruning,
+                               label_sampling=args.label_sampling, instance_sampling=args.instance_sampling,
+                               feature_sampling=args.feature_sampling, holdout=args.holdout,
+                               feature_binning=args.feature_binning, head_type=args.head_type,
+                               min_coverage=args.min_coverage, max_conditions=args.max_conditions,
+                               lift_function=args.lift_function, max_head_refinements=args.max_head_refinements,
+                               num_threads_rule_refinement=args.num_threads_rule_refinement,
+                               num_threads_statistic_update=args.num_threads_statistic_update,
+                               num_threads_prediction=args.num_threads_prediction)
 
 
 if __name__ == '__main__':
     parser = ArgumentParserBuilder(description='A multi-label classification experiment using Separate and Conquer') \
         .add_seco_learner_arguments() \
         .build()
-    runnable = SecoRunnable()
+    runnable = SeCoRunnable()
     runnable.run(parser)
