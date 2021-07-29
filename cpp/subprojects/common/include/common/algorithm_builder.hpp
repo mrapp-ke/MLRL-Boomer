75fac4ae39eef300d5099202c6c3782ae51543a8
Michael Rapp <michael.rapp90@googlemail.com>
2021-07-29 02:20:44 +0200

    Add class AlgorithmBuilder.

---
+++ cpp/subprojects/common/include/common/algorithm_builder.hpp
@@ -0,0 +1,124 @@
+/*
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ */
+#pragma once
+
+#include "common/rule_induction/rule_model_assemblage.hpp"
+
+
+/**
+ * TODO
+ */
+class AlgorithmBuilder final {
+
+    private:
+
+        std::shared_ptr<IStatisticsProviderFactory> statisticsProviderFactoryPtr_;
+
+        std::shared_ptr<IThresholdsFactory> thresholdsFactoryPtr_;
+
+        std::shared_ptr<IRuleInduction> ruleInductionPtr_;
+
+        std::shared_ptr<IHeadRefinementFactory> defaultRuleHeadRefinementFactoryPtr_;
+
+        std::shared_ptr<IHeadRefinementFactory> regularRuleHeadRefinementFactoryPtr_;
+
+        std::shared_ptr<IRuleModelAssemblageFactory> ruleModelAssemblageFactoryPtr_;
+
+        std::shared_ptr<ILabelSamplingFactory> labelSamplingFactoryPtr_;
+
+        std::shared_ptr<IInstanceSamplingFactory> instanceSamplingFactoryPtr_;
+
+        std::shared_ptr<IFeatureSamplingFactory> featureSamplingFactoryPtr_;
+
+        std::shared_ptr<IPartitionSamplingFactory> partitionSamplingFactoryPtr_;
+
+        std::shared_ptr<IPruning> pruningPtr_;
+
+        std::shared_ptr<IPostProcessor> postProcessorPtr_;
+
+        std::forward_list<std::shared_ptr<IStoppingCriterion>> stoppingCriteria_;
+
+    public:
+
+        /**
+         * @param statisticsProviderFactoryPtr          TODO
+         * @param thresholdsFactoryPtr                  TODO
+         * @param ruleInductionPtr                      TODO
+         * @param defaultRuleHeadRefinementFactoryPtr   TODO
+         * @param regularRuleHeadRefinementFactoryPtr   TODO
+         * @param ruleModelAssemblageFactoryPtr         TODO
+         */
+        AlgorithmBuilder(std::shared_ptr<IStatisticsProviderFactory> statisticsProviderFactoryPtr,
+                         std::shared_ptr<IThresholdsFactory> thresholdsFactoryPtr,
+                         std::shared_ptr<IRuleInduction> ruleInductionPtr,
+                         std::shared_ptr<IHeadRefinementFactory> defaultRuleHeadRefinementFactoryPtr,
+                         std::shared_ptr<IHeadRefinementFactory> regularRuleHeadRefinementFactoryPtr,
+                         std::shared_ptr<IRuleModelAssemblageFactory> ruleModelAssemblageFactoryPtr);
+
+        /**
+         * TODO
+         *
+         * @param labelSamplingFactoryPtr   TODO
+         * @return                          TODO
+         */
+        AlgorithmBuilder& setLabelSamplingFactory(std::shared_ptr<ILabelSamplingFactory> labelSamplingFactoryPtr);
+
+        /**
+         * TODO
+         *
+         * @param instanceSamplingFactoryPtr    TODO
+         * @return                              TODO
+         */
+        AlgorithmBuilder& setInstanceSamplingFactory(
+            std::shared_ptr<IInstanceSamplingFactory> instanceSamplingFactoryPtr);
+
+        /**
+         * TODO
+         *
+         * @param featureSamplingFactoryPtr TODO
+         * @return                          TODO
+         */
+        AlgorithmBuilder& setFeatureSamplingFactory(std::shared_ptr<IFeatureSamplingFactory> featureSamplingFactoryPtr);
+
+        /**
+         * TODO
+         *
+         * @param partitionSamplingFactoryPtr   TODO
+         * @return                              TODO
+         */
+        AlgorithmBuilder& setPartitionSamplingFactory(
+            std::shared_ptr<IPartitionSamplingFactory> partitionSamplingFactoryPtr);
+
+        /**
+         * TODO
+         *
+         * @param pruningPtr    TODO
+         * @return              TODO
+         */
+        AlgorithmBuilder& setPruning(std::shared_ptr<IPruning> pruningPtr);
+
+        /**
+         * TODO
+         *
+         * @param postProcessorPtr  TODO
+         * @return                  TODO
+         */
+        AlgorithmBuilder& setPostProcessor(std::shared_ptr<IPostProcessor> postProcessorPtr);
+
+        /**
+         * TODO
+         *
+         * @param stoppingCriterionPtr  TODO
+         * @return                      TODO
+         */
+        AlgorithmBuilder& addStoppingCriterion(std::shared_ptr<IStoppingCriterion> stoppingCriterionPtr);
+
+        /**
+         * TODO
+         *
+         * @return TODO
+         */
+        std::unique_ptr<IRuleModelAssemblage> build() const;
+
+};
---
+++ cpp/subprojects/common/src/common/algorithm_builder.cpp
@@ -0,0 +1,74 @@
+#include "common/algorithm_builder.hpp"
+#include "common/post_processing/post_processor_no.hpp"
+#include "common/pruning/pruning_no.hpp"
+#include "common/sampling/feature_sampling_no.hpp"
+#include "common/sampling/instance_sampling_no.hpp"
+#include "common/sampling/label_sampling_no.hpp"
+#include "common/sampling/partition_sampling_no.hpp"
+
+
+AlgorithmBuilder::AlgorithmBuilder(std::shared_ptr<IStatisticsProviderFactory> statisticsProviderFactoryPtr,
+                                   std::shared_ptr<IThresholdsFactory> thresholdsFactoryPtr,
+                                   std::shared_ptr<IRuleInduction> ruleInductionPtr,
+                                   std::shared_ptr<IHeadRefinementFactory> defaultRuleHeadRefinementFactoryPtr,
+                                   std::shared_ptr<IHeadRefinementFactory> regularRuleHeadRefinementFactoryPtr,
+                                   std::shared_ptr<IRuleModelAssemblageFactory> ruleModelAssemblageFactoryPtr)
+    : statisticsProviderFactoryPtr_(statisticsProviderFactoryPtr), thresholdsFactoryPtr_(thresholdsFactoryPtr),
+      ruleInductionPtr_(ruleInductionPtr), defaultRuleHeadRefinementFactoryPtr_(defaultRuleHeadRefinementFactoryPtr),
+      regularRuleHeadRefinementFactoryPtr_(regularRuleHeadRefinementFactoryPtr),
+      ruleModelAssemblageFactoryPtr_(ruleModelAssemblageFactoryPtr),
+      labelSamplingFactoryPtr_(std::make_shared<NoLabelSamplingFactory>()),
+      instanceSamplingFactoryPtr_(std::make_shared<NoInstanceSamplingFactory>()),
+      featureSamplingFactoryPtr_(std::make_shared<NoFeatureSamplingFactory>()),
+      partitionSamplingFactoryPtr_(std::make_shared<NoPartitionSamplingFactory>()),
+      pruningPtr_(std::make_shared<NoPruning>()), postProcessorPtr_(std::make_shared<NoPostProcessor>()) {
+
+}
+
+AlgorithmBuilder& AlgorithmBuilder::setLabelSamplingFactory(
+        std::shared_ptr<ILabelSamplingFactory> labelSamplingFactoryPtr) {
+    labelSamplingFactoryPtr_ = labelSamplingFactoryPtr;
+    return *this;
+}
+
+AlgorithmBuilder& AlgorithmBuilder::setInstanceSamplingFactory(
+        std::shared_ptr<IInstanceSamplingFactory> instanceSamplingFactoryPtr) {
+    instanceSamplingFactoryPtr_ = instanceSamplingFactoryPtr;
+    return *this;
+}
+
+AlgorithmBuilder& AlgorithmBuilder::setFeatureSamplingFactory(
+        std::shared_ptr<IFeatureSamplingFactory> featureSamplingFactoryPtr) {
+    featureSamplingFactoryPtr_ = featureSamplingFactoryPtr;
+    return *this;
+}
+
+AlgorithmBuilder& AlgorithmBuilder::setPartitionSamplingFactory(
+        std::shared_ptr<IPartitionSamplingFactory> partitionSamplingFactoryPtr) {
+    partitionSamplingFactoryPtr_ = partitionSamplingFactoryPtr;
+    return *this;
+}
+
+AlgorithmBuilder& AlgorithmBuilder::setPruning(std::shared_ptr<IPruning> pruningPtr) {
+    pruningPtr_ = pruningPtr;
+    return *this;
+}
+
+AlgorithmBuilder& AlgorithmBuilder::setPostProcessor(std::shared_ptr<IPostProcessor> postProcessorPtr) {
+    postProcessorPtr_ = postProcessorPtr;
+    return *this;
+}
+
+AlgorithmBuilder& AlgorithmBuilder::addStoppingCriterion(std::shared_ptr<IStoppingCriterion> stoppingCriterionPtr) {
+    stoppingCriteria_.push_front(stoppingCriterionPtr);
+    return *this;
+}
+
+std::unique_ptr<IRuleModelAssemblage> AlgorithmBuilder::build() const {
+    return ruleModelAssemblageFactoryPtr_->create(statisticsProviderFactoryPtr_, thresholdsFactoryPtr_,
+                                                  ruleInductionPtr_, defaultRuleHeadRefinementFactoryPtr_,
+                                                  regularRuleHeadRefinementFactoryPtr_, labelSamplingFactoryPtr_,
+                                                  instanceSamplingFactoryPtr_, featureSamplingFactoryPtr_,
+                                                  partitionSamplingFactoryPtr_, pruningPtr_, postProcessorPtr_,
+                                                  stoppingCriteria_);
+}
