f914bb5af2610dbf2585a8639a0c782222886b30
Michael Rapp <michael.rapp90@googlemail.com>
2021-08-01 19:38:02 +0200

    Use unique pointers to store objects of type IStoppingCriterion.

--- python/mlrl/common/cython/algorithm_builder.pyx
+++ python/mlrl/common/cython/algorithm_builder.pyx
@@ -120,7 +120,7 @@ cdef class AlgorithmBuilder:
         :param stopping_criterion:  TODO
         :return:                    TODO
         """
-        self.builder_ptr.get().addStoppingCriterion(<shared_ptr[IStoppingCriterion]>move(stopping_criterion.stopping_criterion_ptr)) # TODO Remove cast
+        self.builder_ptr.get().addStoppingCriterion(move(stopping_criterion.stopping_criterion_ptr))
         return self
 
     def build(self) -> RuleModelAssemblage:
