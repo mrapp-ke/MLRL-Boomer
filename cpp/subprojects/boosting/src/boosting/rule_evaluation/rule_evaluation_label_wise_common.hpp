03e2a5eb0ff5902691c42bfed77f4a7b7aa01cda
Michael Rapp <michael.rapp90@googlemail.com>
2021-08-06 02:59:05 +0200

    Add function "calculateLabelWiseQualityScore".

--- cpp/subprojects/boosting/src/boosting/rule_evaluation/rule_evaluation_label_wise_common.hpp
+++ cpp/subprojects/boosting/src/boosting/rule_evaluation/rule_evaluation_label_wise_common.hpp
@@ -24,33 +24,18 @@ namespace boosting {
     }
 
     /**
-     * Calculates and returns an overall quality score that assesses the quality of the scores that are predicted for
-     * individual labels.
+     * Calculates and returns a quality score that assesses the quality of the score that is predicted for a single
+     * label.
      *
-     * @tparam ScoreIterator            The type of the iterator that provides access to the predicted scores
-     * @param scoreIterator             An iterator that provides random access to the predicted scores
-     * @param statisticIterator         An iterator that provides random access to the gradients and Hessians
-     * @param numElements               The number of predicted scores
-     * @param l2RegularizationWeight    The weight of the l2 regularization
-     * @return                          The overall quality score that has been calculated
+     * @param score                     The predicted score
+     * @param gradient                  The gradient
+     * @param hessian                   The Hessian
+     * @param l2RegularizationWeight    The weight of the L2 regularization
      */
-    template<typename ScoreIterator>
-    static inline constexpr float64 calculateLabelWiseOverallQualityScore(
-            ScoreIterator scoreIterator, DenseLabelWiseStatisticVector::const_iterator statisticIterator,
-            uint32 numElements, float64 l2RegularizationWeight) {
-        float64 overallQualityScore = 0;
-
-        for (uint32 i = 0; i < numElements; i++) {
-            const Tuple<float64>& tuple = statisticIterator[i];
-            float64 gradient = tuple.first;
-            float64 hessian = tuple.second;
-            float64 score = scoreIterator[i];
-            float64 scorePow = score * score;
-            overallQualityScore += (gradient * score) + (0.5 * scorePow * hessian)
-                                    + (0.5 * l2RegularizationWeight * scorePow);
-        }
-
-        return overallQualityScore;
+    static inline constexpr float64 calculateLabelWiseQualityScore(float64 score, float64 gradient, float64 hessian,
+                                                                   float64 l2RegularizationWeight) {
+        float64 scorePow = score * score;
+        return (gradient * score) + (0.5 * scorePow * hessian) + (0.5 * l2RegularizationWeight * scorePow);
     }
 
 
