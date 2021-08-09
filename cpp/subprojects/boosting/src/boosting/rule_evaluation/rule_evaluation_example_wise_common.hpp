e6e96b581a1a3984e739c82a4a418785c77b8521
Michael Rapp <michael.rapp90@googlemail.com>
2021-08-09 21:37:48 +0200

    Implement class ExampleWiseCompleteRuleEvaluation.

---
+++ cpp/subprojects/boosting/src/boosting/rule_evaluation/rule_evaluation_example_wise_common.hpp
@@ -0,0 +1,79 @@
+/*
+ * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ */
+#pragma once
+
+#include "boosting/rule_evaluation/rule_evaluation_example_wise.hpp"
+#include "boosting/math/lapack.hpp"
+
+
+namespace boosting {
+
+    /**
+     * An abstract base class for all classes that allow to calculate the predictions of rules, as well as corresponding
+     * quality scores, based on the gradients and Hessians that have been calculated according to a loss function that
+     * is applied example-wise.
+     *
+     * @tparam StatisticVector  The type of the vector that provides access to the gradients and Hessians
+     * @tparam T                The type of the vector that provides access to the labels for which predictions should
+     *                          be calculated
+     */
+    template<typename StatisticVector, typename T>
+    class AbstractExampleWiseRuleEvaluation : public IExampleWiseRuleEvaluation<StatisticVector> {
+
+        protected:
+
+            /**
+             * The `lwork` parameter that is used for executing the LAPACK routine DSYSV.
+             */
+            int dsysvLwork_;
+
+            /**
+             * A pointer to a temporary array that is used for executing the LAPACK routine DSYSV.
+             */
+            float64* dsysvTmpArray1_;
+
+            /**
+             * A pointer to a second temporary array that is used for executing the LAPACK routine DSYSV.
+             */
+            int* dsysvTmpArray2_;
+
+            /**
+             * A pointer to a third temporary array that is used for executing the LAPACK routine DSYSV.
+             */
+            double* dsysvTmpArray3_;
+
+            /**
+             * A pointer to a temporary array that is used for executing the LAPACK routine DSPMV.
+             */
+            float64* dspmvTmpArray_;
+
+        public:
+
+            /**
+             * @param labelIndices  A reference to an object of template type `T` that provides access to the indices of
+             *                      the labels for which the rules may predict
+             * @param lapack        A reference to an object of type `Lapack` that allows to execute different LAPACK
+             *                      routines
+             */
+            AbstractExampleWiseRuleEvaluation(const T& labelIndices, const Lapack& lapack) {
+                uint32 numPredictions = labelIndices.getNumElements();
+                dsysvTmpArray1_ = new float64[numPredictions * numPredictions];
+                dsysvTmpArray2_ = new int[numPredictions];
+                dspmvTmpArray_ = new float64[numPredictions];
+
+                // Query the optimal "lwork" parameter to be used by LAPACK's DSYSV routine...
+                dsysvLwork_ = lapack.queryDsysvLworkParameter(dsysvTmpArray1_, dspmvTmpArray_, numPredictions);
+                dsysvTmpArray3_ = new double[dsysvLwork_];
+            }
+
+            virtual ~AbstractExampleWiseRuleEvaluation() {
+                delete[] dsysvTmpArray1_;
+                delete[] dsysvTmpArray2_;
+                delete[] dsysvTmpArray3_;
+                delete[] dspmvTmpArray_;
+            }
+
+    };
+
+}
--- cpp/subprojects/boosting/src/boosting/rule_evaluation/rule_evaluation_example_wise_complete.cpp
+++ cpp/subprojects/boosting/src/boosting/rule_evaluation/rule_evaluation_example_wise_complete.cpp
@@ -1,8 +1,43 @@
 #include "boosting/rule_evaluation/rule_evaluation_example_wise_complete.hpp"
+#include "boosting/math/math.hpp"
+#include "common/rule_evaluation/score_vector_dense.hpp"
 #include "common/validation.hpp"
+#include "rule_evaluation_example_wise_common.hpp"
 
 
 namespace boosting {
+
+    static inline void copyCoefficients(DenseExampleWiseStatisticVector::hessian_const_iterator hessianIterator,
+                                        float64* coefficients, uint32 numPredictions) {
+        for (uint32 c = 0; c < numPredictions; c++) {
+            uint32 offset = c * numPredictions;
+
+            for (uint32 r = 0; r <= c; r++) {
+                coefficients[offset + r] = *hessianIterator;
+                hessianIterator++;
+            }
+        }
+    }
+
+    static inline void addRegularizationWeight(float64* coefficients, uint32 numPredictions,
+                                               float64 l2RegularizationWeight) {
+        for (uint32 i = 0; i < numPredictions; i++) {
+            coefficients[(i * numPredictions) + i] += l2RegularizationWeight;
+        }
+    }
+
+    static inline void copyOrdinates(DenseExampleWiseStatisticVector::gradient_const_iterator gradientIterator,
+                                     float64* ordinates, uint32 numPredictions) {
+        for (uint32 i = 0; i < numPredictions; i++) {
+            ordinates[i] = -gradientIterator[i];
+        }
+    }
+
+    static inline float64 calculateOverallQualityScore(float64* scores, float64* gradients, float64* hessians,
+                                                       float64* tmpArray, uint32 numPredictions, const Blas& blas) {
+        blas.dspmv(hessians, scores, tmpArray, numPredictions);
+        return blas.ddot(scores, gradients, numPredictions) + (0.5 * blas.ddot(scores, tmpArray, numPredictions));
+    }
 
     /**
      * Allows to calculate the predictions of complete rules, as well as an overall quality score, based on the
@@ -11,11 +46,17 @@ namespace boosting {
      * @tparam T The type of the vector that provides access to the labels for which predictions should be calculated
      */
     template<typename T>
-    class ExampleWiseCompleteRuleEvaluation final : public IExampleWiseRuleEvaluation<DenseExampleWiseStatisticVector> {
+    class ExampleWiseCompleteRuleEvaluation final : public AbstractExampleWiseRuleEvaluation<DenseExampleWiseStatisticVector, T> {
 
         private:
 
+            DenseScoreVector<T> scoreVector_;
+
             float64 l2RegularizationWeight_;
+
+            const Blas& blas_;
+
+            const Lapack& lapack_;
 
         public:
 
@@ -24,9 +65,16 @@ namespace boosting {
              *                                  the indices of the labels for which the rules may predict
              * @param l2RegularizationWeight    The weight of the L2 regularization that is applied for calculating the
              *                                  scores to be predicted by rules
+             * @param blas                      A reference to an object of type `Blas` that allows to execute different
+             *                                  BLAS routines
+             * @param lapack                    A reference to an object of type `Lapack` that allows to execute
+             *                                  different LAPACK routines
              */
-            ExampleWiseCompleteRuleEvaluation(const T& labelIndices, float64 l2RegularizationWeight)
-                : l2RegularizationWeight_(l2RegularizationWeight) {
+            ExampleWiseCompleteRuleEvaluation(const T& labelIndices, float64 l2RegularizationWeight, const Blas& blas,
+                                              const Lapack& lapack)
+                : AbstractExampleWiseRuleEvaluation<DenseExampleWiseStatisticVector, T>(labelIndices, lapack),
+                  scoreVector_(DenseScoreVector<T>(labelIndices)), l2RegularizationWeight_(l2RegularizationWeight),
+                  blas_(blas), lapack_(lapack) {
 
             }
 
@@ -41,7 +89,30 @@ namespace boosting {
             }
 
             const IScoreVector& calculatePrediction(DenseExampleWiseStatisticVector& statisticVector) override {
-                // TODO Implement
+                uint32 numPredictions = scoreVector_.getNumElements();
+
+                // Copy Hessians to the matrix of coefficients and add regularization weight to its diagonal...
+                copyCoefficients(statisticVector.hessians_cbegin(), this->dsysvTmpArray1_, numPredictions);
+                addRegularizationWeight(this->dsysvTmpArray1_, numPredictions, l2RegularizationWeight_);
+
+                // Copy gradients to the vector of ordinates...
+                typename DenseScoreVector<T>::score_iterator scoreIterator = scoreVector_.scores_begin();
+                copyOrdinates(statisticVector.gradients_cbegin(), scoreIterator, numPredictions);
+
+                // Calculate the scores to be predicted for individual labels by solving a system of linear equations...
+                lapack_.dsysv(this->dsysvTmpArray1_, this->dsysvTmpArray2_, this->dsysvTmpArray3_, scoreIterator,
+                              numPredictions, this->dsysvLwork_);
+
+                // Calculate the overall quality score...
+                float64 qualityScore = calculateOverallQualityScore(scoreIterator, statisticVector.gradients_begin(),
+                                                                    statisticVector.hessians_begin(),
+                                                                    this->dspmvTmpArray_, numPredictions, blas_);
+
+                // Evaluate regularization term...
+                float64 regularizationTerm = 0.5 * l2RegularizationWeight_ * l2NormPow(scoreIterator, numPredictions);
+
+                scoreVector_.overallQualityScore = qualityScore + regularizationTerm;
+                return scoreVector_;
             }
 
     };
@@ -58,13 +129,15 @@ namespace boosting {
     std::unique_ptr<IExampleWiseRuleEvaluation<DenseExampleWiseStatisticVector>> ExampleWiseCompleteRuleEvaluationFactory::createDense(
             const CompleteIndexVector& indexVector) const {
         return std::make_unique<ExampleWiseCompleteRuleEvaluation<CompleteIndexVector>>(indexVector,
-                                                                                        l2RegularizationWeight_);
+                                                                                        l2RegularizationWeight_,
+                                                                                        *blasPtr_, *lapackPtr_);
     }
 
     std::unique_ptr<IExampleWiseRuleEvaluation<DenseExampleWiseStatisticVector>> ExampleWiseCompleteRuleEvaluationFactory::createDense(
             const PartialIndexVector& indexVector) const {
         return std::make_unique<ExampleWiseCompleteRuleEvaluation<PartialIndexVector>>(indexVector,
-                                                                                       l2RegularizationWeight_);
+                                                                                       l2RegularizationWeight_,
+                                                                                       *blasPtr_, *lapackPtr_);
     }
 
 }
