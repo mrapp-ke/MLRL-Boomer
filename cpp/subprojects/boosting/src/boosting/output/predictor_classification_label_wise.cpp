b900a430f5d35f3422667e0c7d22ec900e4ff337
Michael Rapp <michael.rapp90@googlemail.com>
2021-08-24 01:21:30 +0200

    Revert changes.

--- cpp/subprojects/boosting/src/boosting/output/predictor_classification_label_wise.cpp
+++ cpp/subprojects/boosting/src/boosting/output/predictor_classification_label_wise.cpp
@@ -7,10 +7,8 @@
 namespace boosting {
 
     static inline void applyThreshold(CContiguousConstView<float64>::const_iterator originalIterator,
-                                      uint32 numElements, CContiguousView<uint8>& predictionMatrix, uint32 row,
+                                      CContiguousView<uint8>::iterator transformedIterator, uint32 numElements,
                                       float64 threshold) {
-        CContiguousView<uint8>::iterator transformedIterator = predictionMatrix.row_begin(row);
-
         for (uint32 i = 0; i < numElements; i++) {
             float64 originalValue = originalIterator[i];
             uint8 transformedValue = originalValue > threshold ? 1 : 0;
@@ -18,33 +16,23 @@ namespace boosting {
         }
     }
 
-    static inline void applyThreshold(CContiguousConstView<float64>::const_iterator originalIterator,
-                                      uint32 numElements, LilMatrix<uint8>& predictionMatrix, uint32 row,
-                                      float64 threshold) {
-        LilMatrix<uint8>::Row& rowReference = predictionMatrix.getRow(row);
-
-        for (uint32 i = numElements - 1; i >= 0; i--) {
-            float64 originalValue = originalIterator[i];
-
-            if (originalValue > threshold) {
-                rowReference.emplace_front(i, 1);
-            }
-        }
+    LabelWiseClassificationPredictor::LabelWiseClassificationPredictor(float64 threshold, uint32 numThreads)
+        : threshold_(threshold), numThreads_(numThreads) {
+        assertGreaterOrEqual<uint32>("numThreads", numThreads, 1);
     }
 
-    template<typename PredictionMatrix>
-    static void inline predictInternally(const CContiguousFeatureMatrix& featureMatrix,
-                                         PredictionMatrix& predictionMatrix, const RuleModel& model,
-                                         float64 threshold, uint32 numThreads) {
+    void LabelWiseClassificationPredictor::predict(const CContiguousFeatureMatrix& featureMatrix,
+                                                   CContiguousView<uint8>& predictionMatrix,
+                                                   const RuleModel& model, const LabelVectorSet* labelVectors) const {
         uint32 numExamples = featureMatrix.getNumRows();
         uint32 numLabels = predictionMatrix.getNumCols();
         const CContiguousFeatureMatrix* featureMatrixPtr = &featureMatrix;
-        PredictionMatrix* predictionMatrixPtr = &predictionMatrix;
+        CContiguousView<uint8>* predictionMatrixPtr = &predictionMatrix;
         const RuleModel* modelPtr = &model;
 
-        #pragma omp parallel for firstprivate(numExamples) firstprivate(numLabels) firstprivate(threshold) \
+        #pragma omp parallel for firstprivate(numExamples) firstprivate(numLabels) firstprivate(threshold_) \
         firstprivate(modelPtr) firstprivate(featureMatrixPtr) firstprivate(predictionMatrixPtr) schedule(dynamic) \
-        num_threads(numThreads)
+        num_threads(numThreads_)
         for (uint32 i = 0; i < numExamples; i++) {
             float64 scoreVector[numLabels] = {};
 
@@ -53,23 +41,23 @@ namespace boosting {
                 applyRule(rule, featureMatrixPtr->row_cbegin(i), featureMatrixPtr->row_cend(i), &scoreVector[0]);
             }
 
-            applyThreshold(&scoreVector[0], numLabels, *predictionMatrixPtr, i, threshold);
+            applyThreshold(&scoreVector[0], predictionMatrixPtr->row_begin(i), numLabels, threshold_);
         }
     }
 
-    template<typename PredictionMatrix>
-    static inline void predictInternally(const CsrFeatureMatrix& featureMatrix, PredictionMatrix& predictionMatrix,
-                                         const RuleModel& model, float64 threshold, uint32 numThreads) {
+    void LabelWiseClassificationPredictor::predict(const CsrFeatureMatrix& featureMatrix,
+                                                   CContiguousView<uint8>& predictionMatrix,
+                                                   const RuleModel& model, const LabelVectorSet* labelVectors) const {
         uint32 numExamples = featureMatrix.getNumRows();
         uint32 numLabels = predictionMatrix.getNumCols();
         uint32 numFeatures = featureMatrix.getNumCols();
         const CsrFeatureMatrix* featureMatrixPtr = &featureMatrix;
-        PredictionMatrix* predictionMatrixPtr = &predictionMatrix;
+        CContiguousView<uint8>* predictionMatrixPtr = &predictionMatrix;
         const RuleModel* modelPtr = &model;
 
         #pragma omp parallel for firstprivate(numExamples) firstprivate(numLabels) firstprivate(numFeatures) \
-        firstprivate(threshold) firstprivate(modelPtr) firstprivate(featureMatrixPtr) \
-        firstprivate(predictionMatrixPtr) schedule(dynamic) num_threads(numThreads)
+        firstprivate(threshold_) firstprivate(modelPtr) firstprivate(featureMatrixPtr) \
+        firstprivate(predictionMatrixPtr) schedule(dynamic) num_threads(numThreads_)
         for (uint32 i = 0; i < numExamples; i++) {
             float64 scoreVector[numLabels] = {};
             float32 tmpArray1[numFeatures];
@@ -84,19 +72,8 @@ namespace boosting {
                 n++;
             }
 
-            applyThreshold(&scoreVector[0], numLabels, *predictionMatrixPtr, i, threshold);
+            applyThreshold(&scoreVector[0], predictionMatrixPtr->row_begin(i), numLabels, threshold_);
         }
-    }
-
-    LabelWiseClassificationPredictor::LabelWiseClassificationPredictor(float64 threshold, uint32 numThreads)
-        : threshold_(threshold), numThreads_(numThreads) {
-        assertGreaterOrEqual<uint32>("numThreads", numThreads, 1);
-    }
-
-    void LabelWiseClassificationPredictor::predict(const CContiguousFeatureMatrix& featureMatrix,
-                                                   CContiguousView<uint8>& predictionMatrix,
-                                                   const RuleModel& model, const LabelVectorSet* labelVectors) const {
-        predictInternally(featureMatrix, predictionMatrix, model, threshold_, numThreads_);
     }
 
     std::unique_ptr<SparsePredictionMatrix<uint8>> LabelWiseClassificationPredictor::predict(
@@ -104,12 +81,6 @@ namespace boosting {
             const LabelVectorSet* labelVectors) const {
         // TODO
         return nullptr;
-    }
-
-    void LabelWiseClassificationPredictor::predict(const CsrFeatureMatrix& featureMatrix,
-                                                   CContiguousView<uint8>& predictionMatrix,
-                                                   const RuleModel& model, const LabelVectorSet* labelVectors) const {
-        predictInternally(featureMatrix, predictionMatrix, model, threshold_, numThreads_);
     }
 
     std::unique_ptr<SparsePredictionMatrix<uint8>> LabelWiseClassificationPredictor::predict(
