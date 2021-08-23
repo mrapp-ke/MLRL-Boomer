2b658baf853182ed63e8348a1b6228a3715302f7
Michael Rapp <michael.rapp90@googlemail.com>
2021-08-24 01:43:44 +0200

    Implement functions to obtain sparse predictions.

--- cpp/subprojects/boosting/src/boosting/output/predictor_classification_label_wise.cpp
+++ cpp/subprojects/boosting/src/boosting/output/predictor_classification_label_wise.cpp
@@ -16,6 +16,35 @@ namespace boosting {
         }
     }
 
+    static inline uint32 applyThreshold(CContiguousConstView<float64>::const_iterator originalIterator,
+                                        LilMatrix<uint8>::Row& row, uint32 numElements, float64 threshold) {
+        uint32 numNonZeroElements = 0;
+        uint32 i = 0;
+
+        for (; i < numElements; i++) {
+            float64 originalValue = originalIterator[i];
+
+            if (originalValue > threshold) {
+                row.emplace_front(i, 1);
+                numNonZeroElements++;
+                break;
+            }
+        }
+
+        LilMatrix<uint8>::Row::iterator it = row.begin();
+
+        for (i = i + 1; i < numElements; i++) {
+            float64 originalValue = originalIterator[i];
+
+            if (originalValue > threshold) {
+                it = row.emplace_after(it, i, 1);
+                numNonZeroElements++;
+            }
+        }
+
+        return numNonZeroElements;
+    }
+
     LabelWiseClassificationPredictor::LabelWiseClassificationPredictor(float64 threshold, uint32 numThreads)
         : threshold_(threshold), numThreads_(numThreads) {
         assertGreaterOrEqual<uint32>("numThreads", numThreads, 1);
@@ -79,15 +108,64 @@ namespace boosting {
     std::unique_ptr<SparsePredictionMatrix<uint8>> LabelWiseClassificationPredictor::predict(
             const CContiguousFeatureMatrix& featureMatrix, uint32 numLabels, const RuleModel& model,
             const LabelVectorSet* labelVectors) const {
-        // TODO
-        return nullptr;
+        uint32 numExamples = featureMatrix.getNumRows();
+        std::unique_ptr<LilMatrix<uint8>> lilMatrixPtr = std::make_unique<LilMatrix<uint8>>(numExamples);
+        const CContiguousFeatureMatrix* featureMatrixPtr = &featureMatrix;
+        LilMatrix<uint8>* predictionMatrixPtr = lilMatrixPtr.get();
+        const RuleModel* modelPtr = &model;
+        uint32 numNonZeroElements = 0;
+
+        #pragma omp parallel for reduction(+:numNonZeroElements) firstprivate(numExamples) firstprivate(numLabels) \
+        firstprivate(threshold_) firstprivate(modelPtr) firstprivate(featureMatrixPtr) \
+        firstprivate(predictionMatrixPtr) schedule(dynamic) num_threads(numThreads_)
+        for (uint32 i = 0; i < numExamples; i++) {
+            float64 scoreVector[numLabels] = {};
+
+            for (auto it = modelPtr->used_cbegin(); it != modelPtr->used_cend(); it++) {
+                const Rule& rule = *it;
+                applyRule(rule, featureMatrixPtr->row_cbegin(i), featureMatrixPtr->row_cend(i), &scoreVector[0]);
+            }
+
+            numNonZeroElements += applyThreshold(&scoreVector[0], predictionMatrixPtr->getRow(i), numLabels,
+                                                 threshold_);
+        }
+
+        return std::make_unique<SparsePredictionMatrix<uint8>>(std::move(lilMatrixPtr), numLabels, numNonZeroElements);
     }
 
     std::unique_ptr<SparsePredictionMatrix<uint8>> LabelWiseClassificationPredictor::predict(
             const CsrFeatureMatrix& featureMatrix, uint32 numLabels, const RuleModel& model,
             const LabelVectorSet* labelVectors) const {
-        // TODO
-        return nullptr;
+        uint32 numExamples = featureMatrix.getNumRows();
+        uint32 numFeatures = featureMatrix.getNumCols();
+        std::unique_ptr<LilMatrix<uint8>> lilMatrixPtr = std::make_unique<LilMatrix<uint8>>(numExamples);
+        const CsrFeatureMatrix* featureMatrixPtr = &featureMatrix;
+        LilMatrix<uint8>* predictionMatrixPtr = lilMatrixPtr.get();
+        const RuleModel* modelPtr = &model;
+        uint32 numNonZeroElements = 0;
+
+        #pragma omp parallel for reduction(+:numNonZeroElements) firstprivate(numExamples) firstprivate(numLabels) \
+        firstprivate(numFeatures) firstprivate(threshold_) firstprivate(modelPtr) firstprivate(featureMatrixPtr) \
+        firstprivate(predictionMatrixPtr) schedule(dynamic) num_threads(numThreads_)
+        for (uint32 i = 0; i < numExamples; i++) {
+            float64 scoreVector[numLabels] = {};
+            float32 tmpArray1[numFeatures];
+            uint32 tmpArray2[numFeatures] = {};
+            uint32 n = 1;
+
+            for (auto it = modelPtr->used_cbegin(); it != modelPtr->used_cend(); it++) {
+                const Rule& rule = *it;
+                applyRuleCsr(rule, featureMatrixPtr->row_indices_cbegin(i), featureMatrixPtr->row_indices_cend(i),
+                             featureMatrixPtr->row_values_cbegin(i), featureMatrixPtr->row_values_cend(i),
+                             &scoreVector[0], &tmpArray1[0], &tmpArray2[0], n);
+                n++;
+            }
+
+            numNonZeroElements += applyThreshold(&scoreVector[0], predictionMatrixPtr->getRow(i), numLabels,
+                                                 threshold_);
+        }
+
+        return std::make_unique<SparsePredictionMatrix<uint8>>(std::move(lilMatrixPtr), numLabels, numNonZeroElements);
     }
 
 }
