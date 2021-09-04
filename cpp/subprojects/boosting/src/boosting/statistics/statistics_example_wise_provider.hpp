e2bdf8cbd4afa9d77fba9d849b1981315d898203
Michael Rapp <michael.rapp90@googlemail.com>
2021-09-04 23:46:13 +0200

    Add class ConvertibleExampleWiseStatisticsProvider.

--- cpp/subprojects/boosting/src/boosting/statistics/statistics_example_wise_provider.hpp
+++ cpp/subprojects/boosting/src/boosting/statistics/statistics_example_wise_provider.hpp
@@ -1,5 +1,6 @@
 #include "common/statistics/statistics_provider.hpp"
 #include "boosting/statistics/statistics_example_wise.hpp"
+#include "boosting/statistics/statistics_label_wise.hpp"
 
 
 namespace boosting {
@@ -48,6 +49,78 @@ namespace boosting {
 
             void switchToPruningRuleEvaluation() override {
                 statisticsPtr_->setRuleEvaluationFactory(pruningRuleEvaluationFactory_);
+            }
+
+    };
+
+    /**
+     * Provides access to an object of type `IExampleWiseStatistics` that can be converted in to an object of type
+     * `ILabelWiseStatistics`.
+     */
+    class ConvertibleExampleWiseStatisticsProvider : public IStatisticsProvider {
+
+        private:
+
+            const ILabelWiseRuleEvaluationFactory& regularRuleEvaluationFactory_;
+
+            const ILabelWiseRuleEvaluationFactory& pruningRuleEvaluationFactory_;
+
+            std::unique_ptr<IExampleWiseStatistics> exampleWiseStatisticsPtr_;
+
+            std::unique_ptr<ILabelWiseStatistics> labelWiseStatisticsPtr_;
+
+        public:
+
+            /**
+             * @param regularRuleEvaluationFactory  A reference to an object of type `ILabelWiseRuleEvaluationFactory`
+             *                                      to switch to when invoking the function
+             *                                      `switchToRegularRuleEvaluation`
+             * @param pruningRuleEvaluationFactory  A reference to an object of type `ILabelWiseRuleEvaluationFactory`
+             *                                      to switch to when invoking the function
+             *                                      `switchToPruningRuleEvaluation`
+             * @param statisticsPtr                 An unique pointer to an object of type `IExampleWiseStatistics` to
+             *                                      provide access to
+             */
+            ConvertibleExampleWiseStatisticsProvider(
+                    const ILabelWiseRuleEvaluationFactory& regularRuleEvaluationFactory,
+                    const ILabelWiseRuleEvaluationFactory& pruningRuleEvaluationFactory,
+                    std::unique_ptr<IExampleWiseStatistics> statisticsPtr)
+                : regularRuleEvaluationFactory_(regularRuleEvaluationFactory),
+                  pruningRuleEvaluationFactory_(pruningRuleEvaluationFactory),
+                  exampleWiseStatisticsPtr_(std::move(statisticsPtr)) {
+
+            }
+
+            IStatistics& get() const override {
+                IExampleWiseStatistics* exampleWiseStatistics = exampleWiseStatisticsPtr_.get();
+
+                if (exampleWiseStatistics != nullptr) {
+                    return *exampleWiseStatistics;
+                } else {
+                    return *labelWiseStatisticsPtr_;
+                }
+            }
+
+            void switchToRegularRuleEvaluation() override {
+                IExampleWiseStatistics* exampleWiseStatistics = exampleWiseStatisticsPtr_.get();
+
+                if (exampleWiseStatistics != nullptr) {
+                    labelWiseStatisticsPtr_ = exampleWiseStatistics->toLabelWiseStatistics();
+                    exampleWiseStatisticsPtr_.reset();
+                }
+
+                labelWiseStatisticsPtr_->setRuleEvaluationFactory(regularRuleEvaluationFactory_);
+            }
+
+            void switchToPruningRuleEvaluation() override {
+                IExampleWiseStatistics* exampleWiseStatistics = exampleWiseStatisticsPtr_.get();
+
+                if (exampleWiseStatistics != nullptr) {
+                    labelWiseStatisticsPtr_ = exampleWiseStatistics->toLabelWiseStatistics();
+                    exampleWiseStatisticsPtr_.reset();
+                }
+
+                labelWiseStatisticsPtr_->setRuleEvaluationFactory(pruningRuleEvaluationFactory_);
             }
 
     };
