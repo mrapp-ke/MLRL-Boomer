d236ed4eecfc1706ea927d4ee9fbc3a5010b9198
Michael Rapp <michael.rapp90@googlemail.com>
2021-09-12 15:02:47 +0200

    Add class SparseLabelWiseStatisticsProviderFactory (not implemented yet).

---
+++ cpp/subprojects/boosting/src/boosting/statistics/statistics_provider_factory_label_wise_sparse.cpp
@@ -0,0 +1,35 @@
+#include "boosting/statistics/statistics_provider_factory_label_wise_sparse.hpp"
+#include "common/validation.hpp"
+
+
+namespace boosting {
+
+    SparseLabelWiseStatisticsProviderFactory::SparseLabelWiseStatisticsProviderFactory(
+            std::unique_ptr<ISparseLabelWiseLoss> lossFunctionPtr,
+            std::unique_ptr<ILabelWiseRuleEvaluationFactory> defaultRuleEvaluationFactoryPtr,
+            std::unique_ptr<ILabelWiseRuleEvaluationFactory> regularRuleEvaluationFactoryPtr,
+            std::unique_ptr<ILabelWiseRuleEvaluationFactory> pruningRuleEvaluationFactoryPtr, uint32 numThreads)
+        : lossFunctionPtr_(std::move(lossFunctionPtr)),
+          defaultRuleEvaluationFactoryPtr_(std::move(defaultRuleEvaluationFactoryPtr)),
+          regularRuleEvaluationFactoryPtr_(std::move(regularRuleEvaluationFactoryPtr)),
+          pruningRuleEvaluationFactoryPtr_(std::move(pruningRuleEvaluationFactoryPtr)), numThreads_(numThreads) {
+        assertNotNull("lossFunctionPtr", lossFunctionPtr_.get());
+        assertNotNull("defaultRuleEvaluationFactoryPtr", defaultRuleEvaluationFactoryPtr_.get());
+        assertNotNull("regularRuleEvaluationFactoryPtr", regularRuleEvaluationFactoryPtr_.get());
+        assertNotNull("pruningRuleEvaluationFactoryPtr", pruningRuleEvaluationFactoryPtr_.get());
+        assertGreaterOrEqual<uint32>("numThreads", numThreads, 1);
+    }
+
+    std::unique_ptr<IStatisticsProvider> SparseLabelWiseStatisticsProviderFactory::create(
+            const CContiguousLabelMatrix& labelMatrix) const {
+        // TODO Implement
+        return nullptr;
+    }
+
+    std::unique_ptr<IStatisticsProvider> SparseLabelWiseStatisticsProviderFactory::create(
+            const CsrLabelMatrix& labelMatrix) const {
+        // TODO Implement
+        return nullptr;
+    }
+
+}
