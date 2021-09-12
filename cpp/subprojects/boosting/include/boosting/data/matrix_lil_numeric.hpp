a697dd88e7eef3d0d7b7bbd5d0e64dcbbc1e5b1f
Michael Rapp <michael.rapp90@googlemail.com>
2021-09-12 23:08:31 +0200

    Add function "addToRowFromSubset" to class NumericLilMatrix.

--- cpp/subprojects/boosting/include/boosting/data/matrix_lil_numeric.hpp
+++ cpp/subprojects/boosting/include/boosting/data/matrix_lil_numeric.hpp
@@ -4,6 +4,8 @@
 #pragma once
 
 #include "common/data/matrix_lil.hpp"
+#include "common/indices/index_vector_complete.hpp"
+#include "common/indices/index_vector_partial.hpp"
 
 
 namespace boosting {
@@ -23,6 +25,36 @@ namespace boosting {
              * @param numRows The number of rows in the matrix
              */
             NumericLilMatrix(uint32 numRows);
+
+            /**
+             * Adds all values in another vector to certain elements, whose positions are given as a
+             * `CompleteIndexVector`, at a specific row of this matrix.
+             *
+             * @param row           The row
+             * @param begin         An iterator to the beginning of the vector
+             * @param end           An iterator to the end of the vector
+             * @param indicesBegin  An iterator to the beginning of the indices
+             * @param indicesEnd    An iterator to the end of the indices
+             */
+            void addToRowFromSubset(uint32 row, typename DenseVector<T>::const_iterator begin,
+                                    typename DenseVector<T>::const_iterator end,
+                                    CompleteIndexVector::const_iterator indicesBegin,
+                                    CompleteIndexVector::const_iterator indicesEnd);
+
+            /**
+             * Adds all values in another vector to certain elements, whose positions are given as a
+             * `PartialIndexVector`, at a specific row of this matrix.
+             *
+             * @param row           The row
+             * @param begin         An iterator to the beginning of the vector
+             * @param end           An iterator to the end of the vector
+             * @param indicesBegin  An iterator to the beginning of the indices
+             * @param indicesEnd    An iterator to the end of the indices
+             */
+            void addToRowFromSubset(uint32 row, typename DenseVector<T>::const_iterator begin,
+                                    typename DenseVector<T>::const_iterator end,
+                                    PartialIndexVector::const_iterator indicesBegin,
+                                    PartialIndexVector::const_iterator indicesEnd);
 
     };
 
--- cpp/subprojects/boosting/src/boosting/data/matrix_lil_numeric.cpp
+++ cpp/subprojects/boosting/src/boosting/data/matrix_lil_numeric.cpp
@@ -9,6 +9,22 @@ namespace boosting {
 
     }
 
+    template<typename T>
+    void NumericLilMatrix<T>::addToRowFromSubset(uint32 row, typename DenseVector<T>::const_iterator begin,
+                                                 typename DenseVector<T>::const_iterator end,
+                                                 CompleteIndexVector::const_iterator indicesBegin,
+                                                 CompleteIndexVector::const_iterator indicesEnd) {
+        // TODO Implement
+    }
+
+    template<typename T>
+    void NumericLilMatrix<T>::addToRowFromSubset(uint32 row, typename DenseVector<T>::const_iterator begin,
+                                                 typename DenseVector<T>::const_iterator end,
+                                                 PartialIndexVector::const_iterator indicesBegin,
+                                                 PartialIndexVector::const_iterator indicesEnd) {
+        // TODO Implement
+    }
+
     template class NumericLilMatrix<uint8>;
     template class NumericLilMatrix<uint32>;
     template class NumericLilMatrix<float32>;
