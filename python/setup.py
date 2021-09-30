ced8bb0443d6d45e79dba42f0efff4705a403488
Michael Rapp <michael.rapp90@googlemail.com>
2021-09-30 22:53:14 +0200

    Update version to 0.7.0.

--- python/setup.py
+++ python/setup.py
@@ -60,7 +60,7 @@ extensions = [
 
 setuptools.setup(
     name='boomer',
-    version='0.6.1',
+    version='0.7.0',
     description='BOOMER - An algorithm for learning gradient boosted multi-label classification rules',
     url='https://github.com/mrapp-ke/Boomer',
     author='Michael Rapp',
