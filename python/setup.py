22abbe335c2257e58992a1f5512486d16a8d3357
Michael Rapp <michael.rapp90@googlemail.com>
2021-11-07 18:03:15 +0100

    Remove Cython extensions from setup.py.

--- python/setup.py
+++ python/setup.py
@@ -3,60 +3,7 @@
 """
 Author: Michael Rapp (mrapp@ke.tu-darmstadt.de)
 """
-import numpy
 import setuptools
-from Cython.Build import cythonize
-
-# True, if annotated Cython source files that highlight Python interactions should be created
-ANNOTATE = False
-
-# True, if all Cython compiler optimizations should be disabled
-DEBUG = False
-
-sources = [
-    '**/*.pyx'
-]
-
-library_dirs = [
-    '../cpp/build/mlrl/common',
-    '../cpp/build/mlrl/boosting',
-    '../cpp/build/mlrl/seco'
-]
-
-runtime_library_dirs = [
-    'cpp/build/mlrl/common',
-    'cpp/build/mlrl/boosting',
-    'cpp/build/mlrl/seco'
-]
-
-libraries = [
-    'mlrlcommon',
-    'mlrlboosting',
-    'mlrlseco'
-]
-
-include_dirs = [
-    '../cpp/mlrl/common/include',
-    '../cpp/mlrl/boosting/include',
-    '../cpp/mlrl/seco/include'
-]
-
-define_macros = [
-    ("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")
-]
-
-compiler_directives = {
-    'boundscheck': DEBUG,
-    'wraparound': DEBUG,
-    'cdivision': not DEBUG,
-    'initializedcheck': DEBUG
-}
-
-extensions = [
-    setuptools.Extension(name='*', language='c++', sources=sources, library_dirs=library_dirs, libraries=libraries,
-                         runtime_library_dirs=runtime_library_dirs, include_dirs=include_dirs,
-                         define_macros=define_macros)
-]
 
 setuptools.setup(
     name='boomer',
@@ -70,10 +17,7 @@ setuptools.setup(
     install_requires=[
         'numpy>=1.21.0',
         'scipy>=1.7.0',
-        'Cython>=0.29.0',
         'scikit-learn>=1.0.0',
         'liac-arff>=2.5.0'
     ],
-    python_requires='>=3.7',
-    ext_modules=cythonize(extensions, language_level='3', annotate=ANNOTATE, compiler_directives=compiler_directives),
-    include_dirs=[numpy.get_include()])
+    python_requires='>=3.7')
