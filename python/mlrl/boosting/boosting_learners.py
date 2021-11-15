28f10c110863d746b55de70ebf81c4c7cc785a3c
Michael Rapp <michael.rapp90@googlemail.com>
2021-11-15 13:35:57 +0100

    Add parameter "l1_regularization_weight" to the boosting algorithm.

--- python/mlrl/boosting/boosting_learners.py
+++ python/mlrl/boosting/boosting_learners.py
@@ -38,8 +38,6 @@ from mlrl.common.cython.statistics import StatisticsProviderFactory
 from mlrl.common.cython.stopping import StoppingCriterion, MeasureStoppingCriterion, AggregationFunction, MinFunction, \
     MaxFunction, ArithmeticMeanFunction
 from mlrl.common.cython.thresholds import ThresholdsFactory
-from sklearn.base import ClassifierMixin
-
 from mlrl.common.options import BooleanOption
 from mlrl.common.rule_learners import AUTOMATIC, SAMPLING_WITHOUT_REPLACEMENT, HEAD_TYPE_SINGLE, ARGUMENT_BIN_RATIO, \
     ARGUMENT_MIN_BINS, ARGUMENT_MAX_BINS, ARGUMENT_NUM_THREADS
@@ -47,6 +45,7 @@ from mlrl.common.rule_learners import MLRuleLearner, SparsePolicy, LabelCharacte
 from mlrl.common.rule_learners import create_pruning, create_feature_sampling_factory, create_label_sampling_factory, \
     create_instance_sampling_factory, create_partition_sampling_factory, create_stopping_criteria, \
     create_num_threads, create_thresholds_factory, parse_param, parse_param_and_options
+from sklearn.base import ClassifierMixin
 
 EARLY_STOPPING_LOSS = 'loss'
 
@@ -129,8 +128,8 @@ class Boomer(MLRuleLearner, ClassifierMixin):
                  recalculate_predictions: str = BooleanOption.TRUE.value,
                  feature_sampling: str = SAMPLING_WITHOUT_REPLACEMENT, holdout: str = None, feature_binning: str = None,
                  label_binning: str = AUTOMATIC, pruning: str = None, shrinkage: float = 0.3,
-                 l2_regularization_weight: float = 1.0, min_coverage: int = 1, max_conditions: int = 0,
-                 max_head_refinements: int = 1, parallel_rule_refinement: str = AUTOMATIC,
+                 l1_regularization_weight: float = 0.0, l2_regularization_weight: float = 1.0, min_coverage: int = 1,
+                 max_conditions: int = 0, max_head_refinements: int = 1, parallel_rule_refinement: str = AUTOMATIC,
                  parallel_statistic_update: str = AUTOMATIC, parallel_prediction: str = BooleanOption.TRUE.value):
         """
         :param max_rules:                           The maximum number of rules to be induced (including the default
@@ -188,6 +187,8 @@ class Boomer(MLRuleLearner, ClassifierMixin):
         :param shrinkage:                           The shrinkage parameter that should be applied to the predictions of
                                                     newly induced rules to reduce their effect on the entire model. Must
                                                     be in (0, 1]
+        :param l1_regularization_weight:            The weight of the L1 regularization that is applied for calculating
+                                                    the scores that are predicted by rules. Must be at least 0
         :param l2_regularization_weight:            The weight of the L2 regularization that is applied for calculating
                                                     the scores that are predicted by rules. Must be at least 0
         :param min_coverage:                        The minimum number of training examples that must be covered by a
@@ -229,6 +230,7 @@ class Boomer(MLRuleLearner, ClassifierMixin):
         self.label_binning = label_binning
         self.pruning = pruning
         self.shrinkage = shrinkage
+        self.l1_regularization_weight = l1_regularization_weight
         self.l2_regularization_weight = l2_regularization_weight
         self.min_coverage = min_coverage
         self.max_conditions = max_conditions
@@ -262,6 +264,8 @@ class Boomer(MLRuleLearner, ClassifierMixin):
             name += '_pruning=' + str(self.pruning)
         if float(self.shrinkage) < 1.0:
             name += '_shrinkage=' + str(self.shrinkage)
+        if float(self.l1_regularization_weight) > 0.0:
+            name += '_l1=' + str(self.l1_regularization_weight)
         if float(self.l2_regularization_weight) > 0.0:
             name += '_l2=' + str(self.l2_regularization_weight)
         if int(self.min_coverage) > 1:
