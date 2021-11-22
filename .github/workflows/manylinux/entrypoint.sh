50f8b7b52af86c3847dcb42f7d21f428f30e9540
Michael Rapp <michael.rapp90@googlemail.com>
2021-11-22 22:21:13 +0100

    Update Docker entrypoint.

--- .github/workflows/manylinux/entrypoint.sh
+++ .github/workflows/manylinux/entrypoint.sh
@@ -5,5 +5,6 @@ PYTHON_VERSIONS=("cp37-cp37m" "cp38-cp38" "cp39-cp39")
 for VERSION in "${PYTHON_VERSIONS[@]}"; do
   PYTHON="/opt/python/${VERSION}/bin/python"
   ln -fs ${PYTHON} /usr/bin/python
-  make venv
+  make install
+  make clean
 done
