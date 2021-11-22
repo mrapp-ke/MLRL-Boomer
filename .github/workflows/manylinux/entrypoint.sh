19339d4a8cbf1219bcce6bee2af003f2b0906aa1
Michael Rapp <michael.rapp90@googlemail.com>
2021-11-22 19:44:47 +0100

    Update Docker entrypoint.

--- .github/workflows/manylinux/entrypoint.sh
+++ .github/workflows/manylinux/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 for PYBIN in /opt/python/*/bin; do
-  "${PYBIN}/pip" install -r python/requirements.txt
-  cd cpp/
-  meson setup build/
-  cd build/
-  meson compile
-  meson install
+  echo ${PYBIN}
 done
