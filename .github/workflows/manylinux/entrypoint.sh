6ff2db9e464407391350de10eb1c25abbc921cc4
Michael Rapp <michael.rapp90@googlemail.com>
2021-11-22 21:52:25 +0100

    Update Docker entrypoint.

--- .github/workflows/manylinux/entrypoint.sh
+++ .github/workflows/manylinux/entrypoint.sh
@@ -5,6 +5,7 @@ PYTHON_VERSIONS="cp37-cp37m cp38-cp38 cp39-cp39"
 python -v
 
 for VERSION in ${PYTHON_VERSION}; do
+  echo ${VERSION}
   PYTHON="/opt/python/${VERSION}/bin"
   ln -fs ${PYTHON} /usr/bin/python
   python -v
