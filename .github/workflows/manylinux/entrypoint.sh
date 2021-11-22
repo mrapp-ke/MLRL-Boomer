68e748f23b663d46e5bae25cc800f1f12dc8f79b
Michael Rapp <michael.rapp90@googlemail.com>
2021-11-22 22:57:19 +0100

    Invoke auditwheel.

--- .github/workflows/manylinux/entrypoint.sh
+++ .github/workflows/manylinux/entrypoint.sh
@@ -6,5 +6,13 @@ for VERSION in "${PYTHON_VERSIONS[@]}"; do
   PYTHON="/opt/python/${VERSION}/bin/python"
   ln -fs ${PYTHON} /usr/bin/python
   make install
+  . venv/bin/activate
+  pip install auditwheel
+
+  for WHEEL in python/subprojects/*/dist/*.whl; do
+    auditwheel repair ${WHEEL}
+  done
+
+  deactivate
   make clean
 done
