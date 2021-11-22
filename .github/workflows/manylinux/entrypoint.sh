080d3d52e93ed399927f95fcaf982e4f51fa2b4e
Michael Rapp <michael.rapp90@googlemail.com>
2021-11-22 22:47:40 +0100

    Update build target.

--- .github/workflows/manylinux/entrypoint.sh
+++ .github/workflows/manylinux/entrypoint.sh
@@ -5,6 +5,6 @@ PYTHON_VERSIONS=("cp37-cp37m" "cp38-cp38" "cp39-cp39")
 for VERSION in "${PYTHON_VERSIONS[@]}"; do
   PYTHON="/opt/python/${VERSION}/bin/python"
   ln -fs ${PYTHON} /usr/bin/python
-  make compile
+  make install
   make clean
 done
