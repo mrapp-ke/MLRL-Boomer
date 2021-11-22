9af18e0818e6576635a68e506cff7c0d4c5939b1
Michael Rapp <michael.rapp90@googlemail.com>
2021-11-22 19:41:28 +0100

    Update Docker entrypoint.

--- .github/workflows/manylinux/entrypoint.sh
+++ .github/workflows/manylinux/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-apt-get install python3-venv
-make install
+for PYBIN in /opt/python/*/bin; do
+  "${PYBIN}/pip" install -r python/requirements.txt
+  cd cpp/
+  meson setup build/
+  cd build/
+  meson compile
+  meson install
+done
