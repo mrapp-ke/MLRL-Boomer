97c9828d6f8da76ce404f02e7987b7b873e3005f
Michael Rapp <michael.rapp90@googlemail.com>
2021-11-22 21:23:48 +0100

    Update Docker entrypoint.

--- .github/workflows/manylinux/entrypoint.sh
+++ .github/workflows/manylinux/entrypoint.sh
@@ -4,5 +4,5 @@ PYTHON_VERSIONS="cp37-cp37m cp38-cp38 cp39-cp39"
 
 for VERSION in ${PYTHON_VERSION}; do
   PYTHON="/opt/python/${VERSION}/bin"
-  "${PYTHON}/pip" install -r python/requirements.txt
+  ${PYTHON} -m venv venv
 done
