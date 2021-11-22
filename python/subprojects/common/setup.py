1dc9bda2e87063dfa9ecddd2badb58e0e6fccaa5
Michael Rapp <michael.rapp90@googlemail.com>
2021-11-22 15:10:43 +0100

    Read version from text file.

--- python/subprojects/common/setup.py
+++ python/subprojects/common/setup.py
@@ -7,9 +7,11 @@ from pathlib import Path
 
 from setuptools import setup, find_packages
 
+VERSION = (Path(__file__).resolve().parent.parent.parent.parent / 'VERSION').read_text()
+
 setup(
     name='mlrl-common',
-    version='0.7.0',
+    version=VERSION,
     description='Provides common modules to be used by different types of multi-label rule learning algorithms',
     long_description=(Path(__file__).resolve().parent / 'README.md').read_text(),
     long_description_content_type='text/markdown',
