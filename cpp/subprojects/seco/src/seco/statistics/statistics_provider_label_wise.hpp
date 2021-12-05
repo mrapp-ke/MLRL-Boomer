431d16b48c32c2495b39bf631064ee3474519910
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-05 19:38:03 +0100

    Update e-mail address.

--- cpp/subprojects/seco/src/seco/statistics/statistics_provider_label_wise.hpp
+++ cpp/subprojects/seco/src/seco/statistics/statistics_provider_label_wise.hpp
@@ -1,5 +1,5 @@
 /*
- * @author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+ * @author Michael Rapp (michael.rapp.ml@gmail.com)
  */
 #include "common/statistics/statistics_provider_factory.hpp"
 #include "seco/statistics/statistics_label_wise.hpp"
--- python/subprojects/seco/mlrl/seco/cython/heuristics.pyx
+++ python/subprojects/seco/mlrl/seco/cython/heuristics.pyx
@@ -1,6 +1,6 @@
 """
 @author Jakob Steeg (jakob.steeg@gmail.com)
-@author Michael Rapp (mrapp@ke.tu-darmstadt.de)
+@author Michael Rapp (michael.rapp.ml@gmail.com)
 @author Andreas Seidl Fernandez (aseidlfernandez@gmail.com)
 """
 from libcpp.memory cimport make_unique
