f1bc36aedaa67dfa797f512dde60b4b77ca296dd
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-16 17:10:50 +0100

    Add final modifier to class DenseConfusionMatrixVector.

--- cpp/subprojects/seco/include/seco/data/vector_confusion_matrix_dense.hpp
+++ cpp/subprojects/seco/include/seco/data/vector_confusion_matrix_dense.hpp
@@ -14,7 +14,7 @@ namespace seco {
     /**
      * An one-dimensional vector that stores a fixed number of confusion matrices in a C-contiguous array.
      */
-    class DenseConfusionMatrixVector {
+    class DenseConfusionMatrixVector final {
 
         private:
 
