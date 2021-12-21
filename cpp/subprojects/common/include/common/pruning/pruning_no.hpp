275b5b5a4491cd8055ba514b8e8ae7b4d6065601
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-21 22:16:45 +0100

    Move class NoPruning to cpp file.

--- cpp/subprojects/common/include/common/pruning/pruning_no.hpp
+++ cpp/subprojects/common/include/common/pruning/pruning_no.hpp
@@ -7,18 +7,6 @@
 
 
 /**
- * An implementation of the class `IPruning` that does not actually perform any pruning.
- */
-class NoPruning final : public IPruning {
-
-    public:
-
-        std::unique_ptr<ICoverageState> prune(IThresholdsSubset& thresholdsSubset, IPartition& partition,
-                                              ConditionList& conditions, const AbstractPrediction& head) const override;
-
-};
-
-/**
  * Allows to create instances of the type `IPruning` that do not actually perform any pruning.
  */
 class NoPruningFactory final : public IPruningFactory {
