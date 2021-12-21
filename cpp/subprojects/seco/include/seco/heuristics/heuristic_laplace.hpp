12283c1d8c52f8a59d745122d65e23c0419ceaef
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-22 00:05:09 +0100

    Move class Laplace to cpp file.

--- cpp/subprojects/seco/include/seco/heuristics/heuristic_laplace.hpp
+++ cpp/subprojects/seco/include/seco/heuristics/heuristic_laplace.hpp
@@ -9,18 +9,6 @@
 namespace seco {
 
     /**
-     * Laplace-corrected variant of the "Precision" metric.
-     */
-    class Laplace final : public IHeuristic {
-
-        public:
-
-            float64 evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
-                                            float64 uip, float64 urn, float64 urp) const override;
-
-    };
-
-    /**
      * Allows to create instances of the type `IHeuristic` that implement a Laplace-corrected variant of the "Precision"
      * metric.
      */
