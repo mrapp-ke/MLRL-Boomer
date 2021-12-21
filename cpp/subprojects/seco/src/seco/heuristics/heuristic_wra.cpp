83c2a539eae8e8eeeda5298af6788f9a4814c349
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-22 00:11:09 +0100

    Move class WRA to cpp file.

--- cpp/subprojects/seco/src/seco/heuristics/heuristic_wra.cpp
+++ cpp/subprojects/seco/src/seco/heuristics/heuristic_wra.cpp
@@ -4,13 +4,23 @@
 
 namespace seco {
 
-    float64 WRA::evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin, float64 uip,
-                                         float64 urn, float64 urp) const {
-        return wra(cin, cip, crn, crp, uin, uip, urn, urp);
-    }
+    /**
+     * An implementation of the type `IHeuristic` that calculates as `1 - wra`, where `wra` corresponds to the "Weighted
+     * Relative Accuracy" metric.
+     */
+    class Wra final : public IHeuristic {
+
+        public:
+
+            float64 evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
+                                            float64 uip, float64 urn, float64 urp) const override {
+                return wra(cin, cip, crn, crp, uin, uip, urn, urp);
+            }
+
+    };
 
     std::unique_ptr<IHeuristic> WraFactory::create() const {
-        return std::make_unique<WRA>();
+        return std::make_unique<Wra>();
     }
 
 }
