65cea1bd6b1469e1b550f216bbd3232c13e71439
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-21 23:00:19 +0100

    Move class ConstantShrinkage to cpp file.

--- cpp/subprojects/boosting/include/boosting/post_processing/shrinkage_constant.hpp
+++ cpp/subprojects/boosting/include/boosting/post_processing/shrinkage_constant.hpp
@@ -9,29 +9,6 @@
 namespace boosting {
 
     /**
-     * Post-processes the predictions of rules by shrinking their weights by a constant shrinkage parameter.
-     */
-    class ConstantShrinkage final : public IPostProcessor {
-
-        private:
-
-            float64 shrinkage_;
-
-        public:
-
-            /**
-             * @param shrinkage The shrinkage parameter. Must be in (0, 1).
-             */
-            ConstantShrinkage(float64 shrinkage);
-
-            /**
-             * @see `IPostProcessor::postProcess`
-             */
-            void postProcess(AbstractPrediction& prediction) const override;
-
-    };
-
-    /**
      * Allows to create instances of the type `IPostProcessor` that post-process the predictions of rules by shrinking
      * their weights by a constant shrinkage parameter.
      */
