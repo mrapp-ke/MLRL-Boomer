8d9aad0caee9e9bbc17d9d271dd24357c9d9fed0
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-22 00:04:07 +0100

    Move class FMeasure to cpp file.

--- cpp/subprojects/seco/include/seco/heuristics/heuristic_f_measure.hpp
+++ cpp/subprojects/seco/include/seco/heuristics/heuristic_f_measure.hpp
@@ -10,30 +10,6 @@
 namespace seco {
 
     /**
-     * A heuristic that calculates as the (weighted) harmonic mean between the heuristics "Precision" and "Recall",
-     * where the parameter "beta" allows to trade off between both heuristics. If beta = 1, both heuristics are weighed
-     * equally. If beta = 0, this heuristic is equivalent to "Precision". As beta approaches infinity, this heuristic
-     * becomes equivalent to "Recall".
-     */
-    class FMeasure final : public IHeuristic {
-
-        private:
-
-            float64 beta_;
-
-        public:
-
-            /**
-             * @param beta The value of the "beta" parameter. Must be at least 0
-             */
-            FMeasure(float64 beta);
-
-            float64 evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
-                                            float64 uip, float64 urn, float64 urp) const override;
-
-    };
-
-    /**
      * Allows to create instances of the type `IHeuristic` that calculate as the (weighted) harmonic mean between the
      * heuristics "Precision" and "Recall", where the parameter "beta" allows to trade off between both heuristics. If
      * beta = 1, both heuristics are weighed equally. If beta = 0, this heuristic is equivalent to "Precision". As beta
