492014587acfac8af7cbe146d2d5ea8726353eb8
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-21 22:58:27 +0100

    Move class NoPostProcessor to cpp file.

--- cpp/subprojects/common/include/common/post_processing/post_processor_no.hpp
+++ cpp/subprojects/common/include/common/post_processing/post_processor_no.hpp
@@ -7,18 +7,6 @@
 
 
 /**
- * An implementation of the class `IPostProcessor` that does not perform any post-processing, but retains the original
- * predictions of rules.
- */
-class NoPostProcessor final : public IPostProcessor {
-
-    public:
-
-        void postProcess(AbstractPrediction& prediction) const override;
-
-};
-
-/**
  * Allows to create instances of the type `IPostProcessor` that do not perform any post-processing, but retain the
  * original predictions of rules.
  */
