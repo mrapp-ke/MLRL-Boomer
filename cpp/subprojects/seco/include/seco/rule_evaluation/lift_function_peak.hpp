51b4b0c787afa0e0339d697f293f70d1436b2db3
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-22 00:44:23 +0100

    Add class PeakLiftFunctionFactory.

--- cpp/subprojects/seco/include/seco/rule_evaluation/lift_function_peak.hpp
+++ cpp/subprojects/seco/include/seco/rule_evaluation/lift_function_peak.hpp
@@ -42,4 +42,35 @@ namespace seco {
 
     };
 
+    /**
+     * Allows to create instances of the type `ILiftFunction` that monotonously increase until a certain number of
+     * labels, where the maximum lift is reached, and monotonously decrease afterwards.
+     */
+    class PeakLiftFunctionFactory final : public ILiftFunctionFactory {
+
+        private:
+
+            uint32 numLabels_;
+
+            uint32 peakLabel_;
+
+            float64 maxLift_;
+
+            float64 exponent_;
+
+        public:
+
+            /**
+             * @param numLabels The total number of available labels. Must be greater than 0
+             * @param peakLabel The number of labels for which the lift is maximum. Must be in [1, numLabels]
+             * @param maxLift   The lift at the peak label. Must be at least 1
+             * @param curvature The curvature of the lift function. A greater value results in a steeper curvature, a
+             *                  smaller value results in a flatter curvature. Must be greater than 0
+             */
+            PeakLiftFunctionFactory(uint32 numLabels, uint32 peakLabel, float64 maxLift, float64 curvature);
+
+            std::unique_ptr<ILiftFunction> create() const override;
+
+    };
+
 }
