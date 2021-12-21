11ff64d51f7a44c6aa9b197cc4504435c802aa56
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-21 23:00:19 +0100

    Move class ConstantShrinkage to cpp file.

--- cpp/subprojects/boosting/src/boosting/post_processing/shrinkage_constant.cpp
+++ cpp/subprojects/boosting/src/boosting/post_processing/shrinkage_constant.cpp
@@ -4,20 +4,36 @@
 
 namespace boosting {
 
-    ConstantShrinkage::ConstantShrinkage(float64 shrinkage)
-        : shrinkage_(shrinkage) {
-        assertGreater<float64>("shrinkage", shrinkage, 0);
-        assertLess<float64>("shrinkage", shrinkage, 1);
-    }
+    /**
+     * Post-processes the predictions of rules by shrinking their weights by a constant shrinkage parameter.
+     */
+    class ConstantShrinkage final : public IPostProcessor {
 
-    void ConstantShrinkage::postProcess(AbstractPrediction& prediction) const {
-        uint32 numElements = prediction.getNumElements();
-        AbstractPrediction::score_iterator iterator = prediction.scores_begin();
+        private:
 
-        for (uint32 i = 0; i < numElements; i++) {
-            iterator[i] *= shrinkage_;
-        }
-    }
+            float64 shrinkage_;
+
+        public:
+
+            /**
+             * @param shrinkage The shrinkage parameter. Must be in (0, 1)
+             */
+            ConstantShrinkage(float64 shrinkage)
+                : shrinkage_(shrinkage) {
+                assertGreater<float64>("shrinkage", shrinkage, 0);
+                assertLess<float64>("shrinkage", shrinkage, 1);
+            }
+
+            void postProcess(AbstractPrediction& prediction) const override {
+                uint32 numElements = prediction.getNumElements();
+                AbstractPrediction::score_iterator iterator = prediction.scores_begin();
+
+                for (uint32 i = 0; i < numElements; i++) {
+                    iterator[i] *= shrinkage_;
+                }
+            }
+
+    };
 
     ConstantShrinkageFactory::ConstantShrinkageFactory(float64 shrinkage)
         : shrinkage_(shrinkage) {
