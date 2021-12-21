fd3c4b29b5dfd86c7b545483bdc46c5a4190c8be
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-21 23:18:59 +0100

    Edit comments.

--- cpp/subprojects/seco/include/seco/heuristics/heuristic_f_measure.hpp
+++ cpp/subprojects/seco/include/seco/heuristics/heuristic_f_measure.hpp
@@ -10,10 +10,10 @@
 namespace seco {
 
     /**
-     * A heuristic that calculates as the (weighted) harmonic mean between the heuristics `Precision` and `Recall`,
-     * where the parameter `beta` allows to trade-off between both heuristics. If `beta = 1`, both heuristics are
-     * weighed equally. If `beta = 0`, this heuristic is equivalent to the heuristic `Precision`. As `beta` approaches
-     * infinity, this heuristic becomes equivalent to the heuristic `Recall`.
+     * A heuristic that calculates as the (weighted) harmonic mean between the heuristics "Precision" and "Recall",
+     * where the parameter "beta" allows to trade off between both heuristics. If beta = 1, both heuristics are weighed
+     * equally. If beta = 0, this heuristic is equivalent to "Precision". As beta approaches infinity, this heuristic
+     * becomes equivalent to "Recall".
      */
     class FMeasure final : public IHeuristic {
 
@@ -27,10 +27,7 @@ namespace seco {
         public:
 
             /**
-             * Creates a new heuristic that calculates as the (weighted) harmonic mean between the heuristics
-             * `Precision` and `Recall`.
-             *
-             * @param beta The value of the beta-parameter. Must be at least 0
+             * @param beta The value of the "beta" parameter. Must be at least 0
              */
             FMeasure(float64 beta);
 
