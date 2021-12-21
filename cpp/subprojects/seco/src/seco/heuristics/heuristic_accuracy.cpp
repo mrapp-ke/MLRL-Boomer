194ccf33bc95bee0469ffd5121ec0f3f2ae44824
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-22 00:02:12 +0100

    Move class Accuracy to cpp file.

--- cpp/subprojects/seco/src/seco/heuristics/heuristic_accuracy.cpp
+++ cpp/subprojects/seco/src/seco/heuristics/heuristic_accuracy.cpp
@@ -3,13 +3,23 @@
 
 namespace seco {
 
-    float64 Accuracy::evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
-                                              float64 uip, float64 urn, float64 urp) const {
-        float64 numUncoveredCorrect = urp + uin;
-        float64 numCoveredIncorrect = cip + crn;
-        float64 numTotal = numUncoveredCorrect + numCoveredIncorrect + cin + crp + uip + urn;
-        return (numUncoveredCorrect + numCoveredIncorrect) / numTotal;
-    }
+    /**
+     * An implementation of the type `IHeuristic` that measures the fraction of incorrectly predicted labels among all
+     * labels.
+     */
+    class Accuracy final : public IHeuristic {
+
+        public:
+
+            float64 evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
+                                            float64 uip, float64 urn, float64 urp) const override {
+                float64 numUncoveredCorrect = urp + uin;
+                float64 numCoveredIncorrect = cip + crn;
+                float64 numTotal = numUncoveredCorrect + numCoveredIncorrect + cin + crp + uip + urn;
+                return (numUncoveredCorrect + numCoveredIncorrect) / numTotal;
+            }
+
+    };
 
     std::unique_ptr<IHeuristic> AccuracyFactory::create() const {
         return std::make_unique<Accuracy>();
