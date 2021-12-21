e5e1ddbaae4cfd9804c8f36309f99afa5d305bc2
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-22 00:02:12 +0100

    Move class Accuracy to cpp file.

--- cpp/subprojects/seco/include/seco/heuristics/heuristic_accuracy.hpp
+++ cpp/subprojects/seco/include/seco/heuristics/heuristic_accuracy.hpp
@@ -9,22 +9,6 @@
 namespace seco {
 
     /**
-     * A heuristic that measures the fraction of incorrectly predicted labels among all labels, i.e., in contrast to the
-     * precision metric, examples that are not covered by a rule are taken into account as well.
-     *
-     * This heuristic is used in the pruning phase of IREP ("Incremental Reduced Error Pruning", Fürnkranz, Widmer 1994,
-     * see https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.43.7813&rep=rep1&type=pdf).
-     */
-    class Accuracy final : public IHeuristic {
-
-        public:
-
-            float64 evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
-                                            float64 uip, float64 urn, float64 urp) const override;
-
-    };
-
-    /**
      * Allows to create instances of the type `IHeuristic` that measure the fraction of incorrectly predicted labels
      * among all labels, i.e., in contrast to the precision metric, examples that are not covered by a rule are taken
      * into account as well.
