be9f414da6bc019d9a42e05302510eee963eb5bb
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-22 00:04:07 +0100

    Move class FMeasure to cpp file.

--- cpp/subprojects/seco/src/seco/heuristics/heuristic_f_measure.cpp
+++ cpp/subprojects/seco/src/seco/heuristics/heuristic_f_measure.cpp
@@ -6,33 +6,50 @@
 
 namespace seco {
 
-    FMeasure::FMeasure(float64 beta)
-        : beta_(beta) {
-        assertGreaterOrEqual<float64>("beta", beta, 0);
-    }
+    /**
+     * An implementation of the type `IHeuristic` that calculates as the (weighted) harmonic mean between the heuristics
+     * "Precision" and "Recall", where the parameter "beta" allows to trade off between both heuristics.
+     */
+    class FMeasure final : public IHeuristic {
 
-    float64 FMeasure::evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
-                                              float64 uip, float64 urn, float64 urp) const {
-        if (std::isinf(beta_)) {
-            // Equivalent to recall
-            return recall(cin, crp, uin, urp);
-        } else if (beta_ > 0) {
-            // Weighted harmonic mean between precision and recall
-            float64 numCoveredEqual = cin + crp;
-            float64 betaPow = beta_ * beta_;
-            float64 numerator = (1 + betaPow) * numCoveredEqual;
-            float64 denominator = numerator + (betaPow * (uin + urp)) + (cip + crn);
+        private:
 
-            if (denominator == 0) {
-                return 1;
+            float64 beta_;
+
+        public:
+
+            /**
+             * @param beta The value of the "beta" parameter. Must be at least 0
+             */
+            FMeasure(float64 beta)
+                : beta_(beta) {
+                assertGreaterOrEqual<float64>("beta", beta, 0);
             }
 
-            return 1 - (numerator / denominator);
-        } else {
-            // Equivalent to precision
-            return precision(cin, cip, crn, crp);
-        }
-    }
+            float64 evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
+                                            float64 uip, float64 urn, float64 urp) const override {
+                if (std::isinf(beta_)) {
+                    // Equivalent to recall
+                    return recall(cin, crp, uin, urp);
+                } else if (beta_ > 0) {
+                    // Weighted harmonic mean between precision and recall
+                    float64 numCoveredEqual = cin + crp;
+                    float64 betaPow = beta_ * beta_;
+                    float64 numerator = (1 + betaPow) * numCoveredEqual;
+                    float64 denominator = numerator + (betaPow * (uin + urp)) + (cip + crn);
+
+                    if (denominator == 0) {
+                        return 1;
+                    }
+
+                    return 1 - (numerator / denominator);
+                } else {
+                    // Equivalent to precision
+                    return precision(cin, cip, crn, crp);
+                }
+            }
+
+    };
 
     FMeasureFactory::FMeasureFactory(float64 beta)
         : beta_(beta) {
