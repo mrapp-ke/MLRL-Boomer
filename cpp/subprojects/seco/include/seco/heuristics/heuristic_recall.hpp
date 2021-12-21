1bf09b5698ed2040b863d1a33341b21abf555dd7
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-22 00:09:46 +0100

    Move class Recall to cpp file.

--- cpp/subprojects/seco/include/seco/heuristics/heuristic_recall.hpp
+++ cpp/subprojects/seco/include/seco/heuristics/heuristic_recall.hpp
@@ -10,19 +10,6 @@
 namespace seco {
 
     /**
-     * A heuristic that measures the fraction of uncovered labels among all labels for which the rule's prediction is
-     * (or would be) correct, i.e., for which the ground truth is equal to the rule's prediction.
-     */
-    class Recall final : public IHeuristic {
-
-        public:
-
-            float64 evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
-                                            float64 uip, float64 urn, float64 urp) const override;
-
-    };
-
-    /**
      * Allows to create instances of the type `IHeuristic` that measure the fraction of uncovered labels among all
      * labels for which a rule's prediction is (or would be) correct, i.e., for which the ground truth is equal to the
      * rule's prediction.
--- cpp/subprojects/seco/src/seco/heuristics/heuristic_recall.cpp
+++ cpp/subprojects/seco/src/seco/heuristics/heuristic_recall.cpp
@@ -4,10 +4,20 @@
 
 namespace seco {
 
-    float64 Recall::evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
-                                            float64 uip, float64 urn, float64 urp) const {
-        return recall(cin, crp, uin, urp);
-    }
+    /**
+     * An implementation of the type `IHeuristic` that measures the fraction of uncovered labels among all labels for
+     * which the rule's prediction is (or would be) correct.
+     */
+    class Recall final : public IHeuristic {
+
+        public:
+
+            float64 evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
+                                            float64 uip, float64 urn, float64 urp) const override {
+                return recall(cin, crp, uin, urp);
+            }
+
+    };
 
     std::unique_ptr<IHeuristic> RecallFactory::create() const {
         return std::make_unique<Recall>();
