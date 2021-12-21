f85d046d3aa3bbb1153be404ac88fd344c24bd00
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-22 00:11:09 +0100

    Move class WRA to cpp file.

--- cpp/subprojects/seco/include/seco/heuristics/heuristic_wra.hpp
+++ cpp/subprojects/seco/include/seco/heuristics/heuristic_wra.hpp
@@ -10,18 +10,6 @@
 namespace seco {
 
     /**
-     * A heuristic that calculates as `1 - wra`, where `wra` corresponds to the weighted relative accuracy metric.
-     */
-    class WRA final : public IHeuristic {
-
-        public:
-
-            float64 evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
-                                            float64 uip, float64 urn, float64 urp) const override;
-
-    };
-
-    /**
      * Allows to create instances of the type `IHeuristic` that calculate as `1 - wra`, where `wra` corresponds to the
      * "Weighted Relative Accuracy" metric.
      */
