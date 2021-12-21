34c2895ca1ae98e379fb5af0c642ad61a7d76c02
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-22 00:07:32 +0100

    Move class MEstimate to cpp file.

--- cpp/subprojects/seco/include/seco/heuristics/heuristic_m_estimate.hpp
+++ cpp/subprojects/seco/include/seco/heuristics/heuristic_m_estimate.hpp
@@ -10,33 +10,10 @@
 namespace seco {
 
     /**
-     * A heuristic that allows to trade off between the heuristics "Precision2 and "WRA". The "m" parameter allows to
-     * control the trade-off between both heuristics. If m = 0, this heuristic is equivalent to "Precision". As m
-     * approaches infinity, the isometrics of this heuristic become equivalent to those of "WRA".
-     */
-    class MEstimate final : public IHeuristic {
-
-        private:
-
-            float64 m_;
-
-        public:
-
-            /**
-             * @param m The value of the "m" parameter. Must be at least 0
-             */
-            MEstimate(float64 m);
-
-            float64 evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
-                                            float64 uip, float64 urn, float64 urp) const override;
-
-    };
-
-    /**
      * Allows to create instances of the type `IHeuristic` that allow to trade off between the heuristics "Precision"
-     * and "WRA". The "m" parameter allows to control the trade-off between both heuristics. If m = 0, this heuristic is
-     * equivalent to "Precision". As m approaches infinity, the isometrics of this heuristic become equivalent to those
-     * of "WRA".
+     * and "WRA", where the "m" parameter allows to control the trade-off between both heuristics. If m = 0, this
+     * heuristic is equivalent to "Precision". As m approaches infinity, the isometrics of this heuristic become
+     * equivalent to those of "WRA".
      */
     class MEstimateFactory final : public IHeuristicFactory {
 
