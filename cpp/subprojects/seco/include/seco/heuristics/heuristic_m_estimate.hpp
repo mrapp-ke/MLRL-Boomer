ffda210c1e7038cdfe176299699c8bc8c9d00f14
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-21 23:26:05 +0100

    Edit comments.

--- cpp/subprojects/seco/include/seco/heuristics/heuristic_m_estimate.hpp
+++ cpp/subprojects/seco/include/seco/heuristics/heuristic_m_estimate.hpp
@@ -10,26 +10,20 @@
 namespace seco {
 
     /**
-     * A heuristic that allows to trade off between the heuristics `Precision` and `WRA`. The `m`-parameter allows to
-     * control the trade-off between both heuristics. If `m = 0`, this heuristic is equivalent to the heuristic
-     * `Precision`. As `m` approaches infinity, the isometrics of this heuristic become equivalent to those of the
-     * heuristic `WRA`.
+     * A heuristic that allows to trade off between the heuristics "Precision2 and "WRA". The "m" parameter allows to
+     * control the trade-off between both heuristics. If m = 0, this heuristic is equivalent to "Precision". As m
+     * approaches infinity, the isometrics of this heuristic become equivalent to those of "WRA".
      */
     class MEstimate final : public IHeuristic {
 
         private:
 
-            /**
-             * The value of the m-parameter.
-             */
             float64 m_;
 
         public:
 
             /**
-             * Creates a new heuristic that allows to trade off between the heuristics `Precision` and `WRA`.
-             *
-             * @param m The value of the m-parameter. Must be at least 0
+             * @param m The value of the "m" parameter. Must be at least 0
              */
             MEstimate(float64 m);
 
