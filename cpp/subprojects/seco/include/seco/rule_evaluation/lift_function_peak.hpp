d2b92235b0c0da522939cdc7a64e73dc3be8267b
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-22 00:57:34 +0100

    Move class PeakLiftFunction to cpp file.

--- cpp/subprojects/seco/include/seco/rule_evaluation/lift_function_peak.hpp
+++ cpp/subprojects/seco/include/seco/rule_evaluation/lift_function_peak.hpp
@@ -10,39 +10,6 @@
 namespace seco {
 
     /**
-     * A lift function that monotonously increases until a certain number of labels, where the maximum lift is reached,
-     * and monotonously decreases afterwards.
-     */
-    class PeakLiftFunction final : public ILiftFunction {
-
-        private:
-
-            uint32 numLabels_;
-
-            uint32 peakLabel_;
-
-            float64 maxLift_;
-
-            float64 exponent_;
-
-        public:
-
-            /**
-             * @param numLabels The total number of available labels. Must be greater than 0
-             * @param peakLabel The number of labels for which the lift is maximum. Must be in [1, numLabels]
-             * @param maxLift   The lift at the peak label. Must be at least 1
-             * @param curvature The curvature of the lift function. A greater value results in a steeper curvature, a
-             *                  smaller value results in a flatter curvature. Must be greater than 0
-             */
-            PeakLiftFunction(uint32 numLabels, uint32 peakLabel, float64 maxLift, float64 curvature);
-
-            float64 calculateLift(uint32 numLabels) const override;
-
-            float64 getMaxLift() const override;
-
-    };
-
-    /**
      * Allows to create instances of the type `ILiftFunction` that monotonously increase until a certain number of
      * labels, where the maximum lift is reached, and monotonously decrease afterwards.
      */
