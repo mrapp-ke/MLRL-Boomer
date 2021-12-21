61da33aad75a1adeb3732052e719dd305d55a171
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-22 00:05:09 +0100

    Move class Laplace to cpp file.

--- cpp/subprojects/seco/src/seco/heuristics/heuristic_laplace.cpp
+++ cpp/subprojects/seco/src/seco/heuristics/heuristic_laplace.cpp
@@ -3,12 +3,21 @@
 
 namespace seco {
 
-    float64 Laplace::evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
-                                             float64 uip, float64 urn, float64 urp) const {
-        float64 numCoveredIncorrect = cip + crn;
-        float64 numCovered = numCoveredIncorrect + cin + crp;
-        return (numCoveredIncorrect + 1) / (numCovered + 2);
-    }
+    /**
+     * An implementation of the type `IHeuristic` that implements a Laplace-corrected variant of the "Precision" metric.
+     */
+    class Laplace final : public IHeuristic {
+
+        public:
+
+            float64 evaluateConfusionMatrix(float64 cin, float64 cip, float64 crn, float64 crp, float64 uin,
+                                            float64 uip, float64 urn, float64 urp) const override {
+                float64 numCoveredIncorrect = cip + crn;
+                float64 numCovered = numCoveredIncorrect + cin + crp;
+                return (numCoveredIncorrect + 1) / (numCovered + 2);
+            }
+
+    };
 
     std::unique_ptr<IHeuristic> LaplaceFactory::create() const {
         return std::make_unique<Laplace>();
