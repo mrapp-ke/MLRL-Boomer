db58e2b978bb2622fa2b10141dfa51e9e4b68752
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-22 15:20:15 +0100

    The class ApproximateThresholdsFactory now takes a IFeatureBinningFactory as a constructor argument.

--- python/subprojects/common/mlrl/common/cython/feature_binning.pyx
+++ python/subprojects/common/mlrl/common/cython/feature_binning.pyx
@@ -5,32 +5,16 @@
 from libcpp.memory cimport make_unique
 
 
-cdef class FeatureBinning:
+cdef class FeatureBinningFactory:
     """
-    A wrapper for the pure virtual C++ class `IFeatureBinning`.
+    A wrapper for the pure virtual C++ class `IFeatureBinningFactory`.
     """
     pass
 
 
-cdef class EqualFrequencyFeatureBinning(FeatureBinning):
+cdef class EqualFrequencyFeatureBinningFactory(FeatureBinningFactory):
     """
-    A wrapper for the C++ class `EqualFrequencyFeatureBinning`.
-    """
-
-    def __cinit__(self, float32 bin_ratio, uint32 min_bins, uint32 max_bins):
-        """
-        :param bin_ratio:   A percentage that specifies how many bins should be used
-        :param min_bins:    The minimum number of bins to be used
-        :param max_bins:    The maximum number of bins to be used
-        """
-        self.binning_ptr = <unique_ptr[IFeatureBinning]>make_unique[EqualFrequencyFeatureBinningImpl](bin_ratio,
-                                                                                                      min_bins,
-                                                                                                      max_bins)
-
-
-cdef class EqualWidthFeatureBinning(FeatureBinning):
-    """
-    A wrapper for the C++ class `EqualWidthFeatureBinning`.
+    A wrapper for the C++ class `EqualFrequencyFeatureBinningFactory`.
     """
 
     def __cinit__(self, float32 bin_ratio, uint32 min_bins, uint32 max_bins):
@@ -39,5 +23,20 @@ cdef class EqualWidthFeatureBinning(FeatureBinning):
         :param min_bins:    The minimum number of bins to be used
         :param max_bins:    The maximum number of bins to be used
         """
-        self.binning_ptr = <unique_ptr[IFeatureBinning]>make_unique[EqualWidthFeatureBinningImpl](bin_ratio, min_bins,
-                                                                                                  max_bins)
+        self.feature_binning_factory_ptr = <unique_ptr[IFeatureBinningFactory]>make_unique[EqualFrequencyFeatureBinningFactoryImpl](
+            bin_ratio, min_bins, max_bins)
+
+
+cdef class EqualWidthFeatureBinningFactory(FeatureBinningFactory):
+    """
+    A wrapper for the C++ class `EqualWidthFeatureBinningFactory`.
+    """
+
+    def __cinit__(self, float32 bin_ratio, uint32 min_bins, uint32 max_bins):
+        """
+        :param bin_ratio:   A percentage that specifies how many bins should be used
+        :param min_bins:    The minimum number of bins to be used
+        :param max_bins:    The maximum number of bins to be used
+        """
+        self.feature_binning_factory_ptr = <unique_ptr[IFeatureBinningFactory]>make_unique[EqualWidthFeatureBinningFactoryImpl](
+            bin_ratio, min_bins, max_bins)
