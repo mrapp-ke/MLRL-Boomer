5e1ebb8611043cf214f25d4f75f3ff653d542b9f
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-23 03:24:45 +0100

    Move class TimeStoppingCriterion to cpp file.

--- cpp/subprojects/common/include/common/stopping/stopping_criterion_time.hpp
+++ cpp/subprojects/common/include/common/stopping/stopping_criterion_time.hpp
@@ -4,36 +4,7 @@
 #pragma once
 
 #include "common/stopping/stopping_criterion.hpp"
-#include <chrono>
 
-
-/**
- * A stopping criterion that ensures that a certain time limit is not exceeded.
- */
-class TimeStoppingCriterion final : public IStoppingCriterion {
-
-    private:
-
-        typedef std::chrono::steady_clock timer;
-
-        typedef std::chrono::seconds timer_unit;
-
-        timer_unit timeLimit_;
-
-        std::chrono::time_point<timer> startTime_;
-
-        bool timerStarted_;
-
-    public:
-
-        /**
-         * @param timeLimit The time limit in seconds. Must be at least 1
-         */
-        TimeStoppingCriterion(uint32 timeLimit);
-
-        Result test(const IPartition& partition, const IStatistics& statistics, uint32 numRules) override;
-
-};
 
 /**
  * Allows to create instances of the type `IStoppingCriterion` that ensure that a certain time limit is not exceeded.
--- cpp/subprojects/common/src/common/stopping/stopping_criterion_time.cpp
+++ cpp/subprojects/common/src/common/stopping/stopping_criterion_time.cpp
@@ -1,35 +1,59 @@
 #include "common/stopping/stopping_criterion_time.hpp"
 #include "common/validation.hpp"
+#include <chrono>
 
 
-TimeStoppingCriterion::TimeStoppingCriterion(uint32 timeLimit)
-    : timeLimit_(std::chrono::duration_cast<timer_unit>(std::chrono::seconds(timeLimit))), startTime_(timer::now()),
-      timerStarted_(false) {
+/**
+ * An implementation of the type `IStoppingCriterion` that ensures that a certain time limit is not exceeded.
+ */
+class TimeStoppingCriterion final : public IStoppingCriterion {
 
-}
+    private:
 
-IStoppingCriterion::Result TimeStoppingCriterion::test(const IPartition& partition, const IStatistics& statistics,
-                                                       uint32 numRules) {
-    Result result;
+        typedef std::chrono::steady_clock timer;
 
-    if (timerStarted_) {
-        auto currentTime = timer::now();
-        auto duration = std::chrono::duration_cast<timer_unit>(currentTime - startTime_);
+        typedef std::chrono::seconds timer_unit;
 
-        if (duration < timeLimit_) {
-            result.action = CONTINUE;
-        } else {
-            result.action = FORCE_STOP;
-            result.numRules = numRules;
+        timer_unit timeLimit_;
+
+        std::chrono::time_point<timer> startTime_;
+
+        bool timerStarted_;
+
+    public:
+
+        /**
+         * @param timeLimit The time limit in seconds. Must be at least 1
+         */
+        TimeStoppingCriterion(uint32 timeLimit)
+            : timeLimit_(std::chrono::duration_cast<timer_unit>(std::chrono::seconds(timeLimit))),
+              startTime_(timer::now()), timerStarted_(false) {
+
         }
-    } else {
-        startTime_ = timer::now();
-        timerStarted_ = true;
-        result.action = CONTINUE;
-    }
 
-    return result;
-}
+        Result test(const IPartition& partition, const IStatistics& statistics, uint32 numRules) override {
+            Result result;
+
+            if (timerStarted_) {
+                auto currentTime = timer::now();
+                auto duration = std::chrono::duration_cast<timer_unit>(currentTime - startTime_);
+
+                if (duration < timeLimit_) {
+                    result.action = CONTINUE;
+                } else {
+                    result.action = FORCE_STOP;
+                    result.numRules = numRules;
+                }
+            } else {
+                startTime_ = timer::now();
+                timerStarted_ = true;
+                result.action = CONTINUE;
+            }
+
+            return result;
+        }
+
+};
 
 TimeStoppingCriterionFactory::TimeStoppingCriterionFactory(uint32 timeLimit)
     : timeLimit_(timeLimit) {
