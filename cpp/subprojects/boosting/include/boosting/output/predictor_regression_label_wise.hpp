c0d40bc23f8388fcf7f0b1ef783cc80f39a96096
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-29 19:18:34 +0100

    Edit comment.

--- cpp/subprojects/boosting/include/boosting/output/predictor_regression_label_wise.hpp
+++ cpp/subprojects/boosting/include/boosting/output/predictor_regression_label_wise.hpp
@@ -55,8 +55,8 @@ namespace boosting {
 
     /**
      * An implementation of the type `ILabelWiseRegressionPredictor` that allows to predict regression scores for given
-     * query examples by summing up the scores that are provided by the individual rules of an existing model for each
-     * label individually.
+     * query examples by summing up the scores that are provided by the individual rules of an existing rule-based model
+     * for each label individually.
      */
     class LabelWiseRegressionPredictor final : public ILabelWiseRegressionPredictor {
 
