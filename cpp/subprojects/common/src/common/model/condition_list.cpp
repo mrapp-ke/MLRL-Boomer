5e981f62c385c471c0e5bbfdb3c65cabafd250f1
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-29 17:43:54 +0100

    Remove unused function.

--- cpp/subprojects/common/src/common/model/condition_list.cpp
+++ cpp/subprojects/common/src/common/model/condition_list.cpp
@@ -9,14 +9,6 @@ ConditionList::const_iterator ConditionList::cend() const {
     return list_.cend();
 }
 
-ConditionList::size_type ConditionList::getNumConditions() const {
-    return list_.size();
-}
-
-uint32 ConditionList::getNumConditions(Comparator comparator) const {
-    return numConditionsPerComparator_[comparator];
-}
-
 void ConditionList::addCondition(const Condition& condition) {
     numConditionsPerComparator_[condition.comparator] += 1;
     list_.emplace_back(condition);
