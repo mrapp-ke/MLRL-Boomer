8e179bca4ffa5b42a85af6c83f2bd7adc094e590
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-30 00:45:18 +0100

    Move class LabelWiseProbabilityPredictor to cpp file.

--- cpp/subprojects/boosting/src/boosting/output/predictor_probability_label_wise.cpp
+++ cpp/subprojects/boosting/src/boosting/output/predictor_probability_label_wise.cpp
@@ -32,74 +32,99 @@ namespace boosting {
         }
     }
 
-    LabelWiseProbabilityPredictor::LabelWiseProbabilityPredictor(
-            std::unique_ptr<IProbabilityFunction> probabilityFunctionPtr, uint32 numThreads)
-        : probabilityFunctionPtr_(std::move(probabilityFunctionPtr)), numThreads_(numThreads) {
+    /**
+     * An implementation of the type `ILabelWiseProbabilityPredictor` that allows to predict label-wise probabilities
+     * for given query examples, which estimate the chance of individual labels to be relevant, by summing up the scores
+     * that are provided by individual rules of an existing rule-based models and transforming the aggregated scores
+     * into probabilities in [0, 1] according to a certain transformation function that is applied to each label
+     * individually.
+     */
+    class LabelWiseProbabilityPredictor final : public IProbabilityPredictor {
 
-    }
+        private:
 
-    void LabelWiseProbabilityPredictor::predict(const CContiguousFeatureMatrix& featureMatrix,
-                                                CContiguousView<float64>& predictionMatrix,
-                                                const RuleModel& model, const LabelVectorSet* labelVectors) const {
-        uint32 numExamples = featureMatrix.getNumRows();
-        uint32 numLabels = predictionMatrix.getNumCols();
-        const CContiguousFeatureMatrix* featureMatrixPtr = &featureMatrix;
-        CContiguousView<float64>* predictionMatrixPtr = &predictionMatrix;
-        const RuleModel* modelPtr = &model;
-        const IProbabilityFunction* probabilityFunctionPtr = probabilityFunctionPtr_.get();
+            std::unique_ptr<IProbabilityFunction> probabilityFunctionPtr_;
 
-        #pragma omp parallel for firstprivate(numExamples) firstprivate(numLabels) firstprivate(modelPtr) \
-        firstprivate(featureMatrixPtr) firstprivate(predictionMatrixPtr) firstprivate(probabilityFunctionPtr) \
-        schedule(dynamic) num_threads(numThreads_)
-        for (int64 i = 0; i < numExamples; i++) {
-            float64* scoreVector = new float64[numLabels] {};
+            uint32 numThreads_;
 
-            for (auto it = modelPtr->used_cbegin(); it != modelPtr->used_cend(); it++) {
-                const Rule& rule = *it;
-                applyRule(rule, featureMatrixPtr->row_cbegin(i), featureMatrixPtr->row_cend(i), &scoreVector[0]);
+        public:
+
+            /**
+             * @param probabilityFunctionPtr    An unique pointer to an object of type `IProbabilityFunction` that
+             *                                  should be used to transform predicted scores into probabilities
+             * @param numThreads                The number of CPU threads to be used to make predictions for different
+             *                                  query examples in parallel. Must be at least 1
+             */
+            LabelWiseProbabilityPredictor(std::unique_ptr<IProbabilityFunction> probabilityFunctionPtr,
+                                          uint32 numThreads)
+                : probabilityFunctionPtr_(std::move(probabilityFunctionPtr)), numThreads_(numThreads) {
+
             }
 
-            applyTransformationFunction(&scoreVector[0], predictionMatrixPtr->row_begin(i), numLabels,
-                                        *probabilityFunctionPtr);
-            delete[] scoreVector;
-        }
-    }
+            void predict(const CContiguousFeatureMatrix& featureMatrix, CContiguousView<float64>& predictionMatrix,
+                         const RuleModel& model, const LabelVectorSet* labelVectors) const override {
+                uint32 numExamples = featureMatrix.getNumRows();
+                uint32 numLabels = predictionMatrix.getNumCols();
+                const CContiguousFeatureMatrix* featureMatrixPtr = &featureMatrix;
+                CContiguousView<float64>* predictionMatrixPtr = &predictionMatrix;
+                const RuleModel* modelPtr = &model;
+                const IProbabilityFunction* probabilityFunctionPtr = probabilityFunctionPtr_.get();
 
-    void LabelWiseProbabilityPredictor::predict(const CsrFeatureMatrix& featureMatrix,
-                                                CContiguousView<float64>& predictionMatrix,
-                                                const RuleModel& model, const LabelVectorSet* labelVectors) const {
-        uint32 numExamples = featureMatrix.getNumRows();
-        uint32 numLabels = predictionMatrix.getNumCols();
-        uint32 numFeatures = featureMatrix.getNumCols();
-        const CsrFeatureMatrix* featureMatrixPtr = &featureMatrix;
-        CContiguousView<float64>* predictionMatrixPtr = &predictionMatrix;
-        const RuleModel* modelPtr = &model;
-        const IProbabilityFunction* probabilityFunctionPtr = probabilityFunctionPtr_.get();
+                #pragma omp parallel for firstprivate(numExamples) firstprivate(numLabels) firstprivate(modelPtr) \
+                firstprivate(featureMatrixPtr) firstprivate(predictionMatrixPtr) firstprivate(probabilityFunctionPtr) \
+                schedule(dynamic) num_threads(numThreads_)
+                for (int64 i = 0; i < numExamples; i++) {
+                    float64* scoreVector = new float64[numLabels] {};
 
-        #pragma omp parallel for firstprivate(numExamples) firstprivate(numLabels) firstprivate(numFeatures) \
-        firstprivate(modelPtr) firstprivate(featureMatrixPtr) firstprivate(predictionMatrixPtr) \
-        firstprivate(probabilityFunctionPtr) schedule(dynamic) num_threads(numThreads_)
-        for (int64 i = 0; i < numExamples; i++) {
-            float64* scoreVector = new float64[numLabels] {};
-            float32* tmpArray1 = new float32[numFeatures];
-            uint32* tmpArray2 = new uint32[numFeatures] {};
-            uint32 n = 1;
+                    for (auto it = modelPtr->used_cbegin(); it != modelPtr->used_cend(); it++) {
+                        const Rule& rule = *it;
+                        applyRule(rule, featureMatrixPtr->row_cbegin(i), featureMatrixPtr->row_cend(i),
+                                  &scoreVector[0]);
+                    }
 
-            for (auto it = modelPtr->used_cbegin(); it != modelPtr->used_cend(); it++) {
-                const Rule& rule = *it;
-                applyRuleCsr(rule, featureMatrixPtr->row_indices_cbegin(i), featureMatrixPtr->row_indices_cend(i),
-                             featureMatrixPtr->row_values_cbegin(i), featureMatrixPtr->row_values_cend(i),
-                             &scoreVector[0], &tmpArray1[0], &tmpArray2[0], n);
-                n++;
+                    applyTransformationFunction(&scoreVector[0], predictionMatrixPtr->row_begin(i), numLabels,
+                                                *probabilityFunctionPtr);
+                    delete[] scoreVector;
+                }
             }
 
-            applyTransformationFunction(&scoreVector[0], predictionMatrixPtr->row_begin(i), numLabels,
-                                        *probabilityFunctionPtr);
-            delete[] scoreVector;
-            delete[] tmpArray1;
-            delete[] tmpArray2;
-        }
-    }
+            void predict(const CsrFeatureMatrix& featureMatrix, CContiguousView<float64>& predictionMatrix,
+                         const RuleModel& model, const LabelVectorSet* labelVectors) const override {
+                uint32 numExamples = featureMatrix.getNumRows();
+                uint32 numLabels = predictionMatrix.getNumCols();
+                uint32 numFeatures = featureMatrix.getNumCols();
+                const CsrFeatureMatrix* featureMatrixPtr = &featureMatrix;
+                CContiguousView<float64>* predictionMatrixPtr = &predictionMatrix;
+                const RuleModel* modelPtr = &model;
+                const IProbabilityFunction* probabilityFunctionPtr = probabilityFunctionPtr_.get();
+
+                #pragma omp parallel for firstprivate(numExamples) firstprivate(numLabels) firstprivate(numFeatures) \
+                firstprivate(modelPtr) firstprivate(featureMatrixPtr) firstprivate(predictionMatrixPtr) \
+                firstprivate(probabilityFunctionPtr) schedule(dynamic) num_threads(numThreads_)
+                for (int64 i = 0; i < numExamples; i++) {
+                    float64* scoreVector = new float64[numLabels] {};
+                    float32* tmpArray1 = new float32[numFeatures];
+                    uint32* tmpArray2 = new uint32[numFeatures] {};
+                    uint32 n = 1;
+
+                    for (auto it = modelPtr->used_cbegin(); it != modelPtr->used_cend(); it++) {
+                        const Rule& rule = *it;
+                        applyRuleCsr(rule, featureMatrixPtr->row_indices_cbegin(i),
+                                     featureMatrixPtr->row_indices_cend(i), featureMatrixPtr->row_values_cbegin(i),
+                                     featureMatrixPtr->row_values_cend(i), &scoreVector[0], &tmpArray1[0],
+                                     &tmpArray2[0], n);
+                        n++;
+                    }
+
+                    applyTransformationFunction(&scoreVector[0], predictionMatrixPtr->row_begin(i), numLabels,
+                                                *probabilityFunctionPtr);
+                    delete[] scoreVector;
+                    delete[] tmpArray1;
+                    delete[] tmpArray2;
+                }
+            }
+
+    };
 
     std::unique_ptr<IProbabilityFunction> LogisticFunctionFactory::create() const {
         return std::make_unique<LogisticFunction>();
