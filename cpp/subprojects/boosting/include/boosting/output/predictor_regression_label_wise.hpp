5873b2cb325638fb8f412ec0d56198fee89abc69
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-29 22:53:54 +0100

    Add class LabelWiseRegressionPredictorFactory.

--- cpp/subprojects/boosting/include/boosting/output/predictor_regression_label_wise.hpp
+++ cpp/subprojects/boosting/include/boosting/output/predictor_regression_label_wise.hpp
@@ -41,4 +41,27 @@ namespace boosting {
 
     };
 
+    /**
+     * Allows to create instances of the type `IRegressionPredictor` that allow to predict label-wise regression scores
+     * for given query examples by summing up the scores that are provided by the individual rules of an existing
+     * rule-based model for each label individually.
+     */
+    class LabelWiseRegressionPredictorFactory final : public IRegressionPredictorFactory {
+
+        private:
+
+            uint32 numThreads_;
+
+        public:
+
+            /**
+             * @param numThreads The number of CPU threads to be used to make predictions for different query examples
+             *                   in parallel. Must be at least 1
+             */
+            LabelWiseRegressionPredictorFactory(uint32 numThreads);
+
+            std::unique_ptr<IRegressionPredictor> create(const RuleModel& model) const override;
+
+    };
+
 }
