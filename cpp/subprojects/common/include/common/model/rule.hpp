4f3f8f273f27318a251003f281ae47a6d712614d
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-30 23:07:00 +0100

    Add class IRule.

--- cpp/subprojects/common/include/common/model/rule.hpp
+++ cpp/subprojects/common/include/common/model/rule.hpp
@@ -9,9 +9,48 @@
 
 
 /**
+ * Defines an interface for all rules that consist of a body and a head.
+ */
+class IRule {
+
+    public:
+
+        virtual ~IRule() { };
+
+        /**
+         * Returns the body of the rule.
+         *
+         * @return A reference to an object of type `IBody` that represents the body of the rule
+         */
+        virtual const IBody& getBody() const = 0;
+
+        /**
+         * Returns the head of the rule.
+         *
+         * @return A reference to an object of type `IHead` that represents the head of the rule
+         */
+        virtual const IHead& getHead() const = 0;
+
+        /**
+         * Invokes some of the given visitor functions, depending on which ones are able to handle the rule's particular
+         * type of body and head.
+         *
+         * @param emptyBodyVisitor          The visitor function for handling objects of the type `EmptyBody`
+         * @param conjunctiveBodyVisitor    The visitor function for handling objects of the type `ConjunctiveBody`
+         * @param completeHeadVisitor       The visitor function for handling objects of the type `CompleteHead`
+         * @param partialHeadVisitor        The visitor function for handling objects of the type `PartialHead`
+         */
+        virtual void visit(IBody::EmptyBodyVisitor emptyBodyVisitor,
+                           IBody::ConjunctiveBodyVisitor conjunctiveBodyVisitor,
+                           IHead::CompleteHeadVisitor completeHeadVisitor,
+                           IHead::PartialHeadVisitor partialHeadVisitor) const = 0;
+
+};
+
+/**
  * A rule that consists of a body and a head.
  */
-class Rule final {
+class Rule final : public IRule {
 
     private:
 
@@ -27,30 +66,13 @@ class Rule final {
          */
         Rule(std::unique_ptr<IBody> bodyPtr, std::unique_ptr<IHead> headPtr);
 
-        /**
-         * Returns the body of the rule.
-         *
-         * @return A reference to an object of type `IBody` that represents the body of the rule
-         */
-        const IBody& getBody() const;
+        const IBody& getBody() const override;
 
-        /**
-         * Returns the head of the rule.
-         *
-         * @return A reference to an object of type `IHead` that represents the head of the rule
-         */
-        const IHead& getHead() const;
+        const IHead& getHead() const override;
 
-        /**
-         * Invokes some of the given visitor functions, depending on which ones are able to handle the rule's particular
-         * type of body and head.
-         *
-         * @param emptyBodyVisitor          The visitor function for handling objects of the type `EmptyBody`
-         * @param conjunctiveBodyVisitor    The visitor function for handling objects of the type `ConjunctiveBody`
-         * @param completeHeadVisitor       The visitor function for handling objects of the type `CompleteHead`
-         * @param partialHeadVisitor        The visitor function for handling objects of the type `PartialHead`
-         */
-        void visit(IBody::EmptyBodyVisitor emptyBodyVisitor, IBody::ConjunctiveBodyVisitor conjunctiveBodyVisitor,
-                   IHead::CompleteHeadVisitor completeHeadVisitor, IHead::PartialHeadVisitor partialHeadVisitor) const;
+        void visit(IBody::EmptyBodyVisitor emptyBodyVisitor,
+                   IBody::ConjunctiveBodyVisitor conjunctiveBodyVisitor,
+                   IHead::CompleteHeadVisitor completeHeadVisitor,
+                   IHead::PartialHeadVisitor partialHeadVisitor) const override;
 
 };
