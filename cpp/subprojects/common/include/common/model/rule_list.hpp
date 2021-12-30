9d5bd682b449688f21779d0fb4e84ede03e212cc
Michael Rapp <michael.rapp90@googlemail.com>
2021-12-30 22:38:19 +0100

    Add class IRuleModel.

--- cpp/subprojects/common/include/common/model/rule_list.hpp
+++ cpp/subprojects/common/include/common/model/rule_list.hpp
@@ -3,23 +3,15 @@
  */
 #pragma once
 
-#include "common/model/rule.hpp"
+#include "common/model/rule_model.hpp"
 #include <forward_list>
 #include <iterator>
 
-// Forward declarations
-class IClassificationPredictorFactory;
-class IClassificationPredictor;
-class IRegressionPredictorFactory;
-class IRegressionPredictor;
-class IProbabilityPredictorFactory;
-class IProbabilityPredictor;
-
 
 /**
- * A model that stores several rules in a list.
+ * An implementation of the type `IRuleModel` that stores several rules in a list.
  */
-class RuleList final {
+class RuleList final : public IRuleModel {
 
     private:
 
@@ -156,92 +148,32 @@ class RuleList final {
          */
         used_const_iterator used_cend() const;
 
-        /**
-         * Returns the total number of rules in the model.
-         *
-         * @return The number of rules
-         */
-        uint32 getNumRules() const;
+        uint32 getNumRules() const override;
 
-        /**
-         * Returns the number of used rules.
-         *
-         * @return The number of used rules
-         */
-        uint32 getNumUsedRules() const;
+        uint32 getNumUsedRules() const override;
 
-        /**
-         * Sets the number of used rules.
-         *
-         * @param numUsedRules The number of used rules to be set or 0, if all rules are used
-         */
-        void setNumUsedRules(uint32 numUsedRules);
+        void setNumUsedRules(uint32 numUsedRules) override;
 
-        /**
-         * Creates a new rule from a given body and head and adds it to the model.
-         *
-         * @param bodyPtr An unique pointer to an object of type `IBody` that should be used as the body of the rule
-         * @param headPtr An unique pointer to an object of type `IHead` that should be used as the head of the rule
-         */
-        void addRule(std::unique_ptr<IBody> bodyPtr, std::unique_ptr<IHead> headPtr);
+        void addRule(std::unique_ptr<IBody> bodyPtr, std::unique_ptr<IHead> headPtr) override;
 
-        /**
-         * Invokes some of the given visitor functions, depending on which ones are able to handle the bodies and heads
-         * of the rules that are contained in this model.
-         *
-         * @param emptyBodyVisitor          The visitor function for handling objects of the type `EmptyBody`
-         * @param conjunctiveBodyVisitor    The visitor function for handling objects of the type `ConjunctiveBody`
-         * @param completeHeadVisitor       The visitor function for handling objects of the type `CompleteHead`
-         * @param partialHeadVisitor        The visitor function for handling objects of the type `PartialHead`
-         */
-        void visit(IBody::EmptyBodyVisitor emptyBodyVisitor, IBody::ConjunctiveBodyVisitor conjunctiveBodyVisitor,
-                   IHead::CompleteHeadVisitor completeHeadVisitor, IHead::PartialHeadVisitor partialHeadVisitor) const;
+        void visit(IBody::EmptyBodyVisitor emptyBodyVisitor,
+                   IBody::ConjunctiveBodyVisitor conjunctiveBodyVisitor,
+                   IHead::CompleteHeadVisitor completeHeadVisitor,
+                   IHead::PartialHeadVisitor partialHeadVisitor) const override;
 
 
-        /**
-         * Invokes some of the given visitor functions, depending on which ones are able to handle the bodies and heads
-         * of the used rules that are contained in this model.
-         *
-         * @param emptyBodyVisitor          The visitor function for handling objects of the type `EmptyBody`
-         * @param conjunctiveBodyVisitor    The visitor function for handling objects of the type `ConjunctiveBody`
-         * @param completeHeadVisitor       The visitor function for handling objects of the type `CompleteHead`
-         * @param partialHeadVisitor        The visitor function for handling objects of the type `PartialHead`
-         */
-        void visitUsed(IBody::EmptyBodyVisitor emptyBodyVisitor, IBody::ConjunctiveBodyVisitor conjunctiveBodyVisitor,
+        void visitUsed(IBody::EmptyBodyVisitor emptyBodyVisitor,
+                       IBody::ConjunctiveBodyVisitor conjunctiveBodyVisitor,
                        IHead::CompleteHeadVisitor completeHeadVisitor,
-                       IHead::PartialHeadVisitor partialHeadVisitor) const;
+                       IHead::PartialHeadVisitor partialHeadVisitor) const override;
 
-        /**
-         * Creates and returns a new instance of the class `IClassificationPredictor`, based on the type of this
-         * rule-based model.
-         *
-         * @param factory   A reference to an object of type `IClassificationPredictorFactory` that should be used to
-         *                  create the instance
-         * @return          An unique pointer to an object of type `IClassificationPredictor` that has been created
-         */
         std::unique_ptr<IClassificationPredictor> createClassificationPredictor(
-            const IClassificationPredictorFactory& factory) const;
+            const IClassificationPredictorFactory& factory) const override;
 
-        /**
-         * Creates and returns a new instance of the class `IRegressionPredictor`, based on the type of this rule-based
-         * model.
-         *
-         * @param factory   A reference to an object of type `IRegressionPredictorFactory` that should be used to create
-         *                  the instance
-         * @return          An unique pointer to an object of type `IRegressionPredictor` that has been created
-         */
         std::unique_ptr<IRegressionPredictor> createRegressionPredictor(
-            const IRegressionPredictorFactory& factory) const;
+            const IRegressionPredictorFactory& factory) const override;
 
-        /**
-         * Creates and returns a new instance of the class `IProbabilityPredictor`, based on the type of this rule-based
-         * model.
-         *
-         * @param factory   A reference to an object of type `IProbabilityPredictorFactory` that should be used to
-         *                  create the instance
-         * @return          An unique pointer to an object of type `IProbabilityPredictor` that has been created
-         */
         std::unique_ptr<IProbabilityPredictor> createProbabilityPredictor(
-            const IProbabilityPredictorFactory& factory) const;
+            const IProbabilityPredictorFactory& factory) const override;
 
 };
---
+++ cpp/subprojects/common/include/common/model/rule_model.hpp
@@ -0,0 +1,117 @@
+/*
+ * @author Michael Rapp (michael.rapp.ml@gmail.com)
+ */
+#pragma once
+
+#include "common/model/rule.hpp"
+
+// Forward declarations
+class IClassificationPredictorFactory;
+class IClassificationPredictor;
+class IRegressionPredictorFactory;
+class IRegressionPredictor;
+class IProbabilityPredictorFactory;
+class IProbabilityPredictor;
+
+
+/**
+ * Defines an interface for all rule-based models.
+ */
+class IRuleModel {
+
+    public:
+
+        virtual ~IRuleModel() { };
+
+        /**
+         * Returns the total number of rules in the model.
+         *
+         * @return The number of rules
+         */
+        virtual uint32 getNumRules() const = 0;
+
+        /**
+         * Returns the number of used rules.
+         *
+         * @return The number of used rules
+         */
+        virtual uint32 getNumUsedRules() const = 0;
+
+        /**
+         * Sets the number of used rules.
+         *
+         * @param numUsedRules The number of used rules to be set or 0, if all rules are used
+         */
+        virtual void setNumUsedRules(uint32 numUsedRules) = 0;
+
+        /**
+         * Creates a new rule from a given body and head and adds it to the model.
+         *
+         * @param bodyPtr An unique pointer to an object of type `IBody` that should be used as the body of the rule
+         * @param headPtr An unique pointer to an object of type `IHead` that should be used as the head of the rule
+         */
+        virtual void addRule(std::unique_ptr<IBody> bodyPtr, std::unique_ptr<IHead> headPtr) = 0;
+
+        /**
+         * Invokes some of the given visitor functions, depending on which ones are able to handle the bodies and heads
+         * of the rules that are contained in this model.
+         *
+         * @param emptyBodyVisitor          The visitor function for handling objects of the type `EmptyBody`
+         * @param conjunctiveBodyVisitor    The visitor function for handling objects of the type `ConjunctiveBody`
+         * @param completeHeadVisitor       The visitor function for handling objects of the type `CompleteHead`
+         * @param partialHeadVisitor        The visitor function for handling objects of the type `PartialHead`
+         */
+        virtual void visit(IBody::EmptyBodyVisitor emptyBodyVisitor,
+                           IBody::ConjunctiveBodyVisitor conjunctiveBodyVisitor,
+                           IHead::CompleteHeadVisitor completeHeadVisitor,
+                           IHead::PartialHeadVisitor partialHeadVisitor) const = 0;
+
+
+        /**
+         * Invokes some of the given visitor functions, depending on which ones are able to handle the bodies and heads
+         * of the used rules that are contained in this model.
+         *
+         * @param emptyBodyVisitor          The visitor function for handling objects of the type `EmptyBody`
+         * @param conjunctiveBodyVisitor    The visitor function for handling objects of the type `ConjunctiveBody`
+         * @param completeHeadVisitor       The visitor function for handling objects of the type `CompleteHead`
+         * @param partialHeadVisitor        The visitor function for handling objects of the type `PartialHead`
+         */
+        virtual void visitUsed(IBody::EmptyBodyVisitor emptyBodyVisitor,
+                               IBody::ConjunctiveBodyVisitor conjunctiveBodyVisitor,
+                               IHead::CompleteHeadVisitor completeHeadVisitor,
+                               IHead::PartialHeadVisitor partialHeadVisitor) const = 0;
+
+        /**
+         * Creates and returns a new instance of the class `IClassificationPredictor`, based on the type of this
+         * rule-based model.
+         *
+         * @param factory   A reference to an object of type `IClassificationPredictorFactory` that should be used to
+         *                  create the instance
+         * @return          An unique pointer to an object of type `IClassificationPredictor` that has been created
+         */
+        virtual std::unique_ptr<IClassificationPredictor> createClassificationPredictor(
+            const IClassificationPredictorFactory& factory) const = 0;
+
+        /**
+         * Creates and returns a new instance of the class `IRegressionPredictor`, based on the type of this rule-based
+         * model.
+         *
+         * @param factory   A reference to an object of type `IRegressionPredictorFactory` that should be used to create
+         *                  the instance
+         * @return          An unique pointer to an object of type `IRegressionPredictor` that has been created
+         */
+        virtual std::unique_ptr<IRegressionPredictor> createRegressionPredictor(
+            const IRegressionPredictorFactory& factory) const = 0;
+
+        /**
+         * Creates and returns a new instance of the class `IProbabilityPredictor`, based on the type of this rule-based
+         * model.
+         *
+         * @param factory   A reference to an object of type `IProbabilityPredictorFactory` that should be used to
+         *                  create the instance
+         * @return          An unique pointer to an object of type `IProbabilityPredictor` that has been created
+         */
+        virtual std::unique_ptr<IProbabilityPredictor> createProbabilityPredictor(
+            const IProbabilityPredictorFactory& factory) const = 0;
+
+};
