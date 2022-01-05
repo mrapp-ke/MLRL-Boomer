ba30b7dab2d77ddd459f459d31bcc3dacb61cf4f
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-05 16:05:03 +0100

    Add class BoostingRuleLearner.

---
+++ cpp/subprojects/boosting/include/boosting/learner.hpp
@@ -0,0 +1,15 @@
+/*
+ * @author Michael Rapp (michael.rapp.ml@gmail.com)
+ */
+#pragma once
+
+#include "common/learner.hpp"
+
+
+namespace boosting {
+
+    class BoostingRuleLearner final : public AbstractRuleLearner {
+
+    };
+
+}
---
+++ cpp/subprojects/boosting/src/boosting/learner.cpp
@@ -0,0 +1,6 @@
+#include "boosting/learner.hpp"
+
+
+namespace boosting {
+
+}
