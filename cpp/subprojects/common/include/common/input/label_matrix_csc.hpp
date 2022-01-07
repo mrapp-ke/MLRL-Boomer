23d8fb2da4a110b648a7b767026f626f903971e1
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-07 03:13:57 +0100

    Replace usages of the classes CsrLabelMatrix and CContiguousLabelMatrix with the respective views.

--- cpp/subprojects/common/include/common/input/label_matrix_csc.hpp
+++ cpp/subprojects/common/include/common/input/label_matrix_csc.hpp
@@ -4,8 +4,8 @@
 #pragma once
 
 #include "common/data/view_csc_binary.hpp"
-#include "common/input/label_matrix_c_contiguous.hpp"
-#include "common/input/label_matrix_csr.hpp"
+#include "common/data/view_c_contiguous.hpp"
+#include "common/data/view_csr_binary.hpp"
 #include "common/indices/index_vector_complete.hpp"
 #include "common/indices/index_vector_partial.hpp"
 
@@ -14,8 +14,8 @@
  * Implements column-wise read-only access to the labels of individual training examples that are stored in a matrix in
  * the compressed sparse column (CSC) format.
  *
- * This class provides copy constructors for copying an existing `CContiguousLabelMatrix`, which provides random access,
- * or a `CsrLabelMatrix`, which provides row-wise access to the labels of the training examples. These constructors
+ * This class provides copy constructors for copying an existing `CContiguousConstView`, which provides random access,
+ * or a `BinaryCsrConstView`, which provides row-wise access to the labels of the training examples. These constructors
  * expect the indices of the examples to be considered when copying the existing label matrix to be provided.
  */
 class CscLabelMatrix final : public BinaryCscConstView {
@@ -23,43 +23,44 @@ class CscLabelMatrix final : public BinaryCscConstView {
     public:
 
         /**
-         * @param labelMatrix   A reference to an object of type `CContiguousLabelMatrix` to be copied
+         * @param labelMatrix   A reference to an object of type `CContiguousConstView` to be copied
          * @param indicesBegin  A `CompleteIndexVector::const_iterator` to the beginning of the indices of the examples
          *                      to be considered
          * @param indicesEnd    A `CompleteIndexVector::const_iterator` to the end of the indices of the examples to be
          *                      considered
          */
-        CscLabelMatrix(const CContiguousLabelMatrix& labelMatrix, CompleteIndexVector::const_iterator indicesBegin,
+        CscLabelMatrix(const CContiguousConstView<const uint8>& labelMatrix,
+                       CompleteIndexVector::const_iterator indicesBegin,
                        CompleteIndexVector::const_iterator indicesEnd);
 
         /**
-         * @param labelMatrix   A reference to an object of type `CContiguousLabelMatrix` to be copied
+         * @param labelMatrix   A reference to an object of type `CContiguousConstView` to be copied
          * @param indicesBegin  A `PartialIndexVector::const_iterator` to the beginning of the indices of the examples
          *                      to be considered
          * @param indicesEnd    A `PartialIndexVector::const_iterator` to the end of the indices of the examples to be
          *                      considered
          */
-        CscLabelMatrix(const CContiguousLabelMatrix& labelMatrix, PartialIndexVector::const_iterator indicesBegin,
-                       PartialIndexVector::const_iterator indicesEnd);
+        CscLabelMatrix(const CContiguousConstView<const uint8>& labelMatrix,
+                       PartialIndexVector::const_iterator indicesBegin, PartialIndexVector::const_iterator indicesEnd);
 
         /**
-         * @param labelMatrix   A reference to an object of type `CsrLabelMatrix` to be copied
+         * @param labelMatrix   A reference to an object of type `BinaryCsrConstView` to be copied
          * @param indicesBegin  A `CompleteIndexVector::const_iterator` to the beginning of the indices of the examples
          *                      to be considered
          * @param indicesEnd    A `CompleteIndexVector::const_iterator` to the end of the indices of the examples to be
          *                      considered
          */
-        CscLabelMatrix(const CsrLabelMatrix& labelMatrix, CompleteIndexVector::const_iterator indicesBegin,
+        CscLabelMatrix(const BinaryCsrConstView& labelMatrix, CompleteIndexVector::const_iterator indicesBegin,
                        CompleteIndexVector::const_iterator indicesEnd);
 
         /**
-         * @param labelMatrix   A reference to an object of type `CsrLabelMatrix` to be copied
+         * @param labelMatrix   A reference to an object of type `BinaryCsrConstView` to be copied
          * @param indicesBegin  A `PartialIndexVector::const_iterator` to the beginning of the indices of the examples
          *                      to be considered
          * @param indicesEnd    A `PartialIndexVector::const_iterator` to the end of the indices of the examples to be
          *                      considered
          */
-        CscLabelMatrix(const CsrLabelMatrix& labelMatrix, PartialIndexVector::const_iterator indicesBegin,
+        CscLabelMatrix(const BinaryCsrConstView& labelMatrix, PartialIndexVector::const_iterator indicesBegin,
                        PartialIndexVector::const_iterator indicesEnd);
 
         ~CscLabelMatrix();
