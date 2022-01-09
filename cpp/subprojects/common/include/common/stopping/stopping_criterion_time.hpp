139117c54c4f8599b5fa2acd33d1b32becfc426e
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-09 17:21:42 +0100

    Add class TimeStoppingCriterionConfig.

--- cpp/subprojects/common/include/common/stopping/stopping_criterion_time.hpp
+++ cpp/subprojects/common/include/common/stopping/stopping_criterion_time.hpp
@@ -7,6 +7,37 @@
 
 
 /**
+ * Allows to configure a stopping criterion that ensures that a certain time limit is not exceeded.
+ */
+class TimeStoppingCriterionConfig final : public IStoppingCriterionConfig {
+
+    private:
+
+        uint32 timeLimit_;
+
+    public:
+
+        TimeStoppingCriterionConfig();
+
+        /**
+         * Returns the time limit.
+         *
+         * @return The time limit in seconds
+         */
+        uint32 getTimeLimit() const;
+
+        /**
+         * Sets the time limit.
+         *
+         * @param timeLimit The time limit in seconds. Must be at least 1
+         * @return          A reference to an object of type `TimeStoppingCriterionConfig` that allows further
+         *                  configuration of the stopping criterion
+         */
+        TimeStoppingCriterionConfig& setTimeLimit(uint32 timeLimit);
+
+};
+
+/**
  * Allows to create instances of the type `IStoppingCriterion` that ensure that a certain time limit is not exceeded.
  */
 class TimeStoppingCriterionFactory final : public IStoppingCriterionFactory {
