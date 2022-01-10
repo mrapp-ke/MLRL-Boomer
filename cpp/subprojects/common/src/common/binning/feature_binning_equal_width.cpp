133d7e624cccf461fb37aab225cec454ce0014f1
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-10 22:26:17 +0100

    Replace double quotes with single quotes.

--- cpp/subprojects/common/src/common/binning/feature_binning_equal_width.cpp
+++ cpp/subprojects/common/src/common/binning/feature_binning_equal_width.cpp
@@ -185,8 +185,8 @@ float32 EqualWidthFeatureBinningConfig::getBinRatio() const {
 }
 
 EqualWidthFeatureBinningConfig& EqualWidthFeatureBinningConfig::setBinRatio(float32 binRatio) {
-    assertGreater<float32>("binRatio", binRatio, 0);
-    assertLess<float32>("binRatio", binRatio, 1);
+    assertGreater<float32>('binRatio', binRatio, 0);
+    assertLess<float32>('binRatio', binRatio, 1);
     binRatio_ = binRatio;
     return *this;
 }
@@ -196,7 +196,7 @@ uint32 EqualWidthFeatureBinningConfig::getMinBins() const {
 }
 
 EqualWidthFeatureBinningConfig& EqualWidthFeatureBinningConfig::setMinBins(uint32 minBins) {
-    assertGreaterOrEqual<uint32>("minBins", minBins, 2);
+    assertGreaterOrEqual<uint32>('minBins', minBins, 2);
     minBins_ = minBins;
     return *this;
 }
@@ -206,7 +206,7 @@ uint32 EqualWidthFeatureBinningConfig::getMaxBins() const {
 }
 
 EqualWidthFeatureBinningConfig& EqualWidthFeatureBinningConfig::setMaxBins(uint32 maxBins) {
-    if (maxBins != 0) { assertGreaterOrEqual<uint32>("maxBins", maxBins, minBins_); }
+    if (maxBins != 0) { assertGreaterOrEqual<uint32>('maxBins', maxBins, minBins_); }
     maxBins_ = maxBins;
     return *this;
 }
