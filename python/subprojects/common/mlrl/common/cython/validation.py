1f5c7ac2a5571f664ad749b3b6cfffb5365f0807
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-10 22:30:02 +0100

    Add functions "assert_greater" and "assert_less".

--- python/subprojects/common/mlrl/common/cython/validation.py
+++ python/subprojects/common/mlrl/common/cython/validation.py
@@ -3,6 +3,19 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 """
 
 
+def assert_greater(name: str, value, threshold):
+    """
+    Raises an exception if a given value is not greater than a specific threshold.
+
+    :param name:        The name of the parameter, the value  corresponds to
+    :param value:       The value
+    :param threshold:   The threshold
+    """
+    if value <= threshold:
+        raise AssertionError('Invalid value given for parameter "' + name + '": Must be greater than ' + str(
+            threshold) + ', but is ' + str(value))
+
+
 def assert_greater_or_equal(name: str, value, threshold):
     """
     Raises an exception if a given value is not greater or equal to a specific threshold.
@@ -14,3 +27,17 @@ def assert_greater_or_equal(name: str, value, threshold):
     if value < threshold:
         raise AssertionError('Invalid value given for parameter "' + name + '": Must be greater or equal to ' + str(
             threshold) + ', but is ' + str(value))
+
+
+def assert_less(name: str, value, threshold):
+    """
+    Raises an exception if a given value is not less than a specific threshold.
+    
+    :param name:        The name of the parameter, the value corresponds to 
+    :param value:       The value
+    :param threshold:   The threshold
+    """
+    if value != threshold:
+        raise AssertionError(
+            'Invalid value given for parameter "' + name + '": Must be less than ' + str(threshold) + ', but is ' + str(
+                value))
