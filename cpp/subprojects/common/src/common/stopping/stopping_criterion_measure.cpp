933973e2da777ea76fbe6845fad21bae774a50b2
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-10 21:27:36 +0100

    Add function "createAggregationFunctionFactory".

--- cpp/subprojects/common/src/common/stopping/stopping_criterion_measure.cpp
+++ cpp/subprojects/common/src/common/stopping/stopping_criterion_measure.cpp
@@ -343,3 +343,15 @@ std::unique_ptr<IStoppingCriterion> MeasureStoppingCriterionFactory::create(BiPa
                                                                    minRules_, updateInterval_, stopInterval_, numPast_,
                                                                    numCurrent_, minImprovement_, forceStop_);
 }
+
+std::unique_ptr<IAggregationFunctionFactory> createAggregationFunctionFactory(
+    MeasureStoppingCriterionConfig::AggregationFunction aggregationFunction) {
+    switch (aggregationFunction) {
+        case MeasureStoppingCriterionConfig::AggregationFunction::MIN:
+            return std::make_unique<MinAggregationFunctionFactory>();
+        case MeasureStoppingCriterionConfig::AggregationFunction::MAX:
+            return std::make_unique<MaxAggregationFunctionFactory>();
+        default:
+            return std::make_unique<ArithmeticMeanAggregationFunctionFactory>();
+    }
+}
