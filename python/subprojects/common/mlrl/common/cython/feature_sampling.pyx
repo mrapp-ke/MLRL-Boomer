a880b36db95f8d4bd5e7a093cd59c02c733373e3
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-10 23:33:52 +0100

    Add files feature_sampling.pxd/pyx.

---
+++ python/subprojects/common/mlrl/common/cython/feature_sampling.pyx
@@ -0,0 +1,22 @@
+from mlrl.common.cython.validation import assert_greater_or_equal, assert_less
+
+
+cdef class FeatureSamplingWithoutReplacementConfig:
+    """
+    A wrapper for the C++ class `FeatureSamplingWithoutReplacementConfig`.
+    """
+
+    def set_sample_size(self, sample_size: float) -> FeatureSamplingWithoutReplacementConfig:
+        """
+        Sets the fraction of features that should be included in a sample.
+
+        :param sample_size: The fraction of features that should be included in a sample, e.g., a value of 0.6
+                            corresponds to 60 % of the available features. Must be in (0, 1) or 0, if the default sample
+                            size `floor(log2(numFeatures - 1) + 1)` should be used
+        :return:            A `FeatureSamplingWithoutReplacementConfig` that allows further configuration of the
+                            strategy for sampling features
+        """
+        assert_greater_or_equal('sample_size', sample_size, 0)
+        assert_less('sample_size', sample_size, 1)
+        self.config_ptr.setSampleSize(sample_size)
+        return self
