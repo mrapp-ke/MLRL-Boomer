5fb6cd0a96c662ffe5ed2a1b27f07d6c0d52e09f
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-11 23:54:46 +0100

    Add class FMeasureConfig.

--- cpp/subprojects/seco/include/seco/heuristics/heuristic_f_measure.hpp
+++ cpp/subprojects/seco/include/seco/heuristics/heuristic_f_measure.hpp
@@ -10,6 +10,40 @@
 namespace seco {
 
     /**
+     * A class that allows to configure a heuristic that calculates as the (weighted) harmonic mean between the
+     * heuristics "Precision" and "Recall", where the parameter "beta" allows to trade off between both heuristics. If
+     * beta = 1, both heuristics are weighed equally. If beta = 0, this heuristic is equivalent to "Precision". As beta
+     * approaches infinity, this heuristic becomes equivalent to "Recall".
+     */
+    class FMeasureConfig final : public IHeuristicConfig {
+
+        private:
+
+            float64 beta_;
+
+        public:
+
+            FMeasureConfig();
+
+            /**
+             * Returns the value of the "beta" parameter.
+             *
+             * @return The value of the "beta" parameter
+             */
+            float64 getBeta() const;
+
+            /**
+             * Sets the value of the "beta" parameter
+             *
+             * @param beta  The value of the "beta" parameter. Must be at least 0
+             * @return      A reference to an object of type `FMeasureConfig` that allows further configuration of the
+             *              heuristic
+             */
+            FMeasureConfig& setBeta(float64 beta);
+
+    };
+
+    /**
      * Allows to create instances of the type `IHeuristic` that calculate as the (weighted) harmonic mean between the
      * heuristics "Precision" and "Recall", where the parameter "beta" allows to trade off between both heuristics. If
      * beta = 1, both heuristics are weighed equally. If beta = 0, this heuristic is equivalent to "Precision". As beta
