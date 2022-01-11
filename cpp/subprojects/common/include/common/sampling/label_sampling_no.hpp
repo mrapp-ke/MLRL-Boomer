be40b0304e01ea5b6d634687a648067cf5d85e96
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-11 22:52:06 +0100

    Add class NoLabelSamplingConfig.

--- cpp/subprojects/common/include/common/sampling/label_sampling_no.hpp
+++ cpp/subprojects/common/include/common/sampling/label_sampling_no.hpp
@@ -7,6 +7,13 @@
 
 
 /**
+ * Allows to configure a method for sampling labels that does not perform any sampling, but includes all labels.
+ */
+class NoLabelSamplingConfig final : public ILabelSamplingConfig {
+
+};
+
+/**
  * Allows to create objects of the class `ILabelSampling` that do not perform any sampling, but include all labels.
  */
 class NoLabelSamplingFactory final : public ILabelSamplingFactory {
