c8473fab297779d2d8ecdb4ef79a6cf57715c1c4
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-11 23:42:48 +0100

    The class IBoostingRuleLearner::IConfig does now allow configure the method that should be used for label binning.

--- cpp/subprojects/boosting/src/boosting/learner.cpp
+++ cpp/subprojects/boosting/src/boosting/learner.cpp
@@ -8,62 +8,51 @@
 
 namespace boosting {
 
-    /**
-     * A rule learner that makes use of gradient boosting.
-     */
-    class BoostingRuleLearner final : public AbstractRuleLearner, virtual public IBoostingRuleLearner {
+    const ILabelBinningConfig* BoostingRuleLearner::Config::getLabelBinningConfig() const {
+        return labelBinningConfigPtr_.get();
+    }
 
-        public:
+    BoostingRuleLearner::Config::Config() {
 
-            /**
-             * Allows to configure a rule learner that makes use of gradient boosting.
-             */
-            class Config : public AbstractRuleLearner::Config, virtual public IBoostingRuleLearner::IConfig {
+    }
 
-            };
+    EqualWidthLabelBinningConfig& BoostingRuleLearner::Config::useEqualWidthLabelBinning() {
+        std::unique_ptr<EqualWidthLabelBinningConfig> ptr = std::make_unique<EqualWidthLabelBinningConfig>();
+        EqualWidthLabelBinningConfig& ref = *ptr;
+        labelBinningConfigPtr_ = std::move(ptr);
+        return ref;
+    }
 
-        protected:
+    std::unique_ptr<IStatisticsProviderFactory> BoostingRuleLearner::createStatisticsProviderFactory() const {
+        // TODO Implement
+        float64 l1RegularizationWeight = 0;
+        float64 l2RegularizationWeight = 1;
+        uint32 numThreads = 1;
+        return std::make_unique<DenseLabelWiseStatisticsProviderFactory>(
+            std::make_unique<LabelWiseLogisticLossFactory>(),
+            std::make_unique<LabelWiseLogisticLossFactory>(),
+            std::make_unique<LabelWiseSingleLabelRuleEvaluationFactory>(l1RegularizationWeight, l2RegularizationWeight),
+            std::make_unique<LabelWiseSingleLabelRuleEvaluationFactory>(l1RegularizationWeight, l2RegularizationWeight),
+            std::make_unique<LabelWiseSingleLabelRuleEvaluationFactory>(l1RegularizationWeight, l2RegularizationWeight),
+            numThreads);
+    }
 
-            std::unique_ptr<IStatisticsProviderFactory> createStatisticsProviderFactory() const override {
-                // TODO Implement
-                float64 l1RegularizationWeight = 0;
-                float64 l2RegularizationWeight = 1;
-                uint32 numThreads = 1;
-                return std::make_unique<DenseLabelWiseStatisticsProviderFactory>(
-                    std::make_unique<LabelWiseLogisticLossFactory>(),
-                    std::make_unique<LabelWiseLogisticLossFactory>(),
-                    std::make_unique<LabelWiseSingleLabelRuleEvaluationFactory>(l1RegularizationWeight,
-                                                                                l2RegularizationWeight),
-                    std::make_unique<LabelWiseSingleLabelRuleEvaluationFactory>(l1RegularizationWeight,
-                                                                                l2RegularizationWeight),
-                    std::make_unique<LabelWiseSingleLabelRuleEvaluationFactory>(l1RegularizationWeight,
-                                                                                l2RegularizationWeight),
-                    numThreads);
-            }
 
-            std::unique_ptr<IModelBuilder> createModelBuilder() const override {
-                return std::make_unique<RuleListBuilder>();
-            }
+    std::unique_ptr<IModelBuilder> BoostingRuleLearner::createModelBuilder() const {
+        return std::make_unique<RuleListBuilder>();
+    }
 
-            std::unique_ptr<IClassificationPredictorFactory> createClassificationPredictorFactory() const override {
-                // TODO Implement
-                float64 threshold = 0;
-                uint32 numThreads = 1;
-                return std::make_unique<LabelWiseClassificationPredictorFactory>(threshold, numThreads);
-            }
+    std::unique_ptr<IClassificationPredictorFactory> BoostingRuleLearner::createClassificationPredictorFactory() const {
+        // TODO Implement
+        float64 threshold = 0;
+        uint32 numThreads = 1;
+        return std::make_unique<LabelWiseClassificationPredictorFactory>(threshold, numThreads);
+    }
 
-        public:
+    BoostingRuleLearner::BoostingRuleLearner(std::unique_ptr<IBoostingRuleLearner::IConfig> configPtr)
+        : AbstractRuleLearner(std::move(configPtr)) {
 
-            /**
-             * @param configPtr An unique pointer to an object of type `IBoostingRuleLearner::IConfig` that specifies
-             *                  the configuration that should be used by the rule learner
-             */
-            BoostingRuleLearner(std::unique_ptr<IBoostingRuleLearner::IConfig> configPtr)
-                : AbstractRuleLearner(std::move(configPtr)) {
-
-            }
-
-    };
+    }
 
     std::unique_ptr<IBoostingRuleLearner::IConfig> createBoostingRuleLearnerConfig() {
         return std::make_unique<BoostingRuleLearner::Config>();
