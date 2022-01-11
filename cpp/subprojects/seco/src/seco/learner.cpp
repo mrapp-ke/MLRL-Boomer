7d41b634a46ac13f2f8d247083e5755c47a4236c
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-12 00:21:01 +0100

    The class ISeCoRuleLearner::IConfig does now allow configure the heuristic that should be used for learning rules.

--- cpp/subprojects/seco/src/seco/learner.cpp
+++ cpp/subprojects/seco/src/seco/learner.cpp
@@ -3,8 +3,62 @@
 
 namespace seco {
 
-    SeCoRuleLearner::Config::Config() {
+    SeCoRuleLearner::Config::Config()
+        : heuristicConfigPtr_(std::make_unique<FMeasureConfig>()) {
 
+    }
+
+    const IHeuristicConfig& SeCoRuleLearner::Config::getHeuristicConfig() const {
+        return *heuristicConfigPtr_;
+    }
+
+    AccuracyConfig& SeCoRuleLearner::Config::useAccuracyHeuristic() {
+        std::unique_ptr<AccuracyConfig> ptr = std::make_unique<AccuracyConfig>();
+        AccuracyConfig& ref = *ptr;
+        heuristicConfigPtr_ = std::move(ptr);
+        return ref;
+    }
+
+    FMeasureConfig& SeCoRuleLearner::Config::useFMeasureHeuristic() {
+        std::unique_ptr<FMeasureConfig> ptr = std::make_unique<FMeasureConfig>();
+        FMeasureConfig& ref = *ptr;
+        heuristicConfigPtr_ = std::move(ptr);
+        return ref;
+    }
+
+    LaplaceConfig& SeCoRuleLearner::Config::useLaplaceHeuristic() {
+        std::unique_ptr<LaplaceConfig> ptr = std::make_unique<LaplaceConfig>();
+        LaplaceConfig& ref = *ptr;
+        heuristicConfigPtr_ = std::move(ptr);
+        return ref;
+    }
+
+    MEstimateConfig& SeCoRuleLearner::Config::useMEstimateHeuristic() {
+        std::unique_ptr<MEstimateConfig> ptr = std::make_unique<MEstimateConfig>();
+        MEstimateConfig& ref = *ptr;
+        heuristicConfigPtr_ = std::move(ptr);
+        return ref;
+    }
+
+    PrecisionConfig& SeCoRuleLearner::Config::usePrecisionHeuristic() {
+        std::unique_ptr<PrecisionConfig> ptr = std::make_unique<PrecisionConfig>();
+        PrecisionConfig& ref = *ptr;
+        heuristicConfigPtr_ = std::move(ptr);
+        return ref;
+    }
+
+    RecallConfig& SeCoRuleLearner::Config::useRecallHeuristic() {
+        std::unique_ptr<RecallConfig> ptr = std::make_unique<RecallConfig>();
+        RecallConfig& ref = *ptr;
+        heuristicConfigPtr_ = std::move(ptr);
+        return ref;
+    }
+
+    WraConfig& SeCoRuleLearner::Config::useWraHeuristic() {
+        std::unique_ptr<WraConfig> ptr = std::make_unique<WraConfig>();
+        WraConfig& ref = *ptr;
+        heuristicConfigPtr_ = std::move(ptr);
+        return ref;
     }
 
     std::unique_ptr<IStatisticsProviderFactory> SeCoRuleLearner::createStatisticsProviderFactory() const {
