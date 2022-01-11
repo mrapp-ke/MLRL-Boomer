542e516a84408276ca56846c7dd027c87c93e878
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-11 23:42:48 +0100

    The class IBoostingRuleLearner::IConfig does now allow configure the method that should be used for label binning.

--- cpp/subprojects/boosting/include/boosting/learner.hpp
+++ cpp/subprojects/boosting/include/boosting/learner.hpp
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "common/learner.hpp"
+#include "boosting/binning/label_binning_equal_width.hpp"
 
 
 namespace boosting {
@@ -21,13 +22,80 @@ namespace boosting {
              */
             class IConfig : virtual public IRuleLearner::IConfig {
 
+                friend class BoostingRuleLearner;
+
+                private:
+
+                    /**
+                     * Returns the configuration of the method for the assignment of labels to bins.
+                     *
+                     * @return A pointer to an object of type `ILabelBinningConfig` that specifies the configuration of
+                     *         the algorithm for the assignment of labels to bins or a null pointer, if no such method
+                     *         should be used
+                     */
+                    virtual const ILabelBinningConfig* getLabelBinningConfig() const = 0;
+
                 public:
 
                     virtual ~IConfig() override { };
 
+                    /**
+                     * Configures the algorithm to use a method for the assignment of labels to bins in a way such that
+                     * each bin contains labels for which the predicted score is expected to belong to the same value
+                     * range.
+                     *
+                     * @return A reference to an object of type `EqualWidthLabelBinningConfig` that allows further
+                     *         configuration of the method for the assignment of labels to bins
+                     */
+                    virtual EqualWidthLabelBinningConfig& useEqualWidthLabelBinning() = 0;
+
             };
 
             virtual ~IBoostingRuleLearner() override { };
+
+    };
+
+    /**
+     * A rule learner that makes use of gradient boosting.
+     */
+    class BoostingRuleLearner final : public AbstractRuleLearner, virtual public IBoostingRuleLearner {
+
+        public:
+
+            /**
+             * Allows to configure a rule learner that makes use of gradient boosting.
+             */
+            class Config : public AbstractRuleLearner::Config, virtual public IBoostingRuleLearner::IConfig {
+
+                private:
+
+                    std::unique_ptr<ILabelBinningConfig> labelBinningConfigPtr_;
+
+                    const ILabelBinningConfig* getLabelBinningConfig() const override;
+
+                public:
+
+                    Config();
+
+                    EqualWidthLabelBinningConfig& useEqualWidthLabelBinning() override;
+
+            };
+
+        protected:
+
+            std::unique_ptr<IStatisticsProviderFactory> createStatisticsProviderFactory() const override;
+
+            std::unique_ptr<IModelBuilder> createModelBuilder() const override;
+
+            std::unique_ptr<IClassificationPredictorFactory> createClassificationPredictorFactory() const override;
+
+        public:
+
+            /**
+             * @param configPtr An unique pointer to an object of type `IBoostingRuleLearner::IConfig` that specifies
+             *                  the configuration that should be used by the rule learner
+             */
+            BoostingRuleLearner(std::unique_ptr<IBoostingRuleLearner::IConfig> configPtr);
 
     };
 
