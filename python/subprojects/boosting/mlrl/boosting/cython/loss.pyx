6916dfedee1520db793ed06789ddd137424c8292
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-12 14:07:44 +0100

    Add files loss.pxd/pyx.

---
+++ python/subprojects/boosting/mlrl/boosting/cython/loss.pyx
@@ -0,0 +1,31 @@
+"""
+@author: Michael Rapp (michael.rapp.ml@gmail.com)
+"""
+
+
+cdef class ExampleWiseLogisticLossConfig:
+    """
+    A wrapper for the C++ class `ExampleWiseLogisticLossConfig`.
+    """
+    pass
+
+
+cdef class LabelWiseLogisticLossConfig:
+    """
+    A wrapper for the C++ class `LabelWiseLogisticLossConfig`.
+    """
+    pass
+
+
+cdef class LabelWiseSquaredErrorLossConfig:
+    """
+    A wrapper for the C++ class `LabelWiseSquaredErrorLossConfig`.
+    """
+    pass
+
+
+cdef class LabelWiseSquaredHingeLossConfig:
+    """
+    A wrapper for the C++ class `LabelWiseSquaredHingeLossConfig`.
+    """
+    pass
