decb1f88f06ef837228d7e213c73b716642cd370
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-14 23:51:04 +0100

    Add class ExampleWiseClassificationPredictorConfig.

--- cpp/subprojects/boosting/include/boosting/output/predictor_classification_example_wise.hpp
+++ cpp/subprojects/boosting/include/boosting/output/predictor_classification_example_wise.hpp
@@ -10,6 +10,44 @@
 namespace boosting {
 
     /**
+     * Allows to configure a predictor that predicts known label vectors for given query examples by summing up the
+     * scores that are provided by an existing rule-based model and comparing the aggregated score vector to the known
+     * label vectors according to a certain distance measure. The label vector that is closest to the aggregated score
+     * vector is finally predicted.
+     */
+    class ExampleWiseClassificationPredictorConfig final : public IClassificationPredictorConfig {
+
+        private:
+
+            uint32 numThreads_;
+
+        public:
+
+            ExampleWiseClassificationPredictorConfig();
+
+            /**
+             * Returns the number of CPU threads that are used to make predictions for different query examples in
+             * parallel.
+             *
+             * @return The number of CPU threads that are used to make predictions for different query examples in
+             *         parallel
+             */
+            uint32 getNumThreads() const;
+
+            /**
+             * Sets the number of CPU threads that should be used to make predictions for different query examples in
+             * parallel.
+             *
+             * @param numThreads    The number of CPU threads that should be used. Must be at least 1 or 0, if the
+             *                      number of CPU threads should be chosen automatically
+             * @return              A reference to an object of type `ExampleWiseClassificationPredictorConfig` that
+             *                      allows further configuration of the predictor
+             */
+            ExampleWiseClassificationPredictorConfig& setNumThreads(uint32 numThreads);
+
+    };
+
+    /**
      * Allows to create instances of the type `IClassificationPredictor` that allow to predict known label vectors for
      * given query examples by summing up the scores that are provided by an existing rule-based model and comparing the
      * aggregated score vector to the known label vectors according to a certain distance measure. The label vector that
