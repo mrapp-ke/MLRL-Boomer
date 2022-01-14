117f2c36edfe8d60ec6a5e87b0c45293b43a4286
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-14 21:56:25 +0100

    Add class NoPostProcessorConfig.

--- cpp/subprojects/common/include/common/post_processing/post_processor_no.hpp
+++ cpp/subprojects/common/include/common/post_processing/post_processor_no.hpp
@@ -7,6 +7,15 @@
 
 
 /**
+ * Allows to configure a post-processor that does not perform any post-processing, but retains the original predictions
+ * of rules.
+ */
+class NoPostProcessorConfig final : public IPostProcessorConfig {
+
+};
+
+
+/**
  * Allows to create instances of the type `IPostProcessor` that do not perform any post-processing, but retain the
  * original predictions of rules.
  */
