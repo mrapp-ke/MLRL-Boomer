d049dbb67118b1e5054ce9307c22fdbd6dd8c47d
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-16 19:26:32 +0100

    Add Cython class ExampleWiseClassificationPredictorConfig.

--- python/subprojects/boosting/mlrl/boosting/cython/predictor.pxd
+++ python/subprojects/boosting/mlrl/boosting/cython/predictor.pxd
@@ -1,6 +1,17 @@
 from mlrl.common.cython._types cimport uint32
 
 
+cdef extern from "boosting/output/predictor_classification_example_wise.hpp" namespace "boosting" nogil:
+
+    cdef cppclass ExampleWiseClassificationPredictorConfigImpl"boosting::ExampleWiseClassificationPredictorConfig":
+
+        # Functions:
+
+        uint32 getNumThreads() const
+
+        ExampleWiseClassificationPredictorConfigImpl& setNumThreads(uint32 numThreads) except +
+
+
 cdef extern from "boosting/output/predictor_classification_label_wise.hpp" namespace "boosting" nogil:
 
     cdef cppclass LabelWiseClassificationPredictorConfigImpl"boosting::LabelWiseClassificationPredictorConfig":
@@ -32,6 +43,13 @@ cdef extern from "boosting/output/predictor_probability_label_wise.hpp" namespac
         uint32 getNumThreads() const
 
         LabelWiseProbabilityPredictorConfigImpl& setNumThreads(uint32 numThreads) except +
+
+
+cdef class ExampleWiseClassificationPredictorConfig:
+
+    # Attributes:
+
+    cdef ExampleWiseClassificationPredictorConfigImpl* config_ptr
 
 
 cdef class LabelWiseClassificationPredictorConfig:
--- python/subprojects/boosting/mlrl/boosting/cython/predictor.pyx
+++ python/subprojects/boosting/mlrl/boosting/cython/predictor.pyx
@@ -4,6 +4,26 @@
 from mlrl.common.cython._validation import assert_greater_or_equal
 
 
+cdef class ExampleWiseClassificationPredictorConfig:
+    """
+    A wrapper for the C++ class `ExampleWiseClassificationPredictorConfig`.
+    """
+
+    def set_num_threads(self, num_threads: int) -> ExampleWiseClassificationPredictorConfig:
+        """
+        Sets the number of CPU threads that should be used to make predictions for different query examples in parallel.
+
+        :param num_threads: The number of CPU threads that should be used. Must be at least 1 or 0, if the number of CPU
+                            threads should be chosen automatically
+        :return:            An `ExampleWiseClassificationPredictorConfig` that allows further configuration of the
+                            predictor
+        """
+        if num_threads != 0:
+            assert_greater_or_equal('num_threads', num_threads, 1)
+        self.config_ptr.setNumThreads(num_threads)
+        return self
+
+
 cdef class LabelWiseClassificationPredictorConfig:
     """
     A wrapper for the C++ class `LabelWiseClassificationPredictorConfig`.
