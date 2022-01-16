df7ee08fdfafcc34c2fc92e3026f0e5277cc6bd5
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-16 18:23:57 +0100

    Add files predictor.pyx/pxd.

---
+++ python/subprojects/boosting/mlrl/boosting/cython/predictor.pxd
@@ -0,0 +1,55 @@
+from mlrl.common.cython._types cimport uint32
+
+
+cdef extern from "boosting/output/predictor_classification_label_wise.hpp" namespace "boosting" nogil:
+
+    cdef cppclass LabelWiseClassificationPredictorConfigImpl"boosting::LabelWiseClassificationPredictorConfig":
+
+        # Functions:
+
+        uint32 getNumThreads() const
+
+        LabelWiseClassificationPredictorConfigImpl& setNumThreads(uint32 numThreads) except +
+
+
+cdef extern from "boosting/output/predictor_regression_label_wise.hpp" namespace "boosting" nogil:
+
+    cdef cppclass LabelWiseRegressionPredictorConfigImpl"boosting::LabelWiseRegressionPredictorConfig":
+
+        # Functions:
+
+        uint32 getNumThreads() const
+
+        LabelWiseRegressionPredictorConfigImpl& setNumThreads(uint32 numThreads) except +
+
+
+cdef extern from "boosting/output/predictor_probability_label_wise.hpp" namespace "boosting" nogil:
+
+    cdef cppclass LabelWiseProbabilityPredictorConfigImpl"boosting::LabelWiseProbabilityPredictorConfig":
+
+        # Functions:
+
+        uint32 getNumThreads() const
+
+        LabelWiseProbabilityPredictorConfigImpl& setNumThreads(uint32 numThreads) except +
+
+
+cdef class LabelWiseClassificationPredictorConfig:
+
+    # Attributes:
+
+    cdef LabelWiseClassificationPredictorConfigImpl* config_ptr
+
+
+cdef class LabelWiseRegressionPredictorConfig:
+
+    # Attributes:
+
+    cdef LabelWiseRegressionPredictorConfigImpl* config_ptr
+
+
+cdef class LabelWiseProbabilityPredictorConfig:
+
+    # Attributes:
+
+    cdef LabelWiseProbabilityPredictorConfigImpl* config_ptr
---
+++ python/subprojects/boosting/mlrl/boosting/cython/predictor.pyx
@@ -0,0 +1,62 @@
+"""
+@author: Michael Rapp (michael.rapp.ml@gmail.com)
+"""
+from mlrl.common.cython._validation import assert_greater_or_equal
+
+
+cdef class LabelWiseClassificationPredictorConfig:
+    """
+    A wrapper for the C++ class `LabelWiseClassificationPredictorConfig`.
+    """
+
+    def set_num_threads(self, num_threads: int) -> LabelWiseClassificationPredictorConfig:
+        """
+        Sets the number of CPU threads that should be used to make predictions for different query examples in parallel.
+
+        :param num_threads: The number of CPU threads that should be used. Must be at least 1 or 0, if the number of CPU
+                            threads should be chosen automatically
+        :return:            A `LabelWiseClassificationPredictorConfig` that allows further configuration of the
+                            predictor
+        """
+        if num_threads != 0:
+            assert_greater_or_equal('num_threads', num_threads, 1)
+        self.config_ptr.setNumThreads(num_threads)
+        return self
+
+
+cdef class LabelWiseRegressionPredictorConfig:
+    """
+    A wrapper for the C++ class `LabelWiseRegressionPredictorConfig`.
+    """
+
+    def set_num_threads(self, num_threads: int) -> LabelWiseRegressionPredictorConfig:
+        """
+        Sets the number of CPU threads that should be used to make predictions for different query examples in parallel.
+
+        :param num_threads: The number of CPU threads that should be used. Must be at least 1 or 0, if the number of CPU
+                            threads should be chosen automatically
+        :return:            A `LabelWiseRegressionPredictorConfig` that allows further configuration of the predictor
+        """
+        if num_threads != 0:
+            assert_greater_or_equal('num_threads', num_threads, 1)
+        self.config_ptr.setNumThreads(num_threads)
+        return self
+
+
+cdef class LabelWiseProbabilityPredictorConfig:
+    """
+    A wrapper for the C++ class `LabelWiseProbabilityPredictorConfig`.
+    """
+
+    def set_num_threads(self, num_threads: int) -> LabelWiseProbabilityPredictorConfig:
+        """
+        Sets the number of CPU threads that should be used to make predictions for different query examples in parallel.
+
+        :param num_threads: The number of CPU threads that should be used. Must be at least 1 or 0, if the number of CPU
+                            threads should be chosen automatically
+        :return:            A `LabelWiseProbabilityPredictorConfig` that allows further configuration of the predictor
+        """
+        if num_threads != 0:
+            assert_greater_or_equal('num_threads', num_threads, 1)
+        self.config_ptr.setNumThreads(num_threads)
+        return self
