fb46f618d77814dc6842fdc8b7032aba69297c79
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-18 00:20:07 +0100

    Add class CoverageStoppingCriterionConfig.

--- cpp/subprojects/seco/include/seco/stopping/stopping_criterion_coverage.hpp
+++ cpp/subprojects/seco/include/seco/stopping/stopping_criterion_coverage.hpp
@@ -10,6 +10,39 @@
 namespace seco {
 
     /**
+     * Allows to configure a stopping criterion that stops the induction of rules as soon as the sum of the weights of
+     * the uncovered labels is smaller or equal to a certain threshold.
+     */
+    class CoverageStoppingCriterionConfig : public IStoppingCriterionConfig {
+
+        private:
+
+            float64 threshold_;
+
+        public:
+
+            CoverageStoppingCriterionConfig();
+
+            /**
+             * Returns the threshold that is used by the stopping criterion.
+             *
+             * @return The threshold that is used by the stopping criterion
+             */
+            float64 getThreshold() const;
+
+            /**
+             * Sets the threshold that should be used by the stopping criterion.
+             *
+             * @param threshold The threshold that should be used by the stopping criterion. The threshold must be at
+             *                  least 0
+             * @return          A reference to an object of type `CoverageStoppingCriterionConfig` that allows further
+             *                  configuration of the stopping criterion
+             */
+            CoverageStoppingCriterionConfig& setThreshold(float64 threshold);
+
+    };
+
+    /**
      * Allows to create instances of the type `IStoppingCriterion` that stop the induction of rules as soon as the sum
      * of the weights of the uncovered labels, as provided by an object of type `ICoverageStatistics`, is smaller or
      * equal to a certain threshold.
