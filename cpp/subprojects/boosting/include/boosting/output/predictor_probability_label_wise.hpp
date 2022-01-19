1769cd733530be36ea92a1b75626c47b65e900bb
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-19 14:47:01 +0100

    Move classes to cpp file.

--- cpp/subprojects/boosting/include/boosting/output/predictor_probability_label_wise.hpp
+++ cpp/subprojects/boosting/include/boosting/output/predictor_probability_label_wise.hpp
@@ -9,56 +9,6 @@
 namespace boosting {
 
     /**
-     * Defines an interface for all classes that allow to transform the scores that are predicted for individual labels
-     * into probabilities.
-     */
-    class IProbabilityFunction {
-
-        public:
-
-            virtual ~IProbabilityFunction() { };
-
-            /**
-             * Transforms the score that is predicted for an individual label into a probability.
-             *
-             * @param predictedScore    The predicted score
-             * @return                  The probability
-             */
-            virtual float64 transform(float64 predictedScore) const = 0;
-
-    };
-
-    /**
-     * Defines an interface for all factories that allow to create instances of the type `IProbabilityFunction`.
-     */
-    class IProbabilityFunctionFactory {
-
-        public:
-
-            virtual ~IProbabilityFunctionFactory() { };
-
-            /**
-             * Creates and returns a new object of the type `IProbabilityFunction`.
-             *
-             * @return An unique pointer to an object of type `IProbabilityFunction` that has been created
-             */
-            virtual std::unique_ptr<IProbabilityFunction> create() const = 0;
-
-    };
-
-    /**
-     * Allows to create instances of the type `IProbabilityFunction` that transform the score that is predicted for an
-     * individual label into a probability by applying the logistic sigmoid function.
-     */
-    class LogisticFunctionFactory final : public IProbabilityFunctionFactory {
-
-        public:
-
-            std::unique_ptr<IProbabilityFunction> create() const override;
-
-    };
-
-    /**
      * Defines an interface for all classes that allow to configure a predictor that predicts label-wise probabilities
      * for given query examples, which estimate the chance of individual labels to be relevant, by summing up the scores
      * that are provided by individual rules of an existing rule-based models and transforming the aggregated scores
