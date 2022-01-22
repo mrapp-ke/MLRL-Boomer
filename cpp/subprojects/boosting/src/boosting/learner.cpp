e9daef1ec5db95f254e7041a42fd96ebd1ca919b
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-23 00:15:13 +0100

    Remove obsolete code.

--- cpp/subprojects/boosting/src/boosting/learner.cpp
+++ cpp/subprojects/boosting/src/boosting/learner.cpp
@@ -1,12 +1,9 @@
 #include "boosting/learner.hpp"
 #include "boosting/binning/feature_binning_auto.hpp"
 #include "boosting/binning/label_binning_no.hpp"
-#include "boosting/losses/loss_label_wise_logistic.hpp"
 #include "boosting/model/rule_list_builder.hpp"
 #include "boosting/multi_threading/parallel_rule_refinement_auto.hpp"
 #include "boosting/multi_threading/parallel_statistic_update_auto.hpp"
-#include "boosting/rule_evaluation/rule_evaluation_label_wise_single.hpp"
-#include "boosting/statistics/statistics_provider_label_wise_dense.hpp"
 
 
 namespace boosting {
@@ -147,16 +144,7 @@ namespace boosting {
     std::unique_ptr<IStatisticsProviderFactory> BoostingRuleLearner::createStatisticsProviderFactory(
             const IRowWiseLabelMatrix& labelMatrix) const {
         // TODO Implement
-        float64 l1RegularizationWeight = 0;
-        float64 l2RegularizationWeight = 1;
-        uint32 numThreads = 1;
-        return std::make_unique<DenseLabelWiseStatisticsProviderFactory>(
-            std::make_unique<LabelWiseLogisticLossFactory>(),
-            std::make_unique<LabelWiseLogisticLossFactory>(),
-            std::make_unique<LabelWiseSingleLabelRuleEvaluationFactory>(l1RegularizationWeight, l2RegularizationWeight),
-            std::make_unique<LabelWiseSingleLabelRuleEvaluationFactory>(l1RegularizationWeight, l2RegularizationWeight),
-            std::make_unique<LabelWiseSingleLabelRuleEvaluationFactory>(l1RegularizationWeight, l2RegularizationWeight),
-            numThreads);
+        return nullptr;
     }
 
     std::unique_ptr<IModelBuilder> BoostingRuleLearner::createModelBuilder() const {
