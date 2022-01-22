3257433d7f6df0e2bb594a8eaafe8b4b08be2979
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-23 00:34:27 +0100

    Add Cython wrappers.

---
+++ python/subprojects/boosting/mlrl/boosting/cython/head_type.pyx
@@ -0,0 +1,17 @@
+"""
+@author: Michael Rapp (michael.rapp.ml@gmail.com)
+"""
+
+
+cdef class CompleteHeadConfig:
+    """
+    A wrapper for the C++ class `CompleteHeadConfig`.
+    """
+    pass
+
+
+cdef class SingleLabelHeadConfig:
+    """
+    A wrapper for the C++ class `SingleLabelHeadConfig`.
+    """
+    pass
