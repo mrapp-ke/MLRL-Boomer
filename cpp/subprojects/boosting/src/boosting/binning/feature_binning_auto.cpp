6e9132d723b1013438489ec9eac57dcb44a882e1
Michael Rapp <michael.rapp90@googlemail.com>
2022-01-23 22:02:43 +0100

    Add class AutomaticFeatureBinningConfig to namespace "boosting".

--- cpp/subprojects/boosting/src/boosting/binning/feature_binning_auto.cpp
+++ cpp/subprojects/boosting/src/boosting/binning/feature_binning_auto.cpp
@@ -3,17 +3,21 @@
 #include "common/binning/feature_binning_no.hpp"
 
 
-AutomaticFeatureBinningConfig::AutomaticFeatureBinningConfig(
-        const std::unique_ptr<IMultiThreadingConfig>& multiThreadingConfigPtr)
-    : multiThreadingConfigPtr_(multiThreadingConfigPtr) {
+namespace boosting {
 
-}
+    AutomaticFeatureBinningConfig::AutomaticFeatureBinningConfig(
+            const std::unique_ptr<IMultiThreadingConfig>& multiThreadingConfigPtr)
+        : multiThreadingConfigPtr_(multiThreadingConfigPtr) {
 
-std::unique_ptr<IThresholdsFactory> AutomaticFeatureBinningConfig::configure(
-        const IFeatureMatrix& featureMatrix) const {
-    if (!featureMatrix.isSparse() && featureMatrix.getNumRows() > 200000) {
-        return EqualWidthFeatureBinningConfig(multiThreadingConfigPtr_).configure(featureMatrix);
-    } else {
-        return NoFeatureBinningConfig(multiThreadingConfigPtr_).configure(featureMatrix);
     }
+
+    std::unique_ptr<IThresholdsFactory> AutomaticFeatureBinningConfig::configure(
+            const IFeatureMatrix& featureMatrix) const {
+        if (!featureMatrix.isSparse() && featureMatrix.getNumRows() > 200000) {
+            return EqualWidthFeatureBinningConfig(multiThreadingConfigPtr_).configure(featureMatrix);
+        } else {
+            return NoFeatureBinningConfig(multiThreadingConfigPtr_).configure(featureMatrix);
+        }
+    }
+
 }
