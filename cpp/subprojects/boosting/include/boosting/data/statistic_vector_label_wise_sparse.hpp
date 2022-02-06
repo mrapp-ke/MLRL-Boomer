42d26287e6e5a5a63847bba4c0de20a6a68bf4d1
Michael Rapp <michael.rapp90@googlemail.com>
2022-02-06 22:16:42 +0100

    Add TODO.

--- cpp/subprojects/boosting/include/boosting/data/statistic_vector_label_wise_sparse.hpp
+++ cpp/subprojects/boosting/include/boosting/data/statistic_vector_label_wise_sparse.hpp
@@ -95,6 +95,7 @@ namespace boosting {
      * An one-dimensional sparse vector that stores gradients and Hessians that have been calculated using a label-wise
      * decomposable loss function. For each element in the vector a single gradient and Hessian is stored.
      */
+    // TODO Add copy constructor
     class SparseLabelWiseStatisticVector final {
 
         private:
