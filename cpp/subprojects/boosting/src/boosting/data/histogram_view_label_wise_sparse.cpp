cba9b2a4484d61a3fab49418db7ceba2729fe4ce
Michael Rapp <michael.rapp90@googlemail.com>
2022-02-23 22:34:28 +0100

    Add class SparseLabelWiseHistogramConstView and SparseLabelWiseHistogramView.

---
+++ cpp/subprojects/boosting/src/boosting/data/histogram_view_label_wise_sparse.cpp
@@ -0,0 +1,50 @@
+#include "boosting/data/histogram_view_label_wise_sparse.hpp"
+
+
+namespace boosting {
+
+    SparseLabelWiseHistogramConstView::SparseLabelWiseHistogramConstView(uint32 numCols,
+                                                                         LilMatrix<Triple<float64>>* histogram)
+        : numCols_(numCols), histogram_(histogram) {
+
+    }
+
+    SparseLabelWiseHistogramConstView::const_iterator SparseLabelWiseHistogramConstView::row_cbegin(uint32 row) const {
+        return histogram_->getRow(row).cbegin();
+    }
+
+    SparseLabelWiseHistogramConstView::const_iterator SparseLabelWiseHistogramConstView::row_cend(uint32 row) const {
+        return histogram_->getRow(row).cend();
+    }
+
+    const SparseLabelWiseHistogramConstView::Row& SparseLabelWiseHistogramConstView::getRow(uint32 row) const {
+        return histogram_->getRow(row);
+    }
+
+    uint32 SparseLabelWiseHistogramConstView::getNumRows() const {
+        return histogram_->getNumRows();
+    }
+
+    uint32 SparseLabelWiseHistogramConstView::getNumCols() const {
+        return numCols_;
+    }
+
+    SparseLabelWiseHistogramView::SparseLabelWiseHistogramView(uint32 numCols, LilMatrix<Triple<float64>>* histogram)
+        : SparseLabelWiseHistogramConstView(numCols, histogram) {
+
+    }
+
+    SparseLabelWiseHistogramView::Row& SparseLabelWiseHistogramView::getRow(uint32 row) {
+        return histogram_->getRow(row);
+    }
+
+    void SparseLabelWiseHistogramView::clear() {
+        histogram_->clear();
+    }
+
+    void SparseLabelWiseHistogramView::addToRow(uint32 row, SparseLabelWiseStatisticConstView::const_iterator begin,
+                                                SparseLabelWiseStatisticConstView::const_iterator end, float64 weight) {
+        // TODO Implement
+    }
+
+}
