00dc64b8c18b83d6d659be196d7906a020509cf2
Michael Rapp <michael.rapp90@googlemail.com>
2022-02-23 23:08:43 +0100

    Use the class SparseLabelWiseHistogram to implement sparse histograms.

--- cpp/subprojects/boosting/src/boosting/statistics/statistics_provider_label_wise_sparse.cpp
+++ cpp/subprojects/boosting/src/boosting/statistics/statistics_provider_label_wise_sparse.cpp
@@ -64,7 +64,7 @@ namespace boosting {
     class SparseLabelWiseStatistics final : public AbstractLabelWiseStatistics<LabelMatrix,
                                                                                SparseLabelWiseStatisticVector,
                                                                                SparseLabelWiseStatisticView,
-                                                                               SparseLabelWiseStatisticMatrix,
+                                                                               SparseLabelWiseHistogram,
                                                                                NumericLilMatrix<float64>,
                                                                                ISparseLabelWiseLoss,
                                                                                ISparseEvaluationMeasure,
@@ -96,9 +96,8 @@ namespace boosting {
                                       std::unique_ptr<SparseLabelWiseStatisticView> statisticViewPtr,
                                       std::unique_ptr<NumericLilMatrix<float64>> scoreMatrixPtr)
                 : AbstractLabelWiseStatistics<LabelMatrix, SparseLabelWiseStatisticVector, SparseLabelWiseStatisticView,
-                                              SparseLabelWiseStatisticMatrix, NumericLilMatrix<float64>,
-                                              ISparseLabelWiseLoss, ISparseEvaluationMeasure,
-                                              ISparseLabelWiseRuleEvaluationFactory>(
+                                              SparseLabelWiseHistogram, NumericLilMatrix<float64>, ISparseLabelWiseLoss,
+                                              ISparseEvaluationMeasure, ISparseLabelWiseRuleEvaluationFactory>(
                       std::move(lossPtr), std::move(evaluationMeasurePtr), ruleEvaluationFactory, labelMatrix,
                       std::move(statisticViewPtr), std::move(scoreMatrixPtr)) {
 
