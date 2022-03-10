bcdab42136be7ce4ec15e8b82a90995903684912
Michael Rapp <michael.rapp90@googlemail.com>
2022-03-10 23:44:00 +0100

    Add class LabelWiseFixedRuleEvaluationFactory.

---
+++ cpp/subprojects/boosting/include/boosting/rule_evaluation/rule_evaluation_label_wise_fixed.hpp
@@ -0,0 +1,43 @@
+/*
+ * @author Michael Rapp (michael.rapp.ml@gmail.com)
+ */
+#pragma once
+
+#include "boosting/rule_evaluation/rule_evaluation_label_wise.hpp"
+
+
+namespace boosting {
+
+    /**
+     * Allows to create instances of the class `ILabelWiseRuleEvaluationFactory` that allow to calculate the predictions
+     * of partial rules, which predict for a predefined number of labels.
+     */
+    class LabelWiseFixedRuleEvaluationFactory final : public ILabelWiseRuleEvaluationFactory {
+
+        private:
+
+            float64 l1RegularizationWeight_;
+
+            float64 l2RegularizationWeight_;
+
+        public:
+
+            /**
+             * @param l1RegularizationWeight    The weight of the L1 regularization that is applied for calculating the
+             *                                  scores to be predicted by rules
+             * @param l2RegularizationWeight    The weight of the L2 regularization that is applied for calculating the
+             *                                  scores to be predicted by rules
+             */
+            LabelWiseFixedRuleEvaluationFactory(float64 l1RegularizationWeight, float64 l2RegularizationWeight);
+
+            std::unique_ptr<IRuleEvaluation<DenseLabelWiseStatisticVector>> create(
+                const DenseLabelWiseStatisticVector& statisticVector,
+                const CompleteIndexVector& indexVector) const override;
+
+            std::unique_ptr<IRuleEvaluation<DenseLabelWiseStatisticVector>> create(
+                const DenseLabelWiseStatisticVector& statisticVector,
+                const PartialIndexVector& indexVector) const override;
+
+    };
+
+}
