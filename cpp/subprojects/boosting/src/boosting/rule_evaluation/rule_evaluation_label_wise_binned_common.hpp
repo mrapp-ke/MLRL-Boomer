e97a91c9f93a0451f67e553a19a8c7621f6a79d0
Michael Rapp <michael.rapp90@googlemail.com>
2022-03-17 15:05:15 +0100

    Add class AbstractLabelWiseBinnedRuleEvaluation.

--- cpp/subprojects/boosting/src/boosting/rule_evaluation/rule_evaluation_label_wise_binned_common.hpp
+++ cpp/subprojects/boosting/src/boosting/rule_evaluation/rule_evaluation_label_wise_binned_common.hpp
@@ -42,4 +42,126 @@ namespace boosting {
         return overallQualityScore;
     }
 
+    /**
+     * An abstract base class for all classes that allow to calculate the predictions of rules, as well as corresponding
+     * quality scores, based on the gradients and Hessians that have been calculated according to a loss function that
+     * is applied label-wise and using gradient-based label binning.
+     *
+     * @tparam StatisticVector  The type of the vector that provides access to the gradients and Hessians
+     * @tparam T                The type of the vector that provides access to the labels for which predictions should
+     *                          be calculated
+     */
+    template<typename StatisticVector, typename T>
+    class AbstractLabelWiseBinnedRuleEvaluation : public IRuleEvaluation<StatisticVector> {
+
+        private:
+
+            uint32 maxBins_;
+
+            DenseBinnedScoreVector<T> scoreVector_;
+
+            DenseLabelWiseStatisticVector aggregatedStatisticVector_;
+
+            uint32* numElementsPerBin_;
+
+            float64* criteria_;
+
+            float64 l1RegularizationWeight_;
+
+            float64 l2RegularizationWeight_;
+
+            std::unique_ptr<ILabelBinning> binningPtr_;
+
+        protected:
+
+            /**
+             * Must be implemented by subclasses in order to calculate label-wise criteria that are used to determine
+             * the mapping from labels to bins.
+             *
+             * @param statisticVector           A reference to an object of template type `StatisticVector` that stores
+             *                                  the gradients and Hessians
+             * @param criteria                  A pointer to an array of type `float64`, shape `(numCriteria)`, the
+             *                                  label-wise criteria should be written to
+             * @param numCriteria               The number of label-wise criteria to be calculated
+             * @param l1RegularizationWeight    The L1 regularization weight
+             * @param l2RegularizationWeight    The L2 regularization weight
+             */
+            virtual void calculateLabelWiseCriteria(const StatisticVector& statisticVector, float64* criteria,
+                                                    uint32 numCriteria, float64 l1RegularizationWeight,
+                                                    float64 l2RegularizationWeight) = 0;
+
+        public:
+
+            /**
+             * @param labelIndices              A reference to an object of template type `T` that provides access to
+             *                                  the indices of the labels for which the rules may predict
+             * @param l1RegularizationWeight    The weight of the L1 regularization that is applied for calculating the
+             *                                  scores to be predicted by rules
+             * @param l2RegularizationWeight    The weight of the L2 regularization that is applied for calculating the
+             *                                  scores to be predicted by rules
+             * @param binningPtr                An unique pointer to an object of type `ILabelBinning` that should be
+             *                                  used to assign labels to bins
+             */
+            AbstractLabelWiseBinnedRuleEvaluation(const T& labelIndices, float64 l1RegularizationWeight,
+                                                  float64 l2RegularizationWeight,
+                                                  std::unique_ptr<ILabelBinning> binningPtr)
+                : maxBins_(binningPtr->getMaxBins(labelIndices.getNumElements())),
+                  scoreVector_(DenseBinnedScoreVector<T>(labelIndices, maxBins_ + 1, true)),
+                  aggregatedStatisticVector_(DenseLabelWiseStatisticVector(maxBins_)),
+                  numElementsPerBin_(new uint32[maxBins_]), criteria_(new float64[labelIndices.getNumElements()]),
+                  l1RegularizationWeight_(l1RegularizationWeight), l2RegularizationWeight_(l2RegularizationWeight),
+                  binningPtr_(std::move(binningPtr)) {
+                // The last bin is used for labels for which the corresponding criterion is zero. For this particular
+                // bin, the prediction is always zero.
+                scoreVector_.scores_binned_begin()[maxBins_] = 0;
+            }
+
+            virtual ~AbstractLabelWiseBinnedRuleEvaluation() override {
+                delete[] numElementsPerBin_;
+                delete[] criteria_;
+            }
+
+            const IScoreVector& calculatePrediction(StatisticVector& statisticVector) override final {
+                // Calculate label-wise criteria...
+                uint32 numLabels = statisticVector.getNumElements();
+                this->calculateLabelWiseCriteria(statisticVector, criteria_, numLabels, l1RegularizationWeight_,
+                                                 l2RegularizationWeight_);
+
+                // Obtain information about the bins to be used...
+                LabelInfo labelInfo = binningPtr_->getLabelInfo(criteria_, numLabels);
+                uint32 numBins = labelInfo.numPositiveBins + labelInfo.numNegativeBins;
+                scoreVector_.setNumBins(numBins, false);
+
+                // Reset arrays to zero...
+                DenseLabelWiseStatisticVector::iterator aggregatedStatisticIterator =
+                    aggregatedStatisticVector_.begin();
+                setArrayToZeros(aggregatedStatisticIterator, numBins);
+                setArrayToZeros(numElementsPerBin_, numBins);
+
+                // Apply binning method in order to aggregate the gradients and Hessians that belong to the same bins...
+                DenseLabelWiseStatisticVector::const_iterator statisticIterator = statisticVector.cbegin();
+                typename DenseBinnedScoreVector<T>::index_binned_iterator binIndexIterator =
+                    scoreVector_.indices_binned_begin();
+                auto callback = [=](uint32 binIndex, uint32 labelIndex) {
+                    aggregatedStatisticIterator[binIndex] += statisticIterator[labelIndex];
+                    numElementsPerBin_[binIndex] += 1;
+                    binIndexIterator[labelIndex] = binIndex;
+                };
+                auto zeroCallback = [=](uint32 labelIndex) {
+                    binIndexIterator[labelIndex] = maxBins_;
+                };
+                binningPtr_->createBins(labelInfo, criteria_, numLabels, callback, zeroCallback);
+
+                // Compute predictions, as well as an overall quality score...
+                typename DenseBinnedScoreVector<T>::score_binned_iterator scoreIterator =
+                    scoreVector_.scores_binned_begin();
+                scoreVector_.overallQualityScore = calculateBinnedScores(aggregatedStatisticIterator, scoreIterator,
+                                                                         numElementsPerBin_, numBins,
+                                                                         l1RegularizationWeight_,
+                                                                         l2RegularizationWeight_);
+                return scoreVector_;
+            }
+
+    };
+
 }
