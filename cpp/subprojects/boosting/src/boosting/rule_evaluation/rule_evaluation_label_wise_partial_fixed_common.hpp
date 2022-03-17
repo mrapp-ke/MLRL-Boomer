e052f6a639581552a1f82069927fa4fd2999d408
Michael Rapp <michael.rapp90@googlemail.com>
2022-03-17 16:36:52 +0100

    Add missing namespace.

--- cpp/subprojects/boosting/src/boosting/rule_evaluation/rule_evaluation_label_wise_partial_fixed_common.hpp
+++ cpp/subprojects/boosting/src/boosting/rule_evaluation/rule_evaluation_label_wise_partial_fixed_common.hpp
@@ -8,59 +8,64 @@
 #include <queue>
 
 
-/**
- * A priority queue with fixed capacity that stores quality scores and corresponding label indices. If the maximum
- * capacity of the queue is exceeded, the entry with the lowest quality score is discarded.
- */
-class FixedPriorityQueue final {
+namespace boosting {
 
-    private:
+    /**
+     * A priority queue with fixed capacity that stores quality scores and corresponding label indices. If the maximum
+     * capacity of the queue is exceeded, the entry with the lowest quality score is discarded.
+     */
+    class FixedPriorityQueue final {
 
-        typedef IndexedValue<float64> T;
+        private:
 
-        std::priority_queue<T, std::vector<T>, T::Compare> priorityQueue_;
+            typedef IndexedValue<float64> T;
 
-        uint32 maxCapacity_;
+            std::priority_queue<T, std::vector<T>, T::Compare> priorityQueue_;
 
-    public:
+            uint32 maxCapacity_;
 
-        /**
-         * @param maxCapacity The maximum capacity
-         */
-        FixedPriorityQueue(uint32 maxCapacity)
-            : maxCapacity_(maxCapacity) {
+        public:
 
-        }
+            /**
+             * @param maxCapacity The maximum capacity
+             */
+            FixedPriorityQueue(uint32 maxCapacity)
+                : maxCapacity_(maxCapacity) {
 
-        /**
-         * Adds a new quality score and corresponding label index to the priority queue.
-         *
-         * @param index         The index to be added
-         * @param qualityScore  The quality score to be added
-         */
-        void emplace(uint32 index, float64 qualityScore) {
-            if (priorityQueue_.size() < maxCapacity_) {
-                priorityQueue_.emplace(index, qualityScore);
-            } else if (priorityQueue_.top().value > qualityScore) {
-                priorityQueue_.pop();
-                priorityQueue_.emplace(index, qualityScore);
             }
-        }
 
-        /**
-         * Returns the entry with the lowest quality score.
-         *
-         * @return A reference to an object of type `IndexedValue` that stores the quality score and corresponding index
-         */
-        const IndexedValue<float64>& top() const {
-            return priorityQueue_.top();
-        }
+            /**
+             * Adds a new quality score and corresponding label index to the priority queue.
+             *
+             * @param index         The index to be added
+             * @param qualityScore  The quality score to be added
+             */
+            void emplace(uint32 index, float64 qualityScore) {
+                if (priorityQueue_.size() < maxCapacity_) {
+                    priorityQueue_.emplace(index, qualityScore);
+                } else if (priorityQueue_.top().value > qualityScore) {
+                    priorityQueue_.pop();
+                    priorityQueue_.emplace(index, qualityScore);
+                }
+            }
 
-        /**
-         * Removes the entry with the lowest quality score.
-         */
-        void pop() {
-            priorityQueue_.pop();
-        }
+            /**
+             * Returns the entry with the lowest quality score.
+             *
+             * @return A reference to an object of type `IndexedValue` that stores the quality score and corresponding
+             *         index
+             */
+            const IndexedValue<float64>& top() const {
+                return priorityQueue_.top();
+            }
 
-};
+            /**
+             * Removes the entry with the lowest quality score.
+             */
+            void pop() {
+                priorityQueue_.pop();
+            }
+
+    };
+
+}
