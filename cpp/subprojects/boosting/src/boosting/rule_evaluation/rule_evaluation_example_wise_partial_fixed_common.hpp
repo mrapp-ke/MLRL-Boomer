c5c3b2aaba47bf35bbcd858db9a7242e54060e40
Michael Rapp <michael.rapp90@googlemail.com>
2022-03-19 21:52:59 +0100

    Fix comparator that is used to compare label-wise criteria.

--- cpp/subprojects/boosting/src/boosting/rule_evaluation/rule_evaluation_example_wise_partial_fixed_common.hpp
+++ cpp/subprojects/boosting/src/boosting/rule_evaluation/rule_evaluation_example_wise_partial_fixed_common.hpp
@@ -13,10 +13,31 @@
 namespace boosting {
 
     /**
+     * Allows to compare two objects of type `IndexedValue` that store the optimal prediction for a labels, as well as
+     * its index, according to the following strict weak ordering: If the absolute value of the first object is greater,
+     * it goes before the second one.
+     */
+    struct CompareLabelWiseCriteria {
+
+        /**
+         * Returns whether the a given object of type `IndexedValue` that stores the optimal prediction for a label, as
+         * well as its index, should go before a second one.
+         *
+         * @param lhs   A reference to a first object of type `IndexedValue`
+         * @param rhs   A reference to a second object of type `IndexedValue`
+         * @return      True, if the first object should go before the second one, false otherwise
+         */
+        inline bool operator()(const IndexedValue<float64>& lhs, const IndexedValue<float64>& rhs) const {
+            return std::abs(lhs.value) > std::abs(rhs.value);
+        }
+
+    };
+
+    /**
      * The type of a priority queue which is used to identify the labels for which a rule is able to predict most
      * accurately.
      */
-    typedef std::priority_queue<IndexedValue<float64>, std::vector<IndexedValue<float64>>, IndexedValue<float64>::Compare> PriorityQueue;
+    typedef std::priority_queue<IndexedValue<float64>, std::vector<IndexedValue<float64>>, CompareLabelWiseCriteria> PriorityQueue;
 
     /**
      * Calculates scores that assess the quality of optimal predictions for each label and adds them to a priority queue
@@ -45,7 +66,7 @@ namespace boosting {
 
             if (priorityQueue.size() < maxCapacity) {
                 priorityQueue.emplace(indexIterator[i], score);
-            } else if (priorityQueue.top().value > score) {
+            } else if (std::abs(priorityQueue.top().value) < std::abs(score)) {
                 priorityQueue.pop();
                 priorityQueue.emplace(indexIterator[i], score);
             }
