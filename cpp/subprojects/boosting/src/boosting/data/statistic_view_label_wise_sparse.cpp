a225f3908394cb3f400b674489a8a1d854605141
Michael Rapp <michael.rapp90@googlemail.com>
2022-04-24 18:29:07 +0200

    Add class SparseLabelWiseStatisticConstView and SparseLabelWiseStatisticView.

---
+++ cpp/subprojects/boosting/src/boosting/data/statistic_view_label_wise_sparse.cpp
@@ -0,0 +1,45 @@
+#include "boosting/data/statistic_view_label_wise_sparse.hpp"
+
+
+namespace boosting {
+
+    SparseLabelWiseStatisticConstView::SparseLabelWiseStatisticConstView(uint32 numCols,
+                                                                         LilMatrix<Tuple<float64>>* statistics)
+        : numCols_(numCols), statistics_(statistics) {
+
+    }
+
+    SparseLabelWiseStatisticConstView::const_iterator SparseLabelWiseStatisticConstView::row_cbegin(uint32 row) const {
+        return statistics_->getRow(row).cbegin();
+    }
+
+    SparseLabelWiseStatisticConstView::const_iterator SparseLabelWiseStatisticConstView::row_cend(uint32 row) const {
+        return statistics_->getRow(row).cend();
+    }
+
+    const SparseLabelWiseStatisticConstView::Row& SparseLabelWiseStatisticConstView::getRow(uint32 row) const {
+        return statistics_->getRow(row);
+    }
+
+    uint32 SparseLabelWiseStatisticConstView::getNumRows() const {
+        return statistics_->getNumRows();
+    }
+
+    uint32 SparseLabelWiseStatisticConstView::getNumCols() const {
+        return numCols_;
+    }
+
+    SparseLabelWiseStatisticView::SparseLabelWiseStatisticView(uint32 numCols, LilMatrix<Tuple<float64>>* statistics)
+        : SparseLabelWiseStatisticConstView(numCols, statistics) {
+
+    }
+
+    SparseLabelWiseStatisticView::Row& SparseLabelWiseStatisticView::getRow(uint32 row) {
+        return statistics_->getRow(row);
+    }
+
+    void SparseLabelWiseStatisticView::clear() {
+        statistics_->clear();
+    }
+
+}
