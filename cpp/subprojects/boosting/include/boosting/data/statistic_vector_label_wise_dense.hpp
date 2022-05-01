89024be2eaa5b1df95d76ddea822f3a8ad0fbd63
Michael Rapp <michael.rapp90@googlemail.com>
2022-05-02 00:43:06 +0200

    Remove unused include.

--- cpp/subprojects/boosting/include/boosting/data/statistic_vector_label_wise_dense.hpp
+++ cpp/subprojects/boosting/include/boosting/data/statistic_vector_label_wise_dense.hpp
@@ -6,7 +6,6 @@
 #include "common/indices/index_vector_complete.hpp"
 #include "common/indices/index_vector_partial.hpp"
 #include "boosting/data/statistic_view_label_wise_dense.hpp"
-#include <memory>
 
 
 namespace boosting {
