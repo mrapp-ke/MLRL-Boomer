268240aecd28903d6cae2c6aef3a9e0d74310d2b
Michael Rapp <michael.rapp90@googlemail.com>
2022-05-04 13:13:15 +0200

    Implement "addToRowFromSubset" functions of the class NumericLilMatrix.

--- cpp/subprojects/boosting/src/boosting/data/matrix_lil_numeric.cpp
+++ cpp/subprojects/boosting/src/boosting/data/matrix_lil_numeric.cpp
@@ -1,8 +1,18 @@
 #include "boosting/data/matrix_lil_numeric.hpp"
-#include <iostream>  // TODO Remove
 
 
 namespace boosting {
+
+    template<typename T, typename IndexIterator>
+    static inline void addToRowFromSubsetInternally(typename NumericLilMatrix<T>::Row& row,
+                                                    typename VectorConstView<T>::const_iterator iterator,
+                                                    IndexIterator indexIterator, uint32 numElements) {
+        for (uint32 i = 0; i < numElements; i++) {
+            uint32 index = indexIterator[i];
+            IndexedValue<T>& entry = row.emplace(index, 0);
+            entry.value += iterator[i];
+        }
+    }
 
     template<typename T>
     NumericLilMatrix<T>::NumericLilMatrix(uint32 numRows, uint32 numCols)
@@ -15,9 +25,9 @@ namespace boosting {
                                                  typename VectorConstView<T>::const_iterator end,
                                                  CompleteIndexVector::const_iterator indicesBegin,
                                                  CompleteIndexVector::const_iterator indicesEnd) {
-        // TODO Implement
-        std::cout << "NumericLilMatrix::addToRowFromSubset(CompleteIndexVector)\n";
-        std::exit(-1);
+        uint32 numElements = indicesEnd - indicesBegin;
+        addToRowFromSubsetInternally<T, CompleteIndexVector::const_iterator>(this->getRow(row), begin, indicesBegin,
+                                                                             numElements);
     }
 
     template<typename T>
@@ -25,9 +35,9 @@ namespace boosting {
                                                  typename VectorConstView<T>::const_iterator end,
                                                  PartialIndexVector::const_iterator indicesBegin,
                                                  PartialIndexVector::const_iterator indicesEnd) {
-        // TODO Implement
-        std::cout << "NumericLilMatrix::addToRowFromSubset(PartialIndexVector)\n";
-        std::exit(-1);
+        uint32 numElements = indicesEnd - indicesBegin;
+        addToRowFromSubsetInternally<T, PartialIndexVector::const_iterator>(this->getRow(row), begin, indicesBegin,
+                                                                            numElements);
     }
 
     template class NumericLilMatrix<uint8>;
