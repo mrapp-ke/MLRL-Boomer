9872870eb6553125fd693cfcfc25747ba648fdeb
Michael Rapp <michael.rapp90@googlemail.com>
2022-05-23 02:02:11 +0200

    Add integration tests that test the functionality to store/print predictions.

--- python/subprojects/testbed/tests/test_boomer.py
+++ python/subprojects/testbed/tests/test_boomer.py
@@ -71,6 +71,52 @@ class BoostingIntegrationTests(IntegrationTests):
             .set_model_dir()
         self.run_cmd(builder, 'boomer_model_persistence_single_fold')
 
+    def test_predictions_train_test(self):
+        """
+        Tests the functionality to store the predictions of the BOOMER algorithm when using a predefined split of the
+        dataset into training and test data.
+        """
+        builder = CmdBuilder() \
+            .set_output_dir() \
+            .print_predictions() \
+            .store_predictions()
+        self.run_cmd(builder, 'boomer_predictions_train_test')
+
+    def test_predictions_cross_validation(self):
+        """
+        Tests the functionality to store the predictions of the BOOMER algorithm when using a cross validation.
+        """
+        builder = CmdBuilder() \
+            .cross_validation() \
+            .set_output_dir() \
+            .print_predictions() \
+            .store_predictions()
+        self.run_cmd(builder, 'boomer_predictions_cross_validation')
+
+    def test_predictions_single_fold(self):
+        """
+        Tests the functionality to store the predictions of the BOOMER algorithm when using a single fold of a cross
+        validation.
+        """
+        builder = CmdBuilder() \
+            .cross_validation(current_fold=1) \
+            .set_output_dir() \
+            .print_predictions() \
+            .store_predictions()
+        self.run_cmd(builder, 'boomer_predictions_single_fold')
+
+    def test_predictions_training_data(self):
+        """
+        Tests the functionality to store the predictions of the BOOMER algorithm when using a single fold of a cross
+        validation.
+        """
+        builder = CmdBuilder() \
+            .evaluate_training_data() \
+            .set_output_dir() \
+            .print_predictions() \
+            .store_predictions()
+        self.run_cmd(builder, 'boomer_predictions_training_data')
+
 
 if __name__ == '__main__':
     main()
