d7638bb3d98c700f0e50a02ba2785c0a50d805ba
Michael Rapp <michael.rapp90@googlemail.com>
2022-05-24 13:13:37 +0200

    Edit comment.

--- python/subprojects/common/mlrl/common/arrays.py
+++ python/subprojects/common/mlrl/common/arrays.py
@@ -9,12 +9,13 @@ from scipy.sparse import issparse
 
 def enforce_dense(a, order: str, dtype):
     """
-    Converts a given array into a `np.ndarray`, if necessary, and enforces a specific memory layout and type.
+    Converts a given array into a `np.ndarray`, if necessary, and enforces a specific memory layout and data type to be
+    used.
 
     :param a:       A `np.ndarray` or `scipy.sparse.matrix` to be converted
     :param order:   The memory layout to be used. Must be `C` or `F`
-    :param dtype:   The type to be used
-    :return:        A `np.ndarray` that uses the given memory layout
+    :param dtype:   The data type to be used
+    :return:        A `np.ndarray` that uses the given memory layout and data type
     """
     if issparse(a):
         return np.require(a.toarray(order=order), dtype=dtype)
