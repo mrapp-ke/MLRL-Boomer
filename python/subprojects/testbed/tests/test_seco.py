e225bd29e659b660a80649ee202955fb687f3b36
Michael Rapp <michael.rapp90@googlemail.com>
2022-05-25 16:24:42 +0200

    Add integration tests for the SeCo algorithm.

---
+++ python/subprojects/testbed/tests/test_seco.py
@@ -0,0 +1,19 @@
+"""
+Author: Michael Rapp (michael.rapp.ml@gmail.com)
+"""
+
+from integration_tests import DATASET_WEATHER
+from test_common import CommonIntegrationTests
+
+
+class SeCoIntegrationTests(CommonIntegrationTests):
+    """
+    Defines a series of integration tests for the separate-and-conquer (SeCo) algorithm.
+    """
+
+    def __init__(self, methodName='runTest'):
+        """
+        :param methodName: The name of the test method to be executed
+        """
+        super(SeCoIntegrationTests, self).__init__('seco', dataset_one_hot_encoding=DATASET_WEATHER,
+                                                   methodName=methodName)
