1999c2265ae2ca0a64a99d3ff154c647566d4f51
Michael Rapp <michael.rapp90@googlemail.com>
2022-05-30 16:07:43 +0200

    Move typedef BinWeightVector to new file bin_weight_vector.hpp.

---
+++ cpp/subprojects/common/include/common/binning/bin_weight_vector.hpp
@@ -0,0 +1,12 @@
+/*
+ * @author Michael Rapp (michael.rapp.ml@gmail.com)
+ */
+#pragma once
+
+#include "common/data/vector_bit.hpp"
+
+
+/**
+ * A vector that stores the weights of individual bins.
+ */
+typedef BitVector BinWeightVector;
