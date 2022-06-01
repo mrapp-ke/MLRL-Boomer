8993e81bb9c92f4e2ef9f3e5f86020ee2957c20d
Michael Rapp <michael.rapp90@googlemail.com>
2022-06-01 17:50:04 +0200

    Add class OutOfSampleWeightVector.

---
+++ cpp/subprojects/common/include/common/sampling/weight_vector_out_of_sample.hpp
@@ -0,0 +1,36 @@
+/*
+ * @author Michael Rapp (michael.rapp.ml@gmail.com)
+ */
+#pragma once
+
+#include "common/data/types.hpp"
+
+/**
+ * An one-dimensional vector that provides random access to a fixed number of weights that are obtained from another
+ * vector by setting zero weights to one and non-zero weights to zero.
+ *
+ * @tparam T The type of the other vector
+ */
+template<typename T>
+class OutOfSampleWeightVector final {
+
+    private:
+
+        const T& vector_;
+
+    public:
+
+        /**
+         * @param vector A reference to an object of template type `T` that provides access to the original weights
+         */
+        OutOfSampleWeightVector(const T& vector);
+
+        /**
+         * Returns the weight at a specific index.
+         *
+         * @param pos   The index
+         * @return      The weight at the given index
+         */
+        float64 getWeight(uint32 pos) const;
+
+};
---
+++ cpp/subprojects/common/src/common/sampling/weight_vector_out_of_sample.cpp
@@ -0,0 +1,20 @@
+#include "common/sampling/weight_vector_out_of_sample.hpp"
+#include "common/sampling/weight_vector_equal.hpp"
+#include "common/sampling/weight_vector_bit.hpp"
+#include "common/sampling/weight_vector_dense.hpp"
+
+
+template<typename T>
+OutOfSampleWeightVector<T>::OutOfSampleWeightVector(const T& vector)
+    : vector_(vector) {
+
+}
+
+template<typename T>
+float64 OutOfSampleWeightVector<T>::getWeight(uint32 pos) const {
+    return vector_.getWeight(pos) > 0 ? 0 : 1;
+}
+
+template class OutOfSampleWeightVector<EqualWeightVector>;
+template class OutOfSampleWeightVector<BitWeightVector>;
+template class OutOfSampleWeightVector<DenseWeightVector<uint32>>;
