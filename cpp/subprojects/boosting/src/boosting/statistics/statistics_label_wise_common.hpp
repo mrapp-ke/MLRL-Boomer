8ba6fc7dfb20ea1dce99e06e58aa3eff06b1472e
Michael Rapp <michael.rapp90@googlemail.com>
2022-06-01 13:54:27 +0200

    The class LabelWiseHistogram now stores an object of template type BinIndexVector.

--- cpp/subprojects/boosting/src/boosting/statistics/statistics_label_wise_common.hpp
+++ cpp/subprojects/boosting/src/boosting/statistics/statistics_label_wise_common.hpp
@@ -369,6 +369,8 @@ namespace boosting {
 
             std::unique_ptr<Histogram> histogramPtr_;
 
+            const BinIndexVector& binIndexVector_;
+
             const StatisticView& originalStatisticView_;
 
             const StatisticVector& totalSumVector_;
@@ -378,6 +380,8 @@ namespace boosting {
             /**
              * @param histogramPtr          An unique pointer to an object of template type `Histogram` that stores the
              *                              gradients and Hessians in the histogram
+             * @param binIndexVector        A reference to an object of template type `BinIndexVector` that stores the
+             *                              indices of the bins, individual examples have been assigned to
              * @param originalStatisticView A reference to an object of template type `StatisticView` that provides
              *                              access to the original gradients and Hessians, the histogram was created
              *                              from
@@ -387,13 +391,13 @@ namespace boosting {
              *                              create instances of the class that should be used for calculating the
              *                              predictions of rules, as well as corresponding quality scores
              */
-            LabelWiseHistogram(std::unique_ptr<Histogram> histogramPtr, const StatisticView& originalStatisticView,
-                               const StatisticVector& totalSumVector,
+            LabelWiseHistogram(std::unique_ptr<Histogram> histogramPtr, const BinIndexVector& binIndexVector,
+                               const StatisticView& originalStatisticView, const StatisticVector& totalSumVector,
                                const RuleEvaluationFactory& ruleEvaluationFactory)
                 : AbstractLabelWiseImmutableWeightedStatistics<StatisticVector, Histogram, RuleEvaluationFactory,
                                                                BinWeightVector>(*histogramPtr, ruleEvaluationFactory),
-                  histogramPtr_(std::move(histogramPtr)), originalStatisticView_(originalStatisticView),
-                  totalSumVector_(totalSumVector) {
+                  histogramPtr_(std::move(histogramPtr)), binIndexVector_(binIndexVector),
+                  originalStatisticView_(originalStatisticView), totalSumVector_(totalSumVector) {
 
             }
 
@@ -575,7 +579,8 @@ namespace boosting {
                     std::make_unique<Histogram>(numBins, originalStatisticView.getNumCols());
                 return std::make_unique<LabelWiseHistogram<StatisticVector, StatisticView, Histogram,
                                                            RuleEvaluationFactory, DenseBinIndexVector>>(
-                    std::move(histogramPtr), originalStatisticView, *totalSumVectorPtr_, this->ruleEvaluationFactory_);
+                    std::move(histogramPtr), binIndexVector, originalStatisticView, *totalSumVectorPtr_,
+                    this->ruleEvaluationFactory_);
             }
 
             /**
@@ -588,7 +593,8 @@ namespace boosting {
                     std::make_unique<Histogram>(numBins, originalStatisticView.getNumCols());
                 return std::make_unique<LabelWiseHistogram<StatisticVector, StatisticView, Histogram,
                                                            RuleEvaluationFactory, DokBinIndexVector>>(
-                    std::move(histogramPtr), originalStatisticView, *totalSumVectorPtr_, this->ruleEvaluationFactory_);
+                    std::move(histogramPtr), binIndexVector, originalStatisticView, *totalSumVectorPtr_,
+                    this->ruleEvaluationFactory_);
             }
 
             /**
