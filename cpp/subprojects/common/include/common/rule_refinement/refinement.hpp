93160f40bdf6d9e6a617d2a273fc296e1eceaf05
Michael Rapp <michael.rapp90@googlemail.com>
2022-06-07 00:02:51 +0200

    Remove function "isBetterThan" from struct Refinement.

--- cpp/subprojects/common/include/common/rule_refinement/refinement.hpp
+++ cpp/subprojects/common/include/common/rule_refinement/refinement.hpp
@@ -26,23 +26,6 @@ struct Refinement : public Condition {
     }
 
     /**
-     * Returns whether this refinement is better than another one.
-     *
-     * @param another   A reference to an object of type `Refinement` to be compared to
-     * @return          True, if this refinement is better than the given one, false otherwise
-     */
-    bool isBetterThan(const Refinement& another) const {
-        const AbstractEvaluatedPrediction* head = headPtr.get();
-
-        if (head) {
-            const AbstractEvaluatedPrediction* anotherHead = another.headPtr.get();
-            return !anotherHead || head->overallQualityScore < anotherHead->overallQualityScore;
-        }
-
-        return false;
-    }
-
-    /**
      * An unique pointer to an object of type `AbstractEvaluatedPrediction` that stores the scores that are
      * predicted by the refined rule, as well as a corresponding quality score.
      */
