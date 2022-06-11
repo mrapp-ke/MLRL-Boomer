3d6ae611af3487fdd28cd1b36a06ddebeeea23a9
Michael Rapp <michael.rapp90@googlemail.com>
2022-06-11 21:58:38 +0200

    Fix order of refinements that are stored by a FixedRefinementComparator.

--- cpp/subprojects/common/src/common/rule_refinement/refinement_comparator_fixed.cpp
+++ cpp/subprojects/common/src/common/rule_refinement/refinement_comparator_fixed.cpp
@@ -28,11 +28,11 @@ uint32 FixedRefinementComparator::getNumElements() const {
 }
 
 FixedRefinementComparator::iterator FixedRefinementComparator::begin() {
-    return order_.rbegin();
+    return order_.begin();
 }
 
 FixedRefinementComparator::iterator FixedRefinementComparator::end() {
-    return order_.rend();
+    return order_.end();
 }
 
 bool FixedRefinementComparator::isImprovement(const IScoreVector& scoreVector) const {
@@ -49,17 +49,17 @@ void FixedRefinementComparator::pushRefinement(const Refinement& refinement, con
         scoreProcessor.processScores(scoreVector);
         order_.push_back(newRefinement);
     } else {
-        Refinement& worstRefinement = order_.front();
+        Refinement& worstRefinement = order_.back();
         worstRefinement = refinement;
         ScoreProcessor scoreProcessor(worstRefinement.headPtr);
         scoreProcessor.processScores(scoreVector);
     }
 
     std::sort(order_.begin(), order_.end(), [=](const Refinement& a, const Refinement& b) {
-        return a.headPtr->overallQualityScore > a.headPtr->overallQualityScore;
+        return a.headPtr->overallQualityScore < b.headPtr->overallQualityScore;
     });
 
-    const Refinement& worstRefinement = order_.front();
+    const Refinement& worstRefinement = order_.back();
     minQualityScore_ = worstRefinement.headPtr->overallQualityScore;
 }
 
@@ -68,54 +68,56 @@ bool FixedRefinementComparator::merge(FixedRefinementComparator& comparator) {
     Refinement* tmp = new Refinement[maxRefinements_];
     uint32 n = 0;
 
-    while (n < maxRefinements_ && !order_.empty() && !comparator.order_.empty()) {
-        Refinement& refinement1 = order_.back();
-        Refinement& refinement2 = comparator.order_.back();
+    auto it1 = order_.begin();
+    auto end1 = order_.end();
+    auto it2 = comparator.order_.begin();
+    auto end2 = comparator.order_.end();
+
+    while (n < maxRefinements_ && it1 != end1 && it2 != end2) {
+        Refinement& refinement1 = *it1;
+        Refinement& refinement2 = *it2;
         Refinement& newRefinement = tmp[n];
 
         if (refinement1.headPtr->overallQualityScore < refinement2.headPtr->overallQualityScore) {
             newRefinement = refinement1;
             newRefinement.headPtr = std::move(refinement1.headPtr);
-            order_.pop_back();
+            it1++;
         } else {
             result = true;
             newRefinement = refinement2;
             newRefinement.headPtr = std::move(refinement2.headPtr);
-            comparator.order_.pop_back();
+            it2++;
         }
 
         n++;
     }
 
-    while (!order_.empty()) {
-        if (n < maxRefinements_) {
-            Refinement& refinement = order_.back();
-            Refinement& newRefinement = tmp[n];
-            newRefinement = refinement;
-            newRefinement.headPtr = std::move(refinement.headPtr);
-            n++;
-        }
-
-        order_.pop_back();
-    }
-
-    while (n < maxRefinements_ && !comparator.order_.empty()) {
-        result = true;
-        Refinement& refinement = comparator.order_.back();
+    for (; n < maxRefinements_ && it1 != end1; it1++) {
+        Refinement& refinement = *it1;
         Refinement& newRefinement = tmp[n];
         newRefinement = refinement;
         newRefinement.headPtr = std::move(refinement.headPtr);
-        comparator.order_.pop_back();
         n++;
     }
 
+    for (; n < maxRefinements_ && it2 != end2; it2++) {
+        result = true;
+        Refinement& refinement = *it2;
+        Refinement& newRefinement = tmp[n];
+        newRefinement = refinement;
+        newRefinement.headPtr = std::move(refinement.headPtr);
+        n++;
+    }
+
+    order_.clear();
+
     for (uint32 i = 0; i < n; i++) {
-        Refinement& newRefinement = tmp[n - i - 1];
+        Refinement& newRefinement = tmp[i];
         order_.push_back(newRefinement);
     }
 
     if (n > 0) {
-        const Refinement& worstRefinement = order_.front();
+        const Refinement& worstRefinement = order_.back();
         minQualityScore_ = worstRefinement.headPtr->overallQualityScore;
     }
 
