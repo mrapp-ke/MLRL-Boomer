0a00c1af22a404acf95000e3e04c5f017307e392
Michael Rapp <michael.rapp90@googlemail.com>
2022-06-14 17:49:04 +0200

    Add class AbstractBoostingLearner.

---
+++ cpp/subprojects/boosting/include/boosting/learner_common.hpp
@@ -0,0 +1,368 @@
+/*
+ * @author Michael Rapp (michael.rapp.ml@gmail.com)
+ */
+#pragma once
+
+#ifdef _WIN32
+    #pragma warning( push )
+    #pragma warning( disable : 4250 )
+#endif
+
+#include "common/learner.hpp"
+#include "boosting/binning/label_binning.hpp"
+#include "boosting/losses/loss.hpp"
+#include "boosting/math/blas.hpp"
+#include "boosting/math/lapack.hpp"
+#include "boosting/post_processing/shrinkage_constant.hpp"
+#include "boosting/rule_evaluation/head_type.hpp"
+#include "boosting/rule_evaluation/regularization_manual.hpp"
+#include "boosting/statistics/statistic_format.hpp"
+
+
+namespace boosting {
+
+    /**
+     * Defines an interface for all rule learners that make use of gradient boosting.
+     */
+    class MLRLBOOSTING_API IBoostingRuleLearner : virtual public IRuleLearner {
+
+        public:
+
+            /**
+             * Defines an interface for all classes that allow to configure a rule learner that makes use of gradient
+             * boosting.
+             */
+            class IConfig : virtual public IRuleLearner::IConfig {
+
+                friend class AbstractBoostingRuleLearner;
+
+                private:
+
+                    /**
+                     * Returns the configuration of the rule heads that should be induced by the rule learner.
+                     *
+                     * @return A reference to an object of type `IHeadConfig` that specifies the configuration of the
+                     *         rule heads
+                     */
+                    virtual const IHeadConfig& getHeadConfig() const = 0;
+
+                    /**
+                     * Returns the configuration of the statistics that should be used by the rule learner.
+                     *
+                     * @return A reference to an object of type `IStatisticsConfig` that specifies the configuration of
+                     *         the statistics
+                     */
+                    virtual const IStatisticsConfig& getStatisticsConfig() const = 0;
+
+                    /**
+                     * Returns the configuration of the L1 regularization term.
+                     *
+                     * @return A reference to an object of type `IRegularizationConfig` that specifies the configuration
+                     *         of the L1 regularization term
+                     */
+                    virtual const IRegularizationConfig& getL1RegularizationConfig() const = 0;
+
+                    /**
+                     * Returns the configuration of the L2 regularization term.
+                     *
+                     * @return A reference to an object of type `IRegularizationConfig` that specifies the configuration
+                     *         of the L2 regularization term
+                     */
+                    virtual const IRegularizationConfig& getL2RegularizationConfig() const = 0;
+
+                    /**
+                     * Returns the configuration of the loss function.
+                     *
+                     * @return A reference to an object of type `ILossConfig` that specifies the configuration of the
+                     *         loss function
+                     */
+                    virtual const ILossConfig& getLossConfig() const = 0;
+
+                    /**
+                     * Returns the configuration of the method for the assignment of labels to bins.
+                     *
+                     * @return A reference to an object of type `ILabelBinningConfig` that specifies the configuration
+                     *         of the method for the assignment of labels to bins
+                     */
+                    virtual const ILabelBinningConfig& getLabelBinningConfig() const = 0;
+
+                    /**
+                     * Returns the configuration of the predictor that predicts whether individual labels of given query
+                     * examples are relevant or irrelevant.
+                     *
+                     * @return A reference to an object of type `IClassificationPredictorConfig` that specifies the
+                     *         configuration of the predictor that predicts whether individual labels of given query
+                     *         examples are relevant or irrelevant
+                     */
+                    virtual const IClassificationPredictorConfig& getClassificationPredictorConfig() const = 0;
+
+                    /**
+                     * Returns the configuration of the predictor that predicts regression scores for individual labels.
+                     *
+                     * @return A reference to an object of type `IClassificationPredictorConfig` that specifies the
+                     *         configuration of the predictor that predicts regression scores for individual labels
+                     */
+                    virtual const IRegressionPredictorConfig& getRegressionPredictorConfig() const = 0;
+
+                    /**
+                     * Returns the configuration of the predictor that predicts probability estimates for individual
+                     * labels.
+                     *
+                     * @return A reference to an object of type `IClassificationPredictorConfig` that specifies the
+                     *         configuration of the predictor that predicts probability estimates for individual labels
+                     */
+                    virtual const IProbabilityPredictorConfig& getProbabilityPredictorConfig() const = 0;
+
+                public:
+
+                    virtual ~IConfig() override { };
+
+                    /**
+                     * Configures the rule learner to induce rules with single-label heads that predict for a single
+                     * label.
+                     */
+                    virtual void useSingleLabelHeads() = 0;
+
+                    /**
+                     * Configures the rule learner to use a dense representation of gradients and Hessians.
+                     */
+                    virtual void useDenseStatistics() = 0;
+
+                    /**
+                     * Configures the rule learner to not use L1 regularization.
+                     */
+                    virtual void useNoL1Regularization() = 0;
+
+                    /**
+                     * Configures the rule learner to not use L2 regularization.
+                     */
+                    virtual void useNoL2Regularization() = 0;
+
+                    /**
+                     * Configures the rule learner to use a loss function that implements a multi-label variant of the
+                     * logistic loss that is applied label-wise.
+                     */
+                    virtual void useLabelWiseLogisticLoss() = 0;
+
+                    /**
+                     * Configures the rule learner to not use any method for the assignment of labels to bins.
+                     */
+                    virtual void useNoLabelBinning() = 0;
+
+                    /**
+                     * Configures the rule learner to use a predictor for predicting whether individual labels are
+                     * relevant or irrelevant by summing up the scores that are provided by the individual rules of an
+                     * existing rule-based model and transforming them into binary values according to a certain
+                     * threshold that is applied to each label individually.
+                     */
+                    virtual void useLabelWiseClassificationPredictor() = 0;
+
+                    /**
+                     * Configures the rule learner to use a predictor for predicting regression scores by summing up the
+                     * scores that are provided by the individual rules of an existing rule-based model for each label
+                     * individually.
+                     */
+                    virtual void useLabelWiseRegressionPredictor() = 0;
+
+                    /**
+                     * Configures the rule learner to use a predictor for predicting probability estimates by summing up
+                     * the scores that are provided by individual rules of an existing rule-based model and transforming
+                     * the aggregated scores into probabilities according to a certain transformation function that is
+                     * applied to each label individually.
+                     */
+                    virtual void useLabelWiseProbabilityPredictor() = 0;
+
+            };
+
+
+            /**
+             * Defines an interface for all classes that allow to configure a rule learner to use a post processor that
+             * shrinks the weights fo rules by a "shrinkage" parameter.
+             */
+            class IShrinkageMixin {
+
+                public:
+
+                    virtual ~IShrinkageMixin() { };
+
+                    /**
+                     * Configures the rule learner to use a post processor that shrinks the weights of rules by a
+                     * constant "shrinkage" parameter.
+                     *
+                     * @return A reference to an object of type `IConstantShrinkageConfig` that allows further
+                     *         configuration of the loss function
+                     */
+                    virtual IConstantShrinkageConfig& useConstantShrinkagePostProcessor() = 0;
+
+            };
+
+            /**
+             * Defines an interface for all classes that allow to configure a rule learner to use regularization.
+             */
+            class IRegularizationMixin {
+
+                public:
+
+                    virtual ~IRegularizationMixin() { };
+
+                    /**
+                     * Configures the rule learner to use L1 regularization.
+                     *
+                     * @return A reference to an object of type `IManualRegularizationConfig` that allows further
+                     *         configuration of the regularization term
+                     */
+                    virtual IManualRegularizationConfig& useL1Regularization() = 0;
+
+                    /**
+                     * Configures the rule learner to use L2 regularization.
+                     *
+                     * @return A reference to an object of type `IManualRegularizationConfig` that allows further
+                     *         configuration of the regularization term
+                     */
+                    virtual IManualRegularizationConfig& useL2Regularization() = 0;
+
+            };
+
+            virtual ~IBoostingRuleLearner() override { };
+
+    };
+
+    /**
+     * An abstract base class for all rule learners that makes use of gradient boosting.
+     */
+    class AbstractBoostingRuleLearner : public AbstractRuleLearner, virtual public IBoostingRuleLearner {
+
+        public:
+
+            /**
+             * Allows to configure a rule learner that makes use of gradient boosting.
+             */
+            class Config : public AbstractRuleLearner::Config, virtual public IBoostingRuleLearner::IConfig {
+
+                private:
+
+                    std::unique_ptr<IHeadConfig> headConfigPtr_;
+
+                    std::unique_ptr<IStatisticsConfig> statisticsConfigPtr_;
+
+                    std::unique_ptr<ILossConfig> lossConfigPtr_;
+
+                    std::unique_ptr<IRegularizationConfig> l1RegularizationConfigPtr_;
+
+                    std::unique_ptr<IRegularizationConfig> l2RegularizationConfigPtr_;
+
+                    std::unique_ptr<ILabelBinningConfig> labelBinningConfigPtr_;
+
+                    std::unique_ptr<IClassificationPredictorConfig> classificationPredictorConfigPtr_;
+
+                    std::unique_ptr<IRegressionPredictorConfig> regressionPredictorConfigPtr_;
+
+                    std::unique_ptr<IProbabilityPredictorConfig> probabilityPredictorConfigPtr_;
+
+                    const IHeadConfig& getHeadConfig() const override final;
+
+                    const IStatisticsConfig& getStatisticsConfig() const override final;
+
+                    const IRegularizationConfig& getL1RegularizationConfig() const override final;
+
+                    const IRegularizationConfig& getL2RegularizationConfig() const override final;
+
+                    const ILossConfig& getLossConfig() const override final;
+
+                    const ILabelBinningConfig& getLabelBinningConfig() const override final;
+
+                    const IClassificationPredictorConfig& getClassificationPredictorConfig() const override final;
+
+                    const IRegressionPredictorConfig& getRegressionPredictorConfig() const override final;
+
+                    const IProbabilityPredictorConfig& getProbabilityPredictorConfig() const override final;
+
+                public:
+
+                    Config();
+
+                    void useSingleLabelHeads() override;
+
+                    void useDenseStatistics() override;
+
+                    void useNoL1Regularization() override;
+
+                    void useNoL2Regularization() override;
+
+                    void useLabelWiseLogisticLoss() override;
+
+                    void useNoLabelBinning() override;
+
+                    void useLabelWiseClassificationPredictor() override;
+
+                    void useLabelWiseRegressionPredictor() override;
+
+                    void useLabelWiseProbabilityPredictor() override;
+
+            };
+
+        private:
+
+            std::unique_ptr<IBoostingRuleLearner::IConfig> configPtr_;
+
+            Blas blas_;
+
+            Lapack lapack_;
+
+        protected:
+
+            /**
+             * @see `AbstractRuleLearner::createStatisticsProviderFactory`
+             */
+            std::unique_ptr<IStatisticsProviderFactory> createStatisticsProviderFactory(
+                const IFeatureMatrix& featureMatrix, const IRowWiseLabelMatrix& labelMatrix) const override;
+
+            /**
+             * @see `AbstractRuleLearner::createModelBuilderFactory`
+             */
+            std::unique_ptr<IModelBuilderFactory> createModelBuilderFactory() const override;
+
+            /**
+             * @see `AbstractRuleLearner::createLabelSpaceInfo`
+             */
+            std::unique_ptr<ILabelSpaceInfo> createLabelSpaceInfo(
+                const IRowWiseLabelMatrix& labelMatrix) const override;
+
+            /**
+             * @see `AbstractRuleLearner::createClassificationPredictorFactory`
+             */
+            std::unique_ptr<IClassificationPredictorFactory> createClassificationPredictorFactory(
+                const IFeatureMatrix& featureMatrix, uint32 numLabels) const override;
+
+            /**
+             * @see `AbstractRuleLearner::createRegressionPredictorFactory`
+             */
+            std::unique_ptr<IRegressionPredictorFactory> createRegressionPredictorFactory(
+                const IFeatureMatrix& featureMatrix, uint32 numLabels) const override;
+
+            /**
+             * @see `AbstractRuleLearner::createProbabilityPredictorFactory`
+             */
+            std::unique_ptr<IProbabilityPredictorFactory> createProbabilityPredictorFactory(
+                const IFeatureMatrix& featureMatrix, uint32 numLabels) const override;
+
+        public:
+
+            /**
+             * @param configPtr     An unique pointer to an object of type `IBoostingRuleLearner::IConfig` that
+             *                      specifies the configuration that should be used by the rule learner
+             * @param ddotFunction  A function pointer to BLAS' DDOT routine
+             * @param dspmvFunction A function pointer to BLAS' DSPMV routine
+             * @param dsysvFunction A function pointer to LAPACK'S DSYSV routine
+             */
+            AbstractBoostingRuleLearner(std::unique_ptr<IBoostingRuleLearner::IConfig> configPtr,
+                                        Blas::DdotFunction ddotFunction, Blas::DspmvFunction dspmvFunction,
+                                        Lapack::DsysvFunction dsysvFunction);
+
+    };
+
+}
+
+#ifdef _WIN32
+    #pragma warning ( pop )
+#endif
---
+++ cpp/subprojects/boosting/src/boosting/learner_common.cpp
@@ -0,0 +1,152 @@
+#include "boosting/learner_common.hpp"
+#include "boosting/binning/label_binning_no.hpp"
+#include "boosting/losses/loss_label_wise_logistic.hpp"
+#include "boosting/model/rule_list_builder.hpp"
+#include "boosting/output/predictor_classification_label_wise.hpp"
+#include "boosting/output/predictor_regression_label_wise.hpp"
+#include "boosting/output/predictor_probability_label_wise.hpp"
+#include "boosting/rule_evaluation/head_type_single.hpp"
+#include "boosting/rule_evaluation/regularization_no.hpp"
+#include "boosting/statistics/statistic_format_dense.hpp"
+#include "common/output/label_space_info_no.hpp"
+
+
+namespace boosting {
+
+    AbstractBoostingRuleLearner::Config::Config() {
+        this->useSingleLabelHeads();
+        this->useDenseStatistics();
+        this->useNoL1Regularization();
+        this->useNoL2Regularization();
+        this->useLabelWiseLogisticLoss();
+        this->useNoLabelBinning();
+        this->useLabelWiseClassificationPredictor();
+        this->useLabelWiseRegressionPredictor();
+        this->useLabelWiseProbabilityPredictor();
+    }
+
+    const IHeadConfig& AbstractBoostingRuleLearner::Config::getHeadConfig() const {
+        return *headConfigPtr_;
+    }
+
+    const IStatisticsConfig& AbstractBoostingRuleLearner::Config::getStatisticsConfig() const {
+        return *statisticsConfigPtr_;
+    }
+
+    const IRegularizationConfig& AbstractBoostingRuleLearner::Config::getL1RegularizationConfig() const {
+        return *l1RegularizationConfigPtr_;
+    }
+
+    const IRegularizationConfig& AbstractBoostingRuleLearner::Config::getL2RegularizationConfig() const {
+        return *l2RegularizationConfigPtr_;
+    }
+
+    const ILossConfig& AbstractBoostingRuleLearner::Config::getLossConfig() const {
+        return *lossConfigPtr_;
+    }
+
+    const ILabelBinningConfig& AbstractBoostingRuleLearner::Config::getLabelBinningConfig() const {
+        return *labelBinningConfigPtr_;
+    }
+
+    const IClassificationPredictorConfig& AbstractBoostingRuleLearner::Config::getClassificationPredictorConfig() const {
+        return *classificationPredictorConfigPtr_;
+    }
+
+    const IRegressionPredictorConfig& AbstractBoostingRuleLearner::Config::getRegressionPredictorConfig() const {
+        return *regressionPredictorConfigPtr_;
+    }
+
+    const IProbabilityPredictorConfig& AbstractBoostingRuleLearner::Config::getProbabilityPredictorConfig() const {
+        return *probabilityPredictorConfigPtr_;
+    }
+
+    void AbstractBoostingRuleLearner::Config::useSingleLabelHeads() {
+        headConfigPtr_ = std::make_unique<SingleLabelHeadConfig>(
+            labelBinningConfigPtr_, parallelStatisticUpdateConfigPtr_, l1RegularizationConfigPtr_,
+            l2RegularizationConfigPtr_);
+    }
+
+    void AbstractBoostingRuleLearner::Config::useDenseStatistics() {
+        statisticsConfigPtr_ = std::make_unique<DenseStatisticsConfig>(lossConfigPtr_);
+    }
+
+    void AbstractBoostingRuleLearner::Config::useNoL1Regularization() {
+        l1RegularizationConfigPtr_ = std::make_unique<NoRegularizationConfig>();
+    }
+
+    void AbstractBoostingRuleLearner::Config::useNoL2Regularization() {
+        l2RegularizationConfigPtr_ = std::make_unique<NoRegularizationConfig>();
+    }
+
+    void AbstractBoostingRuleLearner::Config::useLabelWiseLogisticLoss() {
+        lossConfigPtr_ = std::make_unique<LabelWiseLogisticLossConfig>(headConfigPtr_);
+    }
+
+    void AbstractBoostingRuleLearner::Config::useNoLabelBinning() {
+        labelBinningConfigPtr_ =
+            std::make_unique<NoLabelBinningConfig>(l1RegularizationConfigPtr_, l2RegularizationConfigPtr_);
+    }
+
+    void AbstractBoostingRuleLearner::Config::useLabelWiseClassificationPredictor() {
+        classificationPredictorConfigPtr_ =
+            std::make_unique<LabelWiseClassificationPredictorConfig>(lossConfigPtr_, parallelPredictionConfigPtr_);
+    }
+
+    void AbstractBoostingRuleLearner::Config::useLabelWiseRegressionPredictor() {
+        regressionPredictorConfigPtr_ =
+            std::make_unique<LabelWiseRegressionPredictorConfig>(parallelPredictionConfigPtr_);
+    }
+
+    void AbstractBoostingRuleLearner::Config::useLabelWiseProbabilityPredictor() {
+        probabilityPredictorConfigPtr_ =
+            std::make_unique<LabelWiseProbabilityPredictorConfig>(lossConfigPtr_, parallelPredictionConfigPtr_);
+    }
+
+    AbstractBoostingRuleLearner::AbstractBoostingRuleLearner(std::unique_ptr<IBoostingRuleLearner::IConfig> configPtr,
+                                                             Blas::DdotFunction ddotFunction,
+                                                             Blas::DspmvFunction dspmvFunction,
+                                                             Lapack::DsysvFunction dsysvFunction)
+        : AbstractRuleLearner(*configPtr), configPtr_(std::move(configPtr)), blas_(Blas(ddotFunction, dspmvFunction)),
+          lapack_(Lapack(dsysvFunction)) {
+
+    }
+
+    std::unique_ptr<IStatisticsProviderFactory> AbstractBoostingRuleLearner::createStatisticsProviderFactory(
+            const IFeatureMatrix& featureMatrix, const IRowWiseLabelMatrix& labelMatrix) const {
+        return configPtr_->getStatisticsConfig()
+            .createStatisticsProviderFactory(featureMatrix, labelMatrix, blas_, lapack_);
+    }
+
+    std::unique_ptr<IModelBuilderFactory> AbstractBoostingRuleLearner::createModelBuilderFactory() const {
+        return std::make_unique<RuleListBuilderFactory>();
+    }
+
+    std::unique_ptr<IClassificationPredictorFactory> AbstractBoostingRuleLearner::createClassificationPredictorFactory(
+            const IFeatureMatrix& featureMatrix, uint32 numLabels) const {
+        return configPtr_->getClassificationPredictorConfig()
+            .createClassificationPredictorFactory(featureMatrix, numLabels);
+    }
+
+    std::unique_ptr<IRegressionPredictorFactory> AbstractBoostingRuleLearner::createRegressionPredictorFactory(
+            const IFeatureMatrix& featureMatrix, uint32 numLabels) const {
+        return configPtr_->getRegressionPredictorConfig().createRegressionPredictorFactory(featureMatrix, numLabels);
+    }
+
+    std::unique_ptr<IProbabilityPredictorFactory> AbstractBoostingRuleLearner::createProbabilityPredictorFactory(
+            const IFeatureMatrix& featureMatrix, uint32 numLabels) const {
+        return configPtr_->getProbabilityPredictorConfig().createProbabilityPredictorFactory(featureMatrix, numLabels);
+    }
+
+    std::unique_ptr<ILabelSpaceInfo> AbstractBoostingRuleLearner::createLabelSpaceInfo(
+            const IRowWiseLabelMatrix& labelMatrix) const {
+        if (configPtr_->getClassificationPredictorConfig().isLabelVectorSetNeeded()
+            || configPtr_->getProbabilityPredictorConfig().isLabelVectorSetNeeded()
+            || configPtr_->getRegressionPredictorConfig().isLabelVectorSetNeeded()) {
+            return createLabelVectorSet(labelMatrix);
+        } else {
+            return createNoLabelSpaceInfo();
+        }
+    }
+
+}
