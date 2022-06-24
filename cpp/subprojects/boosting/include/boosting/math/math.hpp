434a0a4193f3ab72481393828dc110c7bbf8a57a
Michael Rapp <michael.rapp90@googlemail.com>
2022-06-24 16:02:42 +0200

    Move utility function "logisticFunction" from file math.hpp to loss_label_wise_logistic.cpp.

--- cpp/subprojects/boosting/include/boosting/math/math.hpp
+++ cpp/subprojects/boosting/include/boosting/math/math.hpp
@@ -20,26 +20,6 @@ namespace boosting {
     }
 
     /**
-     * Calculates and returns the logistic function `1 / (1 + exp(-x))`, given a specific value `x`.
-     *
-     * This implementation exploits the identity `1 / (1 + exp(-x)) = exp(x) / (1 + exp(x))` to increase numerical
-     * stability (see, e.g., section "Numerically stable sigmoid function" in
-     * https://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/).
-     *
-     * @param x The value `x`
-     * @return  The value that has been calculated
-     */
-    static inline constexpr float64 logisticFunction(float64 x) {
-        if (x >= 0) {
-            float64 exponential = std::exp(-x);  // Evaluates to 0 for large x, resulting in 1 ultimately
-            return 1 / (1 + exponential);
-        } else {
-            float64 exponential = std::exp(x);  // Evaluates to 0 for large x, resulting in 0 ultimately
-            return exponential / (1 + exponential);
-        }
-    }
-
-    /**
      * Computes and returns the L1 norm of a specific vector, i.e., the sum of the absolute values of its elements.
      *
      * @tparam Iterator The type of the iterator that provides access to the elements in the vector
