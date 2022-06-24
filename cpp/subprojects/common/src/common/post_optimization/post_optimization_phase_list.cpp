cf5a9811c2e6b4601f7da2a8e62ae64a39ae8418
Michael Rapp <michael.rapp90@googlemail.com>
2022-06-24 18:02:01 +0200

    Add PostOptimizationPhaseListFactory.

---
+++ cpp/subprojects/common/src/common/post_optimization/post_optimization_phase_list.cpp
@@ -0,0 +1,56 @@
+#include "common/post_optimization/post_optimization_phase_list.hpp"
+
+
+/**
+ * An implementation of the class `IPostOptimization` that does not perform any optimizations, but retains a previously
+ * learned rule-based model.
+ */
+class NoPostOptimization final : public IPostOptimization {
+
+    private:
+
+        std::unique_ptr<IModelBuilder> modelBuilderPtr_;
+
+    public:
+
+        /**
+         * @param modelBuilderPtr An unique pointer to an object of type `IModelBuilder` that should be used to build
+         *                        the model
+         */
+        NoPostOptimization(std::unique_ptr<IModelBuilder> modelBuilderPtr)
+            : modelBuilderPtr_(std::move(modelBuilderPtr)) {
+
+        }
+
+        void setDefaultRule(std::unique_ptr<AbstractEvaluatedPrediction>& predictionPtr) override {
+            modelBuilderPtr_->setDefaultRule(predictionPtr);
+        }
+
+        void addRule(std::unique_ptr<ConditionList>& conditionListPtr,
+                     std::unique_ptr<AbstractEvaluatedPrediction>& predictionPtr) override {
+            modelBuilderPtr_->addRule(conditionListPtr, predictionPtr);
+        }
+
+        void optimizeModel(IThresholds& thresholds, const IRuleInduction& ruleInduction, IPartition& partition,
+                           IInstanceSampling& instanceSampling, IFeatureSampling& featureSampling,
+                           const IPruning& pruning, const IPostProcessor& postProcessor, RNG& rng) override {
+            return;
+        }
+
+        std::unique_ptr<IRuleModel> buildModel(uint32 numUsedRules) override {
+            return modelBuilderPtr_->buildModel(numUsedRules);
+        }
+
+};
+
+void PostOptimizationPhaseListFactory::addPostOptimizationPhaseFactory(
+        std::unique_ptr<IPostOptimizationPhaseFactory> postOptimizationPhaseFactoryPtr) {
+    postOptimizationPhaseFactories_.push_back(std::move(postOptimizationPhaseFactoryPtr));
+}
+
+std::unique_ptr<IPostOptimization> PostOptimizationPhaseListFactory::create(
+        const IModelBuilderFactory& modelBuilderFactory) const {
+    // TODO check if empty
+    std::unique_ptr<IModelBuilder> modelBuilderPtr = modelBuilderFactory.create();
+    return std::make_unique<NoPostOptimization>(std::move(modelBuilderPtr));
+}
