ba1559f20aac4ffc873091373f4c4630a49e5b90
Michael Rapp <michael.rapp90@googlemail.com>
2022-07-01 16:12:22 +0200

    Add class LilMatrix.

---
+++ cpp/subprojects/common/include/common/data/matrix_lil.hpp
@@ -0,0 +1,110 @@
+/*
+ * @author Michael Rapp (michael.rapp.ml@gmail.com)
+ */
+#pragma once
+
+#include "common/data/indexed_value.hpp"
+#include <vector>
+
+
+/**
+ * A two-dimensional matrix that provides row-wise access to data that is stored in the list of lists (LIL) format.
+ *
+ * @tparam T The type of the data that is stored by the matrix
+ */
+template<typename T>
+class LilMatrix final {
+
+    public:
+
+        /**
+         * The type of a row in the matrix.
+         */
+        typedef std::vector<IndexedValue<T>> Row;
+
+    private:
+
+        uint32 numRows_;
+
+        Row* array_;
+
+    public:
+
+        /**
+         * @param numRows The number of rows in the matrix
+         */
+        LilMatrix(uint32 numRows);
+
+        ~LilMatrix();
+
+        /**
+         * An iterator that provides access to the elements at a row and allows to modify them.
+         */
+        typedef typename Row::iterator iterator;
+
+        /**
+         * An iterator that provides read-only access to the elements at a row.
+         */
+        typedef typename Row::const_iterator const_iterator;
+
+        /**
+         * Returns an `iterator` to the beginning of a specific row.
+         *
+         * @param row   The row
+         * @return      An `iterator` to the beginning
+         */
+        iterator row_begin(uint32 row);
+
+        /**
+         * Returns an `iterator` to the end of a specific row.
+         *
+         * @param row   The row
+         * @return      An `iterator` to the end
+         */
+        iterator row_end(uint32 row);
+
+        /**
+         * Returns a `const_iterator` to the beginning of a specific row.
+         *
+         * @param row   The row
+         * @return      A `const_iterator` to the beginning
+         */
+        const_iterator row_cbegin(uint32 row) const;
+
+        /**
+         * Returns a `const_iterator` to the end of a specific row.
+         *
+         * @param row   The row
+         * @return      A `const_iterator` to the end
+         */
+        const_iterator row_cend(uint32 row) const;
+
+        /**
+         * Returns a reference to a specific row.
+         *
+         * @param row   The index of the row to be returned
+         * @return      A reference to the row
+         */
+        Row& getRow(uint32 row);
+
+        /**
+         * Returns a const reference to a specific row.
+         *
+         * @param row   The index of the row to be returned
+         * @return      A const reference to the row
+         */
+        const Row& getRow(uint32 row) const;
+
+        /**
+         * Returns the number of rows in the matrix.
+         *
+         * @return The number of rows
+         */
+        uint32 getNumRows() const;
+
+        /**
+         * Sets the values of all elements to zero.
+         */
+        void clear();
+
+};
