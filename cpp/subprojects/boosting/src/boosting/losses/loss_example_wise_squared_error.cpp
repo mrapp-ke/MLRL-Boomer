8ef462df6165706ea1b26eaebb13fa5e22ba03a3
Michael Rapp <michael.rapp90@googlemail.com>
2022-07-28 00:21:15 +0200

    Implement "updateExampleWiseStatistics" functions of the class ExampleWiseSquaredErrorLoss.

--- cpp/subprojects/boosting/src/boosting/losses/loss_example_wise_squared_error.cpp
+++ cpp/subprojects/boosting/src/boosting/losses/loss_example_wise_squared_error.cpp
@@ -1,8 +1,71 @@
 #include "boosting/losses/loss_example_wise_squared_error.hpp"
+#include "common/iterator/binary_forward_iterator.hpp"
 #include "common/math/math.hpp"
 
 
 namespace boosting {
+
+    template<typename LabelIterator>
+    static inline void updateExampleWiseStatisticsInternally(
+            CContiguousConstView<float64>::value_const_iterator scoreIterator, LabelIterator labelIterator,
+            DenseExampleWiseStatisticView::gradient_iterator gradientIterator,
+            DenseExampleWiseStatisticView::hessian_iterator hessianIterator, uint32 numLabels) {
+        LabelIterator labelIterator2 = labelIterator;
+        LabelIterator labelIterator3 = labelIterator;
+
+        // For each label `i`, calculate `x_i = predictedScore_i^2 + (-2 * expectedScore_i * predictedScore_i) + 1` and
+        // sum up those values. The sum is used as a denominator when calculating the gradients and Hessians
+        // afterwards...
+        float64 denominator = 0;
+
+        for (uint32 i = 0; i < numLabels; i++) {
+            float64 predictedScore = scoreIterator[i];
+            bool trueLabel = *labelIterator;
+            float64 expectedScore = trueLabel ? 1 : -1;
+            float64 x = (predictedScore * predictedScore) + (-2 * expectedScore * predictedScore) + 1;
+            gradientIterator[i] = x;  // Temporarily store `x` in the array of gradients
+            denominator += x;
+            labelIterator++;
+        }
+
+        // The denominator that is used for the calculation of gradients is `sqrt(x_1 + x_2 + ...)`...
+        float64 denominatorGradient = std::sqrt(denominator);
+
+        // The denominator that is used for the calculation of Hessians is `(x_1 + x_2 + ...)^1.5`...
+        float64 denominatorHessian = std::pow(denominator, 1.5);
+
+        // Calculate the gradients and Hessians...
+        for (uint32 i = 0; i < numLabels; i++) {
+            float64 predictedScore = scoreIterator[i];
+            bool trueLabel = *labelIterator2;
+            float64 expectedScore = trueLabel ? 1 : -1;
+            float64 x = gradientIterator[i];
+
+            // Calculate the Hessians that belong to the part of the Hessian matrix' upper triangle that corresponds to
+            // the current label. Such a hessian calculates as
+            // `-(predictedScore_i - expectedScore_i) * (predictedScore_j - expectedScore_j) / (x_1 + x_2 + ...)^1.5`
+            LabelIterator labelIterator4 = labelIterator3;
+
+            for (uint32 j = 0; j < i; j++) {
+                float64 predictedScore2 = scoreIterator[j];
+                bool trueLabel2 = *labelIterator4;
+                float64 expectedScore2 = trueLabel2 ? 1 : -1;
+                *hessianIterator = divideOrZero<float64>(-(predictedScore - expectedScore)
+                                                         * (predictedScore2 - expectedScore2), denominatorHessian);
+                hessianIterator++;
+                labelIterator4++;
+            }
+
+            // Calculate the gradient as `(predictedScore_i - expectedScore_i) / sqrt(x_1 + x_2 + ...)`...
+            gradientIterator[i] = divideOrZero<float64>(predictedScore - expectedScore, denominatorGradient);
+
+            // Calculate the Hessian on the diagonal of the Hessian matrix as
+            // `(x_1 + ... + x_i-1 + x_i+1 + ...) / (x_1 + x_2 + ...)^1.5`...
+            *hessianIterator = divideOrZero<float64>(denominator - x, denominatorHessian);
+            hessianIterator++;
+            labelIterator2++;
+        }
+    }
 
     /**
      * An implementation of the type `IExampleWiseLoss` that implements a multi-label variant of the squared error loss
@@ -49,13 +112,22 @@ namespace boosting {
             void updateExampleWiseStatistics(uint32 exampleIndex, const CContiguousConstView<const uint8>& labelMatrix,
                                              const CContiguousConstView<float64>& scoreMatrix,
                                              DenseExampleWiseStatisticView& statisticView) const override {
-                // TODO Implement
+                updateExampleWiseStatisticsInternally(scoreMatrix.row_values_cbegin(exampleIndex),
+                                                      labelMatrix.row_values_cbegin(exampleIndex),
+                                                      statisticView.gradients_row_begin(exampleIndex),
+                                                      statisticView.hessians_row_begin(exampleIndex),
+                                                      labelMatrix.getNumCols());
             }
 
             void updateExampleWiseStatistics(uint32 exampleIndex, const BinaryCsrConstView& labelMatrix,
                                              const CContiguousConstView<float64>& scoreMatrix,
                                              DenseExampleWiseStatisticView& statisticView) const override {
-                // TODO Implement
+                auto labelIterator = make_binary_forward_iterator(labelMatrix.row_indices_cbegin(exampleIndex),
+                                                                  labelMatrix.row_indices_cend(exampleIndex));
+                updateExampleWiseStatisticsInternally(scoreMatrix.row_values_cbegin(exampleIndex), labelIterator,
+                                                      statisticView.gradients_row_begin(exampleIndex),
+                                                      statisticView.hessians_row_begin(exampleIndex),
+                                                      labelMatrix.getNumCols());
             }
 
             /**
