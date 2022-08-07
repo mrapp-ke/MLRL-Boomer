b1af17babba03491d4ba38d8e84e5c2696cf5646
Michael Rapp <mrapp@ke.tu-darmstadt.de>
2022-08-07 14:49:16 +0200

    Include executed command in assertion errors.

--- python/subprojects/testbed/tests/test_common.py
+++ python/subprojects/testbed/tests/test_common.py
@@ -480,7 +480,7 @@ class IntegrationTests(ABC, TestCase):
         else:
             return name + '_overall.' + suffix
 
-    def __assert_file_exists(self, directory: str, file_name: str):
+    def __assert_file_exists(self, directory: str, file_name: str, args: List[str]):
         """
         Asserts that a specific file exists.
 
@@ -488,7 +488,9 @@ class IntegrationTests(ABC, TestCase):
         :param file_name:   The name of the file
         """
         file = path.join(directory, file_name)
-        self.assertTrue(path.isfile(file), 'File ' + str(file) + ' does not exist')
+        self.assertTrue(path.isfile(file),
+                        'Command "' + self.__format_cmd(args) + '" is expected to create file ' + str(
+                            file) + ', but it does not exist')
 
     def __assert_files_exist(self, builder: CmdBuilder, directory: str, file_name: str, suffix: str):
         """
@@ -500,16 +502,18 @@ class IntegrationTests(ABC, TestCase):
         :param builder:     The builder
         """
         if directory is not None:
+            args = builder.build()
+
             if builder.num_folds > 0:
                 current_fold = builder.current_fold
 
                 if current_fold > 0:
-                    self.__assert_file_exists(directory, self.__get_file_name(file_name, suffix, current_fold))
+                    self.__assert_file_exists(directory, self.__get_file_name(file_name, suffix, current_fold), args)
                 else:
                     for i in range(builder.num_folds):
-                        self.__assert_file_exists(directory, self.__get_file_name(file_name, suffix, i + 1))
+                        self.__assert_file_exists(directory, self.__get_file_name(file_name, suffix, i + 1), args)
             else:
-                self.__assert_file_exists(directory, self.__get_file_name(file_name, suffix))
+                self.__assert_file_exists(directory, self.__get_file_name(file_name, suffix), args)
 
     def __assert_model_files_exist(self, builder: CmdBuilder):
         """
@@ -667,7 +671,9 @@ class IntegrationTests(ABC, TestCase):
                     line = line.strip('\n')
 
                     if not line.startswith('INFO Configuration:') and not line.endswith('seconds'):
-                        self.assertEqual(stdout[i], line, 'Output differs at line ' + str(i + 1))
+                        self.assertEqual(stdout[i], line,
+                                         'Output of command "' + self.__format_cmd(args) + '" differs at line ' + str(
+                                             i + 1))
 
         self.__assert_model_files_exist(builder)
         self.__assert_evaluation_files_exist(builder)
