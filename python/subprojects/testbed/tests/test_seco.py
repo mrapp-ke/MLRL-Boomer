6c6410ad8241b3dd34a92c334e1e2d66dbbfc66d
Michael Rapp <michael.rapp90@googlemail.com>
2022-09-18 14:43:31 +0200

    Add functions "heuristic" and "pruning_heuristic" to class SeCoCmdBuilder.

--- python/subprojects/testbed/tests/test_seco.py
+++ python/subprojects/testbed/tests/test_seco.py
@@ -8,6 +8,20 @@ from test_common import CommonIntegrationTests, CmdBuilder, DIR_OUT, DIR_DATA, D
 
 CMD_SECO = 'seco'
 
+HEURISTIC_ACCURACY = 'accuracy'
+
+HEURISTIC_PRECISION = 'precision'
+
+HEURISTIC_LAPLACE = 'laplace'
+
+HEURISTIC_RECALL = 'recall'
+
+HEURISTIC_WRA = 'weighted-relative-accuracy'
+
+HEURISTIC_F_MEASURE = 'f-measure'
+
+HEURISTIC_M_ESTIMATE = 'm-estimate'
+
 
 class SeCoCmdBuilder(CmdBuilder):
     """
@@ -16,6 +30,28 @@ class SeCoCmdBuilder(CmdBuilder):
 
     def __init__(self, data_dir: str = DIR_DATA, dataset: str = DATASET_EMOTIONS):
         super(SeCoCmdBuilder, self).__init__(cmd=CMD_SECO, data_dir=data_dir, dataset=dataset)
+
+    def heuristic(self, heuristic: str = HEURISTIC_F_MEASURE):
+        """
+        Configures the algorithm to use a specific heuristic for learning rules.
+
+        :param heuristic:   The name of the heuristic that should be used for learning rules
+        :return:            The builder itself
+        """
+        self.args.append('--heuristic')
+        self.args.append(heuristic)
+        return self
+
+    def pruning_heuristic(self, heuristic: str = HEURISTIC_ACCURACY):
+        """
+        Configures the algorithm to use a specific heuristic for pruning rules.
+
+        :param heuristic:   The name of the heuristic that should be used for pruning rules
+        :return:            The builder itself
+        """
+        self.args.append('--pruning-heuristic')
+        self.args.append(heuristic)
+        return self
 
 
 class SeCoIntegrationTests(CommonIntegrationTests):
