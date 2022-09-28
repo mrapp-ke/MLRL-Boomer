0bb1d2a976b636d068cab7fa750d49ac922c8534
Michael Rapp <michael.rapp90@googlemail.com>
2022-09-28 18:39:28 +0200

    Use the class Formattable to format data characteristics.

--- python/subprojects/testbed/mlrl/testbed/characteristics.py
+++ python/subprojects/testbed/mlrl/testbed/characteristics.py
@@ -4,7 +4,21 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 Provides functions to determine certain characteristics of feature or label matrices.
 """
 import numpy as np
+from mlrl.testbed.format import Formattable
 from scipy.sparse import issparse
+from typing import List
+
+ARGUMENT_LABELS = 'labels'
+
+ARGUMENT_LABEL_DENSITY = 'label_density'
+
+ARGUMENT_LABEL_SPARSITY = 'label_sparsity'
+
+ARGUMENT_LABEL_IMBALANCE_RATIO = 'label_imbalance_ratio'
+
+ARGUMENT_LABEL_CARDINALITY = 'label_cardinality'
+
+ARGUMENT_DISTINCT_LABEL_VECTORS = 'distinct_label_vectors'
 
 
 def density(m) -> float:
@@ -97,3 +111,29 @@ class LabelCharacteristics:
     @property
     def label_sparsity(self):
         return 1 - self.label_density
+
+
+class Characteristic(Formattable):
+    """
+    Allows to create textual representations of characteristics.
+    """
+
+    def __init__(self, argument: str, name: str, getter_function, percentage: bool = False):
+        """
+        :param getter_function: The getter function that should be used to retrieve the characteristic
+        """
+        super().__init__(argument, name, percentage)
+        self.getter_function = getter_function
+
+    def format(self, value, **kwargs) -> str:
+        return super().format(self.getter_function(value), **kwargs)
+
+
+LABEL_CHARACTERISTICS: List[Characteristic] = [
+    Characteristic(ARGUMENT_LABELS, 'Labels', lambda x: x.num_labels),
+    Characteristic(ARGUMENT_LABEL_DENSITY, 'Label Density', lambda x: x.label_density, percentage=True),
+    Characteristic(ARGUMENT_LABEL_SPARSITY, 'Label Sparsity', lambda x: x.label_sparsity, percentage=True),
+    Characteristic(ARGUMENT_LABEL_IMBALANCE_RATIO, 'Label Imbalance Ratio', lambda x: x.avg_label_imbalance_ratio),
+    Characteristic(ARGUMENT_LABEL_CARDINALITY, 'Label Cardinality', lambda x: x.avg_label_cardinality),
+    Characteristic(ARGUMENT_DISTINCT_LABEL_VECTORS, 'Distinct Label Vectors', lambda x: x.num_distinct_label_vectors)
+]
