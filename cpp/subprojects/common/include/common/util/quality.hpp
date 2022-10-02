c3b40486584d2c950cd28708ffc4dac06f71b0ab
Michael Rapp <michael.rapp90@googlemail.com>
2022-10-02 17:12:10 +0200

    Add new struct Quality.

---
+++ cpp/subprojects/common/include/common/util/quality.hpp
@@ -0,0 +1,19 @@
+/*
+ * @author Michael Rapp (michael.rapp.ml@gmail.com)
+ */
+#pragma once
+
+#include "common/data/types.hpp"
+
+
+/**
+ * A struct that stores a quality.
+ */
+struct Quality {
+
+    /**
+     * A numerical score that represents the quality.
+     */
+    float64 quality;
+
+};
