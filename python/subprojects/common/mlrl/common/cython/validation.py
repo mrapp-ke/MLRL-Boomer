8b7c92f714ec27eb04a263ee98e05f5a1b293d9d
Michael Rapp <michael.rapp90@googlemail.com>
2022-12-16 00:32:28 +0100

    Edit comments.

--- python/subprojects/common/mlrl/common/cython/validation.py
+++ python/subprojects/common/mlrl/common/cython/validation.py
@@ -5,7 +5,7 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 
 def assert_greater(name: str, value, threshold):
     """
-    Raises an exception if a given value is not greater than a specific threshold.
+    Raises an `AssertionError` if a given value is not greater than a specific threshold.
 
     :param name:        The name of the parameter, the value  corresponds to
     :param value:       The value
@@ -18,7 +18,7 @@ def assert_greater(name: str, value, threshold):
 
 def assert_greater_or_equal(name: str, value, threshold):
     """
-    Raises an exception if a given value is not greater or equal to a specific threshold.
+    Raises an `AssertionError` if a given value is not greater or equal to a specific threshold.
 
     :param name:        The name of the parameter, the value corresponds to
     :param value:       The value
@@ -31,7 +31,7 @@ def assert_greater_or_equal(name: str, value, threshold):
 
 def assert_less(name: str, value, threshold):
     """
-    Raises an exception if a given value is not less than a specific threshold.
+    Raises an `AssertionError` if a given value is not less than a specific threshold.
     
     :param name:        The name of the parameter, the value corresponds to 
     :param value:       The value
@@ -45,7 +45,7 @@ def assert_less(name: str, value, threshold):
 
 def assert_less_or_equal(name: str, value, threshold):
     """
-    Raises an exception if a given value is not less or equal to a specific threshold.
+    Raises an `AssertionError` if a given value is not less or equal to a specific threshold.
 
     :param name:        The name of the parameter, the value corresponds to
     :param value:       The value
@@ -58,7 +58,7 @@ def assert_less_or_equal(name: str, value, threshold):
 
 def assert_multiple(name: str, value, other):
     """
-    Raises an exception if a given value is not a multiple of another value.
+    Raises an `AssertionError` if a given value is not a multiple of another value.
 
     :param name:    The name of the parameter, the value corresponds to
     :param value:   The value that should be a multiple of `other`
@@ -72,7 +72,7 @@ def assert_multiple(name: str, value, other):
 
 def assert_not_none(name: str, value):
     """
-    Raises an exception if a given value is None.
+    Raises an `AssertionError` if a given value is None.
 
     :param name:    The name of the parameter, the value corresponds to
     :param value:   The value
