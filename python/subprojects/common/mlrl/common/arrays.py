8b519219ac1cc81a044bf5abd189a9c1ede048d8
Michael Rapp <michael.rapp90@googlemail.com>
2022-12-17 18:33:46 +0100

    Add function "enforce_2d" to file arrays.py.

--- python/subprojects/common/mlrl/common/arrays.py
+++ python/subprojects/common/mlrl/common/arrays.py
@@ -21,3 +21,16 @@ def enforce_dense(a, order: str, dtype) -> np.ndarray:
         return np.require(a.toarray(order=order), dtype=dtype)
     else:
         return np.require(a, dtype=dtype, requirements=[order])
+
+
+def enforce_2d(a: np.ndarray) -> np.ndarray:
+    """
+    Converts a given `np.ndarray` into a two-dimensional array if it is one-dimensional.
+
+    :param a:   A `np.ndarray` to be converted
+    :return:    A `np.ndarray` with at least two dimensions
+    """
+    if a.ndim == 1:
+        return np.expand_dims(a, axis=1)
+    else:
+        return a
