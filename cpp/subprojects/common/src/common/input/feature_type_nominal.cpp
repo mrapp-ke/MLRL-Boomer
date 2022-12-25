45d08ee0ed51e10013582cf67bcf7314dc396d8d
Michael Rapp <michael.rapp90@googlemail.com>
2022-12-25 15:10:54 +0100

    Add class BinaryFeatureType, NominalFeatureType and NumericalFeatureType.

---
+++ cpp/subprojects/common/src/common/input/feature_type_nominal.cpp
@@ -0,0 +1,6 @@
+#include "common/input/feature_type_nominal.hpp"
+
+
+bool NominalFeatureType::isNumerical() const {
+    return false;
+}
---
+++ cpp/subprojects/common/src/common/input/feature_type_numerical.cpp
@@ -0,0 +1,6 @@
+#include "common/input/feature_type_numerical.hpp"
+
+
+bool NumericalFeatureType::isNumerical() const {
+    return true;
+}
