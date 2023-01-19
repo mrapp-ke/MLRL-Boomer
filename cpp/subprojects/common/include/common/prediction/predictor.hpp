289aaf80385f04c992d41eea6691b0e6c2eb0271
Michael Rapp <michael.rapp90@googlemail.com>
2023-01-19 15:56:12 +0100

    Add class IPredictorFactory.

--- cpp/subprojects/common/include/common/prediction/predictor.hpp
+++ cpp/subprojects/common/include/common/prediction/predictor.hpp
@@ -5,6 +5,8 @@
 
 #include "common/data/view_c_contiguous.hpp"
 #include "common/data/view_csr.hpp"
+#include "common/model/rule_list.hpp"
+#include "common/prediction/label_vector_set.hpp"
 #include <memory>
 
 
@@ -40,6 +42,53 @@ class IPredictor {
          *                      predictions
          */
         virtual std::unique_ptr<PredictionMatrix> predict(const CsrConstView<const float32>& featureMatrix) const = 0;
+
+};
+
+/**
+ * Defines an interface for all factories that allow to create instances of the type `IPredictor`.
+ *
+ * @tparam PredictionMatrix The type of the matrix that is used to store the predictions
+ */
+template<typename PredictionMatrix>
+class IPredictorFactory {
+
+    public:
+
+        virtual ~IPredictorFactory() { };
+
+        /**
+         * Creates and returns a new object of the type `IPredictor`.
+         *
+         * @param featureMatrix     A reference to an object of type `CsrConstView` that stores the feature values of
+         *                          the query examples to predict for
+         * @param model             A reference to an object of type `RuleList` that should be used to obtain
+         *                          predictions
+         * @param labelVectorSet    A pointer to an object of type `LabelVectorSet` that stores all known label vectors
+         *                          or a null pointer, if no such set is available
+         * @param numLabels         The number of labels to predict for
+         * @return                  An unique pointer to an object of type `IPredictor` that has been created
+         */
+        virtual std::unique_ptr<IPredictor<PredictionMatrix>> create(
+            const CContiguousConstView<const float32>& featureMatrix, const RuleList& model,
+            const LabelVectorSet* labelVectorSet, uint32 numLabels) const = 0;
+
+        /**
+         * Creates and returns a new object of the type `IPredictor`.
+         *
+         * @param featureMatrix     A reference to an object of type `CsrConstView` that stores the feature values of
+         *                          the query examples to predict for
+         * @param model             A reference to an object of type `RuleList` that should be used to obtain
+         *                          predictions
+         * @param labelVectorSet    A pointer to an object of type `LabelVectorSet` that stores all known label vectors
+         *                          or a null pointer, if no such set is available
+         * @param numLabels         The number of labels to predict for
+         * @return                  An unique pointer to an object of type `IPredictor` that has been created
+         */
+        virtual std::unique_ptr<IPredictor<PredictionMatrix>> create(const CsrConstView<const float32>& featureMatrix,
+                                                                     const RuleList& model,
+                                                                     const LabelVectorSet* labelVectorSet,
+                                                                     uint32 numLabels) const = 0;
 
 };
 
