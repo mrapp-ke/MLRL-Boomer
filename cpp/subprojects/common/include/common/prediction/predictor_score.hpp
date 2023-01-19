91976048acade8b2f52e9bdbd6393cb0188a5144
Michael Rapp <michael.rapp90@googlemail.com>
2023-01-19 16:04:38 +0100

    Add typedef IScorePredictorFactory.

--- cpp/subprojects/common/include/common/prediction/predictor_score.hpp
+++ cpp/subprojects/common/include/common/prediction/predictor_score.hpp
@@ -11,3 +11,8 @@
  * Defines an interface for all classes that allow to predict regression scores for given query examples.
  */
 typedef IPredictor<DensePredictionMatrix<float64>> IScorePredictor;
+
+/**
+ * Defines an interface for all classes that allow to create instances of the type `IScorePredictor`.
+ */
+typedef IPredictorFactory<DensePredictionMatrix<float64>> IScorePredictorFactory;
