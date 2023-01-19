e52cb3c954615f41612ff40a71650f24874539a4
Michael Rapp <michael.rapp90@googlemail.com>
2023-01-19 16:06:31 +0100

    Add typedef IProbabilityPredictorFactory.

--- cpp/subprojects/common/include/common/prediction/predictor_probability.hpp
+++ cpp/subprojects/common/include/common/prediction/predictor_probability.hpp
@@ -11,3 +11,8 @@
  * Defines an interface for all classes that allow to predict probability estimates for given query examples.
  */
 typedef IPredictor<DensePredictionMatrix<float64>> IProbabilityPredictor;
+
+/**
+ * Defines an interface for all classes that allow to create instances of the type `IProbabilityPredictor`.
+ */
+typedef IPredictorFactory<DensePredictionMatrix<uint8>> IProbabilityPredictorFactory;
