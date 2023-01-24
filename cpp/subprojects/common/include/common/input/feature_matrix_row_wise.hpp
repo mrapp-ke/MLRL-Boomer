7bb4988bc6915547b1bde50741b3de9c281bea88
Michael Rapp <michael.rapp90@googlemail.com>
2023-01-24 13:24:04 +0100

    Remove obsolete functions from class IRowWiseFeatureMatrix.

--- cpp/subprojects/common/include/common/input/feature_matrix_row_wise.hpp
+++ cpp/subprojects/common/include/common/input/feature_matrix_row_wise.hpp
@@ -17,11 +17,6 @@ class IScorePredictor;
 class IScorePredictorFactory;
 class IProbabilityPredictor;
 class IProbabilityPredictorFactory;
-template<typename T> class DensePredictionMatrix; // TODO Remove
-class BinarySparsePredictionMatrix; // TODO Remove
-class IClassificationPredictor; // TODO Remove
-class IOldRegressionPredictor; // TODO Remove
-class IOldProbabilityPredictor; // TODO Remove
 
 
 /**
@@ -51,19 +46,6 @@ class MLRLCOMMON_API IRowWiseFeatureMatrix : virtual public IFeatureMatrix {
                                                                       uint32 numLabels) const = 0;
 
         /**
-         * Obtains and returns dense predictions for all examples in this feature matrix, using a specific
-         * `IClassificationPredictor`, depending on the type of this feature matrix.
-         *
-         * @param predictor A reference to an object of type `IClassificationPredictor` that should be used to obtain
-         *                  predictions
-         * @param numLabels The number of labels to predict for
-         * @return          An unique pointer to an object of type `DensePredictionMatrix` that stores the predictions
-         */
-        // TODO Remove
-        virtual std::unique_ptr<DensePredictionMatrix<uint8>> predictLabels(
-            const IClassificationPredictor& predictor, uint32 numLabels) const = 0;
-
-        /**
          * Creates and returns a new instance of the class `ISparseLabelPredictor`, based on the type of this feature
          * matrix.
          *
@@ -79,20 +61,6 @@ class MLRLCOMMON_API IRowWiseFeatureMatrix : virtual public IFeatureMatrix {
         virtual std::unique_ptr<ISparseLabelPredictor> createSparseLabelPredictor(
             const ISparseLabelPredictorFactory& factory, const IRuleModel& ruleModel,
             const ILabelSpaceInfo& labelSpaceInfo, uint32 numLabels) const = 0;
-
-        /**
-         * Obtains and returns sparse predictions for all examples in this feature matrix, using a specific
-         * `IClassificationPredictor`, depending on the type of this feature matrix.
-         *
-         * @param predictor A reference to an object of type `IClassificationPredictor` that should be used to obtain
-         *                  predictions
-         * @param numLabels The number of labels to predict for
-         * @return          An unique pointer to an object of type `BinarySparsePredictionMatrix` that stores the
-         *                  predictions
-         */
-        // TODO Remove
-        virtual std::unique_ptr<BinarySparsePredictionMatrix> predictSparseLabels(
-            const IClassificationPredictor& predictor, uint32 numLabels) const = 0;
 
         /**
          * Creates and returns a new instance of the class `IScorePredictor`, based on the type of this feature matrix.
@@ -112,19 +80,6 @@ class MLRLCOMMON_API IRowWiseFeatureMatrix : virtual public IFeatureMatrix {
                                                                       uint32 numLabels) const = 0;
 
         /**
-         * Obtains and returns regression scores for all examples in this feature matrix, using a specific
-         * `IRegressionPredictor`, depending on the type of this feature matrix.
-         *
-         * @param predictor A reference to an object of type `IRegressionPredictor` that should be used to obtain
-         *                  predictions
-         * @param numLabels The number of labels to predict for
-         * @return          An unique pointer to an object of type `DensePredictionMatrix` that stores the predictions
-         */
-        // TODO Remove
-        virtual std::unique_ptr<DensePredictionMatrix<float64>> predictScores(
-            const IOldRegressionPredictor& predictor, uint32 numLabels) const = 0;
-
-        /**
          * Creates and returns a new instance of the class `IProbabilityPredictor`, based on the type of this feature
          * matrix.
          *
@@ -140,18 +95,5 @@ class MLRLCOMMON_API IRowWiseFeatureMatrix : virtual public IFeatureMatrix {
         virtual std::unique_ptr<IProbabilityPredictor> createProbabilityPredictor(
             const IProbabilityPredictorFactory& factory, const IRuleModel& ruleModel,
             const ILabelSpaceInfo& labelSpaceInfo, uint32 numLabels) const = 0;
-
-        /**
-         * Obtains and returns probability estimates for all examples in this feature matrix, using a specific
-         * `IProbabilityPredictor`, depending on the type of this feature matrix.
-         *
-         * @param predictor A reference to an object of type `IProbabilityPredictor` that should be used to obtain
-         *                  predictions
-         * @param numLabels The number of labels to predict for
-         * @return          An unique pointer to an object of type `DensePredictionMatrix` that stores the predictions
-         */
-        // TODO Remove
-        virtual std::unique_ptr<DensePredictionMatrix<float64>> predictProbabilities(
-            const IOldProbabilityPredictor& predictor, uint32 numLabels) const = 0;
 
 };
