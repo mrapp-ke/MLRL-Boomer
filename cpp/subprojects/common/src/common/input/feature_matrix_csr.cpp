6c0f0bce3ce1c4e9817867e6eba6f51b8d76925e
Michael Rapp <michael.rapp90@googlemail.com>
2023-01-24 13:25:15 +0100

    Add missing function implementations.

--- cpp/subprojects/common/src/common/input/feature_matrix_csr.cpp
+++ cpp/subprojects/common/src/common/input/feature_matrix_csr.cpp
@@ -18,30 +18,26 @@ std::unique_ptr<ILabelPredictor> CsrFeatureMatrix::createLabelPredictor(const IL
                                                                         const IRuleModel& ruleModel,
                                                                         const ILabelSpaceInfo& labelSpaceInfo,
                                                                         uint32 numLabels) const {
-    // TODO
-    return nullptr;
+    return ruleModel.createLabelPredictor(factory, *this, labelSpaceInfo, numLabels);
 }
 
 std::unique_ptr<ISparseLabelPredictor> CsrFeatureMatrix::createSparseLabelPredictor(
         const ISparseLabelPredictorFactory& factory, const IRuleModel& ruleModel, const ILabelSpaceInfo& labelSpaceInfo,
         uint32 numLabels) const {
-    // TODO
-    return nullptr;
+    return ruleModel.createSparseLabelPredictor(factory, *this, labelSpaceInfo, numLabels);
 }
 
 std::unique_ptr<IScorePredictor> CsrFeatureMatrix::createScorePredictor(const IScorePredictorFactory& factory,
                                                                         const IRuleModel& ruleModel,
                                                                         const ILabelSpaceInfo& labelSpaceInfo,
                                                                         uint32 numLabels) const {
-    // TODO
-    return nullptr;
+    return ruleModel.createScorePredictor(factory, *this, labelSpaceInfo, numLabels);
 }
 
 std::unique_ptr<IProbabilityPredictor> CsrFeatureMatrix::createProbabilityPredictor(
         const IProbabilityPredictorFactory& factory, const IRuleModel& ruleModel, const ILabelSpaceInfo& labelSpaceInfo,
         uint32 numLabels) const {
-    // TODO
-    return nullptr;
+    return ruleModel.createProbabilityPredictor(factory, *this, labelSpaceInfo, numLabels);
 }
 
 std::unique_ptr<ICsrFeatureMatrix> createCsrFeatureMatrix(uint32 numRows, uint32 numCols, const float32* data,
