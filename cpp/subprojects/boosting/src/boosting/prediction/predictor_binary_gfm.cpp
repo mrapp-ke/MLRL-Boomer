6e181cb3d887a1f69b3a85eda41359816a6c5c41
Michael Rapp <michael.rapp90@googlemail.com>
2023-02-24 14:37:52 +0100

    The class GfmBinaryPredictor now uses a BinarySparsePredictionDispatcher.

--- cpp/subprojects/boosting/src/boosting/prediction/predictor_binary_gfm.cpp
+++ cpp/subprojects/boosting/src/boosting/prediction/predictor_binary_gfm.cpp
@@ -394,29 +394,47 @@ namespace boosting {
      * @tparam Model            The type of the rule-based model that is used to obtain predictions
      */
     template<typename FeatureMatrix, typename Model>
-    class GfmSparseBinaryPredictor final : AbstractBinarySparsePredictor<FeatureMatrix, Model>,
-                                           virtual public ISparseBinaryPredictor {
+    class GfmSparseBinaryPredictor final : public ISparseBinaryPredictor {
         private:
+
+            typedef BinarySparsePredictionDispatcher<FeatureMatrix, Model> Dispatcher;
+
+            class Delegate final : public Dispatcher::IPredictionDelegate {
+                private:
+
+                    const LabelVectorSet& labelVectorSet_;
+
+                    const IProbabilityFunction& probabilityFunction_;
+
+                    uint32 maxLabelCardinality_;
+
+                public:
+
+                    Delegate(const LabelVectorSet& labelVectorSet, const IProbabilityFunction& probabilityFunction,
+                             uint32 maxLabelCardinality)
+                        : labelVectorSet_(labelVectorSet), probabilityFunction_(probabilityFunction),
+                          maxLabelCardinality_(maxLabelCardinality) {}
+
+                    void predictForExample(const Model& model, const FeatureMatrix& featureMatrix,
+                                           BinaryLilMatrix::row predictionRow, uint32 numLabels, uint32 maxRules,
+                                           uint32 exampleIndex) const override {
+                        predictForExampleInternally(model, featureMatrix, predictionRow, numLabels, maxRules,
+                                                    exampleIndex, labelVectorSet_, probabilityFunction_,
+                                                    maxLabelCardinality_);
+                    }
+            };
+
+            const FeatureMatrix& featureMatrix_;
+
+            const Model& model_;
+
+            uint32 numLabels_;
+
+            uint32 numThreads_;
 
             const LabelVectorSet& labelVectorSet_;
 
             std::unique_ptr<IProbabilityFunction> probabilityFunctionPtr_;
-
-            uint32 maxLabelCardinality_;
-
-        protected:
-
-            /**
-             * @see `AbstractBinarySparsePredictor::predictForExample`
-             */
-            void predictForExample(const Model& model, const FeatureMatrix& featureMatrix,
-                                   BinaryLilMatrix::row predictionRow, uint32 numLabels, uint32 maxRules,
-                                   uint32 exampleIndex) const override {
-                if (labelVectorSet_.getNumLabelVectors() > 0) {
-                    predictForExampleInternally(model, featureMatrix, predictionRow, numLabels, maxRules, exampleIndex,
-                                                labelVectorSet_, *probabilityFunctionPtr_, maxLabelCardinality_);
-                }
-            }
 
         public:
 
@@ -436,9 +454,28 @@ namespace boosting {
             GfmSparseBinaryPredictor(const FeatureMatrix& featureMatrix, const Model& model,
                                      const LabelVectorSet& labelVectorSet, uint32 numLabels,
                                      std::unique_ptr<IProbabilityFunction> probabilityFunctionPtr, uint32 numThreads)
-                : AbstractBinarySparsePredictor<FeatureMatrix, Model>(featureMatrix, model, numLabels, numThreads),
-                  labelVectorSet_(labelVectorSet), probabilityFunctionPtr_(std::move(probabilityFunctionPtr)),
-                  maxLabelCardinality_(getMaxLabelCardinality(labelVectorSet)) {}
+                : featureMatrix_(featureMatrix), model_(model), numLabels_(numLabels), numThreads_(numThreads),
+                  labelVectorSet_(labelVectorSet), probabilityFunctionPtr_(std::move(probabilityFunctionPtr)) {}
+
+            /**
+             * @see `IPredictor::predict`
+             */
+            std::unique_ptr<BinarySparsePredictionMatrix> predict(uint32 maxRules) const override {
+                uint32 numExamples = featureMatrix_.getNumRows();
+                BinaryLilMatrix predictionMatrix(numExamples);
+                uint32 numNonZeroElements;
+
+                if (labelVectorSet_.getNumLabelVectors() > 0) {
+                    uint32 maxLabelCardinality = getMaxLabelCardinality(labelVectorSet_);
+                    Delegate delegate(labelVectorSet_, *probabilityFunctionPtr_, maxLabelCardinality);
+                    numNonZeroElements = Dispatcher().predict(delegate, featureMatrix_, model_, predictionMatrix,
+                                                              numLabels_, maxRules, numThreads_);
+                } else {
+                    numNonZeroElements = 0;
+                }
+
+                return createBinarySparsePredictionMatrix(predictionMatrix, numLabels_, numNonZeroElements);
+            }
 
             /**
              * @see `IPredictor::canPredictIncrementally`
