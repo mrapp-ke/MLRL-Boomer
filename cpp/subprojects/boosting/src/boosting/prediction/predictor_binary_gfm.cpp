2d2d436b4911a39b4e85d3d29f895fde87503e5d
Michael Rapp <michael.rapp90@googlemail.com>
2023-02-24 16:13:44 +0100

    Organize includes.

--- cpp/subprojects/boosting/src/boosting/prediction/predictor_binary_gfm.cpp
+++ cpp/subprojects/boosting/src/boosting/prediction/predictor_binary_gfm.cpp
@@ -3,8 +3,8 @@
 #include "common/data/matrix_sparse_set.hpp"
 #include "common/data/vector_sparse_array.hpp"
 #include "common/math/math.hpp"
-#include "predictor_score_common.hpp"
 #include "predictor_probability_common.hpp"
+#include "predictor_score_common.hpp"
 
 #include <algorithm>
 #include <stdexcept>
