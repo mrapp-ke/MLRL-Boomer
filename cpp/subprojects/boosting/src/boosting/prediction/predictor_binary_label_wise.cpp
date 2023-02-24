a07ac5071c786e79340343a0555e95db2e231fca
Michael Rapp <michael.rapp90@googlemail.com>
2023-02-24 16:03:09 +0100

    Rename file predictor_common.hpp to predictor_score_common.hpp.

--- cpp/subprojects/boosting/src/boosting/prediction/predictor_binary_label_wise.cpp
+++ cpp/subprojects/boosting/src/boosting/prediction/predictor_binary_label_wise.cpp
@@ -1,7 +1,7 @@
 #include "boosting/prediction/predictor_binary_label_wise.hpp"
 
 #include "common/iterator/index_iterator.hpp"
-#include "predictor_common.hpp"
+#include "predictor_score_common.hpp"
 
 #include <stdexcept>
 
