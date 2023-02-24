ecd8591bc5a5f38013d10d8f2d4561fec37e965c
Michael Rapp <michael.rapp90@googlemail.com>
2023-02-24 13:09:22 +0100

    The class LabelWiseProbabilityPredictor now uses a PredictionDispatcher.

--- cpp/subprojects/boosting/src/boosting/prediction/predictor_probability_label_wise.cpp
+++ cpp/subprojects/boosting/src/boosting/prediction/predictor_probability_label_wise.cpp
@@ -55,23 +55,38 @@ namespace boosting {
      * @tparam Model            The type of the rule-based model that is used to obtain predictions
      */
     template<typename FeatureMatrix, typename Model>
-    class LabelWiseProbabilityPredictor final : public AbstractPredictor<float64, FeatureMatrix, Model>,
-                                                virtual public IProbabilityPredictor {
+    class LabelWiseProbabilityPredictor final : public IProbabilityPredictor {
         private:
 
+            typedef PredictionDispatcher<float64, FeatureMatrix, Model> Dispatcher;
+
+            class Delegate final : public Dispatcher::IPredictionDelegate {
+                private:
+
+                    const IProbabilityFunction& probabilityFunction_;
+
+                public:
+
+                    Delegate(const IProbabilityFunction& probabilityFunction)
+                        : probabilityFunction_(probabilityFunction) {}
+
+                    void predictForExample(const Model& model, const FeatureMatrix& featureMatrix,
+                                           CContiguousView<float64>& predictionMatrix, uint32 maxRules,
+                                           uint32 exampleIndex) const override {
+                        return predictForExampleInternally(model, featureMatrix, predictionMatrix, maxRules,
+                                                           exampleIndex, probabilityFunction_);
+                    }
+            };
+
+            const FeatureMatrix& featureMatrix_;
+
+            const Model& model_;
+
+            uint32 numLabels_;
+
+            uint32 numThreads_;
+
             std::unique_ptr<IProbabilityFunction> probabilityFunctionPtr_;
-
-        protected:
-
-            /**
-             * @see `AbstractPredictor::predictForExample`
-             */
-            void predictForExample(const Model& model, const FeatureMatrix& featureMatrix,
-                                   DensePredictionMatrix<float64>& predictionMatrix, uint32 maxRules,
-                                   uint32 exampleIndex) const override {
-                predictForExampleInternally(model, featureMatrix, predictionMatrix, maxRules, exampleIndex,
-                                            *probabilityFunctionPtr_);
-            }
 
         public:
 
@@ -89,8 +104,20 @@ namespace boosting {
             LabelWiseProbabilityPredictor(const FeatureMatrix& featureMatrix, const Model& model, uint32 numLabels,
                                           std::unique_ptr<IProbabilityFunction> probabilityFunctionPtr,
                                           uint32 numThreads)
-                : AbstractPredictor<float64, FeatureMatrix, Model>(featureMatrix, model, numLabels, numThreads, true),
+                : featureMatrix_(featureMatrix), model_(model), numLabels_(numLabels), numThreads_(numThreads),
                   probabilityFunctionPtr_(std::move(probabilityFunctionPtr)) {}
+
+            /**
+             * @see `IPredictor::predict`
+             */
+            std::unique_ptr<DensePredictionMatrix<float64>> predict(uint32 maxRules) const override {
+                uint32 numExamples = featureMatrix_.getNumRows();
+                std::unique_ptr<DensePredictionMatrix<float64>> predictionMatrixPtr =
+                  std::make_unique<DensePredictionMatrix<float64>>(numExamples, numLabels_, true);
+                Delegate delegate(*probabilityFunctionPtr_);
+                Dispatcher().predict(delegate, featureMatrix_, model_, *predictionMatrixPtr, maxRules, numThreads_);
+                return predictionMatrixPtr;
+            }
 
             /**
              * @see `IPredictor::canPredictIncrementally`
