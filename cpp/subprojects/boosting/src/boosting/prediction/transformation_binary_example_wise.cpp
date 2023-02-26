b7f624925bb3e62d284f2b79c7d48398d023f3af
Michael Rapp <michael.rapp90@googlemail.com>
2023-02-26 19:01:36 +0100

    Add class ExampleWiseBinaryTransformation.

---
+++ cpp/subprojects/boosting/src/boosting/prediction/transformation_binary_example_wise.cpp
@@ -0,0 +1,78 @@
+#include "boosting/prediction/transformation_binary_example_wise.hpp"
+
+namespace boosting {
+
+    static inline const LabelVector* measureDistance(CContiguousConstView<float64>::value_const_iterator realBegin,
+                                                     CContiguousConstView<float64>::value_const_iterator realEnd,
+                                                     LabelVectorSet::const_iterator labelVectorIterator,
+                                                     const IDistanceMeasure& distanceMeasure, float64& distance,
+                                                     uint32& count) {
+        const auto& entry = *labelVectorIterator;
+        const std::unique_ptr<LabelVector>& labelVectorPtr = entry.first;
+        distance = distanceMeasure.measureDistance(*labelVectorPtr, realBegin, realEnd);
+        count = entry.second;
+        return labelVectorPtr.get();
+    }
+
+    static inline const LabelVector& findClosestLabelVector(
+      CContiguousConstView<float64>::value_const_iterator realBegin,
+      CContiguousConstView<float64>::value_const_iterator realEnd, const LabelVectorSet& labelVectorSet,
+      const IDistanceMeasure& distanceMeasure) {
+        float64 minDistance;
+        uint32 maxCount;
+        LabelVectorSet::const_iterator labelVectorIterator = labelVectorSet.cbegin();
+        const LabelVector* closestLabelVector =
+          measureDistance(realBegin, realEnd, labelVectorIterator, distanceMeasure, minDistance, maxCount);
+        labelVectorIterator++;
+
+        for (; labelVectorIterator != labelVectorSet.cend(); labelVectorIterator++) {
+            float64 distance;
+            uint32 count;
+            const LabelVector* labelVector =
+              measureDistance(realBegin, realEnd, labelVectorIterator, distanceMeasure, distance, count);
+
+            if (distance < minDistance || (distance == minDistance && count > maxCount)) {
+                closestLabelVector = labelVector;
+                minDistance = distance;
+                maxCount = count;
+            }
+        }
+
+        return *closestLabelVector;
+    }
+
+    ExampleWiseBinaryTransformation::ExampleWiseBinaryTransformation(
+      const LabelVectorSet& labelVectorSet, std::unique_ptr<IDistanceMeasure> distanceMeasurePtr)
+        : labelVectorSet_(labelVectorSet), distanceMeasurePtr_(std::move(distanceMeasurePtr)) {}
+
+    void ExampleWiseBinaryTransformation::apply(CContiguousConstView<float64>::value_const_iterator realBegin,
+                                                CContiguousConstView<float64>::value_const_iterator realEnd,
+                                                CContiguousView<uint8>::value_iterator predictionBegin,
+                                                CContiguousView<uint8>::value_iterator predictionEnd) const {
+        const LabelVector& labelVector =
+          findClosestLabelVector(realBegin, realEnd, labelVectorSet_, *distanceMeasurePtr_);
+        uint32 numIndices = labelVector.getNumElements();
+        LabelVector::const_iterator indexIterator = labelVector.cbegin();
+
+        for (uint32 i = 0; i < numIndices; i++) {
+            uint32 labelIndex = indexIterator[i];
+            predictionBegin[labelIndex] = 1;
+        }
+    }
+
+    void ExampleWiseBinaryTransformation::apply(CContiguousConstView<float64>::value_const_iterator realBegin,
+                                                CContiguousConstView<float64>::value_const_iterator realEnd,
+                                                BinaryLilMatrix::row predictionRow) const {
+        const LabelVector& labelVector =
+          findClosestLabelVector(realBegin, realEnd, labelVectorSet_, *distanceMeasurePtr_);
+        uint32 numIndices = labelVector.getNumElements();
+        LabelVector::const_iterator indexIterator = labelVector.cbegin();
+        predictionRow.reserve(numIndices);
+
+        for (uint32 i = 0; i < numIndices; i++) {
+            uint32 labelIndex = indexIterator[i];
+            predictionRow.emplace_back(labelIndex);
+        }
+    }
+
+}
