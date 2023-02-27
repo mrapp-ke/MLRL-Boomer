6b11658c6a30102bc5d05bc588df51781f5e9891
Michael Rapp <michael.rapp90@googlemail.com>
2023-02-27 18:49:40 +0100

    The class MarginalizedProbabilityPredictorFactory does now create objects of type ProbabilityPredictor.

--- cpp/subprojects/boosting/src/boosting/prediction/predictor_probability_marginalized.cpp
+++ cpp/subprojects/boosting/src/boosting/prediction/predictor_probability_marginalized.cpp
@@ -3,101 +3,12 @@
 #include "boosting/prediction/predictor_probability_common.hpp"
 #include "boosting/prediction/transformation_probability_marginalized.hpp"
 
-#include <stdexcept>
-
 namespace boosting {
 
-    /**
-     * An implementation of the type `IProbabilityPredictor` that allows to predict marginalized probabilities for given
-     * for given query examples, which estimate the chance of individual labels to be relevant, by summing up the scores
-     * that are provided by individual rules of an existing rule-based models and comparing the aggregated score vector
-     * to the known label vectors according to a certain distance measure. The probability for an individual label
-     * calculates as the sum of the distances that have been obtained for all label vectors, where the respective label
-     * is specified to be relevant, divided by the total sum of all distances.
-     *
-     * @tparam FeatureMatrix    The type of the feature matrix that provides row-wise access to the feature values of
-     *                          the query examples
-     * @tparam Model            The type of the rule-based model that is used to obtain predictions
-     */
     template<typename FeatureMatrix, typename Model>
-    class MarginalizedProbabilityPredictor final : public IProbabilityPredictor {
-        private:
-
-            const FeatureMatrix& featureMatrix_;
-
-            const Model& model_;
-
-            uint32 numLabels_;
-
-            uint32 numThreads_;
-
-            const LabelVectorSet& labelVectorSet_;
-
-            std::unique_ptr<IProbabilityTransformation> probabilityTransformationPtr_;
-
-        public:
-
-            /**
-             * @param featureMatrix             A reference to an object of template type `FeatureMatrix` that provides
-             *                                  row-wise access to the feature values of the query examples
-             * @param model                     A reference to an object of template type `Model` that should be used to
-             *                                  obtain predictions
-             * @param labelVectorSet            A reference to an object of type `LabelVectorSet` that stores all known
-             *                                  label vectors
-             * @param numLabels                 The number of labels to predict for
-             * @param probabilityFunctionPtr    An unique pointer to an object of type `IProbabilityFunction` that
-             *                                  should be used to transform predicted scores into probabilities
-             * @param numThreads                The number of CPU threads to be used to make predictions for different
-             *                                  query examples in parallel. Must be at least 1
-             */
-            MarginalizedProbabilityPredictor(const FeatureMatrix& featureMatrix, const Model& model,
-                                             const LabelVectorSet& labelVectorSet, uint32 numLabels,
-                                             std::unique_ptr<IProbabilityFunction> probabilityFunctionPtr,
-                                             uint32 numThreads)
-                : featureMatrix_(featureMatrix), model_(model), numLabels_(numLabels), numThreads_(numThreads),
-                  labelVectorSet_(labelVectorSet),
-                  probabilityTransformationPtr_(std::make_unique<MarginalizedProbabilityTransformation>(
-                    labelVectorSet, std::move(probabilityFunctionPtr))) {}
-
-            /**
-             * @see `IPredictor::predict`
-             */
-            std::unique_ptr<DensePredictionMatrix<float64>> predict(uint32 maxRules) const override {
-                uint32 numExamples = featureMatrix_.getNumRows();
-                std::unique_ptr<DensePredictionMatrix<float64>> predictionMatrixPtr =
-                  std::make_unique<DensePredictionMatrix<float64>>(numExamples, numLabels_, true);
-
-                if (labelVectorSet_.getNumLabelVectors() > 0) {
-                    ProbabilityPredictionDelegate<FeatureMatrix, Model> delegate(*predictionMatrixPtr,
-                                                                                 *probabilityTransformationPtr_);
-                    PredictionDispatcher<float64, FeatureMatrix, Model>().predict(delegate, featureMatrix_, model_,
-                                                                                  maxRules, numThreads_);
-                }
-
-                return predictionMatrixPtr;
-            }
-
-            /**
-             * @see `IPredictor::canPredictIncrementally`
-             */
-            bool canPredictIncrementally() const override {
-                return false;
-            }
-
-            /**
-             * @see `IPredictor::createIncrementalPredictor`
-             */
-            std::unique_ptr<IIncrementalPredictor<DensePredictionMatrix<float64>>> createIncrementalPredictor(
-              uint32 minRules, uint32 maxRules) const override {
-                throw std::runtime_error(
-                  "The rule learner does not support to predict probability estimates incrementally");
-            }
-    };
-
-    template<typename FeatureMatrix>
-    static inline std::unique_ptr<IProbabilityPredictor> createMarginalizedProbabilityPredictor(
-      const FeatureMatrix& featureMatrix, const RuleList& model, const LabelVectorSet* labelVectorSet, uint32 numLabels,
-      const IProbabilityFunctionFactory& probabilityFunctionFactory, uint32 numThreads) {
+    static inline std::unique_ptr<IProbabilityPredictor> createPredictor(
+      const FeatureMatrix& featureMatrix, const Model& model, uint32 numLabels, uint32 numThreads,
+      const LabelVectorSet* labelVectorSet, const IProbabilityFunctionFactory& probabilityFunctionFactory) {
         if (!labelVectorSet) {
             throw std::runtime_error(
               "Information about the label vectors that have been encountered in the training data is required for "
@@ -105,9 +16,15 @@ namespace boosting {
               "was intended to use a different prediction method when it has been trained.");
         }
 
-        std::unique_ptr<IProbabilityFunction> probabilityFunctionPtr = probabilityFunctionFactory.create();
-        return std::make_unique<MarginalizedProbabilityPredictor<FeatureMatrix, RuleList>>(
-          featureMatrix, model, *labelVectorSet, numLabels, std::move(probabilityFunctionPtr), numThreads);
+        std::unique_ptr<IProbabilityTransformation> probabilityTransformationPtr;
+
+        if (labelVectorSet->getNumLabelVectors() > 0) {
+            probabilityTransformationPtr = std::make_unique<MarginalizedProbabilityTransformation>(
+              *labelVectorSet, probabilityFunctionFactory.create());
+        }
+
+        return std::make_unique<ProbabilityPredictor<FeatureMatrix, Model>>(featureMatrix, model, numLabels, numThreads,
+                                                                            std::move(probabilityTransformationPtr));
     }
 
     /**
@@ -144,8 +61,8 @@ namespace boosting {
             std::unique_ptr<IProbabilityPredictor> create(const CContiguousConstView<const float32>& featureMatrix,
                                                           const RuleList& model, const LabelVectorSet* labelVectorSet,
                                                           uint32 numLabels) const override {
-                return createMarginalizedProbabilityPredictor(featureMatrix, model, labelVectorSet, numLabels,
-                                                              *probabilityFunctionFactoryPtr_, numThreads_);
+                return createPredictor(featureMatrix, model, numLabels, numThreads_, labelVectorSet,
+                                       *probabilityFunctionFactoryPtr_);
             }
 
             /**
@@ -154,8 +71,8 @@ namespace boosting {
             std::unique_ptr<IProbabilityPredictor> create(const CsrConstView<const float32>& featureMatrix,
                                                           const RuleList& model, const LabelVectorSet* labelVectorSet,
                                                           uint32 numLabels) const override {
-                return createMarginalizedProbabilityPredictor(featureMatrix, model, labelVectorSet, numLabels,
-                                                              *probabilityFunctionFactoryPtr_, numThreads_);
+                return createPredictor(featureMatrix, model, numLabels, numThreads_, labelVectorSet,
+                                       *probabilityFunctionFactoryPtr_);
             }
     };
 
