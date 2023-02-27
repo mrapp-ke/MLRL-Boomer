4ffbb7a1b633a55c69d06688850b06c214a8ab7f
Michael Rapp <michael.rapp90@googlemail.com>
2023-02-27 18:45:27 +0100

    The class LabelWiseScorePredictorFactory does now create objects of type ScorePredictor.

--- cpp/subprojects/boosting/src/boosting/prediction/predictor_score_label_wise.cpp
+++ cpp/subprojects/boosting/src/boosting/prediction/predictor_score_label_wise.cpp
@@ -2,75 +2,7 @@
 
 #include "boosting/prediction/predictor_score_common.hpp"
 
-#include <stdexcept>
-
 namespace boosting {
-
-    /**
-     * An implementation of the type `IScorePredictor` that allows to predict label-wise regression scores for given
-     * query examples by summing up the scores that are provided by the individual rules of an existing rule-based model
-     * for each label individually.
-     *
-     * @tparam FeatureMatrix    The type of the feature matrix that provides row-wise access to the feature values of
-     *                          the query examples
-     * @tparam Model            The type of the rule-based model that is used to obtain predictions
-     */
-    template<typename FeatureMatrix, typename Model>
-    class LabelWiseScorePredictor final : public IScorePredictor {
-        private:
-
-            const FeatureMatrix& featureMatrix_;
-
-            const Model& model_;
-
-            uint32 numLabels_;
-
-            uint32 numThreads_;
-
-        public:
-
-            /**
-             * @param featureMatrix A reference to an object of template type `FeatureMatrix` that provides row-wise
-             *                      access to the feature values of the query examples
-             * @param model         A reference to an object of template type `Model` that should be used to obtain
-             *                      predictions
-             * @param numLabels     The number of labels to predict for
-             * @param numThreads    The number of CPU threads to be used to make predictions for different query
-             *                      examples in parallel. Must be at least 1
-             */
-            LabelWiseScorePredictor(const FeatureMatrix& featureMatrix, const Model& model, uint32 numLabels,
-                                    uint32 numThreads)
-                : featureMatrix_(featureMatrix), model_(model), numLabels_(numLabels), numThreads_(numThreads) {}
-
-            /**
-             * @see `IPredictor::predict`
-             */
-            std::unique_ptr<DensePredictionMatrix<float64>> predict(uint32 maxRules) const override {
-                uint32 numExamples = featureMatrix_.getNumRows();
-                std::unique_ptr<DensePredictionMatrix<float64>> predictionMatrixPtr =
-                  std::make_unique<DensePredictionMatrix<float64>>(numExamples, numLabels_, true);
-                ScorePredictionDelegate<FeatureMatrix, Model> delegate(*predictionMatrixPtr);
-                PredictionDispatcher<float64, FeatureMatrix, Model>().predict(delegate, featureMatrix_, model_,
-                                                                              maxRules, numThreads_);
-                return predictionMatrixPtr;
-            }
-
-            /**
-             * @see `IPredictor::canPredictIncrementally`
-             */
-            bool canPredictIncrementally() const override {
-                return false;
-            }
-
-            /**
-             * @see `IPredictor::createIncrementalPredictor`
-             */
-            std::unique_ptr<IIncrementalPredictor<DensePredictionMatrix<float64>>> createIncrementalPredictor(
-              uint32 minRules, uint32 maxRules) const override {
-                throw std::runtime_error(
-                  "The rule learner does not support to predict regression scores incrementally");
-            }
-    };
 
     /**
      * Allows to create instances of the type `IScorePredictor` that allow to predict label-wise regression scores for
@@ -96,7 +28,7 @@ namespace boosting {
             std::unique_ptr<IScorePredictor> create(const CContiguousConstView<const float32>& featureMatrix,
                                                     const RuleList& model, const LabelVectorSet* labelVectorSet,
                                                     uint32 numLabels) const override {
-                return std::make_unique<LabelWiseScorePredictor<CContiguousConstView<const float32>, RuleList>>(
+                return std::make_unique<ScorePredictor<CContiguousConstView<const float32>, RuleList>>(
                   featureMatrix, model, numLabels, numThreads_);
             }
 
@@ -106,8 +38,8 @@ namespace boosting {
             std::unique_ptr<IScorePredictor> create(const CsrConstView<const float32>& featureMatrix,
                                                     const RuleList& model, const LabelVectorSet* labelVectorSet,
                                                     uint32 numLabels) const override {
-                return std::make_unique<LabelWiseScorePredictor<CsrConstView<const float32>, RuleList>>(
-                  featureMatrix, model, numLabels, numThreads_);
+                return std::make_unique<ScorePredictor<CsrConstView<const float32>, RuleList>>(featureMatrix, model,
+                                                                                               numLabels, numThreads_);
             }
     };
 
