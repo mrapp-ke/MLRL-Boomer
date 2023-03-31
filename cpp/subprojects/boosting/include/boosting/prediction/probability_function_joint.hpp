7899ec588bcbc21ac3c22b0d30a2c8a94b4f3ca2
Michael Rapp <mrapp@ke.tu-darmstadt.de>
2023-03-31 18:40:19 +0200

    Add function "transformScoresIntoJointProbabilities" to class IJointProbabilityFunction.

--- cpp/subprojects/boosting/include/boosting/prediction/probability_function_joint.hpp
+++ cpp/subprojects/boosting/include/boosting/prediction/probability_function_joint.hpp
@@ -4,7 +4,9 @@
 #pragma once
 
 #include "boosting/prediction/probability_function_marginal.hpp"
-#include "common/data/view_vector.hpp"
+#include "common/data/vector_dense.hpp"
+#include "common/math/math.hpp"
+#include "common/prediction/label_vector_set.hpp"
 
 namespace boosting {
 
@@ -18,8 +20,8 @@ namespace boosting {
             virtual ~IJointProbabilityFunction() {};
 
             /**
-             * Transforms the regression scores that are predicted an example into a joint probability that corresponds
-             * to the chance of a given label vector being correct.
+             * Transforms the regression scores that are predicted for an example into a joint probability that
+             * corresponds to the chance of a given label vector being correct.
              *
              * @param scoresBegin   A `VectorConstView::const_iterator` to the beginning of the scores
              * @param scoresEnd     A `VectorConstView::const_iterator` to the end of the scores
@@ -30,6 +32,47 @@ namespace boosting {
             virtual float64 transformScoresIntoJointProbability(VectorConstView<float64>::const_iterator scoresBegin,
                                                                 VectorConstView<float64>::const_iterator scoresEnd,
                                                                 const LabelVector& labelVector) const = 0;
+
+            /**
+             * Transforms the regression scores that are predicted for an example into joint probabilities that
+             * correspond to the chance of individual label vectors contained by a `LabelVectorSet` being correct.
+             *
+             * @param scoresBegin       A `VectorConstView::const_iterator` to the beginning of the scores
+             * @param scoresEnd         A `VectorConstView::const_iterator` to the end of the scores
+             * @param labelVectorSet    A reference to an object of type `LabelVectorSet` that contains the label
+             *                          vectors the scores should be compared to
+             * @return                  An unique pointer to an object of type `DenseVector` that stores the joint
+             *                          probabilities that correspond to the chance of the given label vectors being
+             *                          correct
+             */
+            virtual std::unique_ptr<DenseVector<float64>> transformScoresIntoJointProbabilities(
+              VectorConstView<float64>::const_iterator scoresBegin, VectorConstView<float64>::const_iterator scoresEnd,
+              const LabelVectorSet& labelVectorSet) const {
+                uint32 numLabelVectors = labelVectorSet.getNumLabelVectors();
+                std::unique_ptr<DenseVector<float64>> jointProbabilityVectorPtr =
+                  std::make_unique<DenseVector<float64>>(numLabelVectors);
+                DenseVector<float64>::iterator jointProbabilityIterator = jointProbabilityVectorPtr->begin();
+                float64 sumOfJointProbabilities = 0;
+                uint32 i = 0;
+
+                // Calculate joint probabilities...
+                for (auto it = labelVectorSet.cbegin(); it != labelVectorSet.cend(); it++) {
+                    const LabelVector& labelVector = *((*it).first);
+                    float64 jointProbability =
+                      this->transformScoresIntoJointProbability(scoresBegin, scoresEnd, labelVector);
+                    sumOfJointProbabilities += jointProbability;
+                    jointProbabilityIterator[i] = jointProbability;
+                    i++;
+                }
+
+                // Normalize joint probabilities...
+                for (i = 0; i < numLabelVectors; i++) {
+                    float64 jointProbability = jointProbabilityIterator[i];
+                    jointProbabilityIterator[i] = divideOrZero(jointProbability, sumOfJointProbabilities);
+                }
+
+                return jointProbabilityVectorPtr;
+            }
     };
 
     /**
