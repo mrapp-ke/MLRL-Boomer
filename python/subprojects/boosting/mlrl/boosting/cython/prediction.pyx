031686d5deee8739f57886ff41125994d9800f52
Michael Rapp <michael.rapp90@googlemail.com>
2023-04-10 13:49:20 +0200

    Add Cython wrapper for the class ILabelWiseBinaryPredictorConfig.

--- python/subprojects/boosting/mlrl/boosting/cython/prediction.pyx
+++ python/subprojects/boosting/mlrl/boosting/cython/prediction.pyx
@@ -30,3 +30,33 @@ cdef class ExampleWiseBinaryPredictorConfig:
         """
         self.config_ptr.setBasedOnProbabilities(based_on_probabilities)
         return self
+
+
+cdef class LabelWiseBinaryPredictorConfig:
+    """
+    Allows to configure a predictor that predicts whether individual labels of given query examples are relevant or
+    irrelevant by discretizing the regression scores or probability estimates that are predicted for each label
+    individually.
+    """
+
+    def is_based_on_probabilities(self) -> bool:
+        """
+        Returns whether binary predictions are derived from probability estimates rather than regression scores or not.
+
+        :return: True, if binary predictions are derived from probability estimates rather than regression scores, False
+                 otherwise
+        """
+        return self.config_ptr.isBasedOnProbabilities()
+
+    def set_based_on_probabilities(self, based_on_probabilities: bool) -> LabelWiseBinaryPredictorConfig:
+        """
+        Sets whether binary predictions should be derived from probability estimates rather than regression scores or
+        not.
+
+        :param based_on_probabilities:  True, if binary predictions should be derived from probability estimates rather
+                                        than regression scores, False otherwise
+        :return:                        A `LabelWiseBinaryPredictorConfig` that allows further configuration of the
+                                        predictor
+        """
+        self.config_ptr.setBasedOnProbabilities(based_on_probabilities)
+        return self
