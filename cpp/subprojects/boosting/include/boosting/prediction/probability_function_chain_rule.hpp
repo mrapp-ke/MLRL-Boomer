04e2ecd6c709bad0fb4f1088b007b0b8838bb0d8
Michael Rapp <michael.rapp90@googlemail.com>
2023-04-13 16:20:19 +0200

    Move class ChainRule into cpp file.

--- cpp/subprojects/boosting/include/boosting/prediction/probability_function_chain_rule.hpp
+++ cpp/subprojects/boosting/include/boosting/prediction/probability_function_chain_rule.hpp
@@ -9,30 +9,6 @@
 namespace boosting {
 
     /**
-     * An implementation of the class `IJointProbabilityFunction` that transforms regression scores that are
-     * predicted for an example into joint probabilities by applying an `IMarginalProbabilityFunction` to each one and
-     * calculating the product of the resulting marginal probabilities according to the probabilistic chain rule.
-     */
-    class ChainRule final : public IJointProbabilityFunction {
-        private:
-
-            const std::unique_ptr<IMarginalProbabilityFunction> marginalProbabilityFunctionPtr_;
-
-        public:
-
-            /**
-             * @param marginalProbabilityFunctionPtr An unique pointer to an object of type
-             *                                       `IMarginalProbabilityFunction` to be used to transform regression
-             *                                       scores into marginal probabilities
-             */
-            ChainRule(std::unique_ptr<IMarginalProbabilityFunction> marginalProbabilityFunctionPtr);
-
-            float64 transformScoresIntoJointProbability(
-              const VectorConstView<uint32>& relevantLabelIndices, VectorConstView<float64>::const_iterator scoresBegin,
-              VectorConstView<float64>::const_iterator scoresEnd) const override;
-    };
-
-    /**
      * Allows to create instances of the type `IJointProbabilityFunction` that transform regression scores that are
      * predicted for an example into joint probabilities by applying an `IMarginalProbabilityFunction` to each one and
      * calculating the product of the resulting marginal probabilities according to the probabilistic chain rule.
