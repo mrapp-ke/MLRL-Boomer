a73b212149ad8c22c55487a8e6cb4b0746730019
Michael Rapp <michael.rapp90@googlemail.com>
2023-04-18 14:07:53 +0200

    Edit comment.

--- cpp/subprojects/boosting/include/boosting/statistics/statistics.hpp
+++ cpp/subprojects/boosting/include/boosting/statistics/statistics.hpp
@@ -6,7 +6,8 @@
 #include "common/statistics/statistics.hpp"
 
 /**
- * Defines an interface for all classes that store gradients and Hessians.
+ * Defines an interface for all classes that provide access to gradients and Hessians which serve as the basis for
+ * learning a new boosted rule or refining an existing one.
  */
 class IBoostingStatistics : public IStatistics {
     public:
