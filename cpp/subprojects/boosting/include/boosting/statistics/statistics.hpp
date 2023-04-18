97d8cac5006e50111c74374eed9d2e6ed68a60ca
Michael Rapp <michael.rapp90@googlemail.com>
2023-04-18 14:46:14 +0200

    Add function "visitScoreMatrix" to class IBoostingStatistics.

--- cpp/subprojects/boosting/include/boosting/statistics/statistics.hpp
+++ cpp/subprojects/boosting/include/boosting/statistics/statistics.hpp
@@ -3,7 +3,11 @@
  */
 #pragma once
 
+#include "common/data/matrix_sparse_set.hpp"
+#include "common/data/view_c_contiguous.hpp"
 #include "common/statistics/statistics.hpp"
+
+#include <functional>
 
 /**
  * Defines an interface for all classes that provide access to gradients and Hessians which serve as the basis for
@@ -13,4 +17,24 @@ class IBoostingStatistics : public IStatistics {
     public:
 
         virtual ~IBoostingStatistics() {};
+
+        /**
+         * A visitor function for handling score matrices of the type `CContiguousConstView`.
+         */
+        typedef std::function<void(const CContiguousConstView<float64>&)> DenseScoreMatrixVisitor;
+
+        /**
+         * A visitor function for handling score matrices of the type `SparseSetMatrix`.
+         */
+        typedef std::function<void(const SparseSetMatrix<float64>&)> SparseScoreMatrixVisitor;
+
+        /**
+         * Invokes one of the given visitor functions, depending on which one is able to handle the type of matrix that
+         * is used to store the currently predicted scores.
+         *
+         * @param denseVisitor  The visitor function for handling objects of the type `CContiguousConstView`
+         * @param sparseVisitor The visitor function for handling objects of the type `SparseSetMatrix`
+         */
+        virtual void visitScoreMatrix(DenseScoreMatrixVisitor denseVisitor,
+                                      SparseScoreMatrixVisitor sparseVisitor) const = 0;
 };
