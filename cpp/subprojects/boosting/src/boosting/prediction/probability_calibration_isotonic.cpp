9fe32de1060e9694021749323ab0c73c08265583
Michael Rapp <michael.rapp90@googlemail.com>
2023-04-21 15:32:17 +0200

    Fix doxygen errors.

--- cpp/subprojects/boosting/src/boosting/prediction/probability_calibration_isotonic.cpp
+++ cpp/subprojects/boosting/src/boosting/prediction/probability_calibration_isotonic.cpp
@@ -6,6 +6,9 @@
 class IsotonicMarginalProbabilityCalibrationModel final : public IIsotonicMarginalProbabilityCalibrationModel {
     public:
 
+        /**
+         * @see `IMarginalProbabilityCalibrationModel::calibrateMarginalProbability`
+         */
         float64 calibrateMarginalProbability(uint32 labelIndex, float64 marginalProbability) const override {
             // TODO Implement
             return marginalProbability;
@@ -19,24 +22,36 @@ class IsotonicMarginalProbabilityCalibrationModel final : public IIsotonicMargin
 class IsotonicMarginalProbabilityCalibrator final : public IMarginalProbabilityCalibrator {
     public:
 
+        /**
+         * @see `IMarginalProbabilityCalibrator::fitProbabilityCalibrationModel`
+         */
         std::unique_ptr<IMarginalProbabilityCalibrationModel> fitProbabilityCalibrationModel(
           const SinglePartition& partition, const CContiguousLabelMatrix& labelMatrix,
           const IStatistics& statistics) const override {
             return std::make_unique<IsotonicMarginalProbabilityCalibrationModel>();
         }
 
+        /**
+         * @see `IMarginalProbabilityCalibrator::fitProbabilityCalibrationModel`
+         */
         std::unique_ptr<IMarginalProbabilityCalibrationModel> fitProbabilityCalibrationModel(
           const SinglePartition& partition, const CsrLabelMatrix& labelMatrix,
           const IStatistics& statistics) const override {
             return std::make_unique<IsotonicMarginalProbabilityCalibrationModel>();
         }
 
+        /**
+         * @see `IMarginalProbabilityCalibrator::fitProbabilityCalibrationModel`
+         */
         std::unique_ptr<IMarginalProbabilityCalibrationModel> fitProbabilityCalibrationModel(
           const BiPartition& partition, const CContiguousLabelMatrix& labelMatrix,
           const IStatistics& statistics) const override {
             return std::make_unique<IsotonicMarginalProbabilityCalibrationModel>();
         }
 
+        /**
+         * @see `IMarginalProbabilityCalibrator::fitProbabilityCalibrationModel`
+         */
         std::unique_ptr<IMarginalProbabilityCalibrationModel> fitProbabilityCalibrationModel(
           const BiPartition& partition, const CsrLabelMatrix& labelMatrix,
           const IStatistics& statistics) const override {
