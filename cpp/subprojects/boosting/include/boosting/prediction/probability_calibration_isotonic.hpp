ff9794714eda0e3c7057c603fdd33aaceacb1b79
Michael Rapp <michael.rapp90@googlemail.com>
2023-04-21 15:49:00 +0200

    Add missing namespace.

--- cpp/subprojects/boosting/include/boosting/prediction/probability_calibration_isotonic.hpp
+++ cpp/subprojects/boosting/include/boosting/prediction/probability_calibration_isotonic.hpp
@@ -5,15 +5,19 @@
 
 #include "common/prediction/probability_calibration_isotonic.hpp"
 
-/**
- * Allows to configure a calibrator that fits a model for the calibration of marginal probabilities via isotonic
- * regression.
- */
-class IsotonicMarginalProbabilityCalibratorConfig final : public IMarginalProbabilityCalibratorConfig {
-    public:
+namespace boosting {
 
-        /**
-         * @see `IMarginalProbabilityCalibratorConfig::createMarginalProbabilityCalibrator`
-         */
-        std::unique_ptr<IMarginalProbabilityCalibrator> createMarginalProbabilityCalibrator() const override;
-};
+    /**
+     * Allows to configure a calibrator that fits a model for the calibration of marginal probabilities via isotonic
+     * regression.
+     */
+    class IsotonicMarginalProbabilityCalibratorConfig final : public IMarginalProbabilityCalibratorConfig {
+        public:
+
+            /**
+             * @see `IMarginalProbabilityCalibratorConfig::createMarginalProbabilityCalibrator`
+             */
+            std::unique_ptr<IMarginalProbabilityCalibrator> createMarginalProbabilityCalibrator() const override;
+    };
+
+}
--- cpp/subprojects/boosting/src/boosting/prediction/probability_calibration_isotonic.cpp
+++ cpp/subprojects/boosting/src/boosting/prediction/probability_calibration_isotonic.cpp
@@ -1,50 +1,53 @@
 #include "boosting/prediction/probability_calibration_isotonic.hpp"
 
-/**
- * An implementation of the type `IMarginalProbabilityCalibrator` that does fit a model for the calibration of marginal
- * probabilities via isotonic regression.
- */
-class IsotonicMarginalProbabilityCalibrator final : public IMarginalProbabilityCalibrator {
-    public:
+namespace boosting {
 
-        /**
-         * @see `IMarginalProbabilityCalibrator::fitProbabilityCalibrationModel`
-         */
-        std::unique_ptr<IMarginalProbabilityCalibrationModel> fitProbabilityCalibrationModel(
-          const SinglePartition& partition, const CContiguousLabelMatrix& labelMatrix,
-          const IStatistics& statistics) const override {
-            return std::make_unique<IsotonicMarginalProbabilityCalibrationModel>();
-        }
+    /**
+     * An implementation of the type `IMarginalProbabilityCalibrator` that does fit a model for the calibration of
+     * marginal probabilities via isotonic regression.
+     */
+    class IsotonicMarginalProbabilityCalibrator final : public IMarginalProbabilityCalibrator {
+        public:
 
-        /**
-         * @see `IMarginalProbabilityCalibrator::fitProbabilityCalibrationModel`
-         */
-        std::unique_ptr<IMarginalProbabilityCalibrationModel> fitProbabilityCalibrationModel(
-          const SinglePartition& partition, const CsrLabelMatrix& labelMatrix,
-          const IStatistics& statistics) const override {
-            return std::make_unique<IsotonicMarginalProbabilityCalibrationModel>();
-        }
+            /**
+             * @see `IMarginalProbabilityCalibrator::fitProbabilityCalibrationModel`
+             */
+            std::unique_ptr<IMarginalProbabilityCalibrationModel> fitProbabilityCalibrationModel(
+              const SinglePartition& partition, const CContiguousLabelMatrix& labelMatrix,
+              const IStatistics& statistics) const override {
+                return std::make_unique<IsotonicMarginalProbabilityCalibrationModel>();
+            }
 
-        /**
-         * @see `IMarginalProbabilityCalibrator::fitProbabilityCalibrationModel`
-         */
-        std::unique_ptr<IMarginalProbabilityCalibrationModel> fitProbabilityCalibrationModel(
-          const BiPartition& partition, const CContiguousLabelMatrix& labelMatrix,
-          const IStatistics& statistics) const override {
-            return std::make_unique<IsotonicMarginalProbabilityCalibrationModel>();
-        }
+            /**
+             * @see `IMarginalProbabilityCalibrator::fitProbabilityCalibrationModel`
+             */
+            std::unique_ptr<IMarginalProbabilityCalibrationModel> fitProbabilityCalibrationModel(
+              const SinglePartition& partition, const CsrLabelMatrix& labelMatrix,
+              const IStatistics& statistics) const override {
+                return std::make_unique<IsotonicMarginalProbabilityCalibrationModel>();
+            }
 
-        /**
-         * @see `IMarginalProbabilityCalibrator::fitProbabilityCalibrationModel`
-         */
-        std::unique_ptr<IMarginalProbabilityCalibrationModel> fitProbabilityCalibrationModel(
-          const BiPartition& partition, const CsrLabelMatrix& labelMatrix,
-          const IStatistics& statistics) const override {
-            return std::make_unique<IsotonicMarginalProbabilityCalibrationModel>();
-        }
-};
+            /**
+             * @see `IMarginalProbabilityCalibrator::fitProbabilityCalibrationModel`
+             */
+            std::unique_ptr<IMarginalProbabilityCalibrationModel> fitProbabilityCalibrationModel(
+              const BiPartition& partition, const CContiguousLabelMatrix& labelMatrix,
+              const IStatistics& statistics) const override {
+                return std::make_unique<IsotonicMarginalProbabilityCalibrationModel>();
+            }
 
-std::unique_ptr<IMarginalProbabilityCalibrator>
-  IsotonicMarginalProbabilityCalibratorConfig::createMarginalProbabilityCalibrator() const {
-    return std::make_unique<IsotonicMarginalProbabilityCalibrator>();
+            /**
+             * @see `IMarginalProbabilityCalibrator::fitProbabilityCalibrationModel`
+             */
+            std::unique_ptr<IMarginalProbabilityCalibrationModel> fitProbabilityCalibrationModel(
+              const BiPartition& partition, const CsrLabelMatrix& labelMatrix,
+              const IStatistics& statistics) const override {
+                return std::make_unique<IsotonicMarginalProbabilityCalibrationModel>();
+            }
+    };
+
+    std::unique_ptr<IMarginalProbabilityCalibrator>
+      IsotonicMarginalProbabilityCalibratorConfig::createMarginalProbabilityCalibrator() const {
+        return std::make_unique<IsotonicMarginalProbabilityCalibrator>();
+    }
 }
