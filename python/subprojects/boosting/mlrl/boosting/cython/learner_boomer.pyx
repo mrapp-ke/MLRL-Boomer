b6259867c3bd57ddbaf2e4c10b7c2e11e80c4ff7
Michael Rapp <michael.rapp90@googlemail.com>
2023-04-21 15:01:06 +0200

    Add Cython wrapper for class IIsotonicMarginalProbabilityCalibrationMixin.

--- python/subprojects/boosting/mlrl/boosting/cython/learner_boomer.pyx
+++ python/subprojects/boosting/mlrl/boosting/cython/learner_boomer.pyx
@@ -524,6 +524,13 @@ cdef class BoomerConfig(BoostingRuleLearnerConfig):
         config.config_ptr = config_ptr
         return config
 
+    def use_isotonic_marginal_probability_calibration(self):
+        """
+        Configures the rule learner to calibrate marginal probabilities via isotonic regression.
+        """
+        cdef IBoomerConfig* rule_learner_config_ptr = self.rule_learner_config_ptr.get()
+        rule_learner_config_ptr.useIsotonicMarginalProbabilityCalibration()
+
     def use_example_wise_binary_predictor(self) -> ExampleWiseBinaryPredictorConfig:
         """
         Configures the rule learner to use a predictor for predicting whether individual labels are relevant or
