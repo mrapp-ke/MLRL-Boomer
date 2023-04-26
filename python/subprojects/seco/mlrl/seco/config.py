d19456f9250f8d2860763fc295e39b5a1deb6410
Michael Rapp <michael.rapp90@googlemail.com>
2023-04-26 16:10:49 +0200

    Remove obsolete imports.

--- python/subprojects/seco/mlrl/seco/config.py
+++ python/subprojects/seco/mlrl/seco/config.py
@@ -5,7 +5,6 @@ Provides utility functions for configuring separate-and-conquer (SeCo) algorithm
 """
 from mlrl.common.config import NONE
 from mlrl.common.options import Options, parse_param, parse_param_and_options
-from mlrl.seco.cython.learner import SeCoRuleLearnerConfig
 from typing import Dict, Set, Optional
 
 HEAD_TYPE_SINGLE = 'single-label'
@@ -51,7 +50,7 @@ LIFT_FUNCTION_VALUES: Dict[str, Set[str]] = {
 }
 
 
-def configure_head_type(config: SeCoRuleLearnerConfig, head_type: Optional[str]):
+def configure_head_type(config, head_type: Optional[str]):
     if head_type is not None:
         value = parse_param('head_type', head_type, HEAD_TYPE_VALUES)
 
@@ -61,7 +60,7 @@ def configure_head_type(config: SeCoRuleLearnerConfig, head_type: Optional[str])
             config.use_partial_heads()
 
 
-def configure_lift_function(config: SeCoRuleLearnerConfig, lift_function: Optional[str]):
+def configure_lift_function(config, lift_function: Optional[str]):
     if lift_function is not None:
         value, options = parse_param_and_options('lift_function', lift_function, LIFT_FUNCTION_VALUES)
 
@@ -77,75 +76,75 @@ def configure_lift_function(config: SeCoRuleLearnerConfig, lift_function: Option
             c.set_k(options.get_float(ARGUMENT_K, c.get_k()))
 
 
-def configure_precision_heuristic(config: SeCoRuleLearnerConfig, value: str):
+def configure_precision_heuristic(config, value: str):
     if value == HEURISTIC_PRECISION:
         config.use_precision_heuristic()
 
 
-def configure_precision_pruning_heuristic(config: SeCoRuleLearnerConfig, value: str):
+def configure_precision_pruning_heuristic(config, value: str):
     if value == HEURISTIC_PRECISION:
         config.use_precision_pruning_heuristic()
 
 
-def configure_accuracy_heuristic(config: SeCoRuleLearnerConfig, value: str):
+def configure_accuracy_heuristic(config, value: str):
     if value == HEURISTIC_ACCURACY:
         config.use_accuracy_heuristic()
 
 
-def configure_accuracy_pruning_heuristic(config: SeCoRuleLearnerConfig, value: str):
+def configure_accuracy_pruning_heuristic(config, value: str):
     if value == HEURISTIC_ACCURACY:
         config.use_accuracy_pruning_heuristic()
 
 
-def configure_recall_heuristic(config: SeCoRuleLearnerConfig, value: str):
+def configure_recall_heuristic(config, value: str):
     if value == HEURISTIC_RECALL:
         config.use_recall_heuristic()
 
 
-def configure_recall_pruning_heuristic(config: SeCoRuleLearnerConfig, value: str):
+def configure_recall_pruning_heuristic(config, value: str):
     if value == HEURISTIC_RECALL:
         config.use_recall_pruning_heuristic()
 
 
-def configure_laplace_heuristic(config: SeCoRuleLearnerConfig, value: str):
+def configure_laplace_heuristic(config, value: str):
     if value == HEURISTIC_LAPLACE:
         config.use_laplace_heuristic()
 
 
-def configure_laplace_pruning_heuristic(config: SeCoRuleLearnerConfig, value: str):
+def configure_laplace_pruning_heuristic(config, value: str):
     if value == HEURISTIC_LAPLACE:
         config.use_laplace_pruning_heuristic()
 
 
-def configure_wra_heuristic(config: SeCoRuleLearnerConfig, value: str):
+def configure_wra_heuristic(config, value: str):
     if value == HEURISTIC_WRA:
         config.use_wra_heuristic()
 
 
-def configure_wra_pruning_heuristic(config: SeCoRuleLearnerConfig, value: str):
+def configure_wra_pruning_heuristic(config, value: str):
     if value == HEURISTIC_WRA:
         config.use_wra_pruning_heuristic()
 
 
-def configure_f_measure_heuristic(config: SeCoRuleLearnerConfig, value: str, options: Options):
+def configure_f_measure_heuristic(config, value: str, options: Options):
     if value == HEURISTIC_F_MEASURE:
         c = config.use_f_measure_heuristic()
         c.set_beta(options.get_float(ARGUMENT_BETA, c.get_beta()))
 
 
-def configure_f_measure_pruning_heuristic(config: SeCoRuleLearnerConfig, value: str, options: Options):
+def configure_f_measure_pruning_heuristic(config, value: str, options: Options):
     if value == HEURISTIC_F_MEASURE:
         c = config.use_f_measure_pruning_heuristic()
         c.set_beta(options.get_float(ARGUMENT_BETA, c.get_beta()))
 
 
-def configure_m_estimate_heuristic(config: SeCoRuleLearnerConfig, value: str, options: Options):
+def configure_m_estimate_heuristic(config, value: str, options: Options):
     if value == HEURISTIC_M_ESTIMATE:
         c = config.use_m_estimate_heuristic()
         c.set_m(options.get_float(ARGUMENT_M, c.get_m()))
 
 
-def configure_m_estimate_pruning_heuristic(config: SeCoRuleLearnerConfig, value: str, options: Options):
+def configure_m_estimate_pruning_heuristic(config, value: str, options: Options):
     if value == HEURISTIC_M_ESTIMATE:
         c = config.use_m_estimate_pruning_heuristic()
         c.set_m(options.get_float(ARGUMENT_M, c.get_m()))
