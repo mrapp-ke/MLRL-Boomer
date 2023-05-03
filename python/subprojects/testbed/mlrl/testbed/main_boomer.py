2ee3ae7bc6ffb2890b960343f80db102bb738322
Michael Rapp <michael.rapp90@googlemail.com>
2023-05-03 20:36:31 +0200

    Use function "create_kwargs_from_parameters".

--- python/subprojects/testbed/mlrl/testbed/main_boomer.py
+++ python/subprojects/testbed/mlrl/testbed/main_boomer.py
@@ -3,7 +3,7 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 """
 from argparse import ArgumentParser
 
-from mlrl.common.config import configure_argument_parser
+from mlrl.common.config import configure_argument_parser, create_kwargs_from_parameters
 
 from mlrl.testbed.runnables import RuleLearnerRunnable
 
@@ -22,34 +22,7 @@ class BoomerRunnable(RuleLearnerRunnable):
         configure_argument_parser(parser, BoomerConfig, BOOSTING_RULE_LEARNER_PARAMETERS)
 
     def _create_learner(self, args):
-        return Boomer(random_state=args.random_state,
-                      feature_format=args.feature_format,
-                      label_format=args.label_format,
-                      prediction_format=args.prediction_format,
-                      statistic_format=args.statistic_format,
-                      default_rule=args.default_rule,
-                      rule_induction=args.rule_induction,
-                      max_rules=args.max_rules,
-                      time_limit=args.time_limit,
-                      global_pruning=args.global_pruning,
-                      sequential_post_optimization=args.sequential_post_optimization,
-                      loss=args.loss,
-                      binary_predictor=args.binary_predictor,
-                      probability_predictor=args.probability_predictor,
-                      rule_pruning=args.rule_pruning,
-                      label_sampling=args.label_sampling,
-                      instance_sampling=args.instance_sampling,
-                      shrinkage=args.shrinkage,
-                      feature_sampling=args.feature_sampling,
-                      holdout=args.holdout,
-                      feature_binning=args.feature_binning,
-                      label_binning=args.label_binning,
-                      head_type=args.head_type,
-                      l1_regularization_weight=args.l1_regularization_weight,
-                      l2_regularization_weight=args.l2_regularization_weight,
-                      parallel_rule_refinement=args.parallel_rule_refinement,
-                      parallel_statistic_update=args.parallel_statistic_update,
-                      parallel_prediction=args.parallel_prediction)
+        return Boomer(**create_kwargs_from_parameters(args, BoomerConfig, BOOSTING_RULE_LEARNER_PARAMETERS))
 
 
 def main():
--- python/subprojects/testbed/mlrl/testbed/main_seco.py
+++ python/subprojects/testbed/mlrl/testbed/main_seco.py
@@ -3,7 +3,7 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 """
 from argparse import ArgumentParser
 
-from mlrl.common.config import configure_argument_parser
+from mlrl.common.config import configure_argument_parser, create_kwargs_from_parameters
 
 from mlrl.seco.cython.learner_seco import MultiLabelSeCoRuleLearnerConfig
 from mlrl.seco.config import SECO_RULE_LEARNER_PARAMETERS
@@ -23,25 +23,7 @@ class SeCoRunnable(RuleLearnerRunnable):
         configure_argument_parser(parser, MultiLabelSeCoRuleLearnerConfig, SECO_RULE_LEARNER_PARAMETERS)
 
     def _create_learner(self, args):
-        return MultiLabelSeCoRuleLearner(random_state=args.random_state,
-                                         feature_format=args.feature_format,
-                                         label_format=args.label_format,
-                                         prediction_format=args.prediction_format,
-                                         rule_induction=args.rule_induction,
-                                         max_rules=args.max_rules,
-                                         time_limit=args.time_limit,
-                                         sequential_post_optimization=args.sequential_post_optimization,
-                                         heuristic=args.heuristic,
-                                         pruning_heuristic=args.pruning_heuristic,
-                                         rule_pruning=args.rule_pruning,
-                                         label_sampling=args.label_sampling,
-                                         instance_sampling=args.instance_sampling,
-                                         feature_sampling=args.feature_sampling,
-                                         head_type=args.head_type,
-                                         lift_function=args.lift_function,
-                                         parallel_rule_refinement=args.parallel_rule_refinement,
-                                         parallel_statistic_update=args.parallel_statistic_update,
-                                         parallel_prediction=args.parallel_prediction)
+        return MultiLabelSeCoRuleLearner(**create_kwargs_from_parameters(args, SECO_RULE_LEARNER_PARAMETERS))
 
 
 def main():
