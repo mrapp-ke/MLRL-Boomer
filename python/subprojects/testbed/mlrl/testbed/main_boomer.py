a852c53a91cdbeec3f5c00760651bfaad761b767
Michael Rapp <michael.rapp90@googlemail.com>
2023-05-03 20:52:54 +0200

    The class RuleLearnerRunnable is not an abstract class anymore.

--- python/subprojects/testbed/mlrl/testbed/main_boomer.py
+++ python/subprojects/testbed/mlrl/testbed/main_boomer.py
@@ -1,32 +1,19 @@
 """
 Author: Michael Rapp (michael.rapp.ml@gmail.com)
 """
-from argparse import ArgumentParser
-
-from mlrl.common.config import configure_argument_parser, create_kwargs_from_parameters
-
-from mlrl.testbed.runnables import RuleLearnerRunnable
-
 from mlrl.boosting.cython.learner_boomer import BoomerConfig
 from mlrl.boosting.config import BOOSTING_RULE_LEARNER_PARAMETERS
 from mlrl.boosting.boosting_learners import Boomer
 
-
-class BoomerRunnable(RuleLearnerRunnable):
-
-    def __init__(self):
-        super().__init__(description='Allows to run experiments using the BOOMER algorithm', learner_name='boomer')
-
-    def _configure_arguments(self, parser: ArgumentParser):
-        super()._configure_arguments(parser)
-        configure_argument_parser(parser, BoomerConfig, BOOSTING_RULE_LEARNER_PARAMETERS)
-
-    def _create_learner(self, args):
-        return Boomer(**create_kwargs_from_parameters(args, BoomerConfig, BOOSTING_RULE_LEARNER_PARAMETERS))
+from mlrl.testbed.runnables import RuleLearnerRunnable
 
 
 def main():
-    BoomerRunnable().run()
+    RuleLearnerRunnable(description='Allows to run experiments using the BOOMER algorithm',
+                        learner_name='boomer',
+                        learner_type=Boomer,
+                        config_type=BoomerConfig,
+                        parameters=BOOSTING_RULE_LEARNER_PARAMETERS).run()
 
 
 if __name__ == '__main__':
--- python/subprojects/testbed/mlrl/testbed/main_seco.py
+++ python/subprojects/testbed/mlrl/testbed/main_seco.py
@@ -1,10 +1,6 @@
 """
 Author: Michael Rapp (michael.rapp.ml@gmail.com)
 """
-from argparse import ArgumentParser
-
-from mlrl.common.config import configure_argument_parser, create_kwargs_from_parameters
-
 from mlrl.seco.cython.learner_seco import MultiLabelSeCoRuleLearnerConfig
 from mlrl.seco.config import SECO_RULE_LEARNER_PARAMETERS
 from mlrl.seco.seco_learners import MultiLabelSeCoRuleLearner
@@ -12,22 +8,12 @@ from mlrl.seco.seco_learners import MultiLabelSeCoRuleLearner
 from mlrl.testbed.runnables import RuleLearnerRunnable
 
 
-class SeCoRunnable(RuleLearnerRunnable):
-
-    def __init__(self):
-        super().__init__(description='Allows to run experiments using the Separate-and-Conquer algorithm',
-                         learner_name='seco')
-
-    def _configure_arguments(self, parser: ArgumentParser):
-        super()._configure_arguments(parser)
-        configure_argument_parser(parser, MultiLabelSeCoRuleLearnerConfig, SECO_RULE_LEARNER_PARAMETERS)
-
-    def _create_learner(self, args):
-        return MultiLabelSeCoRuleLearner(**create_kwargs_from_parameters(args, SECO_RULE_LEARNER_PARAMETERS))
-
-
 def main():
-    SeCoRunnable().run()
+    RuleLearnerRunnable(description='Allows to run experiments using the Separate-and-Conquer algorithm',
+                        learner_name='seco',
+                        learner_type=MultiLabelSeCoRuleLearner,
+                        config_type=MultiLabelSeCoRuleLearnerConfig,
+                        parameters=SECO_RULE_LEARNER_PARAMETERS).run()
 
 
 if __name__ == '__main__':
