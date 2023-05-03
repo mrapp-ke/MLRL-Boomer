aba1bb13428c508ff7eb336fdf1519f9fe4cf916
Michael Rapp <michael.rapp90@googlemail.com>
2023-05-03 17:35:37 +0200

    Remove obsolete functions.

--- python/subprojects/seco/mlrl/seco/config.py
+++ python/subprojects/seco/mlrl/seco/config.py
@@ -10,7 +10,7 @@ from mlrl.seco.cython.learner import SingleLabelHeadMixin, PartialHeadMixin, NoL
     LaplacePruningHeuristicMixin, WraPruningHeuristicMixin, FMeasurePruningHeuristicMixin, \
     MEstimatePruningHeuristicMixin
 from mlrl.common.config import NominalParameter, NONE, RULE_LEARNER_PARAMETERS
-from mlrl.common.options import Options, parse_param, parse_param_and_options
+from mlrl.common.options import Options
 from typing import Dict, Set, Optional
 
 HEAD_TYPE_SINGLE = 'single-label'
@@ -54,106 +54,6 @@ LIFT_FUNCTION_VALUES: Dict[str, Set[str]] = {
     LIFT_FUNCTION_PEAK: {OPTION_PEAK_LABEL, OPTION_MAX_LIFT, OPTION_CURVATURE},
     LIFT_FUNCTION_KLN: {OPTION_K}
 }
-
-
-def configure_head_type(config, head_type: Optional[str]):
-    if head_type is not None:
-        value = parse_param('head_type', head_type, HEAD_TYPE_VALUES)
-
-        if value == HEAD_TYPE_SINGLE:
-            config.use_single_label_heads()
-        elif value == HEAD_TYPE_PARTIAL:
-            config.use_partial_heads()
-
-
-def configure_lift_function(config, lift_function: Optional[str]):
-    if lift_function is not None:
-        value, options = parse_param_and_options('lift_function', lift_function, LIFT_FUNCTION_VALUES)
-
-        if value == NONE:
-            config.use_no_lift_function()
-        elif value == LIFT_FUNCTION_PEAK:
-            c = config.use_peak_lift_function()
-            c.set_peak_label(options.get_int(OPTION_PEAK_LABEL, c.get_peak_label()))
-            c.set_max_lift(options.get_float(OPTION_MAX_LIFT, c.get_max_lift()))
-            c.set_curvature(options.get_float(OPTION_CURVATURE, c.get_curvature()))
-        elif value == LIFT_FUNCTION_KLN:
-            c = config.use_kln_lift_function()
-            c.set_k(options.get_float(OPTION_K, c.get_k()))
-
-
-def configure_precision_heuristic(config, value: str):
-    if value == HEURISTIC_PRECISION:
-        config.use_precision_heuristic()
-
-
-def configure_precision_pruning_heuristic(config, value: str):
-    if value == HEURISTIC_PRECISION:
-        config.use_precision_pruning_heuristic()
-
-
-def configure_accuracy_heuristic(config, value: str):
-    if value == HEURISTIC_ACCURACY:
-        config.use_accuracy_heuristic()
-
-
-def configure_accuracy_pruning_heuristic(config, value: str):
-    if value == HEURISTIC_ACCURACY:
-        config.use_accuracy_pruning_heuristic()
-
-
-def configure_recall_heuristic(config, value: str):
-    if value == HEURISTIC_RECALL:
-        config.use_recall_heuristic()
-
-
-def configure_recall_pruning_heuristic(config, value: str):
-    if value == HEURISTIC_RECALL:
-        config.use_recall_pruning_heuristic()
-
-
-def configure_laplace_heuristic(config, value: str):
-    if value == HEURISTIC_LAPLACE:
-        config.use_laplace_heuristic()
-
-
-def configure_laplace_pruning_heuristic(config, value: str):
-    if value == HEURISTIC_LAPLACE:
-        config.use_laplace_pruning_heuristic()
-
-
-def configure_wra_heuristic(config, value: str):
-    if value == HEURISTIC_WRA:
-        config.use_wra_heuristic()
-
-
-def configure_wra_pruning_heuristic(config, value: str):
-    if value == HEURISTIC_WRA:
-        config.use_wra_pruning_heuristic()
-
-
-def configure_f_measure_heuristic(config, value: str, options: Options):
-    if value == HEURISTIC_F_MEASURE:
-        c = config.use_f_measure_heuristic()
-        c.set_beta(options.get_float(OPTION_BETA, c.get_beta()))
-
-
-def configure_f_measure_pruning_heuristic(config, value: str, options: Options):
-    if value == HEURISTIC_F_MEASURE:
-        c = config.use_f_measure_pruning_heuristic()
-        c.set_beta(options.get_float(OPTION_BETA, c.get_beta()))
-
-
-def configure_m_estimate_heuristic(config, value: str, options: Options):
-    if value == HEURISTIC_M_ESTIMATE:
-        c = config.use_m_estimate_heuristic()
-        c.set_m(options.get_float(OPTION_M, c.get_m()))
-
-
-def configure_m_estimate_pruning_heuristic(config, value: str, options: Options):
-    if value == HEURISTIC_M_ESTIMATE:
-        c = config.use_m_estimate_pruning_heuristic()
-        c.set_m(options.get_float(OPTION_M, c.get_m()))
 
 
 class HeadTypeParameter(NominalParameter):
