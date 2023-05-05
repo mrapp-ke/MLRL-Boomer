92da0a491b9b8f1ba52c56fe0f3b678d30100d0f
Michael Rapp <michael.rapp90@googlemail.com>
2023-05-05 14:59:03 +0200

    Add class ILabelWiseProbabilityPredictorConfig.

--- cpp/subprojects/boosting/include/boosting/prediction/predictor_probability_label_wise.hpp
+++ cpp/subprojects/boosting/include/boosting/prediction/predictor_probability_label_wise.hpp
@@ -4,10 +4,41 @@
 #pragma once
 
 #include "boosting/losses/loss.hpp"
+#include "boosting/macros.hpp"
 #include "common/multi_threading/multi_threading.hpp"
 #include "common/prediction/predictor_probability.hpp"
 
 namespace boosting {
+
+    /**
+     * Defines an interface for all classes that allow to configure a predictor that predicts label-wise probabilities
+     * for given query examples, which estimate the chance of individual labels to be relevant, by summing up the scores
+     * that are provided by individual rules of an existing rule-based model and transforming the aggregated scores into
+     * probabilities in [0, 1] according to a certain transformation function that is applied to each label
+     * individually.
+     */
+    class MLRLBOOSTING_API ILabelWiseProbabilityPredictorConfig {
+        public:
+
+            virtual ~ILabelWiseProbabilityPredictorConfig() {};
+
+            /**
+             * Returns whether a model for the calibration of probabilities is used, if available, or not.
+             *
+             * @return True, if a model for the calibration of probabilities is used, if available, false otherwise
+             */
+            virtual bool isCalibrationModelUsed() const = 0;
+
+            /**
+             * Sets whether a model for the calibration of probabilities should be used, if available, or not.
+             *
+             * @param useCalibrationModel   True, if a model for the calibration of probabilities should be used, if
+             *                              available, false otherwise
+             * @return                      A reference to an object of type `ILabelWiseProbabilityPredictorConfig` that
+             *                              allows further configuration of the predictor
+             */
+            virtual ILabelWiseProbabilityPredictorConfig& setUseCalibrationModel(bool useCalibrationModel) = 0;
+    };
 
     /**
      * Allows to configure a predictor that predicts label-wise probabilities for given query examples, which estimate
@@ -15,8 +46,11 @@ namespace boosting {
      * an existing rule-based model and transforming the aggregated scores into probabilities in [0, 1] according to a
      * certain transformation function that is applied to each label individually.
      */
-    class LabelWiseProbabilityPredictorConfig final : public IProbabilityPredictorConfig {
+    class LabelWiseProbabilityPredictorConfig final : public ILabelWiseProbabilityPredictorConfig,
+                                                      public IProbabilityPredictorConfig {
         private:
+
+            bool useCalibrationModel_;
 
             const std::unique_ptr<ILossConfig>& lossConfigPtr_;
 
@@ -33,6 +67,10 @@ namespace boosting {
              */
             LabelWiseProbabilityPredictorConfig(const std::unique_ptr<ILossConfig>& lossConfigPtr,
                                                 const std::unique_ptr<IMultiThreadingConfig>& multiThreadingConfigPtr);
+
+            bool isCalibrationModelUsed() const override;
+
+            ILabelWiseProbabilityPredictorConfig& setUseCalibrationModel(bool useCalibrationModel) override;
 
             /**
              * @see `IProbabilityPredictorConfig::createPredictorFactory`
