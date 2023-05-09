38baada421afa436340ad5ffcdfe7a109ebfaef1
Michael Rapp <michael.rapp90@googlemail.com>
2023-05-09 15:05:12 +0200

    Implement serialization and deserialization of objects of type IIsotonicMarginalProbabilityCalibrationModel.

--- cpp/subprojects/common/src/common/prediction/probability_calibration_isotonic.cpp
+++ cpp/subprojects/common/src/common/prediction/probability_calibration_isotonic.cpp
@@ -1,13 +1,35 @@
 #include "common/prediction/probability_calibration_isotonic.hpp"
 
+IsotonicMarginalProbabilityCalibrationModel::IsotonicMarginalProbabilityCalibrationModel(uint32 numLabels)
+    : binsPerLabel_(ListOfLists<Tuple<float64>>(numLabels)) {}
+
 float64 IsotonicMarginalProbabilityCalibrationModel::calibrateMarginalProbability(uint32 labelIndex,
                                                                                   float64 marginalProbability) const {
     // TODO Implement
     return marginalProbability;
 }
 
-std::unique_ptr<IIsotonicMarginalProbabilityCalibrationModel> createIsotonicMarginalProbabilityCalibrationModel() {
-    return std::make_unique<IsotonicMarginalProbabilityCalibrationModel>();
+void IsotonicMarginalProbabilityCalibrationModel::addBin(uint32 labelIndex, float64 threshold, float64 probability) {
+    ListOfLists<Tuple<float64>>::row row = binsPerLabel_[labelIndex];
+    row.emplace_back(threshold, probability);
+}
+
+void IsotonicMarginalProbabilityCalibrationModel::visit(BinVisitor visitor) const {
+    uint32 numLabels = binsPerLabel_.getNumRows();
+
+    for (uint32 i = 0; i < numLabels; i++) {
+        ListOfLists<Tuple<float64>>::const_row row = binsPerLabel_[i];
+
+        for (auto it = row.cbegin(); it != row.cend(); it++) {
+            const Tuple<float64>& tuple = *it;
+            visitor(i, tuple.first, tuple.second);
+        }
+    }
+}
+
+std::unique_ptr<IIsotonicMarginalProbabilityCalibrationModel> createIsotonicMarginalProbabilityCalibrationModel(
+  uint32 numLabels) {
+    return std::make_unique<IsotonicMarginalProbabilityCalibrationModel>(numLabels);
 }
 
 float64 IsotonicJointProbabilityCalibrationModel::calibrateJointProbability(float64 jointProbability) const {
