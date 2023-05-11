bca01702d05c0f021d2fd4d141fc344d368724ef
Michael Rapp <michael.rapp90@googlemail.com>
2023-05-11 17:15:40 +0200

    Implement isotonic regression.

--- cpp/subprojects/boosting/src/boosting/prediction/probability_calibration_isotonic.cpp
+++ cpp/subprojects/boosting/src/boosting/prediction/probability_calibration_isotonic.cpp
@@ -180,6 +180,74 @@ namespace boosting {
         bins.resize(n);
     }
 
+    static inline void performIsotonicRegression(IsotonicMarginalProbabilityCalibrationModel::bin_list bins) {
+        // We apply the "pool adjacent violators algorithm" (PAVA) to merge adjacent bins with non-increasing
+        // probabilities. A temporary array `pools` is used to mark the beginning and end of subsequences with
+        // non-increasing probabilities. If such a subsequence was found in range [i, j] then `pools[i] = j` and
+        // `pools[j] = i`...
+        uint32 numBins = (uint32) bins.size();
+        uint32* pools = new uint32[numBins];
+        setArrayToIncreasingValues<uint32>(pools, numBins, 0, 1);
+        uint32 i = 0;
+        uint32 j = 0;
+
+        while (i < numBins && j < numBins && (j = pools[i] + 1) < numBins) {
+            Tuple<float64>& previousBin = bins[i];
+            Tuple<float64>& currentBin = bins[j];
+
+            // Check if the probabilities of the adjacent bins are monotonically increasing...
+            if (currentBin.second > previousBin.second) {
+                // The probabilities are increasing, i.e., the monotonicity constraint is not violated, and we can
+                // continue with the subsequent bins...
+                i = j;
+            } else {
+                // The probabilities are not increasing, i.e., the monotonicity constraint is violated, and we have to
+                // average the probabilities of all bins within the non-increasing subsequence...
+                uint32 numBinsInSubsequence = 2;
+                previousBin.second =
+                  iterativeArithmeticMean(numBinsInSubsequence, currentBin.second, previousBin.second);
+
+                // Search for the end of the non-increasing subsequence...
+                while ((j = pools[j] + 1) < numBins) {
+                    Tuple<float64>& nextBin = bins[j];
+
+                    if (nextBin.second > currentBin.second) {
+                        // We reached the end of the non-increasing subsequence...
+                        break;
+                    } else {
+                        // We are still within the non-increasing subsequence...
+                        numBinsInSubsequence++;
+                        previousBin.second =
+                          iterativeArithmeticMean(numBinsInSubsequence, nextBin.second, previousBin.second);
+                        currentBin = nextBin;
+                    }
+                }
+
+                // Store the beginning and end of the current subsequence...
+                pools[i] = j - 1;
+                pools[j - 1] = i;
+
+                // Restart at the previous subsequence if there is one...
+                if (i > 0) {
+                    j = pools[i - 1];
+                    i = j;
+                }
+            }
+        }
+
+        // Only keep the first bin within each subsequence...
+        j = 0;
+
+        for (i = 0; i < numBins; i = pools[i] + 1) {
+            bins[j] = bins[i];
+            j++;
+        }
+
+        delete[] pools;
+        bins.resize(j);
+        bins.shrink_to_fit();
+    }
+
     template<typename IndexIterator, typename LabelMatrix>
     static inline std::unique_ptr<IsotonicMarginalProbabilityCalibrationModel> fitMarginalProbabilityCalibrationModel(
       IndexIterator indexIterator, uint32 numExamples, const LabelMatrix& labelMatrix, const IStatistics& statistics,
@@ -213,9 +281,8 @@ namespace boosting {
             // Eliminate duplicate thresholds...
             eliminateDuplicateThresholds(bins);
 
-            // TODO Perform isotonic regression...
-
-            bins.shrink_to_fit();
+            // Perform isotonic regression...
+            performIsotonicRegression(bins);
         }
 
         return calibrationModelPtr;
