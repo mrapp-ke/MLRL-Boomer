d9c0ac6ce342b180df9a8e1531a6027ac354cf82
Michael Rapp <michael.rapp90@googlemail.com>
2023-05-11 18:26:19 +0200

    Implement function "calibrateMarginalProbability" of the class IsotonicMarginalProbabilityCalibrationModel.

--- cpp/subprojects/common/src/common/prediction/probability_calibration_isotonic.cpp
+++ cpp/subprojects/common/src/common/prediction/probability_calibration_isotonic.cpp
@@ -5,8 +5,31 @@ IsotonicMarginalProbabilityCalibrationModel::IsotonicMarginalProbabilityCalibrat
 
 float64 IsotonicMarginalProbabilityCalibrationModel::calibrateMarginalProbability(uint32 labelIndex,
                                                                                   float64 marginalProbability) const {
-    // TODO Implement
-    return marginalProbability;
+    ListOfLists<Tuple<float64>>::const_iterator begin = binsPerLabel_.row_cbegin(labelIndex);
+    ListOfLists<Tuple<float64>>::const_iterator end = binsPerLabel_.row_cend(labelIndex);
+    ListOfLists<Tuple<float64>>::const_iterator it =
+      std::lower_bound(begin, end, marginalProbability, [=](const Tuple<float64>& lhs, const float64& rhs) {
+          return lhs.first < rhs;
+      });
+    uint32 offset = it - begin;
+    Tuple<float64> lowerBound;
+    Tuple<float64> upperBound;
+
+    if (it == end) {
+        lowerBound = begin[offset - 1];
+        upperBound = 1;
+    } else {
+        if (offset > 0) {
+            lowerBound = begin[offset - 1];
+        } else {
+            lowerBound = 0;
+        }
+
+        upperBound = *it;
+    }
+
+    float64 t = marginalProbability / (upperBound.first - lowerBound.first);
+    return lowerBound.second + (t * (upperBound.second - lowerBound.second));
 }
 
 IsotonicMarginalProbabilityCalibrationModel::bin_list IsotonicMarginalProbabilityCalibrationModel::operator[](
