0c7d2c7eb7cd249a5fdfe1ab6793370332bfad99
Michael Rapp <michael.rapp90@googlemail.com>
2023-06-04 17:22:16 +0200

    The class LabelVectorSet now uses an std::vector to store label vectors and their frequencies.

--- cpp/subprojects/common/src/common/prediction/label_vector_set.cpp
+++ cpp/subprojects/common/src/common/prediction/label_vector_set.cpp
@@ -7,6 +7,65 @@
 #include "common/prediction/predictor_probability.hpp"
 #include "common/prediction/predictor_score.hpp"
 
+#include <unordered_map>
+
+/**
+ * Allows to compute hashes for objects of type `LabelVector`.
+ */
+struct LabelVectorHash final {
+    public:
+
+        /**
+         * Computes and returns a hash for an object of type `LabelVector`.
+         *
+         * @param v A reference to an object of type `LabelVector`
+         * @return  The hash that has been computed
+         */
+        inline std::size_t operator()(const LabelVector& v) const {
+            return hashArray(v.cbegin(), v.getNumElements());
+        }
+};
+
+/**
+ * Allows to check whether two objects of type `LabelVector` are equal or not.
+ */
+struct LabelVectorPred final {
+    public:
+
+        /**
+         * Returns whether two objects of type `LabelVector` are equal or not.
+         *
+         * @param lhs   A reference to the first object of type `LabelVector`
+         * @param rhs   A reference to the second object of type `LabelVector`
+         * @return      True, if the given objects are equal, false otherwise
+         */
+        inline bool operator()(const LabelVector& lhs, const LabelVector& rhs) const {
+            return compareArrays(lhs.cbegin(), lhs.getNumElements(), rhs.cbegin(), rhs.getNumElements());
+        }
+};
+
+LabelVectorSet::LabelVectorSet() {}
+
+LabelVectorSet::LabelVectorSet(const IRowWiseLabelMatrix& labelMatrix) {
+    std::unordered_map<std::reference_wrapper<LabelVector>, uint32, LabelVectorHash, LabelVectorPred> map;
+    uint32 numRows = labelMatrix.getNumRows();
+
+    for (uint32 i = 0; i < numRows; i++) {
+        std::unique_ptr<LabelVector> labelVectorPtr = labelMatrix.createLabelVector(i);
+        uint32 frequency = ++map[*labelVectorPtr];
+
+        if (frequency <= 1) {
+            labelVectors_.push_back(std::move(labelVectorPtr));
+        }
+    }
+
+    for (auto it = labelVectors_.cbegin(); it != labelVectors_.cend(); it++) {
+        const std::unique_ptr<LabelVector>& labelVectorPtr = *it;
+        uint32 frequency = map[*labelVectorPtr];
+        frequencies_.emplace_back(frequency);
+    }
+}
+
 LabelVectorSet::const_iterator LabelVectorSet::cbegin() const {
     return labelVectors_.cbegin();
 }
@@ -15,19 +74,30 @@ LabelVectorSet::const_iterator LabelVectorSet::cend() const {
     return labelVectors_.cend();
 }
 
+LabelVectorSet::frequency_const_iterator LabelVectorSet::frequencies_cbegin() const {
+    return frequencies_.cbegin();
+}
+
+LabelVectorSet::frequency_const_iterator LabelVectorSet::frequencies_cend() const {
+    return frequencies_.cend();
+}
+
 uint32 LabelVectorSet::getNumLabelVectors() const {
     return (uint32) labelVectors_.size();
 }
 
-void LabelVectorSet::addLabelVector(std::unique_ptr<LabelVector> labelVectorPtr) {
-    ++labelVectors_[std::move(labelVectorPtr)];
+void LabelVectorSet::addLabelVector(std::unique_ptr<LabelVector> labelVectorPtr, uint32 frequency) {
+    labelVectors_.push_back(std::move(labelVectorPtr));
+    frequencies_.emplace_back(frequency);
 }
 
 void LabelVectorSet::visit(LabelVectorVisitor visitor) const {
-    for (auto it = labelVectors_.cbegin(); it != labelVectors_.cend(); it++) {
-        const auto& entry = *it;
-        const std::unique_ptr<LabelVector>& labelVectorPtr = entry.first;
-        visitor(*labelVectorPtr);
+    uint32 numLabelVectors = this->getNumLabelVectors();
+
+    for (uint32 i = 0; i < numLabelVectors; i++) {
+        const std::unique_ptr<LabelVector>& labelVectorPtr = labelVectors_[i];
+        uint32 frequency = frequencies_[i];
+        visitor(*labelVectorPtr, frequency);
     }
 }
 
@@ -81,15 +151,4 @@ std::unique_ptr<IProbabilityPredictor> LabelVectorSet::createProbabilityPredicto
 
 std::unique_ptr<ILabelVectorSet> createLabelVectorSet() {
     return std::make_unique<LabelVectorSet>();
-}
-
-std::unique_ptr<ILabelVectorSet> createLabelVectorSet(const IRowWiseLabelMatrix& labelMatrix) {
-    std::unique_ptr<LabelVectorSet> labelVectorSetPtr = std::make_unique<LabelVectorSet>();
-    uint32 numRows = labelMatrix.getNumRows();
-
-    for (uint32 i = 0; i < numRows; i++) {
-        labelVectorSetPtr->addLabelVector(labelMatrix.createLabelVector(i));
-    }
-
-    return labelVectorSetPtr;
 }
