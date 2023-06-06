f1ecbd9fe95dc73e830ac05893713daf80047535
Michael Rapp <michael.rapp90@googlemail.com>
2023-06-06 15:24:03 +0200

    Implement class IsotonicJointProbabilityCalibrationModel.

--- cpp/subprojects/common/include/common/prediction/probability_calibration_isotonic.hpp
+++ cpp/subprojects/common/include/common/prediction/probability_calibration_isotonic.hpp
@@ -100,21 +100,79 @@ class MLRLCOMMON_API IIsotonicJointProbabilityCalibrationModel : public IJointPr
     public:
 
         virtual ~IIsotonicJointProbabilityCalibrationModel() override {};
+
+        /**
+         * A visitor function for handling individual bins.
+         */
+        typedef std::function<void(uint32 labelVectorIndex, float64 threshold, float64 probability)> BinVisitor;
+
+        /**
+         * Returns the number of known label vectors.
+         *
+         * @return The number of known label vectors
+         */
+        virtual uint32 getNumLabelVectors() const = 0;
+
+        /**
+         * Adds a new bin to the calibration model.
+         *
+         * @param labelVectorIndex  The index of the label vector, the bin corresponds to
+         * @param threshold         The threshold of the bin
+         * @param probability       The probability of the bin
+         */
+        virtual void addBin(uint32 labelVectorIndex, float64 threshold, float64 probability) = 0;
+
+        /**
+         * Invokes the given visitor function for each bin that is contained by the calibration model.
+         *
+         * @param visitor The visitor function for handling individual bins
+         */
+        virtual void visit(BinVisitor visitor) const = 0;
 };
 
 /**
  * A model for the calibration of joint probabilities via isotonic regression.
  */
 class IsotonicJointProbabilityCalibrationModel final : public IIsotonicJointProbabilityCalibrationModel {
+    private:
+
+        ListOfLists<Tuple<float64>> binsPerLabelVector_;
+
     public:
 
+        /**
+         * @param numLabelVectors The total number of known label vectors
+         */
+        IsotonicJointProbabilityCalibrationModel(uint32 numLabelVectors);
+
+        /**
+         * Provides access to the bins that correspond to a specific label vector and allows to modify them.
+         */
+        typedef ListOfLists<Tuple<float64>>::row bin_list;
+
+        /**
+         * Provides access to the bins that correspond to a specific label vector and allows to modify its elements.
+         *
+         * @param labelVectorIndex  The index of the label vector
+         * @return                  A `bin_list`
+         */
+        bin_list operator[](uint32 labelVectorIndex);
+
         float64 calibrateJointProbability(uint32 labelVectorIndex, float64 jointProbability) const override;
+
+        uint32 getNumLabelVectors() const override;
+
+        void addBin(uint32 labelVectorIndex, float64 threshold, float64 probability) override;
+
+        void visit(BinVisitor visitor) const override;
 };
 
 /**
  * Creates and returns a new object of the type `IIsotonicJointProbabilityCalibrationModel`.
  *
- * @return An unique pointer to an object of type `IIsotonicJointProbabilityCalibrationModel` that has been created
+ * @param numLabelVectors   The total number of known label vectors
+ * @return                  An unique pointer to an object of type `IIsotonicJointProbabilityCalibrationModel` that has
+ *                          been created
  */
 MLRLCOMMON_API std::unique_ptr<IIsotonicJointProbabilityCalibrationModel>
-  createIsotonicJointProbabilityCalibrationModel();
+  createIsotonicJointProbabilityCalibrationModel(uint32 numLabelVectors);
--- python/subprojects/common/mlrl/common/cython/probability_calibration.pyx
+++ python/subprojects/common/mlrl/common/cython/probability_calibration.pyx
@@ -109,9 +109,42 @@ cdef class IsotonicJointProbabilityCalibrationModel(JointProbabilityCalibrationM
 
     cdef IJointProbabilityCalibrationModel* get_joint_probability_calibration_model_ptr(self):
         return self.probability_calibration_model_ptr.get()
-    
+
+    cdef __serialize_bin(self, uint32 label_vector_index, float64 threshold, float64 probability):
+        cdef list bin_list = self.state[label_vector_index]
+        bin_list.append((threshold, probability))
+
     def __reduce(self):
-        return (IsotonicJointProbabilityCalibrationModel, (), ())
+        cdef uint32 num_label_vectors = self.probability_calibration_model_ptr.get().getNumLabelVectors()
+        self.state = [[] for i in range(num_label_vectors)]
+        self.probability_calibration_model_ptr.get().visit(
+            wrapBinVisitor(<void*>self, <BinCythonVisitor>self.__serialize_bin))
+        cdef object state = (SERIALIZATION_VERSION, self.state)
+        self.state = None
+        return (IsotonicJointProbabilityCalibrationModel, (), state)
 
     def __setstate(self, state):
-        self.probability_calibration_model_ptr = createIsotonicJointProbabilityCalibrationModel()
+        cdef int version = state[0]
+
+        if version != SERIALIZATION_VERSION:
+            raise AssertionError('Version of the serialized IsotonicJointProbabilityCalibrationModel is ' + str(version)
+                                 + ', expected ' + str(SERIALIZATION_VERSION))
+        
+        cdef list bins_per_label_vector = state[1]
+        cdef uint32 num_label_vectors = len(bins_per_label_vector)
+        cdef unique_ptr[IIsotonicJointProbabilityCalibrationModel] joint_probability_calibration_model_ptr = \
+            createIsotonicJointProbabilityCalibrationModel(num_label_vectors)
+        cdef list bin_list
+        cdef float64 threshold, probability
+        cdef uint32 i, j, num_bins
+
+        for i in range(num_label_vectors):
+            bin_list = bins_per_label_vector[i]
+            num_bins = len(bin_list)
+
+            for j in range(num_bins):
+                threshold = bin_list[j][0]
+                probability = bin_list[j][1]
+                joint_probability_calibration_model_ptr.get().addBin(i, threshold, probability)
+
+        self.probability_calibration_model_ptr = move(joint_probability_calibration_model_ptr)
