be711cbb67d59326d2ad36aae1d2eea1558eedb8
Michael Rapp <michael.rapp90@googlemail.com>
2023-06-26 16:52:46 +0200

    Add class BinaryCscMatrix.

---
+++ cpp/subprojects/common/src/common/data/matrix_csc_binary.cpp
@@ -0,0 +1,12 @@
+#include "common/data/matrix_csc_binary.hpp"
+
+#include <cstdlib>
+
+BinaryCscMatrix::BinaryCscMatrix(uint32 numRows, uint32 numCols, uint32 numNonZeroElements)
+    : BinaryCscView(numRows, numCols, (uint32*) malloc(numNonZeroElements * sizeof(uint32)),
+                    (uint32*) malloc((numCols + 1) * sizeof(uint32))) {}
+
+BinaryCscMatrix::~BinaryCscMatrix() {
+    free(rowIndices_);
+    free(colIndices_);
+}
