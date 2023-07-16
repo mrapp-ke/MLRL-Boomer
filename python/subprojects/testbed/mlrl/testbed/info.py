6e9df365af29be293932673f71128ef66fccfd2a
Michael Rapp <michael.rapp90@googlemail.com>
2023-07-16 18:55:46 +0200

    Add function "get_package_info" to the file info.py of each Python package.

---
+++ python/subprojects/testbed/mlrl/testbed/info.py
@@ -0,0 +1,16 @@
+"""
+Author: Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides utility functions for retrieving information about this Python package.
+"""
+from mlrl.common.cython.info import get_common_cpp_library_info
+from mlrl.common.info import PythonPackageInfo
+
+
+def get_package_info() -> PythonPackageInfo:
+    """
+    Returns information about this Python package.
+
+    :return: A `PythonPackageInfo` that provides information about the Python package
+    """
+    return PythonPackageInfo('mlrl-testbed', cpp_libraries=[get_common_cpp_library_info])
