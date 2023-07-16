c2dd66f1fc8778bcebc0f4b236ecaba95240afd0
Michael Rapp <michael.rapp90@googlemail.com>
2023-07-16 16:11:15 +0200

    Add file info.py to "common" Python module.

---
+++ python/subprojects/common/mlrl/common/info.py
@@ -0,0 +1,15 @@
+"""
+Author: Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides utility functions for retrieving information about the version of the Python module.
+"""
+from mlrl.common.cython.info import CppLibraryInfo, get_common_cpp_library_info
+
+
+def get_cpp_library_info() -> CppLibraryInfo:
+    """
+    Returns information about the C++ library "libmlrlcommon".
+
+    :return: A `CppLibraryInfo` that provides information about the C++ library
+    """
+    return get_common_cpp_library_info()
