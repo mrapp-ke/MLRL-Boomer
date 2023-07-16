353aba536d4b5063e6ffdccbb4ea4842f12e6328
Michael Rapp <michael.rapp90@googlemail.com>
2023-07-16 16:01:36 +0200

    Add class ILibraryInfo.

--- cpp/subprojects/common/src/common/info.cpp
+++ cpp/subprojects/common/src/common/info.cpp
@@ -2,6 +2,19 @@
 
 #include "config.hpp"
 
-std::string getLibraryVersion() {
-    return MLRLCOMMON_VERSION;
+/**
+ * An implementation of the type `ILibraryInfo` that provides information about the C++ library "libmlrlcommon".
+ */
+class CommonLibraryInfo final : public ILibraryInfo {
+    public:
+
+        std::string getLibraryVersion() const override {
+            return MLRLCOMMON_VERSION;
+        }
+};
+
+const CommonLibraryInfo COMMON_LIBRARY_INFO = CommonLibraryInfo();
+
+const ILibraryInfo& getCommonLibraryInfo() {
+    return COMMON_LIBRARY_INFO;
 }
--- python/subprojects/common/mlrl/common/cython/info.pyx
+++ python/subprojects/common/mlrl/common/cython/info.pyx
@@ -1,13 +1,30 @@
 """
 @author: Michael Rapp (michael.rapp.ml@gmail.com)
 """
+from cython.operator cimport dereference
 
 
-def get_library_version() -> str:
+cdef class LibraryInfo:
     """
-    Returns the version of the library.
-
-    :return: A string that specifies the library version
+    Provides information about a C++ library.
     """
-    cdef string library_version = getLibraryVersion()
-    return library_version.decode('UTF-8')
+
+    def get_library_version(self) -> str:
+        """
+        Returns the version of the C++ library.
+
+        :return: A string that specifies the version
+        """
+        cdef string library_version = self.library_info_ptr.getLibraryVersion()
+        return library_version.decode('UTF-8')
+
+
+def get_common_library_info() -> LibraryInfo:
+    """
+    Returns information about the C++ library "libmlrlcommon".
+
+    :return: A `LibraryInfo` that provides information about the C++ library
+    """
+    cdef LibraryInfo library_info = LibraryInfo()
+    library_info.library_info_ptr = &getCommonLibraryInfo()
+    return library_info
