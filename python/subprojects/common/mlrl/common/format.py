27fc94609b3aa7b093d4d99773439cdc32a4afb5
Michael Rapp <michael.rapp90@googlemail.com>
2023-07-16 19:28:43 +0200

    Add function "format_string_iterable" to file format.py.

--- python/subprojects/common/mlrl/common/format.py
+++ python/subprojects/common/mlrl/common/format.py
@@ -4,7 +4,19 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 Provides utility functions for creating textual representations.
 """
 from functools import reduce
-from typing import Dict, Set
+from typing import Dict, Iterable, Set
+
+
+def format_string_iterable(strings: Iterable[str], separator: str = ', ', delimiter: str = '') -> str:
+    """
+    Creates and returns a textual representation of string in an iterable.
+
+    :params strings:    The iterable of strings to be formatted
+    :param separator:   The string that should be used as a separator
+    :param delimiter:   The string that should be added at the beginning and end of each string
+    :return:            The textual representation that has been created
+    """
+    return reduce(lambda a, b: a + (separator if len(a) > 0 else '') + delimiter + b + delimiter, strings, '')
 
 
 def format_enum_values(enum) -> str:
@@ -14,7 +26,7 @@ def format_enum_values(enum) -> str:
     :param enum:    The enum to be formatted
     :return:        The textual representation that has been created
     """
-    return '{' + reduce(lambda a, b: a + (', ' if len(a) > 0 else '') + '"' + b.value + '"', enum, '') + '}'
+    return '{' + format_string_iterable([x.value for x in enum], delimiter='"') + '}'
 
 
 def format_string_set(strings: Set[str]) -> str:
@@ -24,7 +36,7 @@ def format_string_set(strings: Set[str]) -> str:
     :param strings: The set of strings to be formatted
     :return:        The textual representation that has been created
     """
-    return '{' + reduce(lambda a, b: a + (', ' if len(a) > 0 else '') + '"' + b + '"', strings, '') + '}'
+    return '{' + format_string_iterable(strings, delimiter='"') + '}'
 
 
 def format_dict_keys(dictionary: Dict[str, Set[str]]) -> str:
