8ed09d5900d307b1521d5ccf1bf6be3f0f04ed74
Michael Rapp <michael.rapp90@googlemail.com>
2023-07-16 15:08:10 +0200

    Add file "info.hpp" to "common" C++ library.

---
+++ cpp/subprojects/common/include/common/info.hpp
@@ -0,0 +1,13 @@
+/*
+ * @author Michael Rapp (michael.rapp.ml@gmail.com)
+ */
+#include "common/macros.hpp"
+
+#include <string>
+
+/**
+ * Returns the version of the library.
+ *
+ * @return A string that specifies the library version
+ */
+MLRLCOMMON_API std::string getLibraryVersion();
