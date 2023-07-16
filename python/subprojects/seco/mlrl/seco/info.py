dcade3d75017314b9cb9c5d320549cdceb096561
Michael Rapp <michael.rapp90@googlemail.com>
2023-07-16 17:07:55 +0200

    Add file "info.py" to "seco" package.

---
+++ python/subprojects/seco/mlrl/seco/info.py
@@ -0,0 +1,17 @@
+"""
+Author: Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides utility functions for retrieving information about this Python module.
+"""
+from mlrl.common.cython.info import CppLibraryInfo
+
+from mlrl.seco.cython.info import get_seco_cpp_library_info
+
+
+def get_cpp_library_info() -> CppLibraryInfo:
+    """
+    Returns information about the C++ library "libmlrlseco".
+
+    :return: A `CppLibraryInfo` that provides information about the C++ library
+    """
+    return get_seco_cpp_library_info()
