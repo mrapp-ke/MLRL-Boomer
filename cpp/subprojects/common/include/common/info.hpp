54c867b7ebd6a383601b679cafdb81f728240c2c
Michael Rapp <michael.rapp90@googlemail.com>
2023-07-16 16:01:36 +0200

    Add class ILibraryInfo.

--- cpp/subprojects/common/include/common/info.hpp
+++ cpp/subprojects/common/include/common/info.hpp
@@ -6,8 +6,24 @@
 #include <string>
 
 /**
- * Returns the version of the library.
- *
- * @return A string that specifies the library version
+ * Defines an interface for all classes that provide information about a C++ library.
  */
-MLRLCOMMON_API std::string getLibraryVersion();
+class MLRLCOMMON_API ILibraryInfo {
+    public:
+
+        virtual ~ILibraryInfo() {};
+
+        /**
+         * Returns the version of the C++ library.
+         *
+         * @return A string that specifies the library version
+         */
+        virtual std::string getLibraryVersion() const = 0;
+};
+
+/**
+ * Returns an object of type `ILibraryVersion` that provides information about the C++ library "libmlrlcommon".
+ *
+ * @return A reference to an object of type `ILibraryVersion`
+ */
+MLRLCOMMON_API const ILibraryInfo& getCommonLibraryInfo();
--- python/subprojects/common/mlrl/common/cython/info.pxd
+++ python/subprojects/common/mlrl/common/cython/info.pxd
@@ -3,4 +3,15 @@ from libcpp.string cimport string
 
 cdef extern from "common/info.hpp" nogil:
 
-    string getLibraryVersion()
+    cdef cppclass ILibraryInfo:
+
+        string getLibraryVersion() const
+
+    const ILibraryInfo& getCommonLibraryInfo()
+
+
+cdef class LibraryInfo:
+    
+    # Attributes:
+
+    cdef const ILibraryInfo* library_info_ptr
