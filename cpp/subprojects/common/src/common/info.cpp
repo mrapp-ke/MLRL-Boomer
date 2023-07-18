7eda203aeadffc2d7a1e4b91cc4fa94d92bd578d
Michael Rapp <michael.rapp90@googlemail.com>
2023-07-18 19:13:25 +0200

    Fix doxygen errors.

--- cpp/subprojects/common/src/common/info.cpp
+++ cpp/subprojects/common/src/common/info.cpp
@@ -8,14 +8,23 @@
 class CommonLibraryInfo final : public ILibraryInfo {
     public:
 
+        /**
+         * @see `ILibraryInfo::getLibraryName`
+         */
         std::string getLibraryName() const override {
             return MLRLCOMMON_LIBRARY_NAME;
         }
 
+        /**
+         * @see `ILibraryInfo::getLibraryVersion`
+         */
         std::string getLibraryVersion() const override {
             return MLRLCOMMON_LIBRARY_VERSION;
         }
 
+        /**
+         * @see `ILibraryInfo::getTargetArchitecture`
+         */
         std::string getTargetArchitecture() const override {
             return MLRLCOMMON_TARGET_ARCHITECTURE;
         }
