9207dbd17d4b1ecc4af442b2fd966f4785c8e719
Michael Rapp <michael.rapp90@googlemail.com>
2023-07-18 13:17:55 +0200

    Make class PythonPackageInfo a dataclass.

--- python/subprojects/common/mlrl/common/info.py
+++ python/subprojects/common/mlrl/common/info.py
@@ -3,6 +3,7 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 
 Provides utility functions for retrieving information about this Python package.
 """
+from dataclasses import dataclass
 from typing import List
 
 import pkg_resources
@@ -10,57 +11,27 @@ import pkg_resources
 from mlrl.common.cython.info import CppLibraryInfo, get_common_cpp_library_info
 
 
+@dataclass
 class PythonPackageInfo:
     """
     Provides information about a Python package.
+
+    Attributes:
+        package_name:       A string that specifies the package name
+        package_version:    A string that specifies the package version
+        python_packages:    A list that contains a `PythonPackageInfo` for each Python package used by this package
+        cpp_libraries:      A list that contains a `CppLibraryInfo` for each C++ library used by this package
     """
+    package_name: str
+    python_packages: List['PythonPackageInfo'] = []
+    cpp_libraries: List[CppLibraryInfo] = []
 
-    def __init__(self,
-                 package_name: str,
-                 python_packages: List['PythonPackageInfo'] = [],
-                 cpp_libraries: List[CppLibraryInfo] = []):
+    @property
+    def package_version(self) -> str:
         """
-        :param package_name:    A string that specifies the package name
-        :param package_version: A string that specifies the package version
-        :param python_packages: A list that contains a `PythonPackageInfo` for each Python package used by this package
-        :param cpp_libraries:   A list that contains a `CppLibraryInfo` for each C++ library used by this package
+        The version of the Python package.
         """
-        self.package_name = package_name
-        self.package_version = pkg_resources.get_distribution(package_name).version
-        self.python_packages = python_packages
-        self.cpp_libraries = cpp_libraries
-
-    def get_package_name(self) -> str:
-        """
-        Returns the name of the Python package.
-
-        :return: A string that specifies the package name
-        """
-        return self.package_name
-
-    def get_package_version(self) -> str:
-        """
-        Returns the version of the Python package.
-
-        :return A string that specifies the package version
-        """
-        return self.package_version
-
-    def get_python_packages(self) -> List['PythonPackageInfo']:
-        """
-        Returns information about all Python packages that are used by this Python packages.
-
-        :return: A list that contains a `PythonPackageInfo` for each Python package used by this package
-        """
-        return self.python_packages
-
-    def get_cpp_libraries(self) -> List[CppLibraryInfo]:
-        """
-        Returns information about all C++ libraries that are used by this Python package.
-
-        :return: A list that contains a `CppLibraryInfo` for each C++ library used by this package
-        """
-        return self.cpp_libraries
+        return pkg_resources.get_distribution(self.package_name).version
 
     def __str__(self) -> str:
         return self.get_package_name() + ' ' + self.get_package_version()
