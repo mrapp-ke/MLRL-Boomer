72174185a1a0842ac714c24ebeefe400e5920039
Michael Rapp <michael.rapp90@googlemail.com>
2023-07-23 18:18:06 +0200

    Suppress Pylint check.

--- python/subprojects/testbed/mlrl/testbed/prediction_characteristics.py
+++ python/subprojects/testbed/mlrl/testbed/prediction_characteristics.py
@@ -39,6 +39,7 @@ class PredictionCharacteristicsWriter(OutputWriter):
     def __init__(self, sinks: List[OutputWriter.Sink]):
         super().__init__(sinks)
 
+    # pylint: disable=unused-argument
     def _generate_output_data(self, meta_data: MetaData, x, y, data_split: DataSplit, learner,
                               data_type: Optional[DataType], prediction_type: Optional[PredictionType],
                               prediction_scope: Optional[PredictionScope], predictions: Optional[Any],
