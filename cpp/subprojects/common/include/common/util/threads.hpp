9e8300172c76852c0906800f82841e4619966c60
Michael Rapp <michael.rapp90@googlemail.com>
2023-07-25 16:08:57 +0200

    The function "getNumAvailableThreads" does now check if multi-threading support is enabled.

--- cpp/subprojects/common/include/common/util/threads.hpp
+++ cpp/subprojects/common/include/common/util/threads.hpp
@@ -9,12 +9,17 @@
 #include <thread>
 
 /**
- * Returns the number of threads that are available for parallelized algorithms.
+ * Returns the number of threads that are available for parallelized algorithms. If multi-threading support is disabled,
+ * the number of available threads is limited to 1.
  *
  * @param numPreferredThreads   The preferred number of threads or 0, if all available CPU cores should be used
  * @return                      The number of available threads
  */
 static inline uint32 getNumAvailableThreads(uint32 numPreferredThreads) {
+#if MULTI_THREADING_SUPPORT_ENABLED
     uint32 numAvailableThreads = std::max<uint32>(std::thread::hardware_concurrency(), 1);
     return numPreferredThreads > 0 ? std::min(numAvailableThreads, numPreferredThreads) : numAvailableThreads;
+#else
+    return 1;
+#endif
 }
