c80bd7f30b8f9498be9f2ee30cbde0589862a0d6
Michael Rapp <michael.rapp90@googlemail.com>
2023-07-28 12:20:33 +0200

    Edit comment.

--- cpp/subprojects/common/include/common/util/threads.hpp
+++ cpp/subprojects/common/include/common/util/threads.hpp
@@ -12,7 +12,8 @@
  * Returns the number of threads that are available for parallelized algorithms. If multi-threading support is disabled,
  * the number of available threads is limited to 1.
  *
- * @param numPreferredThreads   The preferred number of threads or 0, if all available CPU cores should be used
+ * @param numPreferredThreads   The preferred number of threads. Must be at least 1 or 0, if all available CPU cores
+ *                              should be utilized
  * @return                      The number of available threads
  */
 static inline uint32 getNumAvailableThreads(uint32 numPreferredThreads) {
