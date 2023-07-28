04af5072d2d8cd8fea13bc6cb7ba4c4706e19abe
Michael Rapp <michael.rapp90@googlemail.com>
2023-07-28 12:59:42 +0200

    Add function "getNumAvailableCpuCores" to file threads.hpp.

--- cpp/subprojects/common/include/common/util/threads.hpp
+++ cpp/subprojects/common/include/common/util/threads.hpp
@@ -9,6 +9,16 @@
 #include <thread>
 
 /**
+ * Returns the number of CPU cores available on the machine, regardless of whether multi-threading support is enabled or
+ * not.
+ *
+ * @return The number of CPU cores available on the machine
+ */
+static inline uint32 getNumAvailableCpuCores() {
+    return std::max<uint32>(std::thread::hardware_concurrency(), 1);
+}
+
+/**
  * Returns the number of threads that are available for parallelized algorithms. If multi-threading support is disabled,
  * the number of available threads is limited to 1.
  *
@@ -18,8 +28,8 @@
  */
 static inline uint32 getNumAvailableThreads(uint32 numPreferredThreads) {
 #if MULTI_THREADING_SUPPORT_ENABLED
-    uint32 numAvailableThreads = std::max<uint32>(std::thread::hardware_concurrency(), 1);
-    return numPreferredThreads > 0 ? std::min(numAvailableThreads, numPreferredThreads) : numAvailableThreads;
+    uint32 numAvailableCpuCores = getNumAvailableCpuCores();
+    return numPreferredThreads > 0 ? std::min(numAvailableCpuCores, numPreferredThreads) : numAvailableCpuCores;
 #else
     return 1;
 #endif
