8e43dc1cc5a7dc36fe2a8a9f1ac952ac89733df4
Michael Rapp <michael.rapp90@googlemail.com>
2023-08-11 16:36:29 +0200

    Add "sphinxext-opengraph" as a dependency for the documentation.

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ release = (Path(__file__).resolve().parent.parent / 'VERSION').read_text()
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser']
+extensions = ['myst_parser', 'sphinxext.opengraph']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
