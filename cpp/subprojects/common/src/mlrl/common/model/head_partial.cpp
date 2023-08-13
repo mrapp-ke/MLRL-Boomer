dea234b5e2f65a1cd7e8ddd3100527c06b3010b0
Michael Rapp <michael.rapp90@googlemail.com>
2023-08-13 16:15:36 +0200

    The class PartialHead now uses DenseVectors.

--- cpp/subprojects/common/src/mlrl/common/model/head_partial.cpp
+++ cpp/subprojects/common/src/mlrl/common/model/head_partial.cpp
@@ -1,47 +1,42 @@
 #include "mlrl/common/model/head_partial.hpp"
 
 PartialHead::PartialHead(uint32 numElements)
-    : numElements_(numElements), scores_(new float64[numElements]), labelIndices_(new uint32[numElements]) {}
-
-PartialHead::~PartialHead() {
-    delete[] scores_;
-    delete[] labelIndices_;
-}
+    : scores_(DenseVector<float64>(numElements)), labelIndices_(DenseVector<uint32>(numElements)) {}
 
 uint32 PartialHead::getNumElements() const {
-    return numElements_;
+    return scores_.getNumElements();
 }
 
 PartialHead::score_iterator PartialHead::scores_begin() {
-    return scores_;
+    return scores_.begin();
 }
 
 PartialHead::score_iterator PartialHead::scores_end() {
-    return &scores_[numElements_];
+    return scores_.end();
 }
 
 PartialHead::score_const_iterator PartialHead::scores_cbegin() const {
-    return scores_;
+    return scores_.cbegin();
 }
 
 PartialHead::score_const_iterator PartialHead::scores_cend() const {
-    return &scores_[numElements_];
+    return scores_.cend();
 }
 
 PartialHead::index_iterator PartialHead::indices_begin() {
-    return labelIndices_;
+    return labelIndices_.begin();
 }
 
 PartialHead::index_iterator PartialHead::indices_end() {
-    return &labelIndices_[numElements_];
+    return labelIndices_.end();
 }
 
 PartialHead::index_const_iterator PartialHead::indices_cbegin() const {
-    return labelIndices_;
+    return labelIndices_.cbegin();
 }
 
 PartialHead::index_const_iterator PartialHead::indices_cend() const {
-    return &labelIndices_[numElements_];
+    return labelIndices_.cend();
 }
 
 void PartialHead::visit(CompleteHeadVisitor completeHeadVisitor, PartialHeadVisitor partialHeadVisitor) const {
