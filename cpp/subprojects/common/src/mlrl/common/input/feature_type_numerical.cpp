0231c5191d0bd9678e768a2ce35826ae9cf15f4e
Michael Rapp <michael.rapp90@googlemail.com>
2023-08-13 19:40:51 +0200

    Format code.

--- cpp/subprojects/common/src/mlrl/common/input/feature_type_numerical.cpp
+++ cpp/subprojects/common/src/mlrl/common/input/feature_type_numerical.cpp
@@ -1,5 +1,9 @@
 #include "mlrl/common/input/feature_type_numerical.hpp"
 
+bool NumericalFeatureType::isOrdinal() const {
+    return false;
+}
+
 bool NumericalFeatureType::isNominal() const {
     return false;
 }
