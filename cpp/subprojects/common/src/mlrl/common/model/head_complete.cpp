f9aa9cad4de82f87109947d50f08dd47168fa9e2
Michael Rapp <michael.rapp90@googlemail.com>
2023-08-13 16:55:43 +0200

    Rename class member.

--- cpp/subprojects/common/src/mlrl/common/model/head_complete.cpp
+++ cpp/subprojects/common/src/mlrl/common/model/head_complete.cpp
@@ -1,25 +1,25 @@
 #include "mlrl/common/model/head_complete.hpp"
 
-CompleteHead::CompleteHead(uint32 numElements) : scores_(DenseVector<float64>(numElements)) {}
+CompleteHead::CompleteHead(uint32 numElements) : vector_(DenseVector<float64>(numElements)) {}
 
 uint32 CompleteHead::getNumElements() const {
-    return scores_.getNumElements();
+    return vector_.getNumElements();
 }
 
 CompleteHead::score_iterator CompleteHead::scores_begin() {
-    return scores_.begin();
+    return vector_.begin();
 }
 
 CompleteHead::score_iterator CompleteHead::scores_end() {
-    return scores_.end();
+    return vector_.end();
 }
 
 CompleteHead::score_const_iterator CompleteHead::scores_cbegin() const {
-    return scores_.cbegin();
+    return vector_.cbegin();
 }
 
 CompleteHead::score_const_iterator CompleteHead::scores_cend() const {
-    return scores_.cend();
+    return vector_.cend();
 }
 
 void CompleteHead::visit(CompleteHeadVisitor completeHeadVisitor, PartialHeadVisitor partialHeadVisitor) const {
