4c5e71ceb4ba142039d6d65edaa2e089d4acc2c0
Michael Rapp <michael.rapp90@googlemail.com>
2023-09-05 21:26:33 +0200

    Add build target for formatting Python code.

---
+++ scons/code_style.py
@@ -0,0 +1,37 @@
+"""
+Author: Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides utility functions for checking and enforcing code style definitions.
+"""
+from modules import BUILD_MODULE, PYTHON_MODULE
+from run import run_program
+
+
+def __isort(directory: str, enforce_changes: bool = False):
+    args = ['--settings-path', '.', '--virtual-env', 'venv', '--skip-gitignore']
+
+    if not enforce_changes:
+        args.append('--check')
+
+    run_program('isort', *args, directory)
+
+
+def __yapf(directory: str, enforce_changes: bool = False):
+    args = ['-r', '-p', '--style=.style.yapf', '--exclude', '**/build/*.py', '-i' if enforce_changes else '--diff']
+    run_program('yapf', *args, directory)
+
+
+def __pylint(directory: str):
+    args = ['--jobs=0', '--recursive=y', '--ignore=build', '--rcfile=.pylintrc', '--score=n']
+    run_program('pylint', *args, directory)
+
+
+def enforce_python_code_style(**_):
+    """
+    Enforces Python source files to adhere to the code style definitions.
+    """
+    for module in [BUILD_MODULE, PYTHON_MODULE]:
+        directory = module.root_dir
+        print('Formatting Python code in directory "' + directory + '"...')
+        __isort(directory, enforce_changes=True)
+        __yapf(directory, enforce_changes=True)
--- scons/sconstruct.py
+++ scons/sconstruct.py
@@ -5,10 +5,22 @@ Defines the individual targets of the build process.
 """
 from os import path
 
+from code_style import enforce_python_code_style
+from modules import BUILD_MODULE
 from SCons.Script.SConscript import SConsEnvironment
 
-from modules import BUILD_MODULE
+
+def __create_phony_target(environment, target, action=None):
+    return environment.AlwaysBuild(environment.Alias(target, None, action))
+
+
+# Define target names...
+TARGET_NAME_FORMAT = 'format'
+TARGET_NAME_FORMAT_PYTHON = TARGET_NAME_FORMAT + '_python'
 
 # Create temporary file ".sconsign.dblite" in the build directory...
 env = SConsEnvironment()
 env.SConsignFile(name=path.join(BUILD_MODULE.build_dir, '.sconsign'))
+
+# Define targets for enforcing code style definitions...
+target_format_python = __create_phony_target(env, TARGET_NAME_FORMAT_PYTHON, action=enforce_python_code_style)
