4cf064cdd384c95c5a55d255164a7c8b8a49331f
Michael Rapp <michael.rapp90@googlemail.com>
2023-09-05 21:44:52 +0200

    Exit build if any invalid targets are given.

--- scons/sconstruct.py
+++ scons/sconstruct.py
@@ -3,10 +3,14 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 
 Defines the individual targets of the build process.
 """
+import sys
+
+from functools import reduce
 from os import path
 
 from code_style import check_cpp_code_style, check_python_code_style, enforce_cpp_code_style, enforce_python_code_style
 from modules import BUILD_MODULE
+from SCons.Script import COMMAND_LINE_TARGETS
 from SCons.Script.SConscript import SConsEnvironment
 
 
@@ -21,6 +25,19 @@ TARGET_NAME_TEST_FORMAT_CPP = TARGET_NAME_TEST_FORMAT + '_cpp'
 TARGET_NAME_FORMAT = 'format'
 TARGET_NAME_FORMAT_PYTHON = TARGET_NAME_FORMAT + '_python'
 TARGET_NAME_FORMAT_CPP = TARGET_NAME_FORMAT + '_cpp'
+
+VALID_TARGETS = {
+    TARGET_NAME_TEST_FORMAT, TARGET_NAME_TEST_FORMAT_PYTHON, TARGET_NAME_TEST_FORMAT_CPP, TARGET_NAME_FORMAT,
+    TARGET_NAME_FORMAT_PYTHON, TARGET_NAME_FORMAT_CPP
+}
+
+# Raise an error if any invalid targets are given...
+invalid_targets = [target for target in COMMAND_LINE_TARGETS if target not in VALID_TARGETS]
+
+if invalid_targets:
+    print('The following targets are unknown: '
+          + reduce(lambda aggr, target: aggr + (', ' if len(aggr) > 0 else '') + target, invalid_targets, ''))
+    sys.exit(-1)
 
 # Create temporary file ".sconsign.dblite" in the build directory...
 env = SConsEnvironment()
