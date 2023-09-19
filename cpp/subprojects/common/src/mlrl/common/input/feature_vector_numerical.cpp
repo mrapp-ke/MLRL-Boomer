0b0f4aa3e0119da1a153eb400e6ec629d7e89c91
Michael Rapp <michael.rapp90@googlemail.com>
2023-09-19 20:33:44 +0200

    Add class NumericalFeatureVector.

---
+++ cpp/subprojects/common/src/mlrl/common/input/feature_vector_numerical.cpp
@@ -0,0 +1,28 @@
+#include "mlrl/common/input/feature_vector_numerical.hpp"
+
+NumericalFeatureVector::NumericalFeatureVector(uint32 numElements, float32 sparseValue)
+    : vector_(SparseArrayVector<float32>(numElements)), sparseValue_(sparseValue) {}
+
+NumericalFeatureVector::iterator NumericalFeatureVector::begin() {
+    return vector_.begin();
+}
+
+NumericalFeatureVector::iterator NumericalFeatureVector::end() {
+    return vector_.end();
+}
+
+NumericalFeatureVector::const_iterator NumericalFeatureVector::cbegin() const {
+    return vector_.cbegin();
+}
+
+NumericalFeatureVector::const_iterator NumericalFeatureVector::cend() const {
+    return vector_.cend();
+}
+
+void NumericalFeatureVector::setNumElements(uint32 numElements, bool freeMemory) {
+    return vector_.setNumElements(numElements, freeMemory);
+}
+
+uint32 NumericalFeatureVector::getNumElements() const {
+    return vector_.getNumElements();
+}
