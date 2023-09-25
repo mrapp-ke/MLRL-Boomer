d1c7d7da1237b2c6fc960f565027fbcc47c256b7
Michael Rapp <michael.rapp90@googlemail.com>
2023-09-25 15:53:32 +0200

    Add "createFeatureVector" functions to class IFeatureType.

--- cpp/subprojects/common/src/mlrl/common/input/feature_type_numerical.cpp
+++ cpp/subprojects/common/src/mlrl/common/input/feature_type_numerical.cpp
@@ -7,3 +7,15 @@ bool NumericalFeatureType::isOrdinal() const {
 bool NumericalFeatureType::isNominal() const {
     return false;
 }
+
+std::unique_ptr<IFeatureVector> NumericalFeatureType::createFeatureVector(
+  uint32 featureIndex, const FortranContiguousConstView<const float32>& featureMatrix) const {
+    // TODO Implement
+    return nullptr;
+}
+
+std::unique_ptr<IFeatureVector> NumericalFeatureType::createFeatureVector(
+  uint32 featureIndex, const CscConstView<const float32>& featureMatrix) const {
+    // TODO Implement
+    return nullptr;
+}
