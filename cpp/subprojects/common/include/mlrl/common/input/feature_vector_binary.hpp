b478e7619f88b28fac4c492149c477d27a68a25a
Michael Rapp <michael.rapp90@googlemail.com>
2023-09-25 14:08:42 +0200

    Add class BinaryFeatureVector.

---
+++ cpp/subprojects/common/include/mlrl/common/input/feature_vector_binary.hpp
@@ -0,0 +1,21 @@
+/*
+ * @author Michael Rapp (michael.rapp.ml@gmail.com)
+ */
+#pragma once
+
+#include "mlrl/common/input/feature_vector_nominal.hpp"
+
+/**
+ * A feature vector that stores the indices of all examples that are associated with the minority value, i.e., the least
+ * frequent value, of a binary feature.
+ */
+class BinaryFeatureVector final : public NominalFeatureVector {
+    public:
+
+        /**
+         * @param numExamples   The number of examples associated with the minority value
+         * @param minorityValue The minority value, i.e., the least frequent value, of the binary feature
+         * @param majorityValue The majority value, i.e., the most frequent value, of the binary feature
+         */
+        BinaryFeatureVector(uint32 numExamples, int32 minorityValue, int32 majorityValue);
+};
