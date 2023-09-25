6f3469f4b1f16f062500a6a9ef8ed303c1e846fa
Michael Rapp <michael.rapp90@googlemail.com>
2023-09-25 14:11:37 +0200

    Rename constructor arguments.

--- cpp/subprojects/common/src/mlrl/common/input/feature_vector_binary.cpp
+++ cpp/subprojects/common/src/mlrl/common/input/feature_vector_binary.cpp
@@ -1,7 +1,7 @@
 #include "mlrl/common/input/feature_vector_binary.hpp"
 
-BinaryFeatureVector::BinaryFeatureVector(uint32 numExamples, int32 minorityValue, int32 majorityValue)
-    : NominalFeatureVector(1, numExamples, majorityValue) {
+BinaryFeatureVector::BinaryFeatureVector(uint32 numElements, int32 minorityValue, int32 majorityValue)
+    : NominalFeatureVector(1, numElements, majorityValue) {
     this->values_begin()[0] = minorityValue;
     this->indptr_begin()[0] = 0;
 }
