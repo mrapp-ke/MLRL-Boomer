913007b48b9682d04ac3f9af6f92475415dc88f8
Michael Rapp <michael.rapp90@googlemail.com>
2023-09-30 13:29:57 +0200

    Add build target for running C++ tests.

--- scons/testing.py
+++ scons/testing.py
@@ -5,21 +5,31 @@ Provides utility functions for running automated tests.
 """
 from os import path
 
-from modules import PYTHON_MODULE
-from run import run_python_program
+from modules import CPP_MODULE, PYTHON_MODULE
+from run import run_python_program, run_venv_program
 
 
-def __run_python_tests(directory: str):
+def __meson_test(build_dir: str):
+    run_venv_program('meson', 'test', '-C', build_dir, print_args=True)
+
+def __python_unittest(directory: str):
     run_python_program('unittest', 'discover', '-v', '-f', '-s', directory)
 
 
-def run_tests(**_):
+def tests_cpp(**_):
     """
-    Runs all automated tests.
+    Runs all automated tests of C++ code.
+    """
+    __meson_test(CPP_MODULE.build_dir)
+
+
+def tests_python(**_):
+    """
+    Runs all automated tests of Python code.
     """
     for subproject in PYTHON_MODULE.find_subprojects():
         test_dir = subproject.test_dir
 
         if path.isdir(test_dir):
             print('Running automated tests for subpackage "' + subproject.name + '"...')
-            __run_python_tests(test_dir)
+            __python_unittest(test_dir)
