4f689f3703da2d87761e8741f5a3c58a5fff1fd2
Michael Rapp <michael.rapp90@googlemail.com>
2023-10-02 18:03:23 +0200

    The class EqualFeatureVector now inherits from IFeatureVector instead of AbstractFeatureVector.

--- cpp/subprojects/common/include/mlrl/common/input/feature_vector_equal.hpp
+++ cpp/subprojects/common/include/mlrl/common/input/feature_vector_equal.hpp
@@ -3,13 +3,13 @@
  */
 #pragma once
 
-#include "mlrl/common/input/feature_vector_common.hpp"
+#include "mlrl/common/input/feature_vector.hpp"
 
 /**
  * A feature vector that does not actually store any values. It is used in cases where all training examples have the
  * same value for a certain feature.
  */
-class EqualFeatureVector final : public AbstractFeatureVector {
+class EqualFeatureVector final : public IFeatureVector {
     public:
 
         uint32 getNumElements() const override;
