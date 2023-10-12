bd6e4356d146863a1b1ce0cb1a9990283cbd42e1
Michael Rapp <michael.rapp90@googlemail.com>
2023-10-12 15:06:23 +0200

    Remove field "order_" from class OrdinalFeatureVector.

--- cpp/subprojects/common/include/mlrl/common/input/feature_vector_ordinal.hpp
+++ cpp/subprojects/common/include/mlrl/common/input/feature_vector_ordinal.hpp
@@ -10,10 +10,6 @@
  * value, i.e., the most frequent value, of an ordinal feature.
  */
 class OrdinalFeatureVector : public NominalFeatureVector {
-    private:
-
-        uint32* order_;
-
     public:
 
         /**
@@ -23,39 +19,4 @@ class OrdinalFeatureVector : public NominalFeatureVector {
          * @param majorityValue The majority value, i.e., the most frequent value, of the ordinal feature
          */
         OrdinalFeatureVector(uint32 numValues, uint32 numExamples, int32 majorityValue);
-
-        ~OrdinalFeatureVector() override;
-
-        /**
-         * Returns an `index_iterator` to the beginning of the ordered indices of the values of the ordinal feature.
-         *
-         * @param index The index of the value
-         * @return      An `index_iterator` to the beginning
-         */
-        index_iterator order_begin(uint32 index);
-
-        /**
-         * Returns an `index_iterator` to the end of the ordered indices of the values of the ordinal feature.
-         *
-         * @param index The index of the value
-         * @return      An `index_iterator` to the end
-         */
-        index_iterator order_end(uint32 index);
-
-        /**
-         * Returns an `index_const_iterator` to the beginning of the ordered indices of the values of the ordinal
-         * feature.
-         *
-         * @param index The index of the value
-         * @return      An `index_const_iterator` to the beginning
-         */
-        index_const_iterator order_cbegin(uint32 index) const;
-
-        /**
-         * Returns an `index_const_iterator` to the end of the ordered indices of the values of the ordinal feature.
-         *
-         * @param index The index of the value
-         * @return      An `index_const_iterator` to the end
-         */
-        index_const_iterator order_cend(uint32 index) const;
 };
