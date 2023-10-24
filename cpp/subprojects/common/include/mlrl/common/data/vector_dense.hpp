732b906e2ab3d63c03ba1aa121d56f9cd0a0f073
Michael Rapp <michael.rapp90@googlemail.com>
2023-10-24 22:46:56 +0200

    Add class ResizableVectorDecorator.

--- cpp/subprojects/common/include/mlrl/common/data/vector_dense.hpp
+++ cpp/subprojects/common/include/mlrl/common/data/vector_dense.hpp
@@ -6,6 +6,48 @@
 #include "mlrl/common/data/view_vector.hpp"
 
 /**
+ * Allows to resize a vector.
+ *
+ * @tparam Vector The type of the vector
+ */
+template<typename Vector>
+class ResizableVectorDecorator : public Vector {
+    private:
+
+        uint32 maxCapacity_;
+
+    public:
+
+        /**
+         * @param view The view, the vector should be backed by
+         */
+        ResizableVectorDecorator(typename Vector::view_type&& view)
+            : Vector(std::move(view)), maxCapacity_(view.numElements) {}
+
+        virtual ~ResizableVectorDecorator() override {};
+
+        /**
+         * Sets the number of elements in the vector.
+         *
+         * @param numElements   The number of elements to be set
+         * @param freeMemory    True, if unused memory should be freed, if possible, false otherwise
+         */
+        void setNumElements(uint32 numElements, bool freeMemory) {
+            if (numElements < maxCapacity_) {
+                if (freeMemory) {
+                    Vector::view_.array = reallocateMemory(Vector::view_.array, numElements);
+                    maxCapacity_ = numElements;
+                }
+            } else if (numElements > maxCapacity_) {
+                Vector::view_.array = reallocateMemory(Vector::view_.array, numElements);
+                maxCapacity_ = numElements;
+            }
+
+            Vector::view_.numElements = numElements;
+        }
+};
+
+/**
  * An one-dimensional vector that provides random access to a fixed number of elements stored in a C-contiguous array.
  *
  * @tparam T The type of the data that is stored in the vector
