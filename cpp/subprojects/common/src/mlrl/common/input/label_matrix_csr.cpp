fe60ad48878070ca53962dbfaa684eb71f3dcaa5
Michael Rapp <michael.rapp90@googlemail.com>
2023-10-24 23:39:00 +0200

    The class CsrLabelMatrix::View now inherits from ReadableVectorDecorator.

--- cpp/subprojects/common/src/mlrl/common/input/label_matrix_csr.cpp
+++ cpp/subprojects/common/src/mlrl/common/input/label_matrix_csr.cpp
@@ -8,8 +8,8 @@
 #include "mlrl/common/util/math.hpp"
 
 CsrLabelMatrix::View::View(const CsrLabelMatrix& labelMatrix, uint32 row)
-    : VectorConstView<const uint32>(labelMatrix.indices_cend(row) - labelMatrix.indices_cbegin(row),
-                                    labelMatrix.indices_cbegin(row)) {}
+    : ReadableVectorDecorator<Vector<const uint32>>(Vector<const uint32>(
+      labelMatrix.indices_cbegin(row), labelMatrix.indices_cend(row) - labelMatrix.indices_cbegin(row))) {}
 
 CsrLabelMatrix::CsrLabelMatrix(uint32 numRows, uint32 numCols, uint32* indptr, uint32* colIndices)
     : BinaryCsrConstView(numRows, numCols, indptr, colIndices) {}
