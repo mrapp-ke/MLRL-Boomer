d2c3ac3f58652b536f3371bb26e14997dce11ab8
Michael Rapp <michael.rapp90@googlemail.com>
2023-11-17 19:41:55 +0100

    Use a copy of the global environment when passing environment variables to doxygen.

--- scons/environment.py
+++ scons/environment.py
@@ -7,34 +7,25 @@ from os import environ
 from typing import Optional
 
 
-def get_env(name: str, default: bool = None) -> Optional[str]:
+def get_env(name: str, default: bool = None, env=environ) -> Optional[str]:
     """
     Returns the value of the environment variable with a given name.
 
     :param name:    The name of the environment variable
     :param default: The default value to be returned if the environment variable is not set
+    :param env:     The environment to be accessed
     :return:        The value of the environment variable
     """
-    return environ.get(name, default)
+    return env.get(name, default)
 
 
-def set_env(name: str, value: str):
+def set_env(name: str, value: str, env=environ):
     """
     Sets the value of the environment variable with a given name.
 
     :param name:    The name of the environment variable
     :param value:   The value to be set
+    :param env:     The environment to be modified
     """
-    environ[name] = value
+    env[name] = value
     print('Set environment variable \'' + name + '\' to value \'' + value + '\'')
-
-
-def unset_env(name: str):
-    """
-    Unsets the environment variable with a given name.
-
-    :param name: The name of the environment variable
-    """
-    if name in environ:
-        del environ[name]
-        print('Unset environment variable \'' + name + '\'')
