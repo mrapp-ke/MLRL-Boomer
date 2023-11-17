c7c926c10c12f06756cbe63b475cd0ebca414f69
Michael Rapp <michael.rapp90@googlemail.com>
2023-11-17 23:39:51 +0100

    Use type View::const_iterator and View::iterator instead of Vector::const_iterator and Vector::iterator.

--- cpp/subprojects/common/include/mlrl/common/sampling/partition_bi.hpp
+++ cpp/subprojects/common/include/mlrl/common/sampling/partition_bi.hpp
@@ -33,12 +33,12 @@ class BiPartition final : public VectorDecorator<AllocatedVector<uint32>>,
          * An iterator that provides access to the indices that are contained by the first or second set and allows to
          * modify them.
          */
-        typedef Vector<uint32>::iterator iterator;
+        typedef View<uint32>::iterator iterator;
 
         /**
          * An iterator that provides read-only access to the indices that are contained in the first or second set.
          */
-        typedef Vector<uint32>::const_iterator const_iterator;
+        typedef View<uint32>::const_iterator const_iterator;
 
         /**
          * Returns an `iterator` to the beginning of the elements that are contained by the first set.
