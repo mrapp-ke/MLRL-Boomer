f39819944e1783d382055cb03f507d327d97f663
Michael Rapp <michael.rapp90@googlemail.com>
2023-11-18 01:23:55 +0100

    Add class BinnedVectorDecorator and corresponding subclasses.

--- cpp/subprojects/common/include/mlrl/common/data/vector_binned_dense.hpp
+++ cpp/subprojects/common/include/mlrl/common/data/vector_binned_dense.hpp
@@ -3,7 +3,8 @@
  */
 #pragma once
 
-#include "mlrl/common/data/vector_dense.hpp"
+#include "mlrl/common/data/array.hpp"
+#include "mlrl/common/data/view_vector_binned.hpp"
 #include "mlrl/common/iterator/binned_iterator.hpp"
 
 /**
@@ -13,12 +14,10 @@
  * @tparam T The type of the data that is stored in the vector
  */
 template<typename T>
-class DenseBinnedVector {
+class DenseBinnedVector final : public WritableBinnedVectorDecorator<AllocatedVector<uint32>, AllocatedVector<T>> {
     private:
 
-        DenseVector<uint32> binIndices_;
-
-        DenseVector<T> values_;
+        uint32 maxCapacity_;
 
     public:
 
@@ -28,113 +27,7 @@ class DenseBinnedVector {
          */
         DenseBinnedVector(uint32 numElements, uint32 numBins);
 
-        virtual ~DenseBinnedVector() {};
-
-        /**
-         * An iterator that provides access to the indices that correspond to individual bins and allows to modify them.
-         */
-        typedef typename DenseVector<uint32>::iterator index_iterator;
-
-        /**
-         * An iterator that provides read-only access to the indices that correspond to individual bins.
-         */
-        typedef typename DenseVector<uint32>::const_iterator index_const_iterator;
-
-        /**
-         * An iterator that provides access to the elements that correspond to individual bins and allows to modify
-         * them.
-         */
-        typedef typename DenseVector<T>::iterator value_iterator;
-
-        /**
-         * An iterator that provides read-only access to the elements that correspond to individual bins.
-         */
-        typedef typename DenseVector<T>::const_iterator value_const_iterator;
-
-        /**
-         * An iterator that provides read-only access to the elements in the vector.
-         */
-        typedef BinnedConstIterator<T> const_iterator;
-
-        /**
-         * Returns a `const_iterator` to the beginning of the vector.
-         *
-         * @return A `const_iterator` to the beginning
-         */
-        const_iterator cbegin() const;
-
-        /**
-         * Returns a `const_iterator` to the end of the vector.
-         *
-         * @return A `const_iterator` to the end
-         */
-        const_iterator cend() const;
-
-        /**
-         * Returns an `index_iterator` to the beginning of the bin indices individual elements in the vector correspond
-         * to.
-         *
-         * @return An `index_iterator` to the beginning
-         */
-        index_iterator indices_begin();
-
-        /**
-         * Returns an `index_iterator` to the end of the bin indices individual elements in the vector correspond to.
-         *
-         * @return An `index_iterator` to the end
-         */
-        index_iterator indices_end();
-
-        /**
-         * Returns an `index_const_iterator` to the beginning of the bin indices individual elements in the vector
-         * correspond to.
-         *
-         * @return An `index_const_iterator` to the beginning
-         */
-        index_const_iterator indices_cbegin() const;
-
-        /**
-         * Returns an `index_const_iterator` to the end of the bin indices individual elements in the vector correspond
-         * to.
-         *
-         * @return An `index_const_iterator` to the end
-         */
-        index_const_iterator indices_cend() const;
-
-        /**
-         * Returns a `value_iterator` to the beginning of the values that correspond to individual bins.
-         *
-         * @return A `value_iterator` to the beginning
-         */
-        value_iterator values_begin();
-
-        /**
-         * Returns a `value_iterator` to the end of the values that correspond to individual bins.
-         *
-         * @return A `value_iterator` to the end
-         */
-        value_iterator values_end();
-
-        /**
-         * Returns a `value_const_iterator` to the beginning of the values that correspond to individual bins.
-         *
-         * @return A `value_const_iterator` to the beginning
-         */
-        value_const_iterator values_cbegin() const;
-
-        /**
-         * Returns a `value_const_iterator` to the end of the values that correspond to individual bins.
-         *
-         * @return A `value_const_iterator` to the end
-         */
-        value_const_iterator values_cend() const;
-
-        /**
-         * Returns the number of bins.
-         *
-         * @return The number of bins
-         */
-        uint32 getNumBins() const;
+        virtual ~DenseBinnedVector() override {};
 
         /**
          * Sets the number of bins.
@@ -143,11 +36,4 @@ class DenseBinnedVector {
          * @param freeMemory    True, if unused memory should be freed, if possible, false otherwise
          */
         void setNumBins(uint32 numBins, bool freeMemory);
-
-        /**
-         * Returns the number of elements in the vector.
-         *
-         * @return The number of elements in the vector
-         */
-        uint32 getNumElements() const;
 };
--- cpp/subprojects/common/src/mlrl/common/data/vector_binned_dense.cpp
+++ cpp/subprojects/common/src/mlrl/common/data/vector_binned_dense.cpp
@@ -2,71 +2,23 @@
 
 template<typename T>
 DenseBinnedVector<T>::DenseBinnedVector(uint32 numElements, uint32 numBins)
-    : binIndices_(numElements), values_(numBins) {}
-
-template<typename T>
-typename DenseBinnedVector<T>::const_iterator DenseBinnedVector<T>::cbegin() const {
-    return BinnedConstIterator<T>(binIndices_.cbegin(), values_.cbegin());
-}
-
-template<typename T>
-typename DenseBinnedVector<T>::const_iterator DenseBinnedVector<T>::cend() const {
-    return BinnedConstIterator<T>(binIndices_.cend(), values_.cbegin());
-}
-
-template<typename T>
-typename DenseBinnedVector<T>::index_iterator DenseBinnedVector<T>::indices_begin() {
-    return binIndices_.begin();
-}
-
-template<typename T>
-typename DenseBinnedVector<T>::index_iterator DenseBinnedVector<T>::indices_end() {
-    return binIndices_.end();
-}
-
-template<typename T>
-typename DenseBinnedVector<T>::index_const_iterator DenseBinnedVector<T>::indices_cbegin() const {
-    return binIndices_.cbegin();
-}
-
-template<typename T>
-typename DenseBinnedVector<T>::index_const_iterator DenseBinnedVector<T>::indices_cend() const {
-    return binIndices_.cend();
-}
-
-template<typename T>
-typename DenseBinnedVector<T>::value_iterator DenseBinnedVector<T>::values_begin() {
-    return values_.begin();
-}
-
-template<typename T>
-typename DenseBinnedVector<T>::value_iterator DenseBinnedVector<T>::values_end() {
-    return values_.end();
-}
-
-template<typename T>
-typename DenseBinnedVector<T>::value_const_iterator DenseBinnedVector<T>::values_cbegin() const {
-    return values_.cbegin();
-}
-
-template<typename T>
-typename DenseBinnedVector<T>::value_const_iterator DenseBinnedVector<T>::values_cend() const {
-    return values_.cend();
-}
-
-template<typename T>
-uint32 DenseBinnedVector<T>::getNumElements() const {
-    return binIndices_.getNumElements();
-}
-
-template<typename T>
-uint32 DenseBinnedVector<T>::getNumBins() const {
-    return values_.getNumElements();
-}
+    : WritableBinnedVectorDecorator<AllocatedVector<uint32>, AllocatedVector<T>>(AllocatedVector<uint32>(numElements),
+                                                                                 AllocatedVector<T>(numBins)),
+      maxCapacity_(numBins) {}
 
 template<typename T>
 void DenseBinnedVector<T>::setNumBins(uint32 numBins, bool freeMemory) {
-    values_.setNumElements(numBins, freeMemory);
+    if (numBins < maxCapacity_) {
+        if (freeMemory) {
+            this->secondView_.array = reallocateMemory(this->secondView_.array, numBins);
+            maxCapacity_ = numBins;
+        }
+    } else if (numBins > maxCapacity_) {
+        this->secondView_.array = reallocateMemory(this->secondView_.array, numBins);
+        maxCapacity_ = numBins;
+    }
+
+    this->secondView_.numElements = numBins;
 }
 
 template class DenseBinnedVector<uint8>;
