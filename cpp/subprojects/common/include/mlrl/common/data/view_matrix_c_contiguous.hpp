79a4caa5f7f13a4845a20c4c1c7bc32f6624c690
Michael Rapp <michael.rapp90@googlemail.com>
2023-11-19 22:09:47 +0100

    Add class WriteIterableCContiguousMatrixDecorator.

--- cpp/subprojects/common/include/mlrl/common/data/view_matrix_c_contiguous.hpp
+++ cpp/subprojects/common/include/mlrl/common/data/view_matrix_c_contiguous.hpp
@@ -46,3 +46,45 @@ class ReadIterableCContiguousMatrixDecorator : public Matrix {
             return &Matrix::view.array[(row + 1) * Matrix::view.numCols];
         }
 };
+
+/**
+ * Provides row-wise write access via iterators to the values stored in a C-contiguous matrix.
+ *
+ * @tparam Matrix The type of the matrix
+ */
+template<typename Matrix>
+class WriteIterableCContiguousMatrixDecorator : public Matrix {
+    public:
+
+        /**
+         * @param view The view, the matrix should be backed by
+         */
+        WriteIterableCContiguousMatrixDecorator(typename Matrix::view_type&& view) : Matrix(std::move(view)) {}
+
+        virtual ~WriteIterableCContiguousMatrixDecorator() override {}
+
+        /**
+         * An iterator that provides access to the values stored in the matrix and allows to modify them.
+         */
+        typedef typename Matrix::view_type::iterator value_iterator;
+
+        /**
+         * Returns a `value_iterator` to the beginning of a specific row in the matrix.
+         *
+         * @param row   The index of the row
+         * @return      A `value_iterator` to the beginning of the row
+         */
+        value_iterator values_begin(uint32 row) {
+            return &Matrix::view.array[row * Matrix::view.numCols];
+        }
+
+        /**
+         * Returns a `value_iterator` to the end of a specific row in the matrix.
+         *
+         * @param row   The index of the row
+         * @return      A `value_iterator` to the end of the row
+         */
+        value_iterator values_end(uint32 row) {
+            return &Matrix::view.array[(row + 1) * Matrix::view.numCols];
+        }
+};
