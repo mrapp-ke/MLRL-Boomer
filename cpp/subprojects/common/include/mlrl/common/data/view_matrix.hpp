ddb826f2e975c16b7152270ad4d1f84af2269c5e
Michael Rapp <michael.rapp90@googlemail.com>
2023-11-19 22:07:52 +0100

    Add class Matrix.

---
+++ cpp/subprojects/common/include/mlrl/common/data/view_matrix.hpp
@@ -0,0 +1,65 @@
+/*
+ * @author Michael Rapp (michael.rapp.ml@gmail.com)
+ */
+#pragma once
+
+#include "mlrl/common/data/view.hpp"
+
+/**
+ * A two-dimensional view that provides access to values stored in a pre-allocated array of a specific size.
+ *
+ * @tparam T The type of the values, the view provides access to
+ */
+template<typename T>
+class Matrix : public View<T> {
+    public:
+
+        /**
+         * The number of rows in the view.
+         */
+        uint32 numRows;
+
+        /**
+         * The number of columns in the view.
+         */
+        uint32 numCols;
+
+        /**
+         * @param array     A pointer to an array of template type `T` that stores the values, the view should provide
+         *                  access to
+         * @param numRows   The number of rows in the view
+         * @param numCols   The number of columns in the view
+         */
+        Matrix(T* array, uint32 numRows, uint32 numCols)
+            : View<T>(array, numRows * numCols), numRows(numRows), numCols(numCols) {}
+
+        /**
+         * @param other A const reference to an object of type `Matrix` that should be copied
+         */
+        Matrix(const Matrix<T>& other) : Matrix(other.array, other.numRows, other.numCols) {}
+
+        /**
+         * @param other A reference to an object of type `Matrix` that should be moved
+         */
+        Matrix(Matrix<T>&& other) : Matrix(other.array, other.numRows, other.numCols) {}
+
+        virtual ~Matrix() override {}
+
+        /**
+         * Returns a `const_iterator` to the end of the view.
+         *
+         * @return A `const_iterator` to the end
+         */
+        typename View<T>::const_iterator cend() const {
+            return &View<T>::array[numRows * numCols];
+        }
+
+        /**
+         * Returns an `iterator` to the end of the view.
+         *
+         * @return An `iterator` to the end
+         */
+        typename View<T>::iterator end() {
+            return &View<T>::array[numRows * numCols];
+        }
+};
