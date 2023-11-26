fc24a1f6cd652c9f6ac246a5b4be76988bd87cc2
Michael Rapp <michael.rapp90@googlemail.com>
2023-11-26 15:09:42 +0100

    Remove class BinaryCscConstView.

--- cpp/subprojects/common/src/mlrl/common/data/view_csc_binary.cpp
+++ cpp/subprojects/common/src/mlrl/common/data/view_csc_binary.cpp
@@ -1,35 +1,32 @@
 #include "mlrl/common/data/view_csc_binary.hpp"
 
-BinaryCscConstView::BinaryCscConstView(uint32 numRows, uint32 numCols, uint32* rowIndices, uint32* indptr)
+BinaryCscView::BinaryCscView(uint32 numRows, uint32 numCols, uint32* rowIndices, uint32* indptr)
     : numRows_(numRows), numCols_(numCols), rowIndices_(rowIndices), indptr_(indptr) {}
 
-BinaryCscConstView::index_const_iterator BinaryCscConstView::indices_cbegin(uint32 col) const {
+BinaryCscView::index_const_iterator BinaryCscView::indices_cbegin(uint32 col) const {
     return &rowIndices_[indptr_[col]];
 }
 
-BinaryCscConstView::index_const_iterator BinaryCscConstView::indices_cend(uint32 col) const {
+BinaryCscView::index_const_iterator BinaryCscView::indices_cend(uint32 col) const {
     return &rowIndices_[indptr_[col + 1]];
 }
 
-uint32 BinaryCscConstView::getNumRows() const {
-    return numRows_;
-}
-
-uint32 BinaryCscConstView::getNumCols() const {
-    return numCols_;
-}
-
-uint32 BinaryCscConstView::getNumNonZeroElements() const {
-    return indptr_[numCols_];
-}
-
-BinaryCscView::BinaryCscView(uint32 numRows, uint32 numCols, uint32* rowIndices, uint32* indptr)
-    : BinaryCscConstView(numRows, numCols, rowIndices, indptr) {}
-
 BinaryCscView::index_iterator BinaryCscView::indices_begin(uint32 col) {
-    return &BinaryCscConstView::rowIndices_[BinaryCscConstView::indptr_[col]];
+    return &rowIndices_[indptr_[col]];
 }
 
 BinaryCscView::index_iterator BinaryCscView::indices_end(uint32 col) {
-    return &BinaryCscConstView::rowIndices_[BinaryCscConstView::indptr_[col + 1]];
+    return &rowIndices_[indptr_[col + 1]];
+}
+
+uint32 BinaryCscView::getNumRows() const {
+    return numRows_;
+}
+
+uint32 BinaryCscView::getNumCols() const {
+    return numCols_;
+}
+
+uint32 BinaryCscView::getNumNonZeroElements() const {
+    return indptr_[numCols_];
 }
