b0a61c984ec657b1a79c144c48db4cbe3a458d5a
Michael Rapp <michael.rapp90@googlemail.com>
2023-11-30 00:25:17 +0100

    The class CContiguousFeatureMatrix is not public anymore.

--- cpp/subprojects/common/include/mlrl/common/input/feature_matrix_c_contiguous.hpp
+++ cpp/subprojects/common/include/mlrl/common/input/feature_matrix_c_contiguous.hpp
@@ -8,7 +8,6 @@
     #pragma warning(disable : 4250)
 #endif
 
-#include "mlrl/common/data/view_matrix_c_contiguous.hpp"
 #include "mlrl/common/input/feature_matrix_row_wise.hpp"
 
 /**
@@ -19,51 +18,6 @@ class MLRLCOMMON_API ICContiguousFeatureMatrix : public IRowWiseFeatureMatrix {
     public:
 
         virtual ~ICContiguousFeatureMatrix() override {}
-};
-
-/**
- * An implementation of the type `ICContiguousFeatureMatrix` that provides row-wise read-only access to the feature
- * values of examples that are stored in a C-contiguous array.
- */
-class CContiguousFeatureMatrix final : public MatrixDecorator<CContiguousView<const float32>>,
-                                       public ICContiguousFeatureMatrix {
-    public:
-
-        /**
-         * @param array     A pointer to a C-contiguous array of type `float32` that stores the values, the feature
-         *                  matrix provides access to
-         * @param numRows   The number of rows in the feature matrix
-         * @param numCols   The number of columns in the feature matrix
-         */
-        CContiguousFeatureMatrix(const float32* array, uint32 numRows, uint32 numCols);
-
-        bool isSparse() const override;
-
-        uint32 getNumExamples() const override;
-
-        uint32 getNumFeatures() const override;
-
-        std::unique_ptr<IBinaryPredictor> createBinaryPredictor(
-          const IBinaryPredictorFactory& factory, const IRuleModel& ruleModel, const ILabelSpaceInfo& labelSpaceInfo,
-          const IMarginalProbabilityCalibrationModel& marginalProbabilityCalibrationModel,
-          const IJointProbabilityCalibrationModel& jointProbabilityCalibrationModel, uint32 numLabels) const override;
-
-        std::unique_ptr<ISparseBinaryPredictor> createSparseBinaryPredictor(
-          const ISparseBinaryPredictorFactory& factory, const IRuleModel& ruleModel,
-          const ILabelSpaceInfo& labelSpaceInfo,
-          const IMarginalProbabilityCalibrationModel& marginalProbabilityCalibrationModel,
-          const IJointProbabilityCalibrationModel& jointProbabilityCalibrationModel, uint32 numLabels) const override;
-
-        std::unique_ptr<IScorePredictor> createScorePredictor(const IScorePredictorFactory& factory,
-                                                              const IRuleModel& ruleModel,
-                                                              const ILabelSpaceInfo& labelSpaceInfo,
-                                                              uint32 numLabels) const override;
-
-        std::unique_ptr<IProbabilityPredictor> createProbabilityPredictor(
-          const IProbabilityPredictorFactory& factory, const IRuleModel& ruleModel,
-          const ILabelSpaceInfo& labelSpaceInfo,
-          const IMarginalProbabilityCalibrationModel& marginalProbabilityCalibrationModel,
-          const IJointProbabilityCalibrationModel& jointProbabilityCalibrationModel, uint32 numLabels) const override;
 };
 
 /**
