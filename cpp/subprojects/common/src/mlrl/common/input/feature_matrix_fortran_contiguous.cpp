b9d365d5912c2e45f107c714b84a33e530cc72f9
Michael Rapp <michael.rapp90@googlemail.com>
2023-11-29 23:27:21 +0100

    The class FortranContiguousFeatureMatrix now inherits from IterableDenseMatrixDecorator.

--- cpp/subprojects/common/src/mlrl/common/input/feature_matrix_fortran_contiguous.cpp
+++ cpp/subprojects/common/src/mlrl/common/input/feature_matrix_fortran_contiguous.cpp
@@ -11,8 +11,9 @@
  * An implementation of the type `IFortranContiguousFeatureMatrix` that provides column-wise read-only access to the
  * feature values of examples that are stored in a pre-allocated Fortran-contiguous array.
  */
-class FortranContiguousFeatureMatrix final : public FortranContiguousView<const float32>,
-                                             public IFortranContiguousFeatureMatrix {
+class FortranContiguousFeatureMatrix final
+    : public IterableDenseMatrixDecorator<MatrixDecorator<FortranContiguousView<const float32>>>,
+      public IFortranContiguousFeatureMatrix {
     public:
 
         /**
@@ -21,27 +22,29 @@ class FortranContiguousFeatureMatrix final : public FortranContiguousView<const 
          * @param array     A pointer to a Fortran-contiguous array of type `float32` that stores the feature values
          */
         FortranContiguousFeatureMatrix(const float32* array, uint32 numRows, uint32 numCols)
-            : FortranContiguousView<const float32>(array, numRows, numCols) {}
+            : IterableDenseMatrixDecorator<MatrixDecorator<FortranContiguousView<const float32>>>(
+              FortranContiguousView<const float32>(array, numRows, numCols)) {}
 
         bool isSparse() const override {
             return false;
         }
 
         uint32 getNumExamples() const override {
-            return Matrix::numRows;
+            return this->getNumRows();
         }
 
         uint32 getNumFeatures() const override {
-            return Matrix::numCols;
+            return this->getNumCols();
         }
 
         void fetchFeatureVector(uint32 featureIndex, std::unique_ptr<FeatureVector>& featureVectorPtr) const override {
             value_const_iterator columnIterator = this->values_cbegin(featureIndex);
-            featureVectorPtr = std::make_unique<FeatureVector>(Matrix::numRows);
+            uint32 numElements = this->getNumRows();
+            featureVectorPtr = std::make_unique<FeatureVector>(numElements);
             FeatureVector::iterator vectorIterator = featureVectorPtr->begin();
             uint32 i = 0;
 
-            for (uint32 j = 0; j < Matrix::numRows; j++) {
+            for (uint32 j = 0; j < numElements; j++) {
                 float32 value = columnIterator[j];
 
                 if (std::isnan(value)) {
