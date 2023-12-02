60e13a29c893b6ca431e4a112872c4e3468980ae
Michael Rapp <michael.rapp90@googlemail.com>
2023-12-03 00:23:28 +0100

    Remove class SparseLabelWiseStatisticConstView.

--- cpp/subprojects/boosting/include/mlrl/boosting/data/statistic_view_label_wise_sparse.hpp
+++ cpp/subprojects/boosting/include/mlrl/boosting/data/statistic_view_label_wise_sparse.hpp
@@ -9,10 +9,10 @@
 namespace boosting {
 
     /**
-     * Implements row-wise read-only access to the gradients and Hessians that have been calculated using a label-wise
-     * decomposable loss function and are stored in a pre-allocated matrix in the list of lists (LIL) format.
+     * Implements row-wise read and write access to the gradients and Hessians that have been calculated using a
+     * label-wise decomposable loss function and are stored in a pre-allocated matrix in the list of lists (LIL) format.
      */
-    class SparseLabelWiseStatisticConstView {
+    class SparseLabelWiseStatisticView {
         protected:
 
             /**
@@ -32,14 +32,19 @@ namespace boosting {
              * @param statistics    A pointer to an object of type `SparseSetMatrix` that stores the gradients and
              *                      Hessians
              */
-            SparseLabelWiseStatisticConstView(uint32 numCols, SparseSetMatrix<Tuple<float64>>* statistics);
+            SparseLabelWiseStatisticView(uint32 numCols, SparseSetMatrix<Tuple<float64>>* statistics);
 
-            virtual ~SparseLabelWiseStatisticConstView() {}
+            virtual ~SparseLabelWiseStatisticView() {}
 
             /**
              * Provides read-only access to a row.
              */
             typedef SparseSetMatrix<Tuple<float64>>::const_row const_row;
+
+            /**
+             * Provides access to a row and allows to modify its elements.
+             */
+            typedef SparseSetMatrix<Tuple<float64>>::row row;
 
             /**
              * An iterator that provides read-only access to the elements in the view.
@@ -71,6 +76,19 @@ namespace boosting {
             const_row operator[](uint32 row) const;
 
             /**
+             * Provides access to a specific row and allows to modify its elements.
+             *
+             * @param row   The index of the row
+             * @return      A `row`
+             */
+            row operator[](uint32 row);
+
+            /**
+             * Sets all gradients and Hessians in the matrix to zero.
+             */
+            void clear();
+
+            /**
              * Returns the number of rows in the view.
              *
              * @return The number of rows
@@ -83,41 +101,6 @@ namespace boosting {
              * @return The number of columns
              */
             uint32 getNumCols() const;
-    };
-
-    /**
-     * Implements row-wise read and write access to the gradients and Hessians that have been calculated using a
-     * label-wise decomposable loss function and are stored in a pre-allocated matrix in the list of lists (LIL) format.
-     */
-    class SparseLabelWiseStatisticView : public SparseLabelWiseStatisticConstView {
-        public:
-
-            /**
-             * @param numCols       The number of columns in the view
-             * @param statistics    A pointer to an object of type `SparseSetMatrix` that stores the gradients and
-             *                      Hessians
-             */
-            SparseLabelWiseStatisticView(uint32 numCols, SparseSetMatrix<Tuple<float64>>* statistics);
-
-            virtual ~SparseLabelWiseStatisticView() override {}
-
-            /**
-             * Provides access to a row and allows to modify its elements.
-             */
-            typedef SparseSetMatrix<Tuple<float64>>::row row;
-
-            /**
-             * Provides access to a specific row and allows to modify its elements.
-             *
-             * @param row   The index of the row
-             * @return      A `row`
-             */
-            row operator[](uint32 row);
-
-            /**
-             * Sets all gradients and Hessians in the matrix to zero.
-             */
-            void clear();
     };
 
 }
