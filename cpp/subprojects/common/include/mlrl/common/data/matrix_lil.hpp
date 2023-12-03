60e47bedceb68d83e557f62c7d8c78ea79492dea
Michael Rapp <michael.rapp90@googlemail.com>
2023-12-03 22:07:01 +0100

    Add typedef LilMatrixDecorator.

--- cpp/subprojects/common/include/mlrl/common/data/matrix_lil.hpp
+++ cpp/subprojects/common/include/mlrl/common/data/matrix_lil.hpp
@@ -5,6 +5,16 @@
 
 #include "mlrl/common/data/indexed_value.hpp"
 #include "mlrl/common/data/list_of_lists.hpp"
+#include "mlrl/common/data/view_matrix_lil.hpp"
+
+/**
+ * Provides row-wise read and write access via iterators to the values stored in a sparse matrix in the list of lists
+ * (LIL) format.
+ *
+ * @tparam Matrix The type of the matrix
+ */
+template<typename Matrix>
+using LilMatrixDecorator = IterableListOfListsDecorator<MatrixDecorator<Matrix>>;
 
 /**
  * A two-dimensional matrix that provides row-wise access to data that is stored in the list of lists (LIL) format.
