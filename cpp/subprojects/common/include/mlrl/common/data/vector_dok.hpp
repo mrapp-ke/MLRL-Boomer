3ed6bfed3a25e90d38b0d857948fb9b612c179db
Michael Rapp <michael.rapp90@googlemail.com>
2023-12-17 23:24:58 +0100

    Implement class DokVector in the header file.

--- cpp/subprojects/common/include/mlrl/common/data/vector_dok.hpp
+++ cpp/subprojects/common/include/mlrl/common/data/vector_dok.hpp
@@ -25,7 +25,7 @@ class DokVector final {
         /**
          * @param sparseValue The value of sparse elements
          */
-        DokVector(T sparseValue);
+        DokVector(T sparseValue) : sparseValue_(sparseValue) {}
 
         /**
          * An iterator that provides access to the elements in the vector and allows to modify them.
@@ -42,28 +42,36 @@ class DokVector final {
          *
          * @return An `iterator` to the beginning
          */
-        iterator begin();
+        iterator begin() {
+            return data_.begin();
+        }
 
         /**
          * Returns an `iterator` to the end of the vector.
          *
          * @return An `iterator` to the end
          */
-        iterator end();
+        iterator end() {
+            return data_.end();
+        }
 
         /**
          * Returns a `const_iterator` to the beginning of the vector.
          *
          * @return A `const_iterator` to the beginning
          */
-        const_iterator cbegin() const;
+        const_iterator cbegin() const {
+            return data_.cbegin();
+        }
 
         /**
          * Returns a `const_iterator` to the end of the vector.
          *
          * @return A `const_iterator` to the end
          */
-        const_iterator cend() const;
+        const_iterator cend() const {
+            return data_.cend();
+        }
 
         /**
          * Returns the value of the element at a specific position.
@@ -71,7 +79,10 @@ class DokVector final {
          * @param pos   The position of the element
          * @return      The value of the specified element
          */
-        const T& operator[](uint32 pos) const;
+        const T& operator[](uint32 pos) const {
+            auto it = data_.find(pos);
+            return it != data_.cend() ? it->second : sparseValue_;
+        }
 
         /**
          * Sets a value to the element at a specific position.
@@ -79,10 +90,18 @@ class DokVector final {
          * @param pos   The position of the element
          * @param value The value to be set
          */
-        void set(uint32 pos, T value);
+        void set(uint32 pos, T value) {
+            auto result = data_.emplace(pos, value);
+
+            if (!result.second) {
+                result.first->second = value;
+            }
+        }
 
         /**
          * Sets the values of all elements to zero.
          */
-        void clear();
+        void clear() {
+            data_.clear();
+        }
 };
