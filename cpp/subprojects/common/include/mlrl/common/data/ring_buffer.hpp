20dd5860aeb94b0ed3c50327be8ac5b37f0455a3
Michael Rapp <michael.rapp90@googlemail.com>
2023-12-17 23:27:13 +0100

    Implement class RingBuffer in the header file.

--- cpp/subprojects/common/include/mlrl/common/data/ring_buffer.hpp
+++ cpp/subprojects/common/include/mlrl/common/data/ring_buffer.hpp
@@ -25,7 +25,8 @@ class RingBuffer final : ViewDecorator<AllocatedVector<T>> {
         /**
          * @param capacity The maximum capacity of the buffer. Must be at least 1
          */
-        RingBuffer(uint32 capacity);
+        RingBuffer(uint32 capacity)
+            : ViewDecorator<AllocatedVector<T>>(AllocatedVector<T>(capacity)), pos_(0), full_(capacity == 0) {}
 
         /**
          * An iterator that provides read-only access to the elements in the buffer.
@@ -37,35 +38,45 @@ class RingBuffer final : ViewDecorator<AllocatedVector<T>> {
          *
          * @return A `const_iterator` to the beginning
          */
-        const_iterator cbegin() const;
+        const_iterator cbegin() const {
+            return this->view.cbegin();
+        }
 
         /**
          * Returns a `const_iterator` to the end of the buffer.
          *
          * @return A `const_iterator` to the end
          */
-        const_iterator cend() const;
+        const_iterator cend() const {
+            return &this->view.array[full_ ? this->getCapacity() : pos_];
+        }
 
         /**
          * Returns the maximum capacity of the buffer.
          *
          * @return The maximum capacity
          */
-        uint32 getCapacity() const;
+        uint32 getCapacity() const {
+            return this->view.numElements;
+        }
 
         /**
          * Returns the number of values in the buffer.
          *
          * @return The number of values
          */
-        uint32 getNumElements() const;
+        uint32 getNumElements() const {
+            return full_ ? this->getCapacity() : pos_;
+        }
 
         /**
          * Returns whether the maximum capacity of the buffer has been reached or not.
          *
          * @return True, if the maximum capacity has been reached, false otherwise
          */
-        bool isFull() const;
+        bool isFull() const {
+            return full_;
+        }
 
         /**
          * Adds a new value to the buffer. If the maximum capacity of the buffer has been reached, the oldest value will
@@ -76,5 +87,18 @@ class RingBuffer final : ViewDecorator<AllocatedVector<T>> {
          *              value has been overwritten, the pair's second value is set to the overwritten value, otherwise
          *              it is undefined
          */
-        std::pair<bool, T> push(T value);
+        std::pair<bool, T> push(T value) {
+            std::pair<bool, T> result;
+            result.first = full_;
+            result.second = this->view.array[pos_];
+            this->view.array[pos_] = value;
+            pos_++;
+
+            if (pos_ >= this->getCapacity()) {
+                pos_ = 0;
+                full_ = true;
+            }
+
+            return result;
+        }
 };
