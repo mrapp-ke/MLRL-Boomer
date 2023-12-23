cd6f92e86506bcb5ea9c1971c55b2c45b8a8f02f
Michael Rapp <michael.rapp90@googlemail.com>
2023-12-23 18:10:27 +0100

    The class NumericalFeatureVector is now a view.

--- cpp/subprojects/common/src/mlrl/common/input/feature_type_numerical.cpp
+++ cpp/subprojects/common/src/mlrl/common/input/feature_type_numerical.cpp
@@ -1,17 +1,51 @@
 #include "mlrl/common/input/feature_type_numerical.hpp"
 
+#include "feature_vector_numerical_allocated.hpp"
+#include "mlrl/common/data/view_composite.hpp"
 #include "mlrl/common/input/feature_vector_equal.hpp"
-#include "mlrl/common/input/feature_vector_numerical.hpp"
+#include "mlrl/common/input/feature_vector_missing.hpp"
 #include "mlrl/common/iterator/index_iterator.hpp"
 
 #include <algorithm>
 
+/**
+ * Provides random read and write access, as well as read and write access via iterators, to the indices and values of
+ * training examples stored in an `AllocatedNumericalFeatureVector`.
+ */
+class NumericalFeatureVectorDecorator final
+    : public ViewDecorator<CompositeView<AllocatedNumericalFeatureVector, AllocatedMissingFeatureVector>>,
+      public IFeatureVector {
+    public:
+
+        /**
+         * @param firstView   A reference to an object of type `AllocatedNumericalFeatureVector`
+         * @param secondView  A reference to an object of type `AllocatedMissingFeatureVector`
+         */
+        NumericalFeatureVectorDecorator(AllocatedNumericalFeatureVector&& firstView,
+                                        AllocatedMissingFeatureVector&& secondView)
+            : ViewDecorator<CompositeView<AllocatedNumericalFeatureVector, AllocatedMissingFeatureVector>>(
+              CompositeView<AllocatedNumericalFeatureVector, AllocatedMissingFeatureVector>(std::move(firstView),
+                                                                                            std::move(secondView))) {}
+
+        std::unique_ptr<IFeatureVector> createFilteredFeatureVector(std::unique_ptr<IFeatureVector>& existing,
+                                                                    uint32 start, uint32 end) const override {
+            // TODO Implement
+            return nullptr;
+        }
+
+        std::unique_ptr<IFeatureVector> createFilteredFeatureVector(std::unique_ptr<IFeatureVector>& existing,
+                                                                    const CoverageMask& coverageMask) const override {
+            // TODO Implement
+            return nullptr;
+        }
+};
+
 template<typename IndexIterator, typename ValueIterator>
-static inline std::unique_ptr<NumericalFeatureVector> createNumericalFeatureVector(IndexIterator indexIterator,
-                                                                                   ValueIterator valueIterator,
-                                                                                   uint32 numElements) {
-    std::unique_ptr<NumericalFeatureVector> featureVectorPtr = std::make_unique<NumericalFeatureVector>(numElements, 0);
-    NumericalFeatureVector::iterator vectorIterator = featureVectorPtr->begin();
+static inline std::unique_ptr<NumericalFeatureVectorDecorator> createNumericalFeatureVector(IndexIterator indexIterator,
+                                                                                            ValueIterator valueIterator,
+                                                                                            uint32 numElements) {
+    AllocatedNumericalFeatureVector numericalFeatureVector(numElements);
+    AllocatedMissingFeatureVector missingFeatureVector;
     uint32 n = 0;
 
     for (uint32 i = 0; i < numElements; i++) {
@@ -19,20 +53,21 @@ static inline std::unique_ptr<NumericalFeatureVector> createNumericalFeatureVect
         float32 value = valueIterator[i];
 
         if (std::isnan(value)) {
-            featureVectorPtr->setMissing(index, true);
+            missingFeatureVector.set(index, true);
         } else {
-            IndexedValue<float32>& entry = vectorIterator[n];
+            IndexedValue<float32>& entry = numericalFeatureVector[n];
             entry.index = index;
             entry.value = value;
             n++;
         }
     }
 
-    featureVectorPtr->setNumElements(n, true);
-    return featureVectorPtr;
+    numericalFeatureVector.resize(n, true);
+    return std::make_unique<NumericalFeatureVectorDecorator>(std::move(numericalFeatureVector),
+                                                             std::move(missingFeatureVector));
 }
 
-static inline std::unique_ptr<NumericalFeatureVector> createNumericalFeatureVector(
+static inline std::unique_ptr<NumericalFeatureVectorDecorator> createNumericalFeatureVector(
   uint32 featureIndex, const FortranContiguousView<const float32>& featureMatrix) {
     FortranContiguousView<const float32>::value_const_iterator valueIterator =
       featureMatrix.values_cbegin(featureIndex);
@@ -40,7 +75,7 @@ static inline std::unique_ptr<NumericalFeatureVector> createNumericalFeatureVect
     return createNumericalFeatureVector(IndexIterator(), valueIterator, numElements);
 }
 
-static inline std::unique_ptr<NumericalFeatureVector> createNumericalFeatureVector(
+static inline std::unique_ptr<NumericalFeatureVectorDecorator> createNumericalFeatureVector(
   uint32 featureIndex, const CscView<const float32>& featureMatrix) {
     CscView<const float32>::index_const_iterator indexIterator = featureMatrix.indices_cbegin(featureIndex);
     CscView<const float32>::index_const_iterator indicesEnd = featureMatrix.indices_cend(featureIndex);
@@ -52,17 +87,16 @@ static inline std::unique_ptr<NumericalFeatureVector> createNumericalFeatureVect
 template<typename FeatureMatrix>
 static inline std::unique_ptr<IFeatureVector> createFeatureVectorInternally(uint32 featureIndex,
                                                                             const FeatureMatrix& featureMatrix) {
-    std::unique_ptr<NumericalFeatureVector> featureVectorPtr =
+    std::unique_ptr<NumericalFeatureVectorDecorator> featureVectorPtr =
       createNumericalFeatureVector(featureIndex, featureMatrix);
+    NumericalFeatureVector& numericalFeatureVector = featureVectorPtr->getView().firstView;
 
     // Sort the feature values...
-    std::sort(featureVectorPtr->begin(), featureVectorPtr->end(), IndexedValue<float32>::CompareValue());
+    std::sort(numericalFeatureVector.begin(), numericalFeatureVector.end(), IndexedValue<float32>::CompareValue());
 
     // Check if all feature values are equal...
-    NumericalFeatureVector::const_iterator iterator = featureVectorPtr->cbegin();
-    uint32 numElements = featureVectorPtr->getNumElements();
-    float32 minValue = iterator[0].value;
-    float32 maxValue = iterator[numElements - 1].value;
+    float32 minValue = numericalFeatureVector[0].value;
+    float32 maxValue = numericalFeatureVector[numericalFeatureVector.numElements - 1].value;
 
     if (isEqual(minValue, maxValue)) {
         return std::make_unique<EqualFeatureVector>();
---
+++ cpp/subprojects/common/src/mlrl/common/input/feature_vector_numerical_allocated.hpp
@@ -0,0 +1,38 @@
+/*
+ * @author Michael Rapp (michael.rapp.ml@gmail.com)
+ */
+#pragma once
+
+#include "mlrl/common/input/feature_vector_numerical.hpp"
+
+/**
+ * Allocates the memory, a `NumericalFeatureVector` provides access to.
+ */
+class MLRLCOMMON_API AllocatedNumericalFeatureVector : public ResizableAllocator<NumericalFeatureVector> {
+    public:
+
+        /**
+         * @param numElements   The number of elements in the vector, excluding those associated with the sparse value
+         * @param sparseValue   The value of sparse elements not explicitly stored in the vector
+         */
+        AllocatedNumericalFeatureVector(uint32 numElements, float32 sparseValue = 0)
+            : ResizableAllocator<NumericalFeatureVector>(numElements) {
+            NumericalFeatureVector::sparseValue = sparseValue;
+        }
+
+        /**
+         * @param other A reference to an object of type `AllocatedNumericalFeatureVector` that should be copied
+         */
+        AllocatedNumericalFeatureVector(const AllocatedNumericalFeatureVector& other)
+            : ResizableAllocator<NumericalFeatureVector>(other) {
+            throw std::runtime_error("Objects of type AllocatedNumericalFeatureVector cannot be copied");
+        }
+
+        /**
+         * @param other A reference to an object of type `AllocatedNumericalFeatureVector` that should be moved
+         */
+        AllocatedNumericalFeatureVector(AllocatedNumericalFeatureVector&& other)
+            : ResizableAllocator<NumericalFeatureVector>(std::move(other)) {}
+
+        virtual ~AllocatedNumericalFeatureVector() override {}
+};
