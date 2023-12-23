a4e33903efab5322d5e18e13152352143454809d
Michael Rapp <michael.rapp90@googlemail.com>
2023-12-23 18:11:37 +0100

    Remove function "getNumElements" from class EqualFeatureVector.

--- cpp/subprojects/common/include/mlrl/common/input/feature_vector_equal.hpp
+++ cpp/subprojects/common/include/mlrl/common/input/feature_vector_equal.hpp
@@ -12,8 +12,6 @@
 class EqualFeatureVector final : public IFeatureVector {
     public:
 
-        uint32 getNumElements() const;
-
         std::unique_ptr<IFeatureVector> createFilteredFeatureVector(std::unique_ptr<IFeatureVector>& existing,
                                                                     uint32 start, uint32 end) const override;
 
