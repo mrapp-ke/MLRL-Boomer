bf0ec24151514a6b0b11ba2c1ae273dd98e7119c
Michael Rapp <michael.rapp90@googlemail.com>
2023-12-23 17:02:04 +0100

    Add typedef MissingFeatureVector and AllocatedMissingFeatureVector.

---
+++ cpp/subprojects/common/include/mlrl/common/input/feature_vector_missing.hpp
@@ -0,0 +1,16 @@
+/*
+ * @author Michael Rapp (michael.rapp.ml@gmail.com)
+ */
+#pragma once
+
+#include "mlrl/common/data/view_vector_dok_binary.hpp"
+
+/**
+ * A view that provides access to the indices of training examples with missing values for a certain feature.
+ */
+typedef BinaryDokVector MissingFeatureVector;
+
+/**
+ * Allocates the memory, a `MissingFeatureVector` provides access to.
+ */
+typedef AllocatedBinaryDokVector AllocatedMissingFeatureVector;
