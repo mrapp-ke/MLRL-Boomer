7068421dd8d11955c4d72af45015eecf6978ceda
Michael Rapp <michael.rapp90@googlemail.com>
2024-01-05 01:41:07 +0100

    The "createFilteredFeatureVector" functions of the class EqualFeatureVector now throw an std::runtime_error.

--- cpp/subprojects/common/src/mlrl/common/input/feature_vector_equal.cpp
+++ cpp/subprojects/common/src/mlrl/common/input/feature_vector_equal.cpp
@@ -1,20 +1,11 @@
 #include "mlrl/common/input/feature_vector_equal.hpp"
 
-static inline std::unique_ptr<IFeatureVector> createFilteredFeatureVectorInternally(
-  std::unique_ptr<IFeatureVector>& existing) {
-    if (dynamic_cast<EqualFeatureVector*>(existing.get()) != nullptr) {
-        return std::move(existing);
-    }
-
-    return std::make_unique<EqualFeatureVector>();
-}
-
 std::unique_ptr<IFeatureVector> EqualFeatureVector::createFilteredFeatureVector(
   std::unique_ptr<IFeatureVector>& existing, uint32 start, uint32 end) const {
-    return createFilteredFeatureVectorInternally(existing);
+    throw std::runtime_error("Function EqualFeatureVector::createFilteredFeatureVector should never be called");
 }
 
 std::unique_ptr<IFeatureVector> EqualFeatureVector::createFilteredFeatureVector(
   std::unique_ptr<IFeatureVector>& existing, const CoverageMask& coverageMask) const {
-    return createFilteredFeatureVectorInternally(existing);
+    throw std::runtime_error("Function EqualFeatureVector::createFilteredFeatureVector should never be called");
 }
