75b2366d02bf543da65020e5cb8a42d052c385e3
Michael Rapp <michael.rapp90@googlemail.com>
2024-01-07 00:58:09 +0100

    Add function "createFilteredFeatureVectorDecorator" to file feature_vector_decorator.hpp.

--- cpp/subprojects/common/src/mlrl/common/input/feature_vector_decorator.hpp
+++ cpp/subprojects/common/src/mlrl/common/input/feature_vector_decorator.hpp
@@ -7,6 +7,49 @@
 #include "mlrl/common/input/feature_vector.hpp"
 #include "mlrl/common/input/feature_vector_missing.hpp"
 
+template<typename Decorator>
+static inline std::unique_ptr<Decorator> createFilteredFeatureVectorDecorator(const Decorator& decorator,
+                                                                              std::unique_ptr<IFeatureVector>& existing,
+                                                                              const CoverageMask& coverageMask) {
+    std::unique_ptr<Decorator> filteredDecoratorPtr;
+    Decorator* existingDecorator = dynamic_cast<Decorator*>(existing.get());
+
+    if (existingDecorator) {
+        // Reuse the existing feature vector...
+        existing.release();
+        filteredDecoratorPtr = std::unique_ptr<Decorator>(existingDecorator);
+
+        // Filter the indices of examples with missing feature values...
+        MissingFeatureVector& missingFeatureVector = filteredDecoratorPtr->getView().secondView;
+
+        for (auto it = missingFeatureVector.indices_cbegin(); it != missingFeatureVector.indices_cend();) {
+            uint32 index = *it;
+            it++;  // Iterator must be incremented before call to `MissingFeatureVector::set` invalidates it
+
+            if (!coverageMask.isCovered(index)) {
+                missingFeatureVector.set(index, false);
+            }
+        }
+    } else {
+        // Create a new feature vector...
+        filteredDecoratorPtr = std::make_unique<Decorator>(decorator);
+
+        // Add the indices of examples with missing feature values...
+        MissingFeatureVector& missingFeatureVector = filteredDecoratorPtr->getView().secondView;
+
+        for (auto it = decorator.getView().secondView.indices_cbegin();
+             it != decorator.getView().secondView.indices_cend(); it++) {
+            uint32 index = *it;
+
+            if (coverageMask.isCovered(index)) {
+                missingFeatureVector.set(index, true);
+            }
+        }
+    }
+
+    return filteredDecoratorPtr;
+}
+
 /**
  * An abstract base class for all decorators that provide access to the values and indices of training examples stored
  * in a feature vector.
