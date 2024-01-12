b3cad3ddf474c0018ab792f88bddc15d5414dfc8
Michael Rapp <michael.rapp90@googlemail.com>
2024-01-13 00:30:10 +0100

    Add class RuleRefinementSearch.

---
+++ cpp/subprojects/common/src/mlrl/common/rule_refinement/rule_refinement_search.cpp
@@ -0,0 +1,45 @@
+#include "mlrl/common/rule_refinement/rule_refinement_search.hpp"
+
+void RuleRefinementSearch::searchForNumericalRefinement(const NumericalFeatureVector& featureVector,
+                                                        SingleRefinementComparator& comparator,
+                                                        uint32 minCoverage) const {
+    // TODO Implement
+}
+
+void RuleRefinementSearch::searchForNumericalRefinement(const NumericalFeatureVector& featureVector,
+                                                        FixedRefinementComparator& comparator,
+                                                        uint32 minCoverage) const {
+    // TODO Implement
+}
+
+void RuleRefinementSearch::searchForNominalRefinement(const NominalFeatureVector& featureVector,
+                                                      SingleRefinementComparator& comparator,
+                                                      uint32 minCoverage) const {
+    // TODO Implement
+}
+
+void RuleRefinementSearch::searchForNominalRefinement(const NominalFeatureVector& featureVector,
+                                                      FixedRefinementComparator& comparator, uint32 minCoverage) const {
+    // TODO Implement
+}
+
+void RuleRefinementSearch::searchForBinaryRefinement(const BinaryFeatureVector& featureVector,
+                                                     SingleRefinementComparator& comparator, uint32 minCoverage) const {
+    // TODO Implement
+}
+
+void RuleRefinementSearch::searchForBinaryRefinement(const BinaryFeatureVector& featureVector,
+                                                     FixedRefinementComparator& comparator, uint32 minCoverage) const {
+    // TODO Implement
+}
+
+void RuleRefinementSearch::searchForOrdinalRefinement(const OrdinalFeatureVector& featureVector,
+                                                      SingleRefinementComparator& comparator,
+                                                      uint32 minCoverage) const {
+    // TODO Implement
+}
+
+void RuleRefinementSearch::searchForOrdinalRefinement(const OrdinalFeatureVector& featureVector,
+                                                      FixedRefinementComparator& comparator, uint32 minCoverage) const {
+    // TODO Implement
+}
