eb4752035c210fc640d26732e79cdd6adff00320
Michael Rapp <michael.rapp90@googlemail.com>
2024-01-31 00:13:54 +0100

    The class ExactThresholds does now expect an object of type IFeatureBinning to be provided as a constructor argument.

--- cpp/subprojects/common/src/mlrl/common/binning/feature_binning_no.cpp
+++ cpp/subprojects/common/src/mlrl/common/binning/feature_binning_no.cpp
@@ -2,11 +2,22 @@
 
 #include "mlrl/common/thresholds/thresholds_exact.hpp"
 
+/**
+ * Allows to create instances of the type `IFeatureBinning` that do not actualy perform any feature binning.
+ */
+class NoFeatureBinningFactory final : public IFeatureBinningFactory {
+    public:
+
+        std::unique_ptr<IFeatureBinning> create() const override {
+            return nullptr;
+        }
+};
+
 NoFeatureBinningConfig::NoFeatureBinningConfig(const std::unique_ptr<IMultiThreadingConfig>& multiThreadingConfigPtr)
     : multiThreadingConfigPtr_(multiThreadingConfigPtr) {}
 
 std::unique_ptr<IThresholdsFactory> NoFeatureBinningConfig::createThresholdsFactory(
   const IFeatureMatrix& featureMatrix, const ILabelMatrix& labelMatrix) const {
     uint32 numThreads = multiThreadingConfigPtr_->getNumThreads(featureMatrix, labelMatrix.getNumLabels());
-    return std::make_unique<ExactThresholdsFactory>(numThreads);
+    return std::make_unique<ExactThresholdsFactory>(std::make_unique<NoFeatureBinningFactory>(), numThreads);
 }
