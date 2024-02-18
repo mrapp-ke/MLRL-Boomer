a889343b18ca069019deb159c87b7213d203a532
Michael Rapp <michael.rapp90@googlemail.com>
2024-02-19 00:15:41 +0100

    Remove function "createBins" from class IFeatureBinning.

--- cpp/subprojects/common/include/mlrl/common/input/feature_binning.hpp
+++ cpp/subprojects/common/include/mlrl/common/input/feature_binning.hpp
@@ -4,14 +4,9 @@
  */
 #pragma once
 
-#include "mlrl/common/binning/bin_index_vector.hpp"
-#include "mlrl/common/binning/threshold_vector.hpp"
 #include "mlrl/common/input/feature_matrix.hpp"
 #include "mlrl/common/input/feature_type.hpp"
-#include "mlrl/common/input/feature_vector.hpp"
 #include "mlrl/common/input/label_matrix.hpp"
-
-#include <memory>
 
 /**
  * Defines an interface for methods that assign feature values to bins.
@@ -19,41 +14,7 @@
 class IFeatureBinning : public IFeatureType {
     public:
 
-        /**
-         * The result that is returned by a binning method. It contains an unique pointer to a vector that stores the
-         * thresholds that result from the boundaries of the bins, as well as to a vector that stores the indices of the
-         * bins, individual values have been assigned to.
-         */
-        // TODO Remove
-        struct Result final {
-            public:
-
-                /**
-                 * An unique pointer to an object of type `ThresholdVector` that provides access to the thresholds that
-                 * result from the boundaries of the bins.
-                 */
-                std::unique_ptr<ThresholdVector> thresholdVectorPtr;
-
-                /**
-                 * An unique pointer to an object of type `IBinIndexVector` that provides access to the indices of the
-                 * bins, individual values have been assigned to.
-                 */
-                std::unique_ptr<IBinIndexVector> binIndicesPtr;
-        };
-
         virtual ~IFeatureBinning() override {}
-
-        /**
-         * Assigns the values in a given `FeatureVector` to bins.
-         *
-         * @param featureVector A reference to an object of type `FeatureVector` whose values should be assigned to bins
-         * @param numExamples   The total number of available training examples
-         * @return              An object of type `Result` that contains a vector, which stores thresholds that result
-         *                      from the boundaries between the bins, as well as a vector that stores the indices of the
-         *                      bins, individual values have been assigned to
-         */
-        // TODO Remove
-        virtual Result createBins(FeatureVector& featureVector, uint32 numExamples) const = 0;
 };
 
 /**
--- cpp/subprojects/common/src/mlrl/common/input/feature_binning_equal_width.cpp
+++ cpp/subprojects/common/src/mlrl/common/input/feature_binning_equal_width.cpp
@@ -1,17 +1,11 @@
 #include "mlrl/common/input/feature_binning_equal_width.hpp"
 
-#include "feature_binning_nominal.hpp"
 #include "feature_type_numerical_common.hpp"
 #include "feature_vector_decorator_binned.hpp"
-#include "mlrl/common/binning/bin_index_vector_dense.hpp"
-#include "mlrl/common/binning/bin_index_vector_dok.hpp"
 #include "mlrl/common/data/array.hpp"
 #include "mlrl/common/data/tuple.hpp"
 #include "mlrl/common/util/math.hpp"
 #include "mlrl/common/util/validation.hpp"
-
-#include <tuple>
-#include <unordered_set>
 
 static inline Tuple<float32> getMinAndMaxFeatureValue(const NumericalFeatureVector& numericalFeatureVector) {
     uint32 numElements = numericalFeatureVector.numElements;
@@ -129,55 +123,6 @@ static inline std::unique_ptr<IFeatureVector> createFeatureVectorInternally(
     return std::make_unique<EqualFeatureVector>();
 }
 
-static inline std::tuple<uint32, float32, float32> preprocess(const FeatureVector& featureVector, bool sparse,
-                                                              float32 binRatio, uint32 minBins, uint32 maxBins) {
-    std::tuple<uint32, float32, float32> result;
-    uint32 numElements = featureVector.getNumElements();
-
-    if (numElements > 0) {
-        FeatureVector::const_iterator featureIterator = featureVector.cbegin();
-        float32 minValue;
-        uint32 i;
-
-        if (sparse) {
-            minValue = 0;
-            i = 0;
-        } else {
-            minValue = featureIterator[0].value;
-            i = 1;
-        }
-
-        float32 maxValue = minValue;
-        uint32 numDistinctValues = 1;
-        std::unordered_set<float32> distinctValues;
-
-        for (; i < numElements; i++) {
-            float32 currentValue = featureIterator[i].value;
-
-            if ((!sparse || currentValue != 0) && distinctValues.insert(currentValue).second) {
-                numDistinctValues++;
-
-                if (currentValue < minValue) {
-                    minValue = currentValue;
-                }
-
-                if (currentValue > maxValue) {
-                    maxValue = currentValue;
-                }
-            }
-        }
-
-        std::get<0>(result) =
-          numDistinctValues > 1 ? calculateBoundedFraction(numDistinctValues, binRatio, minBins, maxBins) : 0;
-        std::get<1>(result) = minValue;
-        std::get<2>(result) = maxValue;
-    } else {
-        std::get<0>(result) = 0;
-    }
-
-    return result;
-}
-
 /**
  * An implementation of the type `IFeatureBinning` that assigns numerical feature values to bins, such that each bin
  * contains values from equally sized value ranges.
@@ -202,88 +147,6 @@ class EqualWidthFeatureBinning final : public IFeatureBinning {
          */
         EqualWidthFeatureBinning(float32 binRatio, uint32 minBins, uint32 maxBins)
             : binRatio_(binRatio), minBins_(minBins), maxBins_(maxBins) {}
-
-        Result createBins(FeatureVector& featureVector, uint32 numExamples) const override {
-            Result result;
-            uint32 numElements = featureVector.getNumElements();
-            bool sparse = numElements < numExamples;
-            std::tuple<uint32, float32, float32> tuple =
-              preprocess(featureVector, sparse, binRatio_, minBins_, maxBins_);
-            uint32 numBins = std::get<0>(tuple);
-            result.thresholdVectorPtr = std::make_unique<ThresholdVector>(featureVector, numBins, true);
-
-            if (sparse) {
-                result.binIndicesPtr = std::make_unique<DokBinIndexVector>();
-            } else {
-                result.binIndicesPtr = std::make_unique<DenseBinIndexVector>(numElements);
-            }
-
-            if (numBins > 0) {
-                IBinIndexVector& binIndices = *result.binIndicesPtr;
-                ThresholdVector& thresholdVector = *result.thresholdVectorPtr;
-                FeatureVector::const_iterator featureIterator = featureVector.cbegin();
-                ThresholdVector::iterator thresholdIterator = thresholdVector.begin();
-                float32 min = std::get<1>(tuple);
-                float32 max = std::get<2>(tuple);
-                float32 width = (max - min) / numBins;
-                uint32 sparseBinIndex;
-
-                // If there are any sparse values, identify the bin they belong to...
-                if (sparse) {
-                    sparseBinIndex = getBinIndex(0, min, width, numBins);
-                    thresholdIterator[sparseBinIndex] = 1;
-                    thresholdVector.setSparseBinIndex(sparseBinIndex);
-                } else {
-                    sparseBinIndex = numBins;
-                }
-
-                // Iterate all non-sparse feature values and identify the bins they belong to...
-                for (uint32 i = 0; i < numElements; i++) {
-                    float32 currentValue = featureIterator[i].value;
-
-                    if (!sparse || currentValue != 0) {
-                        uint32 binIndex = getBinIndex(currentValue, min, width, numBins);
-
-                        if (binIndex != sparseBinIndex) {
-                            thresholdIterator[binIndex] = 1;
-                            binIndices.setBinIndex(featureIterator[i].index, binIndex);
-                        }
-                    }
-                }
-
-                // Remove empty bins and calculate thresholds...
-                Array<uint32> mapping(numBins);
-                uint32 n = 0;
-
-                for (uint32 i = 0; i < numBins; i++) {
-                    mapping[i] = n;
-
-                    if (thresholdIterator[i] > 0) {
-                        thresholdIterator[n] = min + ((i + 1) * width);
-                        n++;
-                    }
-                }
-
-                thresholdVector.setNumElements(n, true);
-
-                // Adjust bin indices...
-                DokBinIndexVector* dokBinIndices = dynamic_cast<DokBinIndexVector*>(&binIndices);
-
-                if (dokBinIndices) {
-                    for (auto it = dokBinIndices->begin(); it != dokBinIndices->end(); it++) {
-                        uint32 binIndex = it->second;
-                        it->second = mapping[binIndex];
-                    }
-                } else {
-                    for (uint32 i = 0; i < numElements; i++) {
-                        uint32 binIndex = binIndices.getBinIndex(i);
-                        binIndices.setBinIndex(i, mapping[binIndex]);
-                    }
-                }
-            }
-
-            return result;
-        }
 
         bool isOrdinal() const override {
             return false;
