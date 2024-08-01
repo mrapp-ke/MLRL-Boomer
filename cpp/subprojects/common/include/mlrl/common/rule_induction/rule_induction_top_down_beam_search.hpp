9daec5d987bdaaac4d0997ec7567215cb18ca855
Michael Rapp <michael.rapp90@googlemail.com>
2024-08-01 23:39:48 +0200

    [Bot] Remove redundant includes.

--- cpp/subprojects/common/include/mlrl/common/rule_induction/rule_induction_top_down_beam_search.hpp
+++ cpp/subprojects/common/include/mlrl/common/rule_induction/rule_induction_top_down_beam_search.hpp
@@ -9,8 +9,6 @@
 
 #include <memory>
 
-#include <memory>
-
 /**
  * Defines an interface for all classes that allow to configure an algorithm for the induction of individual rules that
  * uses a top-down beam search.
