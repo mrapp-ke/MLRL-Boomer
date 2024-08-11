c72b61de12453f872d9227bcc01d83be74b8215f
Michael Rapp <michael.rapp90@googlemail.com>
2024-08-11 16:32:20 +0200

    Add class IStatisticsUpdate.

--- cpp/subprojects/common/include/mlrl/common/statistics/statistics.hpp
+++ cpp/subprojects/common/include/mlrl/common/statistics/statistics.hpp
@@ -15,6 +15,32 @@
 #include <memory>
 
 /**
+ * Defines an interface for all classes that allow updating statistics.
+ */
+class IStatisticsUpdate {
+    public:
+
+        virtual ~IStatisticsUpdate() {}
+
+        /**
+         * Updates a specific statistic.
+         *
+         * This function must be called for each statistic that is covered by a new rule before learning the
+         * next rule.
+         *
+         * @param statisticIndex The index of the statistic that should be updated
+         */
+        virtual void applyPrediction(uint32 statisticIndex) = 0;
+
+        /**
+         * Reverts a specific statistic that has previously been updated via the function `applyPrediction`.
+         *
+         * @param statisticIndex The index of the statistic that should be updated
+         */
+        virtual void revertPrediction(uint32 statisticIndex) = 0;
+};
+
+/**
  * Defines an interface for all classes that provide access to statistics about the quality of predictions for training
  * examples, which serve as the basis for learning a new rule or refining an existing one.
  */
