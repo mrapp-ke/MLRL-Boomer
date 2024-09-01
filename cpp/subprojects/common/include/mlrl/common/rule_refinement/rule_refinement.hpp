6f2d948a58ec6fe5b5bd65faaa102b478a00907b
Michael Rapp <michael.rapp90@googlemail.com>
2024-09-01 18:12:22 +0200

    Change arguments and return values of the "findRefinement" functions of the class IRuleRefinement.

--- cpp/subprojects/common/include/mlrl/common/rule_refinement/rule_refinement.hpp
+++ cpp/subprojects/common/include/mlrl/common/rule_refinement/rule_refinement.hpp
@@ -3,6 +3,8 @@
  */
 #pragma once
 
+#include "mlrl/common/indices/index_vector.hpp"
+#include "mlrl/common/rule_refinement/feature_subspace.hpp"
 #include "mlrl/common/rule_refinement/refinement_comparator_fixed.hpp"
 #include "mlrl/common/rule_refinement/refinement_comparator_single.hpp"
 
@@ -17,18 +19,36 @@ class IRuleRefinement {
         /**
          * Finds the best refinement of an existing rule.
          *
-         * @param comparator    A reference to an object of type `SingleRefinementComparator` that should be used for
-         *                      comparing potential refinements
-         * @param minCoverage   The minimum number of examples that must be covered by the refinement
+         * @param comparator        A reference to an object of type `SingleRefinementComparator` that should be used
+         *                          for comparing potential refinements
+         * @param featureSubspace   A reference to an object of type `IFeatureSubspace` that should be used to search
+         *                          for the potential refinements
+         * @param featureIndices    A reference to an object of type `IIndexVector` that provides access to the indices
+         *                          of the features that should be considered
+         * @param outputIndices     A reference to an object of type `IIndexVector` that provides access to the indices
+         *                          of the outputs for which the refinement(s) may predict
+         * @param minCoverage       The minimum number of examples that must be covered by the refinements
+         * @return                  True, if at least one refinement has been found, false otherwise
          */
-        virtual void findRefinement(SingleRefinementComparator& comparator, uint32 minCoverage) const = 0;
+        virtual bool findRefinement(SingleRefinementComparator& comparator, IFeatureSubspace& featureSubspace,
+                                    const IIndexVector& featureIndices, const IIndexVector& outputIndices,
+                                    uint32 minCoverage) const = 0;
 
         /**
          * Finds the best refinements of an existing rule.
          *
-         * @param comparator    A reference to an object of type `MultiRefinementComparator` that should be used for
-         *                      comparing potential refinements
-         * @param minCoverage   The minimum number of examples that must be covered by the refinements
+         * @param comparator        A reference to an object of type `FixedRefinementComparator` that should be used for
+         *                          comparing potential refinements
+         * @param featureSubspace   A reference to an object of type `IFeatureSubspace` that should be used to search
+         *                          for the potential refinements
+         * @param featureIndices    A reference to an object of type `IIndexVector` that provides access to the indices
+         *                          of the features that should be considered
+         * @param outputIndices     A reference to an object of type `IIndexVector` that provides access to the indices
+         *                          of the outputs for which the refinement(s) may predict
+         * @param minCoverage       The minimum number of examples that must be covered by the refinements
+         * @return                  True, if at least one refinement has been found, false otherwise
          */
-        virtual void findRefinement(FixedRefinementComparator& comparator, uint32 minCoverage) const = 0;
+        virtual bool findRefinement(FixedRefinementComparator& comparator, IFeatureSubspace& featureSubspace,
+                                    const IIndexVector& featureIndices, const IIndexVector& outputIndices,
+                                    uint32 minCoverage) const = 0;
 };
