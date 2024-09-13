762a9826f00724ef4af7eb4c76c9c5d7a5e93894
Michael Rapp <michael.rapp90@googlemail.com>
2024-09-13 16:01:54 +0200

    Add class CompressedVector.

---
+++ cpp/subprojects/common/include/mlrl/common/data/view_vector_compressed.hpp
@@ -0,0 +1,85 @@
+/*
+ * @author Michael Rapp (michael.rapp.ml@gmail.com)
+ */
+#pragma once
+
+#include "mlrl/common/data/view_compressed.hpp"
+
+/**
+ * A view that provides access to indices that have been grouped into bins.
+ */
+class MLRLCOMMON_API CompressedVector : public CompressedView {
+    public:
+
+        /**
+         * @param indices   A pointer to an array of type `uint32`, shape `(numIndices)` that stores indices
+         * @param indptr    A pointer to an array that stores the indices of the first element in `indices` that
+         *                  corresponds to a certain bin
+         */
+        CompressedVector(uint32* indices, uint32* indptr) : CompressedView(indices, indptr) {}
+
+        /**
+         * @param other A reference to an object of type `CompressedVector` that should be copied
+         */
+        CompressedVector(const CompressedVector& other) : CompressedVector(other.indices, other.indptr) {}
+
+        /**
+         * @param other A reference to an object of type `CompressedVector` that should be moved
+         */
+        CompressedVector(CompressedVector&& other) : CompressedVector(other.indices, other.indptr) {}
+
+        virtual ~CompressedVector() override {}
+
+        /**
+         * An iterator that provides read-only access to the indices.
+         */
+        typedef const uint32* index_const_iterator;
+
+        /**
+         * An iterator that provides access to the indices and allows to modify them.
+         */
+        typedef uint32* index_iterator;
+
+        /**
+         * Returns an `index_const_iterator` to the beginning of the indices that are associated with a specific bin.
+         *
+         * @param index The index of the nominal value
+         * @return      An `index_const_iterator` to the beginning
+         */
+        index_const_iterator indices_cbegin(uint32 index) const {
+            return &indices[indptr[index]];
+        }
+
+        /**
+         * Returns an `index_const_iterator` to the end of the indices of the examples that are associated with a
+         * specific bin.
+         *
+         * @param index The index of the bin
+         * @return      An `index_const_iterator` to the end
+         */
+        index_const_iterator indices_cend(uint32 index) const {
+            return &indices[indptr[index + 1]];
+        }
+
+        /**
+         * Returns an `index_iterator` to the beginning of the indices of the examples that are associated with a
+         * specific bin.
+         *
+         * @param index The index of the bin
+         * @return      An `index_iterator` to the beginning
+         */
+        index_iterator indices_begin(uint32 index) {
+            return &indices[indptr[index]];
+        }
+
+        /**
+         * Returns an `index_iterator` to the end of the indices of the examples that are associated with a specific
+         * bin.
+         *
+         * @param index The index of the bin
+         * @return      An `index_iterator` to the end
+         */
+        index_iterator indices_end(uint32 index) {
+            return &indices[indptr[index + 1]];
+        }
+};
