46812184268dd0ec2128fdaf7e3ae2b9709cf0e0
Michael Rapp <michael.rapp90@googlemail.com>
2024-11-16 21:12:16 +0100

    Add build target "check_github_actions".

---
+++ scons/github_actions.py
@@ -0,0 +1,12 @@
+"""
+Author: Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides utility functions for checking the project's GitHub workflows for outdated Actions.
+"""
+
+
+def check_github_actions(**_):
+    """
+    Checks the project's GitHub workflows for outdated Actions.
+    """
+    print('Checking for outdated GitHub Actions...')
