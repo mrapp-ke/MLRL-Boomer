58b412e439a569fca379c68f243e1b002e85d3ac
Michael Rapp <michael.rapp90@googlemail.com>
2024-11-21 23:20:27 +0100

    Split file compilation.py into files cpp.py and cython.py.

---
+++ scons/cpp.py
@@ -0,0 +1,42 @@
+"""
+Author: Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides utility functions for compiling C++ code.
+"""
+from build_options import BuildOptions, EnvBuildOption
+from meson import MesonCompile, MesonConfigure, MesonInstall, MesonSetup
+from modules import CPP_MODULE
+
+BUILD_OPTIONS = BuildOptions() \
+        .add(EnvBuildOption(name='subprojects')) \
+        .add(EnvBuildOption(name='test_support', subpackage='common')) \
+        .add(EnvBuildOption(name='multi_threading_support', subpackage='common')) \
+        .add(EnvBuildOption(name='gpu_support', subpackage='common'))
+
+
+def setup_cpp(**_):
+    """
+    Sets up the build system for compiling the C++ code.
+    """
+    MesonSetup(build_directory=CPP_MODULE.build_dir,
+               source_directory=CPP_MODULE.root_dir,
+               build_options=BUILD_OPTIONS) \
+        .add_dependencies('ninja') \
+        .run()
+
+
+def compile_cpp(**_):
+    """
+    Compiles the C++ code.
+    """
+    MesonConfigure(CPP_MODULE.build_dir, BUILD_OPTIONS).run()
+    print('Compiling C++ code...')
+    MesonCompile(CPP_MODULE.build_dir).run()
+
+
+def install_cpp(**_):
+    """
+    Installs shared libraries into the source tree.
+    """
+    print('Installing shared libraries into source tree...')
+    MesonInstall(CPP_MODULE.build_dir).run()
---
+++ scons/cython.py
@@ -0,0 +1,39 @@
+"""
+Author: Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides utility functions for compiling Cython code.
+"""
+from build_options import BuildOptions, EnvBuildOption
+from meson import MesonCompile, MesonConfigure, MesonInstall, MesonSetup
+from modules import PYTHON_MODULE
+
+BUILD_OPTIONS = BuildOptions() \
+        .add(EnvBuildOption(name='subprojects'))
+
+
+def setup_cython(**_):
+    """
+    Sets up the build system for compiling the Cython code.
+    """
+    MesonSetup(build_directory=PYTHON_MODULE.build_dir,
+               source_directory=PYTHON_MODULE.root_dir,
+               build_options=BUILD_OPTIONS) \
+        .add_dependencies('cython') \
+        .run()
+
+
+def compile_cython(**_):
+    """
+    Compiles the Cython code.
+    """
+    MesonConfigure(PYTHON_MODULE.build_dir, BUILD_OPTIONS)
+    print('Compiling Cython code...')
+    MesonCompile(PYTHON_MODULE.build_dir).run()
+
+
+def install_cython(**_):
+    """
+    Installs extension modules into the source tree.
+    """
+    print('Installing extension modules into source tree...')
+    MesonInstall(PYTHON_MODULE.build_dir).run()
