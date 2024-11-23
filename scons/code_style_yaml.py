8feb7bdb8e29b6977ecc5a1410e0e8113f859339
Michael Rapp <michael.rapp90@googlemail.com>
2024-11-23 01:36:00 +0100

    Move class YamlFix into a separate file.

--- scons/code_style_yaml.py
+++ scons/code_style_yaml.py
@@ -3,35 +3,9 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 
 Provides utility functions for checking and enforcing code style definitions.
 """
-from glob import glob
-from os import path
-
-from util.run import Program
+from code_style.yaml.yamlfix import YamlFix
 
 YAML_DIRS = [('.', False), ('.github', True)]
-
-
-class Yamlfix(Program):
-    """
-    Allows to run the external program "yamlfix".
-    """
-
-    def __init__(self, directory: str, recursive: bool = False, enforce_changes: bool = False):
-        """
-        :param directory:       The path to the directory, the program should be applied to
-        :param recursive:       True, if the program should be applied to subdirectories, False otherwise
-        :param enforce_changes: True, if changes should be applied to files, False otherwise
-        """
-        super().__init__('yamlfix', '--config-file', '.yamlfix.toml')
-        self.add_conditional_arguments(not enforce_changes, '--check')
-        glob_path = path.join(directory, '**', '*') if recursive else path.join(directory, '*')
-        glob_path_hidden = path.join(directory, '**', '.*') if recursive else path.join(directory, '.*')
-        yaml_files = [
-            file for file in glob(glob_path) + glob(glob_path_hidden)
-            if path.basename(file).endswith('.yml') or path.basename(file).endswith('.yaml')
-        ]
-        self.add_arguments(yaml_files)
-        self.print_arguments(True)
 
 
 def check_yaml_code_style(**_):
@@ -40,7 +14,7 @@ def check_yaml_code_style(**_):
     """
     for directory, recursive in YAML_DIRS:
         print('Checking YAML files in the directory "' + directory + '"...')
-        Yamlfix(directory, recursive=recursive).run()
+        YamlFix(directory, recursive=recursive).run()
 
 
 def enforce_yaml_code_style(**_):
@@ -49,4 +23,4 @@ def enforce_yaml_code_style(**_):
     """
     for directory, recursive in YAML_DIRS:
         print('Formatting YAML files in the directory "' + directory + '"...')
-        Yamlfix(directory, recursive=recursive, enforce_changes=True).run()
+        YamlFix(directory, recursive=recursive, enforce_changes=True).run()
