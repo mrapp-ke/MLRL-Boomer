8cccf144a4c845417719f337845b6d9797c1a1a3
Michael Rapp <michael.rapp90@googlemail.com>
2024-11-24 21:25:54 +0100

    Allow to set multiple functions or runnables via a PhonyTarget.Builder.

--- scons/versioning/__init__.py
+++ scons/versioning/__init__.py
@@ -8,18 +8,18 @@ from versioning.versioning import apply_development_version, increment_developme
     increment_minor_version, increment_patch_version, print_current_version, reset_development_version
 
 TARGETS = TargetBuilder() \
-    .add_phony_target('increment_development_version').set_function(increment_development_version) \
-    .add_phony_target('reset_development_version').set_function(reset_development_version) \
-    .add_phony_target('apply_development_version').set_function(apply_development_version) \
-    .add_phony_target('increment_patch_version').set_function(increment_patch_version) \
-    .add_phony_target('increment_minor_version').set_function(increment_minor_version) \
-    .add_phony_target('increment_major_version').set_function(increment_major_version) \
-    .add_phony_target('validate_changelog_bugfix').set_function(validate_changelog_bugfix) \
-    .add_phony_target('validate_changelog_feature').set_function(validate_changelog_feature) \
-    .add_phony_target('validate_changelog_main').set_function(validate_changelog_main) \
-    .add_phony_target('update_changelog_bugfix').set_function(update_changelog_bugfix) \
-    .add_phony_target('update_changelog_feature').set_function(update_changelog_feature) \
-    .add_phony_target('update_changelog_main').set_function(update_changelog_main) \
-    .add_phony_target('print_version').set_function(print_current_version) \
-    .add_phony_target('print_latest_changelog').set_function(print_latest_changelog) \
+    .add_phony_target('increment_development_version').set_functions(increment_development_version) \
+    .add_phony_target('reset_development_version').set_functions(reset_development_version) \
+    .add_phony_target('apply_development_version').set_functions(apply_development_version) \
+    .add_phony_target('increment_patch_version').set_functions(increment_patch_version) \
+    .add_phony_target('increment_minor_version').set_functions(increment_minor_version) \
+    .add_phony_target('increment_major_version').set_functions(increment_major_version) \
+    .add_phony_target('validate_changelog_bugfix').set_functions(validate_changelog_bugfix) \
+    .add_phony_target('validate_changelog_feature').set_functions(validate_changelog_feature) \
+    .add_phony_target('validate_changelog_main').set_functions(validate_changelog_main) \
+    .add_phony_target('update_changelog_bugfix').set_functions(update_changelog_bugfix) \
+    .add_phony_target('update_changelog_feature').set_functions(update_changelog_feature) \
+    .add_phony_target('update_changelog_main').set_functions(update_changelog_main) \
+    .add_phony_target('print_version').set_functions(print_current_version) \
+    .add_phony_target('print_latest_changelog').set_functions(print_latest_changelog) \
     .build()
