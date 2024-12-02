e994227cd28700fc5d659bb372ef1bd1a7ba551e
Michael Rapp <michael.rapp90@googlemail.com>
2024-12-02 18:30:39 +0100

    Dynamically register targets for cleaning up output files.

--- scons/util/targets.py
+++ scons/util/targets.py
@@ -6,13 +6,15 @@ Provides base classes for defining individual targets of the build process.
 import sys
 
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from functools import reduce
-from typing import Any, Callable, List, Set
+from typing import Any, Callable, Dict, List, Optional, Set
 from uuid import uuid4
 
 from util.modules import ModuleRegistry
 from util.units import BuildUnit
 
+from SCons.Script import COMMAND_LINE_TARGETS
 from SCons.Script.SConscript import SConsEnvironment as Environment
 
 
@@ -20,6 +22,25 @@ class Target(ABC):
     """
     An abstract base class for all targets of the build system.
     """
+
+    @dataclass
+    class Dependency:
+        """
+        A single dependency of a parent target.
+
+        Attributes:
+            target_name:        The name of the target, the parent target depends on
+            clean_dependency:   True, if the output files of the dependency should also be cleaned when cleaning the
+                                output files of the parent target, False otherwise
+        """
+        target_name: str
+        clean_dependency: bool
+
+        def __eq__(self, other: 'Target.Dependency') -> bool:
+            return self.target_name == other.target_name
+
+        def __hash__(self) -> int:
+            return hash(self.target_name)
 
     class Builder(ABC):
         """
@@ -34,40 +55,48 @@ class Target(ABC):
             self.child_builders = []
             self.dependencies = set()
 
-        def depends_on(self, *target_names: str) -> 'Target.Builder':
+        def depends_on(self, *target_names: str, clean_dependencies: bool = False) -> 'Target.Builder':
             """
             Adds on or several targets, this target should depend on.
 
-            :param target_names:    The names of the targets, this target should depend on
-            :return:                The `Target.Builder` itself
+            :param target_names:        The names of the targets, this target should depend on
+            :param clean_dependencies:  True, if output files of the dependencies should also be cleaned when cleaning
+                                        the output files of this target, False otherwise
+            :return:                    The `Target.Builder` itself
             """
-            self.dependencies.update(target_names)
+            for target_name in target_names:
+                self.dependencies.add(Target.Dependency(target_name=target_name, clean_dependency=clean_dependencies))
+
             return self
 
-        def depends_on_build_target(self) -> 'BuildTarget.Builder':
+        def depends_on_build_target(self, clean_dependency: bool = True) -> 'BuildTarget.Builder':
             """
             Creates and returns a `BuildTarget.Builder` that allows to configure a build target, this target should
             depend on.
 
-            :return: The `BuildTarget.Builder` that has been created
+            :param clean_dependency:    True, if output files of the dependency should also be cleaned when cleaning the
+                                        output files of this target, False otherwise
+            :return:                    The `BuildTarget.Builder` that has been created
             """
             target_name = str(uuid4())
             target_builder = BuildTarget.Builder(self, target_name)
-            self.dependencies.add(target_name)
             self.child_builders.append(target_builder)
+            self.depends_on(target_name, clean_dependencies=clean_dependency)
             return target_builder
 
-        def depends_on_phony_target(self) -> 'PhonyTarget.Builder':
+        def depends_on_phony_target(self, clean_dependency: bool = True) -> 'PhonyTarget.Builder':
             """
             Creates and returns a `PhonyTarget.Builder` that allows to configure a phony target, this target should
             depend on.
 
-            :return: The `PhonyTarget.Builder` that has been created
+            :param clean_dependency:    True, if output files of the dependency should also be cleaned when cleaning the
+                                        output files of this target, False otherwise
+            :return:                    The `PhonyTarget.Builder` that has been created
             """
             target_name = str(uuid4())
             target_builder = PhonyTarget.Builder(self, target_name)
             self.child_builders.append(target_builder)
-            self.dependencies.add(target_name)
+            self.depends_on(target_name, clean_dependencies=clean_dependency)
             return target_builder
 
         @abstractmethod
@@ -89,10 +118,10 @@ class Target(ABC):
             return [self._build(build_unit)] + reduce(lambda aggr, builder: aggr + builder.build(build_unit),
                                                       self.child_builders, [])
 
-    def __init__(self, name: str, dependencies: Set[str]):
+    def __init__(self, name: str, dependencies: Set['Target.Dependency']):
         """
         :param name:            The name of the target
-        :param dependencies:    The names of the targets, this target depends on
+        :param dependencies:    The dependencies of the target
         """
         self.name = name
         self.dependencies = dependencies
@@ -100,12 +129,21 @@ class Target(ABC):
     @abstractmethod
     def register(self, environment: Environment, module_registry: ModuleRegistry) -> Any:
         """
-        Must be implemented by subclasses in order to register the target.
+        Must be implemented by subclasses in order to register this target.
 
         :param environment:     The environment, the target should be registered at
         :param module_registry: The `ModuleRegistry` that can be used by the target for looking up modules
         :return:                The scons target that has been created
         """
+
+    def get_clean_files(self, module_registry: ModuleRegistry) -> Optional[List[str]]:
+        """
+        May be overridden by subclasses in order to return the files that should be cleaned up for this target.
+
+        :param module_registry: The `ModuleRegistry` that can be used by the target for looking up modules
+        :return:                A list that contains the files to be cleaned or None, if cleaning is not necessary
+        """
+        return None
 
 
 class BuildTarget(Target):
@@ -136,6 +174,16 @@ class BuildTarget(Target):
             """
             return []
 
+        def get_clean_files(self, modules: ModuleRegistry) -> List[str]:
+            """
+            May be overridden by subclasses in order to return the output files produced by the target that must be
+            cleaned.
+
+            :param modules: A `ModuleRegistry` that can be used by the target for looking up modules
+            :return:        A list that contains the files to be cleaned
+            """
+            return self.get_output_files(modules)
+
     class Builder(Target.Builder):
         """
         A builder that allows to configure and create build targets.
@@ -163,10 +211,11 @@ class BuildTarget(Target):
         def _build(self, build_unit: BuildUnit) -> Target:
             return BuildTarget(self.name, self.dependencies, self.runnables, build_unit)
 
-    def __init__(self, name: str, dependencies: Set[str], runnables: List[Runnable], build_unit: BuildUnit):
+    def __init__(self, name: str, dependencies: Set[Target.Dependency], runnables: List[Runnable],
+                 build_unit: BuildUnit):
         """
         :param name:            The name of the target or None, if the target does not have a name
-        :param dependencies:    The names of the targets, this target depends on
+        :param dependencies:    The dependencies of the target
         :param runnables:       The `BuildTarget.Runnable` to the be run by the target
         :param build_unit:      The `BuildUnit`, the target belongs to
         """
@@ -187,6 +236,9 @@ class BuildTarget(Target):
             return environment.Command(target, None, action=lambda **_: action())
 
         return environment.AlwaysBuild(environment.Alias(self.name, None, action=lambda **_: action()))
+
+    def get_clean_files(self, module_registry: ModuleRegistry) -> Optional[List[str]]:
+        return reduce(lambda aggr, runnable: runnable.get_clean_files(module_registry), self.runnables, [])
 
 
 class PhonyTarget(Target):
@@ -264,10 +316,10 @@ class PhonyTarget(Target):
 
             return PhonyTarget(self.name, self.dependencies, action)
 
-    def __init__(self, name: str, dependencies: Set[str], action: Callable[[ModuleRegistry], None]):
+    def __init__(self, name: str, dependencies: Set[Target.Dependency], action: Callable[[ModuleRegistry], None]):
         """
         :param name:            The name of the target
-        :param dependencies:    The names of the targets, this target depends on
+        :param dependencies:    The dependencies of the target
         :param action:          The action to be executed by the target
         """
         super().__init__(name, dependencies)
@@ -326,6 +378,55 @@ class TargetRegistry:
     Allows to register targets.
     """
 
+    def __register_scons_targets(self) -> Dict[str, Any]:
+        scons_targets_by_name = {}
+
+        for target_name, target in self.targets_by_name.items():
+            scons_targets_by_name[target_name] = target.register(self.environment, self.module_registry)
+
+        return scons_targets_by_name
+
+    def __register_scons_dependencies(self, scons_targets_by_name: Dict[str, Any]):
+        for target_name, target in self.targets_by_name.items():
+            scons_target = scons_targets_by_name[target_name]
+            scons_dependencies = []
+
+            for dependency in target.dependencies:
+                try:
+                    scons_dependencies.append(scons_targets_by_name[dependency.target_name])
+                except KeyError:
+                    print('Dependency "' + dependency.target_name + '" of target "' + target_name
+                          + '" has not been registered')
+                    sys.exit(-1)
+
+            if scons_dependencies:
+                self.environment.Depends(scons_target, scons_dependencies)
+
+    def __get_parent_targets(self, *target_names: str) -> Set[str]:
+        result = set()
+        parent_targets = reduce(lambda aggr, target_name: aggr | self.parent_targets_by_name.get(target_name, set()),
+                                target_names, set())
+
+        if parent_targets:
+            result.update(self.__get_parent_targets(*parent_targets))
+
+        result.update(parent_targets)
+        return result
+
+    def __register_scons_clean_targets(self, scons_targets_by_name: Dict[str, Any]):
+        if self.environment.GetOption('clean'):
+            for target_name, target in self.targets_by_name.items():
+                parent_targets = {target_name} | self.__get_parent_targets(target_name)
+
+                if not COMMAND_LINE_TARGETS or reduce(
+                        lambda aggr, parent_target: aggr or parent_target in COMMAND_LINE_TARGETS, parent_targets,
+                        False):
+                    clean_files = target.get_clean_files(self.module_registry)
+
+                    if clean_files:
+                        clean_targets = [scons_targets_by_name[parent_target] for parent_target in parent_targets]
+                        self.environment.Clean(clean_targets, clean_files)
+
     def __init__(self, module_registry: ModuleRegistry):
         """
         :param module_registry: The `ModuleRegistry` that should be used by targets for looking up modules
@@ -333,6 +434,7 @@ class TargetRegistry:
         self.environment = Environment()
         self.module_registry = module_registry
         self.targets_by_name = {}
+        self.parent_targets_by_name = {}
 
     def add_target(self, target: Target):
         """
@@ -342,28 +444,18 @@ class TargetRegistry:
         """
         self.targets_by_name[target.name] = target
 
+        for dependency in target.dependencies:
+            if dependency.clean_dependency:
+                parent_targets = self.parent_targets_by_name.setdefault(dependency.target_name, set())
+                parent_targets.add(target.name)
+
     def register(self):
         """
         Registers all targets that have previously been added.
         """
-        scons_targets_by_name = {}
-
-        for target_name, target in self.targets_by_name.items():
-            scons_targets_by_name[target_name] = target.register(self.environment, self.module_registry)
-
-        for target_name, target in self.targets_by_name.items():
-            scons_target = scons_targets_by_name[target_name]
-            scons_dependencies = []
-
-            for dependency in target.dependencies:
-                try:
-                    scons_dependencies.append(scons_targets_by_name[dependency])
-                except KeyError:
-                    print('Dependency "' + dependency + '" of target "' + target_name + '" has not been registered')
-                    sys.exit(-1)
-
-            if scons_dependencies:
-                self.environment.Depends(scons_target, scons_dependencies)
+        scons_targets_by_name = self.__register_scons_targets()
+        self.__register_scons_dependencies(scons_targets_by_name)
+        self.__register_scons_clean_targets(scons_targets_by_name)
 
     @property
     def target_names(self) -> Set[str]:
