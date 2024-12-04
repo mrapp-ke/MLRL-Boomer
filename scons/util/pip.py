9f7daa42bc5d66008e75d16f3659073cb3d06f41
Michael Rapp <michael.rapp90@googlemail.com>
2024-12-04 17:38:32 +0100

    Add class PipList.

--- scons/util/pip.py
+++ scons/util/pip.py
@@ -1,7 +1,7 @@
 """
 Author: Michael Rapp (michael.rapp.ml@gmail.com)
 
-Provides utility functions for installing Python packages via pip.
+Provides classes for installing Python packages via pip.
 """
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
@@ -73,25 +73,6 @@ class Requirement:
 
     def __hash__(self) -> int:
         return hash(self.package)
-
-
-@dataclass
-class Dependency:
-    """
-    Provides information about a dependency.
-
-    Attributes:
-        installed:  The version of the dependency that is currently installed
-        latest:     The latest version of the dependency
-    """
-    installed: Requirement
-    latest: Requirement
-
-    def __eq__(self, other: 'Dependency') -> bool:
-        return self.installed == other.installed
-
-    def __hash__(self) -> int:
-        return hash(self.installed)
 
 
 class Requirements(ABC):
@@ -204,18 +185,6 @@ class Pip:
             super().__init__('install', str(requirement), '--upgrade', '--upgrade-strategy', 'eager', '--prefer-binary')
             self.add_conditional_arguments(dry_run, '--dry-run')
 
-    class ListCommand(Command):
-        """
-        Allows to list information about installed packages via the command `pip list`.
-        """
-
-        def __init__(self, outdated: bool = False):
-            """
-            :param outdated: True, if only outdated packages should be listed, False otherwise
-            """
-            super().__init__('list')
-            self.add_conditional_arguments(outdated, '--outdated')
-
     @staticmethod
     def __would_install_requirement(requirement: Requirement, stdout: str) -> bool:
         prefix = 'Would install'
@@ -230,7 +199,7 @@ class Pip:
         return False
 
     @staticmethod
-    def __install_requirement(requirement: Requirement, dry_run: bool = False):
+    def install_requirement(requirement: Requirement, dry_run: bool = False):
         """
         Installs a requirement.
 
@@ -250,7 +219,7 @@ class Pip:
                 else:
                     print(stdout)
         except RuntimeError:
-            Pip.__install_requirement(requirement)
+            Pip.install_requirement(requirement)
 
     def __init__(self, *requirements_files: str):
         """
@@ -281,44 +250,4 @@ class Pip:
         requirements = self.requirements.lookup_requirements(*packages, accept_missing=accept_missing)
 
         for requirement in requirements:
-            self.__install_requirement(requirement, dry_run=True)
-
-    def install_all_packages(self):
-        """
-        Installs all dependencies in the requirements file.
-        """
-        for requirement in self.requirements.requirements:
-            self.__install_requirement(requirement, dry_run=True)
-
-    def list_outdated_dependencies(self) -> Set[Dependency]:
-        stdout = Pip.ListCommand(outdated=True).print_command(False).capture_output()
-        stdout_lines = stdout.strip().split('\n')
-        i = 0
-
-        for line in stdout_lines:
-            i += 1
-
-            if line.startswith('----'):
-                break
-
-        outdated_dependencies = set()
-
-        for line in stdout_lines[i:]:
-            parts = line.split()
-
-            if len(parts) < 3:
-                raise ValueError(
-                    'Output of command "pip list" is expected to be a table with at least three columns, but got:'
-                    + line)
-
-            package = Package(parts[0])
-            requirement = self.requirements.lookup_requirement(package, accept_missing=True)
-
-            if requirement and requirement.version:
-                installed_version = parts[1]
-                latest_version = parts[2]
-                outdated_dependencies.add(
-                    Dependency(installed=Requirement(package, version=installed_version),
-                               latest=Requirement(package, version=latest_version)))
-
-        return outdated_dependencies
+            self.install_requirement(requirement, dry_run=True)
