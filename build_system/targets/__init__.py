42f12f43be0a90091c021ae5309f50c37ff20412
Michael Rapp <michael.rapp90@googlemail.com>
2024-12-13 01:22:42 +0100

    Define default target.

---
+++ build_system/targets/__init__.py
@@ -0,0 +1,8 @@
+"""
+Author: Michael Rapp (michael.rapp.ml@gmail.com)
+
+Defines the build sytem's default target.
+"""
+from targets.packaging import INSTALL_WHEELS
+
+DEFAULT_TARGET = INSTALL_WHEELS
