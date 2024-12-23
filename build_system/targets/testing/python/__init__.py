c2bdab7d474bd7db7b7e855d49843f3f1fa4279b
Michael Rapp <michael.rapp90@googlemail.com>
2024-12-24 00:09:41 +0100

    Move tests.

--- build_system/targets/testing/python/__init__.py
+++ build_system/targets/testing/python/__init__.py
@@ -3,10 +3,12 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 
 Defines targets and modules for testing Python code.
 """
+from functools import reduce
 from os import path
 
 from core.build_unit import BuildUnit
 from core.targets import PhonyTarget, TargetBuilder
+from util.files import FileType
 
 from targets.packaging import INSTALL_WHEELS
 from targets.paths import Project
@@ -27,8 +29,12 @@ MODULES = [
         result_directory=path.join(Project.Python.root_directory, 'tests', Project.Python.build_directory_name,
                                    'test-results'),
     ) for test_directory in {
-        path.dirname(test_file) for test_file in Project.Python.file_search() \
-            .filter_by_substrings(starts_with='test_', ends_with='.py') \
-            .list(path.join(Project.Python.root_directory, 'tests'))
+        path.dirname(test_file) for test_file in reduce(
+            lambda aggr, suffix : aggr + Project.Python.file_search() \
+                .filter_by_substrings(starts_with='test_', ends_with='.' + suffix) \
+                .list(path.join(Project.Python.root_directory, 'tests')),
+            FileType.python().suffixes,
+            [],
+        )
     }
 ]
