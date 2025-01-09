7fbc65fb2f53e09dff976be60a4811a76489235b
Michael Rapp <michael.rapp90@googlemail.com>
2025-01-09 16:10:14 +0100

    Register targets and modules for formatting TOML files.

--- build_system/targets/code_style/__init__.py
+++ build_system/targets/code_style/__init__.py
@@ -9,13 +9,14 @@ from core.targets import TargetBuilder
 from targets.code_style.cpp import FORMAT_CPP, TEST_FORMAT_CPP
 from targets.code_style.markdown import FORMAT_MARKDOWN, TEST_FORMAT_MARKDOWN
 from targets.code_style.python import FORMAT_PYTHON, TEST_FORMAT_PYTHON
+from targets.code_style.toml import FORMAT_TOML, TEST_FORMAT_TOML
 from targets.code_style.yaml import FORMAT_YAML, TEST_FORMAT_YAML
 
 TARGETS = TargetBuilder(BuildUnit.for_file(__file__)) \
     .add_phony_target('format') \
-        .depends_on(FORMAT_PYTHON, FORMAT_CPP, FORMAT_MARKDOWN, FORMAT_YAML) \
+        .depends_on(FORMAT_PYTHON, FORMAT_CPP, FORMAT_MARKDOWN, FORMAT_YAML, FORMAT_TOML) \
         .nop() \
     .add_phony_target('test_format') \
-       .depends_on(TEST_FORMAT_PYTHON, TEST_FORMAT_CPP, TEST_FORMAT_MARKDOWN, TEST_FORMAT_YAML) \
+       .depends_on(TEST_FORMAT_PYTHON, TEST_FORMAT_CPP, TEST_FORMAT_MARKDOWN, TEST_FORMAT_YAML, TEST_FORMAT_TOML) \
        .nop() \
     .build()
