c5dec26358f192b2f67e7393612edd02e3aa9600
Michael Rapp <michael.rapp90@googlemail.com>
2025-01-22 17:53:47 +0100

    Use typedef "statistic_type" in class AbstractDecomposableBinnedRuleEvaluation.

--- cpp/subprojects/boosting/src/mlrl/boosting/rule_evaluation/rule_evaluation_decomposable_partial_fixed_binned.cpp
+++ cpp/subprojects/boosting/src/mlrl/boosting/rule_evaluation/rule_evaluation_decomposable_partial_fixed_binned.cpp
@@ -27,9 +27,10 @@ namespace boosting {
 
         protected:
 
-            uint32 calculateOutputWiseCriteria(const StatisticVector& statisticVector, float64* criteria,
-                                               uint32 numCriteria, float32 l1RegularizationWeight,
-                                               float32 l2RegularizationWeight) override {
+            uint32 calculateOutputWiseCriteria(
+              const StatisticVector& statisticVector,
+              typename View<typename StatisticVector::value_type::statistic_type>::iterator criteria,
+              uint32 numCriteria, float32 l1RegularizationWeight, float32 l2RegularizationWeight) override {
                 uint32 numElements = statisticVector.getNumElements();
                 typename StatisticVector::const_iterator statisticIterator = statisticVector.cbegin();
                 SparseArrayVector<float64>::iterator tmpIterator = tmpVector_.begin();
