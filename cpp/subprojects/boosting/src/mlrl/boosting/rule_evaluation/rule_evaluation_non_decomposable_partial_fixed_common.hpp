8d9d2784193118cc18e1f1d1bf5e965f6faef2d5
Michael Rapp <michael.rapp90@googlemail.com>
2025-01-24 23:06:46 +0100

    Add template argument to inline function "sortOutputWiseCriteria".

--- cpp/subprojects/boosting/src/mlrl/boosting/rule_evaluation/rule_evaluation_non_decomposable_partial_fixed_common.hpp
+++ cpp/subprojects/boosting/src/mlrl/boosting/rule_evaluation/rule_evaluation_non_decomposable_partial_fixed_common.hpp
@@ -12,6 +12,7 @@ namespace boosting {
      * Calculates scores that assess the quality of optimal predictions for each output and sorts them, such that the
      * first `numPredictions` elements are the best-rated ones.
      *
+     * @tparam StatisticType            The type of the gradients and Hessians
      * @tparam GradientIterator         The type of the iterator that provides access to the gradients
      * @tparam HessianIterator          The type of the iterator that provides access to the Hessians
      * @param tmpIterator               An iterator that provides random access to a temporary array, which should be
@@ -23,20 +24,20 @@ namespace boosting {
      * @param l1RegularizationWeight    The l2 regularization weight
      * @param l2RegularizationWeight    The L1 regularization weight
      */
-    template<typename GradientIterator, typename HessianIterator>
-    static inline void sortOutputWiseCriteria(SparseArrayVector<float64>::iterator tmpIterator,
+    template<typename StatisticType, typename GradientIterator, typename HessianIterator>
+    static inline void sortOutputWiseCriteria(typename SparseArrayVector<StatisticType>::iterator tmpIterator,
                                               GradientIterator gradientIterator, HessianIterator hessianIterator,
                                               uint32 numOutputs, uint32 numPredictions, float32 l1RegularizationWeight,
                                               float32 l2RegularizationWeight) {
         for (uint32 i = 0; i < numOutputs; i++) {
-            IndexedValue<float64>& entry = tmpIterator[i];
+            IndexedValue<StatisticType>& entry = tmpIterator[i];
             entry.index = i;
             entry.value = calculateOutputWiseScore(gradientIterator[i], hessianIterator[i], l1RegularizationWeight,
                                                    l2RegularizationWeight);
         }
 
         std::partial_sort(tmpIterator, &tmpIterator[numPredictions], &tmpIterator[numOutputs],
-                          CompareOutputWiseCriteria<IndexedValue<float64>>());
+                          CompareOutputWiseCriteria<IndexedValue<StatisticType>>());
     }
 
 }
