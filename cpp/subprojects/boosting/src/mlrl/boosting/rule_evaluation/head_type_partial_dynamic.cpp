686aa14242e25915494454787e944835caec9329
Michael Rapp <michael.rapp90@googlemail.com>
2025-01-31 13:30:37 +0100

    Add template argument to class DenseDecomposableRegressionStatisticsProviderFactory.

--- cpp/subprojects/boosting/src/mlrl/boosting/rule_evaluation/head_type_partial_dynamic.cpp
+++ cpp/subprojects/boosting/src/mlrl/boosting/rule_evaluation/head_type_partial_dynamic.cpp
@@ -115,7 +115,7 @@ namespace boosting {
           labelBinningConfig_.get().createDecomposableDynamicPartialRuleEvaluationFactory(threshold_, exponent_);
         std::unique_ptr<IDecomposableRuleEvaluationFactory> pruningRuleEvaluationFactoryPtr =
           labelBinningConfig_.get().createDecomposableDynamicPartialRuleEvaluationFactory(threshold_, exponent_);
-        return std::make_unique<DenseDecomposableRegressionStatisticsProviderFactory>(
+        return std::make_unique<DenseDecomposableRegressionStatisticsProviderFactory<float64>>(
           std::move(lossFactoryPtr), std::move(evaluationMeasureFactoryPtr), std::move(defaultRuleEvaluationFactoryPtr),
           std::move(regularRuleEvaluationFactoryPtr), std::move(pruningRuleEvaluationFactoryPtr),
           multiThreadingSettings);
