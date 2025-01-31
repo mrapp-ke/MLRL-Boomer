00b80a51b1dd48a40d013cf0f2e18146a27ae3e1
Michael Rapp <michael.rapp90@googlemail.com>
2025-01-31 12:37:42 +0100

    Add template argument to class IDecomposableClassificationLossFactory.

--- cpp/subprojects/boosting/src/mlrl/boosting/statistics/statistics_provider_decomposable_dense.cpp
+++ cpp/subprojects/boosting/src/mlrl/boosting/statistics/statistics_provider_decomposable_dense.cpp
@@ -38,7 +38,7 @@ namespace boosting {
     }
 
     DenseDecomposableClassificationStatisticsProviderFactory::DenseDecomposableClassificationStatisticsProviderFactory(
-      std::unique_ptr<IDecomposableClassificationLossFactory> lossFactoryPtr,
+      std::unique_ptr<IDecomposableClassificationLossFactory<float64>> lossFactoryPtr,
       std::unique_ptr<IClassificationEvaluationMeasureFactory<float64>> evaluationMeasureFactoryPtr,
       std::unique_ptr<IDecomposableRuleEvaluationFactory> defaultRuleEvaluationFactoryPtr,
       std::unique_ptr<IDecomposableRuleEvaluationFactory> regularRuleEvaluationFactoryPtr,
