449cc88dd6792e39de9ec991784126828248b1ae
Michael Rapp <michael.rapp90@googlemail.com>
2025-02-03 00:41:35 +0100

    Add functions "create32Bit" and "create64Bit" to class ILabelBinningFactory.

--- cpp/subprojects/boosting/src/mlrl/boosting/rule_evaluation/rule_evaluation_decomposable_complete_binned.cpp
+++ cpp/subprojects/boosting/src/mlrl/boosting/rule_evaluation/rule_evaluation_decomposable_complete_binned.cpp
@@ -14,7 +14,7 @@ namespace boosting {
       DecomposableCompleteBinnedRuleEvaluationFactory::create(
         const DenseDecomposableStatisticVector<float64>& statisticVector,
         const CompleteIndexVector& indexVector) const {
-        std::unique_ptr<ILabelBinning<float64>> labelBinningPtr = labelBinningFactoryPtr_->create();
+        std::unique_ptr<ILabelBinning<float64>> labelBinningPtr = labelBinningFactoryPtr_->create64Bit();
         return std::make_unique<
           DecomposableCompleteBinnedRuleEvaluation<DenseDecomposableStatisticVector<float64>, CompleteIndexVector>>(
           indexVector, l1RegularizationWeight_, l2RegularizationWeight_, std::move(labelBinningPtr));
@@ -23,7 +23,7 @@ namespace boosting {
     std::unique_ptr<IRuleEvaluation<DenseDecomposableStatisticVector<float64>>>
       DecomposableCompleteBinnedRuleEvaluationFactory::create(
         const DenseDecomposableStatisticVector<float64>& statisticVector, const PartialIndexVector& indexVector) const {
-        std::unique_ptr<ILabelBinning<float64>> labelBinningPtr = labelBinningFactoryPtr_->create();
+        std::unique_ptr<ILabelBinning<float64>> labelBinningPtr = labelBinningFactoryPtr_->create64Bit();
         return std::make_unique<
           DecomposableCompleteBinnedRuleEvaluation<DenseDecomposableStatisticVector<float64>, PartialIndexVector>>(
           indexVector, l1RegularizationWeight_, l2RegularizationWeight_, std::move(labelBinningPtr));
