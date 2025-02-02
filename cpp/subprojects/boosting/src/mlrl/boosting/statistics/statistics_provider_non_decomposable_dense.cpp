252e0f94741d1df6e7ce3d0b638a6b6f9f843d5a
Michael Rapp <michael.rapp90@googlemail.com>
2025-02-02 16:22:28 +0100

    Add arguments to function "visitScoreMatrix" of the class IBoostingStatistics.

--- cpp/subprojects/boosting/src/mlrl/boosting/statistics/statistics_provider_non_decomposable_dense.cpp
+++ cpp/subprojects/boosting/src/mlrl/boosting/statistics/statistics_provider_non_decomposable_dense.cpp
@@ -307,7 +307,9 @@ namespace boosting {
              * @see `IBoostingStatistics::visitScoreMatrix`
              */
             void visitScoreMatrix(
+              IBoostingStatistics::DenseScoreMatrixVisitor<float32> dense32BitVisitor,
               IBoostingStatistics::DenseScoreMatrixVisitor<float64> dense64BitVisitor,
+              IBoostingStatistics::SparseScoreMatrixVisitor<float32> sparse32BitVisitor,
               IBoostingStatistics::SparseScoreMatrixVisitor<float64> sparse64BitVisitor) const override {
                 dense64BitVisitor(this->statePtr_->scoreMatrixPtr->getView());
             }
