15d1e00c591009d55271dc9e99c6dd980bdfc755
Michael Rapp <michael.rapp90@googlemail.com>
2025-02-03 01:01:42 +0100

    Add overloaded "create" functions to class ISparseDecomposableRuleEvaluationFactory.

--- cpp/subprojects/boosting/include/mlrl/boosting/rule_evaluation/rule_evaluation_decomposable_sparse.hpp
+++ cpp/subprojects/boosting/include/mlrl/boosting/rule_evaluation/rule_evaluation_decomposable_sparse.hpp
@@ -26,11 +26,80 @@ namespace boosting {
             /**
              * Creates a new instance of the class `IRuleEvaluation` that allows to calculate the predictions of rules
              * that predict for all available outputs, based on the gradients and Hessians that are stored by a
-             * `SparseDecomposableStatisticVector`.
+             * `SparseDecomposableStatisticVector<float32, uint32>`.
              *
-             * @param statisticVector   A reference to an object of type `SparseDecomposableStatisticVector`. This
-             *                          vector is only used to identify the function that is able to deal with this
-             *                          particular type of vector via function overloading
+             * @param statisticVector   A reference to an object of type
+             *                          `SparseDecomposableStatisticVector<float32, uint32>`. This vector is only used
+             *                          to identify the function that is able to deal with this particular type of
+             *                          vector via function overloading
+             * @param indexVector       A reference to an object of the type `CompleteIndexVector` that provides access
+             *                          to the indices of the outputs for which the rules may predict
+             * @return                  An unique pointer to an object of type `IRuleEvaluation` that has been created
+             */
+            virtual std::unique_ptr<IRuleEvaluation<SparseDecomposableStatisticVector<float32, uint32>>> create(
+              const SparseDecomposableStatisticVector<float32, uint32>& statisticVector,
+              const CompleteIndexVector& indexVector) const = 0;
+
+            /**
+             * Creates a new instance of the class `IRuleEvaluation` that allows to calculate the predictions of rules
+             * that predict for a subset of the available outputs, based on the gradients and Hessians that are stored
+             * by a `SparseDecomposableStatisticVector<float32, uint32>`.
+             *
+             * @param statisticVector   A reference to an object of type
+             *                          `SparseDecomposableStatisticVector<float32, uint32>`. This vector is only used
+             *                          to identify the function that is able to deal with this particular type of
+             *                          vector via function overloading
+             * @param indexVector       A reference to an object of the type `PartialIndexVector` that provides access
+             *                          to the indices of the outputs for which the rules may predict
+             * @return                  An unique pointer to an object of type `IRuleEvaluation` that has been created
+             */
+            virtual std::unique_ptr<IRuleEvaluation<SparseDecomposableStatisticVector<float32, uint32>>> create(
+              const SparseDecomposableStatisticVector<float32, uint32>& statisticVector,
+              const PartialIndexVector& indexVector) const = 0;
+
+            /**
+             * Creates a new instance of the class `IRuleEvaluation` that allows to calculate the predictions of rules
+             * that predict for all available outputs, based on the gradients and Hessians that are stored by a
+             * `SparseDecomposableStatisticVector<float32, float32>`.
+             *
+             * @param statisticVector   A reference to an object of type
+             *                          `SparseDecomposableStatisticVector<float32, float32>`. This vector is only used
+             *                          to identify the function that is able to deal with this particular type of
+             *                          vector via function overloading
+             * @param indexVector       A reference to an object of the type `CompleteIndexVector` that provides access
+             *                          to the indices of the outputs for which the rules may predict
+             * @return                  An unique pointer to an object of type `IRuleEvaluation` that has been created
+             */
+            virtual std::unique_ptr<IRuleEvaluation<SparseDecomposableStatisticVector<float32, float32>>> create(
+              const SparseDecomposableStatisticVector<float32, float32>& statisticVector,
+              const CompleteIndexVector& indexVector) const = 0;
+
+            /**
+             * Creates a new instance of the class `IRuleEvaluation` that allows to calculate the predictions of rules
+             * that predict for a subset of the available outputs, based on the gradients and Hessians that are stored
+             * by a `SparseDecomposableStatisticVector<float32, float32>`.
+             *
+             * @param statisticVector   A reference to an object of type
+             *                          `SparseDecomposableStatisticVector<float32, float32>`. This vector is only used
+             *                          to identify the function that is able to deal with this particular type of
+             *                          vector via function overloading
+             * @param indexVector       A reference to an object of the type `PartialIndexVector` that provides access
+             *                          to the indices of the outputs for which the rules may predict
+             * @return                  An unique pointer to an object of type `IRuleEvaluation` that has been created
+             */
+            virtual std::unique_ptr<IRuleEvaluation<SparseDecomposableStatisticVector<float32, float32>>> create(
+              const SparseDecomposableStatisticVector<float32, float32>& statisticVector,
+              const PartialIndexVector& indexVector) const = 0;
+
+            /**
+             * Creates a new instance of the class `IRuleEvaluation` that allows to calculate the predictions of rules
+             * that predict for all available outputs, based on the gradients and Hessians that are stored by a
+             * `SparseDecomposableStatisticVector<float64, uint32>`.
+             *
+             * @param statisticVector   A reference to an object of type
+             *                          `SparseDecomposableStatisticVector<float64, uint32>`. This vector is only used
+             *                          to identify the function that is able to deal with this particular type of
+             *                          vector via function overloading
              * @param indexVector       A reference to an object of the type `CompleteIndexVector` that provides access
              *                          to the indices of the outputs for which the rules may predict
              * @return                  An unique pointer to an object of type `IRuleEvaluation` that has been created
@@ -42,11 +111,12 @@ namespace boosting {
             /**
              * Creates a new instance of the class `IRuleEvaluation` that allows to calculate the predictions of rules
              * that predict for a subset of the available outputs, based on the gradients and Hessians that are stored
-             * by a `SparseDecomposableStatisticVector`.
+             * by a `SparseDecomposableStatisticVector<float64, uint32>`.
              *
-             * @param statisticVector   A reference to an object of type `SparseDecomposableStatisticVector`. This
-             *                          vector is only used to identify the function that is able to deal with this
-             *                          particular type of vector via function overloading
+             * @param statisticVector   A reference to an object of type
+             *                          `SparseDecomposableStatisticVector<float64, uint32>`. This vector is only used
+             *                          to identify the function that is able to deal with this particular type of
+             *                          vector via function overloading
              * @param indexVector       A reference to an object of the type `PartialIndexVector` that provides access
              *                          to the indices of the outputs for which the rules may predict
              * @return                  An unique pointer to an object of type `IRuleEvaluation` that has been created
@@ -58,11 +128,12 @@ namespace boosting {
             /**
              * Creates a new instance of the class `IRuleEvaluation` that allows to calculate the predictions of rules
              * that predict for all available outputs, based on the gradients and Hessians that are stored by a
-             * `SparseDecomposableStatisticVector`.
+             * `SparseDecomposableStatisticVector<float64, float32>`.
              *
-             * @param statisticVector   A reference to an object of type `SparseDecomposableStatisticVector`. This
-             *                          vector is only used to identify the function that is able to deal with this
-             *                          particular type of vector via function overloading
+             * @param statisticVector   A reference to an object of type
+             *                          `SparseDecomposableStatisticVector<float64, float32>`. This vector is only used
+             *                          to identify the function that is able to deal with this particular type of
+             *                          vector via function overloading
              * @param indexVector       A reference to an object of the type `CompleteIndexVector` that provides access
              *                          to the indices of the outputs for which the rules may predict
              * @return                  An unique pointer to an object of type `IRuleEvaluation` that has been created
@@ -74,11 +145,12 @@ namespace boosting {
             /**
              * Creates a new instance of the class `IRuleEvaluation` that allows to calculate the predictions of rules
              * that predict for a subset of the available outputs, based on the gradients and Hessians that are stored
-             * by a `SparseDecomposableStatisticVector`.
+             * by a `SparseDecomposableStatisticVector<float64, float32>`.
              *
-             * @param statisticVector   A reference to an object of type `SparseDecomposableStatisticVector`. This
-             *                          vector is only used to identify the function that is able to deal with this
-             *                          particular type of vector via function overloading
+             * @param statisticVector   A reference to an object of type
+             *                          `SparseDecomposableStatisticVector<float64, float32>`. This vector is only used
+             *                          to identify the function that is able to deal with this particular type of
+             *                          vector via function overloading
              * @param indexVector       A reference to an object of the type `PartialIndexVector` that provides access
              *                          to the indices of the outputs for which the rules may predict
              * @return                  An unique pointer to an object of type `IRuleEvaluation` that has been created
