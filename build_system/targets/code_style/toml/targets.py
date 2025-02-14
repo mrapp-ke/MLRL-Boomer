3988263cff85fb90a3cbe8f9e0aca9d283d3503c
Michael Rapp <michael.rapp90@googlemail.com>
2025-02-14 16:33:42 +0100

    Remove schema validation for TOML files.

--- build_system/targets/code_style/toml/targets.py
+++ build_system/targets/code_style/toml/targets.py
@@ -3,8 +3,6 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 
 Implements targets for checking and enforcing code style definitions for TOML files.
 """
-from os import path
-
 from core.build_unit import BuildUnit
 from core.modules import Module
 from core.targets import PhonyTarget
@@ -22,27 +20,13 @@ class CheckTomlCodeStyle(PhonyTarget.Runnable):
     Checks if TOML files adhere to the code style definitions. If this is not the case, an error is raised.
     """
 
-    SCHEMAS = {
-        'pyproject.template.toml': 'https://json.schemastore.org/pyproject.json',
-    }
-
     def __init__(self):
         super().__init__(MODULE_FILTER)
 
     def run(self, build_unit: BuildUnit, module: Module):
         Log.info('Checking TOML files in the directory "%s"...', module.root_directory)
         source_files = module.find_source_files()
-        files_per_schema = {}
-
-        for file in source_files:
-            file_name = path.basename(file)
-            schema = self.SCHEMAS.get(file_name)
-            file_list = files_per_schema.setdefault(schema, [])
-            file_list.append(file)
-
-        for schema, files in files_per_schema.items():
-            TaploLint(build_unit, files, schema=schema).run()
-
+        TaploLint(build_unit, source_files).run()
         TaploFormat(build_unit, source_files).run()
 
 
