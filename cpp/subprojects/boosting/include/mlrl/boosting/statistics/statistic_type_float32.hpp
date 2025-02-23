92219a564b5c05347042e2af5460535605a4f7c7
Michael Rapp <michael.rapp90@googlemail.com>
2025-02-23 21:39:40 +0100

    Add functions to class IStatisticType.

--- cpp/subprojects/boosting/include/mlrl/boosting/statistics/statistic_type_float32.hpp
+++ cpp/subprojects/boosting/include/mlrl/boosting/statistics/statistic_type_float32.hpp
@@ -3,7 +3,9 @@
  */
 #pragma once
 
+#include "mlrl/boosting/rule_evaluation/head_type.hpp"
 #include "mlrl/boosting/statistics/statistic_type.hpp"
+#include "mlrl/common/util/properties.hpp"
 
 namespace boosting {
 
@@ -11,6 +13,40 @@ namespace boosting {
      * Allows to use 32-bit floating point values for representing statistics about the quality of predictions for
      * training examples.
      */
-    class Float32StatisticsConfig final : public IStatisticTypeConfig {};
+    class Float32StatisticsConfig final : public IStatisticTypeConfig {
+        private:
+
+            const ReadableProperty<IHeadConfig> headConfig_;
+
+        public:
+
+            /**
+             * @param headConfig A `ReadableProperty` that allows to access the `IHeadConfig` that stores the
+             *                   configuration of rule heads
+             */
+            Float32StatisticsConfig(ReadableProperty<IHeadConfig> headConfig);
+
+            std::unique_ptr<IClassificationStatisticsProviderFactory> createClassificationStatisticsProviderFactory(
+              const IFeatureMatrix& featureMatrix, const IRowWiseLabelMatrix& labelMatrix,
+              const IDecomposableClassificationLossConfig& lossConfig) const override;
+
+            std::unique_ptr<IClassificationStatisticsProviderFactory> createClassificationStatisticsProviderFactory(
+              const IFeatureMatrix& featureMatrix, const IRowWiseLabelMatrix& labelMatrix,
+              const ISparseDecomposableClassificationLossConfig& lossConfig) const override;
+
+            std::unique_ptr<IClassificationStatisticsProviderFactory> createClassificationStatisticsProviderFactory(
+              const IFeatureMatrix& featureMatrix, const IRowWiseLabelMatrix& labelMatrix,
+              const INonDecomposableClassificationLossConfig& lossConfig, const BlasFactory& blasFactory,
+              const LapackFactory& lapackFactory) const override;
+
+            std::unique_ptr<IRegressionStatisticsProviderFactory> createRegressionStatisticsProviderFactory(
+              const IFeatureMatrix& featureMatrix, const IRowWiseRegressionMatrix& regressionMatrix,
+              const IDecomposableRegressionLossConfig& lossConfig) const override;
+
+            std::unique_ptr<IRegressionStatisticsProviderFactory> createRegressionStatisticsProviderFactory(
+              const IFeatureMatrix& featureMatrix, const IRowWiseRegressionMatrix& regressionMatrix,
+              const INonDecomposableRegressionLossConfig& lossConfig, const BlasFactory& blasFactory,
+              const LapackFactory& lapackFactory) const override;
+    };
 
 }
