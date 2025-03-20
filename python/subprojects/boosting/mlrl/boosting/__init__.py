3f14bf15de6f8f241a7f3c702ffd7f8249493559
Michael Rapp <michael.rapp90@googlemail.com>
2025-03-20 14:23:38 +0100

    Add Python package "mlrl.boosting.testbed".

--- python/subprojects/boosting/mlrl/boosting/__init__.py
+++ python/subprojects/boosting/mlrl/boosting/__init__.py
@@ -5,7 +5,7 @@ This module provides rule learners that make using of the gradient boosting fram
 from mlrl.boosting.learners import BoomerClassifier
 
 try:
-    from mlrl.boosting.runnables import BoomerRunnable as Runnable
+    from mlrl.boosting.testbed.runnables import BoomerRunnable as Runnable
 except ImportError:
     # Dependency 'mlrl-testbed' is not available, but that's okay since it's optional.
     pass
