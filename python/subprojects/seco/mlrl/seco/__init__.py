0c4764376e25cec1a19435f6be9b5e49b895ef2e
Michael Rapp <michael.rapp90@googlemail.com>
2025-03-20 14:24:14 +0100

    Add Python package "mlrl.seco.testbed".

--- python/subprojects/seco/mlrl/seco/__init__.py
+++ python/subprojects/seco/mlrl/seco/__init__.py
@@ -5,7 +5,7 @@ This module provides rule learners that make use of the Separate-and-Conquer (Se
 from mlrl.seco.learners import SeCoClassifier
 
 try:
-    from mlrl.seco.runnables import SeCoRunnable as Runnable
+    from mlrl.seco.testbed.runnables import SeCoRunnable as Runnable
 except ImportError:
     # Dependency 'mlrl-testbed' is not available, but that's okay since it's optional.
     pass
