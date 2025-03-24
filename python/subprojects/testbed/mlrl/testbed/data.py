b00adaa0823cfdb6d76bcb4f91f9b6640a68228d
Michael Rapp <michael.rapp90@googlemail.com>
2025-03-24 23:32:23 +0100

    Remove functions "get_num_attributes" and "get_attribute_indices" from class ArffMetaData.

--- python/subprojects/testbed/mlrl/testbed/data.py
+++ python/subprojects/testbed/mlrl/testbed/data.py
@@ -7,7 +7,6 @@ import logging as log
 import xml.etree.ElementTree as XmlTree
 
 from dataclasses import dataclass
-from functools import reduce
 from os import path
 from typing import Any, Dict, List, Optional, Set, Tuple
 from xml.dom import minidom
@@ -37,35 +36,6 @@ class ArffMetaData:
     features: List[Attribute]
     outputs: List[Attribute]
     outputs_at_start: bool = False
-
-    def get_num_features(self, *feature_types: AttributeType) -> int:
-        """
-        Returns the number of features with one out of a given set of types.  If no types are given, all features are
-        counted.
-
-        :param feature_types:   The types of the features to be counted
-        :return:                The number of features of the given types
-        """
-        feature_types = set(feature_types)
-
-        if feature_types:
-            return reduce(lambda aggr, feature: aggr + (1 if feature.attribute_type in feature_types else 0),
-                          self.features, 0)
-
-        return len(self.features)
-
-    def get_feature_indices(self, *feature_types: AttributeType) -> List[int]:
-        """
-        Returns a list that contains the indices of all features with one out of a given set of types (in ascending
-        order). If no types are given, all indices are returned.
-
-        :param feature_types:   The types of the features whose indices should be returned
-        :return:                A list that contains the indices of all features of the given types
-        """
-        feature_types = set(feature_types)
-        return [
-            i for i, feature in enumerate(self.features) if not feature_types or feature.attribute_type in feature_types
-        ]
 
 
 def load_data_set_and_meta_data(directory: str,
