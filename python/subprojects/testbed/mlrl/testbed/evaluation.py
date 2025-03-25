10cd710bba78e52891221a665843c1e170507b68
Michael Rapp <michael.rapp90@googlemail.com>
2025-03-25 14:08:06 +0100

    Add argument of type OutputScope to function "write_output" of the class Sink.

--- python/subprojects/testbed/mlrl/testbed/evaluation.py
+++ python/subprojects/testbed/mlrl/testbed/evaluation.py
@@ -5,6 +5,7 @@ Provides classes for evaluating the predictions provided by a machine learning m
 The evaluation results can be written to one or several outputs, e.g., to the console or to a file.
 """
 from abc import ABC, abstractmethod
+from dataclasses import replace
 from typing import Any, Dict, List, Optional, Set, Tuple
 
 import numpy as np
@@ -16,13 +17,11 @@ from mlrl.common.config.options import Options
 from mlrl.common.data.arrays import enforce_dense
 from mlrl.common.data.types import Float32, Uint8
 
-from mlrl.testbed.dataset import Dataset
 from mlrl.testbed.fold import Fold
 from mlrl.testbed.format import OPTION_DECIMALS, OPTION_PERCENTAGE, Formatter, filter_formatters, format_table
 from mlrl.testbed.output_scope import OutputScope
 from mlrl.testbed.output_writer import Formattable, OutputWriter, Tabularizable
 from mlrl.testbed.prediction_scope import PredictionScope, PredictionType
-from mlrl.testbed.problem_type import ProblemType
 
 OPTION_ENABLE_ALL = 'enable_all'
 
@@ -350,17 +349,17 @@ class EvaluationWriter(OutputWriter, ABC):
         def __init__(self, options: Options = Options()):
             super().__init__(title='Evaluation result', options=options)
 
-        def write_output(self, problem_type: ProblemType, dataset: Dataset, fold: Fold,
-                         prediction_scope: Optional[PredictionScope], output_data, **kwargs):
+        def write_output(self, scope: OutputScope, prediction_scope: Optional[PredictionScope], output_data, **kwargs):
             """
             See :func:`mlrl.testbed.output_writer.OutputWriter.Sink.write_output`
             """
+            fold = scope.fold
             new_kwargs = {**kwargs, **{EvaluationWriter.KWARG_FOLD: fold.index if fold.is_cross_validation_used else 0}}
-            super().write_output(problem_type, dataset, fold, prediction_scope, output_data, **new_kwargs)
+            super().write_output(scope, prediction_scope, output_data, **new_kwargs)
 
             if fold.is_cross_validation_used and fold.is_last_fold:
                 overall_fold = Fold(index=None, num_folds=fold.num_folds, is_last_fold=True)
-                super().write_output(problem_type, dataset, overall_fold, prediction_scope, output_data, **kwargs)
+                super().write_output(replace(scope, fold=overall_fold), prediction_scope, output_data, **kwargs)
 
     class CsvFileSink(OutputWriter.CsvFileSink):
         """
@@ -370,17 +369,17 @@ class EvaluationWriter(OutputWriter, ABC):
         def __init__(self, output_dir: str, options: Options = Options()):
             super().__init__(output_dir=output_dir, file_name='evaluation', options=options)
 
-        def write_output(self, problem_type: ProblemType, dataset: Dataset, fold: Fold,
-                         prediction_scope: Optional[PredictionScope], output_data, **kwargs):
+        def write_output(self, scope: OutputScope, prediction_scope: Optional[PredictionScope], output_data, **kwargs):
             """
             See :func:`mlrl.testbed.output_writer.OutputWriter.Sink.write_output`
             """
+            fold = scope.fold
             new_kwargs = {**kwargs, **{EvaluationWriter.KWARG_FOLD: fold.index if fold.is_cross_validation_used else 0}}
-            super().write_output(problem_type, dataset, fold, prediction_scope, output_data, **new_kwargs)
+            super().write_output(scope, prediction_scope, output_data, **new_kwargs)
 
             if fold.is_cross_validation_used and fold.is_last_fold:
                 overall_fold = Fold(index=None, num_folds=fold.num_folds, is_last_fold=True)
-                super().write_output(problem_type, dataset, overall_fold, prediction_scope, output_data, **kwargs)
+                super().write_output(replace(scope, fold=overall_fold), prediction_scope, output_data, **kwargs)
 
     def __init__(self, sinks: List[OutputWriter.Sink]):
         super().__init__(sinks)
