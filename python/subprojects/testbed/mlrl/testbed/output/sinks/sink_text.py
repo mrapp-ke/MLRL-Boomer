4a37589cbf85406285f69ec1a177343189b155a2
Michael Rapp <michael.rapp90@googlemail.com>
2025-03-27 00:22:39 +0100

    Move class TextFileSink into new file sink_text.py.

---
+++ python/subprojects/testbed/mlrl/testbed/output/sinks/sink_text.py
@@ -0,0 +1,40 @@
+"""
+Author Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides classes that allow writing output data to text files.
+"""
+from typing import Optional
+
+from mlrl.testbed.io import open_writable_file
+from mlrl.testbed.output.sinks.sink import FileSink
+from mlrl.testbed.output_scope import OutputScope
+from mlrl.testbed.prediction_result import PredictionResult
+from mlrl.testbed.training_result import TrainingResult
+
+
+class TextFileSink(FileSink):
+    """
+    Allows to write output data to a text file.
+    """
+
+    class PathFormatter(FileSink.PathFormatter):
+        """
+        Allows to determine the path to the text file to which output data is written.
+        """
+
+        def __init__(self,
+                     directory: str,
+                     file_name: str,
+                     include_dataset_type: bool = True,
+                     include_prediction_scope: bool = True,
+                     include_fold: bool = True):
+            super().__init__(directory, file_name, 'txt', include_dataset_type, include_prediction_scope, include_fold)
+
+    # pylint: disable=unused-argument
+    def _write_to_file(self, file_path: str, scope: OutputScope, training_result: Optional[TrainingResult],
+                       prediction_result: Optional[PredictionResult], output_data, **kwargs):
+        text = output_data.to_text(self.options, **kwargs)
+
+        if text:
+            with open_writable_file(file_path) as text_file:
+                text_file.write(text)
