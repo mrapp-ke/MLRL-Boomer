50e72cb3bc3dcd35bc79af8836060fb1270d867b
Michael Rapp <michael.rapp90@googlemail.com>
2025-03-31 01:04:46 +0200

    Replace class LabelVectors with new class LabelVectorHistogram.

--- python/subprojects/testbed/mlrl/testbed/label_vectors.py
+++ python/subprojects/testbed/mlrl/testbed/label_vectors.py
@@ -4,23 +4,17 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 Provides classes for printing unique label vectors that are contained in a data set. The label vectors can be written to
 one or several outputs, e.g., to the console or to a file.
 """
-from typing import Dict, List, Optional, Tuple
+from typing import Optional
 
 import numpy as np
 
-from scipy.sparse import lil_array
-
-from mlrl.common.config.options import Options
 from mlrl.common.cython.output_space_info import LabelVectorSet, LabelVectorSetVisitor
-from mlrl.common.data.types import Uint8
 from mlrl.common.learners import ClassificationRuleLearner
 
-from mlrl.testbed.experiments.output.data import OutputData, TabularOutputData
+from mlrl.testbed.experiments.output.data import OutputData
+from mlrl.testbed.experiments.output.label_vectors.label_vector_histogram import LabelVectorHistogram
 from mlrl.testbed.experiments.output.writer import OutputWriter
 from mlrl.testbed.experiments.state import ExperimentState
-from mlrl.testbed.util.format import format_table
-
-OPTION_SPARSE = 'sparse'
 
 
 class LabelVectorWriter(OutputWriter):
@@ -28,88 +22,9 @@ class LabelVectorWriter(OutputWriter):
     Allows to write unique label vectors that are contained in a data set to one or several sinks.
     """
 
-    class LabelVectors(TabularOutputData):
-        """
-        Stores unique label vectors that are contained in a data set.
-        """
-
-        COLUMN_INDEX = 'Index'
-
-        COLUMN_LABEL_VECTOR = 'Label vector'
-
-        COLUMN_FREQUENCY = 'Frequency'
-
-        def __init__(self, num_labels: int, y=None):
-            """
-            :param num_labels:  The total number of available labels
-            :param y:           A `numpy.ndarray`, `scipy.sparse.spmatrix` or `scipy.sparse.sparray`, shape
-                                `(num_examples, num_labels)`, that stores the ground truth labels
-            """
-            super().__init__('Label vectors', 'label_vectors',
-                             ExperimentState.FormatterOptions(include_dataset_type=False))
-            self.num_labels = num_labels
-
-            if y is None:
-                self.unique_label_vectors = []
-            else:
-                unique_label_vector_strings: Dict[str, int] = {}
-                y = lil_array(y)
-                separator = ','
-
-                for label_vector in y.rows:
-                    label_vector_string = separator.join(map(str, label_vector))
-                    frequency = unique_label_vector_strings.setdefault(label_vector_string, 0)
-                    unique_label_vector_strings[label_vector_string] = frequency + 1
-
-                unique_label_vectors: List[Tuple[np.array, int]] = []
-
-                for label_vector_string, frequency in unique_label_vector_strings.items():
-                    label_vector = np.asarray(
-                        [int(label_index) for label_index in label_vector_string.split(separator)])
-                    unique_label_vectors.append((label_vector, frequency))
-
-                self.unique_label_vectors = unique_label_vectors
-
-        def __format_label_vector(self, sparse_label_vector: np.ndarray, sparse: bool) -> str:
-            if sparse:
-                return str(sparse_label_vector)
-
-            dense_label_vector = np.zeros(shape=self.num_labels, dtype=Uint8)
-            dense_label_vector[sparse_label_vector] = 1
-            return str(dense_label_vector)
-
-        def to_text(self, options: Options, **_) -> Optional[str]:
-            """
-            See :func:`mlrl.testbed.experiments.output.data.OutputData.to_text`
-            """
-            sparse = options.get_bool(OPTION_SPARSE, False)
-            header = [self.COLUMN_INDEX, self.COLUMN_LABEL_VECTOR, self.COLUMN_FREQUENCY]
-            rows = []
-
-            for i, (sparse_label_vector, frequency) in enumerate(self.unique_label_vectors):
-                rows.append([i + 1, self.__format_label_vector(sparse_label_vector, sparse=sparse), frequency])
-
-            return format_table(rows, header=header)
-
-        def to_table(self, options: Options, **_) -> Optional[TabularOutputData.Table]:
-            """
-            See :func:`mlrl.testbed.experiments.output.data.TabularOutputData.to_table`
-            """
-            sparse = options.get_bool(OPTION_SPARSE, False)
-            rows = []
-
-            for i, (sparse_label_vector, frequency) in enumerate(self.unique_label_vectors):
-                rows.append({
-                    self.COLUMN_INDEX: i + 1,
-                    self.COLUMN_LABEL_VECTOR: self.__format_label_vector(sparse_label_vector, sparse=sparse),
-                    self.COLUMN_FREQUENCY: frequency
-                })
-
-            return rows
-
     def _generate_output_data(self, state: ExperimentState) -> Optional[OutputData]:
         dataset = state.dataset
-        return LabelVectorWriter.LabelVectors(num_labels=dataset.num_outputs, y=dataset.y)
+        return LabelVectorHistogram(num_labels=dataset.num_outputs, y=dataset.y)
 
 
 class LabelVectorSetWriter(LabelVectorWriter):
@@ -127,7 +42,7 @@ class LabelVectorSetWriter(LabelVectorWriter):
             """
             :param num_labels: The total number of available labels
             """
-            self.label_vectors = LabelVectorWriter.LabelVectors(num_labels=num_labels)
+            self.label_vectors = LabelVectorHistogram(num_labels)
 
         def visit_label_vector(self, label_vector: np.ndarray, frequency: int):
             """
