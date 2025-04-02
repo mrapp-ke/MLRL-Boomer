46145db3c5f82c436bad203bf953c865b046e9ce
Michael Rapp <michael.rapp90@googlemail.com>
2025-04-02 13:25:27 +0200

    Add class CythonLint.

---
+++ build_system/targets/code_style/python/cython_lint.py
@@ -0,0 +1,23 @@
+"""
+Author: Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides classes that allow to run the external program "cython-lint".
+"""
+from core.build_unit import BuildUnit
+from util.run import Program
+
+from targets.code_style.modules import CodeModule
+
+
+class CythonLint(Program):
+    """
+    Allows to run the external program "cython-lint".
+    """
+
+    def __init__(self, build_unit: BuildUnit, module: CodeModule):
+        """
+        :param build_unit:  The build unit from which the program should be run
+        :param module:      The module, the program should be applied to
+        """
+        super().__init__('cython-lint', *module.find_source_files())
+        self.set_build_unit(build_unit)
