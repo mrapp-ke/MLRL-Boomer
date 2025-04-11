8f5d6eb049a3dd6d5be9b4c92efe8b1502c0dbb3
Michael Rapp <michael.rapp90@googlemail.com>
2025-04-11 22:15:18 +0200

    Add class LabelVector.

--- python/subprojects/testbed/mlrl/testbed/experiments/output/label_vectors/label_vector_histogram.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/output/label_vectors/label_vector_histogram.py
@@ -3,7 +3,8 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 
 Provides classes that provide access to unique label vectors.
 """
-from typing import List, Optional, Tuple
+from dataclasses import dataclass
+from typing import List, Optional
 
 import numpy as np
 
@@ -12,12 +13,28 @@ from scipy.sparse import lil_array
 from mlrl.testbed.dataset import Dataset
 
 
+@dataclass
+class LabelVector:
+    """
+    A label vector.
+
+    Attributes:
+        label_indices:  The indices of all relevant labels in the label vector
+        frequency:      The frequency of the label vector
+    """
+    label_indices: np.ndarray
+    frequency: int
+
+    def __str__(self) -> str:
+        return str(self.label_indices)
+
+
 class LabelVectorHistogram:
     """
     Stores unique label vectors and their respective frequency.
     """
 
-    def __init__(self, num_labels: int, unique_label_vectors: Optional[List[Tuple[np.array, int]]] = None):
+    def __init__(self, num_labels: int, unique_label_vectors: Optional[List[LabelVector]] = None):
         """
         :param num_labels:              The total number of available labels
         :param unique_label_vectors:    A list that contains the unique label vectors, as well as their frequency, or
@@ -46,6 +63,6 @@ class LabelVectorHistogram:
 
         for label_vector_string, frequency in unique_label_vector_strings.items():
             label_vector = np.asarray([int(label_index) for label_index in label_vector_string.split(separator)])
-            unique_label_vectors.append((label_vector, frequency))
+            unique_label_vectors.append(LabelVector(label_indices=label_vector, frequency=frequency))
 
         return LabelVectorHistogram(dataset.num_outputs, unique_label_vectors)
--- python/subprojects/testbed/mlrl/testbed/experiments/output/label_vectors/label_vectors.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/output/label_vectors/label_vectors.py
@@ -11,7 +11,7 @@ from mlrl.common.config.options import Options
 from mlrl.common.data.types import Uint8
 
 from mlrl.testbed.experiments.output.data import TabularOutputData
-from mlrl.testbed.experiments.output.label_vectors.label_vector_histogram import LabelVectorHistogram
+from mlrl.testbed.experiments.output.label_vectors.label_vector_histogram import LabelVector, LabelVectorHistogram
 from mlrl.testbed.experiments.output.table import RowWiseTable, Table
 from mlrl.testbed.experiments.state import ExperimentState
 
@@ -32,12 +32,12 @@ class LabelVectors(TabularOutputData):
                          default_formatter_options=ExperimentState.FormatterOptions(include_dataset_type=False))
         self.label_vector_histogram = label_vector_histogram
 
-    def __format_label_vector(self, label_vector: np.ndarray, sparse: bool) -> str:
+    def __format_label_vector(self, label_vector: LabelVector, sparse: bool) -> str:
         if sparse:
             return str(label_vector)
 
         dense_label_vector = np.zeros(shape=self.label_vector_histogram.num_labels, dtype=Uint8)
-        dense_label_vector[label_vector] = 1
+        dense_label_vector[label_vector.label_indices] = 1
         return str(dense_label_vector)
 
     def to_text(self, options: Options, **kwargs) -> Optional[str]:
@@ -53,7 +53,7 @@ class LabelVectors(TabularOutputData):
         sparse = options.get_bool(self.OPTION_SPARSE, kwargs.get(self.OPTION_SPARSE, False))
         table = RowWiseTable('Index', 'Label vector', 'Frequency')
 
-        for i, (label_vector, frequency) in enumerate(self.label_vector_histogram.unique_label_vectors):
-            table.add_row(i + 1, self.__format_label_vector(label_vector, sparse=sparse), frequency)
+        for i, label_vector in enumerate(self.label_vector_histogram.unique_label_vectors):
+            table.add_row(i + 1, self.__format_label_vector(label_vector, sparse=sparse), label_vector.frequency)
 
         return table.sort_by_columns(2, descending=True)
