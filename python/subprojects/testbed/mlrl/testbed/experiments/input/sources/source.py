cf034e478f8b6bd4dd79e984d872a943f096c6a8
Michael Rapp <michael.rapp90@googlemail.com>
2025-04-13 01:51:35 +0200

    Add class FileSource.

--- python/subprojects/testbed/mlrl/testbed/experiments/input/sources/source.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/input/sources/source.py
@@ -4,8 +4,10 @@ Author Michael Rapp (michael.rapp.ml@gmail.com)
 Provides classes for implementing sources, input data may be read from.
 """
 from abc import ABC, abstractmethod
+import logging as log
 from typing import Optional, Any
 
+from mlrl.testbed.experiments.data import FilePath
 from mlrl.testbed.experiments.input.data import InputData
 from mlrl.testbed.experiments.state import ExperimentState
 
@@ -23,4 +25,44 @@ class Source(ABC):
         :param state:       The state that should be used to store the input data
         :param input_data:  The input data that should be read
         :return:            The data that has been read
+        """
+
+
+class FileSource(Source, ABC):
+    """
+    An abstract base class for all sources that read input data from a file.
+    """
+
+    def __init__(self, directory: str, suffix: str, input_data: InputData):
+        """
+        :param directory:   The path to the directory of the file
+        :param suffix:      The suffix of the file
+        :param input_data:  The input data to be read
+        """
+        self.directory = directory
+        self.suffix = suffix
+        self.input_data = input_data
+
+
+    def read_from_source(self, state: ExperimentState, input_data: InputData) -> Optional[Any]:
+        context = input_data.get_context(type(self))
+        file_path = FilePath(directory=self.directory,
+                             file_name=input_data.file_name,
+                             suffix=self.suffix,
+                             context=context)
+        file_path = file_path.resolve(state)
+        log.debug('Reading input data from file "%s"...', file_path)
+        data = self._read_from_file(file_path, input_data)
+
+        if data:
+            input_data.update_state(state, data)
+
+
+    @abstractmethod
+    def _read_from_file(self, file_path: str, input_data: InputData) -> Optional[Any]:
+        """
+        Must be implemented by subclasses in order to read input data from a specific sink.
+
+        :param file_path:   The path to the file from which the input data should be read
+        :param input_data:  The input data that should be read
         """
