ea0e1df22ff5b89190cef721944984510ade12cb
Michael Rapp <michael.rapp90@googlemail.com>
2025-04-12 03:07:59 +0200

    Replace class FileSink.Path with new class FilePath.

--- python/subprojects/testbed/mlrl/testbed/experiments/output/sinks/sink.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/output/sinks/sink.py
@@ -6,16 +6,14 @@ Provides classes for implementing sinks, output data may be written to.
 import logging as log
 
 from abc import ABC, abstractmethod
-from os import path
 
 from mlrl.common.config.options import Options
 
 from mlrl.testbed.dataset import Dataset
-from mlrl.testbed.experiments.data import Data
+from mlrl.testbed.experiments.data import FilePath
 from mlrl.testbed.experiments.output.data import DatasetOutputData, OutputData, TabularOutputData
 from mlrl.testbed.experiments.output.table import Table
 from mlrl.testbed.experiments.state import ExperimentState
-from mlrl.testbed.util.io import get_file_name_per_fold
 
 
 class Sink(ABC):
@@ -44,45 +42,6 @@ class FileSink(Sink, ABC):
     An abstract base class for all sinks that write output data to a file.
     """
 
-    class Path:
-        """
-        The path to a file to which output data is written.
-        """
-
-        def __init__(self, directory: str, file_name: str, suffix: str, context: Data.Context):
-            """
-            :param directory:   The path to the directory, where the file is located
-            :param file_name:   The name of the file
-            :param suffix:      The suffix of the file (with leading dot)
-            :param context:     A `Data.Context` to be used to determine the path
-            """
-            self.directory = directory
-            self.file_name = file_name
-            self.suffix = suffix
-            self.context = context
-
-        def resolve(self, state: ExperimentState) -> str:
-            """
-            Determines and returns the path to the file to which output data should be written.
-
-            :param state: The state from which the output data has been generated
-            """
-            file_name = self.file_name
-
-            if self.context.include_dataset_type:
-                file_name = state.dataset.type.get_file_name(file_name)
-
-            if self.context.include_prediction_scope:
-                prediction_result = state.prediction_result
-
-                if prediction_result:
-                    file_name = prediction_result.prediction_scope.get_file_name(file_name)
-
-            if self.context.include_fold:
-                file_name = get_file_name_per_fold(file_name, self.suffix, state.fold.index)
-
-            return path.join(self.directory, file_name)
-
     def __init__(self, directory: str, suffix: str, options: Options = Options()):
         """
         :param directory:   The path to the directory of the file
@@ -97,10 +56,10 @@ class FileSink(Sink, ABC):
         See :func:`mlrl.testbed.experiments.output.sinks.sink.Sink.write_to_sink`
         """
         context = output_data.get_context(type(self))
-        file_path = FileSink.Path(directory=self.directory,
-                                  file_name=output_data.file_name,
-                                  suffix=self.suffix,
-                                  context=context)
+        file_path = FilePath(directory=self.directory,
+                             file_name=output_data.file_name,
+                             suffix=self.suffix,
+                             context=context)
         file_path = file_path.resolve(state)
         log.debug('Writing output data to file "%s"...', file_path)
         self._write_to_file(file_path, state, output_data, **kwargs)
