3605e82e3714c05c80334ceaaf6e435b14bd8e67
Michael Rapp <michael.rapp90@googlemail.com>
2025-04-13 02:08:07 +0200

    Add class CsvFileSource.

--- python/subprojects/testbed/mlrl/testbed/experiments/input/sources/__init__.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/input/sources/__init__.py
@@ -4,3 +4,4 @@ Author Michael Rapp (michael.rapp.ml@gmail.com)
 Provides classes that allow to read input data from different sources.
 """
 from mlrl.testbed.experiments.input.sources.source import Source
+from mlrl.testbed.experiments.input.sources.source_csv import CsvFileSource
