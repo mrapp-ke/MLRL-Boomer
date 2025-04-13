1beceaabd5afabd3d96fff3f2d0c3646bcd42ce3
Michael Rapp <michael.rapp90@googlemail.com>
2025-04-14 00:36:56 +0200

    Use class ModelWriter instead of class ModelSaver.

--- python/subprojects/testbed/mlrl/testbed/persistence.py
+++ python/subprojects/testbed/mlrl/testbed/persistence.py
@@ -12,8 +12,6 @@ import _pickle as pickle
 from mlrl.testbed.fold import Fold
 from mlrl.testbed.util.io import get_file_name_per_fold
 
-SUFFIX_MODEL = 'model'
-
 
 class ModelLoader:
     """
@@ -26,15 +24,14 @@ class ModelLoader:
         """
         self.directory = directory
 
-    def load_model(self, model_name: str, fold: Fold):
+    def load_model(self, fold: Fold):
         """
         Loads a model from a file.
 
-        :param model_name:  The name of the model to be loaded
-        :param fold:        The fold of the available data, the model corresponds to
-        :return:            The loaded model
+        :param fold:    The fold of the available data, the model corresponds to
+        :return:        The loaded model
         """
-        file_name = get_file_name_per_fold(model_name, SUFFIX_MODEL, fold.index)
+        file_name = get_file_name_per_fold('model', 'pickle', fold.index)
         file_path = path.join(self.directory, file_name)
         log.debug('Loading model from file \"%s\"...', file_path)
 
@@ -46,34 +43,3 @@ class ModelLoader:
         except IOError:
             log.error('Failed to load model from file \"%s\"', file_path)
             return None
-
-
-class ModelSaver:
-    """
-    Allows to save models to a directory.
-    """
-
-    def __init__(self, directory: str):
-        """
-        :param directory: The path to the directory to which models should be saved
-        """
-        self.directory = directory
-
-    def save_model(self, model, model_name: str, fold: Fold):
-        """
-        Saves a model to a file.
-
-        :param model:       The model to be persisted
-        :param model_name:  The name of the model to be persisted
-        :param fold:        The fold of the available data, the model corresponds to
-        """
-        file_name = get_file_name_per_fold(model_name, SUFFIX_MODEL, fold.index)
-        file_path = path.join(self.directory, file_name)
-        log.debug('Saving model to file \"%s\"...', file_path)
-
-        try:
-            with open(file_path, mode='wb') as output_stream:
-                pickle.dump(model, output_stream, -1)
-                log.info('Successfully saved model to file \"%s\"', file_path)
-        except IOError:
-            log.error('Failed to save model to file \"%s\"', file_path)
