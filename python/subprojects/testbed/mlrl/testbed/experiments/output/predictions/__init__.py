9aa4c596588e8f6f591990b4c4aadab8887ad48e
Michael Rapp <michael.rapp90@googlemail.com>
2025-04-17 23:40:40 +0200

    Rename file.

--- python/subprojects/testbed/mlrl/testbed/experiments/output/predictions/__init__.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/output/predictions/__init__.py
@@ -3,4 +3,4 @@ Author Michael Rapp (michael.rapp.ml@gmail.com)
 
 Provides classes that allow to write predictions to different sinks.
 """
-from mlrl.testbed.experiments.output.predictions.writer import PredictionWriter
+from mlrl.testbed.experiments.output.predictions.writer_prediction import PredictionWriter
