55fc2924e4ea87a14328479796883a0532d8e3a1
Michael Rapp <michael.rapp90@googlemail.com>
2025-04-17 22:38:11 +0200

    Move class ArffFileSink into new file sink_arff.py.

--- python/subprojects/testbed/mlrl/testbed/util/io.py
+++ python/subprojects/testbed/mlrl/testbed/util/io.py
@@ -6,9 +6,6 @@ Provides utility functions for reading and writing files.
 from os import path
 from typing import Optional
 
-# The suffix of an ARFF file
-SUFFIX_ARFF = 'arff'
-
 # The suffix of an XML file
 SUFFIX_XML = 'xml'
 
