83c31dbd6e3c5c9ea2a6b386efac56fc19a847f4
Michael Rapp <michael.rapp90@googlemail.com>
2025-04-20 23:28:10 +0200

    Add class InputDataset.

---
+++ python/subprojects/testbed/mlrl/testbed/experiments/input/dataset/__init__.py
@@ -0,0 +1,6 @@
+"""
+Author Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides classes that allow to read datasets from different sources.
+"""
+from mlrl.testbed.experiments.input.dataset.dataset import InputDataset
