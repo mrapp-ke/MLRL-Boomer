196b88b84d99cee9276a9f10b6f5c443e10320a0
Michael Rapp <michael.rapp90@googlemail.com>
2025-04-23 00:34:43 +0200

    Fix broken link.

--- cpp/subprojects/common/include/mlrl/common/data/view_vector_sparse_set.hpp
+++ cpp/subprojects/common/include/mlrl/common/data/view_vector_sparse_set.hpp
@@ -18,7 +18,7 @@
  *
  * This data structure is often referred to as an "unordered sparse set". It was originally proposed in "An efficient
  * representation for sparse sets", Briggs, Torczon, 1993 (see
- * https://citeseerx.ist.psu.edu/document?repid=rep1&type=pdf&doi=c83ae968c51219db68a03fc5b01de751dd2fe9ae).
+ * https://scholar.archive.org/work/fl4opoka5zbqxebcvo7pji7oga/access/wayback/http://www.dcs.gla.ac.uk:80/~pat/ads2/papers/sparseSets.pdf).
  *
  * @tparam T            The type of the values, the view provides access to
  * @tparam ValueArray   The type of the dynamic array that stores the sparse elements
