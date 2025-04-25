a2d9c4db5f9346f24c8664473ffb4b36b46ba594
Michael Rapp <michael.rapp90@googlemail.com>
2025-04-25 16:41:25 +0200

    Remove links to papers from source code.

--- cpp/subprojects/seco/include/mlrl/seco/heuristics/heuristic_precision.hpp
+++ cpp/subprojects/seco/include/mlrl/seco/heuristics/heuristic_precision.hpp
@@ -14,10 +14,9 @@ namespace seco {
      * Allows to configure a heuristic that measures the fraction of correctly predicted labels among all labels that
      * are covered by a rule.
      *
-     * This heuristic is equivalent to RIPPER's pruning heuristic ("Fast Effective Rule Induction", Cohen 1995, see
-     * https://sci2s.ugr.es/keel/pdf/algorithm/congreso/ml-95-ripper.pdf). A proof is provided in the paper "Roc 'n'
-     * Rule Learning — Towards a Better Understanding of Covering Algorithms", Fürnkranz, Flach 2005 (see
-     * https://link.springer.com/content/pdf/10.1007/s10994-005-5011-x.pdf).
+     * This heuristic is equivalent to RIPPER's pruning heuristic (see "Fast Effective Rule Induction", Cohen, 1995). A
+     * proof is provided in the paper "Roc 'n' Rule Learning — Towards a Better Understanding of Covering Algorithms",
+     * Fürnkranz, Flach, 2005.
      */
     class PrecisionConfig final : public IHeuristicConfig {
         public:
