dd60e6f1da7d7add4a696d947249472d75dfff3f
Michael Rapp <michael.rapp90@googlemail.com>
2025-04-30 01:26:06 +0200

    Move class NoSplitter into module "mlrl.testbed.experiments.input.dataset.splitters".

--- python/subprojects/testbed/mlrl/testbed/experiments/input/dataset/splitters/__init__.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/input/dataset/splitters/__init__.py
@@ -4,3 +4,4 @@ Author Michael Rapp (michael.rapp.ml@gmail.com)
 Provides classes that allow to split datasets into training and test datasets.
 """
 from mlrl.testbed.experiments.input.dataset.splitters.splitter import DatasetSplitter
+from mlrl.testbed.experiments.input.dataset.splitters.splitter_no import NoSplitter
--- python/subprojects/testbed/mlrl/testbed/runnables.py
+++ python/subprojects/testbed/mlrl/testbed/runnables.py
@@ -24,7 +24,7 @@ from mlrl.common.learners import RuleLearner, SparsePolicy
 from mlrl.common.package_info import PythonPackageInfo
 from mlrl.common.util.format import format_dict_keys, format_enum_values, format_iterable
 
-from mlrl.testbed.data_splitting import CrossValidationSplitter, NoSplitter, TrainTestSplitter
+from mlrl.testbed.data_splitting import CrossValidationSplitter, TrainTestSplitter
 from mlrl.testbed.experiment import Experiment
 from mlrl.testbed.experiments.input.dataset import DatasetReader, InputDataset
 from mlrl.testbed.experiments.input.dataset.preprocessors import OneHotEncoder, Preprocessor
@@ -32,7 +32,7 @@ from mlrl.testbed.experiments.input.model import ModelReader
 from mlrl.testbed.experiments.input.parameters import ParameterReader
 from mlrl.testbed.experiments.input.reader import InputReader
 from mlrl.testbed.experiments.input.sources import ArffFileSource, CsvFileSource, PickleFileSource
-from mlrl.testbed.experiments.inputs.dataset.splitters import DatasetSplitter
+from mlrl.testbed.experiments.inputs.dataset.splitters import DatasetSplitter, NoSplitter
 from mlrl.testbed.experiments.output.characteristics.data import DataCharacteristics, DataCharacteristicsWriter, \
     OutputCharacteristics, PredictionCharacteristicsWriter
 from mlrl.testbed.experiments.output.characteristics.model import ModelCharacteristicsWriter, \
