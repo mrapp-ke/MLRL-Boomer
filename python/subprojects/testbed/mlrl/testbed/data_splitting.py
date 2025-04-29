d6e934d22a140a49dfe490c1ba2ab1a61df5e2c0
Michael Rapp <michael.rapp90@googlemail.com>
2025-04-30 01:26:06 +0200

    Move class NoSplitter into module "mlrl.testbed.experiments.input.dataset.splitters".

--- python/subprojects/testbed/mlrl/testbed/data_splitting.py
+++ python/subprojects/testbed/mlrl/testbed/data_splitting.py
@@ -20,46 +20,6 @@ from mlrl.testbed.experiments.problem_type import ProblemType
 from mlrl.testbed.experiments.state import ExperimentState
 
 
-class NoSplitter(DatasetSplitter):
-    """
-    Does not split a dataset.
-    """
-
-    class Split(DatasetSplitter.Split):
-        """
-        A split that does not use separate training and test datasets.
-        """
-
-        def __init__(self, state: ExperimentState):
-            """
-            :param state: The state that stores the dataset
-            """
-            self.state = state
-
-        def get_state(self, _: DatasetType) -> ExperimentState:
-            return self.state
-
-    def __init__(self, dataset_reader: DatasetReader):
-        """
-        :param dataset_reader: The reader that should be used for loading datasets
-        """
-        self.dataset_reader = dataset_reader
-        self.folding_strategy = FoldingStrategy(num_folds=1, first=0, last=1)
-        context = dataset_reader.input_data.context
-        context.include_dataset_type = False
-        context.include_fold = False
-
-    def split(self, problem_type: ProblemType) -> Generator[DatasetSplitter.Split]:
-        log.warning('Not using separate training and test sets. The model will be evaluated on the training data...')
-        folding_strategy = self.folding_strategy
-        state = ExperimentState(problem_type=problem_type, folding_strategy=folding_strategy)
-        self.dataset_reader.read(state)
-        state = replace(state, dataset=state.dataset, dataset_type=DatasetType.TRAINING)
-
-        for fold in folding_strategy.folds:
-            yield NoSplitter.Split(state=replace(state, fold=fold))
-
-
 class TrainTestSplitter(DatasetSplitter):
     """
     Splits a dataset into distinct training and test datasets.
