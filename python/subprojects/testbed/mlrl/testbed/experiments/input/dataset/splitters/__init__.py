73edb6751e650a84a39143c131b0884e150bc3d8
Michael Rapp <michael.rapp90@googlemail.com>
2025-04-30 01:30:47 +0200

    Replace class TrainTestSplitter with new class BipartitionSplitter.

--- python/subprojects/testbed/mlrl/testbed/experiments/input/dataset/splitters/__init__.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/input/dataset/splitters/__init__.py
@@ -4,4 +4,5 @@ Author Michael Rapp (michael.rapp.ml@gmail.com)
 Provides classes that allow to split datasets into training and test datasets.
 """
 from mlrl.testbed.experiments.input.dataset.splitters.splitter import DatasetSplitter
+from mlrl.testbed.experiments.input.dataset.splitters.splitter_bipartition import BipartitionSplitter
 from mlrl.testbed.experiments.input.dataset.splitters.splitter_no import NoSplitter
