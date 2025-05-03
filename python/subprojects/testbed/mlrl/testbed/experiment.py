c73037aa69732e5c0b8d5848908b022e8ceafcfd
Michael Rapp <michael.rapp90@googlemail.com>
2025-05-03 23:05:55 +0200

    Add class SkLearnExperiment.

--- python/subprojects/testbed/mlrl/testbed/experiment.py
+++ python/subprojects/testbed/mlrl/testbed/experiment.py
@@ -5,10 +5,10 @@ Provides classes for performing experiments.
 """
 import logging as log
 
-from abc import ABC
+from abc import ABC, abstractmethod
 from dataclasses import replace
 from functools import reduce
-from typing import Any, Dict, List, Optional
+from typing import Any, Callable, Dict, Generator, List, Optional
 
 from sklearn.base import BaseEstimator, clone
 
@@ -20,14 +20,13 @@ from mlrl.testbed.experiments.input.reader import InputReader
 from mlrl.testbed.experiments.output.writer import OutputWriter
 from mlrl.testbed.experiments.prediction import Predictor
 from mlrl.testbed.experiments.problem_type import ProblemType
-from mlrl.testbed.experiments.state import ExperimentState, TrainingState
+from mlrl.testbed.experiments.state import ExperimentState, ParameterDict, PredictionState, TrainingState
 from mlrl.testbed.experiments.timer import Timer
 
 
-class Experiment:
+class Experiment(ABC):
     """
-    An experiment that trains and evaluates a machine learning model on a specific data set using cross validation or
-    separate training and test sets.
+    An abstract base class for all experiments that train and evaluate a machine learning model.
     """
 
     class Listener(ABC):
@@ -102,42 +101,27 @@ class Experiment:
 
             return state
 
-    def __init__(self,
-                 problem_type: ProblemType,
-                 base_learner: BaseEstimator,
-                 learner_name: str,
-                 dataset_splitter: DatasetSplitter,
-                 prediction_output_writers: List[OutputWriter],
-                 train_predictor: Optional[Predictor] = None,
-                 test_predictor: Optional[Predictor] = None,
-                 fit_kwargs: Optional[Dict[str, Any]] = None,
-                 predict_kwargs: Optional[Dict[str, Any]] = None):
+    def __predict(self, state: ExperimentState):
+        for prediction_result in self._predict(learner=state.training_result.learner, dataset=state.dataset):
+            new_state = replace(state, prediction_result=prediction_result)
+
+            for output_writer in self.prediction_output_writers:
+                output_writer.write(new_state)
+
+    def __init__(self, problem_type: ProblemType, learner_name: str, dataset_splitter: DatasetSplitter,
+                 prediction_output_writers: List[OutputWriter]):
         """
         :param problem_type:                    The type of the machine learning problem
-        :param base_learner:                    The machine learning algorithm to be used
         :param learner_name:                    The name of the machine learning algorithm
         :param dataset_splitter:                The method to be used for splitting the dataset into training and test
                                                 datasets
         :param prediction_output_writers:       A list that contains all output writers to be invoked each time
                                                 predictions are obtained from a model
-        :param train_predictor:                 The `Predictor` to be used for obtaining predictions for the training
-                                                data or None, if no such predictions should be obtained
-        :param test_predictor:                  The `Predictor` to be used for obtaining predictions for the test data
-                                                or None, if no such predictions should be obtained
-        :param fit_kwargs:                      Optional keyword arguments to be passed to the learner when fitting a
-                                                model
-        :param predict_kwargs:                  Optional keyword arguments to be passed to the learner when obtaining
-                                                predictions from a model
         """
         self.problem_type = problem_type
-        self.base_learner = base_learner
         self.learner_name = learner_name
         self.dataset_splitter = dataset_splitter
         self.prediction_output_writers = prediction_output_writers
-        self.train_predictor = train_predictor
-        self.test_predictor = test_predictor
-        self.fit_kwargs = fit_kwargs
-        self.predict_kwargs = predict_kwargs
         self.input_readers = []
         self.pre_training_output_writers = []
         self.post_training_output_writers = []
@@ -191,9 +175,14 @@ class Experiment:
         return self
 
     # pylint: disable=too-many-branches
-    def run(self):
+    def run(self, predict_for_training_dataset: bool = False, predict_for_test_dataset: bool = True):
         """
         Runs the experiment.
+
+        :param predict_for_training_dataset:    True, if predictions should be obtained for the training dataset, False
+                                                otherwise
+        :param predict_for_test_dataset:        True, if predictions should be obtained for the test dataset, if
+                                                available, False otherwise
         """
         log.info('Starting experiment using the %s algorithm "%s"...', self.problem_type.value, self.learner_name)
 
@@ -208,40 +197,24 @@ class Experiment:
             for listener in self.listeners:
                 training_state = listener.on_start(self, training_state)
 
-            learner = self._create_learner(training_state)
-
             for listener in self.listeners:
                 training_state = listener.before_training(self, training_state)
 
-            # Load model from disk, if possible, otherwise train a new model...
-            loaded_learner = training_state.training_result.learner if training_state.training_result else None
-
-            if isinstance(loaded_learner, type(learner)):
-                self.__check_for_parameter_changes(expected_params=training_state.parameters,
-                                                   actual_params=loaded_learner.get_params())
-                training_result = TrainingState(learner=loaded_learner)
-            else:
-                training_dataset = training_state.dataset
-                log.info('Fitting model to %s training examples...', training_dataset.num_examples)
-                training_duration = self.__train(learner, training_dataset)
-                training_result = TrainingState(learner=learner, training_duration=training_duration)
-                log.info('Successfully fit model in %s', training_duration)
-
+            # Train model...
+            training_result = self._train(
+                learner=training_state.training_result.learner if training_state.training_result else None,
+                parameters=training_state.parameters,
+                dataset=training_state.dataset)
             training_state = replace(training_state, training_result=training_result)
-            test_state = replace(split.get_state(DatasetType.TEST), training_result=training_result)
+            test_state = split.get_state(DatasetType.TEST)
 
             # Obtain and evaluate predictions for training data, if necessary...
-            test_dataset = test_state.dataset
-            train_predictor = self.train_predictor
-
-            if train_predictor and test_dataset.type != DatasetType.TRAINING:
-                self.__predict_and_evaluate(training_state, train_predictor)
+            if predict_for_training_dataset or (predict_for_test_dataset and not test_state):
+                self.__predict(training_state)
 
             # Obtain and evaluate predictions for test data, if necessary...
-            test_predictor = self.test_predictor
-
-            if test_predictor:
-                self.__predict_and_evaluate(test_state, test_predictor)
+            if test_state and predict_for_test_dataset:
+                self.__predict(replace(test_state, training_result=training_result))
 
             for listener in self.listeners:
                 listener.after_training(self, training_state)
@@ -249,15 +222,37 @@ class Experiment:
         run_time = Timer.stop(start_time)
         log.info('Successfully finished after %s', run_time)
 
-    def _create_learner(self, state: ExperimentState) -> Any:
+    @abstractmethod
+    def _train(self, learner: Optional[Any], parameters: ParameterDict, dataset: Dataset) -> TrainingState:
         """
-        Must be implemented by subclasses in order to create the learner to be used in the experiment.
+        Must be implemented by subclasses in order to fit a learner to a training dataset.
 
-        :param state:   The state that should be used to store the learner
-        :return:        The learner that has been created
+        :param learner: An existing learner or None, if a new learner must be trained from scratch
+        :param dataset: The training dataset
+        :return:        A `TrainingState` that stores the result of the training process
         """
+
+    @abstractmethod
+    def _predict(self, learner: Any, dataset: Dataset) -> Generator[PredictionState]:
+        """
+        Must be implemented by subclasses in order to obtain predictions for given query examples from a previously
+        trained learner.
+
+        :param learner: The learner
+        :param dataset: The dataset that contains the query examples
+        :return:        The `PredictionState` that stores the result of the prediction process
+        """
+
+
+class SkLearnExperiment(Experiment):
+    """
+    An experiment that trains and evaluates a machine learning model using the scikit-learn framework.
+    """
+
+    PredictorFactory = Callable[[], Predictor]
+
+    def __create_learner(self, parameters: ParameterDict) -> BaseEstimator:
         learner = clone(self.base_learner)
-        parameters = state.parameters
 
         if parameters:
             learner.set_params(**parameters)
@@ -265,38 +260,27 @@ class Experiment:
 
         return learner
 
-    def __predict_and_evaluate(self, state: ExperimentState, predictor: Predictor):
-        """
-        Obtains predictions for given query examples from a previously trained model.
+    @staticmethod
+    def __check_for_parameter_changes(expected_parameters: Dict[str, Any], actual_parameters: Dict[str, Any]):
+        changes = []
 
-        :param state:       The state that stores the model
-        :param predictor:   The `Predictor` to be used for obtaining the predictions
-        """
-        learner = state.training_result.learner
-        dataset = state.dataset
-        predict_kwargs = self.predict_kwargs if self.predict_kwargs else {}
+        for key, expected_value in expected_parameters.items():
+            expected_value = str(expected_value)
+            actual_value = str(actual_parameters[key])
 
-        try:
-            for prediction_state in predictor.obtain_predictions(learner, dataset, **predict_kwargs):
-                new_state = replace(state, prediction_result=prediction_state)
+            if actual_value != expected_value:
+                changes.append((key, expected_value, actual_value))
 
-                for output_writer in self.prediction_output_writers:
-                    output_writer.write(new_state)
-        except ValueError as error:
-            if dataset.has_sparse_features:
-                dense_dataset = replace(state, dataset=dataset.enforce_dense_features())
-                Experiment.__predict_and_evaluate(dense_dataset, predictor, **predict_kwargs)
-
-            raise error
+        if changes:
+            log.warning(
+                'The loaded model\'s values for the following parameters differ from the expected configuration: %s',
+                reduce(
+                    lambda aggr, change: aggr +
+                    (', '
+                     if aggr else '') + '"' + change[0] + '" is "' + change[2] + '" instead of "' + change[1] + '"',
+                    changes, ''))
 
     def __train(self, learner: BaseEstimator, dataset: Dataset) -> Timer.Duration:
-        """
-        Fits a learner to training data.
-
-        :param learner: The learner
-        :param dataset: The training dataset
-        :return:        The time needed for training
-        """
         # Set the indices of ordinal features, if supported...
         fit_kwargs = self.fit_kwargs if self.fit_kwargs else {}
 
@@ -315,27 +299,64 @@ class Experiment:
             return Timer.stop(start_time)
         except ValueError as error:
             if dataset.has_sparse_features:
-                return Experiment.__train(learner, dataset.enforce_dense_features(), **fit_kwargs)
+                return self.__train(learner, dataset.enforce_dense_features())
             if dataset.has_sparse_outputs:
-                return Experiment.__train(learner, dataset.enforce_dense_outputs(), **fit_kwargs)
+                return self.__train(learner, dataset.enforce_dense_outputs())
             raise error
 
-    @staticmethod
-    def __check_for_parameter_changes(expected_params, actual_params):
-        changes = []
+    def __init__(self,
+                 problem_type: ProblemType,
+                 base_learner: BaseEstimator,
+                 learner_name: str,
+                 dataset_splitter: DatasetSplitter,
+                 prediction_output_writers: List[OutputWriter],
+                 predictor_factory: PredictorFactory,
+                 fit_kwargs: Optional[Dict[str, Any]] = None,
+                 predict_kwargs: Optional[Dict[str, Any]] = None):
+        """
+        :param problem_type:                    The type of the machine learning problem
+        :param base_learner:                    The machine learning algorithm to be used
+        :param learner_name:                    The name of the machine learning algorithm
+        :param dataset_splitter:                The method to be used for splitting the dataset into training and test
+                                                datasets
+        :param prediction_output_writers:       A list that contains all output writers to be invoked each time
+                                                predictions are obtained from a model
+        :param predictor_factory:               A `PredictorFactory`
+        :param fit_kwargs:                      Optional keyword arguments to be passed to the learner when fitting a
+                                                model
+        :param predict_kwargs:                  Optional keyword arguments to be passed to the learner when obtaining
+                                                predictions from a model
+        """
+        super().__init__(problem_type=problem_type,
+                         learner_name=learner_name,
+                         dataset_splitter=dataset_splitter,
+                         prediction_output_writers=prediction_output_writers)
+        self.base_learner = base_learner
+        self.predictor_factory = predictor_factory
+        self.fit_kwargs = fit_kwargs
+        self.predict_kwargs = predict_kwargs
 
-        for key, expected_value in expected_params.items():
-            expected_value = str(expected_value)
-            actual_value = str(actual_params[key])
+    def _train(self, learner: Optional[Any], parameters: ParameterDict, dataset: Dataset) -> TrainingState:
+        new_learner = self.__create_learner(parameters=parameters)
 
-            if actual_value != expected_value:
-                changes.append((key, expected_value, actual_value))
+        # Use existing model, if possible, otherwise train a new model...
+        if isinstance(learner, type(new_learner)):
+            self.__check_for_parameter_changes(expected_parameters=parameters, actual_parameters=learner.get_params())
+            return TrainingState(learner=learner)
 
-        if changes:
-            log.warning(
-                'The loaded model\'s values for the following parameters differ from the expected configuration: %s',
-                reduce(
-                    lambda aggr, change: aggr +
-                    (', '
-                     if aggr else '') + '"' + change[0] + '" is "' + change[2] + '" instead of "' + change[1] + '"',
-                    changes, ''))
+        log.info('Fitting model to %s training examples...', dataset.num_examples)
+        training_duration = self.__train(new_learner, dataset)
+        log.info('Successfully fit model in %s', training_duration)
+        return TrainingState(learner=new_learner, training_duration=training_duration)
+
+    def _predict(self, learner: Any, dataset: Dataset) -> Generator[PredictionState]:
+        predict_kwargs = self.predict_kwargs if self.predict_kwargs else {}
+        predictor = self.predictor_factory()
+
+        try:
+            return predictor.obtain_predictions(learner, dataset, **predict_kwargs)
+        except ValueError as error:
+            if dataset.has_sparse_features:
+                return self._predict(learner, dataset.enforce_dense_features())
+
+            raise error
