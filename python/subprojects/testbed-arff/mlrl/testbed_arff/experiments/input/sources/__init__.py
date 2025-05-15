5cc5e54cab03c470dce41f44358155c1e062ee83
Michael Rapp <michael.rapp90@googlemail.com>
2025-05-15 21:37:00 +0200

    Move classes ArffFileSink and ArffFileSource to package "mlrl-testbed-arff".

---
+++ python/subprojects/testbed-arff/mlrl/testbed_arff/experiments/input/sources/__init__.py
@@ -0,0 +1,6 @@
+"""
+Author Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides classes that allow to read input data from ARFF files.
+"""
+from mlrl.testbed_arff.experiments.input.sources.source_arff import ArffFileSource
