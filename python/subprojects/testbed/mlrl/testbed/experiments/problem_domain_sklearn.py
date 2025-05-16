7418993a95f74e44a56ce0b85c6bc4797131c627
Michael Rapp <michael.rapp90@googlemail.com>
2025-05-16 20:57:25 +0200

    Move field "data_splitter" from class ProblemDomain into class Experiment.

--- python/subprojects/testbed/mlrl/testbed/experiments/problem_domain_sklearn.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/problem_domain_sklearn.py
@@ -7,7 +7,6 @@ from typing import Any, Callable, Dict, Optional
 
 from sklearn.base import BaseEstimator
 
-from mlrl.testbed.experiments.input.dataset.splitters.splitter import DatasetSplitter
 from mlrl.testbed.experiments.prediction import Predictor
 from mlrl.testbed.experiments.problem_domain import ProblemDomain
 from mlrl.testbed.experiments.problem_type import ProblemType
@@ -23,21 +22,19 @@ class SkLearnProblem(ProblemDomain):
 
     def __init__(self,
                  problem_type: ProblemType,
-                 dataset_splitter: DatasetSplitter,
                  base_learner: BaseEstimator,
                  predictor_factory: PredictorFactory,
                  fit_kwargs: Optional[Dict[str, Any]] = None,
                  predict_kwargs: Optional[Dict[str, Any]] = None):
         """
         :param problem_type:        The type of the machine learning problem
-        :param dataset_splitter:    The method to be used for splitting the dataset into training and test datasets
         :param base_learner:        A sklearn estimator to be used in the experiment
         :param predictor_factory:   A `PredictorFactory`
         :param fit_kwargs:          Optional keyword arguments to be passed to the learner when fitting a model
         :param predict_kwargs:      Optional keyword arguments to be passed to the learner when obtaining predictions
                                     from a model
         """
-        super().__init__(problem_type=problem_type, dataset_splitter=dataset_splitter)
+        super().__init__(problem_type=problem_type)
         self.base_learner = base_learner
         self.predictor_factory = predictor_factory
         self.fit_kwargs = fit_kwargs
