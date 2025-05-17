6a478771785724ea654a5567a0ab6ddd39919982
Michael Rapp <michael.rapp90@googlemail.com>
2025-05-17 18:26:16 +0200

    Remove obsolete class DatasetInputData.Properties.

--- python/subprojects/testbed/mlrl/testbed/experiments/input/dataset/dataset.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/input/dataset/dataset.py
@@ -3,10 +3,9 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 
 Provides classes for representing datasets that are part of input data.
 """
-import numpy as np
 
 from mlrl.testbed.experiments.context import Context
-from mlrl.testbed.experiments.input.data import DatasetInputData
+from mlrl.testbed.experiments.input.data import DatasetInputData, InputData
 
 
 class InputDataset(DatasetInputData):
@@ -18,7 +17,5 @@ class InputDataset(DatasetInputData):
         """
         :param dataset_name: The name of the dataset
         """
-        super().__init__(DatasetInputData.Properties(file_name=dataset_name,
-                                                     feature_dtype=np.float32,
-                                                     output_dtype=np.uint8),
+        super().__init__(properties=InputData.Properties(file_name=dataset_name),
                          context=Context(include_prediction_scope=False))
