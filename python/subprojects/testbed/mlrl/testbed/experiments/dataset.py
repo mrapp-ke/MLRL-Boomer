e3dd8cbbeb8f1736f58c565b28011b527d9c0de3
Michael Rapp <michael.rapp90@googlemail.com>
2025-05-18 23:47:15 +0200

    Replace class Dataset with a type alias.

--- python/subprojects/testbed/mlrl/testbed/experiments/dataset.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/dataset.py
@@ -3,10 +3,6 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 
 Provides classes for representing datasets.
 """
-from abc import ABC
+from typing import Any
 
-
-class Dataset(ABC):
-    """
-    An abstract base class for all datasets.
-    """
+Dataset = Any
