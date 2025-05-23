76beb393be3f91b8c814e52ca7421f3f11eab4f9
Michael Rapp <michael.rapp90@googlemail.com>
2025-05-23 23:25:33 +0200

    Add class Profile.

---
+++ python/subprojects/testbed/mlrl/testbed/profiles/__init__.py
@@ -0,0 +1,6 @@
+"""
+Author Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides classes for implementing profiles that add functionality to the command line API.
+"""
+from mlrl.testbed.profiles.profile import Profile
