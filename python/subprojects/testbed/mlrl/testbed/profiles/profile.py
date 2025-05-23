db37c3abd5c745a70471165b6301126b97e45d30
Michael Rapp <michael.rapp90@googlemail.com>
2025-05-24 00:39:05 +0200

    Add abstract function "configure_experiment" to class Profile.

--- python/subprojects/testbed/mlrl/testbed/profiles/profile.py
+++ python/subprojects/testbed/mlrl/testbed/profiles/profile.py
@@ -5,7 +5,9 @@ Provides classes for implementing profiles that add functionality to the command
 package.
 """
 from abc import ABC, abstractmethod
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
+
+from mlrl.testbed.experiments.experiment import Experiment
 
 
 class Profile(ABC):
@@ -19,4 +21,14 @@ class Profile(ABC):
         Must be implemented by subclasses in order to configure a given argument parser.
 
         :param argument_parser: The argument parser to be configured
+        """
+
+    @abstractmethod
+    def configure_experiment(self, args: Namespace, experiment: Experiment):
+        """
+        Must be implemented by subclasses in order to configure an experiment according to the command line arguments
+        specified by the user.
+
+        :param args:        The command line arguments specified by the user
+        :param experiment:  The experiment to be configured
         """
