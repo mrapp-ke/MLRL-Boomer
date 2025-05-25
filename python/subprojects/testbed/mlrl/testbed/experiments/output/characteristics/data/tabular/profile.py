e3f1c97afeed4423095aefde8828c4949de02f47
Michael Rapp <michael.rapp90@googlemail.com>
2025-05-26 00:38:05 +0200

    Add class TabularDataCharacteristicsProfile.

---
+++ python/subprojects/testbed/mlrl/testbed/experiments/output/characteristics/data/tabular/profile.py
@@ -0,0 +1,90 @@
+"""
+Author: Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides classes that allow configuring the functionality to write characteristics of tabular datasets to outputs.
+"""
+from argparse import ArgumentParser, Namespace
+from typing import Dict, List, Set
+
+from mlrl.testbed.experiments.experiment import Experiment
+from mlrl.testbed.experiments.output.characteristics.data.tabular.characteristics import OutputCharacteristics
+from mlrl.testbed.experiments.output.characteristics.data.tabular.characteristics_data import DataCharacteristics
+from mlrl.testbed.experiments.output.characteristics.data.tabular.writer_data import DataCharacteristicsWriter
+from mlrl.testbed.experiments.output.sinks.sink import Sink
+from mlrl.testbed.experiments.output.sinks.sink_csv import CsvFileSink
+from mlrl.testbed.experiments.output.sinks.sink_log import LogSink
+from mlrl.testbed.profiles.profile import Profile
+from mlrl.testbed.util.format import OPTION_DECIMALS, OPTION_PERCENTAGE
+
+from mlrl.util.format import format_set
+from mlrl.util.options import BooleanOption, parse_param_and_options
+
+
+class TabularDataCharacteristicProfile(Profile):
+    """
+    A profile that configures the functionality to write characteristics of tabular datasets to outputs.
+    """
+
+    PARAM_PRINT_DATA_CHARACTERISTICS = '--print-data-characteristics'
+
+    PRINT_DATA_CHARACTERISTICS_VALUES: Dict[str, Set[str]] = {
+        BooleanOption.TRUE.value: {
+            DataCharacteristics.OPTION_EXAMPLES, DataCharacteristics.OPTION_FEATURES,
+            DataCharacteristics.OPTION_NUMERICAL_FEATURES, DataCharacteristics.OPTION_NOMINAL_FEATURES,
+            DataCharacteristics.OPTION_FEATURE_DENSITY, DataCharacteristics.OPTION_FEATURE_SPARSITY,
+            OutputCharacteristics.OPTION_OUTPUTS, OutputCharacteristics.OPTION_OUTPUT_DENSITY,
+            OutputCharacteristics.OPTION_OUTPUT_SPARSITY, OutputCharacteristics.OPTION_LABEL_IMBALANCE_RATIO,
+            OutputCharacteristics.OPTION_LABEL_CARDINALITY, OutputCharacteristics.OPTION_DISTINCT_LABEL_VECTORS,
+            OPTION_DECIMALS, OPTION_PERCENTAGE
+        },
+        BooleanOption.FALSE.value: {}
+    }
+
+    PARAM_STORE_DATA_CHARACTERISTICS = '--store-data-characteristics'
+
+    STORE_DATA_CHARACTERISTICS_VALUES = PRINT_DATA_CHARACTERISTICS_VALUES
+
+    def configure_arguments(self, argument_parser: ArgumentParser):
+        """
+        See :func:`mlrl.testbed.profiles.profile.Profile.configure_arguments`
+        """
+        argument_parser.add_argument(
+            self.PARAM_PRINT_DATA_CHARACTERISTICS,
+            type=str,
+            default=BooleanOption.FALSE.value,
+            help='Whether the characteristics of the training data should be printed on the console or not. Must be '
+            + 'one of ' + format_set(self.PRINT_DATA_CHARACTERISTICS_VALUES.keys()) + '. For additional options refer '
+            + 'to the documentation.')
+        argument_parser.add_argument(
+            self.PARAM_STORE_DATA_CHARACTERISTICS,
+            type=str,
+            default=BooleanOption.FALSE.value,
+            help='Whether the characteristics of the training data should be written into output files or not. Must be '
+            + 'one of ' + format_set(self.STORE_DATA_CHARACTERISTICS_VALUES.keys()) + '. For additional options refer '
+            + 'to the documentation.')
+
+    def __create_log_sinks(self, args: Namespace) -> List[Sink]:
+        value, options = parse_param_and_options(self.PARAM_PRINT_DATA_CHARACTERISTICS, args.print_data_characteristics,
+                                                 self.PRINT_DATA_CHARACTERISTICS_VALUES)
+
+        if (not value and args.print_all) or value == BooleanOption.TRUE.value:
+            return [LogSink(options)]
+        return []
+
+    def __create_csv_file_sinks(self, args: Namespace) -> List[Sink]:
+        value, options = parse_param_and_options(self.PARAM_STORE_DATA_CHARACTERISTICS, args.store_data_characteristics,
+                                                 self.STORE_DATA_CHARACTERISTICS_VALUES)
+
+        if ((not value and args.store_all) or value == BooleanOption.TRUE.value) and args.output_dir:
+            return [CsvFileSink(directory=args.output_dir, create_directory=args.create_output_dir, options=options)]
+        return []
+
+    def configure_experiment(self, args: Namespace, experiment: Experiment):
+        """
+        See :func:`mlrl.testbed.profiles.profile.Profile.configure_experiment`
+        """
+        sinks = self.__create_log_sinks(args) + self.__create_csv_file_sinks(args)
+
+        if sinks:
+            writer = DataCharacteristicsWriter().add_sinks(*sinks)
+            experiment.add_pre_training_output_writers(writer)
