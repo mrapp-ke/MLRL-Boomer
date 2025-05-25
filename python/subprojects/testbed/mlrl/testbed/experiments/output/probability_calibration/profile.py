fb439c07b913f1488008b5611e3bc75c757929c0
Michael Rapp <michael.rapp90@googlemail.com>
2025-05-25 23:10:08 +0200

    Add class JointProbabilityCalibrationModelProfile.

--- python/subprojects/testbed/mlrl/testbed/experiments/output/probability_calibration/profile.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/output/probability_calibration/profile.py
@@ -8,7 +8,7 @@ from typing import Dict, Set
 
 from mlrl.testbed.experiments.experiment import Experiment
 from mlrl.testbed.experiments.output.probability_calibration.extractor_rules import \
-    IsotonicMarginalProbabilityCalibrationModelExtractor
+    IsotonicJointProbabilityCalibrationModelExtractor, IsotonicMarginalProbabilityCalibrationModelExtractor
 from mlrl.testbed.experiments.output.probability_calibration.writer import ProbabilityCalibrationModelWriter
 from mlrl.testbed.experiments.output.sinks.sink_csv import CsvFileSink
 from mlrl.testbed.experiments.output.sinks.sink_log import LogSink
@@ -77,5 +77,66 @@ class MarginalProbabilityCalibrationModelProfile(Profile):
 
         if sinks:
             writer = ProbabilityCalibrationModelWriter(IsotonicMarginalProbabilityCalibrationModelExtractor(),
+                                                       exit_on_error=args.exit_on_error).add_sinks(*sinks)
+            experiment.add_post_training_output_writers(writer)
+
+
+class JointProbabilityCalibrationModelProfile(Profile):
+    """
+    A profile that configures the functionality to write models for the calibration of joint probabilities to outputs.
+    """
+
+    PARAM_PRINT_JOINT_PROBABILITY_CALIBRATION_MODEL = '--print-joint-probability-calibration-model'
+
+    PRINT_JOINT_PROBABILITY_CALIBRATION_MODEL_VALUES: Dict[str, Set[str]] = {
+        BooleanOption.TRUE.value: {OPTION_DECIMALS},
+        BooleanOption.FALSE.value: {}
+    }
+
+    PARAM_STORE_JOINT_PROBABILITY_CALIBRATION_MODEL = '--store-joint-probability-calibration-model'
+
+    STORE_JOINT_PROBABILITY_CALIBRATION_MODEL_VALUES = PRINT_JOINT_PROBABILITY_CALIBRATION_MODEL_VALUES
+
+    def configure_arguments(self, argument_parser: ArgumentParser):
+        """
+        See :func:`mlrl.testbed.profiles.profile.Profile.configure_arguments`
+        """
+        argument_parser.add_argument(
+            self.PARAM_PRINT_JOINT_PROBABILITY_CALIBRATION_MODEL,
+            type=str,
+            default=BooleanOption.FALSE.value,
+            help='Whether the model for the calibration of joint probabilities should be printed on the console or '
+            + 'not. Must be one of ' + format_enum_values(BooleanOption) + '. For additional options refer to the '
+            + 'documentation.')
+        argument_parser.add_argument(
+            self.PARAM_STORE_JOINT_PROBABILITY_CALIBRATION_MODEL,
+            type=str,
+            default=BooleanOption.FALSE.value,
+            help='Whether the model for the calibration of joint probabilities should be written into an output file '
+            + 'or not. Must be one of ' + format_enum_values(BooleanOption) + '. For additional options refer to the '
+            + 'documentation.')
+
+    def configure_experiment(self, args: Namespace, experiment: Experiment):
+        """
+        See :func:`mlrl.testbed.profiles.profile.Profile.configure_experiment`
+        """
+        sinks = []
+        value, options = parse_param_and_options(self.PARAM_PRINT_JOINT_PROBABILITY_CALIBRATION_MODEL,
+                                                 args.print_joint_probability_calibration_model,
+                                                 self.PRINT_JOINT_PROBABILITY_CALIBRATION_MODEL_VALUES)
+
+        if (not value and args.print_all) or value == BooleanOption.TRUE.value:
+            sinks.append(LogSink(options))
+
+        value, options = parse_param_and_options(self.PARAM_STORE_JOINT_PROBABILITY_CALIBRATION_MODEL,
+                                                 args.store_joint_probability_calibration_model,
+                                                 self.STORE_JOINT_PROBABILITY_CALIBRATION_MODEL_VALUES)
+
+        if ((not value and args.store_all) or value == BooleanOption.TRUE.value) and args.output_dir:
+            sinks.append(
+                CsvFileSink(directory=args.output_dir, create_directory=args.create_output_dir, options=options))
+
+        if sinks:
+            writer = ProbabilityCalibrationModelWriter(IsotonicJointProbabilityCalibrationModelExtractor(),
                                                        exit_on_error=args.exit_on_error).add_sinks(*sinks)
             experiment.add_post_training_output_writers(writer)
