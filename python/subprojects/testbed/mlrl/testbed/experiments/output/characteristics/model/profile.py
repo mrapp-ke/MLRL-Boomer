05560939f0764529dd1e5bf13ae92a40056bf786
Michael Rapp <michael.rapp90@googlemail.com>
2025-05-25 22:41:28 +0200

    Add class RuleModelCharacteristicsProfile.

---
+++ python/subprojects/testbed/mlrl/testbed/experiments/output/characteristics/model/profile.py
@@ -0,0 +1,56 @@
+"""
+Author: Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides classes that allow configuring the functionality to write characteristics of rule models to outputs.
+"""
+from argparse import ArgumentParser, Namespace
+
+from mlrl.testbed.experiments.experiment import Experiment
+from mlrl.testbed.experiments.output.characteristics.model.extractor_rules import RuleModelCharacteristicsExtractor
+from mlrl.testbed.experiments.output.characteristics.model.writer import ModelCharacteristicsWriter
+from mlrl.testbed.experiments.output.sinks.sink_csv import CsvFileSink
+from mlrl.testbed.experiments.output.sinks.sink_log import LogSink
+from mlrl.testbed.profiles.profile import Profile
+
+from mlrl.util.format import format_enum_values
+from mlrl.util.options import BooleanOption
+
+
+class RuleModelCharacteristicsProfile(Profile):
+    """
+    A profile that configures the functionality to write characteristics of rule models to outputs.
+    """
+
+    def configure_arguments(self, argument_parser: ArgumentParser):
+        """
+        See :func:`mlrl.testbed.profiles.profile.Profile.configure_arguments`
+        """
+        argument_parser.add_argument(
+            '--print-model-characteristics',
+            type=BooleanOption.parse,
+            default=BooleanOption.FALSE.value,
+            help='Whether the characteristics of models should be printed on the console or not. Must be one of '
+            + format_enum_values(BooleanOption) + '.')
+        argument_parser.add_argument(
+            '--store-model-characteristics',
+            type=BooleanOption.parse,
+            default=BooleanOption.FALSE.value,
+            help='Whether the characteristics of models should be written into output files or not. Must be one of '
+            + format_enum_values(BooleanOption) + '.')
+
+    def configure_experiment(self, args: Namespace, experiment: Experiment):
+        """
+        See :func:`mlrl.testbed.profiles.profile.Profile.configure_experiment`
+        """
+        sinks = []
+
+        if args.print_model_characteristics or args.print_all:
+            sinks.append(LogSink())
+
+        if (args.store_model_characteristics or args.store_all) and args.output_dir:
+            sinks.append(CsvFileSink(directory=args.output_dir, create_directory=args.create_output_dir))
+
+        if sinks:
+            writer = ModelCharacteristicsWriter(RuleModelCharacteristicsExtractor(),
+                                                exit_on_error=args.exit_on_error).add_sinks(*sinks)
+            experiment.add_post_training_output_writers(writer)
