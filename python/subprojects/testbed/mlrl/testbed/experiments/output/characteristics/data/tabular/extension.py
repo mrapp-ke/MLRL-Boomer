c62574ce6ebe7203aceccf3017e5dbbff5d4f1b0
Michael Rapp <michael.rapp90@googlemail.com>
2025-05-26 12:16:28 +0200

    Rename class Profile to Extension.

--- python/subprojects/testbed/mlrl/testbed/experiments/output/characteristics/data/tabular/extension.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/output/characteristics/data/tabular/extension.py
@@ -13,16 +13,16 @@ from mlrl.testbed.experiments.output.characteristics.data.tabular.writer_data im
 from mlrl.testbed.experiments.output.sinks.sink import Sink
 from mlrl.testbed.experiments.output.sinks.sink_csv import CsvFileSink
 from mlrl.testbed.experiments.output.sinks.sink_log import LogSink
-from mlrl.testbed.profiles.profile import Profile
+from mlrl.testbed.extensions.extension import Extension
 from mlrl.testbed.util.format import OPTION_DECIMALS, OPTION_PERCENTAGE
 
 from mlrl.util.format import format_set
 from mlrl.util.options import BooleanOption, parse_param_and_options
 
 
-class TabularDataCharacteristicProfile(Profile):
+class TabularDataCharacteristicExtension(Extension):
     """
-    A profile that configures the functionality to write characteristics of tabular datasets to outputs.
+    An extension that configures the functionality to write characteristics of tabular datasets to outputs.
     """
 
     PARAM_PRINT_DATA_CHARACTERISTICS = '--print-data-characteristics'
@@ -46,7 +46,7 @@ class TabularDataCharacteristicProfile(Profile):
 
     def configure_arguments(self, argument_parser: ArgumentParser):
         """
-        See :func:`mlrl.testbed.profiles.profile.Profile.configure_arguments`
+        See :func:`mlrl.testbed.extensions.extension.Extension.configure_arguments`
         """
         argument_parser.add_argument(
             self.PARAM_PRINT_DATA_CHARACTERISTICS,
@@ -81,7 +81,7 @@ class TabularDataCharacteristicProfile(Profile):
 
     def configure_experiment(self, args: Namespace, experiment: Experiment):
         """
-        See :func:`mlrl.testbed.profiles.profile.Profile.configure_experiment`
+        See :func:`mlrl.testbed.extensions.extension.Extension.configure_experiment`
         """
         sinks = self.__create_log_sinks(args) + self.__create_csv_file_sinks(args)
 
