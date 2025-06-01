62e9d1e46b145a5863dbba82ea13250e126af12d
Michael Rapp <michael.rapp90@googlemail.com>
2025-06-01 23:52:07 +0200

    Rename function argument.

--- python/subprojects/testbed/mlrl/testbed/experiments/prediction_type.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/prediction_type.py
@@ -17,17 +17,17 @@ class PredictionType(Enum):
     PROBABILITIES = 'probabilities'
 
     @staticmethod
-    def parse(parameter_name: str, value: str) -> 'PredictionType':
+    def parse(argument_name: str, value: str) -> 'PredictionType':
         """
-        Parses and returns a parameter value that specifies the `PredictionType` of the predictions to be obtained from
+        Parses and returns a given value that specifies the `PredictionType` of the predictions to be obtained from
         a model. If the given value is invalid, a `ValueError` is raised.
 
-        :param parameter_name:  The name of the parameter
+        :param argument_name:   The name of the argument, the value corresponds to
         :param value:           The value to be parsed
         :return:                A `PredictionType`
         """
         try:
             return PredictionType(value)
         except ValueError as error:
-            raise ValueError('Invalid value given for argument "' + parameter_name + '": Must be one of '
+            raise ValueError('Invalid value given for argument "' + argument_name + '": Must be one of '
                              + format_enum_values(PredictionType) + ', but is "' + str(value) + '"') from error
