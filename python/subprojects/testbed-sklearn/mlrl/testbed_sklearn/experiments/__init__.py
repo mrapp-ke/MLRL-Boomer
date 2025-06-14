ab406feab63f9fc1e1df0804f349f546e745de79
Michael Rapp <michael.rapp90@googlemail.com>
2025-06-15 00:56:54 +0200

    Move class SkLearnExperiment into package "mlrl-testbed-sklearn".

---
+++ python/subprojects/testbed-sklearn/mlrl/testbed_sklearn/experiments/__init__.py
@@ -0,0 +1,6 @@
+"""
+Author Michael Rapp (michael.rapp.ml@gmail.com)
+
+Provides classes that allow running experiments using the scikit-learn framework.
+"""
+from mlrl.testbed_sklearn.experiments.experiment import SkLearnExperiment
