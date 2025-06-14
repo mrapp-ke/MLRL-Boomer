4c6f7e0e683481497e99d7617fc872318abcdc5d
Michael Rapp <michael.rapp90@googlemail.com>
2025-06-15 01:14:53 +0200

    Move class OneHotEncoder into package "mlrl-testbed-sklearn".

--- python/subprojects/testbed-sklearn/mlrl/testbed_sklearn/experiments/input/dataset/preprocessors/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Author Michael Rapp (michael.rapp.ml@gmail.com)
-
-Provides classes that allow to preprocess tabular datasets.
-"""
-from mlrl.testbed_sklearn.experiments.input.dataset.preprocessors.one_hot_encoder import OneHotEncoder
