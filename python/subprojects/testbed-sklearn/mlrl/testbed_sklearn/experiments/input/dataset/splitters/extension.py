7d6add589fb304e147c40ca646a0aca1f863d045
Michael Rapp <michael.rapp90@googlemail.com>
2025-06-15 01:15:31 +0200

    Move classes BipartitionSplitter and CrossValidationSplitter into package "mlrl-testbed-sklearn".

--- python/subprojects/testbed-sklearn/mlrl/testbed_sklearn/experiments/input/dataset/splitters/extension.py
+++ python/subprojects/testbed-sklearn/mlrl/testbed_sklearn/experiments/input/dataset/splitters/extension.py
@@ -7,12 +7,12 @@ from argparse import Namespace
 from typing import Set
 
 from mlrl.testbed_sklearn.experiments.input.dataset.preprocessors.extension import PreprocessorExtension
+from mlrl.testbed_sklearn.experiments.input.dataset.splitters.splitter_bipartition import BipartitionSplitter
+from mlrl.testbed_sklearn.experiments.input.dataset.splitters.splitter_cross_validation import CrossValidationSplitter
 
 from mlrl.testbed.experiments.input.dataset.splitters.extension import DatasetFileExtension
 from mlrl.testbed.experiments.input.dataset.splitters.splitter import DatasetSplitter
 from mlrl.testbed.experiments.input.dataset.splitters.splitter_no import NoSplitter
-from mlrl.testbed_sklearn.experiments.input.dataset.splitters.splitter_bipartition import BipartitionSplitter
-from mlrl.testbed_sklearn.experiments.input.dataset.splitters.splitter_cross_validation import CrossValidationSplitter
 from mlrl.testbed.extensions.extension import Extension
 
 from mlrl.util.cli import NONE, Argument, IntArgument, SetArgument
