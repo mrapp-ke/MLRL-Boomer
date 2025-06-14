f4cef04eed0a2676d7cada83c59a2982023100a5
Michael Rapp <michael.rapp90@googlemail.com>
2025-06-15 01:20:31 +0200

    Move class SkLearnProblemDomain into package "mlrl-testbed-sklearn".

--- python/subprojects/testbed-sklearn/mlrl/testbed_sklearn/experiments/__init__.py
+++ python/subprojects/testbed-sklearn/mlrl/testbed_sklearn/experiments/__init__.py
@@ -4,3 +4,5 @@ Author Michael Rapp (michael.rapp.ml@gmail.com)
 Provides classes that allow running experiments using the scikit-learn framework.
 """
 from mlrl.testbed_sklearn.experiments.experiment import SkLearnExperiment
+from mlrl.testbed_sklearn.experiments.problem_domain import SkLearnClassificationProblem, SkLearnProblem, \
+    SkLearnRegressionProblem
