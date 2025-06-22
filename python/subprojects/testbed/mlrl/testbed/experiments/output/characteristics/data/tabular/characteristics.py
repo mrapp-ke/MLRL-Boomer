f596477b41f789c0509f4858fb8c256d6a4cd9a3
Michael Rapp <michael.rapp90@googlemail.com>
2025-06-22 21:13:11 +0200

    Move class Characteristic into file characteristics.py.

--- python/subprojects/testbed/mlrl/testbed/experiments/output/characteristics/data/tabular/characteristics.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/output/characteristics/data/tabular/characteristics.py
@@ -3,17 +3,38 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 
 Provides classes for representing characteristics of an output matrix that are part of output data.
 """
-from typing import Optional
+from numbers import Number
+from typing import Any, Callable, Optional
 
 from mlrl.testbed.experiments.context import Context
-from mlrl.testbed.experiments.output.characteristics.data.characteristic import Characteristic
 from mlrl.testbed.experiments.output.characteristics.data.tabular.matrix_output import OutputMatrix
-from mlrl.testbed.experiments.output.data import OutputData, TabularOutputData
+from mlrl.testbed.experiments.output.data import OutputData, OutputValue, TabularOutputData
 from mlrl.testbed.experiments.problem_domain import ClassificationProblem, ProblemDomain
 from mlrl.testbed.experiments.table import RowWiseTable, Table
 from mlrl.testbed.util.format import OPTION_DECIMALS, OPTION_PERCENTAGE
 
 from mlrl.util.options import Options
+
+
+class Characteristic(OutputValue):
+    """
+    An individual characteristic that is part of output data.
+    """
+
+    Function = Callable[[Any], Number]
+
+    def __init__(self, option_key: str, name: str, function: Function, percentage: bool = False):
+        """
+        :param function: The function that should be used to retrieve the value of the characteristic
+        """
+        super().__init__(option_key=option_key, name=name, percentage=percentage)
+        self.function = function
+
+    def format(self, value, **kwargs) -> str:
+        """
+        See :func:`mlrl.testbed.experiments.output.data.OutputValue.format`
+        """
+        return super().format(self.function(value), **kwargs)
 
 
 class OutputCharacteristics(TabularOutputData):
