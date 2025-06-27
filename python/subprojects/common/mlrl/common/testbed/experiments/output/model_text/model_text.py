a52e8d8bea7fec9f3043c57145a70c4a5b4dd711
Michael Rapp <michael.rapp90@googlemail.com>
2025-06-28 01:43:12 +0200

    Use new iterator in class RuleModelAsText.

--- python/subprojects/common/mlrl/common/testbed/experiments/output/model_text/model_text.py
+++ python/subprojects/common/mlrl/common/testbed/experiments/output/model_text/model_text.py
@@ -8,8 +8,7 @@ from typing import Optional
 
 import numpy as np
 
-from mlrl.common.cython.rule_model import CompleteHead, ConjunctiveBody, EmptyBody, PartialHead, RuleModel, \
-    RuleModelVisitor
+from mlrl.common.cython.rule_model import Body, CompleteHead, ConjunctiveBody, EmptyBody, Head, PartialHead, RuleModel
 
 from mlrl.testbed_sklearn.experiments.dataset import TabularDataset
 
@@ -39,188 +38,200 @@ class RuleModelAsText(TextualOutputData):
 
     OPTION_DECIMALS_HEAD = 'decimals_head'
 
-    class Visitor(RuleModelVisitor):
-        """
-        Access the individual rules in a `RuleModel` and creates textual representations of these rules.
-        """
+    @staticmethod
+    def __format_rule_model(rule_model: RuleModel, dataset: TabularDataset, options: Options) -> str:
+        text = StringIO()
 
-        def __format_conditions(self, indices: np.ndarray, thresholds: np.ndarray, operator: str, offset: int) -> int:
-            if indices is not None and thresholds is not None:
-                text = self.text
-                features = self.dataset.features
-                print_feature_names = self.print_feature_names
-                print_nominal_values = self.print_nominal_values
-                decimals = self.body_decimals
+        for rule in rule_model:
+            RuleModelAsText.__format_body(text, rule.body, dataset, options)
+            RuleModelAsText.__format_head(text, rule.head, dataset, options)
 
-                for i in range(indices.shape[0]):
-                    if offset > 0:
-                        text.write(' & ')
+        return text.getvalue()
 
-                    feature_index = indices[i]
-                    threshold = thresholds[i]
-                    feature = features[feature_index] if len(features) > feature_index else None
-
-                    if print_feature_names and feature:
-                        text.write(feature.name)
-                    else:
-                        text.write(str(feature_index))
-
-                    text.write(' ')
-                    text.write(operator)
-                    text.write(' ')
-
-                    if feature and feature.nominal_values:
-                        nominal_value = int(threshold)
-
-                        if print_nominal_values and len(feature.nominal_values) > nominal_value:
-                            text.write('"' + feature.nominal_values[nominal_value] + '"')
-                        else:
-                            text.write(str(nominal_value))
-                    else:
-                        text.write(format_number(threshold, decimals=decimals))
-
-                    offset += 1
-
-            return offset
-
-        def __format_numerical_conditions(self, body: ConjunctiveBody, offset: int) -> int:
-            offset = self.__format_conditions(offset=offset,
-                                              indices=body.numerical_leq_indices,
-                                              thresholds=body.numerical_leq_thresholds,
-                                              operator='<=')
-            offset = self.__format_conditions(offset=offset,
-                                              indices=body.numerical_gr_indices,
-                                              thresholds=body.numerical_gr_thresholds,
-                                              operator='>')
-            return offset
-
-        def __format_ordinal_conditions(
-            self,
-            body: ConjunctiveBody,
-            offset: int,
-        ) -> int:
-            offset = self.__format_conditions(offset=offset,
-                                              indices=body.ordinal_leq_indices,
-                                              thresholds=body.ordinal_leq_thresholds,
-                                              operator='<=')
-            offset = self.__format_conditions(offset=offset,
-                                              indices=body.ordinal_gr_indices,
-                                              thresholds=body.ordinal_gr_thresholds,
-                                              operator='>')
-            return offset
-
-        def __format_nominal_conditions(self, body: ConjunctiveBody, offset: int) -> int:
-            offset = self.__format_conditions(offset=offset,
-                                              indices=body.nominal_eq_indices,
-                                              thresholds=body.nominal_eq_thresholds,
-                                              operator='==')
-            offset = self.__format_conditions(offset=offset,
-                                              indices=body.nominal_neq_indices,
-                                              thresholds=body.nominal_neq_thresholds,
-                                              operator='!=')
-            return offset
-
-        def __init__(self, dataset: TabularDataset, options: Options):
-            """
-            :param dataset: The dataset on which the model has been trained
-            """
-            self.dataset = dataset
-            self.print_feature_names = options.get_bool(RuleModelAsText.OPTION_PRINT_FEATURE_NAMES, True)
-            self.print_output_names = options.get_bool(RuleModelAsText.OPTION_PRINT_OUTPUT_NAMES, True)
-            self.print_nominal_values = options.get_bool(RuleModelAsText.OPTION_PRINT_NOMINAL_VALUES, True)
-            self.print_bodies = options.get_bool(RuleModelAsText.OPTION_PRINT_BODIES, True)
-            self.print_heads = options.get_bool(RuleModelAsText.OPTION_PRINT_HEADS, True)
-            self.body_decimals = options.get_int(RuleModelAsText.OPTION_DECIMALS_BODY, 2)
-            self.head_decimals = options.get_int(RuleModelAsText.OPTION_DECIMALS_HEAD, 2)
-            self.text = StringIO()
-
-        def visit_empty_body(self, _: EmptyBody):
-            """
-            See :func:`mlrl.common.cython.rule_model.RuleModelVisitor.visit_empty_body`
-            """
-            if self.print_bodies:
-                self.text.write('{}')
-
-        def visit_conjunctive_body(self, body: ConjunctiveBody):
-            """
-            See :func:`mlrl.common.cython.rule_model.RuleModelVisitor.visit_conjunctive_body`
-            """
-            if self.print_bodies:
-                text = self.text
+    @staticmethod
+    def __format_body(text: StringIO, body: Body, dataset: TabularDataset, options: Options):
+        if options.get_bool(RuleModelAsText.OPTION_PRINT_BODIES, True):
+            if isinstance(body, EmptyBody):
+                text.write('{}')
+            elif isinstance(body, ConjunctiveBody):
                 text.write('{')
-                offset = self.__format_numerical_conditions(body, offset=0)
-                offset = self.__format_ordinal_conditions(body, offset=offset)
-                self.__format_nominal_conditions(body, offset=offset)
+                offset = RuleModelAsText.__format_numerical_conditions(text, body, dataset, options, offset=0)
+                offset = RuleModelAsText.__format_ordinal_conditions(text, body, dataset, options, offset=offset)
+                RuleModelAsText.__format_nominal_conditions(text, body, dataset, options, offset=offset)
                 text.write('}')
+            else:
+                raise ValueError('Unsupported type of body: ' + str(type(body)))
 
-        def visit_complete_head(self, head: CompleteHead):
-            """
-            See :func:`mlrl.common.cython.rule_model.RuleModelVisitor.visit_complete_head`
-            """
-            text = self.text
+    @staticmethod
+    def __format_head(text: StringIO, head: Head, dataset: TabularDataset, options: Options):
+        if isinstance(head, CompleteHead):
+            RuleModelAsText.__format_complete_head(text, head, dataset, options)
+        elif isinstance(head, PartialHead):
+            RuleModelAsText.__format_partial_head(text, head, dataset, options)
+        else:
+            raise ValueError('Unsupported type of head: ' + str(type(head)))
 
-            if self.print_heads:
-                print_output_names = self.print_output_names
-                decimals = self.head_decimals
-                outputs = self.dataset.outputs
-                scores = head.scores
+    @staticmethod
+    def __format_conditions(text: StringIO, indices: np.ndarray, thresholds: np.ndarray, operator: str, offset: int,
+                            dataset: TabularDataset, options: Options) -> int:
+        if indices is not None and thresholds is not None:
+            features = dataset.features
+            print_feature_names = options.get_bool(RuleModelAsText.OPTION_PRINT_FEATURE_NAMES, True)
+            print_nominal_values = options.get_bool(RuleModelAsText.OPTION_PRINT_NOMINAL_VALUES, True)
+            decimals = options.get_int(RuleModelAsText.OPTION_DECIMALS_BODY, 2)
 
-                if self.print_bodies:
-                    text.write(' => ')
+            for i in range(indices.shape[0]):
+                if offset > 0:
+                    text.write(' & ')
 
-                text.write('(')
+                feature_index = indices[i]
+                threshold = thresholds[i]
+                feature = features[feature_index] if len(features) > feature_index else None
 
-                for i in range(scores.shape[0]):
-                    if i > 0:
-                        text.write(', ')
+                if print_feature_names and feature:
+                    text.write(feature.name)
+                else:
+                    text.write(str(feature_index))
 
-                    if print_output_names and len(outputs) > i:
-                        text.write(outputs[i].name)
+                text.write(' ')
+                text.write(operator)
+                text.write(' ')
+
+                if feature and feature.nominal_values:
+                    nominal_value = int(threshold)
+
+                    if print_nominal_values and len(feature.nominal_values) > nominal_value:
+                        text.write('"' + feature.nominal_values[nominal_value] + '"')
                     else:
-                        text.write(str(i))
+                        text.write(str(nominal_value))
+                else:
+                    text.write(format_number(threshold, decimals=decimals))
 
-                    text.write(' = ')
-                    text.write(format_number(scores[i], decimals=decimals))
+                offset += 1
 
-                text.write(')\n')
-            elif self.print_bodies:
-                text.write('\n')
+        return offset
 
-        def visit_partial_head(self, head: PartialHead):
-            """
-            See :func:`mlrl.common.cython.rule_model.RuleModelVisitor.visit_partial_head`
-            """
-            text = self.text
+    @staticmethod
+    def __format_numerical_conditions(text: StringIO, body: ConjunctiveBody, dataset: TabularDataset, options: Options,
+                                      offset: int) -> int:
+        offset = RuleModelAsText.__format_conditions(text,
+                                                     offset=offset,
+                                                     indices=body.numerical_leq_indices,
+                                                     thresholds=body.numerical_leq_thresholds,
+                                                     operator='<=',
+                                                     dataset=dataset,
+                                                     options=options)
+        offset = RuleModelAsText.__format_conditions(text,
+                                                     offset=offset,
+                                                     indices=body.numerical_gr_indices,
+                                                     thresholds=body.numerical_gr_thresholds,
+                                                     operator='>',
+                                                     dataset=dataset,
+                                                     options=options)
+        return offset
 
-            if self.print_heads:
-                print_output_names = self.print_output_names
-                decimals = self.head_decimals
-                outputs = self.dataset.outputs
-                indices = head.indices
-                scores = head.scores
+    @staticmethod
+    def __format_ordinal_conditions(text: StringIO, body: ConjunctiveBody, dataset: TabularDataset, options: Options,
+                                    offset: int) -> int:
+        offset = RuleModelAsText.__format_conditions(text,
+                                                     offset=offset,
+                                                     indices=body.ordinal_leq_indices,
+                                                     thresholds=body.ordinal_leq_thresholds,
+                                                     operator='<=',
+                                                     dataset=dataset,
+                                                     options=options)
+        offset = RuleModelAsText.__format_conditions(text,
+                                                     offset=offset,
+                                                     indices=body.ordinal_gr_indices,
+                                                     thresholds=body.ordinal_gr_thresholds,
+                                                     operator='>',
+                                                     dataset=dataset,
+                                                     options=options)
+        return offset
 
-                if self.print_bodies:
-                    text.write(' => ')
+    @staticmethod
+    def __format_nominal_conditions(text: StringIO, body: ConjunctiveBody, dataset: TabularDataset, options: Options,
+                                    offset: int) -> int:
+        offset = RuleModelAsText.__format_conditions(text,
+                                                     offset=offset,
+                                                     indices=body.nominal_eq_indices,
+                                                     thresholds=body.nominal_eq_thresholds,
+                                                     operator='==',
+                                                     dataset=dataset,
+                                                     options=options)
+        offset = RuleModelAsText.__format_conditions(text,
+                                                     offset=offset,
+                                                     indices=body.nominal_neq_indices,
+                                                     thresholds=body.nominal_neq_thresholds,
+                                                     operator='!=',
+                                                     dataset=dataset,
+                                                     options=options)
+        return offset
 
-                text.write('(')
+    @staticmethod
+    def __format_complete_head(text: StringIO, head: CompleteHead, dataset: TabularDataset, options: Options):
+        print_bodies = options.get_bool(RuleModelAsText.OPTION_PRINT_BODIES, True)
 
-                for i in range(indices.shape[0]):
-                    if i > 0:
-                        text.write(', ')
+        if options.get_bool(RuleModelAsText.OPTION_PRINT_HEADS, True):
+            print_output_names = options.get_bool(RuleModelAsText.OPTION_PRINT_OUTPUT_NAMES, True)
+            decimals = options.get_int(RuleModelAsText.OPTION_DECIMALS_HEAD, 2)
+            outputs = dataset.outputs
+            scores = head.scores
 
-                    output_index = indices[i]
+            if print_bodies:
+                text.write(' => ')
 
-                    if print_output_names and len(outputs) > output_index:
-                        text.write(outputs[output_index].name)
-                    else:
-                        text.write(str(output_index))
+            text.write('(')
 
-                    text.write(' = ')
-                    text.write(format_number(scores[i], decimals=decimals))
+            for i in range(scores.shape[0]):
+                if i > 0:
+                    text.write(', ')
 
-                text.write(')\n')
-            elif self.print_bodies:
-                text.write('\n')
+                if print_output_names and len(outputs) > i:
+                    text.write(outputs[i].name)
+                else:
+                    text.write(str(i))
+
+                text.write(' = ')
+                text.write(format_number(scores[i], decimals=decimals))
+
+            text.write(')\n')
+        elif print_bodies:
+            text.write('\n')
+
+    @staticmethod
+    def __format_partial_head(text: StringIO, head: PartialHead, dataset: TabularDataset, options: Options):
+        print_bodies = options.get_bool(RuleModelAsText.OPTION_PRINT_BODIES, True)
+
+        if options.get_bool(RuleModelAsText.OPTION_PRINT_HEADS, True):
+            print_output_names = options.get_bool(RuleModelAsText.OPTION_PRINT_OUTPUT_NAMES, True)
+            decimals = options.get_int(RuleModelAsText.OPTION_DECIMALS_HEAD, 2)
+            outputs = dataset.outputs
+            indices = head.indices
+            scores = head.scores
+
+            if print_bodies:
+                text.write(' => ')
+
+            text.write('(')
+
+            for i in range(indices.shape[0]):
+                if i > 0:
+                    text.write(', ')
+
+                output_index = indices[i]
+
+                if print_output_names and len(outputs) > output_index:
+                    text.write(outputs[output_index].name)
+                else:
+                    text.write(str(output_index))
+
+                text.write(' = ')
+                text.write(format_number(scores[i], decimals=decimals))
+
+            text.write(')\n')
+        elif print_bodies:
+            text.write('\n')
 
     def __init__(self, model: RuleModel, dataset: TabularDataset):
         """
@@ -235,6 +246,4 @@ class RuleModelAsText(TextualOutputData):
         """
         See :func:`mlrl.testbed.experiments.output.data.TextualOutputData.to_text`
         """
-        visitor = RuleModelAsText.Visitor(self.dataset, options)
-        self.model.visit_used(visitor)
-        return visitor.text.getvalue()
+        return self.__format_rule_model(self.model, self.dataset, options)
