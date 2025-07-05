449e874d42012af6c073b8ce3fe6606491cc8747
Michael Rapp <michael.rapp90@googlemail.com>
2025-07-06 00:21:15 +0200

    Add parameterized test "test_label_vectors".

--- python/tests/mlrl_tests/common/integration_tests_classification.py
+++ python/tests/mlrl_tests/common/integration_tests_classification.py
@@ -13,7 +13,7 @@ from .integration_tests import IntegrationTests
 from mlrl.common.config.parameters import SAMPLING_STRATIFIED_EXAMPLE_WISE, SAMPLING_STRATIFIED_OUTPUT_WISE
 
 from mlrl.testbed_sklearn.experiments.input.dataset.splitters.extension import OPTION_FIRST_FOLD, OPTION_LAST_FOLD, \
-    VALUE_CROSS_VALIDATION
+    VALUE_CROSS_VALIDATION, VALUE_TRAIN_TEST
 
 from mlrl.util.options import Options
 
@@ -28,31 +28,22 @@ class ClassificationIntegrationTests(IntegrationTests, ABC):
     def dataset(self) -> Dataset:
         return Dataset()
 
-    def test_label_vectors_train_test(self, dataset: Dataset):
+    @pytest.mark.parametrize('data_split, data_split_options', [
+        (VALUE_TRAIN_TEST, Options()),
+        (VALUE_CROSS_VALIDATION, Options()),
+        (VALUE_CROSS_VALIDATION, Options({
+            OPTION_FIRST_FOLD: 1,
+            OPTION_LAST_FOLD: 1,
+        })),
+    ])
+    def test_label_vectors(self, data_split: str, data_split_options: Options, dataset: Dataset):
         builder = self._create_cmd_builder(dataset=dataset.default) \
+            .data_split(data_split, options=data_split_options) \
             .print_evaluation(False) \
             .store_evaluation(False) \
             .print_label_vectors() \
             .store_label_vectors()
-        CmdRunner(builder).run('label-vectors_train-test')
-
-    def test_label_vectors_cross_validation(self, dataset: Dataset):
-        builder = self._create_cmd_builder(dataset=dataset.default) \
-            .data_split(VALUE_CROSS_VALIDATION) \
-            .print_evaluation(False) \
-            .store_evaluation(False) \
-            .print_label_vectors() \
-            .store_label_vectors()
-        CmdRunner(builder).run('label-vectors_cross-validation')
-
-    def test_label_vectors_single_fold(self, dataset: Dataset):
-        builder = self._create_cmd_builder(dataset=dataset.default) \
-            .data_split(VALUE_CROSS_VALIDATION, Options({OPTION_FIRST_FOLD: 1, OPTION_LAST_FOLD: 1})) \
-            .print_evaluation(False) \
-            .store_evaluation(False) \
-            .print_label_vectors() \
-            .store_label_vectors()
-        CmdRunner(builder).run('label-vectors_single-fold')
+        CmdRunner(builder).run(f'label-vectors_{data_split}' + (f'_{data_split_options}' if data_split_options else ''))
 
     def test_instance_sampling_stratified_output_wise(self, dataset: Dataset):
         builder = self._create_cmd_builder(dataset=dataset.default) \
