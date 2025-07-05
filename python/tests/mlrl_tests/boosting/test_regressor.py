b3128561dffd15c4fc421d92a068150f58fd30f0
Michael Rapp <michael.rapp90@googlemail.com>
2025-07-06 01:50:43 +0200

    Add parameterized test "test_non_decomposable_head_type" to class TestBoomerRegressor.

--- python/tests/mlrl_tests/boosting/test_regressor.py
+++ python/tests/mlrl_tests/boosting/test_regressor.py
@@ -89,66 +89,20 @@ class TestBoomerRegressor(RegressionIntegrationTests, BoomerIntegrationTestsMixi
             .print_model_characteristics()
         CmdRunner(builder).run('decomposable-partial-dynamic-heads_64-bit-statistics')
 
-    def test_non_decomposable_single_label_heads_32bit_statistics(self):
+    @pytest.mark.parametrize('head_type', [
+        HeadTypeParameter.HEAD_TYPE_SINGLE,
+        HeadTypeParameter.HEAD_TYPE_COMPLETE,
+        HeadTypeParameter.HEAD_TYPE_PARTIAL_FIXED,
+        HeadTypeParameter.HEAD_TYPE_PARTIAL_DYNAMIC,
+    ])
+    @pytest.mark.parametrize('statistic_type', [
+        StatisticTypeParameter.STATISTIC_TYPE_FLOAT32,
+        StatisticTypeParameter.STATISTIC_TYPE_FLOAT64,
+    ])
+    def test_non_decomposable_head_type(self, head_type: str, statistic_type: str):
         builder = self._create_cmd_builder() \
-            .loss(RegressionLossParameter.LOSS_SQUARED_ERROR_NON_DECOMPOSABLE) \
-            .statistic_type(StatisticTypeParameter.STATISTIC_TYPE_FLOAT32) \
-            .head_type(HeadTypeParameter.HEAD_TYPE_SINGLE) \
+            .loss(RegressionLossParameter.LOSS_SQUARED_ERROR_NON_DECOMPOSABLE,) \
+            .statistic_type(statistic_type) \
+            .head_type(head_type) \
             .print_model_characteristics()
-        CmdRunner(builder).run('non-decomposable-single-heads_32-bit-statistics')
-
-    def test_non_decomposable_single_label_heads_64bit_statistics(self):
-        builder = self._create_cmd_builder() \
-            .loss(RegressionLossParameter.LOSS_SQUARED_ERROR_NON_DECOMPOSABLE) \
-            .statistic_type(StatisticTypeParameter.STATISTIC_TYPE_FLOAT64) \
-            .head_type(HeadTypeParameter.HEAD_TYPE_SINGLE) \
-            .print_model_characteristics()
-        CmdRunner(builder).run('non-decomposable-single-heads_64-bit-statistics')
-
-    def test_non_decomposable_complete_heads_32bit_statistics(self):
-        builder = self._create_cmd_builder() \
-            .loss(RegressionLossParameter.LOSS_SQUARED_ERROR_NON_DECOMPOSABLE) \
-            .statistic_type(StatisticTypeParameter.STATISTIC_TYPE_FLOAT32) \
-            .head_type(HeadTypeParameter.HEAD_TYPE_COMPLETE) \
-            .print_model_characteristics()
-        CmdRunner(builder).run('non-decomposable-complete-heads_32-bit-statistics')
-
-    def test_non_decomposable_complete_heads_64bit_statistics(self):
-        builder = self._create_cmd_builder() \
-            .loss(RegressionLossParameter.LOSS_SQUARED_ERROR_NON_DECOMPOSABLE) \
-            .statistic_type(StatisticTypeParameter.STATISTIC_TYPE_FLOAT64) \
-            .head_type(HeadTypeParameter.HEAD_TYPE_COMPLETE) \
-            .print_model_characteristics()
-        CmdRunner(builder).run('non-decomposable-complete-heads_64-bit-statistics')
-
-    def test_non_decomposable_partial_fixed_heads_32bit_statistics(self):
-        builder = self._create_cmd_builder() \
-            .loss(RegressionLossParameter.LOSS_SQUARED_ERROR_NON_DECOMPOSABLE) \
-            .statistic_type(StatisticTypeParameter.STATISTIC_TYPE_FLOAT32) \
-            .head_type(HeadTypeParameter.HEAD_TYPE_PARTIAL_FIXED) \
-            .print_model_characteristics()
-        CmdRunner(builder).run('non-decomposable-partial-fixed-heads_32-bit-statistics')
-
-    def test_non_decomposable_partial_fixed_heads_64bit_statistics(self):
-        builder = self._create_cmd_builder() \
-            .loss(RegressionLossParameter.LOSS_SQUARED_ERROR_NON_DECOMPOSABLE) \
-            .statistic_type(StatisticTypeParameter.STATISTIC_TYPE_FLOAT64) \
-            .head_type(HeadTypeParameter.HEAD_TYPE_PARTIAL_FIXED) \
-            .print_model_characteristics()
-        CmdRunner(builder).run('non-decomposable-partial-fixed-heads_64-bit-statistics')
-
-    def test_non_decomposable_partial_dynamic_heads_32bit_statistics(self):
-        builder = self._create_cmd_builder() \
-            .loss(RegressionLossParameter.LOSS_SQUARED_ERROR_NON_DECOMPOSABLE) \
-            .statistic_type(StatisticTypeParameter.STATISTIC_TYPE_FLOAT32) \
-            .head_type(HeadTypeParameter.HEAD_TYPE_PARTIAL_DYNAMIC) \
-            .print_model_characteristics()
-        CmdRunner(builder).run('non-decomposable-partial-dynamic-heads_32-bit-statistics')
-
-    def test_non_decomposable_partial_dynamic_heads_64bit_statistics(self):
-        builder = self._create_cmd_builder() \
-            .loss(RegressionLossParameter.LOSS_SQUARED_ERROR_NON_DECOMPOSABLE) \
-            .statistic_type(StatisticTypeParameter.STATISTIC_TYPE_FLOAT64) \
-            .head_type(HeadTypeParameter.HEAD_TYPE_PARTIAL_DYNAMIC) \
-            .print_model_characteristics()
-        CmdRunner(builder).run('non-decomposable-partial-dynamic-heads_64-bit-statistics')
+        CmdRunner(builder).run(f'non-decomposable-{head_type}-heads_{statistic_type}-statistics')
