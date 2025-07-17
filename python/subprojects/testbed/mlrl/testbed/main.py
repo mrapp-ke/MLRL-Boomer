6326746392f4f07a1975aceabb9d3b3cb64c7b31
Michael Rapp <michael.rapp90@googlemail.com>
2025-07-17 23:13:57 +0200

    Add function "__get_runnable" to main.py.

--- python/subprojects/testbed/mlrl/testbed/main.py
+++ python/subprojects/testbed/mlrl/testbed/main.py
@@ -5,10 +5,11 @@ Imports and invokes the program to be run by the command line utility.
 """
 import sys
 
-from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from argparse import ArgumentParser, Namespace, RawDescriptionHelpFormatter
 from importlib import import_module
 from importlib.metadata import version
 from importlib.util import module_from_spec, spec_from_file_location
+from typing import Optional
 
 from mlrl.testbed.program_info import ProgramInfo
 from mlrl.testbed.runnables import Runnable
@@ -35,14 +36,20 @@ def __create_argument_parser() -> ArgumentParser:
     return argument_parser
 
 
-def __get_default_program_info() -> ProgramInfo:
-    package_name = 'mlrl-testbed'
-    return ProgramInfo(
-        name=package_name,
-        version=version(package_name),
-        year='2020 - 2025',
-        authors=['Michael Rapp et al.'],
-    )
+def __get_runnable(argument_parser: ArgumentParser) -> Optional[Runnable]:
+    args = vars(argument_parser.parse_known_args()[0])
+    runnable_module_or_source_file = args.get('runnable_module_or_source_file', None)
+    runnable_class_name = args.get('runnable', None)
+    runnable = None
+
+    if runnable_module_or_source_file and runnable_class_name:
+        runnable = __instantiate_via_default_constructor(module_or_source_file=str(runnable_module_or_source_file),
+                                                         class_name=str(runnable_class_name))
+
+        if not isinstance(runnable, Runnable):
+            raise TypeError('Class "' + str(runnable_class_name) + '" must extend from "' + Runnable.__qualname__ + '"')
+
+    return runnable
 
 
 def __import_module(module_name: str):
@@ -98,24 +105,22 @@ def __instantiate_via_default_constructor(module_or_source_file: str, class_name
         raise TypeError('Class "' + class_name + '" must provide a default constructor') from error
 
 
+def __get_default_program_info() -> ProgramInfo:
+    package_name = 'mlrl-testbed'
+    return ProgramInfo(
+        name=package_name,
+        version=version(package_name),
+        year='2020 - 2025',
+        authors=['Michael Rapp et al.'],
+    )
+
+
 def main():
     """
     The main function to be executed when the program starts.
     """
     argument_parser = __create_argument_parser()
-    args = vars(argument_parser.parse_known_args()[0])
-
-    runnable_module_or_source_file = args.get('runnable_module_or_source_file', None)
-    runnable_class_name = args.get('runnable', None)
-    runnable = None
-
-    if runnable_module_or_source_file and runnable_class_name:
-        runnable = __instantiate_via_default_constructor(module_or_source_file=str(runnable_module_or_source_file),
-                                                         class_name=str(runnable_class_name))
-
-        if not isinstance(runnable, Runnable):
-            raise TypeError('Class "' + str(runnable_class_name) + '" must extend from "' + Runnable.__qualname__ + '"')
-
+    runnable = __get_runnable(argument_parser)
     program_info = runnable.get_program_info() if runnable else __get_default_program_info()
     cli = CommandLineInterface(argument_parser, version_text=str(program_info) if program_info else None)
 
