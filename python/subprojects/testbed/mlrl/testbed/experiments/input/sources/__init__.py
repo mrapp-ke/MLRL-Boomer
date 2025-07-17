670e8304a55b91387168f18e4296b5b63a66ea25
Michael Rapp <michael.rapp90@googlemail.com>
2025-07-17 23:49:56 +0200

    Do not use ArffFileExtension in package "mlrl-testbed".

--- python/subprojects/testbed/mlrl/testbed/experiments/input/sources/__init__.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/input/sources/__init__.py
@@ -3,6 +3,6 @@ Author Michael Rapp (michael.rapp.ml@gmail.com)
 
 Provides classes that allow to read input data from different sources.
 """
-from mlrl.testbed.experiments.input.sources.source import Source
+from mlrl.testbed.experiments.input.sources.source import FileSource, Source
 from mlrl.testbed.experiments.input.sources.source_csv import CsvFileSource
 from mlrl.testbed.experiments.input.sources.source_pickle import PickleFileSource
