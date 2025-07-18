f9c23cb41c7c55ac03b6abbb6e0b2dcc3ba62113
Michael Rapp <michael.rapp90@googlemail.com>
2025-07-19 00:26:21 +0200

    Add function "run_experiment" to class Mode.

--- python/subprojects/testbed/mlrl/testbed/modes/mode_batch.py
+++ python/subprojects/testbed/mlrl/testbed/modes/mode_batch.py
@@ -3,8 +3,10 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 
 Provides classes that implement a mode of operation for performing multiple experiments.
 """
+from argparse import Namespace
 from typing import override
 
+from mlrl.testbed.experiments import Experiment
 from mlrl.testbed.modes.mode import Mode
 
 from mlrl.util.cli import CommandLineInterface, FlagArgument, StringArgument
@@ -30,3 +32,7 @@ class BatchExperimentMode(Mode):
     @override
     def configure_arguments(self, cli: CommandLineInterface):
         cli.add_arguments(self.CONFIG_FILE, self.LIST_COMMANDS)
+
+    @override
+    def run_experiment(self, _: Namespace, experiment_builder_factory: Experiment.Builder.Factory):
+        pass
