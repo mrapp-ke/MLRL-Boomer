62725688e139b3f229f7cdd9d0d56ca4ccdbcb98
Michael Rapp <michael.rapp90@googlemail.com>
2025-07-19 00:26:21 +0200

    Add function "run_experiment" to class Mode.

--- python/subprojects/testbed/mlrl/testbed/modes/mode_single.py
+++ python/subprojects/testbed/mlrl/testbed/modes/mode_single.py
@@ -3,8 +3,10 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 
 Provides classes that implement a mode of operation for performing a single experiment.
 """
+from argparse import Namespace
 from typing import override
 
+from mlrl.testbed.experiments import Experiment
 from mlrl.testbed.modes.mode import Mode
 
 from mlrl.util.cli import CommandLineInterface
@@ -18,3 +20,8 @@ class SingleExperimentMode(Mode):
     @override
     def configure_arguments(self, cli: CommandLineInterface):
         pass
+
+    @override
+    def run_experiment(self, args: Namespace, experiment_builder_factory: Experiment.Builder.Factory):
+        experiment_builder = experiment_builder_factory(args)
+        experiment_builder.run()
