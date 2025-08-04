755642c2d95dcbe939ac98cf4a8ea30bdbe0e59d
Michael Rapp <michael.rapp90@googlemail.com>
2025-08-05 01:25:00 +0200

    Add class YamlFileSink.

--- python/subprojects/testbed/mlrl/testbed/experiments/output/sinks/__init__.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/output/sinks/__init__.py
@@ -8,3 +8,4 @@ from mlrl.testbed.experiments.output.sinks.sink_csv import CsvFileSink
 from mlrl.testbed.experiments.output.sinks.sink_log import LogSink
 from mlrl.testbed.experiments.output.sinks.sink_pickle import PickleFileSink
 from mlrl.testbed.experiments.output.sinks.sink_text import TextFileSink
+from mlrl.testbed.experiments.output.sinks.sink_yaml import YamlFileSink
