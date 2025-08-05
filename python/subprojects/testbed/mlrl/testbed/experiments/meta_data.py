a3b738eff5751f05f12dbbf949c2100e587805b3
Michael Rapp <michael.rapp90@googlemail.com>
2025-08-05 02:02:44 +0200

    Add default values to class MetaData.

--- python/subprojects/testbed/mlrl/testbed/experiments/meta_data.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/meta_data.py
@@ -5,6 +5,7 @@ Provides classes for representing meta-data.
 """
 from dataclasses import dataclass, field
 from datetime import datetime
+from importlib.metadata import version
 from typing import List
 
 from mlrl.testbed.command import Command
@@ -16,15 +17,15 @@ class MetaData:
     Meta-data that provides information about a command that has been used for running MLRL-Testbed.
 
     Attributes:
-        version:        The version of MLRL-Testbed used for running the command
-        timestamp:      The date and time when the command was run
         command:        The command
         child_commands: Commands run by the original command, e.g., in batch mode
+        version:        The version of MLRL-Testbed used for running the command
+        timestamp:      The date and time when the command was run
     """
-    version: str
-    timestamp: datetime
     command: Command
     child_commands: List[Command] = field(default_factory=list)
+    version: str = version('mlrl-testbed')
+    timestamp: datetime = datetime.now()
 
     @property
     def formatted_timestamp(self) -> str:
