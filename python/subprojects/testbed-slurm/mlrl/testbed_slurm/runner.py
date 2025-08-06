dcde4c55b2fe94005be6b4e47cf56a5ce9605859
Michael Rapp <michael.rapp90@googlemail.com>
2025-08-07 01:45:18 +0200

    Add class Sbatch.

--- python/subprojects/testbed-slurm/mlrl/testbed_slurm/runner.py
+++ python/subprojects/testbed-slurm/mlrl/testbed_slurm/runner.py
@@ -3,9 +3,15 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 
 Provides classes that allow to run experiments via the Slurm Workload Manager.
 """
+import logging as log
+import sys
+
 from argparse import Namespace
 from typing import override
 
+from mlrl.testbed_slurm.sbatch import Sbatch
+
+from mlrl.testbed.command import Command
 from mlrl.testbed.experiments.recipe import Recipe
 from mlrl.testbed.modes.mode_batch import Batch, BatchMode
 
@@ -14,6 +20,27 @@ class SlurmRunner(BatchMode.Runner):
     """
     A `BatchMode.Runner` that allows to run experiments via the Slurm Workload manager.
     """
+
+    @staticmethod
+    def __is_command_available() -> bool:
+        sbatch = Sbatch()
+        version = sbatch.version().run()
+
+        if not version.ok:
+            log.error('Command "%s" not found: %s', sbatch.command, version.output)
+            return False
+
+        return True
+
+    @staticmethod
+    def __submit_command(command: Command):
+        result = Sbatch().wrap(str(command)).run()
+
+        if result.ok:
+            log.info('%s', result.output)
+        else:
+            log.error('Submission to Slurm failed:\n%s', result.output)
+            sys.exit(result.exit_code)
 
     def __init__(self):
         super().__init__(name='slurm')
@@ -24,4 +51,12 @@ class SlurmRunner(BatchMode.Runner):
         """
         See :func:`mlrl.testbed.modes.mode_batch.BatchMode.Runner.run_batch`
         """
-        print('Hello there')
+        num_experiments = len(batch)
+
+        if self.__is_command_available():
+            log.info('Submitting %s %s to Slurm...', num_experiments,
+                     'experiments' if num_experiments > 1 else 'experiment')
+
+            for i, command in enumerate(batch):
+                log.info('\nSubmitting experiment (%s / %s): "%s"', i + 1, num_experiments, str(command))
+                self.__submit_command(command)
