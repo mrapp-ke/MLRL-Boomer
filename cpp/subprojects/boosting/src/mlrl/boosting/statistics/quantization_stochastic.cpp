5eb210768ee2df4826a8d0c3aea2babec513dd38
Michael Rapp <michael.rapp90@googlemail.com>
2025-08-21 01:50:10 +0200

    Add class IQuantizationMatrix.

--- cpp/subprojects/boosting/src/mlrl/boosting/statistics/quantization_stochastic.cpp
+++ cpp/subprojects/boosting/src/mlrl/boosting/statistics/quantization_stochastic.cpp
@@ -4,14 +4,125 @@
 
 namespace boosting {
 
-    /**
-     * An implementation of the type `IQuantization` that uses a stochastic rounding strategy.
-     */
-    class StochasticQuantization final : public IQuantization {};
+    template<typename View, typename StatisticType>
+    class StochasticQuantizationMatrix final : public IQuantizationMatrix<CContiguousView<Statistic<StatisticType>>> {
+        private:
 
-    /**
-     * Allows to to create instances of the type `IQuantization` that uses a stochastic rounding strategy.
-     */
+            const View& view_;
+
+            // TODO Use correct type
+            MatrixDecorator<AllocatedCContiguousView<Statistic<StatisticType>>> matrix_;
+
+        public:
+
+            StochasticQuantizationMatrix(const View& view)
+                : view_(view), matrix_(AllocatedCContiguousView<Statistic<StatisticType>>(view.numRows, view.numCols)) {
+            }
+
+            void quantize(const CompleteIndexVector& outputIndices) override {
+                // TODO Implement
+            }
+
+            void quantize(const PartialIndexVector& outputIndices) override {
+                // TODO Implement
+            }
+
+            const typename IQuantizationMatrix<CContiguousView<Statistic<StatisticType>>>::view_type& getView()
+              const override {
+                return matrix_.getView();
+            }
+    };
+
+    template<typename View, typename StatisticType>
+    class StochasticQuantization final : public IQuantization {
+        private:
+
+            static inline void visitInternally(
+              const IQuantizationMatrix<CContiguousView<Statistic<float32>>>& quantizationMatrix,
+              IQuantization::DenseDecomposableMatrixVisitor<float32> denseDecomposable32BitVisitor,
+              IQuantization::DenseDecomposableMatrixVisitor<float64> denseDecomposable64BitVisitor,
+              IQuantization::SparseDecomposableMatrixVisitor<float32> sparseDecomposable32BitVisitor,
+              IQuantization::SparseDecomposableMatrixVisitor<float64> sparseDecomposable64BitVisitor,
+              IQuantization::DenseNonDecomposableMatrixVisitor<float32> denseNonDecomposable32BitVisitor,
+              IQuantization::DenseNonDecomposableMatrixVisitor<float64> denseNonDecomposable64BitVisitor) {
+                denseDecomposable32BitVisitor(quantizationMatrix);
+            }
+
+            static inline void visitInternally(
+              const IQuantizationMatrix<CContiguousView<Statistic<float64>>>& quantizationMatrix,
+              IQuantization::DenseDecomposableMatrixVisitor<float32> denseDecomposable32BitVisitor,
+              IQuantization::DenseDecomposableMatrixVisitor<float64> denseDecomposable64BitVisitor,
+              IQuantization::SparseDecomposableMatrixVisitor<float32> sparseDecomposable32BitVisitor,
+              IQuantization::SparseDecomposableMatrixVisitor<float64> sparseDecomposable64BitVisitor,
+              IQuantization::DenseNonDecomposableMatrixVisitor<float32> denseNonDecomposable32BitVisitor,
+              IQuantization::DenseNonDecomposableMatrixVisitor<float64> denseNonDecomposable64BitVisitor) {
+                denseDecomposable64BitVisitor(quantizationMatrix);
+            }
+
+            static inline void visitInternally(
+              const IQuantizationMatrix<SparseSetView<Statistic<float32>>>& quantizationMatrix,
+              IQuantization::DenseDecomposableMatrixVisitor<float32> denseDecomposable32BitVisitor,
+              IQuantization::DenseDecomposableMatrixVisitor<float64> denseDecomposable64BitVisitor,
+              IQuantization::SparseDecomposableMatrixVisitor<float32> sparseDecomposable32BitVisitor,
+              IQuantization::SparseDecomposableMatrixVisitor<float64> sparseDecomposable64BitVisitor,
+              IQuantization::DenseNonDecomposableMatrixVisitor<float32> denseNonDecomposable32BitVisitor,
+              IQuantization::DenseNonDecomposableMatrixVisitor<float64> denseNonDecomposable64BitVisitor) {
+                sparseDecomposable32BitVisitor(quantizationMatrix);
+            }
+
+            static inline void visitInternally(
+              const IQuantizationMatrix<SparseSetView<Statistic<float64>>>& quantizationMatrix,
+              IQuantization::DenseDecomposableMatrixVisitor<float32> denseDecomposable32BitVisitor,
+              IQuantization::DenseDecomposableMatrixVisitor<float64> denseDecomposable64BitVisitor,
+              IQuantization::SparseDecomposableMatrixVisitor<float32> sparseDecomposable32BitVisitor,
+              IQuantization::SparseDecomposableMatrixVisitor<float64> sparseDecomposable64BitVisitor,
+              IQuantization::DenseNonDecomposableMatrixVisitor<float32> denseNonDecomposable32BitVisitor,
+              IQuantization::DenseNonDecomposableMatrixVisitor<float64> denseNonDecomposable64BitVisitor) {
+                sparseDecomposable64BitVisitor(quantizationMatrix);
+            }
+
+            static inline void visitInternally(
+              const IQuantizationMatrix<DenseNonDecomposableStatisticView<float32>>& quantizationMatrix,
+              IQuantization::DenseDecomposableMatrixVisitor<float32> denseDecomposable32BitVisitor,
+              IQuantization::DenseDecomposableMatrixVisitor<float64> denseDecomposable64BitVisitor,
+              IQuantization::SparseDecomposableMatrixVisitor<float32> sparseDecomposable32BitVisitor,
+              IQuantization::SparseDecomposableMatrixVisitor<float64> sparseDecomposable64BitVisitor,
+              IQuantization::DenseNonDecomposableMatrixVisitor<float32> denseNonDecomposable32BitVisitor,
+              IQuantization::DenseNonDecomposableMatrixVisitor<float64> denseNonDecomposable64BitVisitor) {
+                denseNonDecomposable32BitVisitor(quantizationMatrix);
+            }
+
+            static inline void visitInternally(
+              const IQuantizationMatrix<DenseNonDecomposableStatisticView<float64>>& quantizationMatrix,
+              IQuantization::DenseDecomposableMatrixVisitor<float32> denseDecomposable32BitVisitor,
+              IQuantization::DenseDecomposableMatrixVisitor<float64> denseDecomposable64BitVisitor,
+              IQuantization::SparseDecomposableMatrixVisitor<float32> sparseDecomposable32BitVisitor,
+              IQuantization::SparseDecomposableMatrixVisitor<float64> sparseDecomposable64BitVisitor,
+              IQuantization::DenseNonDecomposableMatrixVisitor<float32> denseNonDecomposable32BitVisitor,
+              IQuantization::DenseNonDecomposableMatrixVisitor<float64> denseNonDecomposable64BitVisitor) {
+                denseNonDecomposable64BitVisitor(quantizationMatrix);
+            }
+
+            std::unique_ptr<IQuantizationMatrix<CContiguousView<Statistic<StatisticType>>>> quantizationMatrixPtr_;
+
+        public:
+
+            StochasticQuantization(const View& view)
+                : quantizationMatrixPtr_(std::make_unique<StochasticQuantizationMatrix<View, StatisticType>>(view)) {}
+
+            void visitQuantizationMatrix(
+              IQuantization::DenseDecomposableMatrixVisitor<float32> denseDecomposable32BitVisitor,
+              IQuantization::DenseDecomposableMatrixVisitor<float64> denseDecomposable64BitVisitor,
+              IQuantization::SparseDecomposableMatrixVisitor<float32> sparseDecomposable32BitVisitor,
+              IQuantization::SparseDecomposableMatrixVisitor<float64> sparseDecomposable64BitVisitor,
+              IQuantization::DenseNonDecomposableMatrixVisitor<float32> denseNonDecomposable32BitVisitor,
+              IQuantization::DenseNonDecomposableMatrixVisitor<float64> denseNonDecomposable64BitVisitor) override {
+                visitInternally(*quantizationMatrixPtr_, denseDecomposable32BitVisitor, denseDecomposable64BitVisitor,
+                                sparseDecomposable32BitVisitor, sparseDecomposable64BitVisitor,
+                                denseNonDecomposable32BitVisitor, denseNonDecomposable64BitVisitor);
+            }
+    };
+
     class StochasticQuantizationFactory final : public IQuantizationFactory {
         private:
 
@@ -26,32 +137,38 @@ namespace boosting {
 
             std::unique_ptr<IQuantization> create(
               const CContiguousView<Statistic<float32>>& statisticMatrix) const override {
-                return std::make_unique<StochasticQuantization>();
+                return std::make_unique<StochasticQuantization<CContiguousView<Statistic<float32>>, float32>>(
+                  statisticMatrix);
             }
 
             std::unique_ptr<IQuantization> create(
               const CContiguousView<Statistic<float64>>& statisticMatrix) const override {
-                return std::make_unique<StochasticQuantization>();
+                return std::make_unique<StochasticQuantization<CContiguousView<Statistic<float64>>, float64>>(
+                  statisticMatrix);
             }
 
             std::unique_ptr<IQuantization> create(
               const SparseSetView<Statistic<float32>>& statisticMatrix) const override {
-                return std::make_unique<StochasticQuantization>();
+                return std::make_unique<StochasticQuantization<SparseSetView<Statistic<float32>>, float32>>(
+                  statisticMatrix);
             }
 
             std::unique_ptr<IQuantization> create(
               const SparseSetView<Statistic<float64>>& statisticMatrix) const override {
-                return std::make_unique<StochasticQuantization>();
+                return std::make_unique<StochasticQuantization<SparseSetView<Statistic<float64>>, float64>>(
+                  statisticMatrix);
             }
 
             std::unique_ptr<IQuantization> create(
               const DenseNonDecomposableStatisticView<float32>& statisticMatrix) const override {
-                return std::make_unique<StochasticQuantization>();
+                return std::make_unique<StochasticQuantization<DenseNonDecomposableStatisticView<float32>, float32>>(
+                  statisticMatrix);
             }
 
             std::unique_ptr<IQuantization> create(
               const DenseNonDecomposableStatisticView<float64>& statisticMatrix) const override {
-                return std::make_unique<StochasticQuantization>();
+                return std::make_unique<StochasticQuantization<DenseNonDecomposableStatisticView<float64>, float64>>(
+                  statisticMatrix);
             }
     };
 
