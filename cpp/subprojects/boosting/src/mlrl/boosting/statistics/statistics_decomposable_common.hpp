42246ae6c695f229c58303245bc1b749d42f2466
Michael Rapp <michael.rapp90@googlemail.com>
2025-08-21 22:53:16 +0200

    Add template argument "QuantizationMatrix" to class AbstractStatistics.

--- cpp/subprojects/boosting/src/mlrl/boosting/statistics/statistics_decomposable_common.hpp
+++ cpp/subprojects/boosting/src/mlrl/boosting/statistics/statistics_decomposable_common.hpp
@@ -16,6 +16,7 @@ namespace boosting {
      * @tparam OutputMatrix             The type of the matrix that provides access to the ground truth of the training
      *                                  examples
      * @tparam StatisticMatrix          The type of the matrix that provides access to the gradients and Hessians
+     * @tparam QuantizationMatrix       The type of the matrix that provides access to quantized gradients and Hessians
      * @tparam ScoreMatrix              The type of the matrices that are used to store predicted scores
      * @tparam Loss                     The type of the loss function that is used to calculate gradients and Hessians
      * @tparam EvaluationMeasure        The type of the evaluation measure that is used to assess the quality of
@@ -24,18 +25,16 @@ namespace boosting {
      *                                  used for calculating the predictions of rules, as well as corresponding quality
      *                                  scores
      */
-    template<typename OutputMatrix, typename StatisticMatrix, typename ScoreMatrix, typename Loss,
-             typename EvaluationMeasure, typename RuleEvaluationFactory>
+    template<typename OutputMatrix, typename StatisticMatrix, typename QuantizationMatrix, typename ScoreMatrix,
+             typename Loss, typename EvaluationMeasure, typename RuleEvaluationFactory>
     class AbstractDecomposableStatistics
         : public AbstractStatistics<
-            DecomposableBoostingStatisticsState<OutputMatrix, StatisticMatrix, ScoreMatrix, Loss>, EvaluationMeasure,
-            RuleEvaluationFactory>,
+            DecomposableBoostingStatisticsState<OutputMatrix, StatisticMatrix, QuantizationMatrix, ScoreMatrix, Loss>,
+            EvaluationMeasure, RuleEvaluationFactory>,
           virtual public IDecomposableStatistics<RuleEvaluationFactory> {
         public:
 
             /**
-             * @param quantizationPtr       An unique pointer to an object of type `IQuantization` that implements the
-             *                              method that should be used for quantizing gradients and Hessians
              * @param lossPtr               An unique pointer to an object of template type `Loss` that implements the
              *                              loss function that should be used for calculating gradients and Hessians
              * @param evaluationMeasurePtr  An unique pointer to an object of template type `EvaluationMeasure` that
@@ -48,23 +47,25 @@ namespace boosting {
              *                              access to the ground truth of the training examples
              * @param statisticMatrixPtr    An unique pointer to an object of template type `StatisticMatrix` that
              *                              provides access to the gradients and Hessians
+             * @param quantizationMatrixPtr An unique pointer to an object of template type `QuantizationMatrix` that
+             *                              provides access to quantized gradients and Hessians
              * @param scoreMatrixPtr        An unique pointer to an object of template type `ScoreMatrix` that stores
              *                              the currently predicted scores
              */
-            AbstractDecomposableStatistics(std::unique_ptr<IQuantization> quantizationPtr,
-                                           std::unique_ptr<Loss> lossPtr,
+            AbstractDecomposableStatistics(std::unique_ptr<Loss> lossPtr,
                                            std::unique_ptr<EvaluationMeasure> evaluationMeasurePtr,
                                            const RuleEvaluationFactory& ruleEvaluationFactory,
                                            const OutputMatrix& outputMatrix,
                                            std::unique_ptr<StatisticMatrix> statisticMatrixPtr,
+                                           std::unique_ptr<QuantizationMatrix> quantizationMatrixPtr,
                                            std::unique_ptr<ScoreMatrix> scoreMatrixPtr)
-                : AbstractStatistics<
-                    DecomposableBoostingStatisticsState<OutputMatrix, StatisticMatrix, ScoreMatrix, Loss>,
-                    EvaluationMeasure, RuleEvaluationFactory>(
-                    std::move(quantizationPtr),
-                    std::make_unique<
-                      DecomposableBoostingStatisticsState<OutputMatrix, StatisticMatrix, ScoreMatrix, Loss>>(
-                      outputMatrix, std::move(statisticMatrixPtr), std::move(scoreMatrixPtr), std::move(lossPtr)),
+                : AbstractStatistics<DecomposableBoostingStatisticsState<OutputMatrix, StatisticMatrix,
+                                                                         QuantizationMatrix, ScoreMatrix, Loss>,
+                                     EvaluationMeasure, RuleEvaluationFactory>(
+                    std::make_unique<DecomposableBoostingStatisticsState<OutputMatrix, StatisticMatrix,
+                                                                         QuantizationMatrix, ScoreMatrix, Loss>>(
+                      outputMatrix, std::move(statisticMatrixPtr), std::move(quantizationMatrixPtr),
+                      std::move(scoreMatrixPtr), std::move(lossPtr)),
                     std::move(evaluationMeasurePtr), ruleEvaluationFactory) {}
 
             /**
