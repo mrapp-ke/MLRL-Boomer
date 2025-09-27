d289788a44ee36ede4261629655991d19f42820f
Michael Rapp <michael.rapp90@googlemail.com>
2025-09-28 01:19:57 +0200

    Move function "update_state" from class DatasetInputData to class InputDataset.

--- python/subprojects/testbed/mlrl/testbed/experiments/input/dataset/dataset.py
+++ python/subprojects/testbed/mlrl/testbed/experiments/input/dataset/dataset.py
@@ -3,10 +3,12 @@ Author: Michael Rapp (michael.rapp.ml@gmail.com)
 
 Provides classes for representing datasets that are part of input data.
 """
+from typing import Any, override
 
 from mlrl.testbed.experiments.context import Context
 from mlrl.testbed.experiments.data import Properties
 from mlrl.testbed.experiments.input.data import DatasetInputData
+from mlrl.testbed.experiments.state import ExperimentState
 
 
 class InputDataset(DatasetInputData):
@@ -22,3 +24,11 @@ class InputDataset(DatasetInputData):
         """
         super().__init__(properties=Properties(name=self.NAME, file_name=name),
                          context=Context(include_prediction_scope=False))
+
+    @override
+    def update_state(self, state: ExperimentState, input_data: Any):
+        """
+        See :func:`mlrl.testbed.experiments.input.data.InputData.update_state`
+        """
+        super().update_state(state, input_data)
+        state.dataset = input_data
