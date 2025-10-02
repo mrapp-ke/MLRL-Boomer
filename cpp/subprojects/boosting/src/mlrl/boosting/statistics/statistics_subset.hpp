3600dc30f2cec3728a743ca4641a5e0e9acbeff2
Michael Rapp <michael.rapp90@googlemail.com>
2025-10-03 01:57:33 +0200

    Add utility function to file statistics_subset.hpp.

--- cpp/subprojects/boosting/src/mlrl/boosting/statistics/statistics_subset.hpp
+++ cpp/subprojects/boosting/src/mlrl/boosting/statistics/statistics_subset.hpp
@@ -125,14 +125,11 @@ namespace boosting {
                     totalCoverableSumVectorPtr_ = std::make_unique<StatisticVector>(*totalSumVector_);
                     totalSumVector_ = totalCoverableSumVectorPtr_.get();
 
-                    for (auto it = excludedStatisticIndices.indices_cbegin();
-                         it != excludedStatisticIndices.indices_cend(); it++) {
-                        // For each output, subtract the statistics of the example at the given index (weighted by the
-                        // given weight) from the total sum of statistics...
-                        uint32 statisticIndex = *it;
-                        removeStatisticsFromVector(*totalCoverableSumVectorPtr_, weights,
-                                                   state.statisticMatrixPtr->getView(), statisticIndex);
-                    }
+                    // For each output, subtract the statistics of the example at the given index (weighted by the given
+                    // weight) from the total sum of statistics...
+                    removeStatisticsFromVector(
+                      *totalCoverableSumVectorPtr_, weights, state.statisticMatrixPtr->getView(),
+                      excludedStatisticIndices.indices_cbegin(), excludedStatisticIndices.indices_cend());
                 }
             }
 
