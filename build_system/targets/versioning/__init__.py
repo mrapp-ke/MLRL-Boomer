5db8efc0d7b4f6a2fc5db792ae5f59a6ad4e7d3b
Michael Rapp <michael.rapp90@googlemail.com>
2025-10-09 00:00:03 +0200

    Add build target "close_milestone".

--- build_system/targets/versioning/__init__.py
+++ build_system/targets/versioning/__init__.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from core.build_unit import BuildUnit
 from core.targets import TargetBuilder
 
+from targets.versioning.milestones import close_milestone
 from targets.versioning.versioning import increment_development_version, increment_major_version, \
     increment_minor_version, increment_patch_version, reset_development_version
 
@@ -15,4 +16,5 @@ TARGETS = TargetBuilder(BuildUnit.for_file(Path(__file__))) \
     .add_phony_target('increment_patch_version').set_functions(increment_patch_version) \
     .add_phony_target('increment_minor_version').set_functions(increment_minor_version) \
     .add_phony_target('increment_major_version').set_functions(increment_major_version) \
+    .add_phony_target('close_milestone').set_functions(close_milestone) \
     .build()
