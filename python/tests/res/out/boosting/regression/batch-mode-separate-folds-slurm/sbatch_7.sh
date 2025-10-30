9f1f0636de2a58f81e862c46e59bfe4f1c761f90
Michael Rapp <michael.rapp90@googlemail.com>
2025-10-30 21:21:12 +0100

    Add integration test.

---
+++ python/tests/res/out/boosting/regression/batch-mode-separate-folds-slurm/sbatch_7.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#SBATCH --output=python/tests/res/tmp/instance-sampling_with-replacement{sample_size=0.5}/loss_squared-error-decomposable/dataset_atp7d-binary/results/std_fold-%a.out
+#SBATCH --error=python/tests/res/tmp/instance-sampling_with-replacement{sample_size=0.5}/loss_squared-error-decomposable/dataset_atp7d-binary/results/std_fold-%a.err
+#SBATCH --array=1-2
+#SBATCH --time=2:30:00
+#SBATCH --cpus-per-tasks=1
+
+python -m venv .venv
+. .venv/bin/activate
+python -m pip install mlrl-testbed-sklearn
+mlrl-testbed mlrl.boosting --base-dir python/tests/res/tmp --data-dir python/tests/res/data --data-split cross-validation'{first_fold=$SLURM_ARRAY_TASK_ID,last_fold=$SLURM_ARRAY_TASK_ID,num_folds=2}' --dataset atp7d-binary --instance-sampling with-replacement'{sample_size=0.5}' --log-level debug --loss squared-error-decomposable --model-save-dir instance-sampling_with-replacement'{sample_size=0.5}'/loss_squared-error-decomposable/dataset_atp7d-binary/models --parameter-save-dir instance-sampling_with-replacement'{sample_size=0.5}'/loss_squared-error-decomposable/dataset_atp7d-binary/parameters --problem-type regression --result-dir instance-sampling_with-replacement'{sample_size=0.5}'/loss_squared-error-decomposable/dataset_atp7d-binary/results --save-evaluation true --save-meta-data false --wipe-result-dir false
+deactivate
---
+++ python/tests/res/out/seco/classification/batch-mode-separate-folds-slurm/sbatch_7.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#SBATCH --output=python/tests/res/tmp/instance-sampling_with-replacement{sample_size=0.5}/heuristic_f-measure/dataset_enron/results/std_fold-%a.out
+#SBATCH --error=python/tests/res/tmp/instance-sampling_with-replacement{sample_size=0.5}/heuristic_f-measure/dataset_enron/results/std_fold-%a.err
+#SBATCH --array=1-2
+#SBATCH --time=2:30:00
+#SBATCH --cpus-per-tasks=1
+
+python -m venv .venv
+. .venv/bin/activate
+python -m pip install mlrl-testbed-sklearn
+mlrl-testbed mlrl.seco --base-dir python/tests/res/tmp --data-dir python/tests/res/data --data-split cross-validation'{first_fold=$SLURM_ARRAY_TASK_ID,last_fold=$SLURM_ARRAY_TASK_ID,num_folds=2}' --dataset enron --heuristic f-measure --instance-sampling with-replacement'{sample_size=0.5}' --log-level debug --model-save-dir instance-sampling_with-replacement'{sample_size=0.5}'/heuristic_f-measure/dataset_enron/models --parameter-save-dir instance-sampling_with-replacement'{sample_size=0.5}'/heuristic_f-measure/dataset_enron/parameters --result-dir instance-sampling_with-replacement'{sample_size=0.5}'/heuristic_f-measure/dataset_enron/results --save-evaluation true --save-meta-data false --wipe-result-dir false
+deactivate
