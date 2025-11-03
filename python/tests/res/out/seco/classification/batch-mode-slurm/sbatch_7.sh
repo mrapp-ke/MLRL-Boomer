392299fd65afba9c42eae7c12193e07648175ca0
Michael Rapp <michael.rapp90@googlemail.com>
2025-11-04 00:33:38 +0100

    Adjust expected output of integration tests.

--- python/tests/res/out/seco/classification/batch-mode-slurm/sbatch_7.sh
+++ python/tests/res/out/seco/classification/batch-mode-slurm/sbatch_7.sh
@@ -8,5 +8,5 @@
 python -m venv .venv
 . .venv/bin/activate
 python -m pip install mlrl-testbed-sklearn
-mlrl-testbed mlrl.seco --base-dir python/tests/res/tmp --data-dir python/tests/res/data --dataset enron --heuristic f-measure --instance-sampling with-replacement'{sample_size=0.5}' --log-level debug --model-save-dir instance-sampling_with-replacement'{sample_size=0.5}'/heuristic_f-measure/dataset_enron/models --parameter-save-dir instance-sampling_with-replacement'{sample_size=0.5}'/heuristic_f-measure/dataset_enron/parameters --result-dir instance-sampling_with-replacement'{sample_size=0.5}'/heuristic_f-measure/dataset_enron/results --save-evaluation true --save-meta-data false
+mlrl-testbed mlrl.seco --base-dir python/tests/res/tmp --data-dir python/tests/res/data --dataset enron --heuristic f-measure --instance-sampling with-replacement\{sample_size=0.5\} --log-level debug --model-save-dir instance-sampling_with-replacement\{sample_size=0.5\}/heuristic_f-measure/dataset_enron/models --parameter-save-dir instance-sampling_with-replacement\{sample_size=0.5\}/heuristic_f-measure/dataset_enron/parameters --result-dir instance-sampling_with-replacement\{sample_size=0.5\}/heuristic_f-measure/dataset_enron/results --save-evaluation true --save-meta-data false
 deactivate
