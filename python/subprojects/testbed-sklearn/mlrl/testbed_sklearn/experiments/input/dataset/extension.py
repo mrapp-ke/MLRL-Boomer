b3d8a452fe09a712f37ff78a736cd2d3fe3f63ab
Michael Rapp <michael.rapp90@googlemail.com>
2025-11-07 12:38:15 +0100

    Add class SvmFileExtension.

--- python/subprojects/testbed-sklearn/mlrl/testbed_sklearn/experiments/input/dataset/extension.py
+++ python/subprojects/testbed-sklearn/mlrl/testbed_sklearn/experiments/input/dataset/extension.py
@@ -9,6 +9,8 @@ from typing import Sequence, override
 
 from mlrl.testbed_arff.experiments.input.sources import ArffFileSource
 
+from mlrl.testbed_sklearn.experiments.input.sources.source_svm import SvmFileSource
+
 from mlrl.testbed.experiments.input.dataset.dataset import InputDataset
 from mlrl.testbed.experiments.input.dataset.extension import DatasetFileExtension
 from mlrl.testbed.experiments.input.sources import FileSource
@@ -27,3 +29,18 @@ class ArffFileExtension(DatasetFileExtension):
     def _create_file_sources(self, dataset_directory: Path, dataset: InputDataset,
                              args: Namespace) -> Sequence[FileSource]:
         return [ArffFileSource(dataset_directory)]
+
+
+class SvmFileExtension(DatasetFileExtension):
+    """
+    An extension that configures the functionality to load datasets from SVM files.
+    """
+
+    def __init__(self):
+        super().__init__(file_type=SvmFileSource.SUFFIX_SVM)
+
+    # pylint: disable=unused-argument
+    @override
+    def _create_file_sources(self, dataset_directory: Path, dataset: InputDataset,
+                             args: Namespace) -> Sequence[FileSource]:
+        return [SvmFileSource(dataset_directory)]
