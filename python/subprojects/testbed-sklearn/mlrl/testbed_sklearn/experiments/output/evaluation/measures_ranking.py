7cfa126b7a2d7ad34bb0486189f74ee59db207d0
Michael Rapp <michael.rapp90@googlemail.com>
2025-11-11 00:13:35 +0100

    Add DCG@k and NDCG@k ranking measures.

--- python/subprojects/testbed-sklearn/mlrl/testbed_sklearn/experiments/output/evaluation/measures_ranking.py
+++ python/subprojects/testbed-sklearn/mlrl/testbed_sklearn/experiments/output/evaluation/measures_ranking.py
@@ -3,12 +3,36 @@ Author Michael Rapp (michael.rapp.ml@gmail.com)
 
 Provides classes that implement ranking evaluation measures.
 """
+from itertools import chain
+from typing import Generator
+
 from sklearn import metrics
 
 from mlrl.testbed_sklearn.experiments.output.evaluation.evaluation_result import EVALUATION_MEASURE_PREDICTION_TIME, \
     EVALUATION_MEASURE_TRAINING_TIME, EvaluationResult
 
 from mlrl.testbed.experiments.output.evaluation.measures import Measure
+
+
+def at_k(measure: Measure) -> Generator[Measure, None, None]:
+    """
+    Returns the given measure, as well as different variants of it (with varying k) that only take the top-k outputs
+    into account.
+
+    :param measure: A measure
+    :return:        A generator that iterates the given measure, as well as several variants of it
+    """
+    yield measure
+
+    for k in [1, 2, 3, 5, 8]:
+        yield Measure(
+            option_key=measure.option_key,
+            name=f'{measure.unformatted_name}@{k}',
+            evaluation_function=measure.evaluation_function,
+            smaller_is_better=measure.smaller_is_better,
+            percentage=measure.percentage,
+            **{'k': k},
+        )
 
 RANKING_EVALUATION_MEASURES = [
     Measure(
@@ -31,17 +55,21 @@ RANKING_EVALUATION_MEASURES = [
         evaluation_function=metrics.label_ranking_average_precision_score,
         percentage=False,
     ),
-    Measure(
-        option_key=EvaluationResult.OPTION_DISCOUNTED_CUMULATIVE_GAIN,
-        name='Discounted Cumulative Gain',
-        evaluation_function=metrics.dcg_score,
-        percentage=False,
-    ),
-    Measure(
-        option_key=EvaluationResult.OPTION_NORMALIZED_DISCOUNTED_CUMULATIVE_GAIN,
-        name='NDCG',
-        evaluation_function=metrics.ndcg_score,
-    ),
     EVALUATION_MEASURE_TRAINING_TIME,
     EVALUATION_MEASURE_PREDICTION_TIME,
-]
+] + list(
+    chain(
+        at_k(
+            Measure(
+                option_key=EvaluationResult.OPTION_NORMALIZED_DISCOUNTED_CUMULATIVE_GAIN,
+                name='NDCG',
+                evaluation_function=metrics.ndcg_score,
+            )),
+        at_k(
+            Measure(
+                option_key=EvaluationResult.OPTION_DISCOUNTED_CUMULATIVE_GAIN,
+                name='Discounted Cumulative Gain',
+                evaluation_function=metrics.dcg_score,
+                percentage=False,
+            )),
+    ))
