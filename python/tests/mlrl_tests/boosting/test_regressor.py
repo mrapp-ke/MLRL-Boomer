6236d5d6b475846a9daed5b9298a60356ac6f0c0
Michael Rapp <michael.rapp90@googlemail.com>
2025-11-12 14:50:47 +0100

    Add tests for scikit-learn compatibility.

--- python/tests/mlrl_tests/boosting/test_regressor.py
+++ python/tests/mlrl_tests/boosting/test_regressor.py
@@ -6,6 +6,8 @@ from typing import Any, override
 
 import pytest
 
+from sklearn.utils.estimator_checks import check_estimator
+
 from ..common.cmd_runner import CmdRunner
 from ..common.datasets import Dataset
 from ..common.integration_tests_regression import RegressionIntegrationTests
@@ -13,6 +15,7 @@ from .cmd_builder_regression import BoomerRegressorCmdBuilder
 from .integration_tests import BoomerIntegrationTestsMixin
 
 from mlrl.boosting.config.parameters import HeadTypeParameter, RegressionLossParameter, StatisticTypeParameter
+from mlrl.boosting.learners import BoomerRegressor
 
 
 @pytest.mark.boosting
@@ -25,6 +28,14 @@ class TestBoomerRegressor(RegressionIntegrationTests, BoomerIntegrationTestsMixi
     @override
     def _create_cmd_builder(self, dataset: str = Dataset.ATP7D) -> Any:
         return BoomerRegressorCmdBuilder(dataset=dataset)
+
+    def test_scikit_learn_compatibility(self):
+        check_estimator(BoomerRegressor(),
+                        expected_failed_checks={
+                            'check_regressors_train': 'Fails because model is too large to pickle',
+                            'check_estimators_pickle': 'Fails because model is too large to pickle',
+                            'check_readonly_memmap_input': 'Fails because model is too large to pickle',
+                        })
 
     @pytest.mark.parametrize('head_type', [
         HeadTypeParameter.HEAD_TYPE_SINGLE,
@@ -56,7 +67,7 @@ class TestBoomerRegressor(RegressionIntegrationTests, BoomerIntegrationTestsMixi
     ])
     def test_non_decomposable_head_type(self, head_type: str, statistic_type: str):
         builder = self._create_cmd_builder() \
-            .loss(RegressionLossParameter.LOSS_SQUARED_ERROR_NON_DECOMPOSABLE,) \
+            .loss(RegressionLossParameter.LOSS_SQUARED_ERROR_NON_DECOMPOSABLE, ) \
             .statistic_type(statistic_type) \
             .head_type(head_type) \
             .print_model_characteristics()
--- python/tests/mlrl_tests/seco/test_classifier.py
+++ python/tests/mlrl_tests/seco/test_classifier.py
@@ -6,6 +6,8 @@ from typing import Any, Optional, override
 
 import pytest
 
+from sklearn.utils.estimator_checks import check_estimator
+
 from ..common.cmd_runner import CmdRunner
 from ..common.datasets import Dataset
 from ..common.integration_tests_classification import ClassificationIntegrationTests
@@ -16,6 +18,7 @@ from mlrl.common.config.parameters import RulePruningParameter
 from mlrl.seco.config.parameters import HEURISTIC_ACCURACY, HEURISTIC_F_MEASURE, HEURISTIC_LAPLACE, \
     HEURISTIC_M_ESTIMATE, HEURISTIC_PRECISION, HEURISTIC_RECALL, HEURISTIC_WRA, HeadTypeParameter, \
     LiftFunctionParameter
+from mlrl.seco.learners import SeCoClassifier
 
 from mlrl.util.cli import NONE
 
@@ -30,6 +33,9 @@ class TestSeCoClassifier(ClassificationIntegrationTests):
     @override
     def _create_cmd_builder(self, dataset: str = Dataset.EMOTIONS) -> Any:
         return SeCoClassifierCmdBuilder(dataset=dataset)
+
+    def test_scikit_learn_compatibility(self):
+        check_estimator(SeCoClassifier())
 
     @pytest.mark.parametrize('heuristic', [
         HEURISTIC_ACCURACY,
