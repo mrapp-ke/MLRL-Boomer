a73c4ec19b17ada52704f0b3de92ad56cffb01d3
Michael Rapp <michael.rapp90@googlemail.com>
2026-02-19 21:49:29 +0100

    Add AVX2 and AVX512 specialization for function "simd::differenceWithSubset".

--- cpp/subprojects/common/src/mlrl/common/simd/functions/difference_with_subset_avx512.cpp
+++ cpp/subprojects/common/src/mlrl/common/simd/functions/difference_with_subset_avx512.cpp
@@ -1,4 +1,4 @@
-#include "mlrl/common/simd/functions/difference_with_subset_generic.hpp"
+#include "mlrl/common/simd/functions/difference_with_subset_gather.hpp"
 
 #if SIMD_SUPPORT_ENABLED
 namespace simd {
