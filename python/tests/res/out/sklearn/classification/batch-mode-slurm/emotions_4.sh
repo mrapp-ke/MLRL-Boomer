4756e7a94fb2f105520c20ae322d1964f20ba7db
Michael Rapp <michael.rapp90@googlemail.com>
2026-04-07 01:17:16 +0200

    Adjust expected output of integration tests.

--- python/tests/res/out/sklearn/classification/batch-mode-slurm/emotions_4.sh
+++ python/tests/res/out/sklearn/classification/batch-mode-slurm/emotions_4.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 #SBATCH --job-name=emotions_4
-#SBATCH --output=python/tests/res/tmp/n-estimators_100/max-depth_4/max-leaf-nodes_4/dataset_emotions/results/std.out
-#SBATCH --error=python/tests/res/tmp/n-estimators_100/max-depth_4/max-leaf-nodes_4/dataset_emotions/results/std.err
+#SBATCH --output=python/tests/res/tmp/<worker-id>/n-estimators_100/max-depth_4/max-leaf-nodes_4/dataset_emotions/results/std.out
+#SBATCH --error=python/tests/res/tmp/<worker-id>/n-estimators_100/max-depth_4/max-leaf-nodes_4/dataset_emotions/results/std.err
 #SBATCH --time=2:30:00
 #SBATCH --cpus-per-tasks=1
 
 python -m venv .venv
 . .venv/bin/activate
 python -m pip install mlrl-testbed-sklearn
-mlrl-testbed mlrl.testbed_sklearn --base-dir python/tests/res/tmp --data-dir python/tests/res/data --dataset emotions --estimator RandomForestClassifier --log-level debug --max-depth 4 --max-leaf-nodes 4 --model-save-dir n-estimators_100/max-depth_4/max-leaf-nodes_4/dataset_emotions/models --n-estimators 100 --parameter-save-dir n-estimators_100/max-depth_4/max-leaf-nodes_4/dataset_emotions/parameters --result-dir n-estimators_100/max-depth_4/max-leaf-nodes_4/dataset_emotions/results --save-evaluation true --save-meta-data false
+mlrl-testbed mlrl.testbed_sklearn --base-dir python/tests/res/tmp/<worker-id> --data-dir python/tests/res/data --dataset emotions --estimator RandomForestClassifier --log-level debug --max-depth 4 --max-leaf-nodes 4 --model-save-dir n-estimators_100/max-depth_4/max-leaf-nodes_4/dataset_emotions/models --n-estimators 100 --parameter-save-dir n-estimators_100/max-depth_4/max-leaf-nodes_4/dataset_emotions/parameters --result-dir n-estimators_100/max-depth_4/max-leaf-nodes_4/dataset_emotions/results --save-evaluation true --save-meta-data false
 deactivate
--- python/tests/res/out/sklearn/regression/batch-mode-separate-folds-slurm/atp7d-binary_6.sh
+++ python/tests/res/out/sklearn/regression/batch-mode-separate-folds-slurm/atp7d-binary_6.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #SBATCH --job-name=atp7d-binary_6
-#SBATCH --output=python/tests/res/tmp/n-estimators_50/max-depth_4/max-leaf-nodes_4/dataset_atp7d-binary/results/std_fold-%a.out
-#SBATCH --error=python/tests/res/tmp/n-estimators_50/max-depth_4/max-leaf-nodes_4/dataset_atp7d-binary/results/std_fold-%a.err
+#SBATCH --output=python/tests/res/tmp/<worker-id>/n-estimators_50/max-depth_4/max-leaf-nodes_4/dataset_atp7d-binary/results/std_fold-%a.out
+#SBATCH --error=python/tests/res/tmp/<worker-id>/n-estimators_50/max-depth_4/max-leaf-nodes_4/dataset_atp7d-binary/results/std_fold-%a.err
 #SBATCH --array=1-2
 #SBATCH --time=2:30:00
 #SBATCH --cpus-per-tasks=1
@@ -10,5 +10,5 @@
 python -m venv .venv
 . .venv/bin/activate
 python -m pip install mlrl-testbed-sklearn
-mlrl-testbed mlrl.testbed_sklearn --base-dir python/tests/res/tmp --data-dir python/tests/res/data --data-split cross-validation\{first_fold=${SLURM_ARRAY_TASK_ID},last_fold=${SLURM_ARRAY_TASK_ID},num_folds=2\} --dataset atp7d-binary --estimator RandomForestRegressor --log-level debug --max-depth 4 --max-leaf-nodes 4 --model-save-dir n-estimators_50/max-depth_4/max-leaf-nodes_4/dataset_atp7d-binary/models --n-estimators 50 --parameter-save-dir n-estimators_50/max-depth_4/max-leaf-nodes_4/dataset_atp7d-binary/parameters --problem-type regression --result-dir n-estimators_50/max-depth_4/max-leaf-nodes_4/dataset_atp7d-binary/results --save-evaluation true --save-meta-data false
+mlrl-testbed mlrl.testbed_sklearn --base-dir python/tests/res/tmp/<worker-id> --data-dir python/tests/res/data --data-split cross-validation\{first_fold=${SLURM_ARRAY_TASK_ID},last_fold=${SLURM_ARRAY_TASK_ID},num_folds=2\} --dataset atp7d-binary --estimator RandomForestRegressor --log-level debug --max-depth 4 --max-leaf-nodes 4 --model-save-dir n-estimators_50/max-depth_4/max-leaf-nodes_4/dataset_atp7d-binary/models --n-estimators 50 --parameter-save-dir n-estimators_50/max-depth_4/max-leaf-nodes_4/dataset_atp7d-binary/parameters --problem-type regression --result-dir n-estimators_50/max-depth_4/max-leaf-nodes_4/dataset_atp7d-binary/results --save-evaluation true --save-meta-data false
 deactivate
