4e433eff21adfa1c3ffeedcb51338b69cc0b0132
Michael Rapp <michael.rapp90@googlemail.com>
2026-09-20 22:22:43 +0200

    Add template argument MemoryAllocator to class seco::DecomposableSingleOutputRuleEvaluationFactory.

--- cpp/subprojects/seco/include/mlrl/seco/rule_evaluation/rule_evaluation_decomposable_single.hpp
+++ cpp/subprojects/seco/include/mlrl/seco/rule_evaluation/rule_evaluation_decomposable_single.hpp
@@ -14,7 +14,10 @@ namespace seco {
     /**
      * Allows to create instances of the class `IDecomposableRuleEvaluationFactory` that allow to calculate the
      * predictions of single-output rules, which predict for a single output.
+     *
+     * @tparam MemoryAllocator The type of the memory allocator to be used
      */
+    template<typename MemoryAllocator>
     class DecomposableSingleOutputRuleEvaluationFactory final : public IDecomposableRuleEvaluationFactory {
         private:
 
